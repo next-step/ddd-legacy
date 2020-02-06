9abdbed764e3367ddc8095301f53f6ce6c1b8ac5
ddingcham <qo0op308@gmail.com>
2020-02-06 22:11:40 +0530

    refactor : JUnit 5의 @DisplayName을 사용하여 테스트 메서드의 의도를 한글로 표현한다.
    * CarTest.java
    * RandomMovingStrategyTest.java

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -3,24 +3,28 @@ package racingcar;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
 
     private static final int POSITION_ZERO = 0;
 
+    @DisplayName("자동차의 이름이 5글자가 넘으면 IllegalArgumentException 을 던진다.")
     @Test
     public void construct_when_name_is_over_5() {
         assertThatThrownBy(() -> new Car("다섯글자넘음"))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("자동차의 이름이 null 이면 IllegalArgumentException 을 던진다.")
     @Test
     public void construct_when_name_is_null() {
         assertThatThrownBy(() -> new Car(null))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
+    @DisplayName("자동차가 움직이는 경우, 위치가 1 증가한다.")
     @Test
     public void move_when_movingStrategy_is_movable() {
         Car car = carWith(POSITION_ZERO);
@@ -36,6 +40,7 @@ class CarTest {
             .isEqualTo(POSITION_ZERO + Car.POINT_VARIATION_OF_MOVING);
     }
 
+    @DisplayName("자동차가 움직이는 경우에만 위치가 1 증가한다.")
     @Test
     public void move_when_movingStrategy_is_not_movable() {
         Car car = carWith(POSITION_ZERO);
--- src/test/java/racingcar/RandomMovingStrategyTest.java
+++ src/test/java/racingcar/RandomMovingStrategyTest.java
@@ -7,15 +7,18 @@ import static org.mockito.Mockito.when;
 
 import java.util.Random;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class RandomMovingStrategyTest {
 
+    @DisplayName("무작위 값이 4 보다 작을 경우, 자동차는 움직이지 않는다.")
     @Test
     public void movable_when_random_number_is_3() {
         assertFalse(new RandomMovingStrategy(mockRandomWillReturn(3)).movable());
     }
 
+    @DisplayName("무작위 값이 4 이상일 경우, 자동차는 움직인다.")
     @Test
     public void movable_when_random_number_is_4() {
         assertTrue(new RandomMovingStrategy(mockRandomWillReturn(4)).movable());
