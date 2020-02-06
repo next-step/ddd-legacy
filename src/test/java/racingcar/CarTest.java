3f7f0ef62527f63d4e76431eb7d58e145b943e66
myhency <hency.yeo@gmail.com>
2020-02-06 22:31:39 +0900

    test(racingcar): add car test (#4)

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,40 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class CarTest {
+
+    @DisplayName("자동차를 생성한다.")
+    @Test
+    void create() {
+        final Car car = new Car("james");
+        assertThat(car).isNotNull();
+    }
+
+    @DisplayName("5 글자가 넘는 경우, IllegalArgumentException이 발생한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"aaaaaa","bbbbbb"})
+    @Test
+    void name(final String name) {
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> {
+                    new Car(name);
+                });
+    }
+
+    @DisplayName("자동차가 움직인다.")
+    @Test
+    void move() {
+        final Car car = new Car("james");
+//        car.move(new TestMovingStrategy());
+        car.move(() -> true);
+        assertThat(car.getPosition()).isEqualTo(1);
+
+    }
+}
