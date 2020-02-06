c324e3255aebd8692aaaa3e0463061e5125792ec
Geonguk Han <umanking@gmail.com>
2020-02-06 22:19:05 +0900

    test(racingcar): add car test

---
+++ src/main/java/racingcar/MovingStrategy.java
@@ -0,0 +1,12 @@
+package racingcar;
+
+import jdk.nashorn.internal.objects.annotations.Function;
+
+/**
+ * @author Geonguk Han
+ * @since 2020-02-06
+ */
+
+public interface MovingStrategy {
+    boolean movable();
+}
