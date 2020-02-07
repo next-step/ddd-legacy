2bcade6ade4251fbece07245734fe282eab2d54d
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-02-07 13:05:23 +0900

    feat(StringCalculator) add numbers when get custom divider
    - 커스텀 구분자가 들어왔을 때 숫자의 합을 구하는 기능을 구현했습니다

--- src/test/java/calculator/StringCalculatorTests.java
+++ src/test/java/calculator/StringCalculatorTests.java
@@ -34,6 +34,14 @@ class StringCalculatorTests {
         assertThat(result).isEqualTo(0);
     }
 
+    @DisplayName("정상적인 문자열이 입력됐을 때 추출된 숫자 콜렉션의 총합을 계산")
+    @ParameterizedTest
+    @ValueSource(strings = {"112:3:5,523:123,1", "112:3:5:523:123:1", "112,3,5,523,123,1"})
+    void addHappyPath(String input) {
+        int result = stringCalculator.add(input);
+        assertThat(result).isEqualTo(767);
+    }
+
     @DisplayName("입력값 중 기본 구분자(:와 쉼표)를 제외한 숫자만 골라낸 콜렉션 출력")
     @ParameterizedTest
     @ValueSource(strings = {"112:3:5,523:123,1", "112:3:5:523:123:1", "112,3,5,523,123,1"})
@@ -43,10 +51,27 @@ class StringCalculatorTests {
         assertThat(numbers.get(3)).isEqualTo(523);
     }
 
-    @DisplayName("정상적인 문자열이 입력됐을 때 추출된 숫자 콜렉션의 총합을 계산")
+    @DisplayName("커스텀 구분자가 있는 경우 커스텀 구분자 반환")
     @ParameterizedTest
-    @ValueSource(strings = {"112:3:5,523:123,1", "112:3:5:523:123:1", "112,3,5,523,123,1"})
-    void addHappyPath(String input) {
+    @ValueSource(strings = {"//!\n112!3:5,523:123!1"})
+    void getCustomDivider(String input) {
+        String customDivider = stringCalculator.getCustomDivider(input);
+        assertThat(customDivider).isEqualTo("!");
+    }
+
+    @DisplayName("커스텀 구분자가 있는 경우 숫자 콜렉션 추출")
+    @ParameterizedTest
+    @ValueSource(strings = {"//!\n112!3:5,523:123!1"})
+    void getNumbersWhenHasCustomDivider(String input) {
+        List<Integer> numbers = stringCalculator.extractNumbers(input);
+        assertThat(numbers.get(0)).isEqualTo(112);
+        assertThat(numbers.get(3)).isEqualTo(523);
+    }
+
+    @DisplayName("커스텀 구분자가 포함된 정상적인 문자열이 입력됐을 때 추출된 숫자 콜렉션의 총합을 계산")
+    @ParameterizedTest
+    @ValueSource(strings = {"112:3:5,523:123,1", "112:3:5:523:123:1", "112,3,5,523,123,1", "//!\n112!3:5,523:123!1"})
+    void addManyCases(String input) {
         int result = stringCalculator.add(input);
         assertThat(result).isEqualTo(767);
     }
