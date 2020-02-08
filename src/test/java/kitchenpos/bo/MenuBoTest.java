c4c935569316b0eb6a648319657834846160c52a
james <hency.yeo@gmail.com>
2020-02-08 23:01:00 +0900

    test(MenuBoTest) list() method test 구현

--- src/test/java/kitchenpos/bo/MenuBoTest.java
+++ src/test/java/kitchenpos/bo/MenuBoTest.java
@@ -18,13 +18,16 @@ import org.mockito.MockitoAnnotations;
 import org.mockito.invocation.InvocationOnMock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.stubbing.Answer;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Bean;
 
 import javax.persistence.EntityManager;
 import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -35,18 +38,11 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
-@SpringBootTest
 class MenuBoTest {
-    @Mock private Menu menu;
-    @Mock private MenuGroup menuGroup;
-    @Mock private MenuProduct menuProduct;
-    @Mock private Product product;
+    @InjectMocks private MenuBo menuBo;
     @Mock private MenuDao menuDao;
-    @Mock private MenuGroupDao menuGroupDao;
-    @Mock private ProductDao productDao;
     @Mock private MenuProductDao menuProductDao;
 
-    @InjectMocks private MenuBo menuBo;
 
     @DisplayName("각 메뉴에 이름,가격을 설정할 수 있다.")
     @Test
@@ -57,18 +53,6 @@ class MenuBoTest {
     @DisplayName("메뉴 목록을 볼 수 있다.")
     @Test
     public void list() {
-//        when(menu.getName()).thenReturn("후라이드치킨");
-//        assertTrue("후라이드치킨".equals(menu.getName()));
-//
-//
-//        Menu menu1 = new Menu();
-//        menu1.setPrice(new BigDecimal(16000));
-//        menu1.setName("test");
-//        when(menuDao.findAll()).thenReturn(new ArrayList<Menu>() {
-//            { this.add(menu1); }
-//        });
-//        assertTrue(menuBo.list().get(0).getName() == "test");
-
         MenuProduct menuProduct = new MenuProduct();
         menuProduct.setMenuId(1L);
         menuProduct.setProductId(1L);
@@ -85,10 +69,11 @@ class MenuBoTest {
         menu.setId(1L);
         List<Menu> menuList = new ArrayList<>();
         menuList.add(menu);
-
-        when(menuProductDao.findAllByMenuId(1L)).thenReturn(menuProductList);
         when(menuDao.findAll()).thenReturn(menuList);
+        when(menuProductDao.findAllByMenuId(1L)).thenReturn(menuProductList);
 
-        assertThat(menuBo.list()).isEqualTo(menuList);
+        List<Menu> result = menuBo.list();
+
+        assertThat(result.get(0).getName()).isEqualTo("test");
     }
 }
