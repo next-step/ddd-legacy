ff0f3937863e8b395ed83cd18cc9d165f8998ba2
ddingcham <qo0op308@gmail.com>
2020-02-08 17:56:45 +0900

    feat : 빈 문자열 또는 null을 입력할 경우 0을 반환해야 한다.

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,13 @@
+package calculator;
+
+import org.apache.logging.log4j.util.Strings;
+
+public class StringCalculator {
+
+    public int add(final String text) {
+        if (Strings.isBlank(text)) {
+            return 0;
+        }
+        return 0;
+    }
+}
