52ade8716f027f05af6696ff564669ca955d97ca
dongchul <sah3122@naver.com>
2020-02-09 15:40:54 +0900

    test(stringcalulator): add calculate test case
    커스텀 구분자 입력, 빈문자. null 입력, Exception 반환 테스트

--- src/test/java/calculator/domain/StringCalculatorTest.java
+++ src/test/java/calculator/domain/StringCalculatorTest.java
@@ -2,10 +2,13 @@ package calculator.domain;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class StringCalculatorTest {
 
@@ -22,5 +25,28 @@ class StringCalculatorTest {
     void calculate(String value) {
         int result = calculator.add(value);
         assertThat(result).isEqualTo(6);
+    }
+
+    @DisplayName("커스텀 구분자 테스트")
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\n1;2;3", "//!\n1!2!3", "//@\n1@2@3"})
+    void customDelimiter(String value) {
+        int result = calculator.add(value);
+        assertThat(result).isEqualTo(6);
+    }
+
+    @ParameterizedTest
+    @DisplayName("빈문자열 또는 Null 입력시 0 리턴")
+    @NullAndEmptySource
+    void emptyAndNullInput(String value) {
+        int result = calculator.add(value);
+        assertThat(result).isEqualTo(0);
+    }
+
+    @ParameterizedTest
+    @DisplayName("음수 또는 문자열 입력시 RuntimeException")
+    @ValueSource(strings = {"1:-2:3", "a:b:c", "1:b:-3"})
+    void calculateFail(String value) {
+        assertThrows(RuntimeException.class, () -> calculator.add(value));
     }
 }
