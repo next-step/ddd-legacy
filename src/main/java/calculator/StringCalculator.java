ec1215b92b5bf6d4d97400e519c28b216f2690c1
Hyeonah <hyeonah@wantedlab.com>
2020-02-09 15:48:25 +0900

    feat: 정규식 패턴으로 구분자 가져와 숫자 합 구하기

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,19 +1,24 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
+    final Pattern pattern = Pattern.compile("//(.)\n(.*)");
+
     public int add(String text) {
 
         if (isValidateString(text)) {
             return 0;
         }
 
-        if (isOneLength(text)) {
-            return Integer.parseInt(text);
-        }
-
         int sum = 0;
-        String[] values = text.split(",|:");
-        for (String value : values){
+        String delimiter = getDelimiter(text);
+        String str = getText(text);
+        String[] values = str.split(delimiter);
+
+        for (String value : values) {
+
             sum += Integer.valueOf(value);
         }
 
@@ -21,12 +26,29 @@ public class StringCalculator {
 
     }
 
-    private boolean isOneLength(String text) {
-        return text.length() == 1;
+    private String getText(String str) {
+        Matcher m = pattern.matcher(str);
+        if (m.find()) {
+            return m.group(2);
+        }
+        return str;
     }
 
+    private String getDelimiter(String str) {
+
+        Matcher m = pattern.matcher(str);
+        if (m.find()) {
+            return m.group(1);
+        }
+        return ",|:";
+    }
+
+
     private boolean isValidateString(String str) {
-        if (str == null || str.isEmpty()) {
+        if (str == null) {
+            return true;
+        }
+        if (str.isEmpty()) {
             return true;
         }
         return false;
