15c3b13a38ed88e2d18371363ccf8edcddd31b1b
Jaekwon Ha <jaekwon.ha@sk.com>
2020-02-09 15:04:44 +0900

    test(StringCalculator): test-oneNumber use @Test instead of @ParameterizedTest

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -2,6 +2,7 @@ package calculator;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -25,10 +26,9 @@ class StringCalculatorTest {
     }
 
     @DisplayName(value = "숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
-    @ParameterizedTest
-    @ValueSource(strings = {"1"})
-    void oneNumber(final String text) {
-        assertThat(calculator.add(text)).isSameAs(Integer.parseInt(text));
+    @Test
+    void oneNumber() {
+        assertThat(calculator.add("1")).isSameAs(Integer.parseInt("1"));
     }
 
     @DisplayName(value = "숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
