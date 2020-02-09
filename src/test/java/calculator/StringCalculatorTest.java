2fbd3ad8a571377738b2ac5abca3464403a2f900
Hyeonah <hyeonah@wantedlab.com>
2020-02-10 00:47:58 +0900

    test: add 함수 calculate로 변경함에따라test 모듈도 calculate로 변경

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -22,7 +22,7 @@ class StringCalculatorTest {
     @ParameterizedTest
     @NullAndEmptySource
     void emptyOrNull(final String text) {
-        assertThat(calculator.add(text)).isZero();
+        assertThat(calculator.calculate(text)).isZero();
     }
 
 
@@ -30,34 +30,34 @@ class StringCalculatorTest {
     @ParameterizedTest
     @ValueSource(strings = {"1"})
     void oneNumber(final String text) {
-        assertThat(calculator.add(text)).isSameAs(Integer.parseInt(text));
+        assertThat(calculator.calculate(text)).isSameAs(Integer.parseInt(text));
     }
 
     @DisplayName(value = "숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
     @ParameterizedTest
     @ValueSource(strings = {"1,2"})
     void twoNumbers(final String text) {
-        assertThat(calculator.add(text)).isSameAs(3);
+        assertThat(calculator.calculate(text)).isSameAs(3);
     }
 
     @DisplayName(value = "구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.")
     @ParameterizedTest
     @ValueSource(strings = {"1,2:3"})
     void colons(final String text) {
-        assertThat(calculator.add(text)).isSameAs(6);
+        assertThat(calculator.calculate(text)).isSameAs(6);
     }
 
     @DisplayName(value = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
     @ParameterizedTest
     @ValueSource(strings = {"//;\n1;2;3"})
     void customDelimiter(final String text) {
-        assertThat(calculator.add(text)).isSameAs(6);
+        assertThat(calculator.calculate(text)).isSameAs(6);
     }
 
     @DisplayName(value = "문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리를 한다.")
     @Test
     void negative() {
         assertThatExceptionOfType(RuntimeException.class)
-                .isThrownBy(() -> calculator.add("-1"));
+                .isThrownBy(() -> calculator.calculate("-1"));
     }
 }
