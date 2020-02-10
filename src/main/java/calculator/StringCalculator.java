edf8a44f9a8dcb742cebc376aeb60f1dd439fdad
naheenosaur <naheenosaur@gmail.com>
2020-02-10 12:42:15 +0900

    feat(StringCalculator) : 사용자 지정 구분자를 추가한다.

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -2,15 +2,26 @@ package calculator;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 public class StringCalculator {
-    String defaultPattern = "[,:]";
+    String defaultPattern = ",|:";
+    String delimiterPattern = "//(.)\n(.*)";
 
     public List<String> separateInputs(String input) {
         return Arrays.stream(input.trim()
                 .split(defaultPattern))
                 .map(String::trim)
                 .collect(Collectors.toList());
+    }
+
+    public String findDelimiter(String input) {
+        Matcher delimiterMatcher = Pattern.compile(delimiterPattern).matcher(input);
+        if (delimiterMatcher.find()) {
+            return defaultPattern + "|" + delimiterMatcher.group(1);
+        }
+        return defaultPattern;
     }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -13,4 +13,10 @@ class StringCalculatorTest {
     void findDefaultSeparator() {
         assertThat(calculator.separateInputs("1,2,3")).containsExactly("1", "2", "3");
     }
+
+    @Test
+    @DisplayName("지정 구분자를 포함한 구분자 정규식을 찾는다")
+    void getRegex() {
+        assertThat(calculator.findDelimiter("//$\n1,2,3")).isEqualTo(",|:|$");
+    }
 }
