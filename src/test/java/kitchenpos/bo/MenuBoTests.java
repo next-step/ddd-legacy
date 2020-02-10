741cf17f1e45d60181d2a725f75bf02906c404c2
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-02-10 22:34:23 +0900

    test(MenuBo) - add unit test code for MenuBo
    - MenuBo의 정상 동작을 확인하기 위해 findAll, create 메서드에 대한 해피 패스를 구현했습니다.
    - 예외 상황이 정상적으로 실행되는지 확인하기 위해 아래와 같이 create 메서드의 상황을 추가로 테스트했습니다.
        - 0보다 작은 가격으로 메뉴 생성 시도
        - 메뉴에 속한 메뉴 상품 가격의 총합보다 높은 가격으로 메뉴 생성 시도
        - 메뉴 그룹에 속하지 않은 메뉴로 메뉴 생성 시도
        - 없는 상품으로 메뉴 생성 시도

--- src/test/java/kitchenpos/bo/MenuBoTests.java
+++ src/test/java/kitchenpos/bo/MenuBoTests.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -18,8 +20,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -45,35 +49,17 @@ class MenuBoTests {
 
     @BeforeEach
     public void defaultSetup() {
+        // Menu { id: 1L, menuGroupId: 1L, price: 16000, name: testMenu, menuProducts: mockMenuProducts }
         setupDefaultMenu();
+
+        // MenuProduct { id: 1L, menuId: 1L, quantity: 100, productId: 2L }
         setupDefaultMenuProduct();
+
+        // Product { id: 2L, name: testProduct, price: 16000 }
         setupDefaultProduct();
+
         mockMenus.add(mockMenu);
         mockMenuProducts.add(mockMenuProduct);
-    }
-
-    private void setupDefaultMenu() {
-        // Menu { id: 1L, menuGroupId: 1L, price: 16000, name: testMenu, menuProducts: mockMenuProducts }
-        mockMenu.setId(1L);
-        mockMenu.setMenuGroupId(1L);
-        mockMenu.setPrice(BigDecimal.valueOf(16000));
-        mockMenu.setName("testMenu");
-        mockMenu.setMenuProducts(mockMenuProducts);
-    }
-
-    private void setupDefaultMenuProduct() {
-        // MenuProduct { id: 1L, menuId: 1L, quantity: 100, productId: 2L }
-        mockMenuProduct.setSeq(1L);
-        mockMenuProduct.setMenuId(1L);
-        mockMenuProduct.setQuantity(100);
-        mockMenuProduct.setProductId(2L);
-    }
-
-    private void setupDefaultProduct() {
-        // Product { id: 2L, name: testProduct, price: 16000 }
-        mockProduct.setId(2L);
-        mockProduct.setName("testProduct");
-        mockProduct.setPrice(BigDecimal.valueOf(16000));
     }
 
     @DisplayName("메뉴 전체 조회 시 메뉴에 맞는 메뉴 상품들을 가져오는지 확인")
@@ -86,5 +72,76 @@ class MenuBoTests {
 
         assertThat(menuList.get(0).getId()).isEqualTo(1L);
         assertThat(menuList.get(0).getMenuProducts().get(0).getMenuId()).isEqualTo(1L);
+    }
+
+    @DisplayName("정상적인 값들로 메뉴 생성 시도 성공")
+    @Test
+    public void createMenuSuccess() {
+        given(menuGroupDao.existsById(1L)).willReturn(true);
+        given(productDao.findById(2L)).willReturn(Optional.ofNullable(mockProduct));
+        given(menuDao.save(mockMenu)).willReturn(mockMenu);
+        given(menuProductDao.save(mockMenuProduct)).willReturn(mockMenuProduct);
+
+        Menu saved = menuBo.create(mockMenu);
+
+        assertThat(saved.getId()).isEqualTo(1L);
+        assertThat(saved.getMenuProducts().get(0).getMenuId()).isEqualTo(1L);
+    }
+
+    @DisplayName("0보다 작은 가격으로 메뉴 생성 시도 시 실패")
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0})
+    public void createMenuFailWithInvalidPrice(int price) {
+        mockMenu.setPrice(BigDecimal.valueOf(price));
+
+        assertThatThrownBy(() -> menuBo.create(mockMenu)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("메뉴에 속한 메뉴 상품 가격의 총합보다 높은 가격으로 메뉴 생성 시도 시 실패")
+    @ParameterizedTest
+    @ValueSource(ints = {1000000, 100000000, 777777})
+    public void createMenuFailWhenMenuPriceIsBiggerThanPriceSum(int price) {
+        mockMenu.setPrice(BigDecimal.valueOf(price));
+        given(menuGroupDao.existsById(1L)).willReturn(true);
+        given(productDao.findById(2L)).willReturn(Optional.ofNullable(mockProduct));
+
+        assertThatThrownBy(() -> menuBo.create(mockMenu)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("메뉴 그룹에 속하지 않은 메뉴로 생성 시도 시 실패")
+    @Test
+    public void createMenuFailWithNotInMenuGroup() {
+        given(menuGroupDao.existsById(1L)).willReturn(false);
+
+        assertThatThrownBy(() -> menuBo.create(mockMenu)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("없는 상품으로 메뉴 생성 시도 시 실패")
+    @Test
+    public void createMenuFailWithNonExistProduct() {
+        given(menuGroupDao.existsById(1L)).willReturn(true);
+
+        assertThatThrownBy(() -> menuBo.create(mockMenu)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private void setupDefaultMenu() {
+        mockMenu.setId(1L);
+        mockMenu.setMenuGroupId(1L);
+        mockMenu.setPrice(BigDecimal.valueOf(16000));
+        mockMenu.setName("testMenu");
+        mockMenu.setMenuProducts(mockMenuProducts);
+    }
+
+    private void setupDefaultMenuProduct() {
+        mockMenuProduct.setSeq(1L);
+        mockMenuProduct.setMenuId(1L);
+        mockMenuProduct.setQuantity(10);
+        mockMenuProduct.setProductId(2L);
+    }
+
+    private void setupDefaultProduct() {
+        mockProduct.setId(2L);
+        mockProduct.setName("testProduct");
+        mockProduct.setPrice(BigDecimal.valueOf(16000));
     }
 }
