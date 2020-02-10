2296ecb3fb2baab22899fecc2e350dfd6fbcc301
dongchul <sah3122@naver.com>
2020-02-10 23:45:07 +0900

    feat(StringCalculator): Set Pattern & Use CalculatorNumber

--- src/main/java/calculator/domain/StringCalculator.java
+++ src/main/java/calculator/domain/StringCalculator.java
@@ -9,27 +9,26 @@ import java.util.stream.Collectors;
 
 public class StringCalculator {
 
-    private static final String CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
     private String delimiter = ",|:";
-
 
     public StringCalculator() {
     }
 
     public int add(String inputString) {
-        if(StringUtils.isEmpty(inputString)) return 0;
+        if(StringUtils.isEmpty(inputString)) {
+            return 0;
+        }
 
         String[] tokens = getTokens(inputString);
 
         return Arrays.stream(tokens)
-                .peek(token -> {
-                    if (isNotNumber(token) || isNegativeNumber(token)) throw new RuntimeException("음수 또는 문자열은 계산할 수 없습니다.");
-                })
-                .collect(Collectors.summingInt(Integer::parseInt));
+                .map(token -> new CalculatorNumber(token))
+                .collect(Collectors.summingInt(CalculatorNumber::parse));
     }
 
     private String[] getTokens(String inputString) {
-        Matcher m = Pattern.compile(CUSTOM_DELIMITER_PATTERN).matcher(inputString);
+        Matcher m = CUSTOM_DELIMITER_PATTERN.matcher(inputString);
         if (m.find()) {
             addDelimeter(m.group(1));
             return m.group(2).split(delimiter);
@@ -39,18 +38,5 @@ public class StringCalculator {
 
     private void addDelimeter(String substring) {
         this.delimiter += "|" + substring;
-    }
-
-    private boolean isNegativeNumber(String number) {
-        return Integer.parseInt(number) < 0 ? true : false;
-    }
-
-    private boolean isNotNumber(String number) {
-        try {
-            Integer.parseInt(number);
-        } catch (NumberFormatException e) {
-            return true;
-        }
-        return false;
     }
 }
