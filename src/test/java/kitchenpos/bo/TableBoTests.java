2b6d391f4b0b860cde99ed8c33e33f44cb61da5f
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-02-11 21:15:32 +0900

    Test(TableBo) - add error case in create OrderTable
    - 존재하지 않는 주문 테이블의 공석 여부 변경 시도에 대한 케이스를 추가했습니다.
    - 테이블 그룹에 속한 주문 테이블의 공석 여부 변경 시도에 대한 케이스를 추가했습니다.
    - 조리중이거나 식사중인 주문 테이블의 공석 여부 변경 시도에 대한 케이스를 추가했습니다.

--- src/test/java/kitchenpos/bo/TableBoTests.java
+++ src/test/java/kitchenpos/bo/TableBoTests.java
@@ -2,6 +2,7 @@ package kitchenpos.bo;
 
 import kitchenpos.dao.OrderDao;
 import kitchenpos.dao.OrderTableDao;
+import kitchenpos.model.OrderStatus;
 import kitchenpos.model.OrderTable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -12,9 +13,12 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -59,10 +63,51 @@ class TableBoTests {
         assertThat(orderTables).hasSize(1);
     }
 
+    @DisplayName("테이블 그룹에 속하지 않고 식사 상태가 완료인 주문 테이블의 공석 여부 변경 시도 성공")
+    @Test
+    public void changeOrderTableEmptySuccess() {
+        OrderTable emptyTable = new OrderTable();
+        emptyTable.setEmpty(true);
+
+        given(orderTableDao.findById(1L)).willReturn(Optional.ofNullable(mockOrderTable));
+        given(orderTableDao.save(mockOrderTable)).willReturn(mockOrderTable);
+
+        OrderTable orderTable = tableBo.changeEmpty(1L, emptyTable);
+
+        assertThat(orderTable.isEmpty()).isEqualTo(true);
+    }
+
+    @DisplayName("존재하지 않는 주문 테이블의 공석 여부 변경 시도 시 실패")
+    @Test
+    public void changeOrderTableEmptyFailWithNotExistOrderTable() {
+        assertThatThrownBy(() -> tableBo.changeEmpty(1L, new OrderTable()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("테이블 그룹에 속한 주문 테이블의 공석 여부 변경 시도 시 실패")
+    @Test
+    public void changeOrderTableEmptyFailWhenInTableGroup() {
+        mockOrderTable.setTableGroupId(33L);
+        given(orderTableDao.findById(1L)).willReturn(Optional.ofNullable(mockOrderTable));
+
+        assertThatThrownBy(() -> tableBo.changeEmpty(1L, new OrderTable()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("조리중이거나 식사중인 주문 테이블의 공석 여부 변경 시도 시 실패")
+    @Test
+    public void changeOrderTableEmptyFailWhenInCookingOrMeal() {
+        List<String> statuses = Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name());
+        given(orderTableDao.findById(1L)).willReturn(Optional.ofNullable(mockOrderTable));
+        given(orderDao.existsByOrderTableIdAndOrderStatusIn(1L, statuses)).willReturn(true);
+
+        assertThatThrownBy(() -> tableBo.changeEmpty(1L, new OrderTable()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
     private void setupOrderTable() {
         mockOrderTable.setId(1L);
         mockOrderTable.setEmpty(false);
-        mockOrderTable.setTableGroupId(2L);
         mockOrderTable.setNumberOfGuests(3);
     }
 }
