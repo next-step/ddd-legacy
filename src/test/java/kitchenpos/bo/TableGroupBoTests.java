08629248bbdc5101e3186a64e67970ebdb967203
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-02-11 22:13:22 +0900

    test(TableGroupBo) - add fail case when creating TabelGroup
    - 한 개 이하의 주문 테이블로 테이블 그룹 생성 시도 케이스를 추가했습니다.
    - 존재하지 않는 주문 테이블로 테이블 그룹 생성 시도 케이스를 추가했습니다.
    - 이미 테이블 그룹에 속한 주문 테이블로 테이블 그룹 생성 시도 케이스를 추가했습니다.

--- src/test/java/kitchenpos/bo/TableGroupBoTests.java
+++ src/test/java/kitchenpos/bo/TableGroupBoTests.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -16,9 +18,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
@@ -60,6 +65,48 @@ class TableGroupBoTests {
         assertThat(tableGroup.getCreatedDate().getDayOfMonth()).isEqualTo(LocalDateTime.now().getDayOfMonth());
         assertThat(tableGroup.getOrderTables().get(0).getTableGroupId()).isEqualTo(1L);
         assertThat(tableGroup.getOrderTables().get(0).isEmpty()).isEqualTo(false);
+    }
+
+    @DisplayName("한 개 이하의 주문 테이블로 테이블 그룹 생성 시도 시 실패")
+    @ParameterizedTest
+    @MethodSource("provideInvalidOrderTables")
+    public void createTableGroupFailWithInvalidOrderTables(List<OrderTable> orderTables) {
+        mockTableGroup.setOrderTables(orderTables);
+
+        assertThatThrownBy(() -> tableGroupBo.create(mockTableGroup)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    private static Stream<List<OrderTable>> provideInvalidOrderTables() {
+        return Stream.of(
+                new ArrayList<>(),
+                Collections.singletonList(new OrderTable())
+        );
+    }
+
+    @DisplayName("존재하지 않는 주문 테이블로 테이블 그룹 생성 시도 시 실패")
+    @Test
+    public void createTableGroupFailWithNotExistOrderTable() {
+        given(orderTableDao.findAllByIdIn(Arrays.asList(1L, 2L))).willReturn(new ArrayList<>());
+
+        assertThatThrownBy(() -> tableGroupBo.create(mockTableGroup)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("비어있지 않은 주문 테이블로 테이블 그룹 생성 시도 시 실패")
+    @Test
+    public void createTableGroupFailWithNotEmptyOrderTable() {
+        mockOnePeopleOrderTable.setEmpty(false);
+        given(orderTableDao.findAllByIdIn(Arrays.asList(1L, 2L))).willReturn(mockOrderTables);
+
+        assertThatThrownBy(() -> tableGroupBo.create(mockTableGroup)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("이미 테이블 그룹에 속한 주문 테이블로 테이블 그룹 생성 시도 시 실패")
+    @Test
+    public void createTableGroupFailWithAlreadyInTableGroup() {
+        mockOnePeopleOrderTable.setTableGroupId(33L);
+        given(orderTableDao.findAllByIdIn(Arrays.asList(1L, 2L))).willReturn(mockOrderTables);
+
+        assertThatThrownBy(() -> tableGroupBo.create(mockTableGroup)).isInstanceOf(IllegalArgumentException.class);
     }
 
     private void setupTableGroup() {
