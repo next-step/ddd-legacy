852a7200ae6ab9b0002918e2b6c4eec80d888741
james <hency.yeo@gmail.com>
2020-02-12 22:54:42 +0900

    test(주문(Orders) 요구사항 반영) - [ ] 테이블에 서빙되는 메뉴의 집합을 주문이라고 부른다. - [ ] 주문을 생성할 수 있다.     - [ ] 주문메뉴가 반드시 있어야 한다.     - [ ] 매장에서 판매하지 않는 메뉴를 주문할 수 없다.     - [ ] 주문할 때 테이블을 반드시 지정해야 한다.     - [ ] 주문이 들어오면 주문상태가 조리중(COOKING)으로 된다. - [ ] 주문 목록을 볼 수 있다. - [ ] 주문의 상태를 변경할 수 있다. (조리중: COOKING, 고객이 식사중인 주문: MEAL, 식사가 끝나고 결재도 끝난 주문: COMPLETION)     - [ ] 이미 생성된 주문의 상태만 변경할 수 있다.     - [ ] 식사가 끝나고 결재도 끝난 주문은 상태를 변경할 수 없다. - create() 부분은 구현하지 못함

--- src/test/java/kitchenpos/bo/OrderBoTest.java
+++ src/test/java/kitchenpos/bo/OrderBoTest.java
@@ -5,6 +5,7 @@ import kitchenpos.dao.OrderDao;
 import kitchenpos.dao.OrderLineItemDao;
 import kitchenpos.dao.OrderTableDao;
 import kitchenpos.model.*;
+import org.aspectj.weaver.ast.Or;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -16,12 +17,13 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.ArgumentMatchers.anyLong;
+import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -40,8 +42,10 @@ class OrderBoTest {
     private OrderLineItem orderLineItem;
     private List<OrderLineItem> orderLineItemList;
     private Order order;
+    private List<Order> orderList;
     private OrderTable orderTable;
     private Optional<OrderTable> optionalOrderTable;
+    private Optional<Order> optionalOrder;
 
     @BeforeEach
     void setup() {
@@ -79,6 +83,9 @@ class OrderBoTest {
         order.setOrderStatus(String.valueOf(OrderStatus.MEAL));
         order.setOrderTableId(1L);
 
+        orderList = new ArrayList<>();
+        orderList.add(order);
+
         orderTable = new OrderTable();
         orderTable.setEmpty(false);
         orderTable.setId(1L);
@@ -86,28 +93,114 @@ class OrderBoTest {
         orderTable.setTableGroupId(1L);
 
         optionalOrderTable = Optional.of(orderTable);
+        optionalOrder = Optional.of(order);
     }
 
-    @DisplayName("주문을 생성한다.")
+    @DisplayName("주문을 생성할 수 있다.")
     @Test
     void create() {
         when(menuDao.countByIdIn(anyList()))
                 .thenReturn(Long.valueOf(orderLineItemList.size()));
         when(orderTableDao.findById(anyLong()))
                 .thenReturn(Optional.of(orderTable));
+        when(orderDao.save(any(Order.class)))
+                .thenReturn(order);
+        when(orderLineItemDao.save(any(OrderLineItem.class)))
+                .thenReturn(orderLineItem);
 
         Order result = orderBo.create(order);
         assertThat(result.getId()).isEqualTo(1L);
 
     }
 
+    @DisplayName("주문메뉴가 반드시 있어야 한다.")
+    @Test
+    void createMustHaveOrderItem() {
+        orderLineItemList = null;
+        Throwable thrown = catchThrowable(() ->{
+            orderBo.create(order);
+        });
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    @DisplayName("매장에서 판매하지 않는 메뉴를 주문할 수 없다.")
+    @Test
+    void createMustHaveItemsServed() {
+        when(menuDao.countByIdIn(anyList()))
+                .thenReturn(Long.valueOf(orderLineItemList.size() + 1));
+        Throwable thrown = catchThrowable(() ->{
+            orderBo.create(order);
+        });
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("주문할 때 테이블을 반드시 지정해야 한다.")
+    @Test
+    void createMustHaveDesignatedTable() {
+        optionalOrderTable = Optional.empty();
+        Throwable thrown = catchThrowable(() ->{
+            orderBo.create(order);
+        });
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("주문이 들어오면 주문상태가 조리중(COOKING)으로 된다.")
+    @Test
+    void createThenChangeStatusToCooking() {
+        when(menuDao.countByIdIn(anyList()))
+                .thenReturn(Long.valueOf(orderLineItemList.size()));
+        when(orderTableDao.findById(anyLong()))
+                .thenReturn(Optional.of(orderTable));
+        when(orderDao.save(any(Order.class)))
+                .thenReturn(order);
+        Order result = orderBo.create(order);
+        assertThat(result.getOrderStatus()).isEqualTo(OrderStatus.COOKING.name());
+    }
+
     @DisplayName("주문 목록을 볼 수 있다.")
     @Test
     void list() {
+        when(orderDao.findAll())
+                .thenReturn(orderList);
+        when(orderLineItemDao.findAllByOrderId(anyLong()))
+                .thenReturn(orderLineItemList);
+        List<Order> result = orderBo.list();
+        assertThat(result.size()).isEqualTo(orderList.size());
     }
 
-    @DisplayName("주문은 상태를 가진다. (고객이 식사중인 주문: MEAL, 결재가 끝난 주문: COMPLETE)")
+    @DisplayName("주문의 상태를 변경할 수 있다.")
     @Test
     void changeOrderStatus() {
+        when(orderDao.findById(anyLong()))
+                .thenReturn(optionalOrder);
+        when(orderDao.save(any(Order.class)))
+                .thenReturn(order);
+        when(orderLineItemDao.findAllByOrderId(anyLong()))
+                .thenReturn(orderLineItemList);
+        Order result = orderBo.changeOrderStatus(order.getId(), order);
+        assertThat(result.getOrderStatus()).isNotEqualTo(OrderStatus.COMPLETION);
     }
+
+    @DisplayName("이미 생성된 주문의 상태만 변경할 수 있다.")
+    @Test
+    void changeOrderStatusOrderedOne() {
+        optionalOrder = Optional.empty();
+        Throwable thrown = catchThrowable(() ->{
+            orderBo.changeOrderStatus(order.getId(), order);
+        });
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("식사가 끝나고 결재도 끝난 주문은 상태를 변경할 수 없다.")
+    @Test
+    void changeOrderStatusOfCompletedOrders() {
+        order.setOrderStatus(OrderStatus.COMPLETION.name());
+        Throwable thrown = catchThrowable(() ->{
+            orderBo.changeOrderStatus(order.getId(), order);
+        });
+        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 }
