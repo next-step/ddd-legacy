8f226aaf002e1f5550d374c2d9191e67766e9c1b
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-02-12 21:57:44 +0900

    test(MenuRestController) - add GET menu collection happy path

--- src/test/java/kitchenpos/controller/MenuRestControllerTests.java
+++ src/test/java/kitchenpos/controller/MenuRestControllerTests.java
@@ -11,12 +11,15 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(MenuRestController.class)
 class MenuRestControllerTests {
@@ -28,11 +31,14 @@ class MenuRestControllerTests {
     private MockMvc mockMvc;
 
     private static Menu mockCreatedMenu;
+    private static List<Menu> mockMenus = new ArrayList<>();
 
     @BeforeAll
     public static void setup() {
         mockCreatedMenu = new Menu();
         mockCreatedMenu.setId(1L);
+
+        mockMenus.add(new Menu());
     }
 
     @DisplayName("알맞는 메뉴로 POST 요청 시 성공(201)")
@@ -55,6 +61,18 @@ class MenuRestControllerTests {
                         "}"))
                 .andExpect(status().isCreated())
                 .andExpect(header().stringValues("Location", "/api/menus/1"))
+        ;
+    }
+
+    @DisplayName("GET 메뉴 콜렉션 요청 시 성공(200")
+    @Test
+    public void getMenusSuccess() throws Exception {
+        given(menuBo.list()).willReturn(mockMenus);
+
+        mockMvc.perform(get("/api/menus"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("[")))
+                .andExpect(content().string(containsString("price")))
         ;
     }
 }
