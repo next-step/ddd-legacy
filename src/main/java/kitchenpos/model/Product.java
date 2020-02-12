5b76fc1415fb059b48c7e27c817d021b39ea66df
zingoworks <zingoworks@gmail.com>
2020-02-12 11:54:14 +0900

    test(productBo): add test cases

--- src/main/java/kitchenpos/model/Product.java
+++ src/main/java/kitchenpos/model/Product.java
@@ -1,6 +1,7 @@
 package kitchenpos.model;
 
 import java.math.BigDecimal;
+import java.util.Objects;
 
 public class Product {
 
@@ -30,5 +31,24 @@ public class Product {
 
     public void setPrice(final BigDecimal price) {
         this.price = price;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof Product)) {
+            return false;
+        }
+        Product product = (Product) o;
+        return Objects.equals(id, product.id) &&
+            Objects.equals(name, product.name) &&
+            Objects.equals(price, product.price);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, price);
     }
 }
---
+++ src/test/java/kitchenpos/bo/ProductBoTest.java
@@ -0,0 +1,66 @@
+package kitchenpos.bo;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+import static org.mockito.Mockito.when;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.stream.Stream;
+import kitchenpos.dao.ProductDao;
+import kitchenpos.model.Product;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class ProductBoTest {
+
+    @Mock
+    private ProductDao productDao;
+
+    @InjectMocks
+    private ProductBo productBo;
+
+    @DisplayName("상품을 생성한다.")
+    @Test
+    void create() {
+        Product product = new Product();
+        product.setId(1L);
+        product.setName("name");
+        product.setPrice(BigDecimal.ONE);
+
+        when(productDao.save(product)).thenReturn(product);
+
+        assertThat(productBo.create(product)).isEqualTo(product);
+    }
+
+    @DisplayName("가격이 책정되지 않았거나 0 미만인 경우 예외를 발생시킨다.")
+    @ParameterizedTest
+    @MethodSource("prices")
+    void exceptionWhenCreate(BigDecimal price) {
+        Product product = new Product();
+        product.setId(1L);
+        product.setName("name");
+        product.setPrice(price);
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+            .isThrownBy(() -> productBo.create(product));
+    }
+
+    private static Stream<BigDecimal> prices() {
+        return Stream.of(null, BigDecimal.valueOf(-1));
+    }
+
+    @DisplayName("상품 목록을 가져온다.")
+    @Test
+    void list() {
+        when(productDao.findAll()).thenReturn(new ArrayList<>());
+        assertThat(productBo.list().isEmpty()).isTrue();
+    }
+}
