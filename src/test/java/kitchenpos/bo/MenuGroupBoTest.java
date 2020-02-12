b2daeffdc48ec9a97bf7bfc30c641628ce47dcbd
james <hency.yeo@gmail.com>
2020-02-12 21:41:27 +0900

    test(메뉴그룹(MenuGroup) 요구사항 반영) - [ ] 메뉴가 제공되는 형태를 메뉴그룹이라고 부른다. - [ ] 메뉴그룹을 설정할 수 있다. - [ ] 메뉴그룹 목록을 볼 수 있다.

--- src/test/java/kitchenpos/bo/MenuGroupBoTest.java
+++ src/test/java/kitchenpos/bo/MenuGroupBoTest.java
@@ -14,7 +14,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
