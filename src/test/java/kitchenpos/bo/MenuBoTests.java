f0e0bf58603d78d188f1bd44728d6548034fd473
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-02-13 17:38:12 +0900

    refactor(MenuBoTests) - change testing style
    - 객체 생성이 너무 여기저기 되는 것 같아 객체 생성 방식을 변경했습니다.
    - BeforeAll을 통해 기본적으로 사용될 객체를 한번만 생성했습니다.
    - 그 이후 테스트 케이스에서 필요한 객채는 간단한 건 테스트 메소드 내부에, 복잡한 건 MethodSource로 빼서 처리했습니다

--- src/test/java/kitchenpos/bo/MenuBoTests.java
+++ src/test/java/kitchenpos/bo/MenuBoTests.java
@@ -7,12 +7,13 @@ import kitchenpos.dao.ProductDao;
 import kitchenpos.model.Menu;
 import kitchenpos.model.MenuProduct;
 import kitchenpos.model.Product;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -21,6 +22,7 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -41,32 +43,28 @@ class MenuBoTests {
     @InjectMocks
     private MenuBo menuBo;
 
-    private Menu mockMenu = new Menu();
-    private MenuProduct mockMenuProduct = new MenuProduct();
-    private Product mockProduct = new Product();
-    private final List<Menu> mockMenus = new ArrayList<>();
-    private final List<MenuProduct> mockMenuProducts = new ArrayList<>();
+    private static final Menu mockDefaultMenu = new Menu();
+    private static final List<Menu> mockDefaultMenus = new ArrayList<>();
 
-    @BeforeEach
-    public void defaultSetup() {
-        // Menu { id: 1L, menuGroupId: 1L, price: 16000, name: testMenu, menuProducts: mockMenuProducts }
+    private static final Product mockDefaultProduct = new Product();
+
+    private static final MenuProduct mockDefaultMenuProduct = new MenuProduct();
+    private static final List<MenuProduct> mockDefaultMenuProducts = new ArrayList<>();
+
+    @BeforeAll
+    public static void setupAll() {
         setupDefaultMenu();
-
-        // MenuProduct { id: 1L, menuId: 1L, quantity: 100, productId: 2L }
-        setupDefaultMenuProduct();
-
-        // Product { id: 2L, name: testProduct, price: 16000 }
+        setupDefaultMenus();
         setupDefaultProduct();
-
-        mockMenus.add(mockMenu);
-        mockMenuProducts.add(mockMenuProduct);
+        setupDefaultMenuProduct();
+        setupDefaultMenuProducts();
     }
 
     @DisplayName("메뉴 전체 조회 시 메뉴에 맞는 메뉴 상품들을 가져오는지 확인")
     @Test
     public void findAllTest() {
-        given(menuDao.findAll()).willReturn(mockMenus);
-        given(menuProductDao.findAllByMenuId(1L)).willReturn(mockMenuProducts);
+        given(menuDao.findAll()).willReturn(mockDefaultMenus);
+        given(menuProductDao.findAllByMenuId(1L)).willReturn(mockDefaultMenuProducts);
 
         List<Menu> menuList = menuBo.list();
 
@@ -75,72 +73,120 @@ class MenuBoTests {
     }
 
     @DisplayName("정상적인 값들로 메뉴 생성 시도 성공")
-    @Test
-    public void createMenuSuccess() {
+    @ParameterizedTest
+    @MethodSource("validCreateRequest")
+    public void createMenuSuccess(Menu validMenu, MenuProduct validMenuProduct) {
         given(menuGroupDao.existsById(1L)).willReturn(true);
-        given(productDao.findById(2L)).willReturn(Optional.ofNullable(mockProduct));
-        given(menuDao.save(mockMenu)).willReturn(mockMenu);
-        given(menuProductDao.save(mockMenuProduct)).willReturn(mockMenuProduct);
+        given(productDao.findById(2L)).willReturn(Optional.of(mockDefaultProduct));
+        given(menuDao.save(validMenu)).willReturn(mockDefaultMenu);
+        given(menuProductDao.save(validMenuProduct)).willReturn(mockDefaultMenuProduct);
 
-        Menu saved = menuBo.create(mockMenu);
+        Menu saved = menuBo.create(validMenu);
 
-        assertThat(saved.getMenuProducts().get(0).getMenuId()).isEqualTo(1L);
+        assertThat(saved.getId()).isEqualTo(1L);
+        assertThat(saved.getMenuProducts()).contains(mockDefaultMenuProduct);
+    }
+
+    private static Object[] validCreateRequest() {
+        Menu validMenu = new Menu();
+        MenuProduct validMenuProduct = new MenuProduct();
+        List<MenuProduct> validMenuProducts = new ArrayList<>();
+        validMenuProducts.add(validMenuProduct);
+
+        validMenuProduct.setSeq(1L);
+        validMenuProduct.setMenuId(1L);
+        validMenuProduct.setQuantity(10);
+        validMenuProduct.setProductId(2L);
+
+        validMenu.setMenuGroupId(1L);
+        validMenu.setPrice(BigDecimal.valueOf(16000));
+        validMenu.setName("createRequestMenu");
+        validMenu.setMenuProducts(validMenuProducts);
+
+        return new Object[]{Arguments.of(validMenu, validMenuProduct)};
     }
 
     @DisplayName("0보다 작은 가격으로 메뉴 생성 시도 시 실패")
     @ParameterizedTest
-    @ValueSource(ints = {-1, 0})
-    public void createMenuFailWithInvalidPrice(int price) {
-        mockMenu.setPrice(BigDecimal.valueOf(price));
+    @MethodSource("invalidPriceMenus")
+    public void createMenuFailWithInvalidPrice(Menu invalidPriceMenu) {
+        assertThatThrownBy(() -> menuBo.create(invalidPriceMenu)).isInstanceOf(IllegalArgumentException.class);
+    }
 
-        assertThatThrownBy(() -> menuBo.create(mockMenu)).isInstanceOf(IllegalArgumentException.class);
+    private static Stream<Menu> invalidPriceMenus() {
+        Menu negativeNumberMenu = new Menu();
+        negativeNumberMenu.setPrice(BigDecimal.valueOf(-1000));
+
+        Menu noPriceMenu = new Menu();
+
+        return Stream.of(negativeNumberMenu, noPriceMenu);
     }
 
     @DisplayName("메뉴에 속한 메뉴 상품 가격의 총합보다 높은 가격으로 메뉴 생성 시도 시 실패")
-    @ParameterizedTest
-    @ValueSource(ints = {1000000, 100000000, 777777})
-    public void createMenuFailWhenMenuPriceIsBiggerThanPriceSum(int price) {
-        mockMenu.setPrice(BigDecimal.valueOf(price));
-        given(menuGroupDao.existsById(1L)).willReturn(true);
-        given(productDao.findById(2L)).willReturn(Optional.ofNullable(mockProduct));
+    @Test
+    public void createMenuFailWhenMenuPriceIsBiggerThanPriceSum() {
+        Menu tooBigPriceMenu = new Menu();
+        tooBigPriceMenu.setMenuGroupId(1L);
+        tooBigPriceMenu.setPrice(BigDecimal.valueOf(999999999));
+        tooBigPriceMenu.setMenuProducts(mockDefaultMenuProducts);
 
-        assertThatThrownBy(() -> menuBo.create(mockMenu)).isInstanceOf(IllegalArgumentException.class);
+        given(menuGroupDao.existsById(1L)).willReturn(true);
+        given(productDao.findById(2L)).willReturn(Optional.of(mockDefaultProduct));
+
+        assertThatThrownBy(() -> menuBo.create(tooBigPriceMenu)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("메뉴 그룹에 속하지 않은 메뉴로 생성 시도 시 실패")
     @Test
     public void createMenuFailWithNotInMenuGroup() {
+        Menu notInMenuGroup = new Menu();
+        notInMenuGroup.setPrice(BigDecimal.valueOf(1));
+        notInMenuGroup.setMenuGroupId(1L);
+
         given(menuGroupDao.existsById(1L)).willReturn(false);
 
-        assertThatThrownBy(() -> menuBo.create(mockMenu)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> menuBo.create(notInMenuGroup)).isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("없는 상품으로 메뉴 생성 시도 시 실패")
     @Test
     public void createMenuFailWithNonExistProduct() {
+        Menu notExistProductMenu = new Menu();
+        notExistProductMenu.setPrice(BigDecimal.valueOf(1));
+        notExistProductMenu.setMenuGroupId(1L);
+        notExistProductMenu.setMenuProducts(new ArrayList<>());
+
         given(menuGroupDao.existsById(1L)).willReturn(true);
 
-        assertThatThrownBy(() -> menuBo.create(mockMenu)).isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> menuBo.create(notExistProductMenu)).isInstanceOf(IllegalArgumentException.class);
     }
 
-    private void setupDefaultMenu() {
-        mockMenu.setId(1L);
-        mockMenu.setMenuGroupId(1L);
-        mockMenu.setPrice(BigDecimal.valueOf(16000));
-        mockMenu.setName("testMenu");
-        mockMenu.setMenuProducts(mockMenuProducts);
+    private static void setupDefaultMenu() {
+        mockDefaultMenu.setId(1L);
+        mockDefaultMenu.setMenuGroupId(1L);
+        mockDefaultMenu.setPrice(BigDecimal.valueOf(16000));
+        mockDefaultMenu.setName("createRequestMenu");
+        mockDefaultMenu.setMenuProducts(mockDefaultMenuProducts);
     }
 
-    private void setupDefaultMenuProduct() {
-        mockMenuProduct.setSeq(1L);
-        mockMenuProduct.setMenuId(1L);
-        mockMenuProduct.setQuantity(10);
-        mockMenuProduct.setProductId(2L);
+    private static void setupDefaultMenus() {
+        mockDefaultMenus.add(mockDefaultMenu);
     }
 
-    private void setupDefaultProduct() {
-        mockProduct.setId(2L);
-        mockProduct.setName("testProduct");
-        mockProduct.setPrice(BigDecimal.valueOf(16000));
+    private static void setupDefaultProduct() {
+        mockDefaultProduct.setId(2L);
+        mockDefaultProduct.setName("testProduct");
+        mockDefaultProduct.setPrice(BigDecimal.valueOf(16000));
+    }
+
+    private static void setupDefaultMenuProduct() {
+        mockDefaultMenuProduct.setSeq(1L);
+        mockDefaultMenuProduct.setMenuId(1L);
+        mockDefaultMenuProduct.setQuantity(10);
+        mockDefaultMenuProduct.setProductId(2L);
+    }
+
+    private static void setupDefaultMenuProducts() {
+        mockDefaultMenuProducts.add(mockDefaultMenuProduct);
     }
 }
