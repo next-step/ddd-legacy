d4c329f73167708d3318b6447a8012c4895749b3
myhency <hency.yeo@gmail.com>
2020-02-14 13:41:42 +0900

    test(BDD스타일로 변경)

--- src/test/java/kitchenpos/bo/TableBoTest.java
+++ src/test/java/kitchenpos/bo/TableBoTest.java
@@ -2,23 +2,27 @@ package kitchenpos.bo;
 
 import kitchenpos.dao.OrderDao;
 import kitchenpos.dao.OrderTableDao;
+import kitchenpos.model.OrderStatus;
 import kitchenpos.model.OrderTable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -55,93 +59,150 @@ class TableBoTest {
     @DisplayName("테이블 정보를 입력할 수 있다. (테이블그룹, 손님 수, 테이블상태(비었는지 아닌지))")
     @Test
     void create() {
-        when(orderTableDao.save(any(OrderTable.class))).thenReturn(orderTable1);
-        OrderTable result = tableBo.create(orderTable1);
-        assertThat(result.getId()).isEqualTo(orderTable1.getId());
+        //given
+        OrderTable givenOrderTable = orderTable1;
+        given(orderTableDao.save(any(OrderTable.class)))
+                .willReturn(givenOrderTable);
+
+        //when
+        OrderTable actualOrderTable = tableBo.create(givenOrderTable);
+
+        //then
+        assertThat(actualOrderTable.getId()).isEqualTo(givenOrderTable.getId());
     }
 
     @DisplayName("테이블 목록을 볼 수 있다.")
     @Test
     void list() {
-        when(orderTableDao.findAll()).thenReturn(orderTableList);
-        List<OrderTable> result = tableBo.list();
-        assertThat(result.size()).isEqualTo(2);
+        //given
+        List<OrderTable> givenOrderTableList = orderTableList;
+        given(orderTableDao.findAll())
+                .willReturn(givenOrderTableList);
+
+        //when
+        List<OrderTable> actualOrderTableList = tableBo.list();
+
+        //then
+        assertThat(actualOrderTableList.size())
+                .isEqualTo(givenOrderTableList.size());
     }
 
     @DisplayName("테이블상태를 변경할 수 있다.")
     @Test
     void changeEmpty() {
-        when(orderTableDao.findById(anyLong()))
-                .thenReturn(optionalOrderTable.of(orderTable1));
-        when(orderDao.existsByOrderTableIdAndOrderStatusIn(anyLong(),any(List.class)))
-                .thenReturn(false);
-        when(orderTableDao.save(any(OrderTable.class)))
-                .thenReturn(orderTable1);
-        OrderTable result = tableBo.changeEmpty(orderTable1.getId(), orderTable1);
-        assertThat(result.isEmpty()).isFalse();
+        //given
+        OrderTable givenOrderTable = orderTable1;
+        given(orderTableDao.findById(anyLong()))
+                .willReturn(Optional.of(givenOrderTable));
+        given(orderDao.existsByOrderTableIdAndOrderStatusIn(anyLong(), any(List.class)))
+                .willReturn(false);
+        given(orderTableDao.save(any(OrderTable.class)))
+                .willReturn(givenOrderTable);
+
+        //when
+        OrderTable actualOrderTable =
+                tableBo.changeEmpty(givenOrderTable.getId(), givenOrderTable);
+
+        //then
+        assertThat(actualOrderTable.isEmpty())
+                .isFalse();
     }
 
     @DisplayName("주문이 없는 테이블의 상태를 변경할 수 없다.")
     @Test
     void changeEmptyWithoutOrderTable() {
-        when(orderTableDao.findById(anyLong()))
-                .thenReturn(Optional.empty());
-        Throwable thrown = catchThrowable(() ->{
-            tableBo.changeEmpty(orderTable1.getId(), orderTable1);
-        });
-        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+        //given
+        OrderTable givenOrderTable = orderTable1;
+        given(orderTableDao.findById(anyLong()))
+                .willReturn(Optional.empty());
+
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            tableBo.changeEmpty(givenOrderTable.getId(), givenOrderTable);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("테이블그룹에 포함되지 않은 테이블은 변경할 수 없다.")
     @Test
     void changeEmptyExclusiveTableGroup() {
-        orderTable1.setTableGroupId(null);
-        Throwable thrown = catchThrowable(() ->{
-            tableBo.changeEmpty(orderTable1.getId(), orderTable1);
-        });
-        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+        //given
+        OrderTable givenOrderTable = orderTable1;
+        givenOrderTable.setTableGroupId(null);
+
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            tableBo.changeEmpty(givenOrderTable.getId(), givenOrderTable);
+        }).isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("조리중이거나 손님이 식사중인 테이블의 상태를 변경할 수 없다.")
-    @Test
-    void changeEmptyCookingOrMeal() {
-        orderTable1.setTableGroupId(null);
-        Throwable thrown = catchThrowable(() ->{
-            tableBo.changeEmpty(orderTable1.getId(), orderTable1);
-        });
-        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+    @ParameterizedTest
+    @ValueSource(booleans = {true})
+    void changeEmptyCookingOrMeal(boolean isEmpty) {
+        //given
+        OrderTable givenOrderTable = orderTable1;
+        givenOrderTable.setTableGroupId(null);
+        given(orderTableDao.findById(givenOrderTable.getId()))
+                .willReturn(Optional.of(givenOrderTable));
+        given(orderDao.existsByOrderTableIdAndOrderStatusIn(givenOrderTable.getId(), Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name())))
+                .willReturn(isEmpty);
+
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            tableBo.changeEmpty(givenOrderTable.getId(), givenOrderTable); })
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("테이블에 손님수를 변경할 수 있다.")
     @Test
     void changeNumberOfGuests() {
-        when(orderTableDao.findById(anyLong()))
-                .thenReturn(optionalOrderTable.of(orderTable1));
-        when(orderTableDao.save(any(OrderTable.class)))
-                .thenReturn(orderTable1);
-        OrderTable result = tableBo.changeNumberOfGuests(orderTable1.getId(), orderTable1);
-        assertThat(result.getNumberOfGuests()).isEqualTo(2);
+        //given
+        OrderTable givenOrderTable = orderTable1;
+        given(orderTableDao.findById(anyLong()))
+                .willReturn(Optional.of(givenOrderTable));
+        given(orderTableDao.save(any(OrderTable.class)))
+                .willReturn(givenOrderTable);
+
+        //when
+        OrderTable actualOrderTable =
+                tableBo.changeNumberOfGuests(givenOrderTable.getId(), givenOrderTable);
+
+        //then
+        assertThat(actualOrderTable.getNumberOfGuests())
+                .isEqualTo(givenOrderTable.getNumberOfGuests());
     }
 
     @DisplayName("손님수는 반드시 양수여야 한다.")
-    @Test
-    void changeNumberOfGuestsByNegativeNumber() {
-        orderTable1.setNumberOfGuests(-1);
-        Throwable thrown = catchThrowable(() ->{
-            tableBo.changeNumberOfGuests(orderTable1.getId(), orderTable1);
-        });
-        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+    @ParameterizedTest
+    @ValueSource(ints = {-1, 0})
+    void changeNumberOfGuestsByNegativeNumber(int numberOfGuests) {
+        //given
+        OrderTable givenOrderTable = orderTable1;
+        givenOrderTable.setNumberOfGuests(numberOfGuests);
+
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            tableBo.changeNumberOfGuests(givenOrderTable.getId(), givenOrderTable); })
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("주문이 있는 테이블의 손님수만 변경할 수 있다.")
     @Test
     void changeNumberOfGuestsTableWithOrder() {
-        when(orderTableDao.findById(anyLong()))
-                .thenReturn(Optional.empty());
-        Throwable thrown = catchThrowable(() ->{
-            tableBo.changeNumberOfGuests(orderTable1.getId(), orderTable1);
-        });
-        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+        //given
+        OrderTable givenOrderTable = orderTable1;
+        given(orderTableDao.findById(anyLong()))
+                .willReturn(Optional.empty());
+
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            tableBo.changeNumberOfGuests(givenOrderTable.getId(), givenOrderTable); })
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
 
