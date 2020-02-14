20824aa44aabc95f1cd57cc43114843906ad1055
myhency <hency.yeo@gmail.com>
2020-02-14 22:06:36 +0900

    test(BDD스타일로 변경)

--- src/test/java/kitchenpos/bo/TableGroupBoTest.java
+++ src/test/java/kitchenpos/bo/TableGroupBoTest.java
@@ -9,6 +9,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -16,11 +21,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.catchThrowable;
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -63,79 +69,122 @@ class TableGroupBoTest {
     @DisplayName("테이블그룹을 생성할 수 있다.")
     @Test
     void create() {
-        when(orderTableDao.findAllByIdIn(any(List.class))).thenReturn(this.orderTableList);
-        when(tableGroupDao.save(any(TableGroup.class))).thenReturn(this.tableGroup);
-        when(orderTableDao.save(any(OrderTable.class))).thenReturn(this.orderTable1);
-        TableGroup result = tableGroupBo.create(this.tableGroup);
-        assertThat(result.getId()).isEqualTo(this.tableGroup.getId());
+        //given
+        List<OrderTable> givenOrderTableList = orderTableList;
+        TableGroup givenTableGroup = tableGroup;
+        given(orderTableDao.findAllByIdIn(any(List.class)))
+                .willReturn(givenOrderTableList);
+        given(tableGroupDao.save(any(TableGroup.class)))
+                .willReturn(givenTableGroup);
+
+        //when
+        TableGroup actualTableGroup = tableGroupBo.create(givenTableGroup);
+
+        //then
+        assertThat(actualTableGroup.getId())
+                .isEqualTo(givenTableGroup.getId());
     }
 
     @DisplayName("주문이 없는 테이블은 테이블그룹으로 생성할 수 없다.")
-    @Test
-    void createWithoutOrder() {
-        this.tableGroup.setOrderTables(null);
-        Throwable thrown = catchThrowable(() ->{
-            tableGroupBo.create(this.tableGroup);
-        });
+    @ParameterizedTest
+    @NullSource
+    void createWithoutOrder(List<OrderTable> orderTableList) {
+        //given
+        TableGroup givenTableGroup = tableGroup;
+        givenTableGroup.setOrderTables(orderTableList);
 
-        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            tableGroupBo.create(givenTableGroup); })
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("주문이 있는 경우에도 두개 이상의 테이블만 테이블그룹으로 생성할 수 있다.")
     @Test
     void createOverTwoTables() {
-        this.orderTableList.remove(orderTable1);
-        Throwable thrown = catchThrowable(() ->{
-            tableGroupBo.create(this.tableGroup);
-        });
+        //given
+        OrderTable givenOrderTable = orderTable1;
+        List<OrderTable> givenOrderTableList = orderTableList;
+        givenOrderTableList.remove(givenOrderTable);
+        TableGroup givenTableGroup = tableGroup;
 
-        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            tableGroupBo.create(givenTableGroup); })
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("주문이 있는 테이블수와 생성하려는 테이블수가 같아야 한다.")
     @Test
     void createNumberOfTable() {
-        List<OrderTable> savedOrderTableList = new ArrayList<>();
-        savedOrderTableList.add(this.orderTable1);
-        when(orderTableDao.findAllByIdIn(any(List.class))).thenReturn(savedOrderTableList);
-        Throwable thrown = catchThrowable(() ->{
-            tableGroupBo.create(this.tableGroup);
-        });
-        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+        //given
+        TableGroup givenTableGroup = tableGroup;
+
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            tableGroupBo.create(givenTableGroup); })
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("테이블그룹을 생성하는 대상 테이블은 비어있어야 한다.")
-    @Test
-    void createEmptyTable() {
-        this.orderTable1.setEmpty(false);
-        Throwable thrown = catchThrowable(() ->{
-            tableGroupBo.create(this.tableGroup);
-        });
-        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+    @ParameterizedTest
+    @ValueSource(booleans = {false})
+    void createEmptyTable(boolean isEmpty) {
+        //given
+        OrderTable givenOrderTable = orderTable1;
+        givenOrderTable.setEmpty(isEmpty);
+        TableGroup givenTableGroup = tableGroup;
+
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            tableGroupBo.create(givenTableGroup); })
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("테이블그룹을 삭제할 수 있다.")
-    @Test
-    void delete() {
-        when(orderTableDao.findAllByTableGroupId(anyLong()))
-                .thenReturn(this.orderTableList);
-        when(orderDao.existsByOrderTableIdInAndOrderStatusIn(any(List.class),any(List.class)))
-                .thenReturn(false);
+    @ParameterizedTest
+    @MethodSource("booleanAndIntProvider")
+    void delete(boolean isExists, int numberOfDeletedTables) {
+        //given
+        List<OrderTable> givenOrderTableList = orderTableList;
+        given(orderTableDao.findAllByTableGroupId(anyLong()))
+                .willReturn(givenOrderTableList);
+        given(orderDao.existsByOrderTableIdInAndOrderStatusIn(any(List.class),any(List.class)))
+                .willReturn(isExists);
+
+        //when
         tableGroupBo.delete(this.tableGroup.getId());
-        verify(orderTableDao,times(2))
+
+        //then
+        verify(orderTableDao,times(numberOfDeletedTables))
                 .save(any(OrderTable.class));
     }
 
+    static Stream<Arguments> booleanAndIntProvider() {
+        return Stream.of(Arguments.arguments(false, 2));
+    }
+
     @DisplayName("테이블상태가 조리중: COOKING, 고객이 식사중인 주문: MEAL 인 경우는 삭제할 수 없다.")
-    @Test
-    void deleteByStatus() {
-        when(orderTableDao.findAllByTableGroupId(anyLong()))
-                .thenReturn(this.orderTableList);
-        when(orderDao.existsByOrderTableIdInAndOrderStatusIn(any(List.class),any(List.class)))
-                .thenReturn(true);
-        Throwable thrown = catchThrowable(() ->{
-            tableGroupBo.delete(this.tableGroup.getId());
-        });
-        assertThat(thrown).isInstanceOf(IllegalArgumentException.class);
+    @ParameterizedTest
+    @ValueSource(booleans = {true})
+    void deleteByStatus(boolean isExists) {
+        //given
+        List<OrderTable> givenOrderTableList = orderTableList;
+        TableGroup givenTableGroup = tableGroup;
+
+        given(orderTableDao.findAllByTableGroupId(anyLong()))
+                .willReturn(givenOrderTableList);
+        given(orderDao.existsByOrderTableIdInAndOrderStatusIn(any(List.class),any(List.class)))
+                .willReturn(isExists);
+
+        //when
+        //then
+        assertThatThrownBy(() -> {
+            tableGroupBo.delete(givenTableGroup.getId()); })
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
