65acd0e878f8970d7605c3806c3a4a595eacfddd
liquidme <liquidme@uneedcomms.com>
2020-02-14 16:57:12 +0900

    test(menuGroup) : 메뉴 그룹 등록 테스트 코드

--- src/main/java/kitchenpos/model/MenuGroup.java
+++ src/main/java/kitchenpos/model/MenuGroup.java
@@ -4,6 +4,15 @@ public class MenuGroup {
     private Long id;
     private String name;
 
+    public MenuGroup() {
+
+    }
+
+    public MenuGroup(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
     public Long getId() {
         return id;
     }
--- src/test/java/kitchenpos/bo/MenuGroupTest.java
+++ src/test/java/kitchenpos/bo/MenuGroupTest.java
@@ -1,12 +1,17 @@
 package kitchenpos.bo;
 
 import kitchenpos.dao.MenuGroupDao;
+import kitchenpos.model.MenuGroup;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 public class MenuGroupTest {
@@ -15,11 +20,26 @@ public class MenuGroupTest {
     private MenuGroupDao menuGroupDao;
 
     @InjectMocks
-    private MenuBo menuBo;
+    private MenuGroupBo menuGroupBo;
+
+    private MenuGroup menuGroup;
+
+    @BeforeEach
+    void setUp() {
+        this.menuGroup = new MenuGroup(1L, "추천메뉴");
+    }
 
     @Test
     @DisplayName("메뉴 그룹(카테고리) 등록")
     void createMenuGroup() {
+        // give
+        given(menuGroupDao.save(menuGroup))
+                .willReturn(menuGroup);
+        // when
+        MenuGroup menuGroupExpected = menuGroupBo.create(menuGroup);
+        MenuGroup menuGroupActual = menuGroup;
+        // then
+        assertThat(menuGroupExpected.getId()).isEqualTo(menuGroupActual.getId());
 
     }
 }
