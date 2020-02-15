78546208b4ffe0b046b07f7aef4be9a69a6c758f
liquidjoo <liquidjoo@gmail.com>
2020-02-15 18:13:20 +0900

    test(order) : 주문 상품과 메뉴 상품의 유효성 검사

--- src/test/java/kitchenpos/bo/OrderBoTest.java
+++ src/test/java/kitchenpos/bo/OrderBoTest.java
@@ -1,7 +1,9 @@
 package kitchenpos.bo;
 
+import kitchenpos.dao.MenuDao;
 import kitchenpos.dao.OrderDao;
 import kitchenpos.model.Order;
+import kitchenpos.model.OrderLineItem;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -10,15 +12,19 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Arrays;
 import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.mockito.BDDMockito.given;
 
 @ExtendWith(MockitoExtension.class)
 class OrderBoTest {
 
     @Mock
     private OrderDao orderDao;
+    @Mock
+    private MenuDao menuDao;
 
     @InjectMocks
     private OrderBo orderBo;
@@ -36,6 +42,19 @@ class OrderBoTest {
     void createExceptionByEmptyOrderLineItem() {
         // give
         order.setOrderLineItems(Collections.emptyList());
+        // when then
+        assertThatIllegalArgumentException().isThrownBy(() -> orderBo.create(order));
+    }
+
+    @Test
+    @DisplayName("주문한 상품과 메뉴의 상품이 일치하는지 확인 한다.")
+    void createExceptionByEqualsMenu() {
+        // give
+        OrderLineItem orderLineItem = new OrderLineItem();
+        orderLineItem.setMenuId(1L);
+        order.setOrderLineItems(Arrays.asList(orderLineItem));
+        given(menuDao.countByIdIn(Arrays.asList(1L)))
+                .willReturn(2L);
         // when then
         assertThatIllegalArgumentException().isThrownBy(() -> orderBo.create(order));
     }
