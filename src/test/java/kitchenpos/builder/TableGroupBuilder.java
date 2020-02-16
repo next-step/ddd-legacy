5f57783e7edaf6a464f913744f222d6838d548aa
james <hency.yeo@gmail.com>
2020-02-16 12:51:43 +0900

    test(Builder 적용)

---
+++ src/test/java/kitchenpos/builder/TableGroupBuilder.java
@@ -0,0 +1,40 @@
+package kitchenpos.builder;
+
+import kitchenpos.model.OrderTable;
+import kitchenpos.model.TableGroup;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class TableGroupBuilder {
+    private Long id;
+    private LocalDateTime createdDate;
+    private List<OrderTable> orderTables;
+    
+    public static TableGroupBuilder tableGroup() {
+        return new TableGroupBuilder();
+    }
+
+    public TableGroupBuilder withId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public TableGroupBuilder withCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+        return this;
+    }
+
+    public TableGroupBuilder withOrderTables(List<OrderTable> orderTables) {
+        this.orderTables = orderTables;
+        return this;
+    }
+
+    public TableGroup build() {
+        TableGroup tableGroup = new TableGroup();
+        tableGroup.setId(this.id);
+        tableGroup.setOrderTables(this.orderTables);
+        tableGroup.setCreatedDate(this.createdDate);
+        return tableGroup;
+    }
+}
