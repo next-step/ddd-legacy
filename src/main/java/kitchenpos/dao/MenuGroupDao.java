081213b7fbdbd3ad3b254ca6cb227612de28b038
이끼룩 <saram4030@gmail.com>
2020-02-19 22:03:48 +0900

    test(menuGroupBo): 메뉴 그룹 Business Object의 테스트코드를 작성하라

--- src/main/java/kitchenpos/dao/MenuGroupDao.java
+++ src/main/java/kitchenpos/dao/MenuGroupDao.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MenuGroupDao {
+
     MenuGroup save(MenuGroup entity);
 
     Optional<MenuGroup> findById(Long id);
--- src/main/java/kitchenpos/dao/MenuProductDao.java
+++ src/main/java/kitchenpos/dao/MenuProductDao.java
@@ -6,6 +6,7 @@ import java.util.List;
 import java.util.Optional;
 
 public interface MenuProductDao {
+
     MenuProduct save(MenuProduct entity);
 
     Optional<MenuProduct> findById(Long id);
