c58fcb435612da6d726a4af5197a89d671dcb424
jun-pil park <iamfeel2@gmail.com>
2020-03-04 08:38:31 +0900

    test(TableGroupBo) : TableGroupBoTest 테스트 케이스 추가

--- src/test/java/kitchenpos/bo/TableGroupBoTest.java
+++ src/test/java/kitchenpos/bo/TableGroupBoTest.java
@@ -4,6 +4,7 @@ import kitchenpos.Fixtures;
 import kitchenpos.dao.OrderDao;
 import kitchenpos.dao.OrderTableDao;
 import kitchenpos.dao.TableGroupDao;
+import kitchenpos.model.OrderStatus;
 import kitchenpos.model.OrderTable;
 import kitchenpos.model.TableGroup;
 import org.assertj.core.api.Assertions;
@@ -18,6 +19,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -44,12 +46,17 @@ class TableGroupBoTest {
     @BeforeEach
     public void setup() {
         defaultTableGroup = Fixtures.getTableGroup(1L, LocalDateTime.now(), defaultListOrderTables);
+        defaultListOrderTables.add(Fixtures.getOrderTable(1L, false,3, defaultTableGroup.getId()));
+        defaultListOrderTables.add(Fixtures.getOrderTable(2L, false,4, defaultTableGroup.getId()));
     }
 
     @DisplayName("테이븚 그룹은 2개 이상의 테이블을 가지고 있어야 한다.")
     @Test
     public void createMustHaveLeastTwoTable() {
-        defaultListOrderTables.add(Fixtures.getOrderTable(1L, false,4, defaultTableGroup.getId()));
+        if(defaultListOrderTables.size() > 1) {
+            defaultListOrderTables.remove(defaultListOrderTables.size() - 1);
+        }
+
         Assertions.assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(
                 () ->tableGroupBo.create(defaultTableGroup));
     }
@@ -57,19 +64,62 @@ class TableGroupBoTest {
     @DisplayName("테이블 그룹을 만들려는 테이블들은 식당에 존재 해야된다.")
     @Test
     public void createSameOrderTableCountSaved() {
-        defaultListOrderTables.add(Fixtures.getOrderTable(1L, false,4, defaultTableGroup.getId()));
-        defaultListOrderTables.add(Fixtures.getOrderTable(2L, false,3, defaultTableGroup.getId()));
-
-        List<Long> defaultOrderTableIds = defaultListOrderTables.stream()
-                .map(OrderTable::getId)
-                .collect(Collectors.toList());
-
+        List<Long> defaultOrderTableIds = getIdList(defaultListOrderTables);
         List<OrderTable> savedOrderTables = Lists.newArrayList(defaultListOrderTables);
-        savedOrderTables.remove(savedOrderTables.size() - 1);
+        if(savedOrderTables.size() > 0) {
+            savedOrderTables.remove(savedOrderTables.size() - 1);
+        }
 
         given(orderTableDao.findAllByIdIn(defaultOrderTableIds)).willReturn(savedOrderTables);
+
         assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(
                 () -> assertThat(tableGroupBo.create(defaultTableGroup)));
     }
 
+    @DisplayName("테이블 그룹을 만들려는 테이블들이 비어 있지 않으면 안된다.")
+    @Test
+    public void createTablesSavedIsEmpty() {
+        List<Long> defaultOrderTableIds = getIdList(defaultListOrderTables);
+        List<OrderTable> savedOrderTables = new ArrayList<>();
+        savedOrderTables.add(Fixtures.getOrderTable(1L, false, 4));
+        savedOrderTables.add(Fixtures.getOrderTable(1L, false, 3));
+
+        given(orderTableDao.findAllByIdIn(defaultOrderTableIds)).willReturn(savedOrderTables);
+
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(
+                () -> assertThat(tableGroupBo.create(defaultTableGroup)));
+    }
+
+    @DisplayName("테이블 그룹을 만들려는 테이블들이 이미 테이블 그룹이 되어 있으면 안된다.")
+    @Test
+    public void createTablesSavedHasNoTableGroup() {
+        List<Long> defaultOrderTableIds = getIdList(defaultListOrderTables);
+        List<OrderTable> savedOrderTables = new ArrayList<>();
+        savedOrderTables.add(Fixtures.getOrderTable(1L, true, 0, 2L));
+        savedOrderTables.add(Fixtures.getOrderTable(1L, true, 0, 2L));
+
+        given(orderTableDao.findAllByIdIn(defaultOrderTableIds)).willReturn(savedOrderTables);
+
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(
+                () -> assertThat(tableGroupBo.create(defaultTableGroup)));
+    }
+
+    @DisplayName("테이블 그룹에 속한 테이블에 요리중, 식사 상태 인 주문이 있다면 삭제할 수 없다.")
+    @Test
+    public void deleteNoCookingAndNoMeal() {
+        long tableGroupId = defaultTableGroup.getId();
+        given(orderTableDao.findAllByTableGroupId(tableGroupId)).willReturn(defaultListOrderTables);
+        given(orderDao.existsByOrderTableIdInAndOrderStatusIn(
+                getIdList(defaultListOrderTables), Arrays.asList(OrderStatus.COOKING.name(), OrderStatus.MEAL.name())))
+                .willReturn(true);
+
+        assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(
+                () -> tableGroupBo.delete(tableGroupId));
+    }
+
+    private List<Long> getIdList(List<OrderTable> orderTables) {
+        return orderTables.stream()
+                .map(OrderTable::getId)
+                .collect(Collectors.toList());
+    }
 }
