01022831fb548dc0c54af2a85cb40eb81c709c73
Pyro(고정완) <rhwjddhks@gmail.com>
2021-07-27 20:50:25 +0900

    feat: Car 클래스 구현

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,31 @@
+package racingcar;
+
+public class Car {
+    private final String name;
+    private int position = 0;
+
+    public Car(String name) {
+        validateName(name);
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move(boolean movable) {
+        if (movable) {
+            position++;
+        }
+    }
+
+    private void validateName(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5 글자를 넘을 수 없습니다.");
+        }
+    }
+}
