129a5f39a6811a4017bc8483b11c2182ca819f2b
apdkeb <apdkeb@naver.com>
2021-07-28 16:53:45 +0900

    feat(stringcalculator) : 커스텀 구분자 추가
    2021. 07. 28.
    - 커스텀 구분자 로직 추가
    
    Resolves : loop-study
    See also : serverwizard

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,6 +1,8 @@
 package stringcalculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class StringCalculator {
@@ -16,10 +18,19 @@ public class StringCalculator {
             return stringToInt(text);
         }
 
-        String[] numbers = text.split(",|:");
-        return Stream.of(numbers)
+        String[] tokens = textToTokens(text);
+        return Stream.of(tokens)
                 .mapToInt(this::stringToInt)
                 .sum();
+    }
+
+    private String[] textToTokens(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return m.group(2).split(customDelimiter);
+        }
+        return text.split(",|:");
     }
 
     private int stringToInt(String text) {
