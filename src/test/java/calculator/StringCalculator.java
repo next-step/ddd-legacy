675b407033e8410c2ac42f6066851e57e5b19b5c
blossun <dnsngu@gmail.com>
2021-07-28 14:22:54 +0900

    feat: 빈 문자열 또는 null 입력 시 0을 반환

---
+++ src/test/java/calculator/StringCalculator.java
@@ -0,0 +1,10 @@
+package calculator;
+
+public class StringCalculator {
+    public int add(String text) {
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+        return 0;
+    }
+}
---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,23 @@
+package calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StringCalculatorTest {
+    private StringCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new StringCalculator();
+    }
+
+    @DisplayName("빈 문자열 또는 null을 입력할 경우 0을 반환해야 한다.")
+    @Test
+    void nullOrEmpty() {
+        assertThat(calculator.add(null)).isZero();
+    }
+}
