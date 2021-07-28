35c59cb77e08bfa27ee131df9be50348dac77b88
blossun <dnsngu@gmail.com>
2021-07-28 14:47:46 +0900

    refactor: 공백 여부 확인 로직을 메서드로 추출

--- src/test/java/calculator/StringCalculator.java
+++ src/test/java/calculator/StringCalculator.java
@@ -4,10 +4,14 @@ import java.util.stream.Stream;
 
 public class StringCalculator {
     public int add(String text) {
-        if (text == null || text.isEmpty()) {
+        if (isBlank(text)) {
             return 0;
         }
         String[] numbers = text.split(",");
         return Stream.of(numbers).mapToInt(Integer::parseInt).sum();
+    }
+
+    private boolean isBlank(String text) {
+        return text == null || text.isEmpty();
     }
 }
