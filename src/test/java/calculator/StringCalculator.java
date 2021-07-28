4312756fee710e2abd66cf6e4bb8d1e8207c8d92
jonny-cho <andante2183@gmail.com>
2021-07-28 20:08:57 +0900

    feat: StringCalculator 기본구현
    컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환

---
+++ src/test/java/calculator/StringCalculator.java
@@ -0,0 +1,18 @@
+package calculator;
+
+import java.util.Arrays;
+import org.thymeleaf.util.StringUtils;
+
+public class StringCalculator {
+
+    public static final String COMMA = ",";
+
+    public int calculate(final String stringNumber) {
+        if (StringUtils.isEmpty(stringNumber)) {
+            return 0;
+        }
+        return Arrays.stream(stringNumber.split(COMMA))
+            .mapToInt(Integer::parseInt)
+            .sum();
+    }
+}
---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,40 @@
+package calculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+
+    final StringCalculator calculator = new StringCalculator();
+
+    @DisplayName("빈 문자열을 입력하는 경우 0을 반환한다")
+    @Test
+    void emptyTest() {
+        final int expect = calculator.calculate("");
+        assertThat(expect).isEqualTo(0);
+    }
+
+    @DisplayName("null을 입력하는 경우 0을 반환한다")
+    @Test
+    void nullTest() {
+        final int expect = calculator.calculate(null);
+        assertThat(expect).isEqualTo(0);
+    }
+
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다")
+    @Test
+    void oneNumberTest() {
+        final int expect = calculator.calculate("1");
+        assertThat(expect).isEqualTo(1);
+    }
+
+    @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다")
+    @Test
+    void twoNumberWithCommaTest() {
+        final int expect = calculator.calculate("1,2");
+        assertThat(expect).isEqualTo(3);
+    }
+
+}
