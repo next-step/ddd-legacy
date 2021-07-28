297cf8618402ca1ff87194fca794f56f6ae47478
bgpark <bgpark82@gmail.com>
2021-07-28 23:05:43 +0900

    refactor(calculator): StringCalculator 객체 setUp으로 이동

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
@@ -10,11 +11,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 @DisplayName("문자열 계산기 테스트")
 public class StringCalculatorTest {
 
+    private StringCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new StringCalculator();
+    }
+
     @DisplayName(value = "빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
     @ParameterizedTest(name = "{0} 입력")
     @NullAndEmptySource
     void emptyOrNull(final String text) {
-        StringCalculator calculator = new StringCalculator();
         assertThat(calculator.add(text)).isZero();
     }
 
@@ -22,7 +29,6 @@ public class StringCalculatorTest {
     @ParameterizedTest(name = "{0} 입력")
     @ValueSource(strings = {"1","10","100"})
     void oneNumber(final String text) {
-        StringCalculator calculator = new StringCalculator();
         assertThat(calculator.add(text)).isSameAs(Integer.parseInt(text));
     }
 }
