89bf09ed8652916517e5e67ca339a5b8e6fb193b
y2o2u2n <y2o2u2n@gmail.com>
2021-07-28 23:30:07 +0900

    feat(calculator): return sum if text is empty or single element

--- src/test/java/kitchenpos/calculator/StringCalculatorTest.java
+++ src/test/java/kitchenpos/calculator/StringCalculatorTest.java
@@ -1,6 +1,9 @@
 package kitchenpos.calculator;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
 class StringCalculatorTest {
     private  StringCalculator sut;
@@ -8,5 +11,18 @@ class StringCalculatorTest {
     @BeforeEach
     void setUp() {
         sut = new StringCalculator();
+    }
+
+    @DisplayName("빈 값을 입력할 경우 0을 반환한다.")
+    @Test
+    void empty() {
+        Assertions.assertThat(sut.sum("")).isEqualTo(0);
+        Assertions.assertThat(sut.sum(null)).isEqualTo(0);
+    }
+
+    @DisplayName("숫자 하나를 입력할 경우 해당 숫자를 반환한다.")
+    @Test
+    void single() {
+        Assertions.assertThat(sut.sum("2")).isEqualTo(2);
     }
 }
