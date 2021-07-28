33557ab4f6c09d4a5ff0857f35448616dc1555b8
apdkeb <apdkeb@naver.com>
2021-07-28 16:39:22 +0900

    feat(stringcalculator) : String to Int
    2021. 07. 28.
    - 문자열을 정수로 변환
    - 마이너스 경우 예외 처리
    
    Resolves : loop-study
    See also : serverwizard

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,11 +1,33 @@
 package stringcalculator;
 
 public class StringCalculator {
+    private static final int TWO_LENGTH = 2;
+    private static final String ERROR_MINUS_NUMBER_MESSAGE = "마이너스 숫자는 불가능합니다.";
 
     public int add(String text) {
         if (isEmpty(text)) {
             return 0;
         }
+
+        if (isLessTwoLength(text)) {
+            return stringToInt(text);
+        }
+
+        return 1;
+    }
+
+    private int stringToInt(String text) {
+        int result = Integer.parseInt(text);
+
+        if (result < 0) {
+            throw new IllegalArgumentException(ERROR_MINUS_NUMBER_MESSAGE);
+        }
+
+        return result;
+    }
+
+    private boolean isLessTwoLength(String text) {
+        return text.length() <= 2;
     }
 
     private boolean isEmpty(String text) {
