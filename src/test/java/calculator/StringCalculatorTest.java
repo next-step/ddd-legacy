4d116a94c26c4729460bbd6582d1b6295627f453
bgpark <bgpark82@gmail.com>
2021-07-28 23:04:04 +0900

    test(calculator): 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환 테스트

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -3,6 +3,7 @@ package calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -15,5 +16,13 @@ public class StringCalculatorTest {
     void emptyOrNull(final String text) {
         StringCalculator calculator = new StringCalculator();
         assertThat(calculator.add(text)).isZero();
+    }
+
+    @DisplayName(value = "숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
+    @ParameterizedTest(name = "{0} 입력")
+    @ValueSource(strings = {"1","10","100"})
+    void oneNumber(final String text) {
+        StringCalculator calculator = new StringCalculator();
+        assertThat(calculator.add(text)).isSameAs(Integer.parseInt(text));
     }
 }
