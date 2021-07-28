8a0565d5f1597ac2eaec13ed3ce91f798ecfb803
jonny-cho <andante2183@gmail.com>
2021-07-28 21:17:07 +0900

    refactor: 들여쓰기 1단계로 수정

--- src/test/java/calculator/StringCalculator.java
+++ src/test/java/calculator/StringCalculator.java
@@ -8,30 +8,31 @@ import org.thymeleaf.util.StringUtils;
 public class StringCalculator {
 
     public static final String DELIMITER = ",|:";
+    public static final String SEPARATOR = "|";
     public static final Pattern pattern = Pattern.compile("//(.)\n(.*)");
+    public static final char NEGATIVE_CHAR = '-';
+    public static final int ZERO = 0;
+    public static final int ONE = 1;
+    public static final int TWO = 2;
 
     public int calculate(final String stringNumber) {
         if (StringUtils.isEmpty(stringNumber)) {
-            return 0;
+            return ZERO;
         }
         final Matcher matcher = pattern.matcher(stringNumber);
         if (matcher.find()) {
-            final String customDelimiter = matcher.group(1);
-            final String targetNumber = matcher.group(2);
-            return calculate(targetNumber, DELIMITER + "|" + customDelimiter);
+            final String customDelimiter = matcher.group(ONE);
+            final String targetNumber = matcher.group(TWO);
+            return calculate(targetNumber, DELIMITER + SEPARATOR + customDelimiter);
         }
         return calculate(stringNumber, DELIMITER);
     }
 
     private int calculate(final String stringNumber, final String delimiter) {
-        final String[] tokens = stringNumber.split(delimiter);
-        if (tokens.length == 1) {
-            final int number = Integer.parseInt(tokens[0]);
-            if (number < 0) {
-                throw new RuntimeException("음수는 계산할 수 없습니다");
-            }
+        if (stringNumber.charAt(ZERO) == NEGATIVE_CHAR) {
+            throw new RuntimeException("음수는 계산할 수 없습니다");
         }
-        return Arrays.stream(tokens)
+        return Arrays.stream(stringNumber.split(delimiter))
             .mapToInt(Integer::parseInt)
             .sum();
     }
