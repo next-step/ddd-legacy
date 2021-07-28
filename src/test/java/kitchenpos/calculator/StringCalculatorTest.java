8f204f72f94dc3ad6db8c49fb8d794a504798e2a
y2o2u2n <y2o2u2n@gmail.com>
2021-07-28 21:52:02 +0900

    feat(calculator): add skeleton code and TODOs

---
+++ src/test/java/kitchenpos/calculator/StringCalculatorTest.java
@@ -0,0 +1,12 @@
+package kitchenpos.calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+
+class StringCalculatorTest {
+    private  StringCalculator sut;
+
+    @BeforeEach
+    void setUp() {
+        sut = new StringCalculator();
+    }
+}
