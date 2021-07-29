30c33fa95ef80025fd58089545ee4ff2b80a2dd9
Pyro(고정완) <rhwjddhks@gmail.com>
2021-07-29 20:24:27 +0900

    refactor: Number 와 Numbers 클래스를 통해 객체의 역할을 분리함

---
+++ src/main/java/calculator/Numbers.java
@@ -0,0 +1,34 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Numbers {
+    private final List<Number> numbers;
+
+    private Numbers(final List<Number> numbers) {
+        this.numbers = numbers;
+    }
+
+    public static Numbers of(final String text) {
+        String delimiter = ",|\\:";
+        final String[] splitText = text.split("\n");
+        if (splitText.length > 1) {
+            delimiter += String.format("|\\%s", splitText[0].charAt(2));
+        }
+        final String numberText = splitText.length > 1
+                ? splitText[1]
+                : splitText[0];
+
+        return new Numbers(Arrays.stream(numberText.split(delimiter))
+                .map(Number::of)
+                .collect(Collectors.toList()));
+    }
+
+    public int sum() {
+        return numbers.stream()
+                .mapToInt(Number::toInteger)
+                .sum();
+    }
+}
--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,29 +1,13 @@
 package calculator;
 
-import java.util.Arrays;
-
 public class StringCalculator {
-    public int add(String text) {
-        if (text == null || text.isEmpty()) {
-            return 0;
-        }
-        StringParser stringParser = StringParser.of(text);
-        String numberText = stringParser.getNumberText();
-        String delimiter = stringParser.getDelimiter();
-        try {
-            return Arrays.stream(numberText.split(delimiter))
-                    .mapToInt(Integer::parseInt)
-                    .map(this::validateNumber)
-                    .sum();
-        } catch (NumberFormatException e) {
-            throw new RuntimeException("숫자가 아닌 텍스트를 더할 수는 없습니다.");
-        }
-    }
+    private static final int ZERO = 0;
 
-    private int validateNumber(int number) {
-        if (number < 0) {
-            throw new RuntimeException("음수를 전달할 경우 RuntimeException 예외가 발생합니다.");
+    public int add(final String text) {
+        if (text == null || text.isEmpty()) {
+            return ZERO;
         }
-        return number;
+        final Numbers numbers = Numbers.of(text);
+        return numbers.sum();
     }
 }
