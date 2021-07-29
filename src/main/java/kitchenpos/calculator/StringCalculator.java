7be902a3abfedbe45059aa02e1ea22019e6b93c6
y2o2u2n <y2o2u2n@gmail.com>
2021-07-30 00:50:02 +0900

    refactor(calculator): extract magic number to constant

--- src/main/java/kitchenpos/calculator/StringCalculator.java
+++ src/main/java/kitchenpos/calculator/StringCalculator.java
@@ -7,6 +7,8 @@ import java.util.regex.Pattern;
 
 public class StringCalculator {
     private static final Pattern CUSTOM_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final Integer CUSTOM_DELIMITER_GROUP = 1;
+    private static final Integer CUSTOM_TEXT_GROUP = 2;
     private static final String DEFAULT_DELIMITERS = "[,:]";
     private static final Integer DEFAULT_VALUE = 0;
 
@@ -30,8 +32,8 @@ public class StringCalculator {
     private String[] split(String text) {
         Matcher m = CUSTOM_PATTERN.matcher(text);
         if (m.find()) {
-            String customDelimiter = m.group(1);
-            return m.group(2).split(customDelimiter);
+            String customDelimiter = m.group(CUSTOM_DELIMITER_GROUP);
+            return m.group(CUSTOM_TEXT_GROUP).split(customDelimiter);
         }
         return text.split(DEFAULT_DELIMITERS);
     }
