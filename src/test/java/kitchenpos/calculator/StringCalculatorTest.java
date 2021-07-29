28450c3565a8c31387d2e25a36301e8581ab1b76
y2o2u2n <y2o2u2n@gmail.com>
2021-07-30 00:37:27 +0900

    refactor(calculator): use @ParameterizedTest

--- src/test/java/kitchenpos/calculator/StringCalculatorTest.java
+++ src/test/java/kitchenpos/calculator/StringCalculatorTest.java
@@ -3,7 +3,10 @@ package kitchenpos.calculator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class StringCalculatorTest {
     private  StringCalculator sut;
@@ -14,36 +17,37 @@ class StringCalculatorTest {
     }
 
     @DisplayName("빈 값을 입력할 경우 0을 반환한다.")
-    @Test
-    void empty() {
-        Assertions.assertThat(sut.sum("")).isEqualTo(0);
-        Assertions.assertThat(sut.sum(null)).isEqualTo(0);
+    @ParameterizedTest
+    @NullAndEmptySource
+    void empty(String text) {
+        Assertions.assertThat(sut.sum(text)).isEqualTo(0);
     }
 
     @DisplayName("숫자 하나를 입력할 경우 해당 숫자를 반환한다.")
-    @Test
-    void single() {
-        Assertions.assertThat(sut.sum("2")).isEqualTo(2);
+    @ParameterizedTest
+    @CsvSource(value = {"2*2"}, delimiter = '*')
+    void single(String text, int expected) {
+        Assertions.assertThat(sut.sum(text)).isEqualTo(expected);
     }
 
     @DisplayName("쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환한다.")
-    @Test
-    void multiple() {
-        Assertions.assertThat(sut.sum("1,2")).isEqualTo(3);
-        Assertions.assertThat(sut.sum("1,2,3")).isEqualTo(6);
-        Assertions.assertThat(sut.sum("1,2:3")).isEqualTo(6);
+    @ParameterizedTest
+    @CsvSource(value = {"1,2*3", "1,2,3*6", "1,2:3*6"}, delimiter = '*')
+    void multiple(String text, int expected) {
+        Assertions.assertThat(sut.sum(text)).isEqualTo(expected);
     }
 
     @DisplayName("앞의 기본 구분자(쉼표, 콜론) 외에 커스텀 구분자를 지정할 수 있다.")
-    @Test
-    void customDelimiter() {
-        Assertions.assertThat(sut.sum("//;\n1;2;3")).isEqualTo(6);
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\n1;2;3"})
+    void customDelimiter(String text) {
+        Assertions.assertThat(sut.sum(text)).isEqualTo(6);
     }
 
     @DisplayName("문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw 한다.")
-    @Test
-    void notNumberOrNegative() {
-        Assertions.assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> sut.sum("1,2,z"));
-        Assertions.assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> sut.sum("1,-1"));
+    @ParameterizedTest
+    @ValueSource(strings = {"a,b,c", "1,-1"})
+    void notNumberOrNegative(String text) {
+        Assertions.assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> sut.sum(text));
     }
 }
