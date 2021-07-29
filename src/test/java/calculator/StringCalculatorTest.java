a753613e1515184e98044241339c878f29c6a0df
Pyro(고정완) <rhwjddhks@gmail.com>
2021-07-29 19:35:13 +0900

    test: 음수만이 아니라 문자열이 들어오는 경우의 테스트를 추가

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -59,4 +59,11 @@ class StringCalculatorTest {
         assertThatExceptionOfType(RuntimeException.class)
                 .isThrownBy(() -> calculator.add("-1"));
     }
+
+    @DisplayName(value = "문자열 계산기에 숫자가 아닌 문자열 전달하는 경우 RuntimeException 예외 처리를 한다.")
+    @Test
+    void notNumber() {
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> calculator.add("abc"));
+    }
 }
