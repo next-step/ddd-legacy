c1dafa7361b9849f9981bff989d2c4b725cca751
bgpark <bgpark82@gmail.com>
2021-07-29 23:42:53 +0900

    test(calculator): 커스텀 구분자로 더하기 테스트

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -45,4 +45,11 @@ public class StringCalculatorTest {
     void colons(final String text) {
         assertThat(calculator.add(text)).isSameAs(6);
     }
+
+    @DisplayName(value = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
+    @ParameterizedTest(name = "{0} 더하기")
+    @ValueSource(strings = {"//;\n1;2;3"})
+    void customDelimiter(final String text) {
+        assertThat(calculator.add(text)).isSameAs(6);
+    }
 }
