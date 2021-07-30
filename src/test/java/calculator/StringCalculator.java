4f5a3e077e7c03011480ec925cf626573d6a995e
blossun <blossunn@gmail.com>
2021-07-30 16:29:42 +0900

    refactor: Numbers 일급 컬렉션 생성

--- src/test/java/calculator/StringCalculator.java
+++ src/test/java/calculator/StringCalculator.java
@@ -1,24 +1,13 @@
 package calculator;
 
-import java.util.stream.Stream;
-
 public class StringCalculator {
     public int add(String text) {
         if (isBlank(text)) {
             return 0;
         }
-        return Stream.of(StringUtil.split(text))
-                .mapToInt(Integer::parseInt)
-                .peek(this::isNegative)
-                .sum();
+        return Numbers.convertToNumbers(text).sum();
     }
 
-    private void isNegative(int n) {
-        if (n < 0) {
-            throw new RuntimeException();
-        }
-    }
-    
     private boolean isBlank(String text) {
         return text == null || text.isEmpty();
     }
