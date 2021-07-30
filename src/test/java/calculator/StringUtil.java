05ccf63f76c5cb7a26c68d58e2d0136a560b5cbb
blossun <blossunn@gmail.com>
2021-07-30 16:05:28 +0900

    refactor: 매직 넘버 제거

--- src/test/java/calculator/StringUtil.java
+++ src/test/java/calculator/StringUtil.java
@@ -4,12 +4,17 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringUtil {
+    private static final String DEFAULT_DELIMITER = "[,:]";
+    private static final Pattern PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final int ONE = 1;
+    private static final int TWO = 2;
+
     public static String[] split(String text) {
-        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(text);
+        Matcher matcher = PATTERN.matcher(text);
         if (matcher.find()) {
-            String customDelimiter = matcher.group(1);
-            return matcher.group(2).split(customDelimiter);
+            String customDelimiter = matcher.group(ONE);
+            return matcher.group(TWO).split(customDelimiter);
         }
-        return text.split("[,:]");
+        return text.split(DEFAULT_DELIMITER);
     }
 }
