42759a146ff0045c71d37fb18317e08ee730892e
bgpark <bgpark82@gmail.com>
2021-08-01 19:23:19 +0900

    refactoring(calculator): @ParameterizedTest로 변경

--- src/test/java/calculator/number/PositiveNumberTest.java
+++ src/test/java/calculator/number/PositiveNumberTest.java
@@ -1,23 +1,27 @@
 package calculator.number;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
+@DisplayName("PositiveNumber 테스트")
 public class PositiveNumberTest {
 
     @DisplayName(value = "음수일 경우, RuntimeException 발생한다")
-    @Test
-    void negative() {
+    @ParameterizedTest(name = "음수 {0}인 경우, RuntimeException 발생")
+    @ValueSource(strings = {"-1","-2","-10"})
+    void negative(final String text) {
         assertThatExceptionOfType(RuntimeException.class)
-                .isThrownBy(() -> new PositiveNumber("-1"));
+                .isThrownBy(() -> new PositiveNumber(text));
     }
 
     @DisplayName(value = "숫자가 아닌 문자인 경우, RuntimeException 발생한다")
-    @Test
-    void text() {
+    @ParameterizedTest(name = "{0} 문자인 경우, RuntimeException 발생")
+    @ValueSource(strings = {"테스트","abc","we2"})
+    void text(final String text) {
         assertThatExceptionOfType(RuntimeException.class)
-                .isThrownBy(() -> new PositiveNumber("a"));
+                .isThrownBy(() -> new PositiveNumber(text));
     }
 }
