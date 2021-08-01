090d28d281654a39dbc11f24430d700720977cab
10M <tenmillionwon@gmail.com>
2021-08-01 20:51:52 +0900

    feat: add runtime exception when the number is negative

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -16,13 +16,29 @@ public class StringCalculator {
 		if (m.find()) {
 			String customDelimiter = m.group(1);
 			String[] tokens= m.group(2).split(customDelimiter);
-			return Arrays.stream(tokens).mapToInt(i -> Integer.parseInt(i)).sum();
+			return Arrays.stream(tokens).mapToInt(i -> {
+				int number = Integer.parseInt(i);
+				if (number < 0) {
+					throw new RuntimeException();
+				}
+				return number;
+			}).sum();
 		}
 
 		String[] numbers = inputNumber.split(",|:");
 
 		if (numbers.length > 1) {
-			return Arrays.stream(numbers).mapToInt(i -> Integer.parseInt(i)).sum();
+			return Arrays.stream(numbers).mapToInt(i -> {
+				int number = Integer.parseInt(i);
+				if (number < 0) {
+					throw new RuntimeException();
+				}
+				return number;
+			}).sum();
+		}
+
+		if (Integer.parseInt(inputNumber) < 0) {
+			throw new RuntimeException();
 		}
 
 		return Integer.parseInt(inputNumber);
