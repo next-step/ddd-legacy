3e07aa546a6ff32d25423b782aeb60ffbc033d3e
10M <tenmillionwon@gmail.com>
2021-08-01 20:19:53 +0900

    feat: add function that returns 0 if input is empty or null

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,4 +1,12 @@
 package stringcalculator;
 
 public class StringCalculator {
+
+	public int add(String inputNumber) {
+
+		if (inputNumber == null || inputNumber.trim().isEmpty()) {
+			return 0;
+		}
+		return 0;
+	}
 }
--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -1,6 +1,11 @@
 package stringcalculator;
 
+import static org.assertj.core.api.Assertions.*;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 public class StringCalculatorTest {
 
@@ -9,6 +14,13 @@ public class StringCalculatorTest {
 	@BeforeEach
 	void setUp() {
 		calculator = new StringCalculator();
+	}
+
+	@DisplayName(value = "빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
+	@ParameterizedTest
+	@NullAndEmptySource
+	void emptyOrNull(final String text) {
+		assertThat(calculator.add(text)).isZero();
 	}
 
 }
