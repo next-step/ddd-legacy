b0c83804263e5dbd6aac592b98a006cf7975acba
Heechul So <thrrkthrrk@gmail.com>
2021-08-01 22:55:47 +0900

    fix: Fix magic number to static final

--- src/test/java/calculator/StringAdditionCalculator.java
+++ src/test/java/calculator/StringAdditionCalculator.java
@@ -1,9 +1,11 @@
 package calculator;
 
 public class StringAdditionCalculator {
+    private static final Integer EMPTY_RESULT = 0;
+
     public Integer add(String text) {
         if (text == null || text.isBlank()) {
-            return 0;
+            return EMPTY_RESULT;
         }
         return new Numbers(text).sum();
     }
