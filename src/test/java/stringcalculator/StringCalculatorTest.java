a9928e6499573cd5e24ce9ac516d2e87a326e6a1
10M <tenmillionwon@gmail.com>
2021-08-01 20:16:16 +0900

    feat: add calculator test class

---
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,14 @@
+package stringcalculator;
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class StringCalculatorTest {
+
+	private StringCalculator calculator;
+
+	@BeforeEach
+	void setUp() {
+		calculator = new StringCalculator();
+	}
+
+}
