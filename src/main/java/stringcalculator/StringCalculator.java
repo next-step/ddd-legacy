930c49bed9ae9e63fcf7c8624107936922607bbc
10M <tenmillionwon@gmail.com>
2021-08-01 20:25:53 +0900

    feat: add function that return the sum of two numbers

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,11 +1,19 @@
 package stringcalculator;
 
+import java.util.Arrays;
+
 public class StringCalculator {
 
 	public int add(String inputNumber) {
 
 		if (inputNumber == null || inputNumber.trim().isEmpty()) {
 			return 0;
+		}
+
+		String[] numbers = inputNumber.split(",");
+
+		if (numbers.length > 1) {
+			return Arrays.stream(numbers).mapToInt(i -> Integer.parseInt(i)).sum();
 		}
 
 		return Integer.parseInt(inputNumber);
