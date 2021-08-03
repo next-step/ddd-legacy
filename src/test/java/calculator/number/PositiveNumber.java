86cec92e0b9cff19b0c62fca885276a435e5da4e
bgpark <bgpark82@gmail.com>
2021-08-03 10:11:50 +0900

    refactoring(calculator): equals와 hashcode 메소드 override

--- src/test/java/calculator/number/PositiveNumber.java
+++ src/test/java/calculator/number/PositiveNumber.java
@@ -1,5 +1,7 @@
 package calculator.number;
 
+import java.util.Objects;
+
 public class PositiveNumber implements Number {
 
     private static final int VALUE_ZERO = 0;
@@ -43,5 +45,21 @@ public class PositiveNumber implements Number {
         if (number < VALUE_ZERO) {
             throw new RuntimeException(NEGATIVE_EXCEPTION_MESSAGE);
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!(o instanceof PositiveNumber)) {
+            return false;
+        }
+        PositiveNumber that = (PositiveNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
