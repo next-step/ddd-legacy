60d44b52c3f10f685a8937a2dbc90141d3d79095
DongHyukki <kderr2791@gmail.com>
2021-08-03 10:19:53 +0900

    feat: test용 profile 구성

--- src/test/java/kitchenpos/integration/IntegrationTestRunner.java
+++ src/test/java/kitchenpos/integration/IntegrationTestRunner.java
@@ -1,7 +1,10 @@
 package kitchenpos.integration;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Profile;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 public class IntegrationTestRunner {
 }
