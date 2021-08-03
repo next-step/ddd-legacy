bfaa2be95bcb50bae91253283232198d84548e5a
DongHyukki <kderr2791@gmail.com>
2021-08-03 23:08:55 +0900

    test: MenuTest 추가
    display Test 추가
    hide Test 추가
    findAll Test 추가

--- src/test/java/kitchenpos/integration/MenuTest.java
+++ src/test/java/kitchenpos/integration/MenuTest.java
@@ -8,7 +8,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
-import java.util.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
@@ -218,7 +221,7 @@ public class MenuTest extends IntegrationTestRunner {
 
     @DisplayName("메뉴를 생성한다 ( 메뉴 생성시 요청된 `메뉴에 속한 제품`의 가격의 합이 영속화 된 제품의 가격의 합보다 클 수 없다. )")
     @TestAndRollback
-    public void create_with_not_equal_persisted_product_price_sum() {
+    public void create_with_bigger_than_persisted_product_price_sum() {
         //give
         int biggerPrice = 40000;
         int lowerPrice = 15000;
@@ -423,7 +426,7 @@ public class MenuTest extends IntegrationTestRunner {
 
     @DisplayName("메뉴의 가격을 변경한다. ( 변경 요청된 가격은 메뉴에 구성된 제품의 합보다 클 수 없다. )")
     @TestAndRollback
-    public void change_price_with_not_equals_products_price_sum() {
+    public void change_price_with_bigger_than_products_price_sum() {
         //given
         final MenuGroup menuGroup = new MenuGroup();
         menuGroup.setId(MENU_GROUP_ID);
@@ -462,7 +465,7 @@ public class MenuTest extends IntegrationTestRunner {
     @TestAndRollback
     public void change_price() {
         //given
-        BigDecimal changePrice = BigDecimal.valueOf(10000);
+        final BigDecimal changePrice = BigDecimal.valueOf(10000);
 
         final MenuGroup menuGroup = new MenuGroup();
         menuGroup.setId(MENU_GROUP_ID);
@@ -498,6 +501,246 @@ public class MenuTest extends IntegrationTestRunner {
         //then
         final Menu findMenu = menuRepository.findById(MENU_ID).get();
         assertThat(findMenu.getPrice()).isEqualTo(changePrice);
+    }
+
+    @DisplayName("해당 메뉴를 노출 시킨다. ( 요청 메뉴 ID는 영속화 된 메뉴여야 한다. )")
+    @TestAndRollback
+    public void display_with_not_persisted() {
+        //given
+
+        //when, then
+        assertThatExceptionOfType(NoSuchElementException.class)
+                .isThrownBy(() -> menuService.display(MENU_ID));
+    }
+
+    @DisplayName("해당 메뉴를 노출 시킨다. ( 요청된 메뉴 ID의 메뉴의 가격은 제품가격의 합보다 작아야 한다. )")
+    @TestAndRollback
+    public void display_with_bigger_than_products_price_sum() {
+        //given
+        final BigDecimal biggerPrice = BigDecimal.valueOf(20000);
+
+        final MenuGroup menuGroup = new MenuGroup();
+        menuGroup.setId(MENU_GROUP_ID);
+        menuGroup.setName("메인 메뉴");
+
+        menuGroupRepository.save(menuGroup);
+
+        final Product product_1 = new Product();
+        product_1.setId(PRODUCT_ID);
+        product_1.setName("후라이드 치킨");
+        product_1.setPrice(BigDecimal.valueOf(15000L));
+
+        productRepository.save(product_1);
+
+        final MenuProduct menuProduct_1 = new MenuProduct();
+        menuProduct_1.setProductId(PRODUCT_ID);
+        menuProduct_1.setQuantity(1L);
+        menuProduct_1.setProduct(product_1);
+
+        final Menu menu = new Menu();
+        menu.setId(MENU_ID);
+        menu.setName("후라이드 한마리");
+        menu.setDisplayed(false);
+        menu.setPrice(biggerPrice);
+        menu.setMenuProducts(List.of(menuProduct_1));
+        menu.setMenuGroup(menuGroup);
+
+        menuRepository.save(menu);
+
+        //when, then
+        assertThatExceptionOfType(IllegalStateException.class)
+                .isThrownBy(() -> menuService.display(MENU_ID));
+    }
+
+    @DisplayName("해당 메뉴를 노출 시킨다. ( 요청된 메뉴 ID의 메뉴의 가격은 제품가격의 합보다 작아야 한다. )")
+    @TestAndRollback
+    public void display_with_minus_price() {
+        //given
+        final BigDecimal minusPrice = BigDecimal.valueOf(-1);
+
+        final MenuGroup menuGroup = new MenuGroup();
+        menuGroup.setId(MENU_GROUP_ID);
+        menuGroup.setName("메인 메뉴");
+
+        menuGroupRepository.save(menuGroup);
+
+        final Product product_1 = new Product();
+        product_1.setId(PRODUCT_ID);
+        product_1.setName("후라이드 치킨");
+        product_1.setPrice(BigDecimal.valueOf(15000L));
+
+        productRepository.save(product_1);
+
+        final MenuProduct menuProduct_1 = new MenuProduct();
+        menuProduct_1.setProductId(PRODUCT_ID);
+        menuProduct_1.setQuantity(1L);
+        menuProduct_1.setProduct(product_1);
+
+        final Menu menu = new Menu();
+        menu.setId(MENU_ID);
+        menu.setName("후라이드 한마리");
+        menu.setDisplayed(false);
+        menu.setPrice(minusPrice);
+        menu.setMenuProducts(List.of(menuProduct_1));
+        menu.setMenuGroup(menuGroup);
+
+        menuRepository.save(menu);
+
+        //when, then
+        assertThatExceptionOfType(IllegalStateException.class)
+                .isThrownBy(() -> menuService.display(MENU_ID));
+    }
+
+    @DisplayName("해당 메뉴를 노출 시킨다.")
+    @TestAndRollback
+    public void display() {
+        //given
+        final MenuGroup menuGroup = new MenuGroup();
+        menuGroup.setId(MENU_GROUP_ID);
+        menuGroup.setName("메인 메뉴");
+
+        menuGroupRepository.save(menuGroup);
+
+        final Product product_1 = new Product();
+        product_1.setId(PRODUCT_ID);
+        product_1.setName("후라이드 치킨");
+        product_1.setPrice(BigDecimal.valueOf(15000));
+
+        productRepository.save(product_1);
+
+        final MenuProduct menuProduct_1 = new MenuProduct();
+        menuProduct_1.setProductId(PRODUCT_ID);
+        menuProduct_1.setQuantity(1L);
+        menuProduct_1.setProduct(product_1);
+
+        final Menu menu = new Menu();
+        menu.setId(MENU_ID);
+        menu.setName("후라이드 한마리");
+        menu.setDisplayed(false);
+        menu.setPrice(BigDecimal.valueOf(15000));
+        menu.setMenuProducts(List.of(menuProduct_1));
+        menu.setMenuGroup(menuGroup);
+
+        menuRepository.save(menu);
+
+        //when
+        menuService.display(MENU_ID);
+
+        //then
+        final Menu findMenu = menuRepository.findById(MENU_ID).get();
+        assertThat(findMenu.isDisplayed()).isEqualTo(true);
+    }
+
+    @DisplayName("해당 메뉴를 감춘다. ( 요청 메뉴 ID는 영속화 된 메뉴여야 한다. )")
+    @TestAndRollback
+    public void hide_with_not_persisted() {
+        //given
+
+        //when, then
+        assertThatExceptionOfType(NoSuchElementException.class)
+                .isThrownBy(() -> menuService.hide(MENU_ID));
+    }
+
+    @DisplayName("해당 메뉴를 감춘다.")
+    @TestAndRollback
+    public void hide() {
+        //given
+        final MenuGroup menuGroup = new MenuGroup();
+        menuGroup.setId(MENU_GROUP_ID);
+        menuGroup.setName("메인 메뉴");
+
+        menuGroupRepository.save(menuGroup);
+
+        final Product product_1 = new Product();
+        product_1.setId(PRODUCT_ID);
+        product_1.setName("후라이드 치킨");
+        product_1.setPrice(BigDecimal.valueOf(15000));
+
+        productRepository.save(product_1);
+
+        final MenuProduct menuProduct_1 = new MenuProduct();
+        menuProduct_1.setProductId(PRODUCT_ID);
+        menuProduct_1.setQuantity(1L);
+        menuProduct_1.setProduct(product_1);
+
+        final Menu menu = new Menu();
+        menu.setId(MENU_ID);
+        menu.setName("후라이드 한마리");
+        menu.setDisplayed(true);
+        menu.setPrice(BigDecimal.valueOf(15000));
+        menu.setMenuProducts(List.of(menuProduct_1));
+        menu.setMenuGroup(menuGroup);
+
+        menuRepository.save(menu);
+
+        //when
+        menuService.hide(MENU_ID);
+
+        //then
+        final Menu findMenu = menuRepository.findById(MENU_ID).get();
+        assertThat(findMenu.isDisplayed()).isEqualTo(false);
+    }
+
+    @DisplayName("모든 메뉴를 조회한다.")
+    @TestAndRollback
+    public void findAll() {
+        //given
+        final MenuGroup menuGroup = new MenuGroup();
+        menuGroup.setId(MENU_GROUP_ID);
+        menuGroup.setName("메인 메뉴");
+
+        menuGroupRepository.save(menuGroup);
+
+        final Product product_1 = new Product();
+        product_1.setId(PRODUCT_ID);
+        product_1.setName("후라이드 치킨");
+        product_1.setPrice(BigDecimal.valueOf(15000));
+
+        productRepository.save(product_1);
+
+        final Product product_2 = new Product();
+        final UUID colaUuid = UUID.randomUUID();
+        product_2.setId(colaUuid);
+        product_2.setName("콜라");
+        product_2.setPrice(BigDecimal.valueOf(5000));
+
+        productRepository.save(product_2);
+
+        final MenuProduct menuProduct_1 = new MenuProduct();
+        menuProduct_1.setProductId(PRODUCT_ID);
+        menuProduct_1.setQuantity(1L);
+        menuProduct_1.setProduct(product_1);
+
+        final MenuProduct menuProduct_2 = new MenuProduct();
+        menuProduct_2.setProductId(colaUuid);
+        menuProduct_2.setQuantity(1L);
+        menuProduct_2.setProduct(product_2);
+
+        final Menu menu_1 = new Menu();
+        menu_1.setId(MENU_ID);
+        menu_1.setName("후라이드 한마리");
+        menu_1.setDisplayed(true);
+        menu_1.setPrice(BigDecimal.valueOf(15000));
+        menu_1.setMenuProducts(List.of(menuProduct_1));
+        menu_1.setMenuGroup(menuGroup);
+
+        final Menu menu_2 = new Menu();
+        menu_2.setId(UUID.randomUUID());
+        menu_2.setName("후라이드 한마리 셋트");
+        menu_2.setDisplayed(true);
+        menu_2.setPrice(BigDecimal.valueOf(20000));
+        menu_2.setMenuProducts(List.of(menuProduct_1, menuProduct_2));
+        menu_2.setMenuGroup(menuGroup);
+
+        menuRepository.saveAll(List.of(menu_1, menu_2));
+
+        //when
+        final List<Menu> menus = menuService.findAll();
+
+        //then
+        assertThat(menus.size()).isEqualTo(2);
+        assertThat(menus.get(0)).isEqualTo(menu_1);
+        assertThat(menus.get(1)).isEqualTo(menu_2);
     }
 
 
