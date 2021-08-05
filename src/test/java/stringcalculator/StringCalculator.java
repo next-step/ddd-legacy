7f4b84e3f7bbed0ec978db2aca1119194d98269d
rn00n <rn00n@naver.com>
2021-08-05 13:54:58 +0900

    Update 매직넘버 상수화

--- src/test/java/stringcalculator/StringCalculator.java
+++ src/test/java/stringcalculator/StringCalculator.java
@@ -1,11 +1,12 @@
 package stringcalculator;
 
 public class StringCalculator {
+    private final int EMPTY_STRING_RESULT_VALUE = 0;
     private Formula formula;
 
     public int add(String text) {
         if (text == null || text.isEmpty()) {
-            return 0;
+            return EMPTY_STRING_RESULT_VALUE;
         }
         this.formula = new Formula(text);
 
