6ed4ca88ef0d1e9fa66f3f229bc2b822d6bbf3dd
DongHyukki <kderr2791@gmail.com>
2021-08-07 22:39:30 +0900

    feat: UnitTestRunner 정의

---
+++ src/test/java/kitchenpos/unit/UnitTestRunner.java
@@ -0,0 +1,8 @@
+package kitchenpos.unit;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class UnitTestRunner {
+}
