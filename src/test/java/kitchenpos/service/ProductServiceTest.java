36030fca2648f11a68b6baa2bfef439753870e5f
bgpark <bgpark82@gmail.com>
2021-08-08 20:54:48 +0900

    test(step3): 상품 이름에 욕설 허용 여부 테스트

--- src/test/java/kitchenpos/service/ProductServiceTest.java
+++ src/test/java/kitchenpos/service/ProductServiceTest.java
@@ -5,14 +5,19 @@ import kitchenpos.domain.MenuRepository;
 import kitchenpos.domain.Product;
 import kitchenpos.domain.ProductRepository;
 import kitchenpos.infra.PurgomalumClient;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import static kitchenpos.step.ProductStep.createProduct;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
 
 @DisplayName("Product 서비스 테스트")
+@ExtendWith(MockitoExtension.class)
 public class ProductServiceTest {
 
     @Mock
@@ -22,12 +27,30 @@ public class ProductServiceTest {
     @Mock
     PurgomalumClient purgomalumClient;
 
+    ProductService productService;
+
+    @BeforeEach
+    void setUp() {
+        productService = new ProductService(productRepository, menuRepository, purgomalumClient);
+    }
+
     @DisplayName("상품의 가격은 0 이상 이어야 한다")
     @Test
     void createWithNegativePrice() {
         // given
-        ProductService productService = new ProductService(productRepository, menuRepository, purgomalumClient);
         Product product = createProduct("강정치킨", -1);
+
+        // when, then
+        assertThatThrownBy(() -> productService.create(product))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품 이름에 욕설을 허용하지 않는다")
+    @Test
+    void createWithoutBadWords() {
+        // given
+        Product product = createProduct("fuck", 17000);
+        when(purgomalumClient.containsProfanity(product.getName())).thenReturn(true);
 
         // when, then
         assertThatThrownBy(() -> productService.create(product))
