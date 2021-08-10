bdafc4a992931665548b565da18cd6edb233dead
bgpark <bgpark82@gmail.com>
2021-08-10 17:46:17 +0900

    test(step3): Menu 인수 테스트

---
+++ src/test/java/kitchenpos/menu/step/MenuProductSaveRequest.java
@@ -0,0 +1,21 @@
+package kitchenpos.menu.step;
+
+import java.util.UUID;
+
+public class MenuProductSaveRequest {
+    private UUID productId;
+    private int quantity;
+
+    public UUID getProductId() {
+        return productId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public MenuProductSaveRequest(final UUID productId, final int quantity) {
+        this.productId = productId;
+        this.quantity = quantity;
+    }
+}
