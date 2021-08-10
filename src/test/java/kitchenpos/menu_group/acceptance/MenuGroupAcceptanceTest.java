bf880e5fc1a757c1b6fdd5c6337f8710d987003d
bgpark <bgpark82@gmail.com>
2021-08-10 10:58:27 +0900

    refactor(step3): Step 메소드 분리

--- src/test/java/kitchenpos/menu_group/acceptance/MenuGroupAcceptanceTest.java
+++ src/test/java/kitchenpos/menu_group/acceptance/MenuGroupAcceptanceTest.java
@@ -1,6 +1,5 @@
 package kitchenpos.menu_group.acceptance;
 
-import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
 import kitchenpos.AcceptanceTest;
@@ -8,9 +7,9 @@ import kitchenpos.domain.MenuGroup;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.test.util.ReflectionTestUtils;
 
+import static kitchenpos.menu_group.step.MenuGroupStep.createMenuGroup;
+import static kitchenpos.menu_group.step.MenuGroupStep.requestCreateMenuGroup;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("MenuGroup 인수 테스트")
@@ -20,18 +19,16 @@ public class MenuGroupAcceptanceTest extends AcceptanceTest {
     @Test
     public void create() {
         // given
-        MenuGroup menuGroup = new MenuGroup();
-        ReflectionTestUtils.setField(menuGroup, "name", "두마리메뉴");
+        MenuGroup menuGroup = createMenuGroup("두마리메뉴");
 
         // when
-        ExtractableResponse<Response> response = RestAssured
-                .given().log().all()
-                .contentType(MediaType.APPLICATION_JSON_VALUE)
-                .body(menuGroup)
-                .when().post("/api/menu-groups")
-                .then().log().all().extract();
+        ExtractableResponse<Response> response = requestCreateMenuGroup(menuGroup);
 
         // then
+        assertCreateMenuGroup(response);
+    }
+
+    private void assertCreateMenuGroup(final ExtractableResponse<Response> response) {
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
         assertThat(response.as(MenuGroup.class).getId()).isNotNull();
         assertThat(response.as(MenuGroup.class).getName()).isEqualTo("두마리메뉴");
---
+++ src/test/java/kitchenpos/menu_group/step/MenuGroupStep.java
@@ -0,0 +1,26 @@
+package kitchenpos.menu_group.step;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import kitchenpos.domain.MenuGroup;
+import org.springframework.http.MediaType;
+import org.springframework.test.util.ReflectionTestUtils;
+
+public class MenuGroupStep {
+
+    public static ExtractableResponse<Response> requestCreateMenuGroup(final MenuGroup menuGroup) {
+        return RestAssured
+                .given().log().all()
+                .contentType(MediaType.APPLICATION_JSON_VALUE)
+                .body(menuGroup)
+                .when().post("/api/menu-groups")
+                .then().log().all().extract();
+    }
+
+    public static MenuGroup createMenuGroup(String name) {
+        MenuGroup menuGroup = new MenuGroup();
+        ReflectionTestUtils.setField(menuGroup, "name", name);
+        return menuGroup;
+    }
+}
