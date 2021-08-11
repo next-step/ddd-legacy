846cd02eacb5c61e89198b07879452754721d30f
jonny-cho <andante2183@gmail.com>
2021-08-12 01:13:42 +0900

    test(step3): ProductRepository 인터페이스 분리

--- src/main/java/kitchenpos/domain/ProductRepository.java
+++ src/main/java/kitchenpos/domain/ProductRepository.java
@@ -1,8 +1,17 @@
 package kitchenpos.domain;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-public interface ProductRepository extends JpaRepository<Product, UUID> {
+public interface ProductRepository {
+
+    Product save(Product product);
+
+    Optional<Product> findById(UUID id);
+
+    List<Product> findAll();
+
+    List<Product> findAllByIdIn(List<UUID> ids);
 }
+
