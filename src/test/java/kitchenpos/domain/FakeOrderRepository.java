0d1aa67ec637fc6a4190dd3f6f6537a489064078
Pyro(고정완) <rhwjddhks@gmail.com>
2021-08-11 17:01:44 +0900

    refactor: Repository  인터페이스를 Jpa 와 Fake 로 분리함

---
+++ src/test/java/kitchenpos/domain/FakeOrderRepository.java
@@ -0,0 +1,30 @@
+package kitchenpos.domain;
+
+import java.util.*;
+
+public class FakeOrderRepository implements OrderRepository {
+    Map<UUID, Order> orderMap = new HashMap<>();
+
+    @Override
+    public Order save(final Order order) {
+        orderMap.put(order.getId(), order);
+        return order;
+    }
+
+    @Override
+    public Optional<Order> findById(final UUID orderId) {
+        return Optional.ofNullable(orderMap.get(orderId));
+    }
+
+    @Override
+    public List<Order> findAll() {
+        return new ArrayList<>(orderMap.values());
+    }
+
+    @Override
+    public boolean existsByOrderTableAndStatusNot(final OrderTable orderTable, final OrderStatus status) {
+        return orderMap.values().stream()
+                .anyMatch(order -> order.getOrderTable().getId().equals(orderTable.getId())
+                        && !order.getStatus().equals(status));
+    }
+}
