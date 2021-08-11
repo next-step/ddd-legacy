da52b5f9d77d351790716621761ebadf6660f411
jonny-cho <andante2183@gmail.com>
2021-08-12 01:22:32 +0900

    test(step3): ProductServiceTest fake repository로 개선

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -1,7 +1,8 @@
 package kitchenpos.application;
 
-import static kitchenpos.application.fixture.MenuFixture.CHEAP_PRODUCT_MENUS;
-import static kitchenpos.application.fixture.MenuFixture.MENUS;
+import static kitchenpos.application.fixture.MenuFixture.CHEAP_PRODUCT_MENU;
+import static kitchenpos.application.fixture.MenuFixture.CHEAP_PRODUCT_MENU2;
+import static kitchenpos.application.fixture.ProductFixture.CHEAP_PRODUCT;
 import static kitchenpos.application.fixture.ProductFixture.CHEAP_PRODUCT_REQUEST;
 import static kitchenpos.application.fixture.ProductFixture.PRICE_NEGATIVE_PRODUCT;
 import static kitchenpos.application.fixture.ProductFixture.PRICE_NEGATIVE_PRODUCT_REQUEST;
@@ -10,17 +11,13 @@ import static kitchenpos.application.fixture.ProductFixture.PRICE_NULL_PRODUCT_R
 import static kitchenpos.application.fixture.ProductFixture.PRODUCT1;
 import static kitchenpos.application.fixture.ProductFixture.PRODUCT1_REQUEST;
 import static kitchenpos.application.fixture.ProductFixture.PRODUCT2;
-import static kitchenpos.application.fixture.ProductFixture.PRODUCTS;
 import static kitchenpos.application.fixture.ProductFixture.PRODUCT_WITH_NAME_REQUEST;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
 import kitchenpos.domain.Menu;
 import kitchenpos.domain.MenuRepository;
 import kitchenpos.domain.Product;
@@ -32,21 +29,15 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.mockito.Mock;
 
-class ProductServiceTest extends MockTest {
+class ProductServiceTest {
 
     private static final int ZERO = 0;
     private static final int ONE = 1;
 
-    @Mock
-    private ProductRepository productRepository;
-
-    @Mock
-    private MenuRepository menuRepository;
-
-    @Mock
-    private PurgomalumClient purgomalumClient;
+    private final ProductRepository productRepository = new InmemoryProductRepository();
+    private final MenuRepository menuRepository = new InmemoryMenuRepository();
+    private final PurgomalumClient purgomalumClient = new FakePurgomalumClient();
 
     private ProductService productService;
 
@@ -61,8 +52,6 @@ class ProductServiceTest extends MockTest {
         //given
         final Product product = PRODUCT1();
         final Product productRequest = PRODUCT1_REQUEST();
-
-        given(productRepository.save(any())).willReturn(product);
 
         //when
         final Product sut = productService.create(productRequest);
@@ -116,8 +105,6 @@ class ProductServiceTest extends MockTest {
         //given
         final Product productRequest = PRODUCT_WITH_NAME_REQUEST(profanityWord);
 
-        given(purgomalumClient.containsProfanity(profanityWord)).willReturn(true);
-
         //when, then
         assertThatExceptionOfType(IllegalArgumentException.class)
             .isThrownBy(() -> productService.create(productRequest));
@@ -127,11 +114,8 @@ class ProductServiceTest extends MockTest {
     @Test
     void changePrice() {
         //given
-        final Product product = PRODUCT1();
+        final Product product = productRepository.save(PRODUCT1());
         final Product productRequest = PRODUCT1_REQUEST();
-
-        given(productRepository.findById(any())).willReturn(Optional.of(product));
-        given(menuRepository.findAllByProductId(any())).willReturn(MENUS());
 
         //when
         final Product sut = productService.changePrice(product.getId(), productRequest);
@@ -144,7 +128,7 @@ class ProductServiceTest extends MockTest {
     @Test
     void changePriceNoPrice() {
         //given
-        final Product product = PRICE_NULL_PRODUCT();
+        final Product product = productRepository.save(PRICE_NULL_PRODUCT());
         final Product productRequest = PRICE_NULL_PRODUCT_REQUEST();
 
         //when, then
@@ -156,7 +140,7 @@ class ProductServiceTest extends MockTest {
     @Test
     void changePriceNegativePrice() {
         //given
-        final Product product = PRICE_NEGATIVE_PRODUCT();
+        final Product product = productRepository.save(PRICE_NEGATIVE_PRODUCT());
         final Product productRequest = PRICE_NEGATIVE_PRODUCT_REQUEST();
 
         //when, then
@@ -171,8 +155,6 @@ class ProductServiceTest extends MockTest {
         final Product product = PRODUCT1();
         final Product productRequest = PRODUCT1_REQUEST();
 
-        given(productRepository.findById(any())).willReturn(Optional.empty());
-
         //when, then
         assertThatExceptionOfType(NoSuchElementException.class)
             .isThrownBy(() -> productService.changePrice(product.getId(), productRequest));
@@ -182,15 +164,15 @@ class ProductServiceTest extends MockTest {
     @Test
     void changePriceMenuDisplay() {
         //given
-        final Product product = PRODUCT1();
+        final Product product = productRepository.save(CHEAP_PRODUCT());
         final Product productRequest = CHEAP_PRODUCT_REQUEST();
-        final List<Menu> menus = CHEAP_PRODUCT_MENUS();
 
-        given(productRepository.findById(any())).willReturn(Optional.of(product));
-        given(menuRepository.findAllByProductId(any())).willReturn(menus);
+        menuRepository.save(CHEAP_PRODUCT_MENU());
+        menuRepository.save(CHEAP_PRODUCT_MENU2());
 
         //when
         productService.changePrice(product.getId(), productRequest);
+        final List<Menu> menus = menuRepository.findAll();
 
         //then
         assertAll(
@@ -205,13 +187,13 @@ class ProductServiceTest extends MockTest {
     @Test
     void findAll() {
         //given
-        given(productRepository.findAll()).willReturn(PRODUCTS());
+        final Product product1 = productRepository.save(PRODUCT1());
+        final Product product2 = productRepository.save(PRODUCT2());
 
+        //when
         final List<Product> products = productService.findAll();
 
-        final Product product1 = PRODUCT1();
-        final Product product2 = PRODUCT2();
-
+        //then
         assertAll(
             () -> assertThat(products.get(ZERO)
                 .getName()).isEqualTo(product1.getName()),
--- src/test/java/kitchenpos/application/fixture/MenuFixture.java
+++ src/test/java/kitchenpos/application/fixture/MenuFixture.java
@@ -73,6 +73,10 @@ public class MenuFixture {
         return createMenu(UUID1, MENU_NAME1, PRICE1, MENU_GROUP_ID1, true, CHEAP_MENU_PRODUCTS());
     }
 
+    public static Menu CHEAP_PRODUCT_MENU2() {
+        return createMenu(UUID2, MENU_NAME2, PRICE2, MENU_GROUP_ID2, true, CHEAP_MENU_PRODUCTS());
+    }
+
     public static List<Menu> HIDED_MENUS() {
         return Arrays.asList(HIDED_MENU(), HIDED_MENU());
     }
@@ -82,7 +86,7 @@ public class MenuFixture {
     }
 
     public static List<Menu> CHEAP_PRODUCT_MENUS() {
-        return Arrays.asList(CHEAP_PRODUCT_MENU(), CHEAP_PRODUCT_MENU());
+        return Arrays.asList(CHEAP_PRODUCT_MENU(), CHEAP_PRODUCT_MENU2());
     }
 
     private static Menu createMenu(final UUID uuid, final String menuName, final long price, final UUID menuGroupId, final boolean isDisplay, final List<MenuProduct> menuProducts) {
