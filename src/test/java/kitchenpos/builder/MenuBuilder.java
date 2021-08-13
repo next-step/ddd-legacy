d20a4cf9eda03a755893c53d6790dae873b3bb52
skid901 <tlrudejr1993@gmail.com>
2021-08-14 01:06:50 +0900

    feat(kitchenpos/builder): add builders for product service testing

---
+++ src/test/java/kitchenpos/builder/MenuBuilder.java
@@ -0,0 +1,95 @@
+package kitchenpos.builder;
+
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuProduct;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.UUID;
+
+public final class MenuBuilder {
+    private UUID id;
+    private String name;
+    private BigDecimal price;
+    private MenuGroup menuGroup;
+    private boolean displayed;
+    private List<MenuProduct> menuProducts;
+    private UUID menuGroupId;
+
+    private MenuBuilder() {
+        id = UUID.randomUUID();
+        name = "후라이드치킨";
+        price = BigDecimal.valueOf(16_000L);
+        displayed = true;
+        menuProducts = Collections.singletonList(MenuProductBuilder.aMenuProduct()
+                .build()
+        );
+        menuGroup = null;
+        menuGroupId = null;
+    }
+
+    public static MenuBuilder aMenu() {
+        return new MenuBuilder();
+    }
+
+    public MenuBuilder setId(UUID id) {
+        this.id = id;
+        return this;
+    }
+
+    public MenuBuilder setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public MenuBuilder setPrice(BigDecimal price) {
+        this.price = price;
+        return this;
+    }
+
+    public MenuBuilder setPrice(Long price) {
+        this.price = BigDecimal.valueOf(price);
+        return this;
+    }
+
+    public MenuBuilder setMenuGroup(MenuGroup menuGroup) {
+        this.menuGroup = menuGroup;
+        this.menuGroupId = menuGroup.getId();
+        return this;
+    }
+
+    public MenuBuilder setDisplayed(boolean displayed) {
+        this.displayed = displayed;
+        return this;
+    }
+
+    public MenuBuilder setMenuProducts(List<MenuProduct> menuProducts) {
+        this.menuProducts = menuProducts;
+        return this;
+    }
+
+    public MenuBuilder setMenuProducts(MenuProduct... menuProducts) {
+        this.menuProducts = Arrays.asList(menuProducts);
+        return this;
+    }
+
+    public MenuBuilder setMenuGroupId(UUID menuGroupId) {
+        this.menuGroupId = menuGroupId;
+        return this;
+    }
+
+    public Menu build() {
+        Menu menu = new Menu();
+        menu.setId(id);
+        menu.setName(name);
+        menu.setPrice(price);
+        menu.setMenuGroup(menuGroup);
+        menu.setDisplayed(displayed);
+        menu.setMenuProducts(menuProducts);
+        menu.setMenuGroupId(menuGroupId);
+        return menu;
+    }
+}
---
+++ src/test/java/kitchenpos/builder/MenuProductBuilder.java
@@ -0,0 +1,56 @@
+package kitchenpos.builder;
+
+import kitchenpos.domain.MenuProduct;
+import kitchenpos.domain.Product;
+
+import java.util.Random;
+import java.util.UUID;
+
+public final class MenuProductBuilder {
+    private Long seq;
+    private Product product;
+    private long quantity;
+    private UUID productId;
+
+    private MenuProductBuilder() {
+        seq = new Random().nextLong();
+        product = ProductBuilder.aProduct()
+                .build();
+        quantity = 1L;
+        productId = product.getId();
+    }
+
+    public static MenuProductBuilder aMenuProduct() {
+        return new MenuProductBuilder();
+    }
+
+    public MenuProductBuilder setSeq(Long seq) {
+        this.seq = seq;
+        return this;
+    }
+
+    public MenuProductBuilder setProduct(Product product) {
+        this.product = product;
+        this.productId = product.getId();
+        return this;
+    }
+
+    public MenuProductBuilder setQuantity(long quantity) {
+        this.quantity = quantity;
+        return this;
+    }
+
+    public MenuProductBuilder setProductId(UUID productId) {
+        this.productId = productId;
+        return this;
+    }
+
+    public MenuProduct build() {
+        MenuProduct menuProduct = new MenuProduct();
+        menuProduct.setSeq(seq);
+        menuProduct.setProduct(product);
+        menuProduct.setQuantity(quantity);
+        menuProduct.setProductId(productId);
+        return menuProduct;
+    }
+}
