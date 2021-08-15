fd7efd5094c09950c8cd496cdaf586c55dc396d4
skid901 <tlrudejr1993@gmail.com>
2021-08-16 00:47:18 +0900

    refactor(kitchenpos): separate order and order table domain from JPA interface

--- src/main/java/kitchenpos/domain/OrderRepository.java
+++ src/main/java/kitchenpos/domain/OrderRepository.java
@@ -1,9 +1,15 @@
 package kitchenpos.domain;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-public interface OrderRepository extends JpaRepository<Order, UUID> {
-    boolean existsByOrderTableAndStatusNot(OrderTable orderTable, OrderStatus status);
+public interface OrderRepository {
+    Order save(Order order);
+
+    Optional<Order> findById(UUID orderId);
+
+    List<Order> findAll();
+
+    boolean existsByOrderTableAndStatusNot(OrderTable orderTable, OrderStatus completed);
 }
