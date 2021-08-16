5a8579fc3e2ee80b74d8f6409522cad87353e065
Sung Du Kim <sungdu.alex.kim@gmail.com>
2021-08-16 21:34:49 +0900

    test: OrderTableService 통합 테스트 추가

---
+++ src/test/java/kitchenpos/application/InMemoryOrderRepository.java
@@ -0,0 +1,47 @@
+package kitchenpos.application;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderRepository;
+import kitchenpos.domain.OrderStatus;
+import kitchenpos.domain.OrderTable;
+
+public class InMemoryOrderRepository implements OrderRepository {
+
+    private Map<UUID, Order> orders = new HashMap<>();
+
+    @Override
+    public boolean existsByOrderTableAndStatusNot(
+        final OrderTable orderTable,
+        final OrderStatus status
+    ) {
+        return orders.values()
+            .stream()
+            .noneMatch(order ->
+                Objects.equals(orderTable.getId(), order.getOrderTable().getId())
+                    && order.getStatus().equals(status)
+            );
+    }
+
+    @Override
+    public Order save(Order order) {
+        orders.put(order.getId(), order);
+        return order;
+    }
+
+    @Override
+    public Optional<Order> findById(final UUID id) {
+        return Optional.ofNullable(orders.get(id));
+    }
+
+    @Override
+    public List<Order> findAll() {
+        return new ArrayList<>(orders.values());
+    }
+}
