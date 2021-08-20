8b844230794b3a1619065c034fb00fdb3f9b1f9e
bgpark <bgpark82@gmail.com>
2021-08-20 22:08:11 +0900

    fix(step3): 메뉴 테스트 수정

--- src/test/java/kitchenpos/menu/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/menu/application/MenuServiceTest.java
@@ -12,10 +12,13 @@ import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.math.BigDecimal;
 import java.util.*;
 
 import static kitchenpos.menu.fixture.MenuFixture.*;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
@@ -35,18 +38,60 @@ public class MenuServiceTest {
 
     private MenuService menuService;
 
-    private MenuGroup 기본_한마리_메뉴_그룹;
-    private Product 기본_후라이드_상품;
-    private Menu 기본_후라이드_치킨_메뉴;
+    private String 후라이드;
+    private int 후라이드_가격;
+    private String 후라이드_치킨;
+    private int 후라이드_치킨_가격;
+    private long 한개;
+
+    private Menu 메뉴_생성_요청;
+    private MenuGroup 한마리_메뉴_그룹;
+    private Product 후라이드_상품;
+    private Menu 후라이드_한마리_메뉴;
+    private MenuProduct 후라이드_한마리_메뉴_상품;
+
     private UUID id;
 
     @BeforeEach
     void setUp() {
         menuService = new MenuService(menuRepository, menuGroupRepository, productRepository, purgomalumClient);
-        기본_한마리_메뉴_그룹 = 한마리_메뉴_그룹();
-        기본_후라이드_상품 = 후라이드_상품();
-        기본_후라이드_치킨_메뉴 = 후라이드_치킨_메뉴();
+        후라이드 = "후라이드";
+        후라이드_치킨 = "후라이드치킨";
+        후라이드_가격 = 16000;
+        후라이드_치킨_가격 = 16000;
+        한개 = 1L;
+
+        메뉴_생성_요청 = 메뉴_생성_요청(후라이드_치킨, 후라이드_가격, true, Arrays.asList(메뉴_상품_생성_요청(한개)));
+        한마리_메뉴_그룹 = 메뉴_그룹("한마리메뉴");
+        후라이드_상품 = 상품(후라이드, 후라이드_가격);
+        후라이드_한마리_메뉴_상품 = 메뉴_상품(1L, 한개, 후라이드_상품);
+        후라이드_한마리_메뉴 = 메뉴(후라이드_치킨, 후라이드_치킨_가격, true, 한마리_메뉴_그룹, Arrays.asList(후라이드_한마리_메뉴_상품));
         id = UUID.randomUUID();
+    }
+
+    @DisplayName("메뉴를 등록한다.")
+    @Test
+    public void create() {
+        // given
+        given(menuGroupRepository.findById(any())).willReturn(Optional.of(한마리_메뉴_그룹));
+        given(productRepository.findAllById(any())).willReturn(Arrays.asList(후라이드_상품));
+        given(productRepository.findById(any())).willReturn(Optional.of(후라이드_상품));
+        given(purgomalumClient.containsProfanity(anyString())).willReturn(false);
+        given(menuRepository.save(any())).willReturn(후라이드_한마리_메뉴);
+
+        // when
+        Menu createdMenu = menuService.create(메뉴_생성_요청);
+
+        // then
+        assertAll(
+                () -> assertThat(createdMenu.getName()).isEqualTo(후라이드_치킨),
+                () -> assertThat(createdMenu.getPrice()).isEqualTo(new BigDecimal(후라이드_치킨_가격)),
+                () -> assertThat(createdMenu.isDisplayed()).isEqualTo(true),
+                () -> assertThat(createdMenu.getMenuProducts().size()).isEqualTo(1),
+                () -> assertThat(createdMenu.getMenuProducts().get(0).getQuantity()).isEqualTo(한개),
+                () -> assertThat(createdMenu.getMenuProducts().get(0).getProduct().getPrice()).isEqualTo(new BigDecimal(후라이드_가격)),
+                () -> assertThat(createdMenu.getMenuProducts().get(0).getProduct().getName()).isEqualTo(후라이드)
+        );
     }
 
     @DisplayName("메뉴 가격이 음수인 경우 IllegalArgumentException을 던진다.")
@@ -54,10 +99,10 @@ public class MenuServiceTest {
     @ValueSource(ints = {-10,-100})
     public void createWithValidPrice(int price) {
         // given
-        Menu 음수_가격_후라이드_치킨_메뉴 = 후라이드_치킨_메뉴(price);
+        Menu 음수_가격_메뉴_요청 = 메뉴_생성_요청(후라이드_치킨, price, true, null);
 
         // when, then
-        assertThatThrownBy(() -> menuService.create(음수_가격_후라이드_치킨_메뉴))
+        assertThatThrownBy(() -> menuService.create(음수_가격_메뉴_요청))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -68,7 +113,7 @@ public class MenuServiceTest {
         given(menuGroupRepository.findById(any())).willReturn(Optional.empty());
 
         // when, then
-        assertThatThrownBy(() -> menuService.create(기본_후라이드_치킨_메뉴))
+        assertThatThrownBy(() -> menuService.create(메뉴_생성_요청))
                 .isInstanceOf(NoSuchElementException.class);
     }
 
@@ -76,13 +121,14 @@ public class MenuServiceTest {
     @Test
     public void createMenuProductToBeRegisteredAsProduct() {
         // given
-        Menu 메뉴상품_없는_후라이드_치킨_메뉴 = 후라이드_치킨_메뉴(Collections.emptyList());
+        List<MenuProduct> 빈_메뉴_상품 = Collections.emptyList();
+        Menu 메뉴상품_없는_메뉴 = 메뉴_생성_요청(후라이드_치킨, 후라이드_치킨_가격, true, 빈_메뉴_상품);
 
         given(menuGroupRepository.findById(any()))
-                .willReturn(Optional.of(기본_한마리_메뉴_그룹));
+                .willReturn(Optional.of(한마리_메뉴_그룹));
 
         // when, then
-        assertThatThrownBy(() -> menuService.create(메뉴상품_없는_후라이드_치킨_메뉴))
+        assertThatThrownBy(() -> menuService.create(메뉴상품_없는_메뉴))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -90,16 +136,15 @@ public class MenuServiceTest {
     @Test
     public void createWithValidNumberOfMenuProduct() {
         // given
-        Menu 후라이드_치킨_메뉴 = 후라이드_치킨_메뉴(Arrays.asList(
-                후라이드_메뉴_상품( 1L)));
+        List<Product> 빈_상품 = Collections.emptyList();
 
         given(menuGroupRepository.findById(any()))
-                .willReturn(Optional.of(기본_한마리_메뉴_그룹));
+                .willReturn(Optional.of(한마리_메뉴_그룹));
         given(productRepository.findAllById(any()))
-                .willReturn(Collections.emptyList());
+                .willReturn(빈_상품);
 
         // when, then
-        assertThatThrownBy(() -> menuService.create(후라이드_치킨_메뉴))
+        assertThatThrownBy(() -> menuService.create(메뉴_생성_요청))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -108,16 +153,16 @@ public class MenuServiceTest {
     @ValueSource(longs = {-10l,-100l})
     public void createWithValidQuantity(long quantity) {
         // given
-        Menu 후라이드_치킨_메뉴 = 후라이드_치킨_메뉴(Arrays.asList(
-                후라이드_메뉴_상품(quantity)));
+        Menu 상품_개수가_음수인_메뉴_생성_요청 = 메뉴_생성_요청(후라이드_치킨, 후라이드_치킨_가격, true, Arrays.asList(
+                메뉴_상품(1L, quantity, 후라이드_상품)));
 
         given(menuGroupRepository.findById(any()))
-                .willReturn(Optional.of(기본_한마리_메뉴_그룹));
+                .willReturn(Optional.of(한마리_메뉴_그룹));
         given(productRepository.findAllById(any()))
                 .willReturn(Collections.emptyList());
 
         // when, then
-        assertThatThrownBy(() -> menuService.create(후라이드_치킨_메뉴))
+        assertThatThrownBy(() -> menuService.create(상품_개수가_음수인_메뉴_생성_요청))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -126,12 +171,11 @@ public class MenuServiceTest {
     public void createWithLowerPriceThanSumOfMenuProduct() {
         // given
         int menuPrice = 17000;
-        Menu 후라이드_치킨_메뉴 = 후라이드_치킨_메뉴(menuPrice, Arrays.asList(
-                후라이드_메뉴_상품(1L)));
+        Menu 후라이드_치킨_메뉴 = 메뉴_생성_요청(후라이드, menuPrice, true, Arrays.asList(후라이드_한마리_메뉴_상품));
 
-        given(menuGroupRepository.findById(any())).willReturn(Optional.of(기본_한마리_메뉴_그룹));
-        given(productRepository.findAllById(any())).willReturn(Arrays.asList(기본_후라이드_상품));
-        given(productRepository.findById(any())).willReturn(Optional.of(기본_후라이드_상품));
+        given(menuGroupRepository.findById(any())).willReturn(Optional.of(한마리_메뉴_그룹));
+        given(productRepository.findAllById(any())).willReturn(Arrays.asList(후라이드_상품));
+        given(productRepository.findById(any())).willReturn(Optional.of(후라이드_상품));
 
         // when, then
         assertThatThrownBy(() -> menuService.create(후라이드_치킨_메뉴))
@@ -143,16 +187,15 @@ public class MenuServiceTest {
     @ValueSource(strings = {"fuck", "shit"})
     public void createWithoutBadWord(String name) {
         // given
-        given(productRepository.findAllById(any())).willReturn(Arrays.asList(기본_후라이드_상품));
-        given(productRepository.findById(any())).willReturn(Optional.of(기본_후라이드_상품));
-        given(menuGroupRepository.findById(any())).willReturn(Optional.of(기본_한마리_메뉴_그룹));
+        Menu 욕설_이름_메뉴_생성_요청 = 메뉴_생성_요청(name, 후라이드_가격, true, Arrays.asList(후라이드_한마리_메뉴_상품));
 
-        Menu 욕설_이름_메뉴 = 메뉴(name, 기본_후라이드_가격, Arrays.asList(
-                후라이드_메뉴_상품(1L)));
+        given(productRepository.findAllById(any())).willReturn(Arrays.asList(후라이드_상품));
+        given(productRepository.findById(any())).willReturn(Optional.of(후라이드_상품));
+        given(menuGroupRepository.findById(any())).willReturn(Optional.of(한마리_메뉴_그룹));
         given(purgomalumClient.containsProfanity(anyString())).willReturn(true);
 
         // when, then
-        assertThatThrownBy(() -> menuService.create(욕설_이름_메뉴))
+        assertThatThrownBy(() -> menuService.create(욕설_이름_메뉴_생성_요청))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -160,11 +203,7 @@ public class MenuServiceTest {
     @ParameterizedTest
     @ValueSource(ints = {-10, -100})
     public void changePriceWithValidPrice(int price) {
-        // given
-        Menu 후라이드_치킨_메뉴 = 후라이드_치킨_메뉴(price);
-
-        // when, then
-        assertThatThrownBy(() -> menuService.changePrice(id, 후라이드_치킨_메뉴))
+        assertThatThrownBy(() -> menuService.changePrice(id, 메뉴_가격_변경_요청(price)))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -173,13 +212,12 @@ public class MenuServiceTest {
     @Test
     public void changePriceWithLowerPriceThanSumOfMenuProduct() {
         // given
-        int menuPrice = 18000;
-        Menu 변경될_후라이드_치킨_메뉴 = 후라이드_치킨_메뉴(menuPrice, Arrays.asList(
-                후라이드_메뉴_상품( 1l)));
-        given(menuRepository.findById(id)).willReturn(Optional.of(기본_후라이드_치킨_메뉴));
+        Menu 메뉴_가격_변경_요청 = 메뉴_가격_변경_요청(18000);
+
+        given(menuRepository.findById(id)).willReturn(Optional.of(후라이드_한마리_메뉴));
 
         // when, then
-        assertThatThrownBy(() -> menuService.changePrice(id, 변경될_후라이드_치킨_메뉴))
+        assertThatThrownBy(() -> menuService.changePrice(id, 메뉴_가격_변경_요청))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -195,23 +233,23 @@ public class MenuServiceTest {
     }
 
 
-    @DisplayName("노출하려는 메뉴의 가격은 메뉴에 등록된 메뉴 상품의 가격의 합보다 크지 않은 경우 IllegalArgumentException을 던진다.")
+    @DisplayName("노출하려는 메뉴의 가격은 메뉴에 등록된 메뉴 상품의 가격의 합보다 크지 않은 경우 IllegalStateException을 던진다.")
     @Test
     public void displayMenuThatLessThanSumOfMenuProductPrice() {
         // given
-        int menuPrice = 18000;
-        int menuProductPrice = 17000;
-        Menu 후라이드_치킨_메뉴 = 후라이드_치킨_메뉴(menuPrice,Arrays.asList(
-                후라이드_메뉴_상품(menuProductPrice, 1l)));
+        int 메뉴_가격 = 18000;
+        int 메뉴_상품_가격 = 17000;
+        Menu 메뉴 = 메뉴(후라이드_치킨, 메뉴_가격, true, 한마리_메뉴_그룹, Arrays.asList(
+                메뉴_상품(1L, 한개, 상품(후라이드, 메뉴_상품_가격))));
 
-        given(menuRepository.findById(id)).willReturn(Optional.of(후라이드_치킨_메뉴));
+        given(menuRepository.findById(id)).willReturn(Optional.of(메뉴));
 
         // when, then
         assertThatThrownBy(() -> menuService.display(id))
                 .isInstanceOf(IllegalStateException.class);
     }
 
-    @DisplayName("등록되지 않은 메뉴인 경우 IllegalArgumentException을 던진다.")
+    @DisplayName("등록되지 않은 메뉴인 경우 NoSuchElementException을 던진다.")
     @Test
     public void hideWithRegisteredMenu() {
         // given
--- src/test/java/kitchenpos/menu/fixture/MenuFixture.java
+++ src/test/java/kitchenpos/menu/fixture/MenuFixture.java
@@ -6,129 +6,66 @@ import kitchenpos.domain.MenuProduct;
 import kitchenpos.domain.Product;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
 public class MenuFixture {
 
-    public static final int 기본_후라이드_가격 = 16000;
-
-    public static Menu 후라이드_치킨_메뉴() {
+    public static Menu 메뉴_생성_요청(String name, int price, boolean displayed, List<MenuProduct> menuProducts) {
         Menu menu = new Menu();
-        menu.setName("후라이드 치킨");
-        menu.setPrice(new BigDecimal(기본_후라이드_가격));
-        menu.setMenuGroup(한마리_메뉴_그룹());
-        menu.setMenuProducts(Arrays.asList(후라이드_메뉴_상품(1l)));
-        return menu;
-    }
-
-    public static Menu 후라이드_치킨_메뉴(List<MenuProduct> menuProducts) {
-        Menu menu = new Menu();
-        menu.setName("후라이드 치킨");
-        menu.setPrice(new BigDecimal(기본_후라이드_가격));
+        menu.setName(name);
+        menu.setPrice(new BigDecimal(price));
+        menu.setDisplayed(displayed);
+        menu.setMenuGroupId(UUID.randomUUID());
         menu.setMenuProducts(menuProducts);
         return menu;
     }
 
-    public static Menu 후라이드_치킨_메뉴(int price) {
+    public static MenuProduct 메뉴_상품_생성_요청(long quantity) {
+        MenuProduct menuProduct = new MenuProduct();
+        menuProduct.setProductId(UUID.randomUUID());
+        menuProduct.setQuantity(quantity);
+        return menuProduct;
+    }
+
+    public static Menu 메뉴(final String name, final int price, boolean displayed, final MenuGroup menuGroup, List<MenuProduct> menuProducts) {
         Menu menu = new Menu();
         menu.setId(UUID.randomUUID());
-        menu.setName("후라이드 치킨");
+        menu.setName(name);
         menu.setPrice(new BigDecimal(price));
+        menu.setDisplayed(displayed);
+        menu.setMenuGroup(menuGroup);
+        menu.setMenuProducts(menuProducts);
         return menu;
     }
 
-    public static Product 후라이드_상품(int price) {
+    public static MenuProduct 메뉴_상품(final long seq, final Long quantity, final Product product) {
+        MenuProduct menuProduct = new MenuProduct();
+        menuProduct.setProductId(UUID.randomUUID());
+        menuProduct.setQuantity(quantity);
+        menuProduct.setProduct(product);
+        menuProduct.setSeq(seq);
+        return menuProduct;
+    }
+
+    public static Product 상품(final String name, final int price) {
         Product product = new Product();
-        product.setName("후라이드");
+        product.setId(UUID.randomUUID());
+        product.setName(name);
         product.setPrice(new BigDecimal(price));
         return product;
     }
 
-    public static Product 후라이드_상품() {
-        Product product = new Product();
-        product.setName("후라이드");
-        product.setPrice(new BigDecimal(기본_후라이드_가격));
-        return product;
-    }
-
-    public static Menu 후라이드_치킨_메뉴(int price, List<MenuProduct> menuProducts) {
-        Menu menu = new Menu();
-        menu.setName("후라이드 치킨");
-        menu.setPrice(new BigDecimal(price));
-        menu.setMenuProducts(menuProducts);
-        return menu;
-    }
-
-    public static Menu 메뉴(String name, int price, List<MenuProduct> menuProducts) {
-        Menu menu = new Menu();
-        menu.setName(name);
-        menu.setPrice(new BigDecimal(price));
-        menu.setMenuProducts(menuProducts);
-        return menu;
-    }
-
-    public static MenuGroup 한마리_메뉴_그룹() {
+    public static MenuGroup 메뉴_그룹(String name) {
         MenuGroup menuGroup = new MenuGroup();
         menuGroup.setId(UUID.randomUUID());
-        menuGroup.setName("한마리메뉴");
-        return menuGroup;
-    }
-
-    public static MenuProduct 후라이드_메뉴_상품(Long quantity) {
-        Product 후라이드_상품 = 후라이드_상품();
-        MenuProduct menuProduct = new MenuProduct();
-        menuProduct.setProduct(후라이드_상품);
-        menuProduct.setQuantity(quantity);
-        return menuProduct;
-    }
-
-    public static MenuProduct 후라이드_메뉴_상품(int price, Long quantity) {
-        Product 후라이드_상품 = 후라이드_상품(price);
-        MenuProduct menuProduct = new MenuProduct();
-        menuProduct.setProduct(후라이드_상품);
-        menuProduct.setQuantity(quantity);
-        return menuProduct;
-    }
-
-    public static Menu createMenu(int price) {
-        Menu menu = new Menu();
-        menu.setPrice(new BigDecimal(price));
-        return menu;
-    }
-
-    public static Menu createMenu(int price, List<MenuProduct> menuProducts) {
-        Menu menu = createMenu(price);
-        menu.setMenuProducts(menuProducts);
-        return menu;
-    }
-
-    public static Menu createMenu(String name, int price, List<MenuProduct> menuProducts) {
-        Menu menu = createMenu(price, menuProducts);
-        menu.setName(name);
-        return menu;
-    }
-
-    public static Menu createMenu(String name, int price, boolean displayed, MenuGroup menuGroup, List<MenuProduct> menuProducts) {
-        Menu menu = createMenu(name, price, menuProducts);
-        menu.setDisplayed(displayed);
-        menu.setMenuGroup(menuGroup);
-        return menu;
-    }
-
-    public static MenuGroup createMenuGroup(UUID id, String name) {
-        MenuGroup menuGroup = new MenuGroup();
-        menuGroup.setId(id);
         menuGroup.setName(name);
         return menuGroup;
     }
 
-    public static MenuProduct createMenuProduct(Product product, Long quantity, UUID productId) {
-        MenuProduct menuProduct = new MenuProduct();
-        menuProduct.setProduct(product);
-        menuProduct.setQuantity(quantity);
-        menuProduct.setProductId(productId);
-        return menuProduct;
+    public static Menu 메뉴_가격_변경_요청(final int price) {
+        Menu menu = new Menu();
+        menu.setPrice(new BigDecimal(price));
+        return menu;
     }
 }
