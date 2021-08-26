4b2e87415e7686d872625c8a6a992a342199d2f2
wrallee <wrallee@naver.com>
2021-08-26 09:41:25 +0900

    test(MenuGroup): 메뉴 그룹 API 테스트 추가

--- src/test/java/kitchenpos/IntegrationTest.java
+++ src/test/java/kitchenpos/IntegrationTest.java
@@ -1,17 +1,25 @@
 package kitchenpos;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 public class IntegrationTest {
 
     @Autowired
-    Flyway flyway;
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @Autowired
+    private Flyway flyway;
 
     @BeforeEach
     void setUp() {
--- src/test/java/kitchenpos/ui/MenuGroupRestControllerTest.java
+++ src/test/java/kitchenpos/ui/MenuGroupRestControllerTest.java
@@ -1,17 +1,52 @@
 package kitchenpos.ui;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import kitchenpos.IntegrationTest;
+import kitchenpos.domain.MenuGroup;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 
 class MenuGroupRestControllerTest extends IntegrationTest {
-
+    @DisplayName("메뉴 그룹을 생성한다")
     @Test
-    void create() {
+    void create() throws Exception {
+        MenuGroup request = new MenuGroup();
+        request.setName("독특한메뉴");
+
+        mockMvc.perform(post("/api/menu-groups")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request)))
+            .andDo(print())
+            .andExpect(status().isCreated())
+            .andExpect(jsonPath("$.id").exists());
     }
 
+    @DisplayName("메뉴 그룹 생성이 실패한다")
     @Test
-    void findAll() {
+    void createFailedByEmptyName() throws Exception {
+        MenuGroup request = new MenuGroup();
+        request.setName("");
+
+        mockMvc.perform(post("/api/menu-groups")
+            .contentType(MediaType.APPLICATION_JSON)
+            .content(objectMapper.writeValueAsString(request)))
+            .andDo(print())
+            .andExpect(status().isBadRequest());
+    }
+
+    @DisplayName("메뉴 그룹 목록을 조회한다")
+    @Test
+    void findAll() throws Exception {
+        mockMvc.perform(get("/api/menu-groups"))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.length()", greaterThanOrEqualTo(2)));
     }
 }
