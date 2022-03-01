f1b37f0f6d3028c539e01b5701438a8b7bd3b7c9
hyunssooo <jhs695@gmail.com>
2022-03-02 00:15:08 +0900

    feat: 구분자를 컴마(,) 이외에 콜론(:)을 사용할 수 있다.

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -4,12 +4,14 @@ import java.util.Arrays;
 
 public class StringCalculator {
 
+    private static final String DEFAULT_DELIMITER_PATTERNS = "[,]|[:]";
+
     public int add(String text) {
         if (text == null || text.isEmpty()) {
             return 0;
         }
 
-        return Arrays.stream(text.split(","))
+        return Arrays.stream(text.split(DEFAULT_DELIMITER_PATTERNS))
             .mapToInt(Integer::parseInt)
             .sum();
     }
