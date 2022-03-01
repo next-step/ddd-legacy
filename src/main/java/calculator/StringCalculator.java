83be9c8be9e10c5fd473e73a524bb5822d07e237
bactoria <bactoria@gmail.com>
2022-03-02 01:47:19 +0900

    refactor: Tokenizer 사용

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,23 +1,33 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class StringCalculator {
 
-	public int add(final String text) {
-		NumberTokenizer numberTokenizer = new SlashTokenizer();
-		NumberTokenizer defaultTokenizer = new DefaultTokenizer();
+	private final List<NumberTokenizer> numberTokenizers;
 
+	public StringCalculator(NumberTokenizer numberTokenizer) {
+		this(List.of(numberTokenizer));
+	}
+
+	public StringCalculator(List<NumberTokenizer> numberTokenizers) {
+		List<NumberTokenizer> l = new ArrayList<>(numberTokenizers);
+		l.add(new DefaultTokenizer());
+		this.numberTokenizers = l;
+	}
+
+	public int add(final String text) {
 		if (text == null || text.isBlank()) {
 			return 0;
 		}
 
-		if (numberTokenizer.canTokenize(text)) {
-			List<Integer> numbers = numberTokenizer.tokenize(text);
-			return add(numbers);
-		}
+		final List<Integer> numbers = numberTokenizers.stream()
+				.filter(tokenizer -> tokenizer.canTokenize(text))
+				.findFirst()
+				.orElseThrow(() -> new IllegalStateException("처리 가능한 토크나이저가 없습니다."))
+				.tokenize(text);
 
-		List<Integer> numbers = defaultTokenizer.tokenize(text);
 		return add(numbers);
 	}
 
