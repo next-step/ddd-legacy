61c4c7f6f6a575ffceaf29a6436065d8f18775de
mynameisdaun <daun9870.jung@gmail.com>
2022-03-02 00:47:14 +0900

    refactor(Calculation.java) add 메써드에서 number Parsing 하는 로직 분리

--- src/test/java/calculator/Calculator.java
+++ src/test/java/calculator/Calculator.java
@@ -11,24 +11,13 @@ public class Calculator {
     private final static String CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)";
 
     public int add(String text) {
-        int answer = 0;
-
-        if(isNullOrEmpty(text)) {
-            return answer;
+        if (isNullOrEmpty(text)) {
+            return 0;
         }
-        if(text.length()==1 && isNumeric(text)) {
+        if (text.length() == 1 && isNumeric(text)) {
             return toInt(text);
         }
-
-        String[] numbers = text.split(DEFAULT_DELIMITER_REGEX);
-
-        /* 커스텀 구분자가 지정된 경우*/
-        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER_REGEX).matcher(text);
-        if(matcher.find()) {
-            String delimiter = matcher.group(1);
-            numbers = matcher.group(2).split(delimiter);
-        }
-
+        String[] numbers = getNumbersFromText(text);
         return stream(numbers)
                 .filter(CalculatorUtil::isNumeric)
                 .map(CalculatorUtil::toInt)
@@ -36,11 +25,21 @@ public class Calculator {
                 .reduce(0, Integer::sum);
     }
 
+    private String[] getNumbersFromText(String text) {
+        String[] numbers = text.split(DEFAULT_DELIMITER_REGEX);
+        /* 커스텀 구분자가 지정된 경우*/
+        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER_REGEX).matcher(text);
+        if (matcher.find()) {
+            String delimiter = matcher.group(1);
+            numbers = matcher.group(2).split(delimiter);
+        }
+        return numbers;
+    }
+
     private boolean isPositive(Integer number) {
-        if(number < 0) {
+        if (number < 0) {
             throw new RuntimeException("문자열 계산기에 음수는 입력될 수 없습니다.");
         }
         return true;
     }
-
 }
