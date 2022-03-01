01d78c911c235c96bd50a63679b89decd83e00d8
limdingdong <38725277+limdingdong@users.noreply.github.com>
2022-03-02 08:23:56 +0900

    test : 커스텀구분자 사용 테스트코드 작성

--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -52,4 +52,10 @@ class StringCalculatorTest {
     void commaColonDelimiter() {
         assertThat(calculator.add("1,2:3")).isEqualTo(6);
     }
+
+    @DisplayName("\"//\"와 \"\\n\" 문자 사이에 커스텀 구분자를 지정할 수 있다.")
+    @Test
+    void customDelimiter() {
+        assertThat(calculator.add("//;\\n1;2;3")).isEqualTo(6);
+    }
 }
