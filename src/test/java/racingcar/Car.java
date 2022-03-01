170aad5e85233326902d79ac1802bef1c3bc74bc
Imaspear <rjsckdd12@gmail.com>
2022-03-01 22:38:35 +0900

    feat : implement the racing car

---
+++ src/test/java/racingcar/Car.java
@@ -0,0 +1,36 @@
+package racingcar;
+
+public class Car {
+	private final String name;
+	private int position;
+
+	public Car(String name) {
+		this(name, 0);
+	}
+
+	public Car(String name, int position) {
+		validateName(name);
+		this.name = name;
+		this.position = position;
+	}
+
+	private void validateName(String name) {
+		if (name.length() > 4) {
+			throw new IllegalArgumentException("자동차 이름은 5글자 미만");
+		}
+	}
+
+	public void move(MovingStrategy movingStrategy) {
+		if (movingStrategy.movable()) {
+			position++;
+		}
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public int getPosition() {
+		return position;
+	}
+}
