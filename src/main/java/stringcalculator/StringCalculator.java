fc165f688477ae4a892d66d01cacb0d80104c27d
limdingdong <38725277+limdingdong@users.noreply.github.com>
2022-03-02 00:12:26 +0900

    feat : 단일숫자 입력 처리로직 구현

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -3,11 +3,24 @@ package stringcalculator;
 public class StringCalculator {
 
     private static final int EMPTY_NUMBER = 0;
+    private static final int SINGLE_INPUT_LENGTH = 1;
 
     public int add(String text) {
         if (text == null  || text.isEmpty()) {
             return EMPTY_NUMBER;
         }
+        if (text.length() == SINGLE_INPUT_LENGTH && isInteger(text)) {
+            return Integer.parseInt(text);
+        }
         throw new IllegalArgumentException();
+    }
+
+    private boolean isInteger(String text) {
+        try {
+            Integer.parseInt(text);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
