6b480c25efa0f52caa7fb5f6ad7f0e1b406503c7
mynameisdaun <daun9870.jung@gmail.com>
2022-03-01 23:35:15 +0900

    feat(Calculator.java) 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다

--- src/test/java/calculator/Calculator.java
+++ src/test/java/calculator/Calculator.java
@@ -1,12 +1,27 @@
 package calculator;
 
-/* 빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다. */
+/* 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다. */
 public class Calculator {
 
     public int add(String text) {
         if(text == null || "".equals(text) || " ".equals(text) ) {
             return 0;
         }
+        if(text.length()==1&&isNumeric(text)) {
+            return Integer.parseInt(text);
+        }
         return 1;
+    }
+
+    private boolean isNumeric(String strNum) {
+        if (strNum == null) {
+            return false;
+        }
+        try {
+            double d = Integer.parseInt(strNum);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
     }
 }
