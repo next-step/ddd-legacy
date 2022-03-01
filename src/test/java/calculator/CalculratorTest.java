8cf72601d0e2ee1179364f49ec18f4b5797de461
YunJinChoi <zzdd1558@gmail.com>
2022-03-02 01:12:17 +0900

    refactor: add import static Assertions

--- src/test/java/calculator/CalculratorTest.java
+++ src/test/java/calculator/CalculratorTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.*;
+
 /**
  * <pre>
  * calculator
