1a76c1cfeee78cf1383568f7edbe4038f498c4ec
limdingdong <38725277+limdingdong@users.noreply.github.com>
2022-03-02 08:32:07 +0900

    feat : 커스텀 문자열 덧셈 로직 구현

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,10 +1,17 @@
 package stringcalculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 
     private static final int EMPTY_NUMBER = 0;
     private static final int SINGLE_INPUT_LENGTH = 1;
     private static final String TOKEN_DELIMITER = ",|:";
+
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final int CUSTOM_DELIMITER_INDEX = 1;
+    private static final int CUSTOM_DELIMITER_INPUT_INDEX = 2;
 
     public int add(String text) {
         if (text == null  || text.isEmpty()) {
@@ -12,6 +19,12 @@ public class StringCalculator {
         }
         if (text.length() == SINGLE_INPUT_LENGTH && isInteger(text)) {
             return Integer.parseInt(text);
+        }
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(text);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(CUSTOM_DELIMITER_INDEX);
+            String[] tokens = matcher.group(CUSTOM_DELIMITER_INPUT_INDEX).split(customDelimiter);
+            return sum(tokens);
         }
         return sum(text.split(TOKEN_DELIMITER));
     }
