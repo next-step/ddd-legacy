55119f65e586ca636b0cf7e7758995d1da97f9e6
Kaeum <oror1024@gmail.com>
2022-03-01 22:30:56 +0900

    refactor: make constant

--- src/test/java/racingcar/Car.java
+++ src/test/java/racingcar/Car.java
@@ -1,11 +1,14 @@
 package racingcar;
 
 public class Car {
+    public static final int DEFAULT_POSITION = 0;
+    public static final int NAME_LENGTH_LIMIT = 5;
+
     private final String name;
     private int position;
 
     public Car(final String name) {
-        this(name, 0);
+        this(name, DEFAULT_POSITION);
     }
 
     public Car(final String name, final int position) {
@@ -14,8 +17,8 @@ public class Car {
         this.position = position;
     }
 
-    private void validate(String name) {
-        if (name.length() > 5) {
+    private void validate(final String name) {
+        if (name.length() > NAME_LENGTH_LIMIT) {
             throw new IllegalArgumentException();
         }
     }
