d7c314dd6994ae11e4e89e157e29fead81b9d397
mynameisdaun <daun9870.jung@gmail.com>
2022-03-01 23:46:35 +0900

    feat(Calculator.java) 숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다

--- src/test/java/calculator/Calculator.java
+++ src/test/java/calculator/Calculator.java
@@ -1,19 +1,27 @@
 package calculator;
 
+import java.util.Arrays;
+
 import static calculator.CalculatorUtil.isNullOrEmpty;
 import static calculator.CalculatorUtil.toInt;
 import static org.hibernate.query.criteria.internal.ValueHandlerFactory.isNumeric;
 
-/* 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다. */
+/* 숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다. */
 public class Calculator {
 
     public int add(String text) {
+        int answer = 0;
+
         if(isNullOrEmpty(text)) {
-            return 0;
+            return answer;
         }
         if(text.length()==1 && isNumeric(text)) {
             return toInt(text);
         }
-        return 1;
+        String[] numbers = text.split(",");
+
+        return Arrays.stream(numbers)
+                .map(CalculatorUtil::toInt)
+                .reduce(0, Integer::sum);
     }
 }
