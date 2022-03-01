fb7a1380506f990f202cfae60f0c50aa03ec6140
강태엽 <ted.kang@woowahan.com>
2022-03-01 23:05:38 +0900

    test(add StringCalculatorTest frame)

---
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -0,0 +1,43 @@
+package stringcalculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class StringCalculatorTest {
+
+    @DisplayName(value = "빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
+    @Test
+    void testEmptyValue() {
+
+    }
+
+    @DisplayName(value = "숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
+    @Test
+    void testSingleNumber() {
+
+    }
+
+    @DisplayName(value = "숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
+    @Test
+    void testCommaSeparatedNumber() {
+
+    }
+
+    @DisplayName(value = "구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.")
+    @Test
+    void testColonDelimiter() {
+
+    }
+
+    @DisplayName(value = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
+    @Test
+    void testCustomDelimiter() {
+
+    }
+
+    @DisplayName(value = "문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리를 한다.")
+    @Test
+    void testNegativeNumber() {
+
+    }
+}
