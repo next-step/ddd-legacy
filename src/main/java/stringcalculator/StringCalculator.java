e414dca2cfcdd0168f6fd3127fff18d088d19324
hyunssooo <jhs695@gmail.com>
2022-03-02 00:01:02 +0900

    feat: 빈 문자열 또는 null일 경우 0 반환

---
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -0,0 +1,12 @@
+package stringcalculator;
+
+public class StringCalculator {
+
+    public int add(String text) {
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+
+        return 0;
+    }
+}
