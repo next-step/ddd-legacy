4303d562f166c5139c9b51abd7f262ad8416e709
강태엽 <ted.kang@woowahan.com>
2022-03-01 23:24:41 +0900

    feat: implementation comma separated number requirement

--- src/test/java/stringcalculator/StringCalculator.java
+++ src/test/java/stringcalculator/StringCalculator.java
@@ -1,13 +1,20 @@
 package stringcalculator;
 
+import java.util.Arrays;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
+
+    private static final Pattern DEFAULT_DELIMITER_PATTERN = Pattern.compile("[,]");
 
 
     public int add(String source) {
         if (isNullOrEmpty(source)) {
             return 0;
         }
-        return Integer.parseInt(source);
+        return Arrays.stream(DEFAULT_DELIMITER_PATTERN.split(source))
+                .map(Integer::parseInt)
+                .reduce(0, Integer::sum);
     }
 
     private boolean isNullOrEmpty(String source) {
--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -28,9 +28,11 @@ public class StringCalculatorTest {
     }
 
     @DisplayName(value = "숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
-    @Test
-    void testCommaSeparatedNumber() {
-
+    @ValueSource(strings = {"1,2"})
+    @ParameterizedTest
+    void testCommaSeparatedNumber(String source) {
+        StringCalculator stringCalculator = new StringCalculator();
+        assertThat(stringCalculator.add(source)).isEqualTo(3);
     }
 
     @DisplayName(value = "구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.")
