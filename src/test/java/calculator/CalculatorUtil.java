119fcd747faccea6082b4d2c6c00e5b78acdc026
mynameisdaun <daun9870.jung@gmail.com>
2022-03-02 00:31:56 +0900

    refactor(CalculatorUtil.java) isNumeric 메서드 유틸로 이동

--- src/test/java/calculator/CalculatorUtil.java
+++ src/test/java/calculator/CalculatorUtil.java
@@ -12,4 +12,13 @@ public class CalculatorUtil {
     public static int toInt(String text) {
         return Integer.parseInt(text);
     }
+
+    public static boolean isNumeric(String text) {
+        try {
+            Double.parseDouble(text);
+        } catch (NumberFormatException e) {
+            return false;
+        }
+        return true;
+    }
 }
