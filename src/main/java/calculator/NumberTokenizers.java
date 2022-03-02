4c4a2f12f8a6a783984fd7653359ae00aabe51d4
bactoria <bactoria@gmail.com>
2022-03-02 21:04:44 +0900

    refactor: 가독성 개선을 위한 변수명 수정

--- src/main/java/calculator/NumberTokenizers.java
+++ src/main/java/calculator/NumberTokenizers.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class NumberTokenizers {
 
-	private final List<NumberTokenizer> values;
+	private final List<NumberTokenizer> numberTokenizers;
 
 	public NumberTokenizers(final NumberTokenizer numberTokenizer) {
 		this(toList(numberTokenizer));
@@ -20,11 +20,11 @@ public class NumberTokenizers {
 	public NumberTokenizers(final List<NumberTokenizer> numberTokenizers) {
 		final List<NumberTokenizer> list = new ArrayList<>(numberTokenizers);
 		list.add(new DefaultTokenizer());
-		this.values = list;
+		this.numberTokenizers = list;
 	}
 
 	public List<Integer> tokenize(final String text) {
-		return values.stream()
+		return numberTokenizers.stream()
 				.filter(tokenizer -> tokenizer.canTokenize(text))
 				.findFirst()
 				.orElseThrow(() -> new IllegalStateException("처리 가능한 토크나이저가 없습니다."))
