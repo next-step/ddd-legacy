e80583f0446a42e43326216c8578464590fbe499
mandykr <hongseokii92@gmail.com>
2022-03-03 13:54:24 +0900

    test(calculator): 덧셈 기능 단위 테스트 추가

---
+++ src/test/java/calculator/Calculator.java
@@ -0,0 +1,5 @@
+package calculator;
+
+public interface Calculator {
+    int add(String text);
+}
---
+++ src/test/java/calculator/StringCalculator.java
@@ -0,0 +1,8 @@
+package calculator;
+
+public class StringCalculator implements Calculator {
+    @Override
+    public int add(String text) {
+        return 0;
+    }
+}
