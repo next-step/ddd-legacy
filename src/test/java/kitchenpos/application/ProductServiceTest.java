187c5256684f63b457b9824becb3b6d29da8371e
seok <rogktlv@gmail.com>
2022-03-03 22:25:38 +0900

    <test> 상품 가격 수정에 대한 테스트 코드를 작성한다.
    - 상품의 가격을 수정할 수 있다.
    - 상품의 가격은 반드시 0보다 큰 값을 가져야 한다.
    - 가격을 변경하는 상품을 포함하는 메뉴의 가격보다 메뉴에 포함한 상품의 가격이 커지는 경우 메뉴를 진열하지 않는다.

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -1,8 +1,6 @@
 package kitchenpos.application;
 
-import kitchenpos.domain.MenuRepository;
-import kitchenpos.domain.Product;
-import kitchenpos.domain.ProductRepository;
+import kitchenpos.domain.*;
 import kitchenpos.infra.PurgomalumClient;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,12 +12,17 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.UUID;
 import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
@@ -94,5 +97,92 @@ class ProductServiceTest {
 
         // & then
         verify(productRepository).save(any(Product.class));
+    }
+
+    @DisplayName("상품 가격 수정 - 상품의 가격은 반드시 0보다 큰 값을 가져야 한다.")
+    @MethodSource("providePriceForNullAndNegative")
+    @ParameterizedTest
+    public void changePrice01(BigDecimal 변경할_상품_가격) {
+        //given
+        Product 변경할_상품 = new Product();
+        변경할_상품.setPrice(변경할_상품_가격);
+        //when & then
+        assertThatThrownBy(() -> productService.changePrice(UUID.randomUUID(), 변경할_상품))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품 가격 수정 - 가격을 변경하는 상품을 포함하는 메뉴의 가격보다 메뉴에 포함한 상품의 가격이 커지는 경우 메뉴를 진열하지 않는다.")
+    @Test
+    public void changePrice02() {
+        //given
+        BigDecimal 변경할_상품_가격 = BigDecimal.valueOf(2700l);
+        BigDecimal 기존_상품_가격 = BigDecimal.valueOf(3000l);
+        BigDecimal 계속_공개될_메뉴_가격 = BigDecimal.valueOf(2500l);
+        BigDecimal 비공개될_메뉴_가격 = BigDecimal.valueOf(2800l);
+
+        Product 변경할_상품 = new Product();
+        변경할_상품.setPrice(변경할_상품_가격);
+
+        Product 저장된_상품 = spy(Product.class);
+        저장된_상품.setPrice(기존_상품_가격);
+        given(productRepository.findById(any(UUID.class))).willReturn(Optional.of(저장된_상품));
+
+        MenuProduct 저장된_메뉴_상품 = mock(MenuProduct.class);
+        given(저장된_메뉴_상품.getProduct()).willReturn(저장된_상품);
+        given(저장된_메뉴_상품.getQuantity()).willReturn(1l);
+
+        Menu 계속_공개될_메뉴 = spy(Menu.class);
+        계속_공개될_메뉴.setDisplayed(true);
+        given(계속_공개될_메뉴.getMenuProducts()).willReturn(new ArrayList<>(Arrays.asList(저장된_메뉴_상품)));
+        given(계속_공개될_메뉴.getPrice()).willReturn(계속_공개될_메뉴_가격);
+
+        Menu 비공개될_메뉴 = spy(Menu.class);
+        비공개될_메뉴.setDisplayed(true);
+        given(비공개될_메뉴.getMenuProducts()).willReturn(new ArrayList<>(Arrays.asList(저장된_메뉴_상품)));
+        given(비공개될_메뉴.getPrice()).willReturn(비공개될_메뉴_가격);
+
+        given(menuRepository.findAllByProductId(any(UUID.class)))
+                .willReturn(new ArrayList<>(Arrays.asList(계속_공개될_메뉴, 비공개될_메뉴)));
+
+
+        //when
+        productService.changePrice(UUID.randomUUID(), 변경할_상품);
+
+        //then
+        assertThat(계속_공개될_메뉴.isDisplayed()).isTrue();
+        assertThat(비공개될_메뉴.isDisplayed()).isFalse();
+    }
+
+
+    @DisplayName("상품 가격 수정 - 상품의 가격을 수정할 수 있다.")
+    @Test
+    public void changePrice03() {
+        //given
+        BigDecimal 변경할_상품_가격 = BigDecimal.valueOf(2700l);
+        BigDecimal 기존_상품_가격 = BigDecimal.valueOf(3000l);
+        BigDecimal 계속_공개될_메뉴_가격 = BigDecimal.valueOf(2500l);
+
+        Product 변경할_상품 = new Product();
+        변경할_상품.setPrice(변경할_상품_가격);
+        Product 저장된_상품 = spy(Product.class);
+        저장된_상품.setPrice(기존_상품_가격);
+        given(productRepository.findById(any(UUID.class))).willReturn(Optional.of(저장된_상품));
+
+        MenuProduct 저장된_메뉴_상품 = mock(MenuProduct.class);
+        given(저장된_메뉴_상품.getProduct()).willReturn(저장된_상품);
+        given(저장된_메뉴_상품.getQuantity()).willReturn(1l);
+
+        Menu 계속_공개될_메뉴 = spy(Menu.class);
+        계속_공개될_메뉴.setDisplayed(true);
+        given(계속_공개될_메뉴.getMenuProducts()).willReturn(new ArrayList<>(Arrays.asList(저장된_메뉴_상품)));
+        given(계속_공개될_메뉴.getPrice()).willReturn(계속_공개될_메뉴_가격);
+
+        given(menuRepository.findAllByProductId(any(UUID.class)))
+                .willReturn(new ArrayList<>(Arrays.asList(계속_공개될_메뉴)));
+        //when
+        Product 변경된_상품 = productService.changePrice(UUID.randomUUID(), 변경할_상품);
+
+        //then
+        assertThat(변경된_상품.getPrice()).isEqualTo(변경할_상품_가격);
     }
 }
