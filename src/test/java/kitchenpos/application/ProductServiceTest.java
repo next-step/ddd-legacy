4392785af2ecdc577623957c12e06c743946021d
limdingdong <38725277+limdingdong@users.noreply.github.com>
2022-03-08 16:05:45 +0900

    test : 상품 가격관련 테스트코드 작성

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -1,13 +1,19 @@
 package kitchenpos.application;
 
 import kitchenpos.domain.Product;
+import kitchenpos.domain.ProductRepository;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
@@ -15,6 +21,8 @@ class ProductServiceTest {
 
     @Autowired
     private ProductService productService;
+    @Autowired
+    private ProductRepository productRepository;
 
     @DisplayName("상품에는 반드시 이름이 있어야 한다.")
     @NullAndEmptySource
@@ -45,5 +53,57 @@ class ProductServiceTest {
         // then
         assertThatThrownBy(() -> productService.create(product))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품에는 반드시 가격이 있어야 한다.")
+    @Test
+    void priceIsMandatory() {
+        // given
+        Product product = new Product();
+
+        // when
+        product.setName("후라이드치킨");
+        product.setPrice(null);
+
+        // then
+        assertThatThrownBy(() -> productService.create(product))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품 가격은 0보다 작을 수 없다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "-1000"})
+    void negativePrice(String price) {
+        // given
+        Product product = new Product();
+
+        // when
+        product.setName("후라이드치킨");
+        product.setPrice(new BigDecimal(price));
+
+        // then
+        assertThatThrownBy(() -> productService.create(product))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품의 가격을 변경할 수 있다.")
+    @Test
+    void changePrice() {
+
+        // given
+        Product productRequest = new Product();
+        productRequest.setName("후라이드치킨");
+        productRequest.setPrice(BigDecimal.ZERO);
+        Product product = productService.create(productRequest);
+
+        // when
+        Product priceChangeRequest = new Product();
+        priceChangeRequest.setPrice(new BigDecimal("1000"));
+        productService.changePrice(product.getId(), priceChangeRequest);
+
+        // then
+        Optional<Product> changedProduct = productRepository.findById(product.getId());
+        assertThat(changedProduct).isPresent();
+        assertThat(changedProduct.get().getPrice()).isEqualByComparingTo(new BigDecimal("1000"));
     }
 }
