bb45cd9c4302020f9f54249ef3b566f158a15273
Imaspear <rjsckdd12@gmail.com>
2022-03-09 08:42:00 +0900

    test : MenuGroupService 테스트

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -5,12 +5,16 @@ import kitchenpos.domain.MenuGroupRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Stream;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -20,11 +24,32 @@ import static org.mockito.Mockito.*;
 class MenuGroupServiceTest {
 	public static final String FIRST_MENU_GROUP = "첫 번째 테스트 메뉴 그룹";
 	public static final String SECOND_MENU_GROUP = "두 번째 테스트 메뉴 그룹";
+
 	@Mock
 	private MenuGroupRepository menuGroupRepository;
 
 	@InjectMocks
 	private MenuGroupService menuGroupService;
+
+	private static Stream<String> menuNameIsEmptyAndNull() {
+		return Stream.of(
+			null,
+			""
+		);
+	}
+
+	@MethodSource("menuNameIsEmptyAndNull")
+	@ParameterizedTest
+	@DisplayName("메뉴를 분류하는 그룹에는 이름이 꼭 필요합니다.")
+	void createMenuGroupButNameless(String name) {
+		//given
+		MenuGroup menuGroup = mock(MenuGroup.class);
+		when(menuGroup.getName()).thenReturn(name);
+
+		//when & then
+		assertThatThrownBy(() -> menuGroupService.create(menuGroup))
+			.isInstanceOf(IllegalArgumentException.class);
+	}
 
 	@Test
 	@DisplayName("가게 점주는 메뉴 그룹을 생성할 수 있습니다.")
@@ -33,19 +58,9 @@ class MenuGroupServiceTest {
 		when(menuGroupRepository.save(any())).thenReturn(menuGroup);
 
 		MenuGroup createMenuGroup = menuGroupService.create(menuGroup);
+
+		verify(menuGroupRepository).save(any(MenuGroup.class));
 		assertThat(createMenuGroup).isEqualTo(menuGroup);
-	}
-
-	@Test
-	@DisplayName("메뉴를 분류하는 그룹에는 이름이 꼭 필요합니다.")
-	void createMenuGroupButNameless() {
-		MenuGroup menuGroup = getMenuGroup("");
-		lenient().when(menuGroupRepository.save(any())).thenReturn(menuGroup);
-
-		assertThatExceptionOfType(IllegalArgumentException.class)
-			.isThrownBy(() -> {
-				MenuGroup createMenuGroup = menuGroupService.create(menuGroup);
-			});
 	}
 
 	@Test
@@ -62,12 +77,15 @@ class MenuGroupServiceTest {
 
 		List<MenuGroup> findMenuGroupList = menuGroupService.findAll();
 
+		verify(menuGroupRepository).findAll();
+
 		assertThat(findMenuGroupList.size()).isEqualTo(menuGroupList.size());
 		assertThat(menuGroupList).isSameAs(findMenuGroupList);
 	}
 
 	private static MenuGroup getMenuGroup(String name) {
 		MenuGroup menuGroup = new MenuGroup();
+		menuGroup.setId(UUID.randomUUID());
 		menuGroup.setName(name);
 		return menuGroup;
 	}
