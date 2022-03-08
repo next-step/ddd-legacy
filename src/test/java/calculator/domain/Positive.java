d2763113716a86e9230542c54c1bbc36707ccecf
mandykr <94955454+mandykr@users.noreply.github.com>
2022-03-08 18:05:34 +0900

    🚀 2단계 - 요구 사항 정리 (#240)
    * refactor(calculator): 양수의 합를 구하는 메소드 분리
    
    * docs: 요구사항 추가

--- src/test/java/calculator/domain/Positive.java
+++ src/test/java/calculator/domain/Positive.java
@@ -1,20 +1,24 @@
 package calculator.domain;
 
 public class Positive {
-    private final int positive;
+    private final int number;
 
     public Positive(int number) {
         if (isNegative(number)) {
             throw new RuntimeException();
         }
-        this.positive = number;
+        this.number = number;
     }
 
     private boolean isNegative(final int number) {
         return number < 0;
     }
 
-    public int getPositive() {
-        return positive;
+    public int getNumber() {
+        return number;
+    }
+
+    public Positive add(Positive positive) {
+        return new Positive(this.number + positive.number);
     }
 }
