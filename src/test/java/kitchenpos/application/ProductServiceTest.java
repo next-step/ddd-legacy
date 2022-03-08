3ec4f6be3295eceba8f26ff476f98f526c376e9c
limdingdong <38725277+limdingdong@users.noreply.github.com>
2022-03-08 15:32:31 +0900

    test : 상품생성 테스트 - 상품 이름에는 비속어 사용

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -4,6 +4,7 @@ import kitchenpos.domain.Product;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -20,6 +21,21 @@ class ProductServiceTest {
     @ParameterizedTest
     void nameIsMandatory(String name) {
 
+        // given
+        Product product = new Product();
+
+        // when
+        product.setName(name);
+
+        // then
+        assertThatThrownBy(() -> productService.create(product))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품 이름에는 비속어를 사용할 수 없다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"stupid"})
+    void nameContainsProfanity(String name) {
         // given
         Product product = new Product();
 
