9b25738405e02406f0a62a98347781e7944438f3
mynameisdaun <daun9870.jung@gmail.com>
2022-03-10 21:14:39 +0900

    test(OrderServiceTest.test) 주문을 등록할 수 있다
    주문을 등록할때 반드시 주문형태를 선택해야 한다
    주문은 반드시 하나 이상의 주문구성메뉴를 포함하고 있어야 한다
    주문의 갯수와 주문구성메뉴의 갯수가 다를 수 없다
    매장식사가 아닌 경우, 주문구성메뉴의 갯수(quantity)는 0 이상이어야 한다
    존재하는 메뉴만 주문할 수 있다
    판매중인 메뉴만 주문할 수 있다
    주문형태가 배달인 경우 반드시 주문주소를 포함하고 있어야 한다
    주문형태가 매장식사인 경우 반드시 주문 테이블을 포함하고 있어야 한다
    공석이 아닌 주문테이블에 주문을 등록할 수 없다

--- src/test/java/kitchenpos/application/OrderServiceTest.java
+++ src/test/java/kitchenpos/application/OrderServiceTest.java
@@ -1,17 +1,24 @@
 package kitchenpos.application;
 
-import kitchenpos.domain.MenuRepository;
-import kitchenpos.domain.OrderRepository;
-import kitchenpos.domain.OrderTableRepository;
+import kitchenpos.domain.*;
 import kitchenpos.infra.KitchenridersClient;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.math.BigDecimal;
+import java.util.*;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class OrderServiceTest {
@@ -30,100 +37,226 @@ class OrderServiceTest {
     @Test
     void create_success() throws Exception {
         //given
+        BigDecimal 메뉴가격 = BigDecimal.valueOf(17000L);
+        BigDecimal 주문구성메뉴가격 = BigDecimal.valueOf(17000L);
+        OrderType 매장식사 = OrderType.EAT_IN;
+        Order 등록할_주문 = mock(Order.class);
+        given(등록할_주문.getType()).willReturn(매장식사);
+        OrderLineItem 주문구성메뉴 = mock(OrderLineItem.class);
+        given(주문구성메뉴.getQuantity()).willReturn(1L);
+        given(주문구성메뉴.getPrice()).willReturn(주문구성메뉴가격);
+        given(등록할_주문.getOrderLineItems()).willReturn(new ArrayList<>(Arrays.asList(주문구성메뉴)));
+        Menu 메뉴 = mock(Menu.class);
+        given(메뉴.isDisplayed()).willReturn(true);
+        given(메뉴.getPrice()).willReturn(메뉴가격);
+        given(menuRepository.findAllByIdIn(any())).willReturn(new ArrayList<>(Arrays.asList(메뉴)));
+        given(menuRepository.findById(any())).willReturn(Optional.of(메뉴));
+        OrderTable 주문테이블 = mock(OrderTable.class);
+        given(orderTableRepository.findById(any())).willReturn(Optional.of(주문테이블));
+        given(주문테이블.isEmpty()).willReturn(false);
 
         //when
+        orderService.create(등록할_주문);
 
         //then
+        verify(orderRepository, times(1)).save(any(Order.class));
     }
 
     @DisplayName(value = "주문을 등록할때 반드시 주문형태를 선택해야 한다")
     @Test
     void create_fail_should_contain_type() throws Exception {
         //given
+        Order 등록할_주문 = mock(Order.class);
 
-        //when
-
-        //then
+        //when,then
+        assertThatThrownBy(() -> orderService.create(등록할_주문))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName(value = "주문은 반드시 하나 이상의 주문구성메뉴를 포함하고 있어야 한다")
-    @Test
-    void create_fail_should_contain_orderLineItem() throws Exception {
+    @ParameterizedTest
+    @MethodSource("잘못된_주문구성메뉴_리스트")
+    void create_fail_should_contain_orderLineItem(final List<OrderLineItem> 주문구성메뉴_리스트) throws Exception {
         //given
+        Order 등록할_주문 = mock(Order.class);
+        given(등록할_주문.getType()).willReturn(OrderType.DELIVERY);
+        given(등록할_주문.getOrderLineItems()).willReturn(주문구성메뉴_리스트);
 
-        //when
-
-        //then
+        //when, then
+        assertThatThrownBy(() -> orderService.create(등록할_주문))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName(value = "주문의 갯수와 주문구성메뉴의 갯수가 다를 수 없다")
     @Test
     void create_fail_menuList_size_should_same_orderLineItemList_size() throws Exception {
         //given
-
-        //when
-
-        //then
+        Order 등록할_주문 = mock(Order.class);
+        given(등록할_주문.getType()).willReturn(OrderType.DELIVERY);
+        OrderLineItem 주문구성메뉴 = mock(OrderLineItem.class);
+        given(등록할_주문.getOrderLineItems()).willReturn(new ArrayList<>(Arrays.asList(주문구성메뉴)));
+        Menu 메뉴1 = mock(Menu.class);
+        Menu 메뉴2 = mock(Menu.class);
+        given(menuRepository.findAllByIdIn(any())).willReturn(new ArrayList<>(Arrays.asList(메뉴1, 메뉴2)));
+        //when, then
+        assertThatThrownBy(() -> orderService.create(등록할_주문))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName(value = "주문구성메뉴의 갯수(quantity)는 0 이상이어야 한다")
+    @DisplayName(value = "매장식사가 아닌경우, 주문구성메뉴의 갯수(quantity)는 0 이상이어야 한다")
     @Test
     void create_fail_orderLineItem_quantity_should_gt_0() throws Exception {
         //given
+        Order 등록할_주문 = mock(Order.class);
+        given(등록할_주문.getType()).willReturn(OrderType.DELIVERY);
+        OrderLineItem 주문구성메뉴 = mock(OrderLineItem.class);
+        given(주문구성메뉴.getQuantity()).willReturn(-1L);
+        given(등록할_주문.getOrderLineItems()).willReturn(new ArrayList<>(Arrays.asList(주문구성메뉴)));
+        Menu 메뉴 = mock(Menu.class);
+        given(menuRepository.findAllByIdIn(any())).willReturn(new ArrayList<>(Arrays.asList(메뉴)));
+        //when, then
+        assertThatThrownBy(() -> orderService.create(등록할_주문))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
-        //when
+    @DisplayName(value = "존재하는 메뉴만 주문할 수 있다")
+    @Test
+    void create_fail_menu_no_exist() throws Exception {
+        //given
+        Order 등록할_주문 = mock(Order.class);
+        given(등록할_주문.getType()).willReturn(OrderType.DELIVERY);
+        OrderLineItem 주문구성메뉴 = mock(OrderLineItem.class);
+        given(주문구성메뉴.getQuantity()).willReturn(1L);
+        given(등록할_주문.getOrderLineItems()).willReturn(new ArrayList<>(Arrays.asList(주문구성메뉴)));
+        Menu 메뉴 = mock(Menu.class);
+        given(메뉴.isDisplayed()).willReturn(true);
+        given(menuRepository.findAllByIdIn(any())).willReturn(new ArrayList<>(Arrays.asList(메뉴)));
+        given(menuRepository.findById(any(UUID.class))).willReturn(Optional.empty());
 
-        //then
+        //when, then
+        assertThatThrownBy(() -> orderService.create(등록할_주문))
+                .isInstanceOf(NoSuchElementException.class);
     }
 
     @DisplayName(value = "판매중인 메뉴만 주문할 수 있다")
     @Test
     void create_fail_menu_should_display() throws Exception {
         //given
+        Order 등록할_주문 = mock(Order.class);
+        given(등록할_주문.getType()).willReturn(OrderType.DELIVERY);
+        OrderLineItem 주문구성메뉴 = mock(OrderLineItem.class);
+        given(주문구성메뉴.getQuantity()).willReturn(1L);
+        given(등록할_주문.getOrderLineItems()).willReturn(new ArrayList<>(Arrays.asList(주문구성메뉴)));
+        Menu 메뉴 = mock(Menu.class);
+        given(메뉴.isDisplayed()).willReturn(false);
+        given(menuRepository.findAllByIdIn(any())).willReturn(new ArrayList<>(Arrays.asList(메뉴)));
+        given(menuRepository.findById(any())).willReturn(Optional.of(메뉴));
 
-        //when
-
-        //then
+       //when, then
+        assertThatThrownBy(() -> orderService.create(등록할_주문))
+                .isInstanceOf(IllegalStateException.class);
     }
 
     @DisplayName(value = "메뉴의 가격과 주문구성메뉴의 가격이 다를 수 없다")
     @Test
     void create_fail_menu_price_should_same_orderLineItem_price() throws Exception {
         //given
+        BigDecimal 메뉴가격 = BigDecimal.valueOf(17000L);
+        BigDecimal 주문구성메뉴가격 = BigDecimal.valueOf(17500L);
+        Order 등록할_주문 = mock(Order.class);
+        given(등록할_주문.getType()).willReturn(OrderType.DELIVERY);
+        OrderLineItem 주문구성메뉴 = mock(OrderLineItem.class);
+        given(주문구성메뉴.getQuantity()).willReturn(1L);
+        given(주문구성메뉴.getPrice()).willReturn(주문구성메뉴가격);
+        given(등록할_주문.getOrderLineItems()).willReturn(new ArrayList<>(Arrays.asList(주문구성메뉴)));
+        Menu 메뉴 = mock(Menu.class);
+        given(메뉴.isDisplayed()).willReturn(true);
+        given(메뉴.getPrice()).willReturn(메뉴가격);
+        given(menuRepository.findAllByIdIn(any())).willReturn(new ArrayList<>(Arrays.asList(메뉴)));
+        given(menuRepository.findById(any())).willReturn(Optional.of(메뉴));
 
-        //when
-
-        //then
+        //when, then
+        assertThatThrownBy(() -> orderService.create(등록할_주문))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName(value = "주문형태가 배달인 경우 반드시 주문주소를 포함하고 있어야 한다")
-    @Test
-    void create_fail_when_type_delivery_should_contain_delivery_address() throws Exception {
+    @ParameterizedTest
+    @MethodSource("잘못된_주문주소")
+    void create_fail_when_type_delivery_should_contain_delivery_address(final String 주문주소) throws Exception {
         //given
+        BigDecimal 메뉴가격 = BigDecimal.valueOf(17000L);
+        BigDecimal 주문구성메뉴가격 = BigDecimal.valueOf(17000L);
+        OrderType 배달 = OrderType.DELIVERY;
+        Order 등록할_주문 = mock(Order.class);
+        given(등록할_주문.getType()).willReturn(배달);
+        given(등록할_주문.getDeliveryAddress()).willReturn(주문주소);
+        OrderLineItem 주문구성메뉴 = mock(OrderLineItem.class);
+        given(주문구성메뉴.getQuantity()).willReturn(1L);
+        given(주문구성메뉴.getPrice()).willReturn(주문구성메뉴가격);
+        given(등록할_주문.getOrderLineItems()).willReturn(new ArrayList<>(Arrays.asList(주문구성메뉴)));
+        Menu 메뉴 = mock(Menu.class);
+        given(메뉴.isDisplayed()).willReturn(true);
+        given(메뉴.getPrice()).willReturn(메뉴가격);
+        given(menuRepository.findAllByIdIn(any())).willReturn(new ArrayList<>(Arrays.asList(메뉴)));
+        given(menuRepository.findById(any())).willReturn(Optional.of(메뉴));
 
-        //when
-
-        //then
+        //when, then
+        assertThatThrownBy(() -> orderService.create(등록할_주문))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName(value = "주문형태가 매장식사인 경우 반드시 주문 테이블을 포함하고 있어야 한다")
     @Test
     void create_fail_when_type_eat_in_should_contain_order_table() throws Exception {
         //given
+        BigDecimal 메뉴가격 = BigDecimal.valueOf(17000L);
+        BigDecimal 주문구성메뉴가격 = BigDecimal.valueOf(17000L);
+        OrderType 매장식사 = OrderType.EAT_IN;
+        Order 등록할_주문 = mock(Order.class);
+        given(등록할_주문.getType()).willReturn(매장식사);
+        OrderLineItem 주문구성메뉴 = mock(OrderLineItem.class);
+        given(주문구성메뉴.getQuantity()).willReturn(1L);
+        given(주문구성메뉴.getPrice()).willReturn(주문구성메뉴가격);
+        given(등록할_주문.getOrderLineItems()).willReturn(new ArrayList<>(Arrays.asList(주문구성메뉴)));
+        Menu 메뉴 = mock(Menu.class);
+        given(메뉴.isDisplayed()).willReturn(true);
+        given(메뉴.getPrice()).willReturn(메뉴가격);
+        given(menuRepository.findAllByIdIn(any())).willReturn(new ArrayList<>(Arrays.asList(메뉴)));
+        given(menuRepository.findById(any())).willReturn(Optional.of(메뉴));
+        given(orderTableRepository.findById(any(UUID.class))).willReturn(Optional.empty());
 
-        //when
-
-        //then
+        //when, then
+        assertThatThrownBy(() -> orderService.create(등록할_주문))
+                .isInstanceOf(NoSuchElementException.class);
     }
 
     @DisplayName(value = "공석이 아닌 주문테이블에 주문을 등록할 수 없다")
     @Test
     void create_fail_table_should_empty() throws Exception {
         //given
+        BigDecimal 메뉴가격 = BigDecimal.valueOf(17000L);
+        BigDecimal 주문구성메뉴가격 = BigDecimal.valueOf(17000L);
+        OrderType 매장식사 = OrderType.EAT_IN;
+        Order 등록할_주문 = mock(Order.class);
+        given(등록할_주문.getType()).willReturn(매장식사);
+        OrderLineItem 주문구성메뉴 = mock(OrderLineItem.class);
+        given(주문구성메뉴.getQuantity()).willReturn(1L);
+        given(주문구성메뉴.getPrice()).willReturn(주문구성메뉴가격);
+        given(등록할_주문.getOrderLineItems()).willReturn(new ArrayList<>(Arrays.asList(주문구성메뉴)));
+        Menu 메뉴 = mock(Menu.class);
+        given(메뉴.isDisplayed()).willReturn(true);
+        given(메뉴.getPrice()).willReturn(메뉴가격);
+        given(menuRepository.findAllByIdIn(any())).willReturn(new ArrayList<>(Arrays.asList(메뉴)));
+        given(menuRepository.findById(any())).willReturn(Optional.of(메뉴));
+        OrderTable 주문테이블 = mock(OrderTable.class);
+        given(orderTableRepository.findById(any(UUID.class))).willReturn(Optional.of(주문테이블));
+        given(주문테이블.isEmpty()).willReturn(true);
 
-        //when
 
-        //then
+        //when, then
+        assertThatThrownBy(() -> orderService.create(등록할_주문))
+                .isInstanceOf(IllegalStateException.class);
     }
 
     @DisplayName(value = "주문상태를 주문수락으로 변경할 수 있다")
@@ -254,5 +387,19 @@ class OrderServiceTest {
         //when
 
         //then
+    }
+
+    private static Stream<List<OrderLineItem>> 잘못된_주문구성메뉴_리스트() {
+        return Stream.of(
+                null,
+                new ArrayList<>()
+        );
+    }
+
+    private static Stream<String> 잘못된_주문주소() {
+        return Stream.of(
+                null,
+                ""
+        );
     }
 }
