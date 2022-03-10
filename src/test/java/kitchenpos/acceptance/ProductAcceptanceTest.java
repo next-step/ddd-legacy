72511a3fa77cbe9ef7e399e88b04fc7f74073b76
mandykr <hongseokii92@gmail.com>
2022-03-10 15:58:54 +0900

    chore(kitchenpos): 상수명 수정

--- src/test/java/kitchenpos/acceptance/ProductAcceptanceTest.java
+++ src/test/java/kitchenpos/acceptance/ProductAcceptanceTest.java
@@ -9,13 +9,13 @@ import static kitchenpos.acceptance.step.ProductSteps.*;
 
 @DisplayName("상품 관리 기능")
 class ProductAcceptanceTest extends AcceptanceTest {
-    private static final String NAME = "양념치킨";
-    private static final int PRICE = 10000;
+    private static final String PRODUCT_NAME = "양념치킨";
+    private static final int PRODUCT_PRICE = 10000;
 
     @DisplayName("상품을 관리한다.")
     @Test
     void manageProduct() {
-        ExtractableResponse<Response> createResponse= 상품_등록_요청(NAME, PRICE);
+        ExtractableResponse<Response> createResponse= 상품_등록_요청(PRODUCT_NAME, PRODUCT_PRICE);
         상품_등록_완료(createResponse);
 
         ExtractableResponse<Response> updateResponse = 가격_수정_요청(createResponse, 15000);
