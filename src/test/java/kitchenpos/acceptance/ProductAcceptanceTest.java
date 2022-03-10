b33271075c4c7a61a840297b3fd59569bb1e7baf
mandykr <hongseokii92@gmail.com>
2022-03-10 15:43:25 +0900

    test(kitchenpos): 상품 관리 인수 테스트 작성

---
+++ src/test/java/kitchenpos/acceptance/ProductAcceptanceTest.java
@@ -0,0 +1,24 @@
+package kitchenpos.acceptance;
+
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static kitchenpos.acceptance.step.ProductSteps.*;
+
+@DisplayName("상품 관리 기능")
+class ProductAcceptanceTest extends AcceptanceTest {
+    private static final String NAME = "양념치킨";
+    private static final int PRICE = 10000;
+
+    @DisplayName("상품을 관리한다.")
+    @Test
+    void manageProduct() {
+        ExtractableResponse<Response> createResponse= 상품_등록_요청(NAME, PRICE);
+        상품_등록_완료(createResponse);
+
+        ExtractableResponse<Response> updateResponse = 가격_수정_요청(createResponse, 15000);
+        가격_수정_완료(updateResponse);
+    }
+}
