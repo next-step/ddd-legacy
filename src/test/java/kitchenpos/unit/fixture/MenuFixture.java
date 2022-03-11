d9bd85d1fe4fb8447cd82056c405934198e44053
mandykr <hongseokii92@gmail.com>
2022-03-11 23:06:10 +0900

    test(kitchenpos): 메뉴 관리 service 단위 테스트 작성

--- src/test/java/kitchenpos/unit/fixture/MenuFixture.java
+++ src/test/java/kitchenpos/unit/fixture/MenuFixture.java
@@ -18,41 +18,60 @@ public class MenuFixture {
     public static final Menu 두그릇_세트;
     public static final Menu 세그릇_세트;
 
+    public static final List<Product> 한그릇_세트_상품목록;
+    public static final List<Product> 두그릇_세트_상품목록;
+    public static final List<Product> 세그릇_세트_상품목록;
+
     static {
-        탕수육_세트 = new MenuGroup();
-        한그릇_세트 = createMenu(탕수육_세트, 14000, 탕수육, 짜장면);
-        두그릇_세트 = createMenu(탕수육_세트, 20000, 탕수육, 짜장면, 짬뽕);
-        세그릇_세트 = createMenu(탕수육_세트, 25000, 탕수육, 짜장면, 짬뽕, 볶음밥);
+        탕수육_세트 = MenuGroupFixture.탕수육_세트;
+        한그릇_세트_상품목록 = Arrays.asList(탕수육, 짜장면);
+        두그릇_세트_상품목록 = Arrays.asList(탕수육, 짜장면, 짬뽕);
+        세그릇_세트_상품목록 = Arrays.asList(탕수육, 짜장면, 짬뽕, 볶음밥);
+
+        한그릇_세트 = createMenu("f1860abc2ea1411bbd4abaa44f0d5580", 탕수육_세트, "한그릇 세트", BigDecimal.valueOf(14000), 한그릇_세트_상품목록);
+        두그릇_세트 = createMenu("cbc75faefeb04bb18be2cb8ce5d8fded", 탕수육_세트, "두그릇 세트", BigDecimal.valueOf(20000), 두그릇_세트_상품목록);
+        세그릇_세트 = createMenu("5e9879b761124791a4cef22e94af8752", 탕수육_세트, "세그릇 세트", BigDecimal.valueOf(25000), 세그릇_세트_상품목록);
     }
 
-    private static Menu createMenu(MenuGroup menuGroup, int price, Product... products) {
+    private static Menu createMenu(String id, MenuGroup menuGroup, String name, BigDecimal price, List<Product> products) {
         Menu menu = new Menu();
         menu.setMenuGroup(menuGroup);
-        menu.setName(createMenuName(products));
-        menu.setPrice(BigDecimal.valueOf(price));
+        menu.setMenuGroupId(menuGroup.getId());
+        menu.setName(name);
+        menu.setPrice(price);
         menu.setMenuProducts(createMenuProducts(products));
         menu.setDisplayed(true);
 
         return menu;
     }
 
-    private static List<MenuProduct> createMenuProducts(Product[] products) {
-        return Arrays.stream(products)
+    public static Menu createMenu(MenuGroup menuGroup, String name, BigDecimal price, List<Product> products) {
+        return createMenu("", menuGroup, name, price, products);
+    }
+
+    public static Menu createMenuWithMenuProducts(MenuGroup menuGroup, String name, BigDecimal price, List<MenuProduct> menuProducts) {
+        Menu menu = new Menu();
+        menu.setMenuGroup(menuGroup);
+        menu.setMenuGroupId(menuGroup.getId());
+        menu.setName(name);
+        menu.setPrice(price);
+        menu.setMenuProducts(menuProducts);
+        menu.setDisplayed(true);
+
+        return menu;
+    }
+
+    public static List<MenuProduct> createMenuProducts(List<Product> products) {
+        return products.stream()
                 .map(p -> createMenuProduct(p, 1))
                 .collect(Collectors.toList());
     }
 
-    private static MenuProduct createMenuProduct(Product product, int quantity) {
+    public static MenuProduct createMenuProduct(Product product, int quantity) {
         MenuProduct menuProduct = new MenuProduct();
         menuProduct.setProduct(product);
         menuProduct.setQuantity(quantity);
+        menuProduct.setProductId(product.getId());
         return menuProduct;
-    }
-
-    private static String createMenuName(Product[] products) {
-        String[] names = Arrays.stream(products)
-                .map(Product::getName)
-                .toArray(String[]::new);
-        return String.join("+", names);
     }
 }
