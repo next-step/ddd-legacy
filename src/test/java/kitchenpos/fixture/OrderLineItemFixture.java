83f3deb4a13b278d2b891fc55eef74d20f055869
mynameisdaun <daun9870.jung@gmail.com>
2022-03-12 01:44:17 +0900

    test(OrderLineItemFixture.test) 주문구성메뉴 테스트 픽스쳐 생성

---
+++ src/test/java/kitchenpos/fixture/OrderLineItemFixture.java
@@ -0,0 +1,58 @@
+package kitchenpos.fixture;
+
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.OrderLineItem;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+public class OrderLineItemFixture {
+
+    public static class OrderLineItemBuilder {
+
+        private Long seq;
+        private Menu menu;
+        private long quantity;
+        private UUID menuId;
+        private BigDecimal price;
+
+        public OrderLineItemBuilder() {
+
+        }
+
+        public OrderLineItemBuilder seq(Long seq) {
+            this.seq=seq;
+            return this;
+        }
+
+        public OrderLineItemBuilder menu(Menu menu) {
+            this.menu=menu;
+            return this;
+        }
+
+        public OrderLineItemBuilder quantity(long quantity) {
+            this.quantity=quantity;
+            return this;
+        }
+
+        public OrderLineItemBuilder menuId(UUID menuId) {
+            this.menuId = menuId;
+            return this;
+        }
+
+        public OrderLineItemBuilder price(BigDecimal price) {
+            this.price=price;
+            return this;
+        }
+
+        public OrderLineItem build() {
+            OrderLineItem orderLineItem = new OrderLineItem();
+            orderLineItem.setSeq(this.seq);
+            orderLineItem.setMenu(this.menu);
+            orderLineItem.setQuantity(this.quantity);
+            orderLineItem.setMenuId(this.menuId);
+            orderLineItem.setPrice(this.price);
+            return orderLineItem;
+        }
+    }
+}
