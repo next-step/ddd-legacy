457639893f98427f626560c48abc5dee427ac336
mandykr <hongseokii92@gmail.com>
2022-03-11 11:47:41 +0900

    test(kitchenpos): 상품 관리 service 단위 테스트 작성

---
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -0,0 +1,35 @@
+package kitchenpos.application;
+
+import kitchenpos.domain.MenuGroupRepository;
+import kitchenpos.domain.MenuRepository;
+import kitchenpos.domain.ProductRepository;
+import kitchenpos.infra.PurgomalumClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuServiceTest {
+    @Autowired
+    private MenuRepository menuRepository;
+    @Autowired
+    private MenuGroupRepository menuGroupRepository;
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private PurgomalumClient purgomalumClient;
+
+    private MenuService menuService;
+
+    @BeforeEach
+    void setUp() {
+        menuService = new MenuService(menuRepository, menuGroupRepository, productRepository, purgomalumClient);
+    }
+
+
+    @Test
+    void create() {
+
+    }
+}
---
+++ src/test/java/kitchenpos/application/fixture/MenuFixture.java
@@ -0,0 +1,58 @@
+package kitchenpos.application.fixture;
+
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuProduct;
+import kitchenpos.domain.Product;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static kitchenpos.application.fixture.ProductFixture.*;
+
+public class MenuFixture {
+    public static final MenuGroup 탕수육_세트;
+    public static final Menu 한그릇_세트;
+    public static final Menu 두그릇_세트;
+    public static final Menu 세그릇_세트;
+
+    static {
+        탕수육_세트 = new MenuGroup();
+        한그릇_세트 = createMenu(탕수육_세트, 14000, 탕수육, 짜장면);
+        두그릇_세트 = createMenu(탕수육_세트, 20000, 탕수육, 짜장면, 짬뽕);
+        세그릇_세트 = createMenu(탕수육_세트, 25000, 탕수육, 짜장면, 짬뽕, 볶음밥);
+    }
+
+    private static Menu createMenu(MenuGroup menuGroup, int price, Product... products) {
+        Menu menu = new Menu();
+        menu.setMenuGroup(menuGroup);
+        menu.setName(createMenuName(products));
+        menu.setPrice(BigDecimal.valueOf(price));
+        menu.setMenuProducts(createMenuProducts(products));
+        menu.setDisplayed(true);
+
+        return menu;
+    }
+
+    private static List<MenuProduct> createMenuProducts(Product[] products) {
+        return Arrays.stream(products)
+                .map(p -> createMenuProduct(p, 1))
+                .collect(Collectors.toList());
+    }
+
+    private static MenuProduct createMenuProduct(Product product, int quantity) {
+        MenuProduct menuProduct = new MenuProduct();
+        menuProduct.setProduct(product);
+        menuProduct.setQuantity(quantity);
+        return menuProduct;
+    }
+
+    private static String createMenuName(Product[] products) {
+        String[] names = Arrays.stream(products)
+                .map(Product::getName)
+                .toArray(String[]::new);
+        return String.join("+", names);
+    }
+}
