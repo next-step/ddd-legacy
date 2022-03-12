dcd992bb43ca5bdb5f11c9daab7c0bfb7fd41da0
Imaspear <rjsckdd12@gmail.com>
2022-03-12 20:17:58 +0900

    test : MenuGroupFixture 추가

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -11,10 +11,10 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import static kitchenpos.KitchenposFixture.*;
+import static kitchenpos.fixture.MenuGroupFixture.정상_메뉴_그룹_리스트;
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
@@ -34,7 +34,7 @@ class MenuGroupServiceTest {
   @DisplayName("이름이 null이거나 empty이면 IllegalArgumentException 예외 발생")
   void createMenuGroupButNameless(String name) {
     //given
-    MenuGroup menuGroup = menuGroup();
+    MenuGroup menuGroup = 정상_메뉴_그룹();
     menuGroup.setName(name);
 
     //then
@@ -46,12 +46,12 @@ class MenuGroupServiceTest {
   @DisplayName("메뉴 그룹 생성")
   void createMenuGroup() {
     //given
-    MenuGroup menuGroup = menuGroup();
+    MenuGroup request = 정상_메뉴_그룹();
 
-    when(menuGroupRepository.save(any())).thenReturn(menuGroup);
+    when(menuGroupRepository.save(any())).thenReturn(request);
 
     assertDoesNotThrow(() -> {
-      MenuGroup createMenuGroup = menuGroupService.create(menuGroup);
+      MenuGroup result = menuGroupService.create(request);
     });
   }
 
@@ -59,17 +59,15 @@ class MenuGroupServiceTest {
   @DisplayName("메뉴 그룹 전체 조회")
   void findMenuGroupAll() {
     //given
-    List<MenuGroup> menuGroupList = new ArrayList<>();
-    menuGroupList.add(menuGroup());
-    menuGroupList.add(menuGroup());
+    List<MenuGroup> menuGroupList = 정상_메뉴_그룹_리스트();
 
     when(menuGroupRepository.findAll()).thenReturn(menuGroupList);
 
-    List<MenuGroup> findMenuGroupList = menuGroupService.findAll();
+    List<MenuGroup> result = menuGroupService.findAll();
 
     verify(menuGroupRepository).findAll();
 
-    assertThat(findMenuGroupList.size()).isEqualTo(menuGroupList.size());
-    assertThat(menuGroupList).isSameAs(findMenuGroupList);
+    assertThat(result.size()).isEqualTo(menuGroupList.size());
+    assertThat(menuGroupList).isSameAs(result);
   }
 }
