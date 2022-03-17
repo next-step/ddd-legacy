2306393ee41ec86207a5a2c31b82c659c2c53762
hiro <kiryuinh32@gmail.com>
2022-03-17 16:13:21 +0900

    feat: create ProductStubRepository

--- src/main/java/kitchenpos/domain/ProductRepository.java
+++ src/main/java/kitchenpos/domain/ProductRepository.java
@@ -3,8 +3,20 @@ package kitchenpos.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-public interface ProductRepository extends JpaRepository<Product, UUID> {
+public interface ProductRepository {
+
+    Product save(Product product);
+
+    Optional<Product> findById(UUID uuid);
+
+    List<Product> findAll();
+
     List<Product> findAllByIdIn(List<UUID> ids);
+}
+
+interface JpaProductRepository extends ProductRepository, JpaRepository<Product, UUID> {
+
 }
