d585c8b0c0c5da8f58e23fd26d9733706c8027cf
hiro <kiryuinh32@gmail.com>
2022-03-17 16:13:07 +0900

    feat: create MenuStubRepository

--- src/main/java/kitchenpos/domain/MenuRepository.java
+++ src/main/java/kitchenpos/domain/MenuRepository.java
@@ -5,11 +5,23 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-public interface MenuRepository extends JpaRepository<Menu, UUID> {
+public interface MenuRepository {
+
+    Menu save(Menu menu);
+
+    List<Menu> findAll();
+
+    Optional<Menu> findById(UUID id);
+
     List<Menu> findAllByIdIn(List<UUID> ids);
 
     @Query("select m from Menu m, MenuProduct mp where mp.product.id = :productId")
     List<Menu> findAllByProductId(@Param("productId") UUID productId);
+}
+
+interface JpaMenuRepository extends MenuRepository, JpaRepository<Menu, UUID> {
+
 }
---
+++ src/test/java/kitchenpos/stub/MenuStubRepository.java
@@ -0,0 +1,44 @@
+package kitchenpos.stub;
+
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuRepository;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MenuStubRepository implements MenuRepository {
+
+    private final Map<UUID, Menu> menus = new HashMap<>();
+
+    @Override
+    public Menu save(Menu menu) {
+        menus.put(menu.getId(), menu);
+        return menu;
+    }
+
+    @Override
+    public List<Menu> findAll() {
+        return new ArrayList<>(menus.values());
+    }
+
+    @Override
+    public Optional<Menu> findById(UUID id) {
+        return Optional.ofNullable(menus.get(id));
+    }
+
+    @Override
+    public List<Menu> findAllByIdIn(List<UUID> ids) {
+        return menus.values().stream()
+                .filter(menu -> ids.contains(menu.getId()))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Menu> findAllByProductId(UUID productId) {
+        return menus.values().stream()
+                .filter(menu -> menu.getMenuProducts()
+                        .stream()
+                        .anyMatch(menuProduct -> menuProduct.getProductId().equals(productId)))
+                .collect(Collectors.toList());
+    }
+}
