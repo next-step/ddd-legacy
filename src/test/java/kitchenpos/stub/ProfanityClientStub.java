349dc1099b42573507e3b67847ba0f2be25a47aa
hiro <kiryuinh32@gmail.com>
2022-03-17 16:14:37 +0900

    feat: create ProfanityClientStub

---
+++ src/test/java/kitchenpos/stub/ProfanityClientStub.java
@@ -0,0 +1,17 @@
+package kitchenpos.stub;
+
+import kitchenpos.infra.ProfanityClient;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ProfanityClientStub implements ProfanityClient {
+
+    private final List<String> profanities = Arrays.asList("xxx", "욕설");
+
+    @Override
+    public boolean containsProfanity(String text) {
+        return profanities.stream()
+                .anyMatch(profanity -> profanity.contains(text));
+    }
+}
