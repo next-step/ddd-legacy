4939180ebbd28a98e77d0fc73cccca5e1242d33a
hiro <kiryuinh32@gmail.com>
2022-03-17 11:58:11 +0900

    feat: MenuGroupStubRepository 구현

--- src/main/java/kitchenpos/domain/MenuGroupRepository.java
+++ src/main/java/kitchenpos/domain/MenuGroupRepository.java
@@ -2,7 +2,17 @@ package kitchenpos.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
-public interface MenuGroupRepository extends JpaRepository<MenuGroup, UUID> {
+public interface MenuGroupRepository {
+    MenuGroup save(MenuGroup menuGroup);
+
+    List<MenuGroup> findAll();
+
+    Optional<MenuGroup> findById(UUID uuid);
+}
+
+interface JpaMenuGroupRepository extends MenuGroupRepository, JpaRepository<MenuGroup, UUID> {
 }
