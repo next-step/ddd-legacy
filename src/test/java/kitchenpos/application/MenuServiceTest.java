ad7f1f35a5acec40925b9508ef0ee6042a70a1e8
yongju <odetsuo@gmail.com>
2022-03-19 12:17:57 +0900

    test: 메뉴 관리 테스트 수정
    mock 대신 fake 객체 활용

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -1,179 +1,166 @@
 package kitchenpos.application;
 
-import static kitchenpos.application.MenuFixture.뿌링클_세트;
 import static kitchenpos.application.MenuGroupFixture.세트메뉴;
 import static kitchenpos.application.MenuProductFixture.맛초킹_1개;
 import static kitchenpos.application.MenuProductFixture.콜라_1개;
 import static kitchenpos.application.MenuProductFixture.콜라_수량_오류;
 import static kitchenpos.application.ProductFixture.맛초킹;
-import static kitchenpos.application.ProductFixture.뿌링클;
 import static kitchenpos.application.ProductFixture.콜라;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyList;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.BDDMockito.given;
 
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.NoSuchElementException;
-import java.util.Optional;
-import java.util.UUID;
-import java.util.stream.Stream;
 import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
 import kitchenpos.domain.MenuGroupRepository;
+import kitchenpos.domain.MenuProduct;
 import kitchenpos.domain.MenuRepository;
+import kitchenpos.domain.Product;
 import kitchenpos.domain.ProductRepository;
-import kitchenpos.infra.PurgomalumClient;
+import kitchenpos.infra.ProfanityClient;
 import org.assertj.core.api.ThrowableAssert.ThrowingCallable;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-@DisplayName("메뉴")
-@ExtendWith(MockitoExtension.class)
+@DisplayName("메뉴 관리")
 class MenuServiceTest {
 
-    @Mock
-    private MenuRepository menuRepository;
-    @Mock
-    private MenuGroupRepository menuGroupRepository;
-    @Mock
-    private ProductRepository productRepository;
-    @Mock(lenient = true)
-    private PurgomalumClient purgomalumClient;
+    private final MenuRepository menuRepository = new InMemoryMenuRepository();
+    private final MenuGroupRepository menuGroupRepository = new InMemoryMenuGroupRepository();
+    private final ProductRepository productRepository = new InMemoryProductRepository();
+    private final ProfanityClient profanityClient = new FakeProfanityClient();
 
-    @InjectMocks
     private MenuService menuService;
-
-    private Menu 맛초킹_세트;
 
     @BeforeEach
     void setUp() {
-        맛초킹_세트 = new Menu();
-        맛초킹_세트.setName("맛초킹 세트");
-        맛초킹_세트.setMenuGroup(세트메뉴);
-        맛초킹_세트.setMenuGroupId(세트메뉴.getId());
-        맛초킹_세트.setMenuProducts(Arrays.asList(맛초킹_1개, 콜라_1개));
-        맛초킹_세트.setPrice(BigDecimal.valueOf(11_000L));
-        맛초킹_세트.setDisplayed(true);
+        menuService = new MenuService(menuRepository, menuGroupRepository, productRepository, profanityClient);
     }
 
-    @DisplayName("메뉴 생성 예외 - 메뉴 가격 0원 미만")
-    @Test
-    void createPriceUnderZeroException() {
+    @DisplayName("메뉴의 가격은 0원 이상이어야 한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"-1"})
+    @NullSource
+    void createPriceUnderZeroException(BigDecimal price) {
         //given
-        맛초킹_세트.setPrice(BigDecimal.valueOf(-1L));
+        Menu 잘못된_가격의_메뉴 = 메뉴_생성(price);
 
         //when
-        ThrowingCallable actual = () -> menuService.create(맛초킹_세트);
+        ThrowingCallable actual = () -> menuService.create(잘못된_가격의_메뉴);
 
         //then
         assertThatThrownBy(actual).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("메뉴 생성 예외 - 메뉴 그룹 없음")
+    @DisplayName("메뉴는 메뉴 그룹에 속해야 한다.")
     @Test
     void createMenuGroupNotExistException() {
         //given
-        맛초킹_세트.setMenuGroup(null);
+        Menu 메뉴_그룹_없는_메뉴 = 메뉴_생성(11_000);
 
         //when
-        ThrowingCallable actual = () -> menuService.create(맛초킹_세트);
+        ThrowingCallable actual = () -> menuService.create(메뉴_그룹_없는_메뉴);
 
         //then
         assertThatThrownBy(actual).isInstanceOf(NoSuchElementException.class);
     }
 
-    @DisplayName("메뉴 생성 예외 - 메뉴 상품 없음")
+    @DisplayName("메뉴는 1개 이상의 상품을 포함해야 한다.")
     @Test
     void createMenuProductsNotExistException() {
         //given
-        맛초킹_세트.setMenuProducts(null);
+        MenuGroup 세트_메뉴 = menuGroupRepository.save(세트메뉴);
 
-        given(menuGroupRepository.findById(any(UUID.class))).willReturn(Optional.of(세트메뉴));
+        Menu 상품_없는_메뉴 = 메뉴_생성(11_000);
+        상품_없는_메뉴.setMenuGroupId(세트_메뉴.getId());
 
         //when
-        ThrowingCallable actual = () -> menuService.create(맛초킹_세트);
+        ThrowingCallable actual = () -> menuService.create(상품_없는_메뉴);
 
         //then
         assertThatThrownBy(actual).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("메뉴 생성 예외 - 메뉴 상품 개수 미일치")
+    @DisplayName("중복된 메뉴상품을 가질 수 없다.")
     @Test
     void createMenuProductsMismatchException() {
         //given
-        맛초킹_세트.setMenuProducts(Arrays.asList(맛초킹_1개, 맛초킹_1개, 콜라_1개));
+        MenuGroup 세트_메뉴 = menuGroupRepository.save(세트메뉴);
+        productRepository.save(맛초킹);
+        productRepository.save(콜라);
 
-        given(menuGroupRepository.findById(any(UUID.class))).willReturn(Optional.of(세트메뉴));
-        given(productRepository.findAllByIdIn(anyList())).willReturn(Arrays.asList(맛초킹, 콜라));
+        Menu 상품_없는_메뉴 = 메뉴_생성(11_000);
+        상품_없는_메뉴.setMenuGroupId(세트_메뉴.getId());
+        상품_없는_메뉴.setMenuProducts(Arrays.asList(맛초킹_1개, 맛초킹_1개, 콜라_1개));
 
         //when
-        ThrowingCallable actual = () -> menuService.create(맛초킹_세트);
+        ThrowingCallable actual = () -> menuService.create(상품_없는_메뉴);
 
         //then
         assertThatThrownBy(actual).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("메뉴 생성 예외 - 메뉴 상품 수량 부족")
+    @DisplayName("메뉴는 수량이 부족한 상품을 포함할 수 없다.")
     @Test
     void createMenuProductsLackQuantityException() {
-        맛초킹_세트.setMenuProducts(Arrays.asList(맛초킹_1개, 콜라_수량_오류));
+        //given
+        MenuGroup 세트_메뉴 = menuGroupRepository.save(세트메뉴);
+        productRepository.save(맛초킹);
+        productRepository.save(콜라);
 
-        given(menuGroupRepository.findById(any(UUID.class))).willReturn(Optional.of(세트메뉴));
-        given(productRepository.findAllByIdIn(anyList())).willReturn(Arrays.asList(맛초킹, 콜라));
-        given(productRepository.findById(맛초킹.getId())).willReturn(Optional.of(맛초킹));
+        Menu 상품_수량_오류_메뉴 = 메뉴_생성(11_000);
+        상품_수량_오류_메뉴.setMenuGroupId(세트_메뉴.getId());
+        상품_수량_오류_메뉴.setMenuProducts(Arrays.asList(맛초킹_1개, 콜라_수량_오류));
 
         //when
-        ThrowingCallable actual = () -> menuService.create(맛초킹_세트);
+        ThrowingCallable actual = () -> menuService.create(상품_수량_오류_메뉴);
 
         //then
         assertThatThrownBy(actual).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("메뉴 생성 예외 - 상품들의 총 가격보다 높음")
+    @DisplayName("메뉴의 가격은 상품들의 합산 가격과 같거나 작아야 한다.")
     @Test
     void createPriceException() {
         //given
-        맛초킹_세트.setPrice(BigDecimal.valueOf(15_000L));
+        MenuGroup 세트_메뉴 = menuGroupRepository.save(세트메뉴);
+        productRepository.save(맛초킹);
+        productRepository.save(콜라);
 
-        given(menuGroupRepository.findById(any(UUID.class))).willReturn(Optional.of(세트메뉴));
-        given(productRepository.findAllByIdIn(anyList())).willReturn(Arrays.asList(맛초킹, 콜라));
-        given(productRepository.findById(맛초킹.getId())).willReturn(Optional.of(맛초킹));
-        given(productRepository.findById(콜라.getId())).willReturn(Optional.of(콜라));
+        Menu 비싼_메뉴 = 메뉴_생성(15_000);
+        비싼_메뉴.setMenuGroupId(세트_메뉴.getId());
+        비싼_메뉴.setMenuProducts(Arrays.asList(맛초킹_1개, 콜라_1개));
 
         //when
-        ThrowingCallable actual = () -> menuService.create(맛초킹_세트);
+        ThrowingCallable actual = () -> menuService.create(비싼_메뉴);
 
         //then
         assertThatThrownBy(actual).isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("메뉴 생성 예외 - 이름에 비속어 포함")
+    @DisplayName("메뉴 이름에는 비속어가 포함될 수 없다.")
     @Test
     void createNameException() {
         //given
-        맛초킹_세트.setName("비속어");
+        MenuGroup 세트_메뉴 = menuGroupRepository.save(세트메뉴);
+        productRepository.save(맛초킹);
+        productRepository.save(콜라);
 
-        given(menuGroupRepository.findById(any(UUID.class))).willReturn(Optional.of(세트메뉴));
-        given(productRepository.findAllByIdIn(anyList())).willReturn(Arrays.asList(맛초킹, 콜라));
-        given(productRepository.findById(맛초킹.getId())).willReturn(Optional.of(맛초킹));
-        given(productRepository.findById(콜라.getId())).willReturn(Optional.of(콜라));
-        given(purgomalumClient.containsProfanity("비속어")).willReturn(true);
+        Menu 비속어_메뉴 = 메뉴_생성(11_000);
+        비속어_메뉴.setMenuGroupId(세트_메뉴.getId());
+        비속어_메뉴.setMenuProducts(Arrays.asList(맛초킹_1개, 콜라_1개));
+        비속어_메뉴.setName("비속어");
 
         //when
-        ThrowingCallable actual = () -> menuService.create(맛초킹_세트);
+        ThrowingCallable actual = () -> menuService.create(비속어_메뉴);
 
         //then
         assertThatThrownBy(actual).isInstanceOf(IllegalArgumentException.class);
@@ -183,59 +170,46 @@ class MenuServiceTest {
     @Test
     void create() {
         //given
-        given(menuGroupRepository.findById(any(UUID.class))).willReturn(Optional.of(세트메뉴));
-        given(productRepository.findAllByIdIn(anyList())).willReturn(Arrays.asList(뿌링클, 콜라));
-        given(productRepository.findById(뿌링클.getId())).willReturn(Optional.of(뿌링클));
-        given(productRepository.findById(콜라.getId())).willReturn(Optional.of(콜라));
-        given(purgomalumClient.containsProfanity(anyString())).willReturn(false);
-        given(menuRepository.save(any(Menu.class))).willReturn(뿌링클_세트);
+        Menu 맛초킹_세트 = 맛초킹_세트_생성(11_000);
 
         //when
-        Menu menu = menuService.create(뿌링클_세트);
+        Menu menu = menuService.create(맛초킹_세트);
 
         //then
         assertAll(
-            () -> assertThat(menu.getName()).isEqualTo("뿌링클 세트"),
-            () -> assertThat(menu.getMenuGroupId()).isEqualTo(세트메뉴.getId()),
+            () -> assertThat(menu.getName()).isEqualTo(맛초킹_세트.getName()),
             () -> assertThat(menu.getMenuProducts()).hasSize(2),
-            () -> assertThat(menu.getPrice()).isEqualTo(뿌링클_세트.getPrice()),
-            () -> assertThat(menu.isDisplayed()).isTrue()
+            () -> assertThat(menu.getPrice()).isEqualTo(맛초킹_세트.getPrice()),
+            () -> assertThat(menu.isDisplayed()).isEqualTo(맛초킹_세트.isDisplayed())
         );
     }
 
-    @DisplayName("가격 변경 예외 - 0원 미만")
+    @DisplayName("가격을 0원 미만으로 변경할 수 없다.")
     @ParameterizedTest(name = "변경 가격: [{arguments}]")
-    @MethodSource("changePriceException")
+    @ValueSource(strings = {"-1"})
+    @NullSource
     void changePriceException(BigDecimal price) {
         //given
-        맛초킹_세트.setId(UUID.randomUUID());
-        맛초킹_세트.setPrice(price);
+        Menu 포장_전용_메뉴 = menuRepository.save(메뉴_생성(20_000));
+
+        Menu 가격_변경 = 메뉴_생성(price);
 
         //when
-        ThrowingCallable actual = () -> menuService.changePrice(맛초킹_세트.getId(), 맛초킹_세트);
+        ThrowingCallable actual = () -> menuService.changePrice(포장_전용_메뉴.getId(), 가격_변경);
 
         //then
         assertThatThrownBy(actual).isInstanceOf(IllegalArgumentException.class);
     }
 
-    private static Stream<Arguments> changePriceException() {
-        return Stream.of(
-            Arguments.of((BigDecimal) null),
-            Arguments.of(BigDecimal.valueOf(-1))
-        );
-    }
-
-    @DisplayName("가격 변경 예외 - 상품들의 총 가격보다 높음")
+    @DisplayName("상품들의 총 가격보다 높은 가격으로 변경할 수 없다.")
     @Test
     void changePriceGreaterThanProductsPricesException() {
         //given
-        맛초킹_세트.setId(UUID.randomUUID());
-        맛초킹_세트.setPrice(BigDecimal.valueOf(15_000L));
-
-        given(menuRepository.findById(any(UUID.class))).willReturn(Optional.of(맛초킹_세트));
+        Menu 맛초킹_세트 = 맛초킹_세트_생성(11_000);
+        Menu 가격_변경 = 메뉴_생성(50_000);
 
         //when
-        ThrowingCallable actual = () -> menuService.changePrice(맛초킹_세트.getId(), 맛초킹_세트);
+        ThrowingCallable actual = () -> menuService.changePrice(맛초킹_세트.getId(), 가격_변경);
 
         //then
         assertThatThrownBy(actual).isInstanceOf(IllegalArgumentException.class);
@@ -245,29 +219,38 @@ class MenuServiceTest {
     @Test
     void changePrice() {
         //given
-        Menu 변경할_가격 = new Menu();
-        변경할_가격.setPrice(BigDecimal.valueOf(10_500L));
-
-        given(menuRepository.findById(any())).willReturn(Optional.of(뿌링클_세트));
+        Menu 맛초킹_세트 = 맛초킹_세트_생성(11_000);
+        Menu 가격_인하 = 메뉴_생성(10_500);
 
         //when
-        Menu menu = menuService.changePrice(뿌링클_세트.getId(), 변경할_가격);
+        Menu menu = menuService.changePrice(맛초킹_세트.getId(), 가격_인하);
         BigDecimal actual = menu.getPrice();
 
         //then
         assertThat(actual).isEqualTo(BigDecimal.valueOf(10_500L));
     }
 
-    @DisplayName("진열 예외 - 상품들의 총 가격보다 높음")
+    @DisplayName("등록되지 않은 메뉴는 진열할 수 없다.")
+    @Test
+    void displayNotExistException() {
+        //given
+        Menu 없는_메뉴 = new Menu();
+
+        //when
+        ThrowingCallable actual = () -> menuService.display(없는_메뉴.getId());
+
+        //then
+        assertThatThrownBy(actual).isInstanceOf(NoSuchElementException.class);
+    }
+
+    @DisplayName("상품들의 총 가격보다 높은 메뉴는 진열할 수 없다.")
     @Test
     void displayException() {
         //given
-        맛초킹_세트.setPrice(BigDecimal.valueOf(15_000L));
-
-        given(menuRepository.findById(any())).willReturn(Optional.of(맛초킹_세트));
+        Menu 마진_대박_맛초킹_세트 = 맛초킹_세트_생성(50_000);
 
         //when
-        ThrowingCallable actual = () -> menuService.display(맛초킹_세트.getId());
+        ThrowingCallable actual = () -> menuService.display(마진_대박_맛초킹_세트.getId());
 
         //then
         assertThatThrownBy(actual).isInstanceOf(IllegalStateException.class);
@@ -277,9 +260,7 @@ class MenuServiceTest {
     @Test
     void display() {
         //given
-        맛초킹_세트.setPrice(BigDecimal.valueOf(11_000L));
-
-        given(menuRepository.findById(any())).willReturn(Optional.of(맛초킹_세트));
+        Menu 맛초킹_세트 = 맛초킹_세트_생성(11_000);
 
         //when
         Menu menu = menuService.display(맛초킹_세트.getId());
@@ -288,14 +269,14 @@ class MenuServiceTest {
         assertThat(menu.isDisplayed()).isTrue();
     }
 
-    @DisplayName("진열 제외 - 등록된 메뉴만 진열 제외 가능")
+    @DisplayName("등록되지 않은 메뉴는 진열에서 제외할 수 없다.")
     @Test
     void hideException() {
         //given
-        given(menuRepository.findById(any())).willThrow(NoSuchElementException.class);
+        Menu 없는_메뉴 = new Menu();
 
         //when
-        ThrowingCallable actual = () -> menuService.hide(뿌링클_세트.getId());
+        ThrowingCallable actual = () -> menuService.hide(없는_메뉴.getId());
 
         //then
         assertThatThrownBy(actual).isInstanceOf(NoSuchElementException.class);
@@ -305,7 +286,7 @@ class MenuServiceTest {
     @Test
     void hide() {
         //given
-        given(menuRepository.findById(any())).willReturn(Optional.of(맛초킹_세트));
+        Menu 맛초킹_세트 = 맛초킹_세트_생성(11_000);
 
         //when
         Menu menu = menuService.hide(맛초킹_세트.getId());
@@ -318,13 +299,48 @@ class MenuServiceTest {
     @Test
     void findALl() {
         //given
-        given(menuRepository.findAll()).willReturn(Arrays.asList(뿌링클_세트, 맛초킹_세트));
+        menuRepository.save(메뉴_생성(10_000));
+        menuRepository.save(메뉴_생성(20_000));
 
         //when
         List<Menu> actual = menuService.findAll();
 
         //then
         assertThat(actual).hasSize(2);
+    }
+
+    private Menu 맛초킹_세트_생성(int price) {
+        MenuGroup 세트_메뉴 = menuGroupRepository.save(세트메뉴);
+        Product 맛초킹 = productRepository.save(ProductFixture.맛초킹);
+        Product 콜라 = productRepository.save(ProductFixture.콜라);
+
+        MenuProduct 맛초킹1개 = 메뉴_상품_생성(맛초킹);
+        MenuProduct 콜라1개 = 메뉴_상품_생성(콜라);
+
+        Menu 맛초킹_세트 = 메뉴_생성(price);
+        맛초킹_세트.setMenuGroupId(세트_메뉴.getId());
+        맛초킹_세트.setMenuProducts(Arrays.asList(맛초킹1개, 콜라1개));
+        맛초킹_세트.setName("맛초킹 세트");
+
+        return menuRepository.save(맛초킹_세트);
+    }
+
+    private MenuProduct 메뉴_상품_생성(Product product) {
+        MenuProduct menuProduct = new MenuProduct();
+        menuProduct.setQuantity(1L);
+        menuProduct.setProduct(product);
+        menuProduct.setProductId(product.getId());
+        return menuProduct;
+    }
+
+    private Menu 메뉴_생성(int price) {
+        return 메뉴_생성(BigDecimal.valueOf(price));
+    }
+
+    private Menu 메뉴_생성(BigDecimal price) {
+        Menu menu = new Menu();
+        menu.setPrice(price);
+        return menu;
     }
 
 }
