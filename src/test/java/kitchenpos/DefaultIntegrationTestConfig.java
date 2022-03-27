cd4f287013f3b56f90dba83fac4f0f25350b88f2
v-park <v.park@navercorp.com>
2022-03-27 15:23:07 +0900

    fix(test): 테스트 관련 설정 수정
    1. application-text.properties를 test/resources로 이동
    
    2. DefaultIntegrationTestConsifg에서 @ExtendWith(SpringExtension.class)를 제거

--- src/test/java/kitchenpos/DefaultIntegrationTestConfig.java
+++ src/test/java/kitchenpos/DefaultIntegrationTestConfig.java
@@ -2,11 +2,8 @@ package kitchenpos;
 
 import static kitchenpos.constant.Profile.TEST;
 
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@ExtendWith(SpringExtension.class)
 @ActiveProfiles(value = TEST)
 public class DefaultIntegrationTestConfig {
 
