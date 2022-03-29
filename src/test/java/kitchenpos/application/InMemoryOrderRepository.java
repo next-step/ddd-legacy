decba09f501381854f541e9507283c0adeed7547
yongju <odetsuo@gmail.com>
2022-03-30 08:54:38 +0900

    refactor: enum 비교
    enum은 equals로 비교할 필요 없다.

--- src/test/java/kitchenpos/application/InMemoryOrderRepository.java
+++ src/test/java/kitchenpos/application/InMemoryOrderRepository.java
@@ -25,7 +25,7 @@ public class InMemoryOrderRepository implements OrderRepository {
     public boolean existsByOrderTableAndStatusNot(OrderTable orderTable, OrderStatus status) {
         return orders.values().stream()
             .anyMatch(
-                order -> order.getOrderTableId().equals(orderTable.getId()) && !order.getStatus().equals(status));
+                order -> order.getOrderTableId().equals(orderTable.getId()) && order.getStatus() != status);
     }
 
     @Override
