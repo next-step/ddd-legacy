bca4cd18c82ea2d51c628cc569a1f9b63f453bef
yongju <odetsuo@gmail.com>
2022-03-30 08:48:39 +0900

    chore: 미사용 코드 삭제

--- src/test/java/kitchenpos/application/MenuGroupFixture.java
+++ src/test/java/kitchenpos/application/MenuGroupFixture.java
@@ -1,7 +1,5 @@
 package kitchenpos.application;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.UUID;
 import kitchenpos.domain.MenuGroup;
 
@@ -9,7 +7,6 @@ public class MenuGroupFixture {
 
     public static final MenuGroup 세트메뉴 = new MenuGroup();
     public static final MenuGroup 추천메뉴 = new MenuGroup();
-    public static final List<MenuGroup> 메뉴판 = Arrays.asList(세트메뉴, 추천메뉴);
 
     static {
         initialize(세트메뉴, "세트메뉴");
