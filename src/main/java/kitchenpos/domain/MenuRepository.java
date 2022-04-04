72e13aef0c4df95d1e8b7b2e54b03423ccd1ab7a
yongju <odetsuo@gmail.com>
2022-04-05 07:44:49 +0900

    refactor: MenuRepository에서 data-jpa 의존 제거

--- src/main/java/kitchenpos/domain/MenuRepository.java
+++ src/main/java/kitchenpos/domain/MenuRepository.java
@@ -3,14 +3,11 @@ package kitchenpos.domain;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface MenuRepository {
     List<Menu> findAllByIdIn(List<UUID> ids);
 
-    @Query("select m from Menu m, MenuProduct mp where mp.product.id = :productId")
-    List<Menu> findAllByProductId(@Param("productId") UUID productId);
+    List<Menu> findAllByProductId(UUID productId);
 
     Optional<Menu> findById(UUID menuId);
 
