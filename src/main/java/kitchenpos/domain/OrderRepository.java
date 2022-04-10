bec4eb06ac1d3903de0a7e08def7e783a651252a
yongju <odetsuo@gmail.com>
2022-04-11 08:02:52 +0900

    style(feedback): 코드 가독성
    Preferences - Tools - Actions to Save
    Reformat code, Optimize imports
    활성화

--- src/main/java/kitchenpos/domain/OrderRepository.java
+++ src/main/java/kitchenpos/domain/OrderRepository.java
@@ -7,6 +7,7 @@ import java.util.UUID;
 public interface OrderRepository {
 
     Optional<Order> findById(UUID orderId);
+
     boolean existsByOrderTableAndStatusNot(OrderTable orderTable, OrderStatus status);
 
     Order save(Order order);
