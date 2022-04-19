b7ddd0ab041e029ae3245785e00f213682fa8f81
YunJinChoi <zzdd1558@gmail.com>
2022-04-20 02:24:15 +0900

    refactor: 메뉴 그룹 Test코드 리펙토링

--- src/main/java/kitchenpos/domain/MenuGroupRepository.java
+++ src/main/java/kitchenpos/domain/MenuGroupRepository.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
-public interface MenuGroupRepository {
+public interface  MenuGroupRepository {
     MenuGroup save(MenuGroup menuGroup);
 
     List<MenuGroup> findAll();
