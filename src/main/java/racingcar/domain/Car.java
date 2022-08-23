72ccf76a7924df981c2c48f3b154185b5a85d556
csh0034 <csh0034@gmail.com>
2022-08-23 21:04:38 +0900

    feat: 이동 전략 추가 및 거리 검증

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -1,18 +1,32 @@
 package racingcar.domain;
 
+import racingcar.strategy.MoveStrategy;
+
 public class Car {
   private static final int MAX_OF_NAME = 5;
 
   private final String name;
+  private int distance;
 
   public Car(String name) {
     validateSizeOfName(name);
     this.name = name;
+    this.distance = 0;
   }
 
   private void validateSizeOfName(String name) {
     if (name.length() > MAX_OF_NAME) {
       throw new IllegalArgumentException();
     }
+  }
+
+  public void move(MoveStrategy moveStrategy) {
+    if (moveStrategy.canMove()) {
+      distance++;
+    }
+  }
+
+  public int getDistance() {
+    return distance;
   }
 }
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -1,12 +1,16 @@
 package racingcar.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.strategy.MoveStrategy;
 
 class CarTest {
+    private static final String VALID_NAME = "valid";
 
     @DisplayName("이름이 기준값 보다 클 경우 예외 발생")
     @ParameterizedTest
@@ -15,4 +19,44 @@ class CarTest {
         assertThatIllegalArgumentException().isThrownBy(() -> new Car(nameOfCar));
     }
 
+    @DisplayName("초기 거리값 검증")
+    @Test
+    void validateInitialDistance() {
+        // given
+        Car car = new Car(VALID_NAME);
+
+        // when
+        int distance = car.getDistance();
+
+        // then
+        assertThat(distance).isZero();
+    }
+
+    @DisplayName("움직이는 전략일 경우 거리 증가")
+    @Test
+    void movable() {
+        // given
+        Car car = new Car(VALID_NAME);
+        MoveStrategy movableStrategy = () -> true;
+
+        // when
+        car.move(movableStrategy);
+
+        // then
+        assertThat(car.getDistance()).isOne();
+    }
+
+    @DisplayName("움직이지 않는 전략일 경우 거리 고정")
+    @Test
+    void immovable() {
+        // given
+        Car car = new Car(VALID_NAME);
+        MoveStrategy immovableStrategy = () -> false;
+
+        // when
+        car.move(immovableStrategy);
+
+        // then
+        assertThat(car.getDistance()).isZero();
+    }
 }
