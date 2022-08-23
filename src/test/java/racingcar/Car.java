94f40ba828d7836917c55c7301e45b7f54812324
jeongjaeeom <jeongjae.eom@gmail.com>
2022-08-23 22:15:21 +0900

    feat($compile): racingcar
    implement the car

---
+++ src/test/java/racingcar/Car.java
@@ -0,0 +1,35 @@
+package racingcar;
+
+public class Car {
+
+  public static final int MAXIMUM_NAME_LENGTH = 5;
+  public static final int MOVING_CONDITION = 4;
+  private final String name;
+  private int position;
+
+  public Car(String name) {
+    this(name, 0);
+  }
+
+  public Car(String name, int position) {
+    if (name == null || name.isBlank()) {
+      throw new IllegalArgumentException("자동차의 이름은 비어 있을 수 없습니다.");
+    }
+
+    if (name.length() > MAXIMUM_NAME_LENGTH) {
+      throw new IllegalArgumentException("자동차의 이름은 5글자를 넘을 수 없습니다.");
+    }
+    this.name = name;
+    this.position = position;
+  }
+
+  public void move(MovingStrategy strategy) {
+    if (strategy.movable()) {
+      position++;
+    }
+  }
+
+  public int getPosition() {
+    return position;
+  }
+}
---
+++ src/test/java/racingcar/HoldStrategy.java
@@ -0,0 +1,9 @@
+package racingcar;
+
+public class HoldStrategy implements MovingStrategy {
+
+  @Override
+  public boolean movable() {
+    return false;
+  }
+}
