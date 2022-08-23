c04b19565904e8ee17e4c59b26a7a130e41c1982
csh0034 <csh0034@gmail.com>
2022-08-23 21:42:24 +0900

    style: indent 수정

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -3,30 +3,30 @@ package racingcar.domain;
 import racingcar.strategy.MoveStrategy;
 
 public class Car {
-  private static final int MAX_OF_NAME = 5;
+    private static final int MAX_OF_NAME = 5;
 
-  private final String name;
-  private int distance;
+    private final String name;
+    private int distance;
 
-  public Car(String name) {
-    validateSizeOfName(name);
-    this.name = name;
-    this.distance = 0;
-  }
-
-  private void validateSizeOfName(String name) {
-    if (name.length() > MAX_OF_NAME) {
-      throw new IllegalArgumentException();
+    public Car(String name) {
+        validateSizeOfName(name);
+        this.name = name;
+        this.distance = 0;
     }
-  }
 
-  public void move(MoveStrategy moveStrategy) {
-    if (moveStrategy.canMove()) {
-      distance++;
+    private void validateSizeOfName(String name) {
+        if (name.length() > MAX_OF_NAME) {
+            throw new IllegalArgumentException();
+        }
     }
-  }
 
-  public int getDistance() {
-    return distance;
-  }
+    public void move(MoveStrategy moveStrategy) {
+        if (moveStrategy.canMove()) {
+            distance++;
+        }
+    }
+
+    public int getDistance() {
+        return distance;
+    }
 }
