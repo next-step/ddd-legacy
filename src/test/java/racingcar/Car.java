2f60ee797c5f50aa850681fcd719ad1192255e5f
dominiqn <dmut7691@gmail.com>
2022-08-23 22:57:46 +0900

    refactor: 이름 제한 길이 상수에 이름 붙이기

--- src/test/java/racingcar/Car.java
+++ src/test/java/racingcar/Car.java
@@ -1,8 +1,10 @@
 package racingcar;
 
 public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
+
     public Car(final String name) {
-        if (name.length() > 5) {
+        if (name.length() > MAX_NAME_LENGTH) {
             throw new IllegalArgumentException("이름은 5자를 넘을 수 없습니다.");
         }
     }
