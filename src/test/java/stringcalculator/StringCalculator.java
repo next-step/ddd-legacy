ff9beccb155f547a28acc0efce045bb6a796b137
flamme <flamme1004@gmail.com>
2022-08-24 23:58:33 +0900

    feat(stringCalculator): 구분자를 쉼표(,) 의외에 콜론(:)을 사용할 수 있도록 하라

--- src/test/java/stringcalculator/StringCalculator.java
+++ src/test/java/stringcalculator/StringCalculator.java
@@ -1,16 +1,16 @@
 package stringcalculator;
 
-import java.util.Arrays;
+import java.util.stream.Stream;
 
 public class StringCalculator {
+    private static final String DEFAULT_SEPARATORS = "[,:]";
     private static final int ZERO = 0;
 
     public int add(String text) {
         if (text == null || text.isBlank()) {
             return ZERO;
         }
-        return Arrays
-                .stream(text.split(","))
+        return Stream.of(text.split(DEFAULT_SEPARATORS))
                 .map(Integer::parseInt)
                 .reduce(0, Integer::sum);
     }
