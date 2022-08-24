21037092562e5cfcec4b4507762e4396e39e3c0c
flamme <flamme1004@gmail.com>
2022-08-25 00:06:58 +0900

    feat(stringCalculator): //와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있도록 하라

--- src/test/java/stringcalculator/StringCalculator.java
+++ src/test/java/stringcalculator/StringCalculator.java
@@ -1,17 +1,31 @@
 package stringcalculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class StringCalculator {
     private static final String DEFAULT_SEPARATORS = "[,:]";
+    private static final Pattern PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final int DELIMITER_INDEX = 1;
+    private static final int NUMBER_INDEX = 2;
     private static final int ZERO = 0;
 
     public int add(String text) {
         if (text == null || text.isBlank()) {
             return ZERO;
         }
-        return Stream.of(text.split(DEFAULT_SEPARATORS))
+        return Stream.of(parseNumbers(text))
                 .map(Integer::parseInt)
                 .reduce(0, Integer::sum);
+    }
+
+    private String[] parseNumbers(String text) {
+        Matcher matcher = PATTERN.matcher(text);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(DELIMITER_INDEX);
+            return matcher.group(NUMBER_INDEX).split(customDelimiter);
+        }
+        return text.split(DEFAULT_SEPARATORS);
     }
 }
