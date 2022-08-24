65ffbbffaa3a3476d1c82786315050fa300ac64a
csh0034 <csh0034@gmail.com>
2022-08-24 22:11:37 +0900

    refactor: PositiveNumber 예외 메세지 추가

--- src/main/java/calculator/PositiveNumber.java
+++ src/main/java/calculator/PositiveNumber.java
@@ -1,6 +1,8 @@
 package calculator;
 
 public class PositiveNumber {
+    private static final String VALIDATE_EXCEPTION_MESSAGE = "양수만 가능합니다. input: %d";
+
     private final int value;
 
     public PositiveNumber(String value) {
@@ -14,7 +16,7 @@ public class PositiveNumber {
 
     private void validate(int value) {
         if (value <= 0) {
-            throw new RuntimeException();
+            throw new RuntimeException(String.format(VALIDATE_EXCEPTION_MESSAGE, value));
         }
     }
 
