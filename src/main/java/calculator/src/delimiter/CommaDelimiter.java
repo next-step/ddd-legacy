8f35d5d0b487751d2cf45ee427fd92bfc9a7774f
hand <sns2831@gmail.com>
2022-08-24 22:11:50 +0900

    refactor(calculator): Delimiter abstract class로 변경.

--- src/main/java/calculator/src/delimiter/CommaDelimiter.java
+++ src/main/java/calculator/src/delimiter/CommaDelimiter.java
@@ -1,18 +1,9 @@
 package calculator.src.delimiter;
 
-import static java.util.stream.Collectors.toList;
-
-import java.util.Arrays;
-import java.util.List;
-
-public class CommaDelimiter implements Delimiter{
-
-	private static final String DELIMITER = ",";
-
+public class CommaDelimiter extends Delimiter {
+	
 	@Override
-	public List<String> tokenize(String input) {
-		return Arrays.stream(input.split(DELIMITER))
-			.map(String::trim)
-			.collect(toList());
+	protected String delimiter() {
+		return ",";
 	}
 }
--- src/main/java/calculator/src/delimiter/Delimiter.java
+++ src/main/java/calculator/src/delimiter/Delimiter.java
@@ -1,8 +1,17 @@
 package calculator.src.delimiter;
 
+import static java.util.stream.Collectors.toList;
+
+import java.util.Arrays;
 import java.util.List;
 
-public interface Delimiter {
+abstract class Delimiter {
 
-	List<String> tokenize(String input);
+	public List<String> tokenize(String input) {
+		return Arrays.stream(input.split(delimiter()))
+			.map(String::trim)
+			.collect(toList());
+	}
+
+	abstract protected String delimiter();
 }
