8ba3a7e03f7977d3da1c22b2790f6368fb22bd45
wlroh <wlroh.paul@gmail.com>
2022-08-25 01:22:02 +0900

    feat: 문자열 계산기의 덧셈을 구현한다.

--- src/test/java/stringcalculator/StringCalculator.java
+++ src/test/java/stringcalculator/StringCalculator.java
@@ -1,11 +1,16 @@
 package stringcalculator;
 
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class StringCalculator {
 
     private static final String SEPARATOR = ",|:";
+    private static final String CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)";
+    private static final int CUSTOM_DELIMITER_INDEX = 1;
+    private static final int NUMBER_VALUE_INDEX = 2;
 
     public StringCalculator() {
     }
@@ -14,7 +19,19 @@ public class StringCalculator {
         if (Objects.isNull(value) || value.isBlank()) {
             throw new RuntimeException("Null 이거나 공란일 수 없습니다.");
         }
-        return Stream.of(value.split(SEPARATOR))
+
+        Matcher m = Pattern.compile(CUSTOM_DELIMITER_REGEX).matcher(value);
+        if (m.find()) {
+            String customDelimiter = m.group(CUSTOM_DELIMITER_INDEX);
+            String[] split = m.group(NUMBER_VALUE_INDEX).split(customDelimiter);
+            return sum(split);
+        }
+        return sum(value.split(SEPARATOR));
+    }
+
+    private int sum(final String[] values) {
+        return Stream.of(values)
+                .parallel()
                 .map(PositiveNumber::new)
                 .mapToInt(PositiveNumber::toInt)
                 .sum();
