b7c51e596dfb5cc65a072d02ad3ce1be0a3d195c
Minsung Son <sns2831@gmail.com>
2022-08-24 17:25:26 +0900

    0단계 - JUnit 5 학습 (#291)
    * feat(racingcar): 자동차 구현.
    
    * refactor(racingcar): 자동차 검증 로직 메서드 분리.

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,44 @@
+package racingcar;
+
+public class Car {
+
+	private static final int MAXIMUM_NAME_LENGTH = 5;
+
+	private final String name;
+	private int position;
+
+	public Car(String name) {
+		this(name, 0);
+	}
+
+	public Car(String name, final int position) {
+		validateName(name);
+
+		this.name = name;
+		this.position = position;
+	}
+
+	private void validateName(String name) {
+		if (name == null) {
+			throw new IllegalArgumentException("자동차 이름은 비어있을 수 없습니다.");
+		}
+
+		if (name.isBlank()) {
+			throw new IllegalArgumentException("자동차 이름은 비어있을 수 없습니다.");
+		}
+
+		if (name.length() > MAXIMUM_NAME_LENGTH) {
+			throw new IllegalArgumentException("자동차 이름은 5글자를 넘을 수 없습니다.");
+		}
+	}
+
+	public void move(final MoveStrategy moveStrategy) {
+		if (moveStrategy.isMovable()) {
+			this.position++;
+		}
+	}
+
+	public int getPosition() {
+		return position;
+	}
+}
