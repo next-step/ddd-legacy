fb9b2a96750c9b05476180f7a3af37543746540b
chosundeveloper <iddaumid@naver.com>
2022-08-25 13:45:09 +0900

    refactor : 전략 패턴 적용

--- src/test/java/racingcar/Car.java
+++ src/test/java/racingcar/Car.java
@@ -3,9 +3,11 @@ package racingcar;
 public class Car{
 
     private String name;
+    private int position;
 
     private Car(String name) {
         this.name = name;
+        this.position = 0;
     }
 
     public static Car of(String name) {
@@ -15,8 +17,13 @@ public class Car{
         return new Car(name);
     }
 
-    public boolean move(int num) {
-        return num >= 4;
+    public void move(MovingStrategy movingStrategy) {
+        if (movingStrategy.movable()) {
+            position++;
+        }
     }
 
+    public int getPosition() {
+        return position;
+    }
 }
