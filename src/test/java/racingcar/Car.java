69a5bac06ffbc31d36c6cc4ee5c433b1d82f5e29
chosundeveloper <iddaumid@naver.com>
2022-08-25 14:43:53 +0900

    refactor : 상수 사용

--- src/test/java/racingcar/Car.java
+++ src/test/java/racingcar/Car.java
@@ -2,17 +2,23 @@ package racingcar;
 
 public class Car{
 
+    private static final int MAXIMUM_NAME_LENGTH = 5;
+    private static final int INITIAL_POSITION = 0;
+
     private String name;
     private int position;
 
     private Car(String name) {
         this.name = name;
-        this.position = 0;
+        this.position = INITIAL_POSITION;
     }
 
     public static Car of(String name) {
-        if (name.length() > 5) {
-            throw new IllegalArgumentException("자동차 이름은 5글자를 넘길 수 없습니다.");
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("자동차의 이름은 비어 있을 수 없습니다.");
+        }
+        if (name.length() > MAXIMUM_NAME_LENGTH) {
+            throw new IllegalArgumentException("자동차의 이름은 5글자를 넘을 수 없습니다.");
         }
         return new Car(name);
     }
