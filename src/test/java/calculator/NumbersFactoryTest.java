6d32b39e4c89f5b4efb1149a20261dffe8c13f37
MoonHKLee <mf311110@gmail.com>
2022-08-26 02:09:00 +0900

    test(string-calculator) : 예외 테스트케이스 추가.

--- src/test/java/calculator/NumbersFactoryTest.java
+++ src/test/java/calculator/NumbersFactoryTest.java
@@ -7,58 +7,50 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class CalculatorTest {
+public class NumbersFactoryTest {
 
-    private StringCalculator calculator;
-
+    private NumbersFactory numbersFactory;
+    
     @BeforeEach
     void setUp() {
-        calculator = new StringCalculator();
-    }
-
-    @DisplayName(value = "빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
-    @ParameterizedTest
-    @NullAndEmptySource
-    void emptyOrNull(final String text) {
-        assertThat(calculator.add(text)).isZero();
+        this.numbersFactory = new NumbersFactory();
     }
 
     @DisplayName(value = "숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
     @ParameterizedTest
     @ValueSource(strings = {"1"})
     void oneNumber(final String text) {
-        assertThat(calculator.add(text)).isSameAs(Integer.parseInt(text));
+        assertThat(numbersFactory.getNumbers(text)).containsExactly(1);
     }
 
-    @DisplayName(value = "숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
+    @DisplayName(value = "숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 배열을 반환한다.")
     @ParameterizedTest
     @ValueSource(strings = {"1,2"})
     void twoNumbers(final String text) {
-        assertThat(calculator.add(text)).isSameAs(3);
+        assertThat(numbersFactory.getNumbers(text)).containsExactly(1,2);
     }
 
     @DisplayName(value = "구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.")
     @ParameterizedTest
     @ValueSource(strings = {"1,2:3"})
     void colons(final String text) {
-        assertThat(calculator.add(text)).isSameAs(6);
+        assertThat(numbersFactory.getNumbers(text)).containsExactly(1,2,3);
     }
 
     @DisplayName(value = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
     @ParameterizedTest
     @ValueSource(strings = {"//;\n1;2;3"})
     void customDelimiter(final String text) {
-        assertThat(calculator.add(text)).isSameAs(6);
+        assertThat(numbersFactory.getNumbers(text)).containsExactly(1,2,3);
     }
 
     @DisplayName(value = "문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리를 한다.")
     @Test
     void negative() {
         assertThatExceptionOfType(RuntimeException.class)
-            .isThrownBy(() -> calculator.add("-1"))
+            .isThrownBy(() -> numbersFactory.getNumbers("-1"))
             .withMessage(ExceptionMessages.WRONG_INPUT_EXCEPTION);
     }
 
@@ -66,7 +58,8 @@ public class CalculatorTest {
     @Test
     void character() {
         assertThatExceptionOfType(RuntimeException.class)
-            .isThrownBy(() -> calculator.add("1,a"))
+            .isThrownBy(() -> numbersFactory.getNumbers("1,a"))
             .withMessage(ExceptionMessages.WRONG_INPUT_EXCEPTION);
     }
+
 }
