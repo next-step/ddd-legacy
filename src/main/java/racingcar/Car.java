397a0338852b301020690d5cfcf7e98c854f9976
wenodev <logicalstark1105@gmail.com>
2022-08-25 12:52:14 +0900

    feat(Car): validate the car name

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,13 +1,29 @@
 package racingcar;
 
 public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
+    private static final String MAX_NAME_LENGTH_MESSAGE = "자동차 이름은 5글자보다 작거나 같아야 합니다.";
+    private static final String EMPTY_NAME_MESSAGE = "자동차 이름은 비어있을 수 없습니다.";
+
     private String name;
 
     public Car(String name) {
+        if (isEmpty(name)) {
+            throw new IllegalArgumentException(EMPTY_NAME_MESSAGE);
+        }
+
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException(MAX_NAME_LENGTH_MESSAGE);
+        }
+
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    private boolean isEmpty(String name) {
+        return name == null || name.isBlank();
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,10 +1,13 @@
 package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 /**
  * 자동차 이름은 5 글자를 넘을 수 없다.
@@ -23,5 +26,24 @@ public class CarTest {
         Car car = new Car(name);
 
         assertThat(car.getName()).isEqualTo(name);
+    }
+
+    @DisplayName("자동차 이름은 5글자보다 클 수 없다.")
+    @Test
+    void construct_with_greater_than_5_letters() {
+        String name = "wenoaa";
+
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> new Car(name)
+        );
+    }
+
+    @DisplayName("자동차 이름은 비어있을 수 없다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void construct_with_empty_name(String name) {
+        assertThatIllegalArgumentException().isThrownBy(
+                () -> new Car(name)
+        );
     }
 }
