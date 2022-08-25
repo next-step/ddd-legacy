f92be81dfc78f3425bd0e5e0895ffb5e74852b39
MoonHKLee <mf311110@gmail.com>
2022-08-26 01:47:44 +0900

    refactor(string-calculator) : NumbersFactory로 숫자배열 추출 객체 분리.

---
+++ src/test/java/calculator/NumbersFactory.java
@@ -0,0 +1,54 @@
+package calculator;
+
+import static calculator.DelimiterPattern.CUSTOM_REGEX;
+import static calculator.DelimiterPattern.DEFAULT_REGEX;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class NumbersFactory {
+
+    public int[] getNumbers(String input) {
+        Matcher matcher = getCustomPatternMatcher(input);
+        if (matcher.find()) {
+            return splitNumbers(getTokenByCustomDelimiter(matcher));
+        }
+        return splitNumbers(getTokenByDefaultDelimiter(input));
+    }
+
+    private Matcher getCustomPatternMatcher(String input) {
+        return Pattern.compile(CUSTOM_REGEX).matcher(input);
+    }
+
+    private String[] getTokenByCustomDelimiter(Matcher m) {
+        String customDelimiter = m.group(1);
+        return m.group(2).split(customDelimiter);
+    }
+
+    private String[] getTokenByDefaultDelimiter(String input) {
+        return input.split(DEFAULT_REGEX);
+    }
+
+    private int[] splitNumbers(String[] tokens) {
+        List<Integer> numbers = new ArrayList<>();
+        for (String number : tokens) {
+            if (!isNaturalNumber(number)) {
+                throw new RuntimeException();
+            }
+            numbers.add(Integer.parseInt(number));
+        }
+        return numbers.stream().mapToInt(v->v).toArray();
+    }
+
+    private boolean isNaturalNumber(String param) {
+        char[] chars = param.toCharArray();
+        for (char aChar : chars) {
+            if (aChar > '9' || aChar < '0') {
+                return false;
+            }
+        }
+        return true;
+    }
+}
--- src/test/java/calculator/StringCalculator.java
+++ src/test/java/calculator/StringCalculator.java
@@ -1,57 +1,24 @@
 package calculator;
 
-import static calculator.DelimiterPattern.CUSTOM_REGEX;
-import static calculator.DelimiterPattern.DEFAULT_REGEX;
-
 import io.micrometer.core.instrument.util.StringUtils;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class StringCalculator {
+    private final NumbersFactory numbersFactory;
+
+    public StringCalculator() {
+        this.numbersFactory = new NumbersFactory();
+    }
 
     public int add(String input) {
         int sum = 0;
         if (StringUtils.isEmpty(input)) {
             return sum;
         }
-        Matcher matcher = getCustomPatternMatcher(input);
-        if (matcher.find()) {
-            return getSum(sum, getTokenByCustomDelimiter(matcher));
-        }
-        return getSum(sum, getTokenByDefaultDelimiter(input));
-    }
-
-    private Matcher getCustomPatternMatcher(String input) {
-        return Pattern.compile(CUSTOM_REGEX).matcher(input);
-    }
-
-    private String[] getTokenByCustomDelimiter(Matcher m) {
-        String customDelimiter = m.group(1);
-        return m.group(2).split(customDelimiter);
-    }
-
-    private String[] getTokenByDefaultDelimiter(String input) {
-        return input.split(DEFAULT_REGEX);
-    }
-
-    private int getSum(int sum, String[] tokens) {
-        for (String number : tokens) {
-            if (!isNaturalNumber(number)) {
-                throw new RuntimeException();
-            }
-            sum += Integer.parseInt(number);
+        int[] numbers = numbersFactory.getNumbers(input);
+        for (int number : numbers) {
+            sum += number;
         }
         return sum;
-    }
-
-    private boolean isNaturalNumber(String param) {
-        char[] chars = param.toCharArray();
-        for (char aChar : chars) {
-            if (aChar > '9' || aChar < '0') {
-                return false;
-            }
-        }
-        return true;
     }
 
 }
