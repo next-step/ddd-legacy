68996dcf5229e3f0878a48c97fff959edd77370e
Chunghwan Kim <49121847+ch0213@users.noreply.github.com>
2022-08-26 15:56:38 +0900

    [도메인 주도 설계 등장 배경] 1단계 - 문자열 덧셈 계산기 (#317)
    * test(calculator): 문자열 덧셈 계산기 테스트 작성
    
    * feat(calculator): 문자열 덧셈 계산기 구현
    
    * refactor(calculator): 미리 정의한 상수를 사용해 예외 메시지 작성

---
+++ src/main/java/calculator/StringNumber.java
@@ -0,0 +1,31 @@
+package calculator;
+
+public class StringNumber {
+    private static final int MINIMUM_NUMBER = 0;
+
+    private final int number;
+
+    public StringNumber() {
+        this(0);
+    }
+
+    public StringNumber(String number) {
+        this(Integer.parseInt(number));
+    }
+
+    public StringNumber(int number) {
+        if (number < MINIMUM_NUMBER) {
+            throw new RuntimeException(MINIMUM_NUMBER + "이상의 숫자를 입력해주세요.");
+        }
+
+        this.number = number;
+    }
+
+    public StringNumber add(StringNumber other) {
+        return new StringNumber(this.number + other.number);
+    }
+
+    public int getNumber() {
+        return number;
+    }
+}
