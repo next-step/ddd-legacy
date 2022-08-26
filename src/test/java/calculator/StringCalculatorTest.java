51d5360f3429f6c0899950562a274ae2b12d9b80
chosundeveloper <iddaumid@naver.com>
2022-08-26 13:27:29 +0900

    refactor : test 코드 정리

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,75 +1,65 @@
 package calculator;
 
-
-/*
-
-문자열 계산기에 문자열을 입력하고
-구분자를 기준으로 분리한 각 숫자의 합을 반환한다.
-
-문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw 한다.
-
-1. 기본 구분자 : 쉼표(,) 또는 콜론(:)
-2. 커스텀 구분자 : 문자열 앞부분의 “//”와 “\n” 사이에 위치하는 문자
-
-공백 문자열을 입력할 경우  : “” => 0
-문자를 입력할 경우 :
-음수를 입력할 경우 :
-기본 구분자 사용 : "1,2" => 3,
-기본 구분자 사용 : "1,2,3" => 6
-기본 구분자 쉼표와 콜론 동시 사용 : “1,2:3” => 6
-커스텀 구분자 사용 : “//;\n1;2;3” => 6
-*/
-
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static calculator.StringCalculator.of;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @DisplayName("문자열 계산기")
 public class StringCalculatorTest {
 
-    @DisplayName("공백 문자열을 입력할 경우 “” => 0 ")
-    @Test
-    void name() {
-        String value = "";
-        assertThat(of(value).calculate()).isEqualTo(0);
+    private StringCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new StringCalculator();
     }
 
-    @DisplayName("기본 구분자 사용 : /* 1,2 => 3 */")
-    @Test
-    void defaultCalculate() {
-
-        //        given
-        String value = "1,2";
-
-        //        then
-        assertThat(of(value).calculate()).isEqualTo(3);
-
+    @DisplayName(value = "빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void emptyOrNull(final String text) {
+        assertThat(calculator.add(text)).isZero();
     }
 
-    @DisplayName("기본 구분자 사용 : \"1,2,3\" => 6")
-    @Test
-    void 세개숫자의합() {
-
-        //        given
-        String value = "1,2,3";
-
-        //        then
-        assertThat(of(value).calculate()).isEqualTo(6);
-
+    @DisplayName(value = "숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1"})
+    void oneNumber(final String text) {
+        assertThat(calculator.add(text)).isSameAs(Integer.parseInt(text));
     }
 
-    @DisplayName("기본 구분자 쉼표와 콜론 동시 사용 : “1,2:3” => 6")
+    @DisplayName(value = "숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2"})
+    void twoNumbers(final String text) {
+        assertThat(calculator.add(text)).isSameAs(3);
+    }
+
+    @DisplayName(value = "구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2:3"})
+    void colons(final String text) {
+        assertThat(calculator.add(text)).isSameAs(6);
+    }
+
+    @DisplayName(value = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\n1;2;3"})
+    void customDelimiter(final String text) {
+        assertThat(calculator.add(text)).isSameAs(6);
+    }
+
+    @DisplayName(value = "문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리를 한다.")
     @Test
-    void 콜론쉼표계산() {
-
-        //        given
-        String value = "1,2,3";
-
-        //        then
-        assertThat(of(value).calculate()).isEqualTo(6);
-
+    void negative() {
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> calculator.add("-1"));
     }
 
 }
