8373ee4b7f1ddcab4269ee64b08b555a39fc4d60
sangw0804 <sangw0804@likelion.org>
2022-08-26 20:53:44 +0900

    feat(stringcalculator): 기능구현 완료

--- src/test/java/stringcalculator/StringCalculator.java
+++ src/test/java/stringcalculator/StringCalculator.java
@@ -1,7 +1,35 @@
 package stringcalculator;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.IntStream;
+
 public class StringCalculator {
-    public int add(String text) {
-        return 0;
+    public int add(String rawStr) {
+        if (rawStr == null || rawStr.isEmpty()) {
+            return 0;
+        }
+
+        Pattern pattern = Pattern.compile("//(.*)\\n(.*)");
+        Matcher matcher = pattern.matcher(rawStr);
+        String target = rawStr;
+        List<String> splitters = new ArrayList<>(List.of(",", ":"));
+        if(matcher.matches()) {
+            String splitter = matcher.group(1);
+            target = matcher.group(2);
+            splitters.add(splitter);
+        }
+
+        String temp = String.join("", splitters);
+        int sum = Arrays.stream(target.split("[" + temp + "]")).mapToInt(Integer::parseInt).peek(num -> {
+            if (num < 0) {
+                throw new RuntimeException();
+            }
+        }).sum();
+
+        return sum;
     }
 }
