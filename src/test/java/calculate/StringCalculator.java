d50465445cfe5247d5807f18ac9de5bff93a069e
jeongjaeeom <jeongjae.eom@gmail.com>
2022-08-26 15:22:47 +0900

    feat($compile): 컴마(,) 및 세미콜론(:)으로 구분된 두 숫자의 합을 반환한다
    컴마(,) 및 세미콜론(:)으로 구분된 두 숫자의 합을 반환한다
    - 컴마(,) 및 세미콜론(:)으로 구분된 두 숫자의 합 반환 구현
    - 숫자 하나를 문자열로 입력할 경우 해당 숫자 반환 구현

--- src/test/java/calculate/StringCalculator.java
+++ src/test/java/calculate/StringCalculator.java
@@ -2,11 +2,19 @@ package calculate;
 
 public class StringCalculator {
 
+  private static final String DEFAULT_DELIMITER = ",|:";
+
   public static int calculate(final String text) {
     if (text == null || text.isBlank()) {
       return 0;
     }
-    return 1;
+
+    String[] operands = text.split(DEFAULT_DELIMITER);
+    int sum = 0;
+    for (String operand : operands) {
+      sum += Integer.parseInt(operand);
+    }
+    return sum;
   }
 
 }
--- src/test/java/calculate/StringCalculatorTest.java
+++ src/test/java/calculate/StringCalculatorTest.java
@@ -3,7 +3,9 @@ package calculate;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 class StringCalculatorTest {
@@ -13,6 +15,23 @@ class StringCalculatorTest {
   @NullAndEmptySource
   void calculate_NotNullAndNotEmpty(String text) {
     assertThat(StringCalculator.calculate(text)).isZero();
+  }
+
+  @Test
+  @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
+  void calculate_OneNumber() {
+    assertThat(StringCalculator.calculate("5")).isEqualTo(5);
+  }
+
+  @DisplayName("컴마(,) 및 세미콜론(:)으로 구분된 두 숫자의 합을 반환한다")
+  @CsvSource({
+      "'1,2,3', 6",
+      "'1:3:5', 9",
+      "'4,5:6', 15",
+  })
+  @ParameterizedTest
+  void calculate_DefaultDelimiter(String text, int sum) {
+    assertThat(StringCalculator.calculate(text)).isEqualTo(sum);
   }
 
 }
