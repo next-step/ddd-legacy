40ed608ee83c25c592eba27db6d811b290026171
chosundeveloper <iddaumid@naver.com>
2022-08-26 14:25:28 +0900

    refactor : 메서드 추출 리팩토링

--- src/test/java/calculator/StringCalculator.java
+++ src/test/java/calculator/StringCalculator.java
@@ -1,14 +1,13 @@
 package calculator;
 
-import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static java.util.Arrays.stream;
+
 public class StringCalculator {
 
-    public StringCalculator() {
-
-    }
+    private final String DELIMITER = ",:";
 
     public int add(String text) {
 
@@ -16,31 +15,41 @@ public class StringCalculator {
             return 0;
         }
 
+        String originText = findOriginText(text);
+        String delimiters = createDelimiters(text);
+
+        return sum(originText.split(delimiters));
+    }
+
+    private String createDelimiters(String text) {
+        String customDelimiter = findCustomDelimiter(text);
+        if (customDelimiter != null) {
+            return "[" + DELIMITER + customDelimiter + "]";
+        }
+        return "[" + DELIMITER + "]";
+    }
+
+    private String findOriginText(String text) {
         Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
-        String[] tokens = null;
+        return m.find() ? m.group(2) : text;
+    }
 
-        if (m.find()) {
-            String customDelimiter = m.group(1);
-            tokens = m.group(2).split(customDelimiter);
-        }
+    private String findCustomDelimiter(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        return m.find() ? m.group(1) : null;
+    }
 
-        int sum = 0;
+    private static int sum(String[] tokens) {
+        validNativeNumber(tokens);
+        return stream(tokens).mapToInt(Integer::parseInt).sum();
+    }
 
-        if (tokens == null) {
-            String[] splitTokens = text.split("[,:]");
-            for (String token : splitTokens) {
-                if (Integer.parseInt(token) < 0) {
-                    throw new IllegalArgumentException();
-                }
-            }
-            return Arrays.stream(splitTokens).mapToInt(Integer::parseInt).sum();
-        }
-
+    private static void validNativeNumber(String[] tokens) {
         for (String token : tokens) {
-            sum = sum + Arrays.stream(token.split("[,:]")).mapToInt(Integer::parseInt).sum();
+            if (Integer.parseInt(token) < 0) {
+                throw new IllegalArgumentException();
+            }
         }
-
-        return sum;
     }
 
 }
