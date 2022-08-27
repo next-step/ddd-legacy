f374016cbe313bda4ffe9762cb8ec3ee97bd3009
wisdomme <sthyun@gmail.com>
2022-08-27 12:03:09 +0900

    refactor (calculator): Parser 클래스 상수선언/변수명 수정

--- src/test/java/calculator/Parser.java
+++ src/test/java/calculator/Parser.java
@@ -5,23 +5,27 @@ import java.util.regex.Pattern;
 
 public class Parser {
 
-    private final Pattern customDelimiterPattern = Pattern.compile("//(.)\n(.*)");
-    private final String DELIMITER_COMMON_REGEX = ",|:";
+    private static final Pattern CUSTOM_PATTERN_COMPILE = Pattern.compile("//(.)\n(.*)");
+    private static final String DELIMITER_COMMON_REGEX = ",|:";
 
     public String[] execute(String text) {
-        Matcher matcher = customDelimiterPattern.matcher(text);
+        Matcher matcher = CUSTOM_PATTERN_COMPILE.matcher(text);
         if (matcher.find()) {
             return splitTextByCustomDelimiter(matcher);
         }
         return splitText(text);
     }
 
-    private String[] splitTextByCustomDelimiter(Matcher m) {
-        String customDelimiter = m.group(1);
+    private String[] splitTextByCustomDelimiter(Matcher matcher) {
+        return matcher.group(2).split(getCustomDelimiter(matcher));
+    }
+
+    private String getCustomDelimiter(Matcher matcher) {
+        String customDelimiter = matcher.group(1);
         if ("+".equals(customDelimiter) || "*".equals(customDelimiter)) {
             customDelimiter = String.format("\\%s", customDelimiter);
         }
-        return m.group(2).split(customDelimiter);
+        return customDelimiter;
     }
 
     private String[] splitText(String text) {
