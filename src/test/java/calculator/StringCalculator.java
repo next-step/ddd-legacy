fa64e781402330488dfb9e6347d3503010949753
dominiqn <dmut7691@gmail.com>
2022-08-28 19:22:00 +0900

    refactor: 각종 리터럴 상수에 이름 붙이기

--- src/test/java/calculator/StringCalculator.java
+++ src/test/java/calculator/StringCalculator.java
@@ -8,7 +8,10 @@ import static java.lang.Integer.parseInt;
 public class StringCalculator {
 
     private static final String DELIMITER_REGEX = ",|:";
-    private static final Pattern VALID_INPUT_PATTERN = Pattern.compile("(?://(?<delimiter>.)\\n)?(?<tokens>.*)");
+    private static final String DELIMITER_PLACEHOLDER = "delimiter";
+    private static final String TOKENS_PLACEHOLDER = "tokens";
+    private static final String VALID_INPUT_REGEX = "(?://(?<" + DELIMITER_PLACEHOLDER + ">.)\\n)?(?<" + TOKENS_PLACEHOLDER + ">.*)";
+    private static final Pattern VALID_INPUT_PATTERN = Pattern.compile(VALID_INPUT_REGEX);
 
     public int add(String text) {
         if (text == null || text.isBlank()) {
@@ -21,7 +24,7 @@ public class StringCalculator {
             throw new IllegalArgumentException("올바르지 않은 형태입니다.");
         }
 
-        var customDelimiter = a.group("delimiter");
+        var customDelimiter = a.group(DELIMITER_PLACEHOLDER);
 
         var delimiterRegex = DELIMITER_REGEX;
 
@@ -29,7 +32,7 @@ public class StringCalculator {
             delimiterRegex = delimiterRegex + "|" + customDelimiter;
         }
 
-        String[] tokens = a.group("tokens").split(delimiterRegex);
+        String[] tokens = a.group(TOKENS_PLACEHOLDER).split(delimiterRegex);
 
         return Arrays.stream(tokens)
                 .mapToInt(this::parseIntegerFromToken)
