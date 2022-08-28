dfd390e5ff9959cb3f039b8061b196307825d8b3
dominiqn <dmut7691@gmail.com>
2022-08-28 16:16:40 +0900

    feat(calculator): 입력값이 비어 있으면 0을 반환

---
+++ src/test/java/calculator/StringCalculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class StringCalculator {
+    public int add(String text) {
+        if (text == null || text.isBlank()) {
+            return 0;
+        }
+
+        return 0;
+    }
+}
