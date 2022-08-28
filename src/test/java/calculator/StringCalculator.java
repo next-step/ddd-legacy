4a671f9ab80dd537b39f3d8e9a494fe3ddbd7125
dominiqn <dmut7691@gmail.com>
2022-08-28 16:57:20 +0900

    refactor: 구분자 상수 이름 붙이기
    오타

--- src/test/java/calculator/StringCalculator.java
+++ src/test/java/calculator/StringCalculator.java
@@ -3,12 +3,15 @@ package calculator;
 import java.util.Arrays;
 
 public class StringCalculator {
+
+    private static final String DELIMITER_REGEX = ",|:";
+
     public int add(String text) {
         if (text == null || text.isBlank()) {
             return 0;
         }
 
-        String[] numbers = text.split(",|:");
+        String[] numbers = text.split(DELIMITER_REGEX);
 
         return Arrays.stream(numbers)
                 .mapToInt(Integer::parseInt)
