ac9a7386a4dbeaa61b59bfd9ba9f400f43bbc1fa
dominiqn <dmut7691@gmail.com>
2022-08-28 19:24:07 +0900

    refactor: 입력 문자열 검증 함수 분리

--- src/test/java/calculator/StringCalculator.java
+++ src/test/java/calculator/StringCalculator.java
@@ -1,13 +1,15 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import static java.lang.Integer.parseInt;
 
 public class StringCalculator {
 
-    private static final String DELIMITER_REGEX = ",|:";
+    private static final String DEFAULT_DELIMITER_REGEX = ",|:";
+    private static final Pattern DEFAULT_DELIMITER_PATTERN = Pattern.compile(DEFAULT_DELIMITER_REGEX);
     private static final String DELIMITER_PLACEHOLDER = "delimiter";
     private static final String TOKENS_PLACEHOLDER = "tokens";
     private static final String VALID_INPUT_REGEX = "(?://(?<" + DELIMITER_PLACEHOLDER + ">.)\\n)?(?<" + TOKENS_PLACEHOLDER + ">.*)";
@@ -18,25 +20,37 @@ public class StringCalculator {
             return 0;
         }
 
-        var a = VALID_INPUT_PATTERN.matcher(text);
+        final var input = getValidMatcher(text);
 
-        if (!a.find()) {
-            throw new IllegalArgumentException("올바르지 않은 형태입니다.");
-        }
+        final var delimiterPattern = compileDelimiterPattern(input.group(DELIMITER_PLACEHOLDER));
 
-        var customDelimiter = a.group(DELIMITER_PLACEHOLDER);
-
-        var delimiterRegex = DELIMITER_REGEX;
-
-        if (customDelimiter != null) {
-            delimiterRegex = delimiterRegex + "|" + customDelimiter;
-        }
-
-        String[] tokens = a.group(TOKENS_PLACEHOLDER).split(delimiterRegex);
+        final String[] tokens = splitTokensByDelimiter(input.group(TOKENS_PLACEHOLDER), delimiterPattern);
 
         return Arrays.stream(tokens)
                 .mapToInt(this::parseIntegerFromToken)
                 .sum();
+    }
+
+    private String[] splitTokensByDelimiter(String tokensPart, Pattern delimiterRegex) {
+        return delimiterRegex.split(tokensPart);
+    }
+
+    private Matcher getValidMatcher(String text) {
+        final var matcher = VALID_INPUT_PATTERN.matcher(text);
+
+        if (!matcher.find()) {
+            throw new IllegalArgumentException("올바르지 않은 형태입니다.");
+        }
+
+        return matcher;
+    }
+
+    private Pattern compileDelimiterPattern(String customizedDelimiter) {
+        if (customizedDelimiter == null) {
+            return DEFAULT_DELIMITER_PATTERN;
+        }
+
+        return Pattern.compile(DEFAULT_DELIMITER_REGEX + "|" + customizedDelimiter);
     }
 
     private int parseIntegerFromToken(String token) {
