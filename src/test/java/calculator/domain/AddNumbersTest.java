52221bd9ccdc585fc07003dfa4f57691b7c76182
Hyeon9mak <jinha3507@gmail.com>
2022-08-29 21:00:51 +0900

    test: 모든 입력값 더하는 로직 테스트 코드 추가

--- src/test/java/calculator/domain/AddNumbersTest.java
+++ src/test/java/calculator/domain/AddNumbersTest.java
@@ -1,2 +1,24 @@
-package calculator.domain;public class AddNumbersTest {
+package calculator.domain;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class AddNumbersTest {
+
+    @DisplayName("모든 값이 더해진 결과를 얻을 수 있다.")
+    @Test
+    void allAddNumbers() {
+        // given
+        List<String> strings = Arrays.asList("1", "2", "3");
+
+        // when
+        AddNumbers numbers = AddNumbers.from(strings);
+
+        // then
+        assertThat(numbers.addAllNumbers()).isEqualTo(new AddNumber(6));
+    }
 }
