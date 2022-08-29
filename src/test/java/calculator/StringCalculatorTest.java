b2b04d9eae76ce80d65914bd3fce73cd84e2bb75
choiseon <com1368@naver.com>
2022-08-29 11:37:47 +0900

    text: 빈 문자열, null, 숫자 하나 테스트 코드 작성

---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,33 @@
+package calculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class StringCalculatorTest {
+
+  private StringCalculator calculator;
+
+  @BeforeEach
+  void setUp() {
+    calculator = new StringCalculator();
+  }
+
+  @DisplayName(value = "빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
+  @ParameterizedTest
+  @NullAndEmptySource
+  void emptyOrNull(final String text) {
+    assertThat(calculator.add(text)).isZero();
+  }
+
+  @DisplayName(value = "숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
+  @ParameterizedTest
+  @ValueSource(strings = {"1"})
+  void oneNumber(final String text) {
+    assertThat(calculator.add(text)).isSameAs(Integer.parseInt(text));
+  }
+}
