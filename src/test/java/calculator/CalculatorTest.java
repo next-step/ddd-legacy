9effee82ac26f334fd193081e498996ffa5a0dd1
lehdqlsl <lehdqlsl@naver.com>
2022-08-29 23:19:54 +0900

    🚀 1단계 - 문자열 덧셈 계산기 (#333)
    * feat(calculator): 클래스 작성 및 널/공백 처리 작성.
    
    * feat(calculator): 덧셈 기능 구현
    
    * feat(calculator): 음수값, 숫자 이외값 예외처리
    
    * feat(calculator): 중복 테스트 통합.
    
    * feat(calculator): 커스텀 구분자 기능 구현.
    
    * feat(calculator): static 제거
    
    * feat(calculator): 공백제거
    
    * feat(calculator): add final
    
    * refactor(calculator): DisplayName적용.
    
    * fix(calculator): public 접근제어 제거
    
    * fix(calculator): 클래스명 수정
    
    * fix(calculator): Pattern 상수화
    
    * fix(calculator): 멤버변수 접근제어자 변경.
    
    * fix(calculator): 변수명 수정
    
    * fix(calculator): 정규표현식 상수화
    
    * fix(calculator): 변수명 대문자 수정.
    
    * fix(test): 테스트 케이스 추가

---
+++ src/test/java/calculator/CalculatorTest.java
@@ -0,0 +1,59 @@
+package calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+class StringCalculatorTest {
+    /**
+     * 쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열을 전달하는 경우 구분자를 기준으로 분리한 각 숫자의 합을 반환 (예: “” => 0, "1,2" => 3, "1,2,3" => 6, “1,2:3” => 6)
+     * 앞의 기본 구분자(쉼표, 콜론) 외에 커스텀 구분자를 지정할 수 있다. 커스텀 구분자는 문자열 앞부분의 “//”와 “\n” 사이에 위치하는 문자를 커스텀 구분자로 사용한다. 예를 들어 “//;\n1;2;3”과 같이 값을 입력할 경우 커스텀 구분자는 세미콜론(;)이며, 결과 값은 6이 반환되어야 한다.
+     * 문자열 계산기에 숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException 예외를 throw 한다
+     */
+
+    private StringCalculator calculator;
+
+    @BeforeEach
+    void setUp() {
+        calculator = new StringCalculator();
+    }
+
+    @NullAndEmptySource
+    @ParameterizedTest(name = "빈 문자열이나 Null 값의 경우 0을 반환한다.")
+    void null_and_empty(final String input) {
+        final int result = calculator.sum(input);
+        assertThat(result).isZero();
+    }
+
+    @ParameterizedTest(name = "숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
+    @ValueSource(strings = "1")
+    void input_one_number(final String input) {
+        final int result = calculator.sum(input);
+        assertThat(result).isEqualTo(1);
+    }
+
+    @ParameterizedTest(name = "쉼표 및 콜론 구분자로 덧셈을 한다.")
+    @ValueSource(strings = {"1,2,3", "1:2:3", "1:2,3"})
+    void colon_and_comma(final String input) {
+        final int result = calculator.sum(input);
+        assertThat(result).isEqualTo(6);
+    }
+
+    @ParameterizedTest(name = "음수 및 숫자 이외의 값을 넣으면 RuntimeException 발생한다.")
+    @ValueSource(strings = {"-1", "a,b,c"})
+    void no_number_and_negative(final String input) {
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> calculator.sum(input));
+    }
+
+    @ParameterizedTest(name = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
+    @ValueSource(strings = {"//;\n1;2;3"})
+    void custom(final String input) {
+        final int result = calculator.sum(input);
+        assertThat(result).isEqualTo(6);
+    }
+}
---
+++ src/test/java/calculator/StringCalculator.java
@@ -0,0 +1,58 @@
+package calculator;
+
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+
+    private static final int DEFAULT_VALUE = 0;
+    private static final String CUSTOM_REGEX = "//(.)\n(.*)";
+    private static final String DEFAULT_REGEX = ",|:";
+    private static final Pattern PATTERN = Pattern.compile(CUSTOM_REGEX);
+    private static final String NUMBER = "\\d";
+
+    public int sum(final String input) {
+        if (Objects.isNull(input) || input.isBlank()) {
+            return DEFAULT_VALUE;
+        }
+
+        String[] numbers = parseInput(input);
+
+        return Arrays.stream(numbers)
+                .mapToInt(this::parseInt)
+                .sum();
+    }
+
+    private String[] parseInput(final String input) {
+        Matcher matcher = PATTERN.matcher(input);
+        if (matcher.find()) {
+            String customInput = matcher.group(2);
+            return customInput.split(matcher.group(1));
+        }
+        return input.split(DEFAULT_REGEX);
+    }
+
+    private int parseInt(final String value) {
+        checkNumber(value);
+        checkNegative(value);
+        return Integer.parseInt(value);
+    }
+
+    private void checkNegative(final String value) {
+        int parseIntValue = Integer.parseInt(value);
+
+        if (parseIntValue < 0) {
+            throw new RuntimeException("문자열 계산기에서 음수값은 허용되지 않습니다.");
+        }
+    }
+
+    private void checkNumber(final String value) {
+        boolean isNumber = value.matches(NUMBER);
+
+        if (!isNumber) {
+            throw new RuntimeException("숫자 이외의 값은 허용되지 않습니다.");
+        }
+    }
+}
