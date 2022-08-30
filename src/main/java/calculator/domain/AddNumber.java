141b81bb75121555c382b6b3c689ca0a9466e6db
hyeon9mak <jinha3507@gmail.com>
2022-08-30 11:12:36 +0900

    refactor: null 또는 0이 주어질 경우 0을 반환하도록 변경

--- src/main/java/calculator/domain/AddNumber.java
+++ src/main/java/calculator/domain/AddNumber.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 public class AddNumber {
 
+    private static final AddNumber ZERO = new AddNumber(0);
     private final int value;
 
     public AddNumber(int value) {
@@ -19,10 +20,18 @@ public class AddNumber {
 
     public static AddNumber from(String value) {
         try {
-            return new AddNumber(Integer.parseInt(value));
+            return convertToAddNumber(value);
         } catch (NumberFormatException e) {
             throw new RuntimeException("정수 외 값은 덧셈 값이 될 수 없습니다.");
         }
+    }
+
+    private static AddNumber convertToAddNumber(String value) {
+        if (Objects.isNull(value) || value.isBlank()) {
+            return ZERO;
+        }
+
+        return new AddNumber(Integer.parseInt(value));
     }
 
     public int getValue() {
--- src/test/java/calculator/domain/AddNumberTest.java
+++ src/test/java/calculator/domain/AddNumberTest.java
@@ -1,8 +1,10 @@
 package calculator.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -18,9 +20,22 @@ class AddNumberTest {
 
     @DisplayName("정수 외 값은 더해질 숫자가 될 수 없다.")
     @ParameterizedTest
-    @ValueSource(strings = {"", " ", "#", "AB", "C"})
+    @ValueSource(strings = {"#", "AB", "C"})
     void negativeException(String invalidString) {
         assertThatExceptionOfType(RuntimeException.class)
             .isThrownBy(() -> AddNumber.from(invalidString));
+    }
+
+    @DisplayName("공백이 주어질 경우 0으로 변환한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {" ", ""})
+    void convertToZero(String value) {
+        assertThat(AddNumber.from(value)).isEqualTo(new AddNumber(0));
+    }
+
+    @DisplayName("null이 주어질 경우 0으로 변환한다.")
+    @Test
+    void convertToZero() {
+        assertThat(AddNumber.from(null)).isEqualTo(new AddNumber(0));
     }
 }
