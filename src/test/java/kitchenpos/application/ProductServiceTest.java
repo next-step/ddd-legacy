c58d371465fb81292c44c9cdf2de4a89158bc5bf
dominiqn <dmut7691@gmail.com>
2022-09-04 03:36:38 +0900

    test(kitchenpos): 상품 가격 변경하기 테스트 코드 추가

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -1,6 +1,11 @@
 package kitchenpos.application;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuProduct;
 import kitchenpos.domain.MenuRepository;
 import kitchenpos.domain.Product;
 import kitchenpos.domain.ProductRepository;
@@ -10,6 +15,7 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -103,4 +109,88 @@ class ProductServiceTest {
             );
         }
     }
-}}
+
+    @DisplayName("상품 가격 변경")
+    @Nested
+    class ChangePrice {
+        private final UUID existingProductId = UUID.fromString("00000000-0000-0000-0000-000000000001");
+
+        @DisplayName("변경하려는 가격은 음수가 아니어야 한다.")
+        @Test
+        void illegalTargetPrice() {
+            // given
+            final var targetPrice = new BigDecimal(-1000);
+            final var request = new Product();
+            request.setPrice(targetPrice);
+
+            // when
+            assertThatThrownBy(() -> testService.changePrice(existingProductId, request))
+                    // then
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("상품의 가격을 바꿀 수 있다.")
+        @Test
+        void changePrice() {
+            // given
+            //// 기존 상품
+            final var existingProduct = new Product();
+            existingProduct.setId(existingProductId);
+            existingProduct.setName("연어");
+            existingProduct.setPrice(new BigDecimal(1000));
+            when(productRepository.findById(existingProductId)).thenReturn(Optional.of(existingProduct));
+
+            //// 요청
+            final var targetPrice = new BigDecimal(8000);
+            final var request = new Product();
+            request.setPrice(targetPrice);
+
+            // when
+            final var result = testService.changePrice(existingProductId, request);
+
+            // then
+            assertThat(result.getPrice()).isEqualTo(new BigDecimal(8000));
+        }
+
+        @DisplayName("상품이 속한 메뉴의 총 가격이 상품 가격 합보다 커지면, 그 메뉴는 비공개로 변경된다.")
+        @ParameterizedTest(name = "메뉴 가격 = {0} 변경하려는 상품 가격 = {1}, 메뉴 공개 여부 = {2}")
+        @CsvSource({
+                "1000, 500, false",
+                "1000, 2000, true"
+        })
+        void changePrice(
+                BigDecimal menuPrice,
+                BigDecimal targetProductPrice,
+                boolean expectedDisplayed
+        ) {
+            // given
+            //// 기존 상품
+            final var existingProduct = new Product();
+            existingProduct.setId(existingProductId);
+            existingProduct.setName("연어");
+            existingProduct.setPrice(new BigDecimal(1000));
+            when(productRepository.findById(existingProductId)).thenReturn(Optional.of(existingProduct));
+
+            //// 기존 메뉴
+            final var menuProduct = new MenuProduct();
+            menuProduct.setProduct(existingProduct);
+            menuProduct.setQuantity(1);
+            final var menu = new Menu();
+            menu.setDisplayed(true);
+            menu.setPrice(menuPrice);
+            menu.setMenuProducts(List.of(menuProduct));
+            when(menuRepository.findAllByProductId(existingProductId)).thenReturn(List.of(menu));
+
+            //// 요쳥
+            final var request = new Product();
+            request.setPrice(targetProductPrice);
+
+            // when
+            testService.changePrice(existingProductId, request);
+            final var actualDisplayed = menu.isDisplayed();
+
+            // then
+            assertThat(actualDisplayed).isEqualTo(expectedDisplayed);
+        }
+    }
+}
