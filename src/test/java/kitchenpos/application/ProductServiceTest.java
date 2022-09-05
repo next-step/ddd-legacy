045a538d5a3d4219e9b18dbfe6e65e19bc0aba75
jeongjaeeom <jeongjae.eom@gmail.com>
2022-09-05 13:55:14 +0900

    feat($compile): 유효한 상품 ID와 변경할 상품가격 정보를 입력하면 변경된 상품정보를 반환한다
    유효한 상품 ID와 변경할 상품가격 정보를 입력하면 변경된 상품정보를 반환한다
    - 상품 가격변경 성공 단위 테스트 구현

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -3,11 +3,16 @@ package kitchenpos.application;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.BDDMockito.given;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuProduct;
 import kitchenpos.domain.MenuRepository;
 import kitchenpos.domain.Product;
 import kitchenpos.domain.ProductRepository;
@@ -38,7 +43,7 @@ class ProductServiceTest {
     this.productService = new ProductService(productRepository, menuRepository, purgomalumClient);
   }
 
-  @DisplayName("유효한 상품명과 가격을 입력하여 상품을 등록할 수 있다.")
+  @DisplayName("유효한 상품명과 가격을 입력하면 등록된 상품을 반환한다.")
   @Test
   void givenProduct_whenCreate_thenReturnProduct() {
     // given
@@ -95,6 +100,42 @@ class ProductServiceTest {
 
     // when & then
     assertThatIllegalArgumentException().isThrownBy(() -> productService.create(product));
+  }
+
+  @DisplayName("유효한 상품 ID와 변경할 상품가격 정보를 입력하면 변경된 상품정보를 반환한다.")
+  @Test
+  void givenChangePrice_whenChangePrice_thenReturnChangedProduct() {
+    // given
+    Product product = new Product();
+    product.setId(UUID.randomUUID());
+    product.setName("후라이드치킨");
+    product.setPrice(BigDecimal.valueOf(11000));
+
+    given(productRepository.findById(product.getId())).willReturn(Optional.of(product));
+
+    MenuProduct menuProduct = new MenuProduct();
+    menuProduct.setProduct(product);
+    menuProduct.setQuantity(1);
+
+    Menu menu = new Menu();
+    menu.setId(UUID.randomUUID());
+    menu.setName("후라이드치킨");
+    menu.setPrice(BigDecimal.valueOf(11000));
+    menu.setMenuProducts(List.of(menuProduct));
+    menu.setMenuProducts(List.of(menuProduct));
+
+    given(menuRepository.findAllByProductId(product.getId())).willReturn(List.of(menu));
+
+    Product changePriceProduct = new Product();
+    changePriceProduct.setPrice(BigDecimal.valueOf(12000));
+
+    // when
+    Product changedProduct = productService.changePrice(product.getId(), changePriceProduct);
+
+    // then
+    assertThat(changedProduct.getId()).isNotNull();
+    assertThat(changedProduct.getName()).isEqualTo(product.getName());
+    assertThat(changedProduct.getPrice()).isEqualTo(changePriceProduct.getPrice());
   }
 
 }
