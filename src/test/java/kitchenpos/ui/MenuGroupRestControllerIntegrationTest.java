481803f2b73170c30f659845c1362e541dfea156
jeongjaeeom <jeongjae.eom@gmail.com>
2022-09-05 15:16:47 +0900

    feat($compile): 메뉴그룹 조회 API 통합테스트 구현
    메뉴그룹 조회 API 통합테스트 구현

--- src/test/java/kitchenpos/ui/MenuGroupRestControllerIntegrationTest.java
+++ src/test/java/kitchenpos/ui/MenuGroupRestControllerIntegrationTest.java
@@ -1,5 +1,7 @@
 package kitchenpos.ui;
 
+import static org.hamcrest.Matchers.hasSize;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
@@ -7,6 +9,8 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.List;
+import java.util.UUID;
 import kitchenpos.domain.MenuGroup;
 import kitchenpos.domain.MenuGroupRepository;
 import org.junit.jupiter.api.AfterEach;
@@ -34,6 +38,7 @@ class MenuGroupRestControllerIntegrationTest {
 
   @AfterEach
   void tearDown() {
+    menuGroupRepository.deleteAll();
   }
 
   @DisplayName("유효한 메뉴그룹 등록 요청에 응답을 201 Created와 함께 등록된 메뉴그룹을 반환한다")
@@ -53,5 +58,27 @@ class MenuGroupRestControllerIntegrationTest {
         .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
         .andExpect(jsonPath("$.id").exists())
         .andExpect(jsonPath("$.name").value(menuGroup.getName()));
+  }
+
+  @DisplayName("메뉴드룹 조회 요청에 응답으로 200 OK 응답과 함께 등록된 메뉴그룹을 반환한다")
+  @Test
+  void givenMenuGroups_whenFindAll_thenReturnMenuGroups() throws Exception {
+    MenuGroup menuGroup1 = new MenuGroup();
+    menuGroup1.setId(UUID.randomUUID());
+    menuGroup1.setName("추천메뉴");
+
+    MenuGroup menuGroup2 = new MenuGroup();
+    menuGroup2.setId(UUID.randomUUID());
+    menuGroup2.setName("점심특선");
+
+    menuGroupRepository.saveAll(List.of(menuGroup1, menuGroup2));
+
+    mvc.perform(get("/api/menu-groups").accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(jsonPath("$", hasSize(2)))
+        .andExpect(jsonPath("$[0].name").value(menuGroup1.getName()))
+        .andExpect(jsonPath("$[1].name").value(menuGroup2.getName()));
   }
 }
