2c1ccf13580b7ea7a6c3c6e71b9b90b917b6ba3e
jeongjaeeom <jeongjae.eom@gmail.com>
2022-09-05 16:47:32 +0900

    feat($compile): 메뉴상품이 존재하지 않으면 메뉴를 등록할 수 없다.
    메뉴상품이 존재하지 않으면 메뉴를 등록할 수 없다.
    - 요청한 메뉴 상품이 존재하지 않으면 메뉴를 등록할 수 없다

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -180,6 +180,38 @@ class MenuServiceTest {
     assertThatIllegalArgumentException().isThrownBy(() -> menuService.create(menu));
   }
 
+  @DisplayName("메뉴상품이 존재하지 않으면 메뉴를 등록할 수 없다.")
+  @Test
+  void givenNotFoundMenuProduct_whenCreate_thenIllegalArgumentException() {
+    // given
+    MenuGroup menuGroup = createMenuGroup("추천메뉴");
+    Product product1 = createProduct("후라이드치킨", BigDecimal.valueOf(11000));
+    Product product2 = createProduct("양념치킨", BigDecimal.valueOf(12000));
+    List<MenuProduct> menuProducts = List.of(
+        createMenuProduct(product1, 1),
+        createMenuProduct(product2, 1)
+    );
+
+    List<Product> products = List.of(product1, product2);
+    List<UUID> productIds = products.stream()
+        .map(Product::getId)
+        .collect(Collectors.toList());
+
+    Menu menu = createMenu(
+        "후라이드 + 양념치킨",
+        BigDecimal.valueOf(23000),
+        true,
+        menuGroup,
+        menuProducts
+    );
+
+    given(menuGroupRepository.findById(menuGroup.getId())).willReturn(Optional.of(menuGroup));
+    given(productRepository.findAllByIdIn(productIds)).willReturn(List.of(product1));
+
+    // when & then
+    assertThatIllegalArgumentException().isThrownBy(() -> menuService.create(menu));
+  }
+
   private static Menu createMenu(
       String name,
       BigDecimal price,
