1f985395caa2e4b95c3cf6381683811d5a9dcb5d
jeongjaeeom <jeongjae.eom@gmail.com>
2022-09-05 14:44:46 +0900

    feat($compile): 상품 API 상품가격 변경 통합테스트 구현
    상품 API 상품가격 변경 통합테스트 구현
    - 기존 상품 등록 테스트명 개선
    - 상품 가격 변경 통합테스트 구현

--- src/test/java/kitchenpos/ui/ProductRestControllerIntegrationTest.java
+++ src/test/java/kitchenpos/ui/ProductRestControllerIntegrationTest.java
@@ -1,7 +1,7 @@
 package kitchenpos.ui;
 
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -9,18 +9,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.math.BigDecimal;
+import java.util.UUID;
 import kitchenpos.domain.Product;
-import org.assertj.core.api.Assertions;
+import kitchenpos.domain.ProductRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.util.NestedServletException;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -32,8 +31,12 @@ class ProductRestControllerIntegrationTest {
   @Autowired
   private ObjectMapper objectMapper;
 
+  @Autowired
+  private ProductRepository productRepository;
+
+  @DisplayName("유효한 상품명과 가격을 통한 상품등록 요청에 응답으로 HTTP 201 상태값과 함께 등록된 상품을 반환한다")
   @Test
-  void 올바른_상품명과_가격을_통한_상품등록_요청에_응답으로_등록된_상품을_반환한다() throws Exception {
+  void givenValidProduct_whenCreateProduct_thenStatus201WithCratedProduct() throws Exception {
     Product product = new Product();
     product.setName("후라이드치킨");
     product.setPrice(BigDecimal.valueOf(11000));
@@ -50,6 +53,31 @@ class ProductRestControllerIntegrationTest {
         .andExpect(jsonPath("$.id").exists())
         .andExpect(jsonPath("$.name").value(product.getName()))
         .andExpect(jsonPath("$.price").value(product.getPrice()));
+  }
+
+  @DisplayName("유효한 상품가격변경 요청에 응답으로 HTTP 200 상태값과 함께 변경된 상품을 반환한다")
+  @Test
+  void givenValidChangeProduct_whenChangeProduct_thenStatus200WithChangedProduct() throws Exception {
+    Product product = new Product();
+    product.setId(UUID.randomUUID());
+    product.setName("후라이드치킨");
+    product.setPrice(BigDecimal.valueOf(11000));
+    Product savedProduct = productRepository.save(product);
+
+    Product changePriceProduct = new Product();
+    changePriceProduct.setPrice(BigDecimal.valueOf(12000));
+
+    mvc.perform(
+            put("/api/products/{productId}/price", savedProduct.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(changePriceProduct)))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(jsonPath("$.id").exists())
+        .andExpect(jsonPath("$.name").value(product.getName()))
+        .andExpect(jsonPath("$.price").value(changePriceProduct.getPrice()));
   }
 
 }
