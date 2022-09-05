db9e95c6238a001367fdcb21d3d152423cdd7445
hyeon9mak <jinha3507@gmail.com>
2022-09-05 22:05:50 +0900

    test: 상품 관련 테스트 추가

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -1,5 +1,8 @@
 package kitchenpos.application;
 
+import static kitchenpos.domain.MenuFixture.MenuWithUUIDAndMenuGroup;
+import static kitchenpos.domain.MenuGroupFixture.MenuGroupWithUUID;
+import static kitchenpos.domain.MenuProductFixture.MenuProductWithProduct;
 import static kitchenpos.domain.ProductFixture.Product;
 import static kitchenpos.domain.ProductFixture.ProductWithUUID;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -9,6 +12,9 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuProduct;
 import kitchenpos.domain.Product;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
@@ -120,6 +126,39 @@ public class ProductServiceTest extends IntegrationTest {
                 UUID.fromString("00000000-0000-0000-0000-000000000000"),
                 request_16000원
             )).isExactlyInstanceOf(NoSuchElementException.class);
+        }
+
+        @DisplayName("상품의 금액이 변한 후, 상품이 속한 메뉴의 금액이 모든 상품의 금액보다 높을 경우 해당 메뉴의 전시(노출)을 중단한다.")
+        @Test
+        void stopDisplayedIfInvalidPrice() {
+            // given
+            Product 햄버거 = productRepository.save(ProductWithUUID("햄버거", 15_000));
+            Product 콜라 = productRepository.save(ProductWithUUID("콜라", 2_000));
+            MenuProduct 햄버거_메뉴상품 = MenuProductWithProduct(햄버거, 1);
+            MenuProduct 콜라_메뉴상품 = MenuProductWithProduct(콜라, 1);
+            MenuGroup 세트메뉴 = menuGroupRepository.save(MenuGroupWithUUID("세트메뉴"));
+            Menu 햄버거_콜라_세트메뉴 = MenuWithUUIDAndMenuGroup(
+                "햄버거 + 콜라 세트메뉴",
+                17_000,
+                세트메뉴,
+                햄버거_메뉴상품, 콜라_메뉴상품
+            );
+            햄버거_콜라_세트메뉴.setDisplayed(true);
+            햄버거_콜라_세트메뉴 = menuRepository.save(햄버거_콜라_세트메뉴);
+
+            Product request = Product("콜라", 1_900);
+
+            // when
+            productService.changePrice(콜라.getId(), request);
+
+            // then
+            assertThat(햄버거_콜라_세트메뉴.isDisplayed()).isTrue();
+            assertThat(ID를_통한_메뉴_조회(햄버거_콜라_세트메뉴.getId()).isDisplayed()).isFalse();
+        }
+
+        private Menu ID를_통한_메뉴_조회(UUID menuId) {
+            return menuRepository.findById(menuId)
+                .orElseThrow(NoSuchElementException::new);
         }
     }
 
