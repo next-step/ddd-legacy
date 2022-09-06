1f142a8275baf2bd2c3fc674b13a58cd7b72e967
jeongjaeeom <jeongjae.eom@gmail.com>
2022-09-06 10:54:42 +0900

    feat($compile): 메뉴 숨김 요청 응답으로 HTTP 200 상태값과 함께 숨겨진 메뉴를 반환한다
    메뉴 숨김 요청 응답으로 HTTP 200 상태값과 함께 숨겨진 메뉴를 반환한다
    - 메뉴숨김 API 통합테스트 작성

--- src/test/java/kitchenpos/ui/MenuRestControllerIntegrationTest.java
+++ src/test/java/kitchenpos/ui/MenuRestControllerIntegrationTest.java
@@ -175,4 +175,43 @@ class MenuRestControllerIntegrationTest {
         .andExpect(jsonPath("$.displayed").value(true));
   }
 
+  @DisplayName("메뉴 숨김 요청 응답으로 HTTP 200 상태값과 함께 숨겨진 메뉴를 반환한다")
+  @Test
+  void givenMenu_whenHideMenu_thenStatus200WithHiddenMenu() throws Exception {
+    // given
+    MenuGroup menuGroup = createMenuGroup("추천메뉴");
+
+    Product product1 = createProduct("후라이드치킨", BigDecimal.valueOf(11000));
+    Product product2 = createProduct("양념치킨", BigDecimal.valueOf(12000));
+
+    MenuGroup savedMenuGroup = menuGroupRepository.save(menuGroup);
+    Product savedProduct1 = productRepository.save(product1);
+    Product savedProduct2 = productRepository.save(product2);
+
+    Menu menu = createMenu(
+        "후라이드 + 양념치킨",
+        BigDecimal.valueOf(23000),
+        true,
+        savedMenuGroup,
+        List.of(
+            createMenuProduct(savedProduct1, 1),
+            createMenuProduct(savedProduct2, 1)
+        )
+    );
+
+    Menu savedMenu = menuRepository.save(menu);
+
+    mvc.perform(
+            put("/api/menus/{menuId}/hide", savedMenu.getId())
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(jsonPath("$.id").value(savedMenu.getId().toString()))
+        .andExpect(jsonPath("$.name").value(menu.getName()))
+        .andExpect(jsonPath("$.price").value(menu.getPrice().longValue()))
+        .andExpect(jsonPath("$.displayed").value(false));
+  }
+
 }
