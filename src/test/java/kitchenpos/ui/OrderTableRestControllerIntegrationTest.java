faf57cd86571d42a21746e45591b98579410e882
jeongjaeeom <jeongjae.eom@gmail.com>
2022-09-06 18:14:43 +0900

    feat($test): 주문 입석 처리 요청에 HTTP 200과 함께 입석처리된 주문테이블을 반환한다
    주문 입석 처리 요청에 HTTP 200과 함께 입석처리된 주문테이블을 반환한다

--- src/test/java/kitchenpos/ui/OrderTableRestControllerIntegrationTest.java
+++ src/test/java/kitchenpos/ui/OrderTableRestControllerIntegrationTest.java
@@ -1,6 +1,7 @@
 package kitchenpos.ui;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -60,6 +61,26 @@ class OrderTableRestControllerIntegrationTest {
         .andExpect(jsonPath("$.name").value("1번"))
         .andExpect(jsonPath("$.numberOfGuests").value(0))
         .andExpect(jsonPath("$.occupied").value(false))
+    ;
+  }
+
+  @DisplayName("주문 입석 처리 요청에 HTTP 200과 함께 입석처리된 주문테이블을 반환한다")
+  @Test
+  void givenValidOrderTable_whenSit_thenStatus200WithOrderTable() throws Exception {
+    OrderTable requestOrderTable = new OrderTable();
+    requestOrderTable.setName("1번");
+    OrderTable orderTable = orderTableService.create(requestOrderTable);
+
+    mvc.perform(
+            put("/api/order-tables/{orderTableId}/sit", orderTable.getId())
+                .accept(MediaType.APPLICATION_JSON))
+        .andDo(print())
+        .andExpect(status().isOk())
+        .andExpect(header().string(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+        .andExpect(jsonPath("$.id").exists())
+        .andExpect(jsonPath("$.name").value("1번"))
+        .andExpect(jsonPath("$.numberOfGuests").value(0))
+        .andExpect(jsonPath("$.occupied").value(true))
     ;
   }
 
