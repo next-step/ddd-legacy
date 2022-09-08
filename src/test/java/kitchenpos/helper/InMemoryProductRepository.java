9a1316b3c3ca6d441ca2872d2be85ef3f422318b
채윤병 <ybchae0801@naver.com>
2022-09-08 23:52:01 +0900

    test(Product): InMemoryProductRepository 구현

---
+++ src/test/java/kitchenpos/helper/InMemoryProductRepository.java
@@ -0,0 +1,40 @@
+package kitchenpos.helper;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import kitchenpos.domain.Product;
+import kitchenpos.domain.ProductRepository;
+
+public class InMemoryProductRepository implements ProductRepository {
+
+    private final Map<UUID, Product> products = new HashMap<>();
+
+    @Override
+    public List<Product> findAllByIdIn(List<UUID> productIds) {
+        return productIds.stream()
+            .map(products::get)
+            .collect(Collectors.toUnmodifiableList());
+    }
+
+    @Override
+    public Product save(Product product) {
+        products.put(product.getId(), product);
+        return product;
+    }
+
+    @Override
+    public Optional<Product> findById(UUID productId) {
+        return Optional.ofNullable(products.get(productId));
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return products.values()
+            .stream()
+            .collect(Collectors.toUnmodifiableList());
+    }
+}
