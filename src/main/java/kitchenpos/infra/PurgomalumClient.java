caf94bd23cb230b2ca6ea8903382a4059db06015
채윤병 <ybchae0801@naver.com>
2022-09-09 00:17:26 +0900

    test(ProfanityClient): InMemoryProfanityClient 구현

--- src/main/java/kitchenpos/infra/PurgomalumClient.java
+++ src/main/java/kitchenpos/infra/PurgomalumClient.java
@@ -1,20 +1,21 @@
 package kitchenpos.infra;
 
+import java.net.URI;
+import kitchenpos.domain.ProfanityClient;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
-
 @Component
-public class PurgomalumClient {
+public class PurgomalumClient implements ProfanityClient {
     private final RestTemplate restTemplate;
 
     public PurgomalumClient(final RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
     }
 
+    @Override
     public boolean containsProfanity(final String text) {
         final URI url = UriComponentsBuilder.fromUriString("https://www.purgomalum.com/service/containsprofanity")
             .queryParam("text", text)
