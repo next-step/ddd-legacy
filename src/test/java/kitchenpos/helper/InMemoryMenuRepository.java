1acff52991c60df292845309e56c33418dc000b9
채윤병 <ybchae0801@naver.com>
2022-09-09 00:07:17 +0900

    test(Menu): InMemoryMenuRepository 구현

---
+++ src/test/java/kitchenpos/helper/InMemoryMenuRepository.java
@@ -0,0 +1,58 @@
+package kitchenpos.helper;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuProduct;
+import kitchenpos.domain.MenuRepository;
+import kitchenpos.domain.Product;
+
+public class InMemoryMenuRepository implements MenuRepository {
+
+    private final Map<UUID, Menu> menus = new HashMap<>();
+
+    @Override
+    public Menu save(Menu menu) {
+        menus.put(menu.getId(), menu);
+        return menu;
+    }
+
+    @Override
+    public Optional<Menu> findById(UUID menuId) {
+        return Optional.ofNullable(menus.get(menuId));
+    }
+
+    @Override
+    public List<Menu> findAll() {
+        return menus.values()
+            .stream()
+            .collect(Collectors.toUnmodifiableList());
+    }
+
+    @Override
+    public List<Menu> findAllByIdIn(List<UUID> menuIds) {
+        return menuIds.stream()
+            .map(menus::get)
+            .collect(Collectors.toUnmodifiableList());
+    }
+
+    @Override
+    public List<Menu> findAllByProductId(UUID productId) {
+        return menus.values()
+            .stream()
+            .filter(menu -> containProduct(menu, productId))
+            .collect(Collectors.toUnmodifiableList());
+    }
+
+    private static boolean containProduct(Menu menu, UUID productId) {
+        return menu.getMenuProducts()
+            .stream()
+            .map(MenuProduct::getProduct)
+            .map(Product::getId)
+            .anyMatch(productId::equals);
+    }
+}
