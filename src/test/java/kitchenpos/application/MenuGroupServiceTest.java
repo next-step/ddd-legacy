5c71bb4168f3c1c6fde66ce05b42e536b3a49912
flamme <flamme1004@gmail.com>
2022-09-09 17:12:45 +0900

    refactor(menuGroup): 메뉴 그룹 테스트를 InMemory로 변경하라

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -2,35 +2,29 @@ package kitchenpos.application;
 
 import kitchenpos.domain.MenuGroup;
 import kitchenpos.domain.MenuGroupRepository;
+import kitchenpos.repository.InMemoryMenuGroupRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
 
+import static kitchenpos.fixture.MenuGroupFixture.menuGroup;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 
-@ExtendWith(MockitoExtension.class)
 class MenuGroupServiceTest {
 
-    @InjectMocks
-    private MenuGroupService menuGroupService;
-
-    @Mock
     private MenuGroupRepository menuGroupRepository;
+    private MenuGroupService menuGroupService;
 
     @BeforeEach
     void beforeEach() {
+        menuGroupRepository = new InMemoryMenuGroupRepository();
         menuGroupService = new MenuGroupService(menuGroupRepository);
     }
 
@@ -38,15 +32,22 @@ class MenuGroupServiceTest {
     @DisplayName("카테고리를 생성한다")
     void menuGroupCrate() {
         // given
-        MenuGroup menuGroup = new MenuGroup();
-        menuGroup.setName("일식");
-        given(menuGroupRepository.save(any())).willReturn(menuGroup);
+        MenuGroup actual = createRequest("일식");
 
         // when
-        MenuGroup result = menuGroupService.create(menuGroup);
+        MenuGroup expect = menuGroupService.create(actual);
 
         // then
-        assertThat(result).isEqualTo(menuGroup);
+        assertAll(
+                () -> assertThat(expect.getId()).isNotNull(),
+                () -> assertThat(actual.getName()).isEqualTo(expect.getName())
+        );
+    }
+
+    private static MenuGroup createRequest(String name) {
+        MenuGroup menuGroup = new MenuGroup();
+        menuGroup.setName(name);
+        return menuGroup;
     }
 
     @ParameterizedTest
@@ -54,12 +55,11 @@ class MenuGroupServiceTest {
     @DisplayName("카테고리 생성 시 이름이 비어있으면 생성할 수 없다")
     void menuGroupCreateNotName(String input) {
         // given
-        MenuGroup menuGroup = new MenuGroup();
-        menuGroup.setName(input);
+        MenuGroup actual = createRequest(input);
 
         // then
         assertThatIllegalArgumentException().isThrownBy(() ->
-                menuGroupService.create(menuGroup)
+                menuGroupService.create(actual)
         );
     }
 
@@ -67,19 +67,13 @@ class MenuGroupServiceTest {
     @DisplayName("카테고리를 조회한다")
     void findMenuGroup() {
         // given
-        MenuGroup menuGroup1 = new MenuGroup();
-        menuGroup1.setName("일식");
-        MenuGroup menuGroup2 = new MenuGroup();
-        menuGroup2.setName("중식");
-        given(menuGroupRepository.findAll()).willReturn(
-                List.of(menuGroup1, menuGroup2)
-        );
+        menuGroupRepository.save(menuGroup("일식"));
 
         // when
         List<MenuGroup> result = menuGroupService.findAll();
 
         // then
-        assertThat(result).containsExactly(menuGroup1, menuGroup2);
+        assertThat(result).hasSize(1);
     }
 
 }
---
+++ src/test/java/kitchenpos/repository/InMemoryMenuGroupRepository.java
@@ -0,0 +1,26 @@
+package kitchenpos.repository;
+
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuGroupRepository;
+
+import java.util.*;
+
+public class InMemoryMenuGroupRepository implements MenuGroupRepository {
+    private final Map<UUID, MenuGroup> menuGroups = new HashMap<>();
+
+    @Override
+    public MenuGroup save(final MenuGroup menuGroup) {
+        menuGroups.put(menuGroup.getId(), menuGroup);
+        return menuGroup;
+    }
+
+    @Override
+    public Optional<MenuGroup> findById(final UUID id) {
+        return Optional.ofNullable(menuGroups.get(id));
+    }
+
+    @Override
+    public List<MenuGroup> findAll() {
+        return new ArrayList<>(menuGroups.values());
+    }
+}
