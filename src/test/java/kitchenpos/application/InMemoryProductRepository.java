8f7dcf2134c9b6aa7e4e0ba144b5917286153c31
dominiqn <dmut7691@gmail.com>
2022-09-11 22:19:11 +0900

    보강

--- src/test/java/kitchenpos/application/InMemoryProductRepository.java
+++ src/test/java/kitchenpos/application/InMemoryProductRepository.java
@@ -19,6 +19,7 @@ public class InMemoryProductRepository extends InMemoryCrudRepository<Product, U
         return ids.stream()
                 .map(storage::get)
                 .filter(Objects::nonNull)
+                .distinct()
                 .collect(Collectors.toList());
     }
 }
