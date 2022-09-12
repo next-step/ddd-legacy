510060c4297b11ccb8e07d58e84d44b58f4db3cf
lehdqlsl <lehdqlsl@naver.com>
2022-09-12 21:07:32 +0900

    add(MenuGroup): Factory 및 Test 파일 생성.

---
+++ src/test/java/kitchenpos/MenuGroupServiceTest.java
@@ -0,0 +1,42 @@
+package kitchenpos;
+
+import kitchenpos.application.MenuGroupService;
+import kitchenpos.application.ProductService;
+import kitchenpos.domain.MenuGroupRepository;
+import kitchenpos.domain.MenuRepository;
+import kitchenpos.domain.Product;
+import kitchenpos.domain.ProductRepository;
+import kitchenpos.factory.ProductFactory;
+import kitchenpos.infra.ProfanityClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+@ExtendWith(MockitoExtension.class)
+public class MenuGroupServiceTest {
+
+    private MenuGroupRepository menuGroupRepository;
+
+    @InjectMocks
+    private MenuGroupService menuGroupService;
+
+    @BeforeEach
+    public void setUp() {
+        menuGroupRepository = new InMemoryMenuGroupRepository();
+        menuGroupService = new MenuGroupService(menuGroupRepository);
+    }
+
+
+}
---
+++ src/test/java/kitchenpos/factory/MenuGroupFactory.java
@@ -0,0 +1,5 @@
+package kitchenpos.factory;
+
+public class MenuGroupFactory {
+
+}
