4c24cc0a26dfd2075c132dbdbdf33bf032a2b19e
lehdqlsl <lehdqlsl@naver.com>
2022-09-12 18:35:18 +0900

    add(ProductFactory): 팩토리 구현

---
+++ src/test/java/kitchenpos/factory/ProductFactory.java
@@ -0,0 +1,30 @@
+package kitchenpos.factory;
+
+import kitchenpos.domain.Product;
+
+import java.math.BigDecimal;
+
+public class ProductFactory {
+
+    public static final String DEFAULT_NAME = "황금올리브";
+    public static final long DEFAULT_PRICE = 20000L;
+
+    public static Product getDefaultProduct() {
+        return ProductFactory.of(DEFAULT_NAME, BigDecimal.valueOf(DEFAULT_PRICE));
+    }
+
+    public static Product of(BigDecimal price) {
+        return ProductFactory.of(DEFAULT_NAME, price);
+    }
+
+    public static Product of(String name) {
+        return ProductFactory.of(name, BigDecimal.valueOf(DEFAULT_PRICE));
+    }
+
+    public static Product of(String name, BigDecimal price) {
+        final Product request = new Product();
+        request.setName(name);
+        request.setPrice(price);
+        return request;
+    }
+}
