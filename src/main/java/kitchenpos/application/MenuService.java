fc1866f81e39c1aeb2790a76276936f727a7d0d1
lehdqlsl <lehdqlsl@naver.com>
2022-09-18 19:04:37 +0900

    fix(MenuService): 메뉴 가격 수정 예외 로직 수정

--- src/main/java/kitchenpos/application/MenuService.java
+++ src/main/java/kitchenpos/application/MenuService.java
@@ -96,9 +96,8 @@ public class MenuService {
         for (final MenuProduct menuProduct : menu.getMenuProducts()) {
             sum = sum.add(
                     menuProduct.getProduct()
-                            .getPrice()
-                            .multiply(BigDecimal.valueOf(menuProduct.getQuantity())
-            ));
+                .getPrice()
+                .multiply(BigDecimal.valueOf(menuProduct.getQuantity())));
         }
         if (price.compareTo(sum) > 0) {
             throw new IllegalArgumentException();
@@ -111,13 +110,16 @@ public class MenuService {
     public Menu display(final UUID menuId) {
         final Menu menu = menuRepository.findById(menuId)
             .orElseThrow(NoSuchElementException::new);
+        BigDecimal sum = BigDecimal.ZERO;
         for (final MenuProduct menuProduct : menu.getMenuProducts()) {
-            final BigDecimal sum = menuProduct.getProduct()
+            sum = sum.add(
+                    menuProduct.getProduct()
                 .getPrice()
-                .multiply(BigDecimal.valueOf(menuProduct.getQuantity()));
-            if (menu.getPrice().compareTo(sum) > 0) {
-                throw new IllegalStateException();
-            }
+                .multiply(BigDecimal.valueOf(menuProduct.getQuantity()))
+            );
+        }
+        if (menu.getPrice().compareTo(sum) > 0) {
+            throw new IllegalStateException();
         }
         menu.setDisplayed(true);
         return menu;
