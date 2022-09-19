13c06a4dd630e01408c4f1a6a6238389217437fd
jeongjaeeom <jeongjae.eom@gmail.com>
2022-09-19 12:02:00 +0900

    feat($test): 상품리포지터리 가짜객체 구현
    상품리포지터리 가짜객체 구현
    - InMemoryProductRepository 가짜객체 구현

---
+++ src/test/java/kitchenpos/application/InMemoryProductRepository.java
@@ -0,0 +1,39 @@
+package kitchenpos.application;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import kitchenpos.domain.Product;
+import kitchenpos.domain.ProductRepository;
+
+public class InMemoryProductRepository implements ProductRepository {
+
+  private static Map<UUID, Product> store = new HashMap<>();
+
+  @Override
+  public Product save(Product product) {
+    store.put(product.getId(), product);
+    return product;
+  }
+
+  @Override
+  public Optional<Product> findById(UUID id) {
+    return Optional.ofNullable(store.get(id));
+  }
+
+  @Override
+  public List<Product> findAllByIdIn(List<UUID> ids) {
+    return ids.stream()
+        .map(it -> store.get(it))
+        .collect(Collectors.toList());
+  }
+
+  @Override
+  public List<Product> findAll() {
+    return new ArrayList<>(store.values());
+  }
+}
