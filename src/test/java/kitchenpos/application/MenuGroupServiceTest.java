2ee501ef2039c795cd0275b698349d972fcc0113
jeongjaeeom <jeongjae.eom@gmail.com>
2022-09-19 10:46:04 +0900

    feat($test): 메뉴그룹 테스트 Fixture 메서드 추가 및 의미 부여
    메뉴그룹 테스트 Fixture 메서드 추가 및 의미 부여
    - MenuGroup Fixture 메서드 추가 -> creationRequestMenuGroup()
    - @Nested를 사용한 테스트 코드 가독성 높이기

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -12,6 +12,7 @@ import kitchenpos.domain.MenuGroupRepository;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -19,6 +20,7 @@ import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+@DisplayName("메뉴그룹 서비스 테스트")
 @ExtendWith(MockitoExtension.class)
 class MenuGroupServiceTest {
 
@@ -32,56 +34,65 @@ class MenuGroupServiceTest {
     this.menuGroupService = new MenuGroupService(menuGroupRepository);
   }
 
-  @DisplayName("유효한 메뉴그룹명을 입력하면 메뉴그룹을 등록할 수 있다.")
-  @Test
-  void givenValidMenuGroup_whenCreate_thenMenuGroup() {
-    // given
-    MenuGroup menuGroup = new MenuGroup();
-    menuGroup.setId(UUID.randomUUID());
-    menuGroup.setName("추천메뉴");
+  @Nested
+  @DisplayName("메뉴그룹을 등록할 때")
+  class WhenCreate {
 
-    given(menuGroupRepository.save(any(MenuGroup.class))).willReturn(menuGroup);
+    @DisplayName("유효한 메뉴그룹명을 입력하면 메뉴그룹을 등록할 수 있다.")
+    @Test
+    void givenValidMenuGroup_whenCreate_thenMenuGroup() {
+      // given
+      MenuGroup menuGroup = creationRequestMenuGroup("추천메뉴");
+      given(menuGroupRepository.save(any(MenuGroup.class))).willReturn(menuGroup);
 
-    // when
-    MenuGroup createdMenuGroup = menuGroupService.create(menuGroup);
+      // when
+      MenuGroup createdMenuGroup = menuGroupService.create(menuGroup);
 
-    // then
-    Assertions.assertThat(createdMenuGroup.getName()).isEqualTo(menuGroup.getName());
+      // then
+      Assertions.assertThat(createdMenuGroup.getName()).isEqualTo("추천메뉴");
+    }
+
+    @DisplayName("메뉴그룹 이름은 비어있을 수 없다.")
+    @NullAndEmptySource
+    @ParameterizedTest(name = "{displayName}: [{index}] {argumentsWithNames}")
+    void givenNotValidPrice_whenCreate_thenIllegalArgumentException(String name) {
+      // given
+      MenuGroup menuGroup = creationRequestMenuGroup(name);
+
+      // when & then
+      assertThatIllegalArgumentException().isThrownBy(() -> menuGroupService.create(menuGroup));
+    }
   }
 
-  @DisplayName("메뉴그룹 이름은 비어있을 수 없다.")
-  @NullAndEmptySource
-  @ParameterizedTest(name = "{displayName}: [{index}] {argumentsWithNames}")
-  void givenNotValidPrice_whenCreate_thenIllegalArgumentException(String name) {
-    // given
-    MenuGroup menuGroup = new MenuGroup();
-    menuGroup.setId(UUID.randomUUID());
-    menuGroup.setName(name);
+  @Nested
+  @DisplayName("메뉴그룹을 조회 할 때")
+  class WhenFind {
 
-    // when & then
-    assertThatIllegalArgumentException().isThrownBy(() -> menuGroupService.create(menuGroup));
+    @DisplayName("등록된 메뉴그룹을 조회하면 등록된 메뉴그룹 목록을 반환한다.")
+    @Test
+    void givenMenuGroups_whenFindAll_thenReturnMenuGroups() {
+      // given
+      given(menuGroupRepository.findAll()).willReturn(
+          List.of(
+              creationRequestMenuGroup("추천메뉴"),
+              creationRequestMenuGroup("점심특선메뉴")
+          )
+      );
+
+      // when
+      List<MenuGroup> menuGroups = menuGroupService.findAll();
+
+      // then
+      assertThat(menuGroups).hasSize(2);
+      assertThat(menuGroups).extracting(MenuGroup::getName).contains("추천메뉴", "점심특선메뉴");
+    }
   }
 
-  @DisplayName("등록된 메뉴그룹을 조회하면 등록된 메뉴그룹 목록을 반환한다.")
-  @Test
-  void givenMenuGroups_whenFindAll_thenReturnMenuGroups() {
-    // given
+  private static MenuGroup creationRequestMenuGroup(String name) {
     MenuGroup menuGroup1 = new MenuGroup();
     menuGroup1.setId(UUID.randomUUID());
-    menuGroup1.setName("추천메뉴");
-
-    MenuGroup menuGroup2 = new MenuGroup();
-    menuGroup2.setId(UUID.randomUUID());
-    menuGroup2.setName("점심특선메뉴");
-
-    given(menuGroupRepository.findAll()).willReturn(List.of(menuGroup1, menuGroup2));
-
-    // when
-    List<MenuGroup> menuGroups = menuGroupService.findAll();
-
-    // then
-    assertThat(menuGroups).hasSize(2);
-    assertThat(menuGroups).extracting(MenuGroup::getName).contains("추천메뉴", "점심특선메뉴");
+    menuGroup1.setName(name);
+    return menuGroup1;
   }
 
 }
