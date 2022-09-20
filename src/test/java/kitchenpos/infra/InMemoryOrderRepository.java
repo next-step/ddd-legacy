f71b502dc0735a5f1b640a00966ae6bb0d950205
jeongjaeeom <jeongjae.eom@gmail.com>
2022-09-20 11:13:57 +0900

    feat($test): 가짜객체 주문 저장소 구현
    가짜객체 주문 저장소 구현
    - InMemoryOrderRepository 구현

---
+++ src/test/java/kitchenpos/infra/InMemoryOrderRepository.java
@@ -0,0 +1,38 @@
+package kitchenpos.infra;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UUID;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderRepository;
+import kitchenpos.domain.OrderStatus;
+import kitchenpos.domain.OrderTable;
+
+public class InMemoryOrderRepository implements OrderRepository {
+
+  private final Map<UUID, Order> store = new HashMap<>();
+
+  @Override
+  public boolean existsByOrderTableAndStatusNot(OrderTable orderTable, OrderStatus status) {
+    return false;
+  }
+
+  @Override
+  public Order save(Order order) {
+    store.put(order.getId(), order);
+    return order;
+  }
+
+  @Override
+  public Optional<Order> findById(UUID orderId) {
+    return Optional.ofNullable(store.get(orderId));
+  }
+
+  @Override
+  public List<Order> findAll() {
+    return new ArrayList<>(store.values());
+  }
+}
