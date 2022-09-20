6b20033d75f30510bd86f330b79cc08b929dd9fa
jeongjaeeom <jeongjae.eom@gmail.com>
2022-09-20 11:15:51 +0900

    feat($compile): 주문 테이블 저장소 인터페이스 추출
    주문 테이블 저장소 인터페이스 추출 및 기존 리포지터리명 변경
    - OrderTableRepository 추출
    - 기존 OrderTableRepository -> JpaOrderTableRepository 로 변경

---
+++ src/main/java/kitchenpos/domain/JpaOrderTableRepository.java
@@ -0,0 +1,7 @@
+package kitchenpos.domain;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaOrderTableRepository extends OrderTableRepository, JpaRepository<OrderTable, UUID> {
+}
