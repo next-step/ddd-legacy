6770c5ed07b97fdc9a54654bf71b41302a50c45c
lehdqlsl <lehdqlsl@naver.com>
2022-09-26 23:01:49 +0900

    test(OrderTableService): 주문 테이블 테스트 코드 작성 완료

---
+++ src/test/java/kitchenpos/OrderTableServiceTest.java
@@ -0,0 +1,154 @@
+package kitchenpos;
+
+import kitchenpos.application.OrderTableService;
+import kitchenpos.application.ProductService;
+import kitchenpos.domain.*;
+import kitchenpos.factory.OrderLineItemFactory;
+import kitchenpos.factory.OrderTableFactory;
+import kitchenpos.factory.ProductFactory;
+import kitchenpos.fakeobject.*;
+import kitchenpos.infra.ProfanityClient;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+@ExtendWith(MockitoExtension.class)
+public class OrderTableServiceTest {
+
+    private OrderTableRepository orderTableRepository;
+    private OrderRepository orderRepository;
+
+    @InjectMocks
+    private OrderTableService orderTableService;
+
+    @BeforeEach
+    public void setUp() {
+        orderTableRepository = new InMemoryOrderTableRepository();
+        orderRepository = new InMemoryOrderRepository();
+        orderTableService = new OrderTableService(orderTableRepository, orderRepository);
+    }
+
+    @DisplayName("주문 테이블을 생성할 수 있다.")
+    @Test
+    public void create() {
+        final OrderTable request = OrderTableFactory.of();
+
+        final OrderTable orderTable = orderTableService.create(request);
+
+        assertThat(orderTable.getId()).isNotNull();
+    }
+
+    @DisplayName("주문 테이블 생성 시, 이름은 필수로 입력되어야 한다.")
+    @Test
+    public void create_input_name() {
+        final OrderTable request = new OrderTable();
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> orderTableService.create(request));
+    }
+
+    @DisplayName("손님은 테이블에 앉을 수 있다.")
+    @Test
+    public void sit() {
+        final OrderTable orderTable = OrderTableFactory.of();
+        final OrderTable request = orderTableRepository.save(orderTable);
+
+        final OrderTable actual = orderTableService.sit(request.getId());
+
+        assertThat(actual.isOccupied()).isTrue();
+    }
+
+    @DisplayName("테이블을 청소한다. (정보를 초기화 한다)")
+    @Test
+    public void clear() {
+        final OrderTable orderTable = OrderTableFactory.of();
+        final OrderTable request = orderTableRepository.save(orderTable);
+
+        final Order order = new Order();
+        order.setType(OrderType.EAT_IN);
+        order.setStatus(OrderStatus.COMPLETED);
+        order.setOrderTable(orderTable);
+        orderRepository.save(order);
+
+        final OrderTable actual = orderTableService.clear(request.getId());
+
+        assertThat(actual.isOccupied()).isFalse();
+        assertThat(actual.getNumberOfGuests()).isZero();
+    }
+
+    @DisplayName("테이블을 청소 시, 완료되지 않는 주문이 없어야 한다.")
+    @Test
+    public void clear_order_status_is_completed() {
+        final OrderTable orderTable = OrderTableFactory.of();
+        final OrderTable request = orderTableRepository.save(orderTable);
+
+        final Order order = new Order();
+        order.setType(OrderType.EAT_IN);
+        order.setStatus(OrderStatus.SERVED);
+        order.setOrderTable(orderTable);
+        orderRepository.save(order);
+
+        assertThatExceptionOfType(IllegalStateException.class)
+                .isThrownBy(() -> orderTableService.clear(request.getId()));
+    }
+
+    @DisplayName("주문 테이블의 손님 숫자를 수정한다.")
+    @Test
+    public void changeNumberOfGuests() {
+        final OrderTable orderTable = OrderTableFactory.of(true);
+
+        final OrderTable request = orderTableRepository.save(orderTable);
+        request.setNumberOfGuests(2);
+
+        OrderTable actual = orderTableService.changeNumberOfGuests(request.getId(), request);
+        assertThat(actual.getNumberOfGuests()).isEqualTo(2);
+    }
+
+
+    @DisplayName("손님 수 변경 시, 음수 일수 없다.")
+    @Test
+    public void changeNumberOfGuests_negative() {
+        final OrderTable orderTable = OrderTableFactory.of();
+
+        final OrderTable request = orderTableRepository.save(orderTable);
+        request.setNumberOfGuests(-1);
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> orderTableService.changeNumberOfGuests(request.getId(), request));
+    }
+
+    @DisplayName("손님 수 변경 시, 사용중인 테이블이 아닐경우 변경할 수 없다")
+    @Test
+    public void changeNumberOfGuests_occupied() {
+        final OrderTable orderTable = OrderTableFactory.of(false);
+
+        final OrderTable request = orderTableRepository.save(orderTable);
+        request.setNumberOfGuests(2);
+
+        assertThatExceptionOfType(IllegalStateException.class)
+                .isThrownBy(() -> orderTableService.changeNumberOfGuests(request.getId(), request));
+    }
+
+    @DisplayName("주문 테이블을 조회할 수 있다.")
+    @Test
+    public void findAll() {
+        final OrderTable orderTable = OrderTableFactory.of();
+        orderTableRepository.save(orderTable);
+
+        List<OrderTable> actual = orderTableService.findAll();
+
+        assertThat(actual).hasSize(1);
+    }
+}
--- src/test/java/kitchenpos/fakeobject/InMemoryOrderRepository.java
+++ src/test/java/kitchenpos/fakeobject/InMemoryOrderRepository.java
@@ -5,12 +5,15 @@ import kitchenpos.domain.OrderRepository;
 import kitchenpos.domain.OrderStatus;
 import kitchenpos.domain.OrderTable;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 public class InMemoryOrderRepository extends AbstractInMemoryRepository<UUID, Order> implements OrderRepository {
 
     @Override
     public boolean existsByOrderTableAndStatusNot(OrderTable orderTable, OrderStatus status) {
-        return false;
+        return new ArrayList<>(maps.values()).stream()
+                .filter(order -> orderTable.getId().equals(orderTable.getId()))
+                .anyMatch(order -> order.getStatus() != status);
     }
 }
