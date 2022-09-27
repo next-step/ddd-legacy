7b350976b2258e76ee6dc2e6a6a1ab24ae15167f
JunGu Kang <jungu.kang@riiid.co>
2022-09-27 23:15:37 +0900

    test: FakeProfanityDetectClient 추가

---
+++ src/test/java/kitchenpos/infra/FakeProfanityDetectClient.java
@@ -0,0 +1,16 @@
+package kitchenpos.infra;
+
+import java.util.Arrays;
+
+class FakeProfanityDetectClient implements ProfanityDetectClient {
+
+    public static final String[] PROFANITY_WORDS = {
+            "holiday", "bed", "anxious", "everyday", "reach",
+    };
+
+    @Override
+    public boolean containsProfanity(String text) {
+        return Arrays.stream(PROFANITY_WORDS)
+                .anyMatch(text::contains);
+    }
+}
