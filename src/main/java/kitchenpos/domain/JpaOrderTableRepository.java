ab0d65f52a2e6bb1869ce3aefe5b56541cad5a1a
JunGu Kang <jungu.kang@riiid.co>
2022-09-27 10:31:04 +0900

    refactor: OrderTableRepository 분리

---
+++ src/main/java/kitchenpos/domain/JpaOrderTableRepository.java
@@ -0,0 +1,10 @@
+package kitchenpos.domain;
+
+import java.util.UUID;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaOrderTableRepository extends
+        OrderTableRepository,
+        JpaRepository<OrderTable, UUID> {
+
+}
