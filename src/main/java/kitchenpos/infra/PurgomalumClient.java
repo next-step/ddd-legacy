9b97c2d78610a453906cddb8dbe604175a667c25
JunGu Kang <jungu.kang@riiid.co>
2022-09-27 22:59:15 +0900

    refactor: ProfanityDetectClient 분리

--- src/main/java/kitchenpos/infra/PurgomalumClient.java
+++ src/main/java/kitchenpos/infra/PurgomalumClient.java
@@ -1,14 +1,16 @@
 package kitchenpos.infra;
 
+import java.net.URI;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.net.URI;
-
 @Component
-public class PurgomalumClient {
+public class PurgomalumClient implements ProfanityDetectClient {
+
+    private static final String ENDPOINT = "https://www.purgomalum.com/service/containsprofanity";
+
     private final RestTemplate restTemplate;
 
     public PurgomalumClient(final RestTemplateBuilder restTemplateBuilder) {
@@ -16,10 +18,10 @@ public class PurgomalumClient {
     }
 
     public boolean containsProfanity(final String text) {
-        final URI url = UriComponentsBuilder.fromUriString("https://www.purgomalum.com/service/containsprofanity")
-            .queryParam("text", text)
-            .build()
-            .toUri();
+        final URI url = UriComponentsBuilder.fromUriString(ENDPOINT)
+                .queryParam("text", text)
+                .build()
+                .toUri();
         return Boolean.parseBoolean(restTemplate.getForObject(url, String.class));
     }
 }
