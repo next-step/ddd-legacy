fb79146d5d1fd3d624620a375e3c94ba38a8137b
이정수 <lsecret@naver.com>
2022-10-16 17:01:27 +0900

    test: 주문 테이블 서비스 테스트 코드 추가

--- src/main/java/kitchenpos/domain/OrderTableJpaRepository.java
+++ src/main/java/kitchenpos/domain/OrderTableJpaRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface MenuGroupJpaRepository extends JpaRepository<MenuGroup, UUID>, MenuGroupRepository {
+public interface OrderTableJpaRepository extends JpaRepository<OrderTable, UUID>, OrderTableRepository {
 }
