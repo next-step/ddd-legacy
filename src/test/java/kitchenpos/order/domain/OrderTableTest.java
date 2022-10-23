e7009f833894aa757034703821968e145521abe8
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-10-23 12:57:38 +0900

    test(OrderTableTest.java) : 주문 테이블 생성 시 주문 테이블명은 필수이다.

--- src/test/java/kitchenpos/order/domain/OrderTableTest.java
+++ src/test/java/kitchenpos/order/domain/OrderTableTest.java
@@ -4,8 +4,11 @@ import kitchenpos.domain.Name;
 import kitchenpos.ordertable.OrderTable;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("주문 테이블")
 class OrderTableTest {
@@ -14,5 +17,14 @@ class OrderTableTest {
     @Test
     void createOrderTable() {
         assertThatNoException().isThrownBy(() -> new OrderTable(new Name("주문테이블명", false)));
+    }
+
+    @DisplayName("주문 테이블 생성 시 주문 테이블명은 필수이다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void createOrderTable(String name) {
+        assertThatThrownBy(() -> new OrderTable(new Name(name, false)))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("null 이나 공백일 수 없습니다.");
     }
 }
