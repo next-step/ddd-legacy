9f3e7c7e92c2c9f5d363ae927a9a59c24b50a4ed
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-10-30 08:54:35 +0900

    test(OrderTableTest.java) : 주문 테이블 파라미터 리팩토링

--- src/test/java/kitchenpos/ordertable/domain/OrderTableTest.java
+++ src/test/java/kitchenpos/ordertable/domain/OrderTableTest.java
@@ -8,10 +8,9 @@ import kitchenpos.order.domain.OrderType;
 import kitchenpos.order.vo.DeliveryAddress;
 import kitchenpos.ordertable.vo.NumberOfGuests;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -24,9 +23,10 @@ import static org.assertj.core.api.Assertions.*;
 class OrderTableTest {
 
     @DisplayName("주문 테이블 생성 시 주문 테이블명을 입력받는다.")
-    @Test
-    void createOrderTable() {
-        assertThatNoException().isThrownBy(() -> orderTable("주문테이블명", 1));
+    @ParameterizedTest
+    @CsvSource({"주문테이블명, 1"})
+    void createOrderTable(String name, int numberOfGuests) {
+        assertThatNoException().isThrownBy(() -> orderTable(name, numberOfGuests));
     }
 
     @DisplayName("주문 테이블 생성 시 주문 테이블명은 필수이다.")
@@ -40,9 +40,9 @@ class OrderTableTest {
 
     @DisplayName("주문 테이블의 착석여부를 착석으로 변경할 수 있다.")
     @ParameterizedTest
-    @ValueSource(strings = {"테이블"})
-    void change(String name) {
-        OrderTable orderTable = orderTable(name, 1);
+    @CsvSource({"주문테이블명, 1"})
+    void change(String name, int numberOfGuests) {
+        OrderTable orderTable = orderTable(name, numberOfGuests);
         assertThat(orderTable.isOccupied()).isFalse();
         orderTable.occupied();
         assertThat(orderTable.isOccupied()).isTrue();
@@ -50,9 +50,9 @@ class OrderTableTest {
 
     @DisplayName("주문 테이블의 착석여부를 공석으로 변경할 수 있다.")
     @ParameterizedTest
-    @ValueSource(strings = {"테이블"})
-    void vacant(String name) {
-        OrderTable orderTable = orderTable(name, 1);
+    @CsvSource({"주문테이블명, 1"})
+    void vacant(String name, int numberOfGuests) {
+        OrderTable orderTable = orderTable(name, numberOfGuests);
         assertThat(orderTable.isOccupied()).isFalse();
         orderTable.occupied();
         assertThat(orderTable.isOccupied()).isTrue();
@@ -61,49 +61,56 @@ class OrderTableTest {
     }
 
     @DisplayName("주문 테이블의 착석 인원 변경 시 0명보다 작을 수 없다.")
-    @Test
-    void validateGuestsNumber() {
-        OrderTable orderTable = orderTable("주문테이블명", 1);
+    @ParameterizedTest
+    @CsvSource({"주문테이블명, 1, -1"})
+    void validateGuestsNumber(String name, int numberOfGuests, int changeNumberOfGuests) {
+        OrderTable orderTable = orderTable(name, numberOfGuests);
         orderTable.occupied();
         assertThat(orderTable.isOccupied()).isTrue();
-        assertThatThrownBy(() -> orderTable.changeNumberOfGuests(new NumberOfGuests(-1)))
+        assertThatThrownBy(() -> orderTable.changeNumberOfGuests(new NumberOfGuests(changeNumberOfGuests)))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("주문 테이블의 착석 인원 변경 시 0명보다 작을 수 없다.");
     }
 
     @DisplayName("주문 테이블이 공석일 경우 착석 인원을 변경 할 수 없다.")
-    @Test
-    void validateOccupied() {
-        OrderTable orderTable = orderTable("주문테이블명", 1);
+    @ParameterizedTest
+    @CsvSource({"주문테이블명, 1, 1"})
+    void validateOccupied(String name, int numberOfGuests, int changeNumberOfGuests) {
+        OrderTable orderTable = orderTable(name, numberOfGuests);
         assertThat(orderTable.isOccupied()).isFalse();
-        assertThatThrownBy(() -> orderTable.changeNumberOfGuests(new NumberOfGuests(1)))
+        assertThatThrownBy(() -> orderTable.changeNumberOfGuests(new NumberOfGuests(changeNumberOfGuests)))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("주문 테이블이 공석일 경우 착석 인원을 변경 할 수 없다.");
     }
 
     @DisplayName("주문 테이블을 생성할 수 있다.")
-    @Test
-    void createProduct() {
-        assertThatNoException().isThrownBy(() -> orderTable("주문테이블명", 1));
+    @ParameterizedTest
+    @CsvSource({"주문테이블명, 1"})
+    void createProduct(String name, int numberOfGuests) {
+        assertThatNoException().isThrownBy(() -> orderTable(name, numberOfGuests));
     }
 
     @DisplayName("주문 테이블의 착석 인원을 변경 할 수 있다.")
-    @Test
-    void changeNumberOfGuests() {
-        OrderTable orderTable = orderTable("주문테이블명", 1);
+    @ParameterizedTest
+    @CsvSource({"주문테이블명, 1, 10"})
+    void changeNumberOfGuests(String name, int numberOfGuests, int changeNumberOfGuests) {
+        OrderTable orderTable = orderTable(name, numberOfGuests);
         orderTable.occupied();
-        orderTable.changeNumberOfGuests(new NumberOfGuests(10));
-        assertThat(orderTable.getNumberOfGuests()).isEqualTo(10);
+        orderTable.changeNumberOfGuests(new NumberOfGuests(changeNumberOfGuests));
+        assertThat(orderTable.getNumberOfGuests()).isEqualTo(changeNumberOfGuests);
     }
 
     @DisplayName("주문 테이블 공석으로 변경 시 주문 상태가 완료일때만 변경 가능하다.")
-    @Test
-    void vacant_status() {
-        List<OrderLineItem> orderLineItems = orderLineItems();
-        Order order = new Order(UUID.randomUUID(), OrderType.DELIVERY, orderLineItems, new OrderTable(UUID.randomUUID(), new Name("테이블명", false), new NumberOfGuests(1)), new DeliveryAddress("주소"), LocalDateTime.now(), OrderStatus.WAITING);
-        assertThatThrownBy(() -> order.vacant())
+    @ParameterizedTest
+    @CsvSource({"주문테이블명, 1, 10"})
+    void vacant_status(String name, int numberOfGuests) {
+        assertThatThrownBy(() -> order(name, numberOfGuests, orderLineItems()).vacant())
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("주문 테이블 공석으로 변경 시 주문 상태가 완료일때만 변경 가능하다.");
+    }
+
+    private static Order order(String name, int numberOfGuests, List<OrderLineItem> orderLineItems) {
+        return new Order(UUID.randomUUID(), OrderType.DELIVERY, orderLineItems, orderTable(name, numberOfGuests), new DeliveryAddress("주소"), LocalDateTime.now(), OrderStatus.WAITING);
     }
 
     private static OrderTable orderTable(String name, int numberOfGuests) {
