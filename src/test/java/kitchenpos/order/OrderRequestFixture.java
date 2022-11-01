5095638ddaf2010fc77b50dd1bffc00bb2110b3f
chosundeveloper <67059660+chosundeveloper@users.noreply.github.com>
2022-11-01 12:47:49 +0900

    test(OrderRequestFixture.java) : 주문 request 리팩토링

--- src/test/java/kitchenpos/order/OrderRequestFixture.java
+++ src/test/java/kitchenpos/order/OrderRequestFixture.java
@@ -1,14 +1,18 @@
 package kitchenpos.order;
 
 import kitchenpos.menu.menu.domain.Menu;
+import kitchenpos.order.domain.Order;
+import kitchenpos.order.domain.OrderLineItem;
 import kitchenpos.order.domain.OrderType;
 import kitchenpos.order.dto.request.OrderLineItemRequest;
 import kitchenpos.order.dto.request.OrderRequest;
+import kitchenpos.order.vo.DeliveryAddress;
 import kitchenpos.ordertable.domain.OrderTable;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 public class OrderRequestFixture {
 
@@ -21,13 +25,6 @@ public class OrderRequestFixture {
 
     public static OrderRequest 주문항목_비어있음(OrderTable orderTable) {
         return new OrderRequest(null, OrderType.TAKEOUT, "주소", orderTable.getId());
-    }
-
-    public static OrderRequest 메뉴수량_주문항목수량_같음(Menu menu, OrderTable orderTable) {
-        final List<OrderLineItemRequest> orderLineItemRequests = new ArrayList<>();
-        OrderLineItemRequest orderLineItemRequest = new OrderLineItemRequest(menu.getId(), BigDecimal.TEN, 1);
-        orderLineItemRequests.add(orderLineItemRequest);
-        return new OrderRequest(orderLineItemRequests, OrderType.TAKEOUT, "주소", orderTable.getId());
     }
 
     public static OrderRequest 메뉴수량_주문항목수량_다름(Menu menu, OrderTable orderTable) {
@@ -71,6 +68,14 @@ public class OrderRequestFixture {
         OrderLineItemRequest orderLineItemRequest = new OrderLineItemRequest(menu.getId(), BigDecimal.TEN, 1);
         orderLineItemRequests.add(orderLineItemRequest);
         return new OrderRequest(orderLineItemRequests, null, null, orderTable.getId());
+    }
+
+    public static Order 배달주문생성(List<OrderLineItem> orderLineItems) {
+        return new Order(UUID.randomUUID(), OrderType.DELIVERY, orderLineItems, null, new DeliveryAddress("배송지"));
+    }
+
+    public static Order 매장주문생성(List<OrderLineItem> orderLineItems) {
+        return new Order(UUID.randomUUID(), OrderType.TAKEOUT, orderLineItems, null, null);
     }
 }
 
--- src/test/java/kitchenpos/order/application/OrderStatusServiceTest.java
+++ src/test/java/kitchenpos/order/application/OrderStatusServiceTest.java
@@ -1,15 +1,16 @@
 package kitchenpos.order.application;
 
-import kitchenpos.common.vo.Name;
-import kitchenpos.common.vo.Price;
 import kitchenpos.common.vo.Quantity;
+import kitchenpos.menu.menu.MenuFixture;
 import kitchenpos.menu.menu.domain.Menu;
-import kitchenpos.menu.menu.domain.MenuProduct;
 import kitchenpos.menu.menu.domain.MenuRepository;
+import kitchenpos.menu.menugroup.MenuGroupFixture;
 import kitchenpos.menu.menugroup.domain.MenuGroup;
 import kitchenpos.menu.menugroup.domain.MenuGroupRepository;
-import kitchenpos.order.domain.*;
-import kitchenpos.order.vo.DeliveryAddress;
+import kitchenpos.order.domain.Order;
+import kitchenpos.order.domain.OrderLineItem;
+import kitchenpos.order.domain.OrderRepository;
+import kitchenpos.order.domain.OrderStatus;
 import kitchenpos.product.domain.Product;
 import kitchenpos.product.domain.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -20,12 +21,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
+import static kitchenpos.order.OrderRequestFixture.매장주문생성;
+import static kitchenpos.order.OrderRequestFixture.배달주문생성;
+import static kitchenpos.product.ProductFixture.product;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -49,26 +51,26 @@ class OrderStatusServiceTest {
     @Autowired
     private MenuRepository menuRepository;
 
-    private Order 매장주문;
-    private Order 배달주문;
+    private Order eatInOrder;
+    private Order deliveryOrder;
 
     @BeforeEach
     void setUp() {
-        MenuGroup menuGroup = menuGroupRepository.save(menuGroup(UUID.randomUUID(), "메뉴 그룹명"));
-        Product product = productRepository.save(product());
-        Menu menu = menuRepository.save(menu(product, menuGroup));
+        MenuGroup menuGroup = menuGroupRepository.save(MenuGroupFixture.menuGroup(UUID.randomUUID()));
+        Product product = productRepository.save(product(UUID.randomUUID(), BigDecimal.ONE));
+        Menu menu = menuRepository.save(MenuFixture.menu(menuGroup, MenuFixture.menuProducts(product.getId())));
         menu.display();
         List<OrderLineItem> orderLineItems = new ArrayList<>();
         orderLineItems.add(new OrderLineItem(menu, new Quantity(1)));
-        매장주문 = orderRepository.save(매장주문생성(orderLineItems));
-        배달주문 = orderRepository.save(배달주문생성(orderLineItems));
+        eatInOrder = orderRepository.save(매장주문생성(orderLineItems));
+        deliveryOrder = orderRepository.save(배달주문생성(orderLineItems));
     }
 
     @DisplayName("WAITING 상태가 아니면 접수를 받을 수 없다.")
     @Test
     void name() {
-        orderStatusService.accept(매장주문.getId());
-        assertThatThrownBy(() -> orderStatusService.accept(매장주문.getId()))
+        orderStatusService.accept(eatInOrder.getId());
+        assertThatThrownBy(() -> orderStatusService.accept(eatInOrder.getId()))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("WAITING 상태만 접수가능합니다.");
     }
@@ -76,14 +78,14 @@ class OrderStatusServiceTest {
     @DisplayName("WAITING 상태가 아니면 접수를 받을 수 없다.")
     @Test
     void accept() {
-        orderStatusService.accept(매장주문.getId());
-        assertThat(매장주문.getStatus()).isEqualTo(OrderStatus.ACCEPTED);
+        orderStatusService.accept(eatInOrder.getId());
+        assertThat(eatInOrder.getStatus()).isEqualTo(OrderStatus.ACCEPTED);
     }
 
     @DisplayName("접수 상태가 아니면 제공할 수 없다.")
     @Test
     void served() {
-        assertThatThrownBy(() -> orderStatusService.serve(매장주문.getId()))
+        assertThatThrownBy(() -> orderStatusService.serve(eatInOrder.getId()))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("ACCEPTED 상태만 SERVED 상태로 변경가능합니다");
     }
@@ -91,18 +93,18 @@ class OrderStatusServiceTest {
     @DisplayName("배송을 시작할 수 있다.")
     @Test
     void startDelivery() {
-        orderStatusService.accept(배달주문.getId());
-        orderStatusService.serve(배달주문.getId());
-        orderStatusService.startDelivery(배달주문.getId());
-        assertThat(배달주문.getStatus()).isEqualTo(OrderStatus.DELIVERING);
+        orderStatusService.accept(deliveryOrder.getId());
+        orderStatusService.serve(deliveryOrder.getId());
+        orderStatusService.startDelivery(deliveryOrder.getId());
+        assertThat(deliveryOrder.getStatus()).isEqualTo(OrderStatus.DELIVERING);
     }
 
     @DisplayName("주문 타입이 DELIVERY일 경우에만 배송 시작을 할 수 있다.")
     @Test
     void asdf() {
-        orderStatusService.accept(매장주문.getId());
-        orderStatusService.serve(매장주문.getId());
-        assertThatThrownBy(() -> orderStatusService.startDelivery(매장주문.getId()))
+        orderStatusService.accept(eatInOrder.getId());
+        orderStatusService.serve(eatInOrder.getId());
+        assertThatThrownBy(() -> orderStatusService.startDelivery(eatInOrder.getId()))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("주문 타입이 DELIVERY일 경우에만 배송 시작을 할 수 있습니다.");
     }
@@ -110,8 +112,8 @@ class OrderStatusServiceTest {
     @DisplayName("주문 상태가 SERVED일 경우에만 배송 시작을 할 수 있다.")
     @Test
     void sdasdf() {
-        orderStatusService.accept(배달주문.getId());
-        assertThatThrownBy(() -> orderStatusService.startDelivery(배달주문.getId()))
+        orderStatusService.accept(deliveryOrder.getId());
+        assertThatThrownBy(() -> orderStatusService.startDelivery(deliveryOrder.getId()))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("주문 상태가 SERVED일 경우에만 배송 시작을 할 수 있다.");
     }
@@ -119,19 +121,19 @@ class OrderStatusServiceTest {
     @DisplayName("배송을 완료할 수 있다.")
     @Test
     void sdasdasdff() {
-        orderStatusService.accept(배달주문.getId());
-        orderStatusService.serve(배달주문.getId());
-        orderStatusService.startDelivery(배달주문.getId());
-        orderStatusService.completeDelivery(배달주문.getId());
-        assertThat(배달주문.getStatus()).isEqualTo(OrderStatus.DELIVERED);
+        orderStatusService.accept(deliveryOrder.getId());
+        orderStatusService.serve(deliveryOrder.getId());
+        orderStatusService.startDelivery(deliveryOrder.getId());
+        orderStatusService.completeDelivery(deliveryOrder.getId());
+        assertThat(deliveryOrder.getStatus()).isEqualTo(OrderStatus.DELIVERED);
     }
 
     @DisplayName("주문 상태가 DELIVERING일 경우에만 배송을 완료할 수 있다.")
     @Test
     void sdasdasdffasf() {
-        orderStatusService.accept(배달주문.getId());
-        orderStatusService.serve(배달주문.getId());
-        assertThatThrownBy(() -> orderStatusService.completeDelivery(배달주문.getId()))
+        orderStatusService.accept(deliveryOrder.getId());
+        orderStatusService.serve(deliveryOrder.getId());
+        assertThatThrownBy(() -> orderStatusService.completeDelivery(deliveryOrder.getId()))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("주문 상태가 DELIVERING일 경우에만 배송을 완료할 수 있다.");
     }
@@ -139,17 +141,17 @@ class OrderStatusServiceTest {
     @DisplayName("주문을 완료할 수 있다.")
     @Test
     void asdasdsdasdasdffasf() {
-        orderStatusService.accept(매장주문.getId());
-        orderStatusService.serve(매장주문.getId());
-        orderStatusService.complete(매장주문.getId());
-        assertThat(매장주문.getStatus()).isEqualTo(OrderStatus.COMPLETED);
+        orderStatusService.accept(eatInOrder.getId());
+        orderStatusService.serve(eatInOrder.getId());
+        orderStatusService.complete(eatInOrder.getId());
+        assertThat(eatInOrder.getStatus()).isEqualTo(OrderStatus.COMPLETED);
     }
 
     @DisplayName("주문 타입이 DELIVERY이고 주문 상태가 DELIVERED가 아니면 주문을 완료할 수 없다.")
     @Test
     void asdasdsdasdasdfasdfasf() {
-        orderStatusService.accept(배달주문.getId());
-        assertThatThrownBy(() -> orderStatusService.complete(배달주문.getId()))
+        orderStatusService.accept(deliveryOrder.getId());
+        assertThatThrownBy(() -> orderStatusService.complete(deliveryOrder.getId()))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("주문 상태가 DELIVERED가 아니면 주문을 완료할 수 없다.");
     }
@@ -157,34 +159,9 @@ class OrderStatusServiceTest {
     @DisplayName("주문 타입이 TAKEOUT 또는 EAT_IN이고 주문상태가 SERVED아니면 주문을 완료할 수 없다.")
     @Test
     void asdasdsdasdasf() {
-        orderStatusService.accept(매장주문.getId());
-        assertThatThrownBy(() -> orderStatusService.complete(매장주문.getId()))
+        orderStatusService.accept(eatInOrder.getId());
+        assertThatThrownBy(() -> orderStatusService.complete(eatInOrder.getId()))
                 .isInstanceOf(IllegalStateException.class)
                 .hasMessageContaining("주문 상태가 SERVED가 아니면 주문을 완료할 수 없다.");
     }
-
-    private Menu menu(Product product, MenuGroup menuGroup) {
-        return new Menu(UUID.randomUUID(), new Name("메뉴명", false), menuGroup, menuProducts(new MenuProduct(product, new Quantity(1))), new Price(BigDecimal.TEN));
-    }
-
-    private Product product() {
-        return new Product(UUID.randomUUID(), new Name("productName", false), new Price(BigDecimal.TEN));
-    }
-
-    private MenuGroup menuGroup(UUID id, String name) {
-        return new MenuGroup(id, new Name(name, false));
-    }
-
-    private List<MenuProduct> menuProducts(final MenuProduct... menuProducts) {
-        return Arrays.asList(menuProducts);
-    }
-
-    public Order 배달주문생성(List<OrderLineItem> orderLineItems) {
-        return new Order(UUID.randomUUID(), OrderType.DELIVERY, orderLineItems, null, new DeliveryAddress("배송지"));
-    }
-
-    public Order 매장주문생성(List<OrderLineItem> orderLineItems) {
-        return new Order(UUID.randomUUID(), OrderType.TAKEOUT, orderLineItems, null, null);
-    }
-
 }
