9d05c1f230f09cd00f37447bed0baaa169fb10ef
jehyeong <5909264@kbfg.com>
2023-08-16 08:30:57 +0900

    feat : add 기능 추가(input text null 처리 로직)

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,15 @@
+package calculator;
+
+public class StringCalculator {
+    private static final int ZERO_OF_STRING_CALCULATOR = 0;
+
+    public StringCalculator() {
+    }
+
+    public  int add(String text){
+        if(text == null || text.isEmpty()){
+            return ZERO_OF_STRING_CALCULATOR;
+        }
+        return ZERO_OF_STRING_CALCULATOR;
+    }
+}
