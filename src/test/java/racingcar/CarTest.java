98a10fdf8e434135149ed47a391c2efc5a70061b
hiblue02 <hiblue02@gmail.com>
2023-08-15 21:49:44 +0900

    test(racingcar): 자동차 이름 테스트 수정
    assertDoesNotThrow -> assertThatNoException

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -7,9 +7,7 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.assertj.core.api.Assertions.*;
 
 class CarTest {
 
@@ -20,7 +18,7 @@ class CarTest {
         @ParameterizedTest
         @ValueSource(strings = {"55555", "66666", "가나라다마사아자차카"})
         void carNameLongerThanFive(String input) {
-            assertDoesNotThrow(() -> new Car(input));
+            assertThatNoException().isThrownBy(() -> new Car(input));
         }
 
         @DisplayName("[실패] 자동차 이름이 5글자 미만이면 안된다.")
@@ -45,18 +43,18 @@ class CarTest {
     class Move {
         @DisplayName("[성공] 전략이 성공하면 자동차가 이동한다.")
         @Test
-        void carMove(){
+        void carMove() {
             //given
             Car car = new Car("carname");
             //when
-            car.move(()-> true);
+            car.move(() -> true);
             //then
             assertThat(car.getPosition()).isEqualTo(1);
         }
 
         @DisplayName("[성공] 전략이 실패하면 자동차가 이동하지 않는다.")
         @Test
-        void carNotMove(){
+        void carNotMove() {
             //given
             Car car = new Car("carname");
             //when
