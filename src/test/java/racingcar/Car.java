bb9673d59b6f70e7dbb0fd322522b23bf6b36e6d
hiblue02 <hiblue02@gmail.com>
2023-08-15 20:56:20 +0900

    feat(racingcar): 자동차 이름 길이 제한 기능 작성

--- src/test/java/racingcar/Car.java
+++ src/test/java/racingcar/Car.java
@@ -1,7 +1,16 @@
 package racingcar;
 
+import org.springframework.util.StringUtils;
+
 public class Car {
     private String name;
     public Car(String name) {
+        if(!StringUtils.hasText(name)){
+            throw new IllegalArgumentException();
+        }
+        if (name.length() < 5) {
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
     }
 }
