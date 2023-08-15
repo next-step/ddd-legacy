69e1e6077870f9a8af354a7ba150f349e8238879
yuhwanwoo <6514687@naver.com>
2023-08-15 22:18:07 +0900

    feat(racingcar): implement the car

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,29 @@
+package racingcar;
+
+import java.util.Random;
+
+class Car {
+    private final String name;
+    private int position;
+
+    public Car(final String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5글자를 넘을 수 없습니다.");
+        }
+        this.name = name;
+    }
+
+    public void move(final MoveCondition condition) {
+        if (condition.movable()) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
