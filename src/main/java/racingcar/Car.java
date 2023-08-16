5becd322082f6e028b1b8c8e08b50df34783c9bc
ryuhistory <ryuhistory@gmail.com>
2023-08-16 23:55:06 +0900

    refactor : step0 피드백 반영

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,27 +1,27 @@
 package racingcar;
 
-class Car {
-	private final String name;
-	private int position;
+public class Car {
+    private final String name;
+    private int position;
 
-	public Car(String car) {
-		if (car.length() > 5) {
-			throw new IllegalArgumentException("이름은 5자리를 넘을 수 없다.");
-		}
-		this.name = car;
-	}
+    public Car(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름은 5자리를 넘을 수 없다.");
+        }
+        this.name = name;
+    }
 
-	public void move(MoveCondition moveCondition) {
-		if (moveCondition.movable()) {
-			position++;
-		}
-	}
+    public void move(MoveCondition moveCondition) {
+        if (moveCondition.movable()) {
+            position++;
+        }
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public int getPosition() {
-		return position;
-	}
+    public int getPosition() {
+        return position;
+    }
 }
--- src/main/java/racingcar/MoveCondition.java
+++ src/main/java/racingcar/MoveCondition.java
@@ -2,19 +2,6 @@ package racingcar;
 
 @FunctionalInterface
 public interface MoveCondition {
-	boolean movable();
+    boolean movable();
 }
 
-class NumberMoveCondition implements MoveCondition {
-
-	private final int condition;
-
-	public NumberMoveCondition(int condition) {
-		this.condition = condition;
-	}
-
-	@Override
-	public boolean movable() {
-		return condition >= 4;
-	}
-}
