6f40109b072bf8f9c56fe21c4352ea6bd689c1cb
Jake <cocahack@naver.com>
2023-08-16 12:30:30 +0900

    feat: null 또는 빈 문자열 입력 시 0을 반환하도록 구현

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class StringCalculator {
+    public int add(String input) {
+        if (input == null || input.isBlank()) {
+            return 0;
+        }
+
+        return -1;
+    }
+}
