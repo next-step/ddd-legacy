6931b7b2d1b1a59f6be1cde5d1262b52d06c59d3
sang5c <skah321@gmail.com>
2023-08-16 15:31:41 +0900

    test(Numbers): 도메인 테스트 작성

---
+++ src/test/java/calculator/NumbersTest.java
@@ -0,0 +1,29 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+class NumbersTest {
+
+    @DisplayName("문자 리스트를 받아 객체를 생성한다")
+    @Test
+    void create() {
+        assertThatCode(() -> Numbers.fromString(List.of("1", "2", "3")))
+                .doesNotThrowAnyException();
+    }
+
+    @DisplayName("숫자의 합을 계산한다")
+    @Test
+    void sum() {
+        Numbers numbers = Numbers.fromString(List.of("1", "2", "3"));
+
+        int actual = numbers.sum();
+
+        assertThat(actual).isEqualTo(6);
+    }
+}
