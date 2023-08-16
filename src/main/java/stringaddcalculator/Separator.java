45faf8b39dbdd7ea1ff08b5e3d7ffedd1c9d664d
soo <yunsoo0510@daum.net>
2023-08-16 22:16:32 +0900

    feat(stringaddcalculator): implement the separator
    - validation
    - custom delimiter

--- src/main/java/stringaddcalculator/Separator.java
+++ src/main/java/stringaddcalculator/Separator.java
@@ -1,12 +1,34 @@
 package stringaddcalculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Separator {
-    private static final String DELIMITER = ",|:";
+    private static final String DEFAULT_DELIMITER = ",|:";
+    private static final String CUSTOM_DELIMITER_PATTERN = "//(.)\n(.*)";
 
     public int[] separate(String expression) {
-        return Arrays.stream(expression.split(DELIMITER))
+        validate(expression);
+        String delimiter = DEFAULT_DELIMITER;
+
+        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER_PATTERN).matcher(expression);
+        if (matcher.find()) {
+            delimiter = matcher.group(1);
+            expression = matcher.group(2);
+        }
+
+        return split(expression, delimiter);
+    }
+
+    private void validate(String expression) {
+        if (expression == null || expression.isBlank()) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private int[] split(String expression, String delimiter) {
+        return Arrays.stream(expression.split(delimiter))
                 .mapToInt(Integer::parseInt)
                 .toArray();
     }
--- src/test/java/stringaddcalculator/SeparatorTest.java
+++ src/test/java/stringaddcalculator/SeparatorTest.java
@@ -1,18 +1,43 @@
 package stringaddcalculator;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class SeparatorTest {
+    private Separator separator;
+
+    @BeforeEach
+    void setup() {
+        separator = new Separator();
+    }
+
     @DisplayName("쉼표(,) 또는 콜론(:)을 구분자로 가지는 문자열에서 숫자를 분리한다")
     @ValueSource(strings = {"1,2,3", "1,2:3"})
     @ParameterizedTest
     void separate(final String expression) {
-        Separator separator = new Separator();
+        int[] answer = separator.separate(expression);
 
+        assertThat(answer).containsExactly(1, 2, 3);
+    }
+
+    @DisplayName("빈 문자열 또는 null을 입력할 경우 예외가 발생한다")
+    @NullAndEmptySource
+    @ParameterizedTest
+    void validate(final String expression) {
+        assertThatThrownBy(() -> separator.separate(expression))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("구분자를 커스텀할 수 있다")
+    @ValueSource(strings = {"//;\n1;2;3"})
+    @ParameterizedTest
+    void custom_delimiter(final String expression) {
         int[] answer = separator.separate(expression);
 
         assertThat(answer).containsExactly(1, 2, 3);
