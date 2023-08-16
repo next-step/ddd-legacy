9cd0d33505b68663d9af2e3e6c0ef3dacbbb393c
soo <yunsoo0510@daum.net>
2023-08-16 22:42:21 +0900

    feat(stringaddcalculator): implement the calculator
    - add expression by delimiter

--- src/test/java/stringaddcalculator/StringAddCalculatorTest.java
+++ src/test/java/stringaddcalculator/StringAddCalculatorTest.java
@@ -1,9 +1,11 @@
 package stringaddcalculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatExceptionOfType;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -28,5 +30,26 @@ class StringAddCalculatorTest {
     @ValueSource(strings = {"1"})
     void oneNumber(final String text) {
         assertThat(calculator.add(text)).isSameAs(Integer.parseInt(text));
+    }
+
+    @DisplayName(value = "숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2"})
+    void twoNumbers(final String text) {
+        assertThat(calculator.add(text)).isSameAs(3);
+    }
+
+    @DisplayName("쉼표(,) 또는 콜론(:)을 구분자로 분리한 각 숫자의 합을 반환한다")
+    @ValueSource(strings = {"1,2,3", "1,2:3"})
+    @ParameterizedTest
+    void delimiter(final String expression) {
+        assertThat(calculator.add(expression)).isEqualTo(6);
+    }
+
+    @DisplayName(value = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\n1;2;3"})
+    void customDelimiter(final String text) {
+        assertThat(calculator.add(text)).isSameAs(6);
     }
 }
