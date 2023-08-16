e14e15d69a373fda041b9138783eef3a4bef23c5
sang5c <skah321@gmail.com>
2023-08-16 11:45:23 +0900

    feat(Calculator): 기본 구분자를 사용하여 계산 결과를 반환한다

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,10 +1,20 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class StringCalculator {
     public int run(String str) {
         if (str == null || str.isEmpty()) {
             return 0;
         }
-        return Integer.parseInt(str);
+
+        if (str.length() == 1) {
+            return Integer.parseInt(str);
+        }
+
+        String[] numbers = str.split("[,:]");
+        return Arrays.stream(numbers)
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -3,6 +3,7 @@ package calculator;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,5 +23,19 @@ class StringCalculatorTest {
     void singleNumber() {
         StringCalculator calculator = new StringCalculator();
         assertThat(calculator.run("123")).isEqualTo(123);
+    }
+
+    @DisplayName("기본 구분자를 사용하여 계산 결과를 반환한다")
+    @ParameterizedTest
+    @CsvSource(
+            value = {"1,2=3", "1,2,3=6", "1,2:3=6", "4:5:6=15"},
+            delimiter = '='
+    )
+    void defaultDelimiter(String str, int expected) {
+        StringCalculator calculator = new StringCalculator();
+
+        int actual = calculator.run(str);
+
+        assertThat(actual).isEqualTo(expected);
     }
 }
