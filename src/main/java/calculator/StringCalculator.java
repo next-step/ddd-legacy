48c9b152cfe145f6c9a06ab95a51fafa29cbe37a
jehyeong <5909264@kbfg.com>
2023-08-16 12:07:42 +0900

    feat : 입력값이 숫자 하나인 경우 기능 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,13 +3,30 @@ package calculator;
 public class StringCalculator {
     private static final int ZERO_OF_STRING_CALCULATOR = 0;
 
-    public StringCalculator() {
-    }
-
-    public  int add(String text){
-        if(text == null || text.isEmpty()){
+    public int add(String text) {
+        if (isNullValue(text)) {
             return ZERO_OF_STRING_CALCULATOR;
         }
-        return ZERO_OF_STRING_CALCULATOR;
+        if (isSingleText(text)) {
+            getNumeric(text);
+        }
+        return getNumeric(text);
     }
+
+    private boolean isNullValue(String text) {
+        return text == null || text.isEmpty();
+    }
+
+    private boolean isSingleText(String text) {
+        return text.length() == 1;
+    }
+
+    private int getNumeric(String text) {
+        try {
+            return Integer.parseInt(text);
+        } catch (NumberFormatException e) {
+            throw new RuntimeException("숫자가 아닌 값이 존재합니다.");
+        }
+    }
+
 }
