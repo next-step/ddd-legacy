434abb886440bf160a251517a5c2ec136d7ab04c
jaejeong1 <wowjd555@gmail.com>
2023-08-16 23:39:18 +0900

    remove unused code

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,7 +1,5 @@
 package racingcar;
 
-import java.util.Random;
-
 class Car {
     private final String name;
     private int position;
@@ -11,12 +9,6 @@ class Car {
             throw new IllegalArgumentException("자동차의 이름은 5글자를 넘을 수 없습니다");
         }
         this.name = name;
-    }
-
-    public void move(final int condition) {
-        if (condition >= 4) {
-            position++;
-        }
     }
 
     public void move(final MoveCondition condition) {
