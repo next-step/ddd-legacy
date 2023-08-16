fcf789afb28f835fef93a13209e685ea3262dc11
sang5c <skah321@gmail.com>
2023-08-16 15:36:56 +0900

    test(NumberParser): 도메인 테스트 작성

--- src/main/java/calculator/Numbers.java
+++ src/main/java/calculator/Numbers.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class Numbers {
@@ -22,5 +23,18 @@ public class Numbers {
         return numbers.stream()
                 .reduce(Number.ZERO, Number::plus)
                 .getValue();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Numbers numbers1 = (Numbers) o;
+        return Objects.equals(numbers, numbers1.numbers);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(numbers);
     }
 }
---
+++ src/test/java/calculator/NumberParserTest.java
@@ -0,0 +1,33 @@
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class NumberParserTest {
+
+    @DisplayName("문자열을 받아 숫자 목록을 반환한다")
+    @Test
+    void parse() {
+        String str = "1,2:3";
+        Numbers expected = Numbers.fromString(List.of("1", "2", "3"));
+
+        Numbers actual = NumberParser.parse(str);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+
+    @DisplayName("커스텀 구분자를 사용하여 분리한 숫자 목록을 반환한다")
+    @Test
+    void parseWithCustomDelimiter() {
+        String str = "//;\n1;2;3";
+        Numbers expected = Numbers.fromString(List.of("1", "2", "3"));
+
+        Numbers actual = NumberParser.parse(str);
+
+        assertThat(actual).isEqualTo(expected);
+    }
+}
