4d042c2128396cef55ee89c1b176311f30f21ac2
soo <yunsoo0510@daum.net>
2023-08-16 22:33:38 +0900

    feat(stringaddcalculator): implement the calculator
    - one number expression

--- src/main/java/stringaddcalculator/StringAddCalculator.java
+++ src/main/java/stringaddcalculator/StringAddCalculator.java
@@ -1,12 +1,16 @@
 package stringaddcalculator;
 
+import java.util.Arrays;
+
 public class StringAddCalculator {
+    private final Separator separator = new Separator();
 
     public int add(String expression) {
         if (expression == null || expression.isBlank()) {
             return 0;
         }
 
-        return -1;
+        return Arrays.stream(separator.separate(expression))
+                .sum();
     }
 }
