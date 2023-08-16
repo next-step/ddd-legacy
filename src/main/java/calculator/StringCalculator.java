104c658d5d129bb588730a8eac33d9eb41b66422
sang5c <skah321@gmail.com>
2023-08-16 11:34:47 +0900

    feat(StringCalculator): 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -5,6 +5,6 @@ public class StringCalculator {
         if (str == null || str.isEmpty()) {
             return 0;
         }
-        return 1;
+        return Integer.parseInt(str);
     }
 }
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
@@ -14,5 +15,12 @@ class StringCalculatorTest {
     void nullOrEmpty(String str) {
         StringCalculator calculator = new StringCalculator();
         assertThat(calculator.run(str)).isZero();
+    }
+
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다")
+    @Test
+    void singleNumber() {
+        StringCalculator calculator = new StringCalculator();
+        assertThat(calculator.run("123")).isEqualTo(123);
     }
 }
