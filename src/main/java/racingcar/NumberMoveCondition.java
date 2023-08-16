faa3ed35021f297a673e202ba23de8751baef7e8
toyablue <137197778+toyablue@users.noreply.github.com>
2023-08-16 11:19:43 +0900

    feat(racingcar): implement car (#399)

---
+++ src/main/java/racingcar/NumberMoveCondition.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+public class NumberMoveCondition implements MoveCondition {
+
+    private final int condition;
+
+    NumberMoveCondition(int condition) {
+        this.condition = condition;
+    }
+
+    @Override
+    public boolean movable() {
+
+        return condition >= 4;
+    }
+}
