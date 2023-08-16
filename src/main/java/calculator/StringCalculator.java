adb0681d222ce426090fd5e2c5757e8fce1ccb52
jehyeong <5909264@kbfg.com>
2023-08-16 12:20:33 +0900

    feat : 콜론 구분를 활용한 문자열 덧샘 기능 추가

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,41 +1,44 @@
 package calculator;
 
-import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class StringCalculator {
-    private static final int ZERO_OF_STRING_CALCULATOR = 0;
+	private static final int ZERO_OF_STRING_CALCULATOR = 0;
+	private static final List<String> ADD_DELIMITER = List.of(",", ":");
 
-    public int add(String text) {
-        if (isNullValue(text)) {
-            return ZERO_OF_STRING_CALCULATOR;
-        }
-        if (isSingleText(text)) {
-            getNumeric(text);
-        }
+	public int add(String text) {
+		if (isNullValue(text)) {
+			return ZERO_OF_STRING_CALCULATOR;
+		}
+		if (isSingleText(text)) {
+			getNumeric(text);
+		}
 
-        return Arrays.stream(splittingText(text))
-            .mapToInt(this::getNumeric)
-            .sum();
-    }
+		return Stream.of(splittingText(text))
+			.mapToInt(this::getNumeric)
+			.sum();
+	}
 
-    private boolean isNullValue(String text) {
-        return text == null || text.isEmpty();
-    }
+	private boolean isNullValue(String text) {
+		return text == null || text.isEmpty();
+	}
 
-    private boolean isSingleText(String text) {
-        return text.length() == 1;
-    }
+	private boolean isSingleText(String text) {
+		return text.length() == 1;
+	}
 
-    private int getNumeric(String text) {
-        try {
-            return Integer.parseInt(text);
-        } catch (NumberFormatException e) {
-            throw new RuntimeException("숫자가 아닌 값이 존재 합니다.");
-        }
-    }
+	private int getNumeric(String text) {
+		try {
+			return Integer.parseInt(text);
+		} catch (NumberFormatException e) {
+			throw new RuntimeException("숫자가 아닌 값이 존재 합니다.");
+		}
+	}
 
-    private String[] splittingText(String text) {
-        return text.split(",");
-    }
-
+	private String[] splittingText(String text) {
+		String regex = String.format("[%s]"
+			, String.join("", ADD_DELIMITER));
+		return text.split(regex);
+	}
 }
