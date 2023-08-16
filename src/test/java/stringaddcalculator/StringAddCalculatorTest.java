be29b53e87fb63c65453b1c01ffc810b32eb247a
soo <yunsoo0510@daum.net>
2023-08-16 23:03:50 +0900

    feat(stringaddcalculator): implement the calculator
    - exception for negative number

--- src/test/java/stringaddcalculator/StringAddCalculatorTest.java
+++ src/test/java/stringaddcalculator/StringAddCalculatorTest.java
@@ -52,4 +52,11 @@ class StringAddCalculatorTest {
     void customDelimiter(final String text) {
         assertThat(calculator.add(text)).isSameAs(6);
     }
+
+    @DisplayName(value = "문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리를 한다.")
+    @Test
+    void negative() {
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> calculator.add("-1"));
+    }
 }
