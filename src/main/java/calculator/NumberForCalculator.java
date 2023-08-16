f47161a38f1c678e799fd6fc29bbbc2fdc6f1530
ryuhistory <ryuhistory@gmail.com>
2023-08-16 22:13:39 +0900

    refactor : number 네이밍 변경

--- src/main/java/calculator/NumberForCalculator.java
+++ src/main/java/calculator/NumberForCalculator.java
@@ -5,9 +5,9 @@ public class NumberForCalculator {
 
 	public NumberForCalculator(String text) {
 		try {
-			int number = Integer.parseInt(text);
-			checkingNegative(number);
-			this.number = number;
+			int resultNumber = Integer.parseInt(text);
+			checkingNegative(resultNumber);
+			this.number = resultNumber;
 		} catch (NumberFormatException e) {
 			throw new RuntimeException("숫자가 아닌 값이 존재 합니다.");
 		}
