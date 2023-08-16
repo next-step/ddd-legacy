96239296aa88dcfad217727996c99c4b90c00f72
이의현 <59961350+t1dmlgus@users.noreply.github.com>
2023-08-17 08:46:09 +0900

    0단계 - JUnit5 학습 (#425)
    * feat: racingCar implement car
    
    * fix: 사용안하는 메서드 제거

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,28 @@
+package racingcar;
+
+class Car{
+    private final String name;
+    private int position;
+
+    public Car(final String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차의 이름은 5글자를 넘을 수 없습니다.");
+        }
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void move(final MoveCondition condition) {
+        if (condition.movable()) {
+            position++;
+        }
+    }
+
+
+    public int getPosition() {
+        return position;
+    }
+}
