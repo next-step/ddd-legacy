643b8fd894da935323f1f6348c946b55dd56059b
koola97620 <koola97620@nate.com>
2023-08-17 23:17:09 +0900

    refactor(StringCalculator): 테스트 중복코드 제거, 한 메서드로 합치기

--- src/test/java/stringcalculator/StrongCalculatorTest.java
+++ src/test/java/stringcalculator/StrongCalculatorTest.java
@@ -1,33 +1,34 @@
 package stringcalculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.platform.commons.util.StringUtils;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 public class StrongCalculatorTest {
 
-    @DisplayName("빈 문자열을 입력하면 결과는 0이다.")
-    @Test
-    void emptyString_thenZero() {
-        StringCalculator stringCalculator = new StringCalculator();
-        int result = stringCalculator.calculate("");
-        assertThat(result).isZero();
+    private StringCalculator stringCalculator;
+
+    @BeforeEach
+    void setUp() {
+        stringCalculator = new StringCalculator();
     }
 
-    @DisplayName("null 입력하면 결과는 0이다.")
-    @Test
-    void nullString_thenZero() {
-        StringCalculator stringCalculator = new StringCalculator();
-        int result = stringCalculator.calculate(null);
+    @DisplayName("빈 문자열 또는 null 을 입력하면 결과는 0이다.")
+    @NullAndEmptySource
+    @ParameterizedTest
+    void empty_or_null_thenZero(String input) {
+        int result = stringCalculator.calculate(input);
         assertThat(result).isZero();
     }
 
     @DisplayName("숫자 문자열 하나 입력시 입력한 숫자 문자열을 반환한다.")
     @Test
     void one_string_sum() {
-        StringCalculator stringCalculator = new StringCalculator();
         int result = stringCalculator.calculate("1");
         assertThat(result).isEqualTo(1);
     }
