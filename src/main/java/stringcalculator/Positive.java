969315cb7004412bf452309843b0e34cc7cbf5cc
hiblue02 <hiblue02@gmail.com>
2023-08-17 22:38:26 +0900

    refactor(stringcalculator) : Positive equals 생성

--- src/main/java/stringcalculator/Positive.java
+++ src/main/java/stringcalculator/Positive.java
@@ -1,5 +1,7 @@
 package stringcalculator;
 
+import java.util.Objects;
+
 public class Positive {
     private final int number;
 
@@ -12,5 +14,18 @@ public class Positive {
 
     public int parseInt() {
         return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Positive positive = (Positive) o;
+        return number == positive.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
