511cb1dd08df33f39a75e3ac843371cb7d54b35f
sang5c <skah321@gmail.com>
2023-08-17 22:05:32 +0900

    refactor(NumberParser): 인덱스 매직 넘버 상수화

--- src/main/java/calculator/NumberParser.java
+++ src/main/java/calculator/NumberParser.java
@@ -8,6 +8,8 @@ public class NumberParser {
     private static final String CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)";
     private static final Pattern PATTERN = Pattern.compile(CUSTOM_DELIMITER_REGEX);
     private static final String DEFAULT_DELIMITER_REGEX = "[,:]";
+    private static final int CUSTOM_DELIMITER_INDEX = 1;
+    private static final int TARGET_TEXT_INDEX = 2;
 
     public static Numbers parse(String str) {
         List<String> numbers = extractNumbers(str);
@@ -17,8 +19,8 @@ public class NumberParser {
     private static List<String> extractNumbers(String str) {
         Matcher m = PATTERN.matcher(str);
         if (m.find()) {
-            String customDelimiter = m.group(1);
-            String[] numbers = m.group(2).split(customDelimiter);
+            String customDelimiter = m.group(CUSTOM_DELIMITER_INDEX);
+            String[] numbers = m.group(TARGET_TEXT_INDEX).split(customDelimiter);
             return List.of(numbers);
         }
 
