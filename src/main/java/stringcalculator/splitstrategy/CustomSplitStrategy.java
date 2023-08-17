e989316fda695acc1665bfa7d5044ecff5079e06
hiblue02 <hiblue02@gmail.com>
2023-08-17 22:09:22 +0900

    feat(stringcalculator) : CustomSplitStrategy 매직넘버 수정

--- src/main/java/stringcalculator/splitstrategy/CustomSplitStrategy.java
+++ src/main/java/stringcalculator/splitstrategy/CustomSplitStrategy.java
@@ -7,6 +7,8 @@ import java.util.regex.Pattern;
 
 public class CustomSplitStrategy implements SplitStrategy {
     private static final Pattern MATCH_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final int CUSTOM_DELIMITER_POSITION = 1;
+    private static final int TARGET_TEXT_POSITION = 2;
 
     @Override
     public String[] split(String text) {
@@ -14,10 +16,10 @@ public class CustomSplitStrategy implements SplitStrategy {
 
         String customDelimiter = "";
         if (m.find()) {
-            customDelimiter = m.group(1);
+            customDelimiter = m.group(CUSTOM_DELIMITER_POSITION);
         }
 
-        return m.group(2).split(customDelimiter);
+        return m.group(TARGET_TEXT_POSITION).split(customDelimiter);
     }
 
     @Override
