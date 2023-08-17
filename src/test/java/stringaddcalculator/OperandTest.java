347d52222c21081818a6c9710eca092b6d291726
soo <yunsoo0510@daum.net>
2023-08-17 23:52:39 +0900

    refactor(str_calculator): add operand happy test case
    - number 0, 1 can be Operand instance

--- src/test/java/stringaddcalculator/OperandTest.java
+++ src/test/java/stringaddcalculator/OperandTest.java
@@ -1,10 +1,19 @@
 package stringaddcalculator;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class OperandTest {
+
+    @ValueSource(ints = {0, 1})
+    @ParameterizedTest
+    void Operand는_0_또는_양수이다(int number) {
+        assertDoesNotThrow(() -> new Operand(number));
+    }
 
     @Test
     void Operand는_음수일_수_없다() {
