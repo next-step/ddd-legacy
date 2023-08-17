8d0ba375019b3b47cfe044bae7be29fcb47b0c63
uram <parkuram12@gmail.com>
2023-08-17 23:13:51 +0900

    test(StringCalculatorTest): import static Assertions && 음수 검증 테스트 추가

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,10 +1,11 @@
 package calculator;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.*;
 
 class StringCalculatorTest {
 
@@ -17,34 +18,43 @@ class StringCalculatorTest {
 
     @Test
     void 빈_문자열을_입력하면_0을_반환받는다() {
-        Assertions.assertThat(calculator.add("")).isZero();
+        assertThat(calculator.add("")).isZero();
     }
 
     @Test
     void NULL_을_입력하면_0을_반환받는다() {
-        Assertions.assertThat(calculator.add(null)).isZero();
+        assertThat(calculator.add(null)).isZero();
     }
+
     @Test
     void 숫자_하나를_문자열로_입력하면_해당_숫자를_반환받는다() {
-        Assertions.assertThat(calculator.add("3")).isEqualTo(3);
+        assertThat(calculator.add("3")).isEqualTo(3);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"3,4"})
     void 숫자_두개를_구분자로_입력하면_합을_반환받는다(final String value) {
-        Assertions.assertThat(calculator.add(value)).isEqualTo(7);
+        assertThat(calculator.add(value)).isEqualTo(7);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"3:4"})
     void 구분자를_콜론으로_사용할_수_있다(final String value) {
-        Assertions.assertThat(calculator.add(value)).isEqualTo(7);
+        assertThat(calculator.add(value)).isEqualTo(7);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"//;\n1;2;3"})
     void 구분자를_커스텀_할_수_있다(final String value) {
-        Assertions.assertThat(calculator.add(value)).isEqualTo(6);
+        assertThat(calculator.add(value)).isEqualTo(6);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\n1;-1;3"})
+    void 음수는_추가될_수_없다(final String value) {
+        assertThatThrownBy(() -> calculator.add(value))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("음수는 추가될 수 없습니다.");
     }
 
 
