e635b6e12abc3dbc7d3a6ce3a2c862a99f4264a0
danalee <eleeje97@gmail.com>
2023-08-18 01:50:30 +0900

    feat: 빈 문자열 또는 null값 입력 처리

---
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -0,0 +1,11 @@
+package stringcalculator;
+
+public class StringCalculator {
+    public int add(String calcInput) {
+        if (calcInput == null || calcInput.isBlank()) {
+            return 0;
+        }
+
+        return 1;
+    }
+}
