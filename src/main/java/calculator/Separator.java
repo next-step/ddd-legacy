5817bd9da605286966d2d9ebb3fcb57f8d2f7421
parkjun5 <fgfg3351@gmail.com>
2023-08-18 17:37:27 +0900

    refactor(Separator): block public constructor

--- src/main/java/calculator/Separator.java
+++ src/main/java/calculator/Separator.java
@@ -1,9 +1,13 @@
 package calculator;
 
 public class Separator {
+    private Separator() { }
+
     public static NumberStrings separate(final TargetString targetString) {
         NumberAppender numberAppender = new NumberAppender(targetString);
-        for (char ch : targetString.toCharArray()) numberAppender.appendToNumberStrings(ch);
+        for (char ch : targetString.toCharArray()) {
+            numberAppender.appendToNumberStrings(ch);
+        }
         return numberAppender.getNumberStrings();
     }
 }
