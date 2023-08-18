0d408e04aedace1f9d3d869f854bdf779fe2ea65
parkjun5 <fgfg3351@gmail.com>
2023-08-18 17:36:30 +0900

    refactor(NumberAppender): apply more IoC and refacotr appendToNUmberStrings Method

--- src/main/java/calculator/NumberAppender.java
+++ src/main/java/calculator/NumberAppender.java
@@ -3,21 +3,25 @@ package calculator;
 import java.util.ArrayList;
 
 public class NumberAppender {
-    private final Delimiter delimiter = new Delimiter();
-    private static final StringBuilder numberBuilder = new StringBuilder();
+    private final Delimiter delimiter;
+    private final StringBuilder numberBuilder = new StringBuilder();
     private final NumberStrings numberStrings = new NumberStrings(new ArrayList<>());
 
     public NumberAppender(final TargetString targetString) {
-        numberBuilder.setLength(0);
-        delimiter.addNewDelimiterIfExist(targetString);
+        delimiter = new Delimiter(targetString);
     }
 
     public void appendToNumberStrings(final char ch) {
-        if (delimiter.contains(ch)) {
+        boolean isNotDigit = !Character.isDigit(ch);
+        boolean isDelimiter = delimiter.contains(ch);
+        if (isNotDigit && !isDelimiter) {
+            throw new RuntimeException(ch + "는 허용된 구분자가 아니거나 숫자가 아닙니다.");
+        }
+
+        if (isDelimiter) {
             numberStrings.addIfNotEmpty(numberBuilder);
             return;
         }
-        if (!Character.isDigit(ch)) throw new RuntimeException(ch + "는 허용된 구분자가 아니거나 숫자가 아닙니다.");
         numberBuilder.append(ch);
     }
 
