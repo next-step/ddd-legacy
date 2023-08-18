924d9ca47fcc78cc69db44e0a14afa0c659b8e1b
jaejeong1 <wowjd555@gmail.com>
2023-08-19 03:06:18 +0900

    feat: step2 StringCalculator

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,51 @@
+package calculator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+    private static final String CUSTOM_DELIMITER_REGEX = "//(.)\n(.*)";
+    private static final String DEFAULT_DELIMITER = ",|:";
+    private static final int CUSTOM_DELIMITER_INDEX = 1;
+    private static final int STRING_TO_SPLIT_INDEX = 2;
+
+    private final Pattern customDelemiterPattern;
+
+    public StringCalculator() {
+        customDelemiterPattern = Pattern.compile(CUSTOM_DELIMITER_REGEX);
+    }
+
+    public int add(final String text) {
+        if (text == null) {return 0;}
+        if (text.isEmpty()) {return 0;}
+
+        String[] tokens = findTokens(text);
+        int sum = 0;
+
+        for (String token : tokens) {
+            int num = Integer.parseInt(token);
+            validate(num);
+            sum += num;
+        }
+
+        return sum;
+    }
+
+    private void validate(final int num) {
+        if (num < 0) {
+            throw new RuntimeException();
+        }
+    }
+
+    private String[] findTokens(final String text) {
+        Matcher m = customDelemiterPattern.matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(CUSTOM_DELIMITER_INDEX);
+            return m.group(STRING_TO_SPLIT_INDEX).split(customDelimiter);
+        }
+
+        return text.split(DEFAULT_DELIMITER);
+    }
+}
+
+
