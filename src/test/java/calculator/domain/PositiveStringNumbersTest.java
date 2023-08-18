9ac278eab63d04e76e2be4f67d9803b654c97807
김경록 <kyeongrok.kim@kurlycorp.com>
2023-08-19 01:56:34 +0900

    test: PositiveStringNumbers 테스트 코드 추가

--- src/test/java/calculator/domain/PositiveStringNumbersTest.java
+++ src/test/java/calculator/domain/PositiveStringNumbersTest.java
@@ -1,14 +1,12 @@
 package calculator.domain;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import calculator.fixture.PositiveStringNumbersFixture;
-import java.util.List;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.CsvFileSource;
-import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 class PositiveStringNumbersTest {
@@ -16,26 +14,22 @@ class PositiveStringNumbersTest {
     @DisplayName("PositiveStringNumbers 컬렉션의 합산을 구한다")
     @ParameterizedTest
     @MethodSource
-    void testAddAll(String a, String b, String c) {
+    void testAddAll(String[] values, PositiveStringNumber expected) {
         // given
-        PositiveStringNumbers positiveStringNumbers = PositiveStringNumbersFixture.create(a, b, c);
+        PositiveStringNumbers positiveStringNumbers = PositiveStringNumbersFixture.create(values);
 
         // when
-        System.out.println(positiveStringNumbers);
+        PositiveStringNumber actual = positiveStringNumbers.addAll();
 
         // then
+        assertThat(actual).isEqualTo(expected);
     }
 
     private static Stream<Arguments> testAddAll() {
         return Stream.of(
-            Arguments.of(List.of(""), PositiveStringNumbers.EMPTY_POSITIVE_STRING_NUMBERS),
-            Arguments.of("1,2,3", PositiveStringNumbersFixture.create("1", "2", "3")),
-            Arguments.of("1,2,3", PositiveStringNumbersFixture.create("1", "2", "3")),
-            Arguments.of("1:2:3", PositiveStringNumbersFixture.create("1", "2", "3")),
-            Arguments.of("1:2:3", PositiveStringNumbersFixture.create("1", "2", "3")),
-            Arguments.of("//;\n1;2;3", PositiveStringNumbersFixture.create("1", "2", "3"))
+            Arguments.of(new String[]{}, PositiveStringNumber.ZERO),
+            Arguments.of(new String[]{"1"}, PositiveStringNumber.of("1")),
+            Arguments.of(new String[]{"1", "2", "3"}, PositiveStringNumber.of("6"))
         );
     }
-
-
 }
