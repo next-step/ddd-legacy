49c7f95246a0c0416404a9da2a346af916d80713
mypark <mypark@miridih.com>
2023-08-18 13:07:47 +0900

    feature(string-additional-calculator): PositiveNumbers에 equals & hashCode 추가

--- src/main/java/string_additional_calculator/PositiveNumbers.java
+++ src/main/java/string_additional_calculator/PositiveNumbers.java
@@ -1,6 +1,7 @@
 package string_additional_calculator;
 
 import java.util.List;
+import java.util.Objects;
 
 public class PositiveNumbers {
     private final List<PositiveNumber> values;
@@ -22,5 +23,18 @@ public class PositiveNumbers {
 
     public List<PositiveNumber> getValues() {
         return values;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PositiveNumbers that = (PositiveNumbers) o;
+        return Objects.equals(values, that.values);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(values);
     }
 }
