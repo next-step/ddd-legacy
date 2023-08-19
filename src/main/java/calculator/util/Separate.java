83d4afb4ae29471527c25e2ff18f7cc2f2d41e82
Shin seongsu <tlstjdtn321@naver.com>
2023-08-20 00:02:29 +0900

    feat: String parser 추가

---
+++ src/main/java/calculator/util/Separate.java
@@ -0,0 +1,22 @@
+package calculator.util;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public final class Separate {
+
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+    private static final String DELIMITER_PATTERN = "[,:]";
+
+    public static String[] parser(final String input) {
+        final Matcher customMatcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
+
+        if (customMatcher.find()) {
+            final String customDelimiter = customMatcher.group(1);
+            return customMatcher.group(2).split(customDelimiter);
+        }
+
+        return input.split(DELIMITER_PATTERN);
+    }
+
+}
