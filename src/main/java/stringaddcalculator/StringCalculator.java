e4da01b25ceafb5368bcaa3a33cb61f40776bc1e
toyablue <hjahn934@gmail.com>
2023-08-19 22:03:44 +0900

    feat: 커스텀 구분자 기능 추가

--- src/main/java/stringaddcalculator/StringCalculator.java
+++ src/main/java/stringaddcalculator/StringCalculator.java
@@ -1,6 +1,8 @@
 package stringaddcalculator;
 
 import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
     public int add(String text) {
@@ -10,6 +12,19 @@ public class StringCalculator {
 
         if (text.length() == 1) {
             return Integer.parseInt(text);
+        }
+
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String [] tokens = m.group(2).split(customDelimiter);
+
+            int result = 0;
+
+            for (String value : tokens) {
+                result = result + Integer.parseInt(value);
+            }
+            return result;
         }
 
         String[] tokens = text.split(",|:");
