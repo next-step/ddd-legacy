1152d0a4f21acd283fec48c3615b7cdf24caa7b4
Shin seongsu <tlstjdtn321@naver.com>
2023-08-19 23:56:18 +0900

    feat: 숫자 저장할 도메인 추가

---
+++ src/main/java/calculator/domain/Operand.java
@@ -0,0 +1,15 @@
+package calculator.domain;
+
+public class Operand {
+
+    private final int number;
+
+    public Operand(final int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+}
