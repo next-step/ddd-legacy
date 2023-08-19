e78a9710aae20c456a17f522639e8d5970b10900
hiblue02 <hiblue02@gmail.com>
2023-08-19 17:20:30 +0900

    test(kitchenpos): 상품 서비스 테스트 케이스 추가

--- src/test/java/kitchenpos/ApplicationTest.java
+++ src/test/java/kitchenpos/ApplicationTest.java
@@ -1,11 +1,8 @@
 package kitchenpos;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ExtendWith(MockitoExtension.class)
 public class ApplicationTest {
-    @Test
-    void contextLoads() {
-    }
 }
--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -1,64 +1,159 @@
 package kitchenpos.application;
 
-import kitchenpos.domain.MenuRepository;
-import kitchenpos.domain.Product;
-import kitchenpos.domain.ProductRepository;
+import kitchenpos.ApplicationTest;
+import kitchenpos.domain.*;
+import kitchenpos.fixture.MenuFixture;
+import kitchenpos.fixture.MenuGroupFixture;
+import kitchenpos.fixture.MenuProductFixture;
 import kitchenpos.fixture.ProductFixture;
-import kitchenpos.utils.fake.FakePurgomalumClient;
-import kitchenpos.utils.fake.FakePurgomalumResponse;
+import kitchenpos.infra.PurgomalumClient;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
-@SpringBootTest
-class ProductServiceTest {
-    @Autowired
+
+@DisplayName("상품")
+class ProductServiceTest extends ApplicationTest {
+    @Mock
     private ProductRepository productRepository;
-    @Autowired
+    @Mock
     private MenuRepository menuRepository;
+    @Mock
+    private PurgomalumClient purgomalumClient;
 
-    private final ProductService productService = new ProductService(productRepository, menuRepository, new FakePurgomalumClient());
+    @InjectMocks
+    private ProductService productService;
 
-    @DisplayName("[예외] 상품의 이름은 공백일 수 없다.")
-    @ParameterizedTest
-    @NullSource
-    void name_test_1(String name) {
-        //when
-        Product product = ProductFixture.create(name);
-        //then
-        assertThatThrownBy(() -> productService.create(product))
-                .isInstanceOf(IllegalArgumentException.class);
+    @DisplayName("이름")
+    @Nested
+    class name {
+        @DisplayName("[예외] 상품의 이름은 공백일 수 없다.")
+        @ParameterizedTest
+        @NullSource
+        void name_test_1(String name) {
+            //given
+            Product product = ProductFixture.create(name);
+            //then
+            assertThatThrownBy(() -> productService.create(product))
+                    .isInstanceOf(IllegalArgumentException.class);
 
+        }
+
+        @DisplayName("[예외] 상품의 이름은 비속어일 수 없다.")
+        @Test
+        void name_test_2() {
+            //given
+            when(purgomalumClient.containsProfanity(any())).thenReturn(true);
+            //when
+            Product product = ProductFixture.create("떡볶이");
+            //then
+            assertThatThrownBy(() -> productService.create(product))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
     }
 
-    @DisplayName("[예외] 상품의 이름은 비속어일 수 없다.")
+    @DisplayName("가격")
+    @Nested
+    class price {
+        @DisplayName("[예외] 상품의 가격은 공백일 수 없다.")
+        @ParameterizedTest
+        @NullSource
+        void price_test_1(BigDecimal price) {
+            //given
+            Product product = ProductFixture.create(price);
+            //then
+            assertThatThrownBy(() -> productService.create(product))
+                    .isInstanceOf(IllegalArgumentException.class);
+
+        }
+
+        @DisplayName("[예외] 상품의 가격은 0원 이상이다.")
+        @ParameterizedTest
+        @ValueSource(longs = {-1, -2})
+        void price_test_2(long price) {
+            //given
+            Product product = ProductFixture.create(BigDecimal.valueOf(price));
+            //then
+            assertThatThrownBy(() -> productService.create(product))
+                    .isInstanceOf(IllegalArgumentException.class);
+
+        }
+    }
+
+    @DisplayName("[성공] 상품을 등록한다.")
     @Test
-    void name_test_2() {
+    void create_test_1() {
+        //given
+        Product product = ProductFixture.create("떡볶이", BigDecimal.valueOf(1000));
+        when(productRepository.save(any())).thenReturn(product);
         //when
-        Product product = ProductFixture.create(FakePurgomalumResponse.비속어.name());
+        Product created = productService.create(product);
         //then
-        assertThatThrownBy(() -> productService.create(product))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThat(created).isEqualTo(product);
     }
 
-    @DisplayName("[예외] 상품의 가격은 공백일 수 없다.")
-    @ParameterizedTest
-    @NullSource
-    void price_test_1(BigDecimal price) {
-        //when
-        Product product = ProductFixture.create(price);
-        //then
-        assertThatThrownBy(() -> productService.create(product))
-                .isInstanceOf(IllegalArgumentException.class);
+    @DisplayName("가격 바꾸기")
+    @Nested
+    class price_change {
+        @DisplayName("[성공] 상품 가격을 바꾼다.")
+        @Test
+        void price_change_test_1() {
+            //given
+            Product product = ProductFixture.create("떡볶이", BigDecimal.valueOf(1000));
+            when(productRepository.findById(any())).thenReturn(Optional.of(product));
+            //when
+            product.setPrice(BigDecimal.valueOf(3000));
+            Product changed = productService.changePrice(product.getId(), product);
+            //then
+            assertThat(changed.getPrice()).isEqualTo(product.getPrice());
+        }
+
+        @DisplayName("[예외] 상품 가격은 0원 이상이어야 한다.")
+        @Test
+        void price_change_test_2() {
+            //given
+            Product product = ProductFixture.create("떡볶이", BigDecimal.valueOf(1000));
+            //when
+            product.setPrice(BigDecimal.valueOf(-1));
+            //then
+            assertThatThrownBy(() -> productService.changePrice(product.getId(), product))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("[성공] 상품 가격을 바꿀 때 메뉴가격이 구성상품 가격의 합보다 작으면 숨긴다. ")
+        @Test
+        void price_change_test_3() {
+            //given
+            Product product1 = ProductFixture.create(BigDecimal.valueOf(1000));
+            Product product2 = ProductFixture.create(BigDecimal.valueOf(1000));
+            Menu menu = MenuFixture.create(
+                    BigDecimal.valueOf(2000)
+                    , MenuProductFixture.createDefaulsWithProduct(product1, product2));
+
+            when(productRepository.findById(product1.getId())).thenReturn(Optional.of(product1));
+            when(menuRepository.findAllByProductId(any())).thenReturn(List.of(menu));
+            assertThat(menu.isDisplayed()).isTrue();
+            //when
+            product1.setPrice(BigDecimal.valueOf(1500));
+            productService.changePrice(product1.getId(), product1);
+            //then
+            assertThat(menu.isDisplayed()).isFalse();
+        }
 
     }
-
-
 }
