d1dee9a707dea4d6f710d58b0c744f09ade3802b
toyablue <hjahn934@gmail.com>
2023-08-19 21:54:44 +0900

    feat: 빈 문자열 or null인 경우 0 반환 기능 생성

---
+++ src/main/java/stringaddcalculator/StringCalculator.java
@@ -0,0 +1,14 @@
+package stringaddcalculator;
+
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+    public int add(String text) {
+        if (Objects.isNull(text) || text.isEmpty()) {
+            return 0;
+        }
+        return 0;
+    }
+}
