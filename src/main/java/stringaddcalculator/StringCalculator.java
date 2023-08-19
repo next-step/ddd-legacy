713de99444660f7d639671300a64ec2fce22c8e0
toyablue <hjahn934@gmail.com>
2023-08-19 22:23:00 +0900

    refactor: add 메서드 코드 리팩토링

--- src/main/java/stringaddcalculator/StringCalculator.java
+++ src/main/java/stringaddcalculator/StringCalculator.java
@@ -5,39 +5,50 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
+    private static final Pattern pattern = Pattern.compile("//(.)\n(.*)");
+    private static final String regex = ",|:";
+    private static final int ZERO = 0;
+    private static final int ONE = 1;
+    private static final int TWO = 2;
+
+    private int result;
+
     public int add(String text) {
         if (Objects.isNull(text) || text.isEmpty()) {
-            return 0;
+            return ZERO;
         }
 
-        if (text.length() == 1) {
+        if (text.length() == ONE) {
             return Integer.parseInt(text);
         }
 
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        Matcher m = pattern.matcher(text);
         if (m.find()) {
-            String customDelimiter = m.group(1);
-            String [] tokens = m.group(2).split(customDelimiter);
-
-            int result = 0;
-
-            for (String value : tokens) {
-                result = result + Integer.parseInt(value);
-            }
-            return result;
+            addByUniquePattern(m);
         }
 
-        String[] tokens = text.split(",|:");
+        String[] tokens = text.split(regex);
+        return parseAdd(tokens);
+    }
 
-        int result = 0;
+    private void addByUniquePattern(Matcher m) {
+        String customDelimiter = m.group(ONE);
+        String [] tokens = m.group(TWO).split(customDelimiter);
+        parseAdd(tokens);
+    }
 
+    private int parseAdd(String [] tokens) {
         for (String value : tokens) {
-            int num = Integer.parseInt(value);
-            if (num < 0) {
-                throw new RuntimeException("음수 입력 안 됨");
-            }
-            result = result + num;
+            int number = Integer.parseInt(value);
+            validateMinus(number);
+            result += number;
         }
         return result;
+    }
+
+    private void validateMinus(int number) {
+        if (number < ZERO) {
+            throw new RuntimeException("음수 값은 사용할 수 없는 값입니다.");
+        }
     }
 }
