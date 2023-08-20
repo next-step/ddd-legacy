fd8411e7eb3306f7ed4b4e352bb32601ad693157
Shin seongsu <tlstjdtn321@naver.com>
2023-08-21 08:56:54 +0900

    refactor: import 수정된 곳 수정

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,17 +1,18 @@
 package calculator;
 
-import calculator.application.OperandParser;
+import calculator.domain.NumberParser;
 import calculator.util.Validate;
 
 public class StringCalculator {
+
+    private final NumberParser numberParser = new NumberParser();
 
     public int add(final String input) {
         if (Validate.isNullOrBlank(input)) {
             return 0;
         }
 
-        return new OperandParser()
-                .parser(input)
+        return numberParser.parser(input)
                 .sum();
     }
 
