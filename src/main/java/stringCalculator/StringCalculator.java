19a4fd9f8a38ea7a9b093add588350021339674b
junlee <laonzenamoon@gmail.com>
2023-08-20 18:33:10 +0900

    feat(#step1): 빈 문자열 또는 null 값을 입력할 경우 0을 반환하도록 add 메서드 수정

--- src/main/java/stringCalculator/StringCalculator.java
+++ src/main/java/stringCalculator/StringCalculator.java
@@ -3,6 +3,11 @@ package stringCalculator;
 public class StringCalculator {
 
     public int add(String text) {
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+
         return 0;
     }
+
 }
