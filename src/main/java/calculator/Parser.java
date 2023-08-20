8345eb131b786ebc85a75fefe0c8de025195f37d
t1dmlgus <dmlgusgngl@gmail.com>
2023-08-20 11:19:45 +0900

    feat: [step1] 리펙토링 - 요구사항 1, 2 수정.

--- src/main/java/calculator/Parser.java
+++ src/main/java/calculator/Parser.java
@@ -7,16 +7,12 @@ public class Parser {
 
     static final Pattern patten = Pattern.compile("//(.)\n(.*)");
 
-    public String[] toNumbers(String text) {
+    public String[] parse(String text) {
 
         final int CUSTOM_DELIMITER = 1;
         final int TARGET = 2;
 
         String[] result = {};
-        if (text.contains(",") | text.contains(":")) {
-            result = text.split(",|:");
-            return result;
-        }
 
         Matcher matcher = patten.matcher(text);
         if (matcher.find()) {
--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -22,10 +22,21 @@ public class StringCalculator {
             return Integer.parseInt(text);
         }
 
-        String[] parse = parser.toNumbers(text);
-        return Arrays.stream(parse).mapToInt(Integer::parseInt).sum();
+        return parseToSum(text);
     }
 
+    private int parseToSum(String text) {
+
+        String[] result = {};
+
+        if (text.contains(",") | text.contains(":")) {
+            result = text.split(",|:");
+            return Arrays.stream(result).mapToInt(Integer::parseInt).sum();
+        }
+
+        result = parser.parse(text);
+        return Arrays.stream(result).mapToInt(Integer::parseInt).sum();
+    }
 
     private boolean isNullAndEmpty(String text) {
         return text == null || text.isEmpty();
