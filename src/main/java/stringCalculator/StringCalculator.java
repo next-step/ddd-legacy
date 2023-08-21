9860f2713a79df851bdf98cd19f250d98b23c651
junlee <laonzenamoon@gmail.com>
2023-08-21 11:08:03 +0900

    refactor(#step1): StringCalculator 코드 리팩토링
    - 정수 파싱 기능 클래스 분리
    - 기본 구분자 상수로 분리
    - Pattern 객체 상수로 분리
    - 매직 넘버 상수로 분리

--- src/main/java/stringCalculator/StringCalculator.java
+++ src/main/java/stringCalculator/StringCalculator.java
@@ -1,10 +1,18 @@
 package stringCalculator;
 
+import static stringCalculator.IntegerUtils.parsePositiveInt;
+
 import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
+
+    private static final String DEFAULT_DELIMITER = "[,:]";
+    private static final int CUSTOM_DELIMITER_GROUP = 1;
+    private static final int NUMBERS_GROUP = 2;
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+
 
     public int add(String text) {
         if (text == null || text.isEmpty()) {
@@ -12,33 +20,25 @@ public class StringCalculator {
         }
 
         if (text.length() == 1) {
-            return Integer.parseInt(text);
+            return parsePositiveInt(text);
         }
 
         return Arrays.stream(extractNumbersUsingDelimiter(text))
-            .mapToInt(this::parsePositiveInt)
+            .mapToInt(IntegerUtils::parsePositiveInt)
             .sum();
     }
 
     private String[] extractNumbersUsingDelimiter(String text) {
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        Matcher m = CUSTOM_DELIMITER_PATTERN.matcher(text);
         if (hasCustomDelimiter(m)) {
-            final String customDelimiter = m.group(1);
-            return m.group(2).split(customDelimiter);
+            final String customDelimiter = m.group(CUSTOM_DELIMITER_GROUP);
+            return m.group(NUMBERS_GROUP).split(customDelimiter);
         }
-        return text.split(",|:");
+        return text.split(DEFAULT_DELIMITER);
     }
 
     private boolean hasCustomDelimiter(Matcher m) {
         return m.find();
-    }
-
-    private int parsePositiveInt(String text) {
-        int textToInt = Integer.parseInt(text);
-        if (textToInt < 0) {
-            throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
-        }
-        return textToInt;
     }
 
 }
