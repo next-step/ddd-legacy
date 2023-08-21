bab0e8d292d81411d121c1802f052c4a8ee9e44d
mytechnic <mytechnic@gmail.com>
2023-08-21 13:22:21 +0900

    refactor(calculator): RuntimeException 대신 IllegalArgumentException 클래스 사용

--- src/main/java/calculator/PositiveNumber.java
+++ src/main/java/calculator/PositiveNumber.java
@@ -1,21 +1,27 @@
 package calculator;
 
+import java.util.Objects;
+
 public class PositiveNumber {
     private final int number;
 
     public PositiveNumber(int number) {
+        if (number < 0) {
+            throw new IllegalArgumentException("음수를 입력함");
+        }
+
         this.number = number;
     }
 
     public PositiveNumber(String stringNumber) {
         if (!isPositiveNumber(stringNumber)) {
-            throw new RuntimeException("음수를 입력함");
+            throw new IllegalArgumentException("음수를 입력함");
         }
 
         try {
             this.number = Integer.parseInt(stringNumber.trim());
         } catch (NumberFormatException e) {
-            throw new RuntimeException("숫자가 아님");
+            throw new IllegalArgumentException("숫자가 아님");
         }
     }
 
@@ -29,5 +35,18 @@ public class PositiveNumber {
 
     public int getNumber() {
         return this.number;
+    }
+
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        final PositiveNumber positiveNumber = (PositiveNumber) o;
+        return this.number == positiveNumber.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(this.number);
     }
 }
