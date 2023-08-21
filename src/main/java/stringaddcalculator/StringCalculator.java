87430ce9972e90cbcc7ad12e402a79c8d6057ebe
toyablue <137197778+toyablue@users.noreply.github.com>
2023-08-21 16:26:59 +0900

    Step1. 문자열 덧셈 계산기 (#470)
    * feat: 빈 문자열 or null인 경우 0 반환 기능 생성
    
    * fix: google java format jre setting 추가
    
    * feat: 숫자 하나인 문자열인 경우 해당 숫자 반환 기능 추가
    
    * feat: 쉼표/콜론 구분자 기능 추가
    
    * feat: 커스텀 구분자 기능 추가
    
    * feat: 음수 값 입력시 RuntimeException 발생 기능 추가
    
    * feat: 커스텀 구분자시 음수값 테스트 추가
    
    * refactor: add 메서드 코드 리팩토링
    
    * feat: regex single character 수정
    
    * fix: 커스텀 구분자 메서드 에러 수정
    
    * remove: junit 중복 적용 삭제
    
    * fix: 숫자 1개 테스트 강화
    
    * refactor: 매직넘버명이 기능 설명 되도록 수정
    
    * refactor: 상태값 삭제(상태값이 한 메서드에서만 필요하니 global 상태값 필요성이 떨어짐)

---
+++ src/main/java/stringaddcalculator/StringCalculator.java
@@ -0,0 +1,60 @@
+package stringaddcalculator;
+
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+    private static final Pattern pattern = Pattern.compile("//(.)\n(.*)");
+    private static final String regex = "[,|:]";
+    private static final int ZERO = 0;
+    private static final int ONE_LENGTH = 1;
+    private static final int CUSTOM_DELIMITER_CHARACTER = 1;
+    private static final int STRING_WITH_CUSTOM_DELIMITER = 2;
+
+    public int add(String text) {
+        if (Objects.isNull(text) || text.isEmpty()) {
+            return ZERO;
+        }
+
+        Matcher m = pattern.matcher(text);
+        if (m.find()) {
+            return addByUniquePattern(m);
+        }
+
+        String[] tokens = text.split(regex);
+        if (tokens.length == ONE_LENGTH) {
+            return parseOneValue(tokens);
+        }
+        return parseAdd(tokens);
+    }
+
+    private int addByUniquePattern(Matcher m) {
+        String customDelimiter = m.group(CUSTOM_DELIMITER_CHARACTER);
+        String [] tokens = m.group(STRING_WITH_CUSTOM_DELIMITER).split(customDelimiter);
+        return parseAdd(tokens);
+    }
+
+    private int parseOneValue(String[] token) {
+        int value = Integer.parseInt(token[ZERO]);
+        validateMinus(value);
+        return value;
+    }
+
+    private int parseAdd(String [] tokens) {
+        int result = 0;
+
+        for (String value : tokens) {
+            int number = Integer.parseInt(value);
+            validateMinus(number);
+            result += number;
+        }
+        return result;
+    }
+
+    private void validateMinus(int number) {
+        if (number < ZERO) {
+            throw new RuntimeException("음수 값은 사용할 수 없는 값입니다.");
+        }
+    }
+}
