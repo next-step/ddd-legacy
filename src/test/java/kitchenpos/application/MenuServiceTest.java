5351ae9bc22d676190b9371574ac6ec0a72bfb0a
윤영택 <ytaek@gmarket.com>
2023-08-21 23:53:07 +0900

    test: ProductService 테스트 코드 추가

---
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -0,0 +1,16 @@
+//package kitchenpos.application;
+//
+//import kitchenpos.domain.MenuGroupRepository;
+//import kitchenpos.domain.MenuRepository;
+//import kitchenpos.domain.ProductRepository;
+//import kitchenpos.infra.PurgomalumClient;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class MenuServiceTest {
+//    @
+//    private MenuRepository menuRepository;
+//    private MenuGroupRepository menuGroupRepository;
+//    private ProductRepository productRepository;
+//    private PurgomalumClient purgomalumClient;
+//}
---
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -0,0 +1,162 @@
+package kitchenpos.application;
+
+import kitchenpos.domain.*;
+import kitchenpos.infra.PurgomalumClient;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class ProductServiceTest {
+    @Mock
+    private ProductRepository productRepository;
+    @Mock
+    private MenuRepository menuRepository;
+    @Mock
+    private PurgomalumClient purgomalumClient;
+
+    @InjectMocks
+    private ProductService sut;
+
+    private final UUID uuid = UUID.randomUUID();
+
+    @Test
+    void 음식의_가격이_null이면_음식을_생성할_수_없다() {
+        // given
+        Product request = createProduct("햄버거", null);
+
+        // when & then
+        assertThatThrownBy(() -> sut.create(request)).isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 음식의_가격이_0미만이면_음식을_생성할_수_없다() {
+        // given
+        Product request = createProduct("햄버거", new BigDecimal("-1"));
+
+        // when & then
+        assertThatThrownBy(() -> sut.create(request)).isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 음식의_이름이_null이면_음식을_생성할_수_없다() {
+        // given
+        Product request = createProduct(null, new BigDecimal("1000"));
+
+        // when & then
+        assertThatThrownBy(() -> sut.create(request)).isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 음식의_이름에_비속어가_포함되어_있으면_음식을_생성할_수_없다() {
+        // given
+        Product request = createProduct("바보", new BigDecimal("1000"));
+
+        given(purgomalumClient.containsProfanity(any())).willReturn(true);
+
+        // when & then
+        assertThatThrownBy(() -> sut.create(request)).isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 음식을_생성할_수_있다() {
+        // given
+        Product request = createProduct("햄버거", new BigDecimal("1000"));
+
+        given(purgomalumClient.containsProfanity(any())).willReturn(false);
+        given(productRepository.save(any())).willReturn(new Product());
+
+        // when
+        Product result = sut.create(request);
+
+        // then
+        assertThat(result).isExactlyInstanceOf(Product.class);
+    }
+
+    @Test
+    void 음식의_가격이_null이면_음식의_가격을_수정할_수_없다() {
+        // given
+        Product request = createProduct("햄버거", null);
+
+        // when & then
+        assertThatThrownBy(() -> sut.changePrice(uuid, request)).isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 음식의_가격이_0미만이면_음식의_가격을_수정할_수_없다() {
+        // given
+        Product request = createProduct("햄버거", new BigDecimal("-1"));
+
+        // when & then
+        assertThatThrownBy(() -> sut.create(request)).isExactlyInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 음식이_생성되어_있는_상태가_아니라면_가격을_수정할_수_없다() {
+        // given
+        Product request = createProduct("햄버거", new BigDecimal("2000"));
+
+        given(productRepository.findById(any())).willReturn(Optional.empty());
+
+        // when & then
+        assertThatThrownBy(() -> sut.changePrice(uuid, request)).isExactlyInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    void 음식의_가격을_수정할_수_있다() {
+        // given
+        Product request = createProduct("햄버거", new BigDecimal("2000"));
+
+        given(productRepository.findById(any())).willReturn(Optional.of(new Product()));
+
+        // when
+        Product result = sut.changePrice(uuid, request);
+
+        // then
+        assertThat(result).isExactlyInstanceOf(Product.class);
+    }
+
+    @Test
+    void 음식의_가격을_수정했을_때_메뉴의_가격이_음식_가격의_합보다_커진다면_메뉴를_숨김_처리한다() {
+        // given
+        Product request = createProduct("햄버거", new BigDecimal("2000"));
+
+        MenuProduct menuProduct = new MenuProduct();
+        menuProduct.setProduct(request);
+
+        Menu menu = mock(Menu.class);
+        given(menu.getPrice()).willReturn(new BigDecimal("3000"));
+        given(menu.getMenuProducts()).willReturn(List.of(menuProduct));
+
+        given(productRepository.findById(any())).willReturn(Optional.of(new Product()));
+        given(menuRepository.findAllByProductId(any())).willReturn(List.of(menu));
+
+        // when
+        sut.changePrice(UUID.randomUUID(), request);
+
+        // then
+        verify(menu).setDisplayed(false);
+    }
+
+    public static Product createProduct(String name, BigDecimal price) {
+        Product request = new Product();
+        request.setName(name);
+        request.setPrice(price);
+        return request;
+    }
+}
