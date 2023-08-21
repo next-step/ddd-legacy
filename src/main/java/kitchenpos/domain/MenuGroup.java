5674e3323ed84c4d69a58683121d49dd99c862fb
jehyeong <5909264@kbfg.com>
2023-08-21 09:14:25 +0900

    feat : MenuGroup 생성자 및, equals hashCode 추가

--- src/main/java/kitchenpos/domain/MenuGroup.java
+++ src/main/java/kitchenpos/domain/MenuGroup.java
@@ -1,10 +1,12 @@
 package kitchenpos.domain;
 
+import java.util.Objects;
+import java.util.UUID;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
-import java.util.UUID;
 
 @Table(name = "menu_group")
 @Entity
@@ -17,6 +19,26 @@ public class MenuGroup {
     private String name;
 
     public MenuGroup() {
+    }
+
+    public MenuGroup(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (!(o instanceof MenuGroup))
+            return false;
+        MenuGroup menuGroup = (MenuGroup)o;
+        return Objects.equals(id, menuGroup.id) && Objects.equals(name, menuGroup.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 
     public UUID getId() {
