85589801dfedd8f773695045371ee29ac652d616
parkjun5 <fgfg3351@gmail.com>
2023-08-21 18:18:09 +0900

    refactor: polishing

--- src/main/java/calculator/Separator.java
+++ src/main/java/calculator/Separator.java
@@ -2,7 +2,7 @@ package calculator;
 
 public class Separator {
     private Separator() {
-        throw new IllegalStateException("");
+        throw new IllegalStateException("구분자는 생성 될 수 없습니다.");
     }
 
     public static NumberStrings separate(final TargetString targetString) {
