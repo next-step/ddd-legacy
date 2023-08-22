45b95354e974ac796a51ef1225a7dfdf5c6e1b72
Gomding <backchida@gmail.com>
2023-08-22 23:42:51 +0900

    test(kitchenpos): 상품의 가격을 수정할 때도 가격은 0이상의 양수로만 수정가능하다.
    상품의 가격을 수정할 때 가격 값이 0미만의 음수라면 사용자에게 등록 불가능함을 알려야한다.

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -121,4 +121,40 @@ class ProductServiceTest {
         assertThat(result.getName()).isEqualTo("테스트 상품");
         assertThat(result.getPrice()).isEqualTo(BigDecimal.valueOf(2000));
     }
+
+    @DisplayName("상품의 가격을 변경할 때 가격이 null이면 예외가 발생한다.")
+    @Test
+    void changePriceWithNullPrice() {
+        // given
+        Product product = ProductTestFixture.create()
+                .changeId(UUID.randomUUID())
+                .getProduct();
+        productRepository.save(product);
+        Product changePriceRequest = ProductTestFixture.create()
+                .changeId(product.getId())
+                .changePrice(null)
+                .getProduct();
+
+        // when then
+        assertThatThrownBy(() -> sut.changePrice(product.getId(), changePriceRequest))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품의 가격을 변경할 때 가격이 0보다 작으면 예외가 발생한다.")
+    @Test
+    void changePriceWithNegativePrice() {
+        // given
+        Product product = ProductTestFixture.create()
+                .changeId(UUID.randomUUID())
+                .getProduct();
+        productRepository.save(product);
+        Product changePriceRequest = ProductTestFixture.create()
+                .changeId(product.getId())
+                .changePrice(BigDecimal.valueOf(-1))
+                .getProduct();
+
+        // when then
+        assertThatThrownBy(() -> sut.changePrice(product.getId(), changePriceRequest))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
