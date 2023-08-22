f118190fce79f73abc9f1fced33464368a98ed1d
hiblue02 <hiblue02@gmail.com>
2023-08-22 21:33:21 +0900

    refactor(kitchenpos): 상수 static 적용
    - MenuAcceptanceTest

--- src/test/java/kitchenpos/acceptance/MenuAcceptanceTest.java
+++ src/test/java/kitchenpos/acceptance/MenuAcceptanceTest.java
@@ -26,8 +26,8 @@ import static org.junit.jupiter.api.Assertions.assertAll;
 @DisplayName("메뉴")
 public class MenuAcceptanceTest extends AcceptanceTest {
 
-    private final String NAME = "기본메뉴";
-    private final BigDecimal PRICE_1000 = BigDecimal.valueOf(1000);
+    private final static String NAME = "기본메뉴";
+    private final static BigDecimal PRICE_1000 = BigDecimal.valueOf(1000);
 
     private MenuGroup menuGroup;
     private Product product;
