8fc7a4ec87e690e9763caccc5e282542ecbdeae3
Gomding <backchida@gmail.com>
2023-08-22 23:17:39 +0900

    test(kitchenpos): 상품을 등록할 때 이름, 가격은 필수 값이다.

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -11,6 +11,7 @@ import java.math.BigDecimal;
 import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @DisplayName("ProductService 클래스")
 @SpringBootTest
@@ -37,5 +38,45 @@ class ProductServiceTest {
         assertThat(result.getPrice()).isEqualTo(BigDecimal.valueOf(1000));
     }
 
+    @DisplayName("새로운 상품을 등록할 때 가격이 null이면 예외가 발생한다.")
+    @Test
+    void createWithNullPrice() {
+        // given
+        Product product = ProductTestFixture.create()
+                .changeId(null)
+                .changePrice(null)
+                .getProduct();
 
+        // when then
+        assertThatThrownBy(() -> sut.create(product))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("새로운 상품을 등록할 때 가격이 0보다 작으면 예외가 발생한다.")
+    @Test
+    void createWithNegativePrice() {
+        // given
+        Product product = ProductTestFixture.create()
+                .changeId(null)
+                .changePrice(BigDecimal.valueOf(-1))
+                .getProduct();
+
+        // when then
+        assertThatThrownBy(() -> sut.create(product))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("새로운 상품을 등록할 때 이름이 null이면 예외가 발생한다.")
+    @Test
+    void createWithNullName() {
+        // given
+        Product product = ProductTestFixture.create()
+                .changeId(null)
+                .changeName(null)
+                .getProduct();
+
+        // when then
+        assertThatThrownBy(() -> sut.create(product))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 }
