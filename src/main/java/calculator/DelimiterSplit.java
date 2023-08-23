88f9a39ea752574cb09937537fcd50f300a0183a
민경윤(Kyeongyoon Min)/Biz플랫폼개발팀/SKP <ky.min@sk.com>
2023-08-23 16:21:30 +0900

    kitchenpos 요구사항 분석

--- src/main/java/calculator/DelimiterSplit.java
+++ src/main/java/calculator/DelimiterSplit.java
@@ -4,17 +4,16 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class DelimiterSplit {
-    private static final String DEFAULT_DELIMITER = ",|:";
-
-    private static final String CUSTOM_DELIMITER = "//(.)\\n(.*)";
+    private static final Pattern DEFAULT_DELIMITER_PATTERN = Pattern.compile("[,:]");
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
 
     public String[] split(final String text) {
-        Matcher matcher = Pattern.compile(CUSTOM_DELIMITER).matcher(text);
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(text);
 
         if (matcher.find()) {
             return matcher.group(2).split(matcher.group(1));
         } else {
-            return text.split(DEFAULT_DELIMITER);
+            return DEFAULT_DELIMITER_PATTERN.split(text);
         }
     }
 }
