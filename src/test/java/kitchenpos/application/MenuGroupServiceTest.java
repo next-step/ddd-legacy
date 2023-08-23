91db3778036dd35d2ec92c4b5b8c41d4faa17191
jeunghoonsong <thdwmdgns@naver.com>
2023-08-23 12:56:28 +0900

    test: 모든 메뉴그룹의 정보를 조회할수 있다

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -4,11 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 
+import java.util.List;
+import java.util.stream.Stream;
 import kitchenpos.domain.MenuGroup;
 import kitchenpos.testHelper.SpringBootTestHelper;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,6 +53,36 @@ class MenuGroupServiceTest extends SpringBootTestHelper {
         assertAll(
             () -> assertThat(result.getId()).isNotNull(),
             () -> assertThat(result.getName()).isEqualTo(request.getName())
+        );
+    }
+
+    @DisplayName("모든 메뉴그룹의 정보를 조회할수 있다.")
+    @ParameterizedTest
+    @MethodSource("test3MethodSource")
+    void test3(List<String> names) {
+        //given
+        for (String name : names) {
+            MenuGroup request = new MenuGroup(name);
+            menuGroupService.create(request);
+        }
+
+        //when
+        List<MenuGroup> menuGroups = menuGroupService.findAll();
+
+        //then
+        assertAll(
+            () -> assertThat(menuGroups).extracting("name")
+                .containsExactlyElementsOf(names),
+            () -> assertThat(menuGroups).extracting("id")
+                .doesNotContainNull()
+        );
+
+    }
+
+    static Stream<List<String>> test3MethodSource() {
+        return Stream.of(
+            List.of("메뉴그룹1", "MenuGroup2", "메#뉴$그%룹^3"),
+            List.of(" 메 뉴 그 룹 1 ", " 메 뉴 그 룹 2", "메 뉴 그 룹 3")
         );
     }
 }
