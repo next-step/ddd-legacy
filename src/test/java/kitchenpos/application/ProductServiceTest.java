c3b54332664191daa304ffe43beaff05f6913b37
Gomding <backchida@gmail.com>
2023-08-23 22:38:31 +0900

    test(kitchenpos): 상품 가격 변경 시 (변경하려는 상품의 가격 > 해당 상품이 포함된 메뉴의 가격)라면 해당 메뉴는 전시되지 않도록 변경한다.

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -1,7 +1,9 @@
 package kitchenpos.application;
 
-import kitchenpos.domain.Product;
-import kitchenpos.domain.ProductRepository;
+import kitchenpos.domain.*;
+import kitchenpos.test_fixture.MenuGroupTestFixture;
+import kitchenpos.test_fixture.MenuProductTestFixture;
+import kitchenpos.test_fixture.MenuTestFixture;
 import kitchenpos.test_fixture.ProductTestFixture;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -15,6 +17,7 @@ import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 @DisplayName("ProductService 클래스")
 @SpringBootTest
@@ -25,6 +28,16 @@ class ProductServiceTest {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private MenuRepository menuRepository;
+
+    @Autowired
+    private MenuGroupRepository menuGroupRepository;
+
+    @Autowired
+    private EntityManager entityManager;
+
 
     @DisplayName("새로운 상품을 등록할 수 있다.")
     @Test
@@ -173,4 +186,46 @@ class ProductServiceTest {
         assertThatThrownBy(() -> sut.changePrice(id, changePriceRequest))
                 .isInstanceOf(NoSuchElementException.class);
     }
+
+    @DisplayName("상품의 가격을 변경할 때 (변경하려는 상품의 가격 > 해당 상품이 포함된 메뉴의 가격)라면 해당 메뉴는 전시되지 않도록 변경한다.")
+    @Test
+    void changePriceWithMenuPriceHigherThanProductPrice() {
+        // given
+        Product product = ProductTestFixture.create()
+                .changeId(UUID.randomUUID())
+                .getProduct();
+        productRepository.save(product);
+        Menu menu = this.createMenu(product);
+
+        Product changePriceRequest = ProductTestFixture.create()
+                .changeId(product.getId())
+                .changePrice(BigDecimal.valueOf(1999))
+                .getProduct();
+
+        // when
+        sut.changePrice(product.getId(), changePriceRequest);
+
+        // then
+        Menu menuWithProduct = menuRepository.findById(menu.getId()).get();
+        assertFalse(menuWithProduct.isDisplayed());
+    }
+
+    private Menu createMenu(Product product) {
+        MenuGroup menuGroup = MenuGroupTestFixture.create()
+                .changeId(UUID.randomUUID())
+                .getMenuGroup();
+        menuGroupRepository.save(menuGroup);
+        MenuProduct menuProduct = MenuProductTestFixture.create()
+                .changeProduct(product)
+                .getMenuProduct();
+        Menu menu = MenuTestFixture.create()
+                .changeId(UUID.randomUUID())
+                .changePrice(BigDecimal.valueOf(2000))
+                .changeMenuGroup(menuGroup)
+                .changeMenuProducts(menuProduct)
+                .changeDisplayed(true)
+                .getMenu();
+        return menuRepository.save(menu);
+    }
 }
+
