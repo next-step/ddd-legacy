8d62ee5175d5fddd5aba06a476a3fdbfb3dbd863
soosue <yunsoo0510@daum.net>
2023-08-24 17:15:49 +0900

    test: MenuRestController: create, changePrice, findAll

---
+++ src/test/java/kitchenpos/acceptance/MenuAcceptanceTest.java
@@ -0,0 +1,99 @@
+package kitchenpos.acceptance;
+
+import static kitchenpos.acceptance.MenuApi.*;
+import static kitchenpos.acceptance.MenuGroupApi.*;
+import static kitchenpos.acceptance.MenuGroupData.*;
+import static kitchenpos.acceptance.ProductApi.*;
+import static kitchenpos.acceptance.ProductData.*;
+
+import java.util.List;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.web.servlet.MockMvc;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Rollback
+public class MenuAcceptanceTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void 메뉴_생성__성공() throws Exception {
+        MockHttpServletResponse menuGroupCreatedResponse = 메뉴그룹_생성_요청(mockMvc, 추천_메뉴.getValue());
+        MockHttpServletResponse productCreatedResponse = 상품_생성_요청(mockMvc, 강정치킨.getValue());
+        String menuGroupId = extractMenuGroupId(menuGroupCreatedResponse);
+        String productId = extractProductId(productCreatedResponse);
+
+        Map<String, Object> request = Map.of(
+                "name", "후라이드+후라이드",
+                "price", 19000,
+                "menuGroupId", menuGroupId,
+                "displayed", true,
+                "menuProducts", List.of(
+                        Map.of("productId", productId,
+                                "quantity", 2)
+                ));
+
+        MockHttpServletResponse response = 메뉴_생성_요청(mockMvc, request);
+
+        메뉴_생성_성공함(response);
+    }
+
+    @Test
+    void 메뉴_가격_변경__성공() throws Exception {
+        MockHttpServletResponse menuGroupCreatedResponse = 메뉴그룹_생성_요청(mockMvc, 추천_메뉴.getValue());
+        MockHttpServletResponse productCreatedResponse = 상품_생성_요청(mockMvc, 강정치킨.getValue());
+        String menuGroupId = extractMenuGroupId(menuGroupCreatedResponse);
+        String productId = extractProductId(productCreatedResponse);
+
+        Map<String, Object> createRequest = Map.of(
+                "name", "후라이드+후라이드",
+                "price", 19000,
+                "menuGroupId", menuGroupId,
+                "displayed", true,
+                "menuProducts", List.of(
+                        Map.of("productId", productId,
+                                "quantity", 2)
+                ));
+        MockHttpServletResponse createdResponse = 메뉴_생성_요청(mockMvc, createRequest);
+
+        String menuId = extractMenuId(createdResponse);
+        Map<String, Object> request = Map.of("price", 17000);
+
+        MockHttpServletResponse response = 메뉴_가격_변경_요청(mockMvc, menuId, request);
+
+        메뉴_가격_변경_성공함(response);
+    }
+
+    @Test
+    void 메뉴_전체조회() throws Exception {
+        MockHttpServletResponse menuGroupCreatedResponse = 메뉴그룹_생성_요청(mockMvc, 추천_메뉴.getValue());
+        MockHttpServletResponse productCreatedResponse = 상품_생성_요청(mockMvc, 강정치킨.getValue());
+        String menuGroupId = extractMenuGroupId(menuGroupCreatedResponse);
+        String productId = extractProductId(productCreatedResponse);
+
+        Map<String, Object> createRequest = Map.of(
+                "name", "후라이드+후라이드",
+                "price", 19000,
+                "menuGroupId", menuGroupId,
+                "displayed", true,
+                "menuProducts", List.of(
+                        Map.of("productId", productId,
+                                "quantity", 2)
+                ));
+        메뉴_생성_요청(mockMvc, createRequest);
+
+        MockHttpServletResponse response = 메뉴_전체조회_요청(mockMvc);
+
+        메뉴_전체조회_성공함(response, 1);
+    }
+
+}
---
+++ src/test/java/kitchenpos/acceptance/MenuApi.java
@@ -0,0 +1,67 @@
+package kitchenpos.acceptance;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class MenuApi {
+    private static final String API_MENUS_URL = "/api/menus";
+    private static final String API_MENUS_CHANGE_PRICE_URL = "/api/menus/{menuId}/price";
+    private static final String MENU_ID_EXTRACT_PATTERN_FROM_LOCATION = "/api/menus/([a-fA-F0-9-]+)";
+    private static final Pattern MENU_ID_EXTRACT_PATTERN = Pattern.compile(MENU_ID_EXTRACT_PATTERN_FROM_LOCATION);
+    private static final int CAPTURING_MENU_ID_INDEX = 1;
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    public static MockHttpServletResponse 메뉴_생성_요청(MockMvc mockMvc, Map<String, Object> request) throws Exception {
+        return mockMvc.perform(post(API_MENUS_URL)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andReturn().getResponse();
+    }
+
+    public static void 메뉴_생성_성공함(MockHttpServletResponse response) {
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.getHeader("Location")).contains(API_MENUS_URL);
+    }
+
+    public static MockHttpServletResponse 메뉴_가격_변경_요청(MockMvc mockMvc, String menuId, Map<String, Object> request) throws Exception {
+        return mockMvc.perform(put(API_MENUS_CHANGE_PRICE_URL.replace("{menuId}", menuId))
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andReturn().getResponse();
+    }
+
+    public static void 메뉴_가격_변경_성공함(MockHttpServletResponse response) {
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+    }
+
+    public static String extractMenuId(MockHttpServletResponse createdResponse) {
+        Matcher matcher = MENU_ID_EXTRACT_PATTERN.matcher(createdResponse.getHeader("Location"));
+        matcher.find();
+        String menuId = matcher.group(CAPTURING_MENU_ID_INDEX);
+        return menuId;
+    }
+
+    public static MockHttpServletResponse 메뉴_전체조회_요청(MockMvc mockMvc) throws Exception {
+        return mockMvc.perform(get(API_MENUS_URL))
+                .andReturn().getResponse();
+    }
+
+    public static void 메뉴_전체조회_성공함(MockHttpServletResponse response, int expectedSize) throws Exception {
+        assertThat(response.getStatus()).isEqualTo(HttpStatus.OK.value());
+        List<Object> menuGroups = objectMapper.readValue(response.getContentAsString(), List.class);
+        assertThat(menuGroups).hasSize(expectedSize);
+    }
+
+}
