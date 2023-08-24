f9c78a26253f73157b32842a008073fd8a78f33e
Gomding <backchida@gmail.com>
2023-08-24 21:21:38 +0900

    test(kitchenpos): 메뉴 그룹의 이름이 null이거나 빈 문자열일 경우 예외가 발생한다.

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -6,10 +6,13 @@ import kitchenpos.test_fixture.MenuGroupTestFixture;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("MenuGroupService 클래스")
 @SpringBootTest
@@ -38,5 +41,19 @@ class MenuGroupServiceTest {
         assertThat(result).isNotNull();
         assertThat(result.getId()).isNotNull();
         assertThat(result.getName()).isEqualTo("테스트 메뉴 그룹");
+    }
+
+    @DisplayName("메뉴 그룹의 이름이 null이거나 빈 문자열일 경우 예외가 발생한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void createWithInvalidName(String name) {
+        // given
+        MenuGroup menuGroup = MenuGroupTestFixture.create()
+                .changeId(null)
+                .changeName(name)
+                .getMenuGroup();
+
+        // when & then
+        assertThrows(IllegalArgumentException.class, () -> sut.create(menuGroup));
     }
 }
