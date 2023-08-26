fe0be9d1d9a86e86ac11f17b60ff0c61afbf8a9c
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2023-08-26 15:15:05 +0900

    test: add TEST static Fixture

--- src/test/java/kitchenpos/fixture/TestFixture.java
+++ src/test/java/kitchenpos/fixture/TestFixture.java
@@ -1,17 +1,16 @@
 package kitchenpos.fixture;
 
-import kitchenpos.domain.Menu;
-import kitchenpos.domain.MenuGroup;
-import kitchenpos.domain.MenuProduct;
-import kitchenpos.domain.Product;
+import kitchenpos.domain.*;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.UUID;
 
 public class TestFixture {
     public static MenuGroup TEST_MENU_GROUP() {
         MenuGroup menuGroup = new MenuGroup();
         menuGroup.setName("테스트 메뉴 그룹");
+        menuGroup.setId(UUID.randomUUID());
         return menuGroup;
     }
 
@@ -19,15 +18,21 @@ public class TestFixture {
         Menu menu = new Menu();
         menu.setName("테스트 메뉴");
         menu.setPrice(new BigDecimal(10_00));
-        menu.setMenuGroup(TEST_MENU_GROUP());
+        MenuGroup menuGroup = TEST_MENU_GROUP();
+        menu.setMenuGroup(menuGroup);
         menu.setDisplayed(true);
         menu.setMenuProducts(List.of(TEST_MENU_PRODUCT()));
+        menu.setMenuGroupId(menuGroup.getId());
+        menu.setId(UUID.randomUUID());
         return menu;
     }
 
     public static MenuProduct TEST_MENU_PRODUCT() {
         MenuProduct menuProduct = new MenuProduct();
-        menuProduct.setQuantity(1);
+        menuProduct.setQuantity(3);
+        Product product = TEST_PRODUCT();
+        menuProduct.setProductId(product.getId());
+        menuProduct.setProduct(product);
         return menuProduct;
     }
 
@@ -35,6 +40,16 @@ public class TestFixture {
         Product product = new Product();
         product.setPrice(new BigDecimal(5_000));
         product.setName("핏자");
+        product.setId(UUID.randomUUID());
         return product;
+    }
+
+    public static OrderTable TEST_ORDER_TABLE() {
+        OrderTable orderTable = new OrderTable();
+        orderTable.setId(UUID.randomUUID());
+        orderTable.setOccupied(false);
+        orderTable.setName("1번 테이블");
+        orderTable.setNumberOfGuests(0);
+        return orderTable;
     }
 }
