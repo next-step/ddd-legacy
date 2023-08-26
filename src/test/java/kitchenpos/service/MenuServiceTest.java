2c82a82d820234657a04c0e0045fd87a71e85c06
soo <yunsoo0510@daum.net>
2023-08-26 15:16:22 +0900

    test: MenuService: check menu-product null and empty

--- src/test/java/kitchenpos/service/MenuServiceTest.java
+++ src/test/java/kitchenpos/service/MenuServiceTest.java
@@ -1,17 +1,32 @@
 package kitchenpos.service;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import kitchenpos.application.MenuService;
 import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuGroupRepository;
 
+@ExtendWith(MockitoExtension.class)
 public class MenuServiceTest {
 
-    private final MenuService menuService = new MenuService(null, null, null, null);
+    @Mock
+    private MenuGroupRepository menuGroupRepository;
+
+    @InjectMocks
+    private MenuService menuService;
 
     @Test
     void 메뉴_생성_실패__가격이_null() {
@@ -26,6 +41,27 @@ public class MenuServiceTest {
     void 메뉴_생성_실패__가격이_음수() {
         Menu menu = new Menu();
         menu.setPrice(new BigDecimal(-1));
+
+        assertThatThrownBy(() -> menuService.create(menu))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 메뉴_생성_실패__메뉴상품이_null() {
+        when(menuGroupRepository.findById(any())).thenReturn(Optional.of(new MenuGroup()));
+        Menu menu = new Menu();
+        menu.setPrice(new BigDecimal(0));
+
+        assertThatThrownBy(() -> menuService.create(menu))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 메뉴_생성_실패__메뉴상품이_0개() {
+        when(menuGroupRepository.findById(any())).thenReturn(Optional.of(new MenuGroup()));
+        Menu menu = new Menu();
+        menu.setPrice(new BigDecimal(0));
+        menu.setMenuProducts(List.of());
 
         assertThatThrownBy(() -> menuService.create(menu))
                 .isInstanceOf(IllegalArgumentException.class);
