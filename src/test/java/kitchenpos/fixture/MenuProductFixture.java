842cc20d8444ff17a0486a7d59731d7964413491
jordy-torvalds <jordy-torvalds@kakao.com>
2023-08-26 12:14:12 +0900

    test(product): 상품 가격 수정에 대한 테스트를 작성합니다.

---
+++ src/test/java/kitchenpos/fixture/MenuProductFixture.java
@@ -0,0 +1,22 @@
+package kitchenpos.fixture;
+
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuProduct;
+import kitchenpos.domain.Product;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class MenuProductFixture {
+
+    private MenuProductFixture() {
+    }
+
+    public static MenuProduct create(Product product, int quantity) {
+        MenuProduct result = new MenuProduct();
+        result.setProduct(product);
+        result.setQuantity(quantity);
+        return result;
+    }
+
+}
