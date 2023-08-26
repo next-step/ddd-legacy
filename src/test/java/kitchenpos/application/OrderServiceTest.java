c734fd30bb0eec02dab85a39c6cc9e97b704137a
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2023-08-26 19:09:31 +0900

    test: add order service all process

--- src/test/java/kitchenpos/application/OrderServiceTest.java
+++ src/test/java/kitchenpos/application/OrderServiceTest.java
@@ -17,7 +17,7 @@ import java.util.Optional;
 import static kitchenpos.fixture.TestFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -229,5 +229,255 @@ class OrderServiceTest {
         // then
         assertThatThrownBy(() -> orderService.create(order))
                 .isExactlyInstanceOf(IllegalStateException.class);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"take_out", "eat_in"})
+    void 테이크아웃과_매장_주문을_수락한다(String typeName) {
+        // given
+        Order order = getOrderByTypeName(typeName);
+        given(orderRepository.findById(order.getId())).willReturn(Optional.of(order));
+
+        // when
+        Order result = orderService.accept(order.getId());
+
+        // then
+        verify(orderRepository, times(1)).findById(order.getId());
+        assertThat(result.getStatus()).isEqualTo(OrderStatus.ACCEPTED);
+    }
+
+    @Test
+    void 수락하려는_주문의_상태가_대기중이어야_한다() {
+        // given
+        Order order = TEST_ORDER_EAT_IN();
+
+        // when
+        order.setStatus(OrderStatus.COMPLETED);
+        given(orderRepository.findById(order.getId())).willReturn(Optional.of(order));
+
+        // then
+        assertThatThrownBy(() -> orderService.accept(order.getId()))
+                .isExactlyInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void 배달_주문이라면_배달을_요청한다() {
+        // given
+        Order order = TEST_ORDER_DELIVERY();
+        given(orderRepository.findById(order.getId())).willReturn(Optional.of(order));
+
+        // when
+        orderService.accept(order.getId());
+
+        // then
+        verify(orderRepository, times(1)).findById(order.getId());
+        verify(kitchenridersClient, times(1))
+                .requestDelivery(eq(order.getId()),any(BigDecimal.class), anyString());
+    }
+
+    @Test
+    void 주문을_조리_완료하고_제공_상태로_변경한다() {
+        // given
+        Order order = TEST_ORDER_DELIVERY();
+        order.setStatus(OrderStatus.ACCEPTED);
+        given(orderRepository.findById(order.getId())).willReturn(Optional.of(order));
+
+        // when
+        Order result = orderService.serve(order.getId());
+
+        // then
+        verify(orderRepository, times(1)).findById(order.getId());
+        assertThat(result.getStatus()).isEqualTo(OrderStatus.SERVED);
+    }
+
+    @Test
+    void 주문의_상태가_수락된_상태이어야_한다() {
+        // given
+        Order order = TEST_ORDER_EAT_IN();
+
+        // when
+        order.setStatus(OrderStatus.COMPLETED);
+        given(orderRepository.findById(order.getId())).willReturn(Optional.of(order));
+
+        // then
+        assertThatThrownBy(() -> orderService.serve(order.getId()))
+                .isExactlyInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void 주문의_배달을_시작한다() {
+        // given
+        Order order = TEST_ORDER_DELIVERY();
+        order.setStatus(OrderStatus.SERVED);
+        given(orderRepository.findById(order.getId())).willReturn(Optional.of(order));
+
+        // when
+        Order result = orderService.startDelivery(order.getId());
+
+        // then
+        verify(orderRepository, times(1)).findById(order.getId());
+        assertThat(result.getStatus()).isEqualTo(OrderStatus.DELIVERING);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"take_out", "eat_in"})
+    void 주문_타입이_배달이어야_한다(String typeName) {
+        // given
+        Order order = getOrderByTypeName(typeName);
+
+        // when
+        given(orderRepository.findById(order.getId())).willReturn(Optional.of(order));
+
+        // then
+        assertThatThrownBy(() -> orderService.startDelivery(order.getId()))
+                .isExactlyInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void 주문_상태가_제공_상태이어야_한다() {
+        // given
+        Order order = TEST_ORDER_DELIVERY();
+
+        // when
+        order.setStatus(OrderStatus.COMPLETED);
+        given(orderRepository.findById(order.getId())).willReturn(Optional.of(order));
+
+        // then
+        assertThatThrownBy(() -> orderService.startDelivery(order.getId()))
+                .isExactlyInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void 주문의_배달을_완료한다() {
+        // given
+        Order order = TEST_ORDER_DELIVERY();
+        order.setStatus(OrderStatus.DELIVERING);
+        given(orderRepository.findById(order.getId())).willReturn(Optional.of(order));
+
+        // when
+        Order result = orderService.completeDelivery(order.getId());
+
+        // then
+        verify(orderRepository, times(1)).findById(order.getId());
+        assertThat(result.getStatus()).isEqualTo(OrderStatus.DELIVERED);
+    }
+
+    @Test
+    void 주문의_상태가_배달중이어야_한다() {
+        // given
+        Order order = TEST_ORDER_DELIVERY();
+
+        // when
+        order.setStatus(OrderStatus.COMPLETED);
+        given(orderRepository.findById(order.getId())).willReturn(Optional.of(order));
+
+        // then
+        assertThatThrownBy(() -> orderService.completeDelivery(order.getId()))
+                .isExactlyInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void 배달_주문을_완료한다() {
+        // given
+        Order order = TEST_ORDER_DELIVERY();
+        order.setStatus(OrderStatus.DELIVERED);
+        given(orderRepository.findById(order.getId())).willReturn(Optional.of(order));
+
+        // when
+        Order result = orderService.complete(order.getId());
+
+        // then
+        verify(orderRepository, times(1)).findById(order.getId());
+        assertThat(result.getStatus()).isEqualTo(OrderStatus.COMPLETED);
+    }
+
+    @Test
+    void 주문이_배달이라면_주문_상태가_배달_완료이어야한다() {
+        // given
+        Order order = TEST_ORDER_DELIVERY();
+
+        // when
+        order.setStatus(OrderStatus.COMPLETED);
+        given(orderRepository.findById(order.getId())).willReturn(Optional.of(order));
+
+        // then
+        assertThatThrownBy(() -> orderService.complete(order.getId()))
+                .isExactlyInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void 매장_주문을_완료하면서_테이블도_정리한다() {
+        // given
+        Order order = TEST_ORDER_EAT_IN();
+        order.setStatus(OrderStatus.SERVED);
+        given(orderRepository.findById(order.getId())).willReturn(Optional.of(order));
+
+        // when
+        Order result = orderService.complete(order.getId());
+
+        // then
+        verify(orderRepository, times(1)).findById(order.getId());
+        verify(orderRepository, times(1))
+                .existsByOrderTableAndStatusNot(order.getOrderTable(), OrderStatus.COMPLETED);
+        assertThat(result.getStatus()).isEqualTo(OrderStatus.COMPLETED);
+    }
+
+    @Test
+    void 포장_주문을_완료한다() {
+        // given
+        Order order = TEST_ORDER_TAKEOUT();
+        order.setStatus(OrderStatus.SERVED);
+        given(orderRepository.findById(order.getId())).willReturn(Optional.of(order));
+
+        // when
+        Order result = orderService.complete(order.getId());
+
+        // then
+        verify(orderRepository, times(1)).findById(order.getId());
+        assertThat(result.getStatus()).isEqualTo(OrderStatus.COMPLETED);
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"take_out", "eat_in"})
+    void 주문이_매장이거나_포장이라면_주문_상태가_제공된_상태이여야한다(String typeName) {
+        // given
+        Order order = getOrderByTypeName(typeName);
+
+        // when
+        order.setStatus(OrderStatus.COMPLETED);
+        given(orderRepository.findById(order.getId())).willReturn(Optional.of(order));
+
+        // then
+        assertThatThrownBy(() -> orderService.complete(order.getId()))
+                .isExactlyInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void 모든_주문_정보를_가져온다() {
+        // given
+        Order delivery = TEST_ORDER_DELIVERY();
+        Order takeout = TEST_ORDER_TAKEOUT();
+        Order eat_in = TEST_ORDER_EAT_IN();
+        given(orderRepository.findAll()).willReturn(List.of(delivery, takeout, eat_in));
+
+        // when
+        List<Order> result = orderService.findAll();
+
+        // then
+        verify(orderRepository, times(1)).findAll();
+        assertThat(result).containsExactly(delivery, takeout, eat_in);
+    }
+
+    private Order getOrderByTypeName(String typeName) {
+        Order order;
+        switch (typeName) {
+            case "take_out":
+                order = TEST_ORDER_TAKEOUT();
+                break;
+            case "eat_in":
+            default:
+                order = TEST_ORDER_EAT_IN();
+        }
+        return order;
     }
 }
