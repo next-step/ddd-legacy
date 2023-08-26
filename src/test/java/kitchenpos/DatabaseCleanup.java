3ba832988a63824b50bb91f1a4d530920b922bfd
jordy-torvalds <jordy-torvalds@kakao.com>
2023-08-26 12:30:13 +0900

    test: 인수 테스트를 위한 환경 구성을 한다

---
+++ src/test/java/kitchenpos/DatabaseCleanup.java
@@ -0,0 +1,40 @@
+package kitchenpos;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Entity;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Table;
+import javax.transaction.Transactional;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+public class DatabaseCleanup implements InitializingBean {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private List<String> tableNames;
+
+    @Override
+    public void afterPropertiesSet() {
+        tableNames = entityManager.getMetamodel().getEntities().stream()
+                .filter(entity -> entity.getJavaType().getAnnotation(Entity.class) != null)
+                .map(entity -> entity.getJavaType().getAnnotation(Table.class).name())
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void execute() {
+        entityManager.flush();
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE").executeUpdate();
+        for (String tableName : tableNames) {
+            entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
+        }
+        entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
+    }
+
+}
---
+++ src/test/java/kitchenpos/ui/ProductRestControllerTest.java
@@ -0,0 +1,8 @@
+package kitchenpos.ui;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductRestControllerTest {
+
+
+}
