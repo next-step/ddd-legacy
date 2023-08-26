9d47b821bfa427e0fb0947e04e328dba1575ca9c
soo <yunsoo0510@daum.net>
2023-08-26 15:26:40 +0900

    test: MenuService: check menu-product of create request exists

--- src/test/java/kitchenpos/service/MenuServiceTest.java
+++ src/test/java/kitchenpos/service/MenuServiceTest.java
@@ -18,12 +18,17 @@ import kitchenpos.application.MenuService;
 import kitchenpos.domain.Menu;
 import kitchenpos.domain.MenuGroup;
 import kitchenpos.domain.MenuGroupRepository;
+import kitchenpos.domain.MenuProduct;
+import kitchenpos.domain.ProductRepository;
 
 @ExtendWith(MockitoExtension.class)
 public class MenuServiceTest {
 
     @Mock
     private MenuGroupRepository menuGroupRepository;
+
+    @Mock
+    private ProductRepository productRepository;
 
     @InjectMocks
     private MenuService menuService;
@@ -62,6 +67,17 @@ public class MenuServiceTest {
         Menu menu = new Menu();
         menu.setPrice(new BigDecimal(0));
         menu.setMenuProducts(List.of());
+
+        assertThatThrownBy(() -> menuService.create(menu))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 메뉴_생성_실패__메뉴_생성_요청의_메뉴상품이_존재하지_않음() {
+        when(menuGroupRepository.findById(any())).thenReturn(Optional.of(new MenuGroup()));
+        Menu menu = new Menu();
+        menu.setPrice(new BigDecimal(0));
+        menu.setMenuProducts(List.of(new MenuProduct()));
 
         assertThatThrownBy(() -> menuService.create(menu))
                 .isInstanceOf(IllegalArgumentException.class);
