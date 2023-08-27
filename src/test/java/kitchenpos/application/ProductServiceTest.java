88fca02ebc69dd60c9f251fb4f97a4ea3bb6e0b6
junlee <laonzenamoon@gmail.com>
2023-08-27 22:30:50 +0900

    test(#step3): 새로운 상품 등록 기능 > 상품명 길이 조건 테스트 코드 추가

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -2,16 +2,17 @@ package kitchenpos.application;
 
 import kitchenpos.domain.Product;
 import kitchenpos.helper.ProductHelper;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 
 import java.math.BigDecimal;
 
+import static kitchenpos.helper.ProductHelper.DEFAULT_PRICE;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
@@ -68,6 +69,49 @@ class ProductServiceTest extends ApplicationTest {
                 // Then
                 assertThatThrownBy(() -> productService.create(product))
                         .isInstanceOf(IllegalArgumentException.class);
+            }
+        }
+
+        @DisplayName("상품명은 비어있을 수 없고, 255자를 초과할 수 없다.")
+        @Nested
+        class Policy2 {
+            @DisplayName("상품명이 0자 이상 255자 이하인 경우 (성공)")
+            @ParameterizedTest
+            @ValueSource(strings = {"", "한", "a", "1", "상품명", "product name", "상품 A", "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one."})
+            void success1(final String name) {
+                // Given
+                Product product = ProductHelper.create(name);
+
+                // When
+                Product createdProduct = productService.create(product);
+
+                // Then
+                assertThat(createdProduct.getName()).isEqualTo(name);
+                assertThat(createdProduct.getPrice()).isEqualTo(DEFAULT_PRICE);
+            }
+
+            @DisplayName("상품명이 null 인 경우 (실패)")
+            @ParameterizedTest
+            @NullSource
+            void fail1(final String name) {
+                // When
+                Product product = ProductHelper.create(name);
+
+                // Then
+                assertThatThrownBy(() -> productService.create(product))
+                        .isInstanceOf(IllegalArgumentException.class);
+            }
+
+            @DisplayName("상품명이 255자를 초과한 경우 (실패)")
+            @ParameterizedTest
+            @ValueSource(strings = {"Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one ."})
+            void fail2(final String name) {
+                // When
+                Product product = ProductHelper.create(name);
+
+                // Then
+                assertThatThrownBy(() -> productService.create(product))
+                        .isInstanceOf(DataIntegrityViolationException.class);
             }
         }
 
--- src/test/java/kitchenpos/helper/ProductHelper.java
+++ src/test/java/kitchenpos/helper/ProductHelper.java
@@ -8,13 +8,18 @@ import java.util.UUID;
 
 public final class ProductHelper {
 
-    private static final String DEFAULT_NAME = "테스트 기본 상품명";
+    public static final String DEFAULT_NAME = "테스트 기본 상품명";
+    public static final BigDecimal DEFAULT_PRICE = new BigDecimal(1000);
 
     private ProductHelper() {
     }
 
     public static Product create(BigDecimal price) {
         return create(DEFAULT_NAME, price);
+    }
+
+    public static Product create(String name) {
+        return create(name, DEFAULT_PRICE);
     }
 
     public static Product create(String name, BigDecimal price) {
