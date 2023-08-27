058ab0c2fb7591b4a84009139afeecdfea6dc280
jeunghoonsong <thdwmdgns@naver.com>
2023-08-27 21:37:10 +0900

    test: Order 요구사항에 대한 테스트 구현

---
+++ src/test/java/kitchenpos/application/OrderServiceTest.java
@@ -0,0 +1,470 @@
+package kitchenpos.application;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.math.BigDecimal;
+import java.util.List;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderStatus;
+import kitchenpos.domain.OrderTable;
+import kitchenpos.domain.OrderType;
+import kitchenpos.testHelper.SpringBootTestHelper;
+import kitchenpos.testHelper.fixture.OrderFixture;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class OrderServiceTest extends SpringBootTestHelper {
+
+    @Autowired
+    OrderService orderService;
+    @Autowired
+    MenuService menuService;
+    @Autowired
+    OrderTableService orderTableService;
+    List<Menu> menus;
+    OrderTable orderTable;
+
+    @BeforeEach
+    public void init() {
+        super.init();
+        super.initProduct();
+        super.initMenuGroup();
+        super.initOrderTable();
+        super.initMenu();
+
+        this.menus = super.getMenus();
+        this.orderTable = super.getOrderTable();
+    }
+
+    @DisplayName("배달주문을 등록할수 있다. 주문이 되면 ID가 설정되고, '대기중' 상태가 된다")
+    @Test
+    void test1() {
+        //given
+        Order createRequest = OrderFixture.createDeliveryOrderRequestBuilder()
+            .deliveryAddress("배달 주소")
+            .menu(menus.get(0), 1L, menus.get(0).getPrice())
+            .type(OrderType.DELIVERY)
+            .build();
+
+        //when
+        Order result = orderService.create(createRequest);
+
+        //then
+        assertAll(
+            () -> assertThat(result.getId()).isNotNull(),
+            () -> assertThat(result.getStatus()).isEqualTo(OrderStatus.WAITING)
+        );
+    }
+
+    @DisplayName("매장주문을 등록할수 있다")
+    @ParameterizedTest
+    @ValueSource(longs = {-1, 0, 1, 2, 3, 4, 5})
+    void test2(long quantity) {
+        //given
+        Order createRequest = OrderFixture.createEatInOrderRequestBuilder()
+            .menu(menus.get(0), quantity, menus.get(0).getPrice())
+            .orderTable(orderTable.getId())
+            .type(OrderType.EAT_IN)
+            .build();
+
+        //when
+        Order result = orderService.create(createRequest);
+
+        //then
+        assertThat(result.getId()).isNotNull();
+    }
+
+    @DisplayName("포장주문을 등록할수 있다")
+    @Test
+    void test3() {
+        //given
+        Order createRequest = OrderFixture.createTakeOutOrderCreateRequestBuilder()
+            .menu(menus.get(0), 1L, menus.get(0).getPrice())
+            .type(OrderType.TAKEOUT)
+            .build();
+
+        //when
+        Order result = orderService.create(createRequest);
+
+        //then
+        assertThat(result.getId()).isNotNull();
+    }
+
+    @DisplayName("주문을 등록할때에는 반드시 주문 유형이 있어야 한다")
+    @Test
+    void test4() {
+        //given
+        Order createRequest = OrderFixture.createTakeOutOrderCreateRequestBuilder()
+            .menu(menus.get(0), 1L, menus.get(0).getPrice())
+            .build();
+
+        //when && then
+        assertThatThrownBy(
+            () -> orderService.create(createRequest)
+        ).isInstanceOf(IllegalArgumentException.class);
+
+    }
+
+    @DisplayName("주문을 등록할때에는 최소 하나 이상의 메뉴를 등록해야 한다")
+    @Test
+    void test5() {
+        //given
+        Order createRequest = OrderFixture.createTakeOutOrderCreateRequestBuilder()
+            .type(OrderType.TAKEOUT)
+            .build();
+
+        //when && then
+        assertThatThrownBy(
+            () -> orderService.create(createRequest)
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("노출되지 않은 메뉴는 주문할수 없다")
+    @Test
+    void test6() {
+        //given
+        Menu orderMenu = menus.get(0);
+        menuService.hide(orderMenu.getId());
+        Order createRequest = OrderFixture.createTakeOutOrderCreateRequestBuilder()
+            .menu(orderMenu, 1L, orderMenu.getPrice())
+            .type(OrderType.TAKEOUT)
+            .build();
+
+        //when && then
+        assertThatThrownBy(
+            () -> orderService.create(createRequest)
+        ).isInstanceOf(IllegalStateException.class);
+    }
+
+    @DisplayName("저장된 메뉴의 가격과 주문한 메뉴의 가격이 다를수 없다")
+    @Test
+    void test7() {
+        //given
+        Menu orderMenu = menus.get(0);
+        Order createRequest = OrderFixture.createTakeOutOrderCreateRequestBuilder()
+            .menu(orderMenu, 1L, orderMenu.getPrice().add(BigDecimal.ONE))
+            .type(OrderType.TAKEOUT)
+            .build();
+
+        //when && then
+        assertThatThrownBy(
+            () -> orderService.create(createRequest)
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+    @DisplayName("'배달주문'과 '포장주문'이라면 메뉴 별로 0개 이상 주문해야 한다")
+    @Test
+    void test9() {
+        //given
+        Menu orderMenu = menus.get(0);
+        long quantity = -1L;
+        Order takeOutOrderRequest = OrderFixture.createTakeOutOrderCreateRequestBuilder()
+            .menu(orderMenu, quantity, orderMenu.getPrice())
+            .type(OrderType.TAKEOUT)
+            .build();
+        Order deliveryOrderRequest = OrderFixture.createDeliveryOrderRequestBuilder()
+            .menu(orderMenu, quantity, orderMenu.getPrice())
+            .type(OrderType.DELIVERY)
+            .deliveryAddress("address")
+            .build();
+
+        //when && then
+        assertAll(
+            () -> assertThatThrownBy(
+                () -> orderService.create(takeOutOrderRequest)
+            ).isInstanceOf(IllegalArgumentException.class),
+            () -> assertThatThrownBy(
+                () -> orderService.create(deliveryOrderRequest)
+            ).isInstanceOf(IllegalArgumentException.class)
+        );
+
+    }
+
+    @DisplayName("배달주문은 배달주소가 반드시 있어야 한다")
+    @Test
+    void test10() {
+        //given
+        Menu orderMenu = menus.get(0);
+        Order createRequest = OrderFixture.createDeliveryOrderRequestBuilder()
+            .menu(orderMenu, 1L, orderMenu.getPrice())
+            .type(OrderType.DELIVERY)
+            .build();
+
+        //when && then
+        assertThatThrownBy(
+            () -> orderService.create(createRequest)
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("매장식사 주문은 사용중인 테이블에서만 주문할수 있다.")
+    @Test
+    void test11() {
+        //given
+        Menu orderMenu = menus.get(0);
+        orderTableService.clear(orderTable.getId());
+        Order createRequest = OrderFixture.createEatInOrderRequestBuilder()
+            .menu(orderMenu, 1L, orderMenu.getPrice())
+            .orderTable(orderTable.getId())
+            .type(OrderType.EAT_IN)
+            .build();
+
+        //when && then
+        assertThatThrownBy(
+            () -> orderService.create(createRequest)
+        ).isInstanceOf(IllegalStateException.class);
+    }
+
+
+    @DisplayName("매장 식사 주문 접수시 '접수 완료'상태로 변경되어야 한다")
+    @Test
+    void test12() {
+        //given
+        Menu orderMenu = menus.get(0);
+        Order order = orderService.create(
+            OrderFixture.createEatInOrderRequestBuilder()
+                .menu(orderMenu, 1L, orderMenu.getPrice())
+                .orderTable(orderTable.getId())
+                .type(OrderType.EAT_IN)
+                .build()
+        );
+        //when
+        Order result = orderService.accept(order.getId());
+
+        //then
+        assertThat(result.getStatus()).isEqualTo(OrderStatus.ACCEPTED);
+    }
+
+    @DisplayName("포장 주문 접수시 '접수 완료'상태로 변경되어야 한다")
+    @Test
+    void test13() {
+        //given
+        Menu orderMenu = menus.get(0);
+        Order order = orderService.create(
+            OrderFixture.createTakeOutOrderCreateRequestBuilder()
+                .menu(orderMenu, 1L, orderMenu.getPrice())
+                .type(OrderType.TAKEOUT)
+                .build()
+        );
+        //when
+        Order result = orderService.accept(order.getId());
+
+        //then
+        assertThat(result.getStatus()).isEqualTo(OrderStatus.ACCEPTED);
+    }
+
+    @DisplayName("배달 주문 접수시 '접수 완료'상태로 변경되어야 한다")
+    @Test
+    void test14() {
+        //given
+        Menu orderMenu = menus.get(0);
+        Order order = orderService.create(
+            OrderFixture.createDeliveryOrderRequestBuilder()
+                .menu(orderMenu, 1L, orderMenu.getPrice())
+                .deliveryAddress("address")
+                .type(OrderType.DELIVERY)
+                .build()
+        );
+        //when
+        Order result = orderService.accept(order.getId());
+
+        //then
+        assertThat(result.getStatus()).isEqualTo(OrderStatus.ACCEPTED);
+    }
+
+    @DisplayName("매장 식사 주문 서빙시 '서빙중' 상태로 변경되어야 한다")
+    @Test
+    void test15() {
+        //given
+        Menu orderMenu = menus.get(0);
+        Order order = orderService.create(
+            OrderFixture.createEatInOrderRequestBuilder()
+                .menu(orderMenu, 1L, orderMenu.getPrice())
+                .orderTable(orderTable.getId())
+                .type(OrderType.EAT_IN)
+                .build()
+        );
+        orderService.accept(order.getId());
+
+        //when
+        Order result = orderService.serve(order.getId());
+
+        //then
+        assertThat(result.getStatus()).isEqualTo(OrderStatus.SERVED);
+    }
+
+    @DisplayName("포장 주문 서빙시 '서빙중' 상태로 변경되어야 한다")
+    @Test
+    void test16() {
+        //given
+        Menu orderMenu = menus.get(0);
+        Order order = orderService.create(
+            OrderFixture.createTakeOutOrderCreateRequestBuilder()
+                .menu(orderMenu, 1L, orderMenu.getPrice())
+                .type(OrderType.TAKEOUT)
+                .build()
+        );
+        orderService.accept(order.getId());
+
+        //when
+        Order result = orderService.serve(order.getId());
+
+        //then
+        assertThat(result.getStatus()).isEqualTo(OrderStatus.SERVED);
+    }
+
+    @DisplayName("배달 주문 서빙시 '서빙중' 상태로 변경되어야 한다")
+    @Test
+    void test17() {
+        //given
+        Menu orderMenu = menus.get(0);
+        Order order = orderService.create(
+            OrderFixture.createDeliveryOrderRequestBuilder()
+                .menu(orderMenu, 1L, orderMenu.getPrice())
+                .deliveryAddress("address")
+                .type(OrderType.DELIVERY)
+                .build()
+        );
+        orderService.accept(order.getId());
+
+        //when
+        Order result = orderService.serve(order.getId());
+
+        //then
+        assertThat(result.getStatus()).isEqualTo(OrderStatus.SERVED);
+    }
+
+    @DisplayName("배달 주문 시작시 '배달 중' 상태로 변경되어야 한다")
+    @Test
+    void test18() {
+        //given
+        Menu orderMenu = menus.get(0);
+        Order order = orderService.create(
+            OrderFixture.createDeliveryOrderRequestBuilder()
+                .menu(orderMenu, 1L, orderMenu.getPrice())
+                .deliveryAddress("address")
+                .type(OrderType.DELIVERY)
+                .build()
+        );
+        orderService.accept(order.getId());
+        orderService.serve(order.getId());
+
+        //when
+        Order result = orderService.startDelivery(order.getId());
+
+        //then
+        assertThat(result.getStatus()).isEqualTo(OrderStatus.DELIVERING);
+    }
+
+    @DisplayName("배달 주문 완료시 '배달 완료' 상태로 변경되어야 한다")
+    @Test
+    void test19() {
+        //given
+        Menu orderMenu = menus.get(0);
+        Order order = orderService.create(
+            OrderFixture.createDeliveryOrderRequestBuilder()
+                .menu(orderMenu, 1L, orderMenu.getPrice())
+                .deliveryAddress("address")
+                .type(OrderType.DELIVERY)
+                .build()
+        );
+        orderService.accept(order.getId());
+        orderService.serve(order.getId());
+        orderService.startDelivery(order.getId());
+
+        //when
+        Order result = orderService.completeDelivery(order.getId());
+
+        //then
+        assertThat(result.getStatus()).isEqualTo(OrderStatus.DELIVERED);
+    }
+
+    @DisplayName("매장 식사 주문을 완료처리 할수 있다. 처리 후 테이블 내 오더가 모두 완료시 테이블이 초기화되어야 한다")
+    @Test
+    void test20() {
+        //given
+        Menu orderMenu = menus.get(0);
+        Order order = orderService.create(
+            OrderFixture.createEatInOrderRequestBuilder()
+                .menu(orderMenu, 1L, orderMenu.getPrice())
+                .orderTable(orderTable.getId())
+                .type(OrderType.EAT_IN)
+                .build()
+        );
+        orderService.accept(order.getId());
+        orderService.serve(order.getId());
+
+        //when
+        Order result = orderService.complete(order.getId());
+        orderTable = orderTableService.findAll().get(0);
+
+        //then
+        assertAll(
+            () -> assertThat(orderTable.isOccupied()).isFalse(),
+            () -> assertThat(result.getStatus()).isEqualTo(OrderStatus.COMPLETED)
+        );
+
+    }
+
+    @DisplayName("매장 식사 주문을 완료처리 할수 있다. 처리 후 테이블 내 완료되지 않은 오더가 존재시 테이블은 초기화되면 안된다")
+    @Test
+    void test21() {
+        //given
+        Menu orderMenu = menus.get(0);
+        Order order1 = orderService.create(
+            OrderFixture.createEatInOrderRequestBuilder()
+                .menu(orderMenu, 1L, orderMenu.getPrice())
+                .orderTable(orderTable.getId())
+                .type(OrderType.EAT_IN)
+                .build()
+        );
+        Order order2 = orderService.create(
+            OrderFixture.createEatInOrderRequestBuilder()
+                .menu(orderMenu, 1L, orderMenu.getPrice())
+                .orderTable(orderTable.getId())
+                .type(OrderType.EAT_IN)
+                .build()
+        );
+        orderService.accept(order1.getId());
+        orderService.serve(order1.getId());
+
+        //when
+        Order result = orderService.complete(order1.getId());
+        orderTable = orderTableService.findAll().get(0);
+
+        //then
+        assertAll(
+            () -> assertThat(orderTable.isOccupied()).isTrue(),
+            () -> assertThat(result.getStatus()).isEqualTo(OrderStatus.COMPLETED)
+        );
+
+    }
+
+    @DisplayName("포장 주문을 완료처리 할수 있다")
+    @Test
+    void test22() {
+        //given
+        Menu orderMenu = menus.get(0);
+        Order order = orderService.create(
+            OrderFixture.createTakeOutOrderCreateRequestBuilder()
+                .menu(orderMenu, 1L, orderMenu.getPrice())
+                .type(OrderType.TAKEOUT)
+                .build()
+        );
+        orderService.accept(order.getId());
+        orderService.serve(order.getId());
+
+        //when
+        Order result = orderService.complete(order.getId());
+
+        //then
+        assertThat(result.getStatus()).isEqualTo(OrderStatus.COMPLETED);
+    }
+}
--- src/test/java/kitchenpos/testHelper/SpringBootTestHelper.java
+++ src/test/java/kitchenpos/testHelper/SpringBootTestHelper.java
@@ -5,11 +5,17 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import kitchenpos.application.MenuGroupService;
+import kitchenpos.application.MenuService;
+import kitchenpos.application.OrderTableService;
 import kitchenpos.application.ProductService;
+import kitchenpos.domain.Menu;
 import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.OrderTable;
 import kitchenpos.domain.Product;
 import kitchenpos.testHelper.fake.PurgomalumClientFake;
 import kitchenpos.testHelper.fake.PurgomalumClientFake.Purgomalum;
+import kitchenpos.testHelper.fixture.MenuFixture;
+import kitchenpos.testHelper.fixture.OrderTableFixture;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.stereotype.Component;
@@ -27,14 +33,20 @@ public abstract class SpringBootTestHelper {
 
     @Autowired
     private MenuGroupService menuGroupService;
-
+    @Autowired
+    private MenuService menuService;
     @Autowired
     private ProductService productService;
 
     @Autowired
+    private OrderTableService orderTableService;
+
+    @Autowired
     private PurgomalumClientFake purgomalumClient;
     private List<Product> products = new ArrayList<>();
+    private List<Menu> menus = new ArrayList<>();
     private MenuGroup menuGroup;
+    private OrderTable orderTable;
 
     public void init() {
         databaseCleanup.execute();
@@ -59,6 +71,38 @@ public abstract class SpringBootTestHelper {
         this.products = productService.findAll();
     }
 
+    protected void initOrderTable() {
+        this.orderTable = orderTableService.create(OrderTableFixture.createRequestBuilder()
+            .name("orderTable")
+            .build()
+        );
+        orderTableService.sit(this.orderTable.getId());
+    }
+
+    protected void initMenu() {
+        Menu menu1 = menuService.create(
+            MenuFixture.createRequestBuilder()
+                .menuGroupId(menuGroup.getId())
+                .menuProduct(products.get(0), 1L)
+                .menuProduct(products.get(1), 1L)
+                .isDisplay(true)
+                .name("menuName")
+                .build()
+        );
+
+        Menu menu2 = menuService.create(
+            MenuFixture.createRequestBuilder()
+                .menuGroupId(menuGroup.getId())
+                .menuProduct(products.get(1), 1L)
+                .menuProduct(products.get(2), 1L)
+                .name("menuName")
+                .isDisplay(true)
+                .build()
+        );
+
+        this.menus = List.of(menu1, menu2);
+    }
+
     protected List<Product> getProducts() {
         return products;
     }
@@ -69,5 +113,17 @@ public abstract class SpringBootTestHelper {
         }
 
         return menuGroup;
+    }
+
+    protected OrderTable getOrderTable() {
+        if (ObjectUtils.isEmpty(this.orderTable)) {
+            throw new IllegalArgumentException("OrderTable Init이 된 후 참조가 되어야 합니다.");
+        }
+
+        return orderTable;
+    }
+
+    public List<Menu> getMenus() {
+        return menus;
     }
 }
