b28e36c45ba9492c037f0943f04285c776dac725
koola97620 <koola97620@nate.com>
2023-08-29 00:21:00 +0900

    refactor(메뉴생성): 메서드 정리

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.mockito.BDDMockito;
 
 import java.math.BigDecimal;
-import java.util.Collections;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
@@ -55,7 +54,7 @@ class MenuServiceTest {
     @DisplayName("메뉴는 메뉴그룹이 없으면 예외를 발생시킨다.")
     @Test
     void menu_create_not_found_menuGroup() {
-        BDDMockito.given(menuGroupRepository.findById(any())).willReturn(Optional.empty());
+        메뉴그룹반환(Optional.empty());
         menu.setPrice(BigDecimal.valueOf(16000));
 
         assertThatExceptionOfType(NoSuchElementException.class)
@@ -66,7 +65,7 @@ class MenuServiceTest {
     @ParameterizedTest
     @NullAndEmptySource
     void menu_create_null_menuProducts(List<MenuProduct> productList) {
-        BDDMockito.given(menuGroupRepository.findById(any())).willReturn(Optional.of(MenuFixture.MenuGroupFixture.한마리메뉴()));
+        메뉴그룹반환(Optional.of(MenuFixture.MenuGroupFixture.한마리메뉴()));
         menu.setPrice(BigDecimal.valueOf(16000));
         menu.setMenuProducts(productList);
 
@@ -77,7 +76,7 @@ class MenuServiceTest {
     @DisplayName("메뉴상품의 수와 상품의 수가 다르면 예외를 발생시킨다.")
     @Test
     void menu_create_menuProducts_not_match_size() {
-        BDDMockito.given(menuGroupRepository.findById(any())).willReturn(Optional.of(MenuFixture.MenuGroupFixture.두마리메뉴()));
+        메뉴그룹반환(Optional.of(MenuFixture.MenuGroupFixture.두마리메뉴()));
         menu.setPrice(BigDecimal.valueOf(16000));
         menu.setMenuProducts(
                 List.of(
@@ -88,31 +87,16 @@ class MenuServiceTest {
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> menuService.create(menu));
     }
-
-    @DisplayName("메뉴상품의 수량이 음수면 예외를 발생시킨다.")
-    @Test
-    void menu_create_menuProducts_negative_quantity() {
-        BDDMockito.given(menuGroupRepository.findById(any())).willReturn(Optional.of(MenuFixture.MenuGroupFixture.두마리메뉴()));
-        menu.setPrice(BigDecimal.valueOf(16000));
-        menu.setMenuProducts(
-                List.of(
-                        MenuFixture.MenuProductFixture.메뉴상품_후라이드(ProductFixture.후라이드()),
-                        MenuFixture.MenuProductFixture.메뉴상품_양념_재고음수(ProductFixture.양념치킨()))
-        );
-
-        assertThatExceptionOfType(IllegalArgumentException.class)
-                .isThrownBy(() -> menuService.create(menu));
-    }
-
     @DisplayName("메뉴상품의 상품이 존재하지 않으면 예외를 발생시킨다.")
     @Test
     void menu_create_menuProducts_not_found_product() {
         Product 후라이드 = ProductFixture.후라이드();
         Product 양념치킨 = ProductFixture.양념치킨();
-        BDDMockito.given(menuGroupRepository.findById(any())).willReturn(Optional.of(MenuFixture.MenuGroupFixture.두마리메뉴()));
-        BDDMockito.given(productRepository.findAllByIdIn(any())).willReturn(List.of(후라이드, 양념치킨));
-        BDDMockito.given(productRepository.findById(후라이드.getId())).willReturn(Optional.of(후라이드));
-        BDDMockito.given(productRepository.findById(양념치킨.getId())).willReturn(Optional.empty());
+        메뉴그룹반환(Optional.of(MenuFixture.MenuGroupFixture.두마리메뉴()));
+        상품목록반환(List.of(후라이드, 양념치킨));
+
+        상품반환(후라이드, Optional.of(후라이드));
+        상품반환(양념치킨, Optional.empty());
         menu.setPrice(BigDecimal.valueOf(16000));
         menu.setMenuProducts(
                 List.of(
@@ -124,15 +108,36 @@ class MenuServiceTest {
                 .isThrownBy(() -> menuService.create(menu));
     }
 
+    @DisplayName("메뉴상품의 수량이 음수면 예외를 발생시킨다.")
+    @Test
+    void menu_create_menuProducts_negative_quantity() {
+        Product 후라이드 = ProductFixture.후라이드();
+        Product 양념치킨 = ProductFixture.양념치킨();
+        메뉴그룹반환(Optional.of(MenuFixture.MenuGroupFixture.두마리메뉴()));
+        상품목록반환(List.of(후라이드, 양념치킨));
+        상품반환(후라이드, Optional.of(후라이드));
+        상품반환(양념치킨, Optional.of(양념치킨));
+        menu.setPrice(BigDecimal.valueOf(16000));
+        menu.setMenuProducts(
+                List.of(
+                        MenuFixture.MenuProductFixture.메뉴상품_후라이드(후라이드),
+                        MenuFixture.MenuProductFixture.메뉴상품_양념_재고음수(양념치킨))
+        );
+
+        assertThatExceptionOfType(IllegalArgumentException.class)
+                .isThrownBy(() -> menuService.create(menu));
+    }
+
+
     @DisplayName("메뉴가격이 메뉴상품들의 가격의 합보다 크면 예외를 발생시킨다.")
     @Test
     void menu_create_menu_price_more_menuProducts_price_sum() {
         Product 후라이드 = ProductFixture.후라이드();
         Product 양념치킨 = ProductFixture.양념치킨();
-        BDDMockito.given(menuGroupRepository.findById(any())).willReturn(Optional.of(MenuFixture.MenuGroupFixture.두마리메뉴()));
-        BDDMockito.given(productRepository.findAllByIdIn(any())).willReturn(List.of(후라이드, 양념치킨));
-        BDDMockito.given(productRepository.findById(후라이드.getId())).willReturn(Optional.of(후라이드));
-        BDDMockito.given(productRepository.findById(양념치킨.getId())).willReturn(Optional.of(양념치킨));
+        메뉴그룹반환(Optional.of(MenuFixture.MenuGroupFixture.두마리메뉴()));
+        상품목록반환(List.of(후라이드, 양념치킨));
+        상품반환(후라이드, Optional.of(후라이드));
+        상품반환(양념치킨, Optional.of(양념치킨));
         menu.setPrice(후라이드.getPrice().add(양념치킨.getPrice()).add(BigDecimal.valueOf(10000)));
         menu.setMenuProducts(
                 List.of(
@@ -149,10 +154,10 @@ class MenuServiceTest {
     void menu_create_menu_name() {
         Product 후라이드 = ProductFixture.후라이드();
         Product 양념치킨 = ProductFixture.양념치킨();
-        BDDMockito.given(menuGroupRepository.findById(any())).willReturn(Optional.of(MenuFixture.MenuGroupFixture.두마리메뉴()));
-        BDDMockito.given(productRepository.findAllByIdIn(any())).willReturn(List.of(후라이드, 양념치킨));
-        BDDMockito.given(productRepository.findById(후라이드.getId())).willReturn(Optional.of(후라이드));
-        BDDMockito.given(productRepository.findById(양념치킨.getId())).willReturn(Optional.of(양념치킨));
+        메뉴그룹반환(Optional.of(MenuFixture.MenuGroupFixture.두마리메뉴()));
+        상품목록반환(List.of(후라이드, 양념치킨));
+        상품반환(후라이드, Optional.of(후라이드));
+        상품반환(양념치킨, Optional.of(양념치킨));
         BDDMockito.given(purgomalumClient.containsProfanity(any())).willReturn(true);
         menu.setPrice(후라이드.getPrice().add(양념치킨.getPrice()).add(BigDecimal.valueOf(33000)));
         menu.setMenuProducts(
@@ -163,6 +168,18 @@ class MenuServiceTest {
 
         assertThatExceptionOfType(IllegalArgumentException.class)
                 .isThrownBy(() -> menuService.create(menu));
+    }
+
+    private void 메뉴그룹반환(Optional<MenuGroup> menuGroup) {
+        BDDMockito.given(menuGroupRepository.findById(any())).willReturn(menuGroup);
+    }
+
+    private void 상품반환(Product product, Optional<Product> optionalProduct) {
+        BDDMockito.given(productRepository.findById(product.getId())).willReturn(optionalProduct);
+    }
+
+    private void 상품목록반환(List<Product> products) {
+        BDDMockito.given(productRepository.findAllByIdIn(any())).willReturn(products);
     }
 
 }
