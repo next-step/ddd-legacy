c2f63eba9956823eec1e24200471b87cee604511
Soora Oh <soora.oh@betalabs.kr>
2023-08-28 15:17:32 +0900

    test: 상품 생성 테스트 개발

---
+++ src/test/java/kitchenpos/domain/ProductTest.java
@@ -0,0 +1,28 @@
+package kitchenpos.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class ProductTest {
+    @DisplayName("상품 생성")
+    @Test
+    void test1() {
+        final UUID id = UUID.randomUUID();
+        final String name = "치킨";
+        final BigDecimal price = BigDecimal.TEN;
+
+        final Product product = new Product();
+        product.setId(id);
+        product.setName(name);
+        product.setPrice(price);
+
+        assertThat(product.getId()).isEqualTo(id);
+        assertThat(product.getName()).isEqualTo(name);
+        assertThat(product.getPrice()).isEqualTo(price);
+    }
+}
