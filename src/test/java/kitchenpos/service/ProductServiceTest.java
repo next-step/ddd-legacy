e2a3f622c5b64d3b17f1467281dae01672a9b4c2
soo <yunsoo0510@daum.net>
2023-08-28 22:25:11 +0900

    test: refactor ProductServiceTest using Fixture

--- src/test/java/kitchenpos/service/ProductServiceTest.java
+++ src/test/java/kitchenpos/service/ProductServiceTest.java
@@ -1,28 +1,75 @@
 package kitchenpos.service;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import java.math.BigDecimal;
+import java.util.NoSuchElementException;
 import java.util.UUID;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import kitchenpos.application.ProductService;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroup;
+import kitchenpos.domain.MenuGroupRepository;
+import kitchenpos.domain.MenuRepository;
 import kitchenpos.domain.Product;
-import kitchenpos.infra.PurgomalumClient;
+import kitchenpos.domain.ProductRepository;
 
-@ExtendWith(MockitoExtension.class)
+@SpringBootTest
+@Transactional
 class ProductServiceTest {
-    private final PurgomalumClient purgomalumClient = new PurgomalumClient(new RestTemplateBuilder());
-    private final ProductService productService = new ProductService(null, null, purgomalumClient);
+
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private MenuRepository menuRepository;
+
+    @Autowired
+    private MenuGroupRepository menuGroupRepository;
+
+    private Product 강정치킨;
+    private Menu 오늘의치킨;
+
+    @BeforeEach
+    void init() {
+        강정치킨 = ProductFixture.Data.강정치킨();
+        productRepository.save(강정치킨);
+
+        MenuGroup 추천메뉴 = MenuGroupFixture.builder()
+                .name("추천 메뉴")
+                .build();
+        menuGroupRepository.save(추천메뉴);
+
+        오늘의치킨 = MenuFixture.builder()
+                .name("오늘의 치킨")
+                .price(new BigDecimal(1000))
+                .menuGroup(추천메뉴)
+                .menuProduct(
+                        MenuProductFixture.create()
+                                .product(강정치킨)
+                                .quantity(1)
+                                .build()
+                )
+                .displayed(true)
+                .build();
+        menuRepository.save(오늘의치킨);
+    }
 
     @Test
     void 상품_생성_실패__가격이_null() {
-        Product product = new Product();
-        product.setPrice(null);
+        Product product = ProductFixture.builder()
+                .price(null)
+                .build();
 
         assertThatThrownBy(() -> productService.create(product))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -30,8 +77,31 @@ class ProductServiceTest {
 
     @Test
     void 상품_생성_실패__가격이_음수() {
-        Product product = new Product();
-        product.setPrice(new BigDecimal(-1));
+        Product product = ProductFixture.builder()
+                .price(new BigDecimal(-1))
+                .build();
+
+        assertThatThrownBy(() -> productService.create(product))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 상품_생성_실패__이름이_null() {
+        Product product = ProductFixture.builder()
+                .price(new BigDecimal(0))
+                .name(null)
+                .build();
+
+        assertThatThrownBy(() -> productService.create(product))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 상품_생성_실패__이름에_욕설_포함() {
+        Product product = ProductFixture.builder()
+                .price(new BigDecimal(0))
+                .name("fuck")
+                .build();
 
         assertThatThrownBy(() -> productService.create(product))
                 .isInstanceOf(IllegalArgumentException.class);
@@ -39,7 +109,7 @@ class ProductServiceTest {
 
     @Test
     void 상품_가격_변경_실패__가격이_null() {
-        UUID productId = null;
+        UUID productId = 강정치킨.getId();
         Product product = new Product();
         product.setPrice(null);
 
@@ -49,31 +119,21 @@ class ProductServiceTest {
 
     @Test
     void 상품_가격_변경_실패__가격이_음수() {
-        UUID productId = null;
-        Product product = new Product();
-        product.setPrice(new BigDecimal(-1));
+        UUID productId = 강정치킨.getId();
+        Product request = new Product();
+        request.setPrice(new BigDecimal(-1));
 
-        assertThatThrownBy(() -> productService.changePrice(productId, product))
+        assertThatThrownBy(() -> productService.changePrice(productId, request))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
-    void 상품_생성_실패__이름이_null() {
-        Product product = new Product();
-        product.setPrice(new BigDecimal(0));
-        product.setName(null);
+    void 상품_가격_변경_실패__상품이_존재하지_않음() {
+        UUID productId = UUID.randomUUID();
+        Product request = new Product();
+        request.setPrice(new BigDecimal(0));
 
-        assertThatThrownBy(() -> productService.create(product))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    void 상품_생성_실패__이름에_욕설_포함() {
-        Product product = new Product();
-        product.setPrice(new BigDecimal(0));
-        product.setName("fuck");
-
-        assertThatThrownBy(() -> productService.create(product))
-                .isInstanceOf(IllegalArgumentException.class);
+        assertThatThrownBy(() -> productService.changePrice(productId, request))
+                .isInstanceOf(NoSuchElementException.class);
     }
 }
