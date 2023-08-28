d515bbbf72a47444367fddb507b1ab1dc21056a9
junlee <laonzenamoon@gmail.com>
2023-08-28 10:49:07 +0900

    test(#step3): 주문 테이블 고객 입장 기능 테스트 코드 추가

--- src/test/java/kitchenpos/application/OrderTableServiceTest.java
+++ src/test/java/kitchenpos/application/OrderTableServiceTest.java
@@ -2,13 +2,19 @@ package kitchenpos.application;
 
 import kitchenpos.domain.OrderTable;
 import kitchenpos.helper.OrderTableHelper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.InvalidDataAccessApiUsageException;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.NoSuchElementException;
+import java.util.UUID;
 
 import static kitchenpos.helper.NameHelper.NAME_OF_255_CHARACTERS;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -53,6 +59,51 @@ class OrderTableServiceTest extends ApplicationTest {
                 assertThatThrownBy(() -> orderTableService.create(orderTable))
                         .isInstanceOf(IllegalArgumentException.class);
             }
+        }
+    }
+
+    @DisplayName("등록된 테이블에 고객을 입장시킨다.")
+    @Nested
+    class SitOrderTable {
+
+        private OrderTable beforeCreatedOrderTable;
+
+        @BeforeEach
+        void beforeEach() {
+            beforeCreatedOrderTable = orderTableService.create(OrderTableHelper.create());
+        }
+
+        @DisplayName("등록된 테이블에 고객을 입장 (성공)")
+        @Test
+        void success1() {
+            // When
+            OrderTable satOrderTable = orderTableService.sit(beforeCreatedOrderTable.getId());
+
+            // Then
+            assertThat(satOrderTable.getId()).isEqualTo(beforeCreatedOrderTable.getId());
+            assertThat(satOrderTable.isOccupied()).isEqualTo(true);
+        }
+
+        @DisplayName("orderTableId null 로 인한 테이블에 고객을 입장 (실패)")
+        @ParameterizedTest
+        @NullSource
+        void fail1(final UUID orderTableId) {
+            // When
+            // Then
+            assertThatThrownBy(() -> orderTableService.sit(orderTableId))
+                    .isInstanceOf(InvalidDataAccessApiUsageException.class);
+        }
+
+        @DisplayName("존재하지 않는 테이블에 고객을 입장 (실패)")
+        @Test
+        void fail2() {
+            // Given
+            UUID notExistedOrderTableId = UUID.randomUUID();
+
+            // When
+            // Then
+            assertThatThrownBy(() -> orderTableService.sit(notExistedOrderTableId))
+                    .isInstanceOf(NoSuchElementException.class);
         }
     }
 
