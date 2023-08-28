8a8f844745d904901cb0cdc7120481c3b5dc592d
saerang <lsg10119@gamail.com>
2023-08-28 22:50:47 +0900

    feat(menuProduct): 메뉴상품 테스트코드 추가

--- src/test/java/kitchenpos/domain/MenuProductTest.java
+++ src/test/java/kitchenpos/domain/MenuProductTest.java
@@ -14,12 +14,15 @@ class MenuProductTest {
     @Test
     @DisplayName("메뉴상품은 상품과, 수량을 가지고 있다.")
     void create() {
+        Product product = Fixtures.createProduct("상품", new BigDecimal("10000"));
         MenuProduct menuProduct = new MenuProduct();
         menuProduct.setSeq(1L);
         menuProduct.setProductId(UUID.randomUUID());
-        menuProduct.setProduct(Fixtures.createProduct("상품", new BigDecimal("10000")));
+        menuProduct.setProduct(product);
         menuProduct.setQuantity(1L);
 
         assertThat(menuProduct.getSeq()).isEqualTo(1L);
+        assertThat(menuProduct.getProduct().getId()).isEqualTo(product.getId());
+        assertThat(menuProduct.getQuantity()).isEqualTo(1L);
     }
 }
