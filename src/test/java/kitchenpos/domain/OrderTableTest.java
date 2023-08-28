edf3a6171b6e503519c8b2a50b4f2f3ce0f72f33
Soora Oh <soora.oh@betalabs.kr>
2023-08-28 16:25:23 +0900

    test: 테이블은 이름과 인원수, 착석 여부로 구성되어 있다

---
+++ src/test/java/kitchenpos/domain/OrderTableTest.java
@@ -0,0 +1,32 @@
+package kitchenpos.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderTableTest {
+    @DisplayName("테이블은 이름과 인원수, 착석 여부로 구성되어 있다")
+    @Test
+    void test1() {
+        final UUID id = UUID.randomUUID();
+        final String name = "테이블1번";
+        final int numberOfGuests = 5;
+        final boolean occupied = true;
+
+        final OrderTable orderTable = new OrderTable();
+        orderTable.setId(id);
+        orderTable.setName(name);
+        orderTable.setNumberOfGuests(numberOfGuests);
+        orderTable.setOccupied(occupied);
+
+        assertThat(orderTable.getId()).isEqualTo(id);
+        assertThat(orderTable.getName()).isEqualTo(name);
+        assertThat(orderTable.getNumberOfGuests()).isEqualTo(numberOfGuests);
+        assertThat(orderTable.isOccupied()).isEqualTo(occupied);
+    }
+
+}
