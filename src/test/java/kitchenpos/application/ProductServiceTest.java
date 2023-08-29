6b3941d688dee0f82f4e8232fecaa0d535582860
Soora Oh <soora.oh@betalabs.kr>
2023-08-29 21:28:28 +0900

    test : 상품 가격이 수정 되었을 때 해당 상품을 포함하여 판매중인 메뉴의 가격이 더 비싸질 경우 해당 메뉴는 미표출 한다.

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -1,7 +1,6 @@
 package kitchenpos.application;
 
-import kitchenpos.domain.Product;
-import kitchenpos.domain.ProductRepository;
+import kitchenpos.domain.*;
 import kitchenpos.infra.PurgomalumClient;
 import kitchenpos.support.BaseServiceTest;
 import org.junit.jupiter.api.DisplayName;
@@ -12,6 +11,9 @@ import java.math.BigDecimal;
 import java.util.List;
 import java.util.UUID;
 
+import static kitchenpos.fixture.MenuFixture.createMenu;
+import static kitchenpos.fixture.MenuGroupFixture.createMenuGroup;
+import static kitchenpos.fixture.MenuProductFixture.createMenuProduct;
 import static kitchenpos.fixture.ProductFixture.createProduct;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -22,13 +24,17 @@ import static org.mockito.Mockito.when;
 class ProductServiceTest extends BaseServiceTest {
     private final ProductService productService;
     private final ProductRepository productRepository;
+    private final MenuGroupRepository menuGroupRepository;
+    private final MenuRepository menuRepository;
 
     @MockBean
     private PurgomalumClient purgomalumClient;
 
-    public ProductServiceTest(final ProductService productService, final ProductRepository productRepository) {
+    public ProductServiceTest(final ProductService productService, final ProductRepository productRepository, final MenuGroupRepository menuGroupRepository, final MenuRepository menuRepository) {
         this.productService = productService;
         this.productRepository = productRepository;
+        this.menuGroupRepository = menuGroupRepository;
+        this.menuRepository = menuRepository;
     }
 
     @DisplayName("상품은 등록이 가능하다")
@@ -78,9 +84,27 @@ class ProductServiceTest extends BaseServiceTest {
         assertThat(changedPriceProduct.getPrice()).isEqualTo(newPriceProduct.getPrice());
     }
 
-    @DisplayName("상품은 전체 조회가 가능하다")
+    @DisplayName("상품 가격이 수정 되었을 때 해당 상품을 포함하여 판매중인 메뉴의 가격이 더 비싸질 경우 해당 메뉴는 미표출 한다.")
     @Test
     void test5() {
+        final Product product = productRepository.save(createProduct(UUID.randomUUID(), "치킨", BigDecimal.TEN));
+        final MenuGroup menuGroup = menuGroupRepository.save(createMenuGroup(UUID.randomUUID()));
+        final MenuProduct menuProduct = createMenuProduct(product);
+        final Menu cheapMenu = menuRepository.save(createMenu(UUID.randomUUID(), "치킨", new BigDecimal(0), menuGroup, true, List.of(menuProduct)));
+        final Menu expensiveMenu = menuRepository.save(createMenu(UUID.randomUUID(), "치킨", new BigDecimal(100000000), menuGroup, true, List.of(menuProduct)));
+        final Product newPriceProduct = createProduct("상품", BigDecimal.ONE);
+
+        final Product changedPriceProduct = productService.changePrice(product.getId(), newPriceProduct);
+
+        assertThat(changedPriceProduct.getId()).isEqualTo(product.getId());
+        assertThat(changedPriceProduct.getPrice()).isEqualTo(newPriceProduct.getPrice());
+        assertThat(cheapMenu.isDisplayed()).isTrue();
+        assertThat(expensiveMenu.isDisplayed()).isFalse();
+    }
+
+    @DisplayName("상품은 전체 조회가 가능하다")
+    @Test
+    void test6() {
         final Product chicken = productRepository.save(createProduct(UUID.randomUUID(), "치킨", BigDecimal.ONE));
         final Product pizza = productRepository.save(createProduct(UUID.randomUUID(), "피자", BigDecimal.ZERO));
 
