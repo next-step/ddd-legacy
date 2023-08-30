4adefeb0b578b165e8eec19611db194b647c574f
soo <yunsoo0510@daum.net>
2023-08-30 23:30:40 +0900

    test: OrderTableService clear: order exists that status is not completed

--- src/test/java/kitchenpos/service/OrderTableServiceTest.java
+++ src/test/java/kitchenpos/service/OrderTableServiceTest.java
@@ -1,19 +1,29 @@
 package kitchenpos.service;
 
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import kitchenpos.application.OrderTableService;
+import kitchenpos.domain.OrderRepository;
 import kitchenpos.domain.OrderTable;
 
 @SpringBootTest
+@ExtendWith(MockitoExtension.class)
 public class OrderTableServiceTest {
 
     @Autowired
     private OrderTableService orderTableService;
+
+    @MockBean
+    private OrderRepository orderRepository;
 
     @Test
     void 주문테이블_생성_실패__이름이_null() {
@@ -31,5 +41,16 @@ public class OrderTableServiceTest {
 
         assertThatThrownBy(() -> orderTableService.create(request))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 주문테이블_정리_실패__해당_주문테이블에_완료되지_않은_주문이_존재() {
+        OrderTable request = new OrderTable();
+        request.setName("1번 테이블");
+        OrderTable orderTable = orderTableService.create(request);
+        when(orderRepository.existsByOrderTableAndStatusNot(any(), any())).thenReturn(true);
+
+        assertThatThrownBy(() -> orderTableService.clear(orderTable.getId()))
+                .isInstanceOf(IllegalStateException.class);
     }
 }
