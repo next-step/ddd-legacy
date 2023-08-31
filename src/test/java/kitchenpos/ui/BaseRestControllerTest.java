b24130dcd9d7487679e3bbafc75f0a7c842489a2
DESKTOP-H9S3HA4\puge3 <fgfg1202@gmail.com>
2023-08-31 20:16:31 +0900

    test: delete Disabled annotation

--- src/test/java/kitchenpos/ui/BaseRestControllerTest.java
+++ src/test/java/kitchenpos/ui/BaseRestControllerTest.java
@@ -2,13 +2,11 @@ package kitchenpos.ui;
 
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.Disabled;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-@Disabled
 @WebMvcTest
 @AutoConfigureMockMvc
 public class BaseRestControllerTest {
