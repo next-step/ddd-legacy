8c9ac6980646e92f7defbb1ef5027c61fedf96ce
kimjio <jiokim222@gmail.com>
2024-05-01 00:17:10 +0900

    feat(racingcar): implement the Car

---
+++ src/test/java/racingcar/Car.java
@@ -0,0 +1,28 @@
+package racingcar;
+
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this(name, 0);
+    }
+
+    public Car(String name, int position) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("이름은 5글자를 넘을 수 없습니다");
+        }
+        this.name = name;
+        this.position = position;
+    }
+
+    public void move(int condition) {
+        if (condition >= 4) {
+            position++;
+        }
+    }
+
+    public int position() {
+        return position;
+    }
+}
---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,33 @@
+package racingcar;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class CarTest {
+
+    @DisplayName("자동차 이름은 5글자를 넘을 수 없다")
+    @Test
+    void constructor() {
+        Assertions.assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Car("동해물과백두산이"));
+    }
+
+    @DisplayName("숫자가 4 이상인 경우 자동차는 전진한다")
+    @Test
+    void move() {
+        Car car = new Car("김지오");
+        car.move(4);
+        assertThat(car.position()).isEqualTo(1);
+    }
+
+    @DisplayName("숫자가 4 미만인 경우 자동차는 움직이지 않는다")
+    @Test
+    void stop() {
+        Car car = new Car("김지오");
+        car.move(3);
+        assertThat(car.position()).isEqualTo(0);
+    }
+}
