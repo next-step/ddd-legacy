68a20029a5d6b9722e142c69e3c4bf68d22c539d
daheecha0612 <dahee.cha@gmail.com>
2024-04-30 22:26:42 +0900

    refactor: 생성자 간략히 만들어라

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -6,9 +6,7 @@ public record Car (
         String name,
         int position
 ){
-    public Car(String name, int position) {
+    public Car {
         Assert.isTrue(name.length() <= 5, "자동차 이름은 5글자를 넘을 수 없습니다.");
-        this.name = name;
-        this.position = position;
     }
 }
