e4e6df8a48fb985258d1870ad45edac0952a2395
daheecha0612 <dahee.cha@gmail.com>
2024-04-30 22:25:11 +0900

    feat: 자동차 생성하라.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,7 +1,14 @@
 package racingcar;
 
+import org.springframework.util.Assert;
+
 public record Car (
         String name,
         int position
 ){
+    public Car(String name, int position) {
+        Assert.isTrue(name.length() <= 5, "자동차 이름은 5글자를 넘을 수 없습니다.");
+        this.name = name;
+        this.position = position;
+    }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,5 +1,7 @@
 package racingcar;
 
+import org.assertj.core.api.Assertions;
+import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,10 +10,25 @@ import static org.junit.jupiter.api.Assertions.*;
 @DisplayName("자동차(Car) 단위 테스트")
 class CarTest {
 
+    @DisplayName("자동차 이름과 자동차 위치로 자동차를 생성한다.")
+    @Test
+    void carInitTest() {
+        // when
+        var car = new Car("소나타V5", 0);
+
+        // then
+        SoftAssertions.assertSoftly(softly-> {
+            softly.assertThat(car.name()).isEqualTo("소나타V5");
+            softly.assertThat(car.position()).isEqualTo(0);
+        });
+    }
+
     @DisplayName("자동차 이름은 5글자를 넘을 경우 예외 발생한다.")
     @Test
     void carNameMaxLengthTest() {
-
+        // when & then
+        Assertions.assertThatThrownBy(() -> new Car("소나타V51", 0))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 
     @DisplayName("자동차는 움직임 값이 4 이상일때만 움직인다.")
