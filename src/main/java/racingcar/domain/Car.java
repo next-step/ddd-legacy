23459670299e94b0103e7363e98e02310d6451b5
dev-yoonjung <dear__yoon@naver.com>
2024-04-30 22:36:06 +0900

    feat(car): Implement a car domain

---
+++ src/main/java/racingcar/domain/Car.java
@@ -0,0 +1,18 @@
+package racingcar.domain;
+
+public class Car {
+
+    private final String name;
+
+    private int position;
+
+    public Car(String name) {
+        this(name, 0);
+    }
+
+    public Car(String name, int position) {
+        this.name = name;
+        this.position = position;
+    }
+
+}
