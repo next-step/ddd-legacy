7a31a7222096e411157bfa90bb7f3ef055af4223
daheecha0612 <dahee.cha@gmail.com>
2024-04-30 22:53:24 +0900

    refactor: 매직넘버를 상수화하라.

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -6,13 +6,17 @@ public record Car (
         String name,
         int position
 ){
+
+    public static final int DISTANCE_MIN = 4;
+    public static final int CAR_NAME_LENGTH_MAX = 5;
+
     public Car {
-        Assert.isTrue(name.length() <= 5, "자동차 이름은 5글자를 넘을 수 없습니다.");
+        Assert.isTrue(name.length() <= CAR_NAME_LENGTH_MAX, "자동차 이름은 5글자를 넘을 수 없습니다.");
     }
 
     public Car move(MoveStrategy strategy) {
         var distance = strategy.getDistance();
-        if (distance < 4) {
+        if (distance < DISTANCE_MIN) {
             return this;
         }
         return new Car(this.name, this.position + distance);
