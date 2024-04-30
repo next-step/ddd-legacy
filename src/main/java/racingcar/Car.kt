8a44bc75f5d55acca01d8caf8103b467385bd343
writer0713 <writer0713@naver.com>
2024-04-30 23:00:12 +0900

    feat: Car 의 move 메서드 구현 - move 메서드 구현 - 0~9 사이의 숫자만 들어갈수 있도록 validation - 4 이상의 숫자가 들어왔을때에만 currentPosition 변경

--- src/main/java/racingcar/Car.kt
+++ src/main/java/racingcar/Car.kt
@@ -1,9 +1,24 @@
 package racingcar
 
 data class Car(val name: String) {
+    var currentPosition = 0
+        private set
+
     init {
         if (name.length > 5) {
             throw IllegalArgumentException()
         }
+    }
+
+    fun move(position: Int) {
+        if (position < 0 || position > 9) {
+            throw IllegalArgumentException("position 은 0 ~ 9 사이의 값만 가능합니다.")
+        }
+
+        if (position < 4) {
+            return
+        }
+
+        currentPosition += position
     }
 }
