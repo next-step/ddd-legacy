95ad6b87b11901611ec383b7010ca9a69611804f
whale0928 <rlagusrl928@gmail.com>
2024-04-30 23:16:12 +0900

    feat: StringCalculator create method splitAndSum

--- src/test/java/kitchenpos/StringCalculator.java
+++ src/test/java/kitchenpos/StringCalculator.java
@@ -1,7 +1,22 @@
 package kitchenpos;
 
+import java.util.Arrays;
+
+import static org.junit.platform.commons.util.StringUtils.isBlank;
+
 public class StringCalculator {
     public int add(String text) {
-        return 0;
+
+        if (isBlank(text)) {
+            return 0;
+        }
+
+        return splitAndSum(text);
+    }
+
+    public int splitAndSum(String text) {
+        return Arrays.stream(text.split("[,:]"))
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
