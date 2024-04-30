6578627947977b0565a788539f22e5cdaa2e564b
JaeKwon Shin <80821744+jkshin0602@users.noreply.github.com>
2024-04-30 23:17:58 +0900

    step0: racing car 테스트 구현 (#558)
    * feat(racingcar): implement the car
    
    * refactor(racingcar): use the moving strategy
    
    * refactor(racingcar): use kotlin funtional interface
    
    * refactor(racingcar): use kotlin funtional programming
    
    * refactor(racingcar): remove invoke()
    
    * refactor(racingcar): use funtional interface in parmeter
    
    * style(nend of line): add newline at a end of file

---
+++ src/test/java/racingcar/GoStrategy.kt
@@ -0,0 +1,5 @@
+package racingcar
+
+class GoStrategy : MovingStrategy {
+    override fun movable(): Boolean = true
+}
---
+++ src/test/java/racingcar/MovingStrategy.kt
@@ -0,0 +1,5 @@
+package racingcar
+
+fun interface MovingStrategy {
+    fun movable(): Boolean
+}
