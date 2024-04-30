24152af73a833664f826002934fac568d1df3394
writer0713 <writer0713@naver.com>
2024-04-30 23:01:08 +0900

    test: Car 의 move 메서드 테스트 추가 - move 메서드의 range validation test - move 메서드에 4 보다 작은값 넣은 경우 test - move 메서드에 4 이상 넣은 경우 test

--- src/test/java/racingcar/CarTest.kt
+++ src/test/java/racingcar/CarTest.kt
@@ -1,5 +1,6 @@
 package racingcar
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -15,5 +16,46 @@ class CarTest {
         assertThrows(IllegalArgumentException::class.java) {
             Car(name = name)
         }
+    }
+
+    @DisplayName("move 메서드에는 0~9 사이의 숫자만 넘길수 있다")
+    @Test
+    fun test2() {
+        // given
+        val name = "벤츠"
+        val car = Car(name = name)
+
+        // when
+        assertThrows(IllegalArgumentException::class.java) {
+            car.move(position = -1)
+        }
+    }
+
+    @DisplayName("move 메서드에 4 보다 작은 숫자를 넘기면 움직이지 않음")
+    @Test
+    fun test3() {
+        // given
+        val name = "벤츠"
+        val car = Car(name = name)
+
+        // when
+        car.move(position = 3)
+
+        // then
+        assertEquals(0, car.currentPosition)
+    }
+
+    @DisplayName("move 메서드에 4 이상의 숫자를 넘기면 움직임")
+    @Test
+    fun test4() {
+        // given
+        val name = "벤츠"
+        val car = Car(name = name)
+
+        // when
+        car.move(4)
+
+        // then
+        assertEquals(4, car.currentPosition)
     }
 }
