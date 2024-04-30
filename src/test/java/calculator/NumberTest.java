3216609c1edea124ae337f9cdc644f39a42690ce
boradol2 <boradol0902@gmail.com>
2024-05-01 03:18:18 +0900

    test(Number): 0을 반환하는 경우의 테스트 작성

--- src/test/java/calculator/NumberTest.java
+++ src/test/java/calculator/NumberTest.java
@@ -3,10 +3,13 @@ package calculator;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static calculator.Number.NEGATIVE_NUMBER_EXCEPTION;
 import static calculator.Number.PARSING_INTEGER_EXCEPTION;
+import static calculator.Number.ZERO_NUMBER;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @DisplayName("음이 아닌 숫자 클래스를 위한 테스트")
 class NumberTest {
@@ -26,5 +29,13 @@ class NumberTest {
         Assertions.assertThatThrownBy(() -> new Number(value))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining(NEGATIVE_NUMBER_EXCEPTION);
+    }
+
+    @DisplayName("입력 받은 값이 빈 문자열이나 null이면 0이 반환된다.")
+    @NullAndEmptySource
+    @ValueSource(strings = " ")
+    @ParameterizedTest
+    void returnZero(String value) {
+        assertThat(Number.from(value)).isEqualTo(ZERO_NUMBER);
     }
 }
