f4f60bb93941977c05a30055fe49621be26a528a
writer0713 <writer0713@naver.com>
2024-04-30 22:40:15 +0900

    test: 자동차 이름 관련 테스트 추가 - 자동차 이름은 5글자를 넘을수 없다. - 5글자가 넘으면 IllegalArgumentException 이 발생한다.

--- src/test/java/racingcar/CarTest.kt
+++ src/test/java/racingcar/CarTest.kt
@@ -1,10 +1,19 @@
 package racingcar
 
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
 
 class CarTest {
+    @DisplayName("자동차 이름은 5 글자를 넘을 수 없다.")
     @Test
-    fun test() {
-        println(">>> test")
+    fun test1() {
+        // given
+        val name = "람보르으기니"
+
+        // when
+        assertThrows(IllegalArgumentException::class.java) {
+            Car(name = name)
+        }
     }
 }
