a53f2185cb0624018132c47853dfa3768217905a
daheecha0612 <dahee.cha@gmail.com>
2024-04-30 22:06:45 +0900

    test: 요구사항을 테스트 코드로 정의하라

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,36 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("자동차(Car) 단위 테스트")
+class CarTest {
+
+    @DisplayName("자동차 이름은 5글자를 넘을 경우 예외 발생한다.")
+    @Test
+    void carNameMaxLengthTest() {
+
+    }
+
+    @DisplayName("자동차는 움직임 값이 4 이상일때만 움직인다.")
+    @Test
+    void carMoveMinTest() {
+
+    }
+
+    @DisplayName("자동차는 앞으로 움직인다.")
+    @Test
+    void carMoveMoveForwardTest() {
+
+    }
+
+
+    @DisplayName("자동차는 뒤로 움직인다.")
+    @Test
+    void carMoveMoveBackwardTest() {
+
+    }
+
+}
