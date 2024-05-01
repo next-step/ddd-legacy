a3b4b3f7b15a7ede67615e6dc0874bf1b870cda8
pawoo0211 <pawoo0211@naver.com>
2024-05-01 16:18:19 +0900

    test: 문자열 덧셈 계산기 뼈대 테스트 코드 작성

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -1,8 +1,41 @@
 package calculator;
 
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 @DisplayName("문자열 덧셈 계산기 테스트")
 public class StringCalculatorTest {
+    private StringCalculator calculator;
 
+    @BeforeEach
+    void setUp() {
+        calculator = new StringCalculator();
+    }
+
+    @DisplayName("빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.(예 : “” => 0, null => 0)")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void checkEmptyAndNull(String input) {
+        Assertions.assertThat(calculator.calculate(input)).isEqualTo(0);
+    }
+
+    @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.(예 : “1”)")
+    @Test
+    void inputOnlyOne() {
+        Assertions.assertThat(calculator.calculate("1")).isEqualTo(1);
+    }
+}
+
+class StringCalculator {
+
+    public int calculate(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+        return Integer.valueOf(input);
+    }
 }
