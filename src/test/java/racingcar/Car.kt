51eba63c9930f226a0d98bbaa26d11baef8b5829
wooddy.kim <wooddy.kim@kakaocorp.com>
2024-05-01 13:35:24 +0900

    refactor: 불필요한 필드 제거

--- src/test/java/racingcar/Car.kt
+++ src/test/java/racingcar/Car.kt
@@ -3,9 +3,8 @@ package racingcar
 class Car(
     val name: String,
 ) {
-    private var _position: Int = 0
-    val position: Int
-        get() = _position
+    var position: Int = 0
+        private set
 
     init {
         require(name.length <= 5) { "자동차 이름은 5글자를 넘을 수 없습니다." }
@@ -13,7 +12,7 @@ class Car(
 
     fun move(movingStrategy: MovingStrategy) {
         if (movingStrategy.isMovable()) {
-            _position += 1
+            position += 1
         }
     }
 }
