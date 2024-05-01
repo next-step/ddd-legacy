ee596e73367d27fe23940991f5dedc08ffbb22f3
joydaheecha <daheecha0612@gmail.com>
2024-05-01 12:51:19 +0900

    feat: 쉼표와 콜론으로 문자열을 구분후, 이들의 합을 반환하라

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,4 +1,32 @@
 package stringcalculator;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 public class StringCalculator {
+    public static final String REGULAR_DELIMITER_REGEX = "[,;]";
+    public static final String EMPTY_USER_INPUT = "";
+    public static final int CALCULATION_RESULT_ZERO = 0;
+    private final List<Integer> numbers;
+
+    public StringCalculator(String userInput) {
+        this.numbers = initNumbers(userInput);
+    }
+
+    private static List<Integer> initNumbers(String userInput) {
+        if (EMPTY_USER_INPUT.equals(userInput) || userInput == null) {
+            return Collections.emptyList();
+        }
+        return Arrays.stream(userInput.split(REGULAR_DELIMITER_REGEX))
+                .map(Integer::valueOf)
+                .toList();
+    }
+
+    public int calculate() {
+        if (numbers.isEmpty()) {
+            return CALCULATION_RESULT_ZERO;
+        }
+        return numbers.stream().mapToInt(Integer::intValue).sum();
+    }
 }
--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -1,13 +1,31 @@
 package stringcalculator;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
 
 class StringCalculatorTest {
+    @ParameterizedTest
+    @MethodSource("successTestFixture")
     @DisplayName("쉼표 또는 콜론을 구분자로 이루어진 숫자를 입력하면, 숫자들의 총합을 반환한다.")
-    void successTest() {
+    void successTest(String userInput, int expected) {
+        // when
+        var result = new StringCalculator(userInput).calculate();
 
+        // then
+        Assertions.assertThat(result).isEqualTo(expected);
+    }
+
+    static Stream<Arguments> successTestFixture() {
+        return Stream.of(
+                Arguments.of("1,2", 3),
+                Arguments.of("1;2;3", 6),
+                Arguments.of("", 0)
+        );
     }
 
     @DisplayName("//와 \\n 사이에 위치한 문자를 문자열에 포함시, 이를 커스텀 구분자로 사용, 숫자들의 총합을 반환한다.")
