29e51a1336a6fae89b819e575219ec4888bf3f41
bkRyusim <bkryusim@gmail.com>
2024-05-01 13:26:44 +0900

    feat: car move

--- src/test/java/racingcar/Car.java
+++ src/test/java/racingcar/Car.java
@@ -3,12 +3,26 @@ package racingcar;
 public class Car {
 
 	private final String name;
+	private final int position;
 
-	public Car(final String name) {
+	private Car(final String name, final int position) {
 		if (name.length() > 5) {
 			throw new IllegalArgumentException("이름은 5글자를 넘을 수 없습니다");
 		}
-		
+
 		this.name = name;
+		this.position = position;
+	}
+
+	public Car(final String name) {
+		this(name, 0);
+	}
+
+	public Car move(final int power) {
+		if (power < 4) {
+			return this;
+		}
+		
+		return new Car(name, position + 1);
 	}
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -5,6 +5,8 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class CarTest {
 
@@ -19,5 +21,21 @@ public class CarTest {
 			.doesNotThrowAnyException();
 	}
 
+	@DisplayName("자동차는 power가 4 이상인 경우에만 움직인다.")
+	@ParameterizedTest
+	@ValueSource(ints = {4, 5, 6, 7, 8, 9})
+	void move(final int power) {
+		assertThat(new Car("백승윤").move(power))
+			.extracting("position")
+			.isEqualTo(1);
+	}
 
+	@DisplayName("자동차는 power가 3 이하인 경우에는 움직이지 않는다.")
+	@ParameterizedTest
+	@ValueSource(ints = {1, 2, 3})
+	void notMove(final int power) {
+		assertThat(new Car("백승윤").move(power))
+			.extracting("position")
+			.isEqualTo(0);
+	}
 }
