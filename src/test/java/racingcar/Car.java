fb3a2fe131a41a5f70f7af67eff233cab860db5b
OhJeUng <bioman3238@gmail.com>
2024-05-01 23:39:23 +0900

    refactor(racingcar) : 의존성 낮추기
    - interface를 이용해 이동, 정지 기능을 구현합니다.

--- src/test/java/racingcar/Car.java
+++ src/test/java/racingcar/Car.java
@@ -1,11 +1,8 @@
 package racingcar;
 
-import java.util.Random;
-
 public class Car {
-  private String name;
+  private final String name;
   private int position;
-  private int currentPosition;
 
   public Car(String name, int position) {
     if (name.length() > 5) {
@@ -14,29 +11,14 @@ public class Car {
 
     this.name = name;
     this.position = position;
-    this.currentPosition = position;
   }
 
-  public void setPosition(int random) {
-    if (random > 4) {
+  public boolean isMoving(MovingStrategy movingStrategy, int condition) {
+    if (movingStrategy.movable(condition)) {
       this.position++;
-    }
-  }
-
-  public int getPosition() {
-    return position;
-  }
-
-  public int getCurrentPosition() {
-    return currentPosition;
-  }
-
-  public boolean isMoving() {
-    if (this.position == this.currentPosition) {
+      return true;
+    } else {
       return false;
     }
-
-    this.currentPosition = this.position;
-    return true;
   }
 }
