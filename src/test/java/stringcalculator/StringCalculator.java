cb7d2270a23cd6325855f3ea0ed01c0e833d73ad
gomshiki <gomshiki@gmail.com>
2024-05-02 00:33:14 +0900

    rafactor: all methods add test for negative number

--- src/test/java/stringcalculator/StringCalculator.java
+++ src/test/java/stringcalculator/StringCalculator.java
@@ -6,26 +6,39 @@ import java.util.regex.Pattern;
 
 public class StringCalculator {
 
-    private String inputString;
+    private String text;
 
     public StringCalculator() {
     }
 
-    public int sum(String inputString) {
+    public int add(String text) {
 
-        String[] split = inputString.split(",|:");
+        // null or 빈 문자열 검증
+        if(text == null || text.isEmpty()) {
+            return 0;
+        }
+
+        // 음수 입력 시 예외 처리
+        if(text.matches("^-\\d+$")){
+            throw new RuntimeException();
+        }
+
+        String[] split;
+
+        Matcher matcher = delimiter(text);
+
+        if (matcher.find()) {
+            String delimiter = matcher.group(1);
+            split = matcher.group(2).split(delimiter);
+        }else{
+            split = text.split(",|:");
+        }
+
         return Arrays.stream(split).mapToInt(Integer::parseInt).sum();
     }
 
-    public int getCustomSeperator(String inputString) {
-        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(inputString);
-        int sum = 0;
-        if (matcher.find()) {
-            String delimiter = matcher.group(1);
-            sum = Arrays.stream(matcher.group(2).split(delimiter)).mapToInt(Integer::parseInt).sum();
-        }
-
-        return sum;
-
+    public Matcher delimiter(String text) {
+        return Pattern.compile("//(.)\n(.*)").matcher(text);
     }
+
 }
--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -4,6 +4,9 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -25,29 +28,47 @@ public class StringCalculatorTest {
     void setUp() {
         stringCalculator = new StringCalculator();
     }
-    @DisplayName("쉼표(,) or 클론(:) 구분자로 ")
-    @Test
-    void seperate1(){
 
-
-        int sum = stringCalculator.sum("1,2");
-
-        assertThat(sum).isEqualTo(3);
+    @DisplayName(value = "빈 문자열일 경우 0으로 반환")
+    @ParameterizedTest
+    @NullAndEmptySource
+    void emptyOrNull(final String text) {
+        assertThat(stringCalculator.add(text)).isZero();
     }
 
-    @DisplayName("쉼표(,) or 클론(:) 구분자로 ")
-    @Test
-    void seperate2(){
-
-        int sum = stringCalculator.sum("1,2:3");
-
-        assertThat(sum).isEqualTo(6);
+    @DisplayName(" 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환")
+    @ParameterizedTest
+    @ValueSource(strings = {"1"})
+    void oneNumber(final String text){
+        assertThat(stringCalculator.add(text)).isSameAs(Integer.parseInt(text));
     }
 
-    @DisplayName("'//' 와 '\n' 사이에 위치한 문자를 커스텀 구분자로")
-    @Test
-    void seperate3(){
-        int customSeperator = stringCalculator.getCustomSeperator("//;\n1;2;3");
-        assertThat(customSeperator).isEqualTo(6);
+    @DisplayName(" 숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환 ")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2"})
+    void twoNumbers(final String text){
+        assertThat(stringCalculator.add(text)).isSameAs(3);
     }
+
+    @DisplayName("구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다")
+    @ParameterizedTest
+    @ValueSource(strings = {"1,2:3"})
+    void colons(final String text){
+        assertThat(stringCalculator.add(text)).isSameAs(6);
+    }
+
+    @DisplayName("//와 \n 문자 사이에 커스텀 구분자를 지정")
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\n1;2;3"})
+    void customDelimiter(final String text){
+        assertThat(stringCalculator.add(text)).isSameAs(6);
+    }
+
+    @DisplayName(value = "문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리")
+    @Test
+    void negative(){
+        assertThatExceptionOfType(RuntimeException.class).isThrownBy(() -> stringCalculator.add("-1"));
+    }
+
+
 }
