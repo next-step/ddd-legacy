1cbc9b9d90b2fb756f50e64b930e20d2e2d8e7f7
OhJeUng <bioman3238@gmail.com>
2024-05-01 22:32:22 +0900

    style (DisplayName 추가, step0 상세 요구사항 문서화)

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,14 +1,16 @@
 package racingcar;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class CarTest {
 
   @Test
+  @DisplayName("자동차_이름은_5글자를_넘을_수_없다")
   void 자동차_이름은_5글자를_넘을_수_없다() {
     Assertions.assertThatIllegalArgumentException()
-            .isThrownBy(() -> new Car("동해물과백두산이"));
+            .isThrownBy(() -> new Car("동해물과백두산이", 0));
   }
 
   @Test
