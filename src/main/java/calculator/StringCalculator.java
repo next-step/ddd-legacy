465218b10225390095b1d199f6e42c89467a9100
hoa0217 <gjwjdghk123@naver.com>
2024-05-01 22:55:44 +0900

    feature(string-calculator): implement split

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,8 +1,12 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.util.StringUtils;
 
 public class StringCalculator {
+
+    private static final String regex = ",|:";
 
     public int add(String text) {
         if (!StringUtils.hasLength(text)) {
@@ -12,4 +16,10 @@ public class StringCalculator {
         return Integer.parseInt(text);
     }
 
+    private List<Integer> splitNumbers(String text) {
+        String[] tokens = text.split(regex);
+        return Arrays.stream(tokens)
+                .map(Integer::parseInt)
+                .toList();
+    }
 }
