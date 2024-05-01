a804710411c3210a1811e601c1bd8206467ea5fe
hoa0217 <gjwjdghk123@naver.com>
2024-05-02 02:59:40 +0900

    refactor(string-calculator): make Delimiter
    - Value Object, 일급컬렉션 생성

---
+++ src/main/java/calculator/Delimiter.java
@@ -0,0 +1,17 @@
+package calculator;
+
+public class Delimiter {
+    private String delimiter;
+
+    public Delimiter(String delimiter) {
+        this.delimiter = delimiter;
+    }
+
+    public void addDelimiter(String delimiter){
+        this.delimiter = this.delimiter + "|" + delimiter;
+    }
+
+    public String[] split(String text){
+        return text.split(delimiter);
+    }
+}
--- src/main/java/calculator/SplitterUtils.java
+++ src/main/java/calculator/SplitterUtils.java
@@ -10,16 +10,13 @@ public class SplitterUtils {
 
     public static String[] split(String text) {
         Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(text);
+        Delimiter delimiter = new Delimiter(DEFAULT_DELIMITER);
         if (matcher.find()) {
-            String customDelimiter = makeCustomDelimiter(matcher.group(1));
+            delimiter.addDelimiter(matcher.group(1));
             String targetText = matcher.group(2);
-            return targetText.split(customDelimiter);
+            return delimiter.split(targetText);
         }
 
-        return text.split(DEFAULT_DELIMITER);
-    }
-
-    private static String makeCustomDelimiter(String delimiter) {
-        return DEFAULT_DELIMITER + "|" + delimiter;
+        return delimiter.split(text);
     }
 }
