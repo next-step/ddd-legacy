b9b793c6cb85b01b1a6abfabae75b1dbdb83b5a8
wsdf25867 <wsdf25867@gmail.com>
2024-05-02 08:20:14 +0900

    feat(stringcalculator): PositiveNumber 새 생성자 적용

--- src/test/java/stringcalculator/StringCalculator.java
+++ src/test/java/stringcalculator/StringCalculator.java
@@ -5,23 +5,24 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator {
+
+    private static final Pattern COSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+
     public int add(String text) {
         if (text == null || text.isEmpty()) {
             return 0;
         }
 
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        Matcher m = COSTOM_DELIMITER_PATTERN.matcher(text);
         if (m.find()) {
             String customDelimiter = m.group(1);
             String[] tokens = m.group(2).split(customDelimiter);
             return Arrays.stream(tokens)
-                    .map(Integer::parseInt)
                     .map(PositiveNumber::new)
                     .reduce(PositiveNumber.ZERO, PositiveNumber::sum).intValue();
         }
 
         return Arrays.stream(text.split("[,:]"))
-                .map(Integer::parseInt)
                 .map(PositiveNumber::new)
                 .reduce(PositiveNumber.ZERO, PositiveNumber::sum).intValue();
     }
