2b2c65b142cf49032a906d4da734d476dffc054c
writer0713 <writer0713@naver.com>
2024-05-01 19:06:03 +0900

    feat: 커스텀 구분자 지정 가능 - "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.

--- src/main/java/calculator/StringCalculator.kt
+++ src/main/java/calculator/StringCalculator.kt
@@ -1,6 +1,12 @@
 package calculator
 
+import java.util.regex.Pattern
+
 class StringCalculator {
+    companion object {
+        private val customDelimiterPattern: Pattern = Pattern.compile("//(.)\n(.*)")
+    }
+
     fun add(text: String?): Int {
         if (text.isNullOrBlank()) {
             return 0
@@ -12,6 +18,14 @@ class StringCalculator {
 
         if (text.length == 1) {
             return text.toInt()
+        }
+
+        val matcher = customDelimiterPattern.matcher(text)
+        if (matcher.find()) {
+            val customDelimiter = matcher.group(1)
+            return matcher.group(2)
+                .split(customDelimiter)
+                .sumOf { it.toInt() }
         }
 
         return -1
