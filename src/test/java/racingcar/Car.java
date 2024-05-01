fe809b0d2f2854884432bde4c61c3b220339abb9
OhJeUng <bioman3238@gmail.com>
2024-05-01 23:09:33 +0900

    test(racingcar) : 값 4이상이어야 움직임

--- src/test/java/racingcar/Car.java
+++ src/test/java/racingcar/Car.java
@@ -1,13 +1,42 @@
 package racingcar;
 
+import java.util.Random;
+
 public class Car {
   private String name;
+  private int position;
+  private int currentPosition;
 
-  public Car(String name) {
+  public Car(String name, int position) {
     if (name.length() > 5) {
       throw new IllegalArgumentException();
     }
 
     this.name = name;
+    this.position = position;
+    this.currentPosition = position;
+  }
+
+  public void setPosition(int random) {
+    if (random > 4) {
+      this.position++;
+    }
+  }
+
+  public int getPosition() {
+    return position;
+  }
+
+  public int getCurrentPosition() {
+    return currentPosition;
+  }
+
+  public boolean isMoving() {
+    if (this.position == this.currentPosition) {
+      return false;
+    }
+
+    this.currentPosition = this.position;
+    return true;
   }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,5 +1,6 @@
 package racingcar;
 
+import java.util.Random;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,7 +13,20 @@ class CarTest {
     Assertions.assertThatIllegalArgumentException()
             .isThrownBy(() -> new Car("동해물과백두산이", 0));
   }
-
+  
   @Test
-  void name() {}
+  @DisplayName("무작위_값_4이상_인_경우에만_자동차가_움직인다")
+  void 무작위_값_4이상_인_경우에만_자동차가_움직인다() {
+    final int condition = new Random().nextInt(10);
+
+    final Car car = new Car("움직인다", 0);
+    car.setPosition(condition);
+
+    if (car.getPosition() > car.getCurrentPosition()) {
+      Assertions.assertThat(car.isMoving()).isTrue();
+
+    } else {
+      Assertions.assertThat(car.isMoving()).isFalse();
+    }
+  }
 }
