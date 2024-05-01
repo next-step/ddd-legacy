b754ef954652fee133713d8073b41562ab042e9f
doorisopen <doorwasopen@gmail.com>
2024-05-01 18:57:32 +0900

    refactor(calculator): refactoring StringCalculator

--- src/test/java/calculator/StringCalculator.java
+++ src/test/java/calculator/StringCalculator.java
@@ -1,31 +1,51 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringCalculator implements Calculator<Integer, String> {
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
+
+    private final List<String> delimiters = new ArrayList<>(List.of(",", ":"));
+
+    public StringCalculator() {
+    }
 
     @Override
     public Integer calculate(String input) {
         if (input == null || "".equals(input)) {
             return 0;
         }
-        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(input);
-        String delimiter = ",|:";
+        input = addCustomDelimiter(input);
+        String[] parts = split(input);
+        validate(parts);
+        return Arrays.stream(parts)
+                .mapToInt(Integer::parseInt)
+                .sum();
+    }
+
+    private String addCustomDelimiter(String input) {
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
         if (matcher.find()) {
-            delimiter += "|" + matcher.group(1);
-            input = matcher.group(2);
+            delimiters.add(matcher.group(1));
+            return matcher.group(2);
         }
-        String[] parts = input.split(delimiter);
+        return input;
+    }
+
+    private String[] split(String input) {
+        return input.split(String.join("|", delimiters));
+    }
+
+    private void validate(String[] parts) {
         boolean isValidPattern = Arrays.stream(parts)
                 .flatMapToInt(String::chars)
                 .allMatch(Character::isDigit);
         if (!isValidPattern) {
             throw new RuntimeException("숫자 이외의 값은 허용하지 않습니다.");
         }
-        return Arrays.stream(parts)
-                .mapToInt(Integer::parseInt)
-                .sum();
     }
 }
