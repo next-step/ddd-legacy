7cc0c2646f505cd97d04e80552d619dee1794e9a
dongyub.park <dongyub.park@42dot.ai>
2024-05-02 08:02:46 +0900

    feat(racingcar): add car's name constraints

--- src/main/java/racingcar/domain/Car.java
+++ src/main/java/racingcar/domain/Car.java
@@ -2,15 +2,26 @@ package racingcar.domain;
 
 public class Car {
 	static final int CAR_NAME_MAX_LENGTH = 5;
-	static final String CAR_NAME_EXCEEDED_MAXIMUM_LENGTH = "자동차 이름은 5글자를 넘을 수 없습니다.";
+	static final String CAR_NAME_SHOULD_NOT_BE_EMPTY = "자동차 이름이 존재해야 합니다.";
+	static final String CAR_NAME_SHOULD_NOT_EXCEED_MAXIMUM_LENGTH = "자동차 이름은 5글자를 넘을 수 없습니다.";
 
 	private final String name;
+	private int position;
 
-	public Car(String name) {
+	public Car(final String name) {
+		if (name == null || name.isBlank()) {
+			throw new IllegalArgumentException(CAR_NAME_SHOULD_NOT_BE_EMPTY);
+		}
+
 		if (name.length() > CAR_NAME_MAX_LENGTH) {
-			throw new IllegalArgumentException(CAR_NAME_EXCEEDED_MAXIMUM_LENGTH);
+			throw new IllegalArgumentException(CAR_NAME_SHOULD_NOT_EXCEED_MAXIMUM_LENGTH);
 		}
 
 		this.name = name;
+		this.position = 0;
+	}
+
+	public int position() {
+		return position;
 	}
 }
--- src/test/java/racingcar/domain/CarTest.java
+++ src/test/java/racingcar/domain/CarTest.java
@@ -4,14 +4,30 @@ import static org.assertj.core.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class CarTest {
 
 	@ParameterizedTest
+	@DisplayName("자동차 이름은 null 값을 가지거나 비어있을 수 없다.")
+	@NullAndEmptySource
+	@ValueSource(strings = {" ", "  "})
+	void constructorWithEmptyCarNames(String carName) {
+		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new Car(carName));
+	}
+
+	@ParameterizedTest
 	@DisplayName("자동차 이름은 5글자를 넘을 수 없다.")
 	@ValueSource(strings = {"동해물과백두", "동해물과백두산"})
+	void constructorWithCarNamesExceedMaximumLength(String carName) {
+		assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new Car(carName));
+	}
+
+	@ParameterizedTest
+	@DisplayName("자동차는 5글자 이하의 이름을 가질 수 있다.")
+	@ValueSource(strings = {"동", "동해", "동해물", "동해물과", "동해물과백"})
 	void constructor(String carName) {
-		assertThatIllegalArgumentException().isThrownBy(() -> new Car(carName));
+		assertThatCode(() -> new Car(carName)).doesNotThrowAnyException();
 	}
 }
