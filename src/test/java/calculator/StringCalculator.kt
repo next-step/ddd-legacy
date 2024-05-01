dd040087342dc8caa17483cf21606ed38a185b14
jkshin0602 <sjk6437@gmail.com>
2024-05-01 14:16:30 +0900

    refactor: 매직넘버 제거 및 메시지, 정규식 패턴 상수화

--- src/test/java/calculator/StringCalculator.kt
+++ src/test/java/calculator/StringCalculator.kt
@@ -5,10 +5,10 @@ import java.util.regex.Pattern
 class StringCalculator {
     fun add(text: String?): Int {
         if (text.isNullOrBlank()) {
-            return 0
+            return EMPTY_RESULT
         }
 
-        return Pattern.compile("//(.)\n(.*)").matcher(text)
+        return Pattern.compile(EXTRACT_CUSTOM_DELIMITER_REGEX).matcher(text)
             .let { matcher ->
                 when {
                     matcher.find() -> {
@@ -18,11 +18,23 @@ class StringCalculator {
                             .sumOf { it.toInt() }
                     }
 
-                    else -> text.split(regex = "[,|:]".toRegex())
-                        .sumOf { s ->
-                            s.toInt().takeIf { it > -1 } ?: throw RuntimeException("음수를 입력할 수 없습니다.")
+                    else -> text.split(regex = DEFAULT_DELIMITER.toRegex())
+                        .sumOf { split ->
+                            split.toInt()
+                                .takeIf { it > NEGATIVE_ONE }
+                                ?: throw RuntimeException(ERROR_MESSAGE_NEGATIVE_NUMBER)
                         }
                 }
             }
+    }
+
+    companion object Constant {
+        private const val DEFAULT_DELIMITER = "[,|:]"
+        private const val EXTRACT_CUSTOM_DELIMITER_REGEX = "//(.)\\n(.*)"
+
+        private const val ERROR_MESSAGE_NEGATIVE_NUMBER = "음수를 입력할 수 없습니다."
+
+        private const val EMPTY_RESULT = 0
+        private const val NEGATIVE_ONE = -1
     }
 }
