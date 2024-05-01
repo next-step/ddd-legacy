d99807b8553f850f83e5ad55dcc5a1d09f88e94b
jinan159 <wlsdhks0423@naver.com>
2024-05-02 00:08:05 +0900

    chore: kotest 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,7 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.kotest:kotest-runner-junit5:5.8.1")
 }
 
 tasks.withType<KotlinCompile> {
