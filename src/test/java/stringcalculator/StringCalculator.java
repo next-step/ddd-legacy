e6a6b20dad9c227c167a32064e2bc00e95ff8112
wsdf25867 <wsdf25867@gmail.com>
2024-05-03 08:21:02 +0900

    refactor(stringcalculator): StringSplitter 필드로

--- src/test/java/stringcalculator/StringCalculator.java
+++ src/test/java/stringcalculator/StringCalculator.java
@@ -4,11 +4,21 @@ import java.util.Arrays;
 
 public class StringCalculator {
 
+    private final StringSplitter splitter;
+
+    public StringCalculator() {
+        this(new StringSplitter());
+    }
+
+    public StringCalculator(StringSplitter splitter) {
+        this.splitter = splitter;
+    }
+
     public int add(String text) {
         if (text == null || text.isEmpty()) {
             return 0;
         }
-        return Arrays.stream(new StringSplitter().split(text))
+        return Arrays.stream(splitter.split(text))
                 .map(PositiveNumber::new)
                 .reduce(PositiveNumber.ZERO, PositiveNumber::sum).intValue();
     }
