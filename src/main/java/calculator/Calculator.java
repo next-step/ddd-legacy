a1adf47a9938e6d96e6644874ef7f0def38aa21f
sjhello <sjhello235@gmail.com>
2024-05-02 23:00:18 +0900

    feat: 숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,9 +1,17 @@
 package calculator;
 
+import java.util.Arrays;
+
 public class Calculator {
 
     public int add(String text) {
         if (text == null || text.isEmpty()) return 0;
+
+        if (text.contains(",")) {
+            String[] numbers = text.split(",");
+            return Arrays.stream(numbers).mapToInt(Integer::parseInt).sum();
+        }
+
         return Integer.parseInt(text);
     }
 }
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -4,7 +4,9 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -29,6 +31,13 @@ class CalculatorTest {
     @DisplayName("숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다")
     void stringToInt() {
         assertThat(calculator.add("2")).isEqualTo(2);
+    }
+
+    @ParameterizedTest
+    @DisplayName("숫자 두개를 컴마(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.")
+    @CsvSource(value = {"1,2:3", "4,5:9"}, delimiter = ':')
+    void comma(String text, int expected) {
+        assertThat(calculator.add(text)).isSameAs(expected);
     }
 
 }
