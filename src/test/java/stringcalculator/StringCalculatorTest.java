0e9f9d045da2993c2ac82dad086019b926f0d5df
jmpark <ppjjmmhp@gmail.com>
2024-05-03 00:10:20 +0900

    refactor: add text case for requirement
    1) 문자열 계산기에 숫자가 아닌 문자를 전달하는 경우 RuntimeException 예외 처리를 한다.
    2) //와 \n 문자 사이에 커스텀 여러 문자로 구성된 구분자를 지정할 수 있다.
    
    etc: other case add parameter

--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -27,7 +27,7 @@ public class StringCalculatorTest {
 
     @DisplayName(value = "숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"1"})
+    @ValueSource(strings = {"1", "100", "0"})
     void oneNumber(final String text) {
         assertThat(calculator.add(text)).isSameAs(Integer.parseInt(text));
     }
@@ -41,22 +41,40 @@ public class StringCalculatorTest {
 
     @DisplayName(value = "구분자를 쉼표(,) 이외에 콜론(:)을 사용할 수 있다.")
     @ParameterizedTest
-    @ValueSource(strings = {"1,2:3"})
+    @ValueSource(strings = {"1,2:3", "1,2,3", "1:2:3"})
     void colons(final String text) {
         assertThat(calculator.add(text)).isSameAs(6);
     }
 
-    @DisplayName(value = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
+    @DisplayName(value = "//와 \\n 문자 사이에 커스텀 단일 문자로 된 구분자를 지정할 수 있다.")
     @ParameterizedTest
-    @ValueSource(strings = {"//;\n1;2;3"})
+    @ValueSource(strings = {"//;\n1;2;3", "//E\n1E2E3", "//+\n1+2+3", "//@\n1@2@3"})
     void customDelimiter(final String text) {
         assertThat(calculator.add(text)).isSameAs(6);
     }
 
+
+    @DisplayName(value = "//와 \\n 문자 사이에 커스텀 여러 문자로 구성된 구분자를 지정할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"//lol\n1lol2lol3", "//EE\n1EE2EE3", "//+_\n1+_2+_3", "//ER\n1ER2ER3"})
+    void customDelimiter2(final String text) {
+        assertThat(calculator.add(text)).isSameAs(6);
+    }
+
+
     @DisplayName(value = "문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리를 한다.")
-    @Test
-    void negative() {
+    @ParameterizedTest
+    @ValueSource(strings = {"-1", "//;\n-1;1;1", "1,-1,1", "1:1:-1", "1:-1,1", "1:1,-1", "-1:-1,-1", "-1:-1,1"})
+    void negative(final String text) {
         assertThatExceptionOfType(RuntimeException.class)
-                .isThrownBy(() -> calculator.add("-1"));
+                .isThrownBy(() -> calculator.add(text));
+    }
+
+    @DisplayName(value = "문자열 계산기에 숫자가 아닌 문자를 전달하는 경우 RuntimeException 예외 처리를 한다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\na;b;c", "a,b,c", "a:b:c", "a:b,c", "1:b,c", "a:b,1", "1:b,1"})
+    void notInteger(final String text) {
+        assertThatExceptionOfType(RuntimeException.class)
+                .isThrownBy(() -> calculator.add(text));
     }
 }
