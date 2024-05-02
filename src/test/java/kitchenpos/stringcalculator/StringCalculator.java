d58489f225b20f436d1deb0e8a8d23356aa631be
안주형 <ajh1016@midasin.com>
2024-05-02 14:34:46 +0900

    style: 주석 제거

--- src/test/java/kitchenpos/stringcalculator/StringCalculator.java
+++ src/test/java/kitchenpos/stringcalculator/StringCalculator.java
@@ -25,20 +25,10 @@ public class StringCalculator {
         return sumNumbers(splitNumbers(input, DEFAULT_DELIMITERS));
     }
 
-    /**
-     * 입력된 문자열을 주어진 구분자 패턴을 사용해 분리한다.
-     * <p>
-     * 구분자를 기준으로 문자열을 여러 숫자 문자열로 나누고, 이를 배열로 반환한다.
-     */
     private String[] splitNumbers(String input, String delimiterPattern) {
         return input.split(delimiterPattern);
     }
 
-    /**
-     * 분리된 숫자 문자열을 합산해 최종 결과를 반환한다.
-     * <p>
-     * 음수거나 숫자가 아닌 문자가 포함되어 있다면 IllegalArgumentException을 던진다.
-     */
     private int sumNumbers(String[] numberStrings) {
         return Arrays.stream(numberStrings)
                      .mapToInt(Integer::parseInt)
