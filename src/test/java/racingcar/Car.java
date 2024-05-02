45b5dd9dfb8e76899bea88da635e71ed972484b7
MoonHKLee <mf311110@gmail.com>
2024-05-03 00:28:20 +0900

    feature(racingcar) : 레이싱카 이름 관련 기능 구현.

---
+++ src/test/java/racingcar/Car.java
@@ -0,0 +1,17 @@
+package racingcar;
+
+public class Car {
+    private String name;
+    private int movingDistance;
+
+    public Car(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("자동차 이름은 null 이거나 빈 값일 수 없다.");
+        }
+        if (name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5 글자를 넘을 수 없다.");
+        }
+        this.name = name;
+        this.movingDistance = 0;
+    }
+}
