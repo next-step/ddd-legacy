3d30caef9fd3dda8453c62688adfa1b94a5b7ab0
hoa0217 <gjwjdghk123@naver.com>
2024-05-02 20:43:49 +0900

    refactor(string-calculator): implement equals in PositiveNumber

--- src/main/java/calculator/PositiveNumber.java
+++ src/main/java/calculator/PositiveNumber.java
@@ -1,6 +1,7 @@
 package calculator;
 
 import calculator.exception.NegativeNumberException;
+import java.util.Objects;
 
 public class PositiveNumber {
 
@@ -28,5 +29,22 @@ public class PositiveNumber {
 
     public int getNumber() {
         return number;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        PositiveNumber that = (PositiveNumber) o;
+        return number == that.number;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
