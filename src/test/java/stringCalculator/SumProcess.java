004387b993e138d6750759042fe9c5f4295740fc
jinyoonYang <didwlsdbs123@naver.com>
2024-05-02 23:19:57 +0900

    feat(stringCalculator): stringCalculator 기능 구현

--- src/test/java/stringCalculator/SumProcess.java
+++ src/test/java/stringCalculator/SumProcess.java
@@ -1,15 +1,32 @@
 package stringCalculator;
 
-import java.util.List;
+import java.util.Arrays;
 
 public class SumProcess {
+    private final Input input;
 
-    public SumProcess() {
+    public SumProcess(String text) {
+        input = new Input(text);
     }
 
-    public int sum(List<PositiveNumber> numbers){
-        return numbers.stream()
-                .mapToInt(PositiveNumber::getNumber)
+    public int sum(){
+        return Arrays.stream(input.getInputStrings())
+                .filter(a -> this.isPositiveNumber(a))
+                .mapToInt(a -> Integer.parseInt(a))
                 .sum();
+    }
+
+    private static boolean isPositiveNumber(String input) {
+        try {
+            int num = Integer.parseInt(input);
+
+            if (num < 0) {
+                throw new RuntimeException("input included negative number");
+            }
+
+            return true;
+        } catch (Exception e) {
+            throw e;
+        }
     }
 }
