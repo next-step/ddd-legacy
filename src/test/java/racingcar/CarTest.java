0f14fa7d226e36da00c9ba73a61b1fe9dd20a29b
Steve Shim <nightpw1@gmail.com>
2024-05-03 01:23:10 +0900

    test(step0): making 1st constraint

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,6 +1,17 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class CarTest {
 
-
+    @Test
+    @DisplayName("제약조건 1: 이름이 5글자가 넘을 수 없다")
+    public void checkTheNameConstraint() {
+        assertThatThrownBy(
+                () -> new Car("아주멋진차량 과학5호기")
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
 }
