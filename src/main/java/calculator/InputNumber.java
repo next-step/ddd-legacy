b2f434451a9aefeba538627f862d1e44ed207e41
dkgell13 <dkgell13@gmail.com>
2024-05-02 23:47:19 +0900

    feat(calculator): 입력된 값을 처리하는 InputNumber class 생성

---
+++ src/main/java/calculator/InputNumber.java
@@ -0,0 +1,34 @@
+package calculator;
+
+
+public class InputNumber {
+    private String inputNumber;
+
+    public InputNumber(String inputNumber){
+        validationNumber(inputNumber);
+        this.inputNumber = inputNumber;
+    }
+
+    public String getNumber(){
+        return this.inputNumber;
+    }
+
+    private void validationNumber(String inputNumber){
+        if(!isNumeric(inputNumber)){
+            throw new RuntimeException("숫자가 아닙니다.");
+        }
+        if(!isPositive(inputNumber)){
+            throw new RuntimeException("음수는 처리할 수 없습니다.");
+        }
+    }
+
+    private boolean isNumeric(String inputNumber){
+        return inputNumber.matches("[0-9]+");
+    }
+    private boolean isPositive(String inputNumber){
+        int number = Integer.parseInt(inputNumber);
+        return (number > 0);
+    }
+
+
+}
