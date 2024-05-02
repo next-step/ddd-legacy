d6308bf624f42fadc168a9457cf7131224959572
wsdf25867 <wsdf25867@gmail.com>
2024-05-03 08:19:31 +0900

    refactor(stringcalculator): add 메서드 한개로

--- src/test/java/stringcalculator/StringCalculator.java
+++ src/test/java/stringcalculator/StringCalculator.java
@@ -5,14 +5,10 @@ import java.util.Arrays;
 public class StringCalculator {
 
     public int add(String text) {
-        return add(text, new StringSplitter());
-    }
-
-    public int add(String text, StringSplitter splitter) {
         if (text == null || text.isEmpty()) {
             return 0;
         }
-        return Arrays.stream(splitter.split(text))
+        return Arrays.stream(new StringSplitter().split(text))
                 .map(PositiveNumber::new)
                 .reduce(PositiveNumber.ZERO, PositiveNumber::sum).intValue();
     }
