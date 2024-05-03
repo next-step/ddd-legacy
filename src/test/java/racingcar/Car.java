a9c3ae274ae9f0baffbb18b38962fea39062fced
rftengineer <nightpw1@naver.com>
2024-05-03 23:06:48 +0900

    0단계 - Junit 5 (#615)
    * style(step0): making package outline
    
    * test(step0): making 1st constraint
    
    * test(step0): making failure test for implementing moving method in Car class
    
    * feat(step0): movementStrategy implementation
    
    * feat(step0): set a default movement strategy as CarMovementStrategy
    
    ---------
    
    Co-authored-by: Steve Shim <nightpw1@gmail.com>

---
+++ src/test/java/racingcar/Car.java
@@ -0,0 +1,20 @@
+package racingcar;
+
+public class Car {
+    private String name;
+
+    public Car(String name) {
+        if(name.length() >= 5) {
+            throw new IllegalArgumentException("자동차 이름은 5글자를 넘을 수 없다.");
+        }
+        this.name = name;
+    }
+
+
+    public boolean isMoving(MovementStrategy ms) {
+        if(ms == null) {
+            return new CarMovementStrategy().move();
+        }
+        return ms.move();
+    }
+}
---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,33 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+
+    @Test
+    @DisplayName("제약조건 1: 이름이 5글자가 넘을 수 없다")
+    public void checkTheNameConstraint() {
+        assertThatThrownBy(
+                () -> new Car("아주멋진차량 과학5호기")
+        ).isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차 이름은 5글자를 넘을 수 없다");
+    }
+
+    @Test
+    @DisplayName("자동차의 움직임 메서드 구현")
+    public void checkThePositionConstraint() {
+        Car k5 = new Car("K5");
+        assertThat(k5.isMoving(() -> true)).isEqualTo(true);
+    }
+
+    @Test
+    @DisplayName("Default Parameter가 반영되어 NPE 발생 안하는 것을 확인")
+    public void checkMovementStrategy() {
+        Car k8 = new Car("K8");
+        assertThat(k8.isMoving(null)).isNotNull();
+    }
+}
