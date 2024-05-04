0cf3f04dcf25da3242c0391484aa187e02932934
tongnamuu <thban33@naver.com>
2024-05-04 20:39:07 +0900

    feat: step1 숫자가 정상적으로 들어오는 경우에 대한 합 처리

--- src/test/java/stringcalculator/StringCalculator.java
+++ src/test/java/stringcalculator/StringCalculator.java
@@ -1,9 +1,17 @@
 package stringcalculator;
 
+import java.util.Arrays;
+import java.util.Optional;
+
 public class StringCalculator {
     public int  add(String text) {
         if (text == null || text.isBlank()) {
             return 0;
+        }
+        String[] split = text.split(",");
+        Optional<Integer> result = Arrays.stream(split).map(Integer::parseInt).reduce((a, b) -> a + b);
+        if (result.isPresent()) {
+            return result.get();
         }
         return -1;
     }
