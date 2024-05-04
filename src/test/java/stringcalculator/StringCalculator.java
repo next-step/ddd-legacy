957002bd6217a03fdf076ed2185b9ab8778e56d4
tongnamuu <thban33@naver.com>
2024-05-04 20:47:44 +0900

    feat: step1 음수가 들어오면 RuntimeException 발생

--- src/test/java/stringcalculator/StringCalculator.java
+++ src/test/java/stringcalculator/StringCalculator.java
@@ -1,20 +1,23 @@
 package stringcalculator;
 
 import java.util.Arrays;
-import java.util.Optional;
+import java.util.List;
 
 public class StringCalculator {
     private static final String DELIMITERS = "[,:]";
 
-    public int  add(String text) {
+    public int add(String text) {
         if (text == null || text.isBlank()) {
             return 0;
         }
         String[] split = text.split(DELIMITERS);
-        Optional<Integer> result = Arrays.stream(split).map(Integer::parseInt).reduce((a, b) -> a + b);
-        if (result.isPresent()) {
-            return result.get();
+        List<Integer> result = Arrays.stream(split)
+                                     .map(Integer::parseInt)
+                                     .toList();
+
+        if (result.stream().anyMatch(it -> it < 0)) {
+            throw new RuntimeException("음수는 처리하지 않습니다");
         }
-        return -1;
+        return result.stream().reduce(0, Integer::sum);
     }
 }
