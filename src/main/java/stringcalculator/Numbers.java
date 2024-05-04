2f301f0b4a3f9f1d33366cdd6a509f80d03daaea
joydaheecha <daheecha0612@gmail.com>
2024-05-04 11:05:36 +0900

    adjust: number 객체 분리하라

--- src/main/java/stringcalculator/Numbers.java
+++ src/main/java/stringcalculator/Numbers.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public record Numbers(
-        List<Integer> numbers
+        List<Number> numbers
 ) {
     private static final int CALCULATION_RESULT_ZERO = 0;
     private static final String EMPTY_USER_INPUT = "";
@@ -15,7 +15,7 @@ public record Numbers(
         this(initNumbers(userInput));
     }
 
-    private static List<Integer> initNumbers(String userInput) {
+    private static List<Number> initNumbers(String userInput) {
         if (isUserInputEmpty(userInput)) {
             return Collections.emptyList();
         }
@@ -24,32 +24,13 @@ public record Numbers(
 
         var results = userInputToSplit.split(delimiter.delimiter());
 
-        validateUserInput(results);
-
         return Arrays.stream(results)
-                .map(Integer::valueOf)
+                .map(Number::of)
                 .toList();
     }
 
     private static boolean isUserInputEmpty(String userInput) {
         return EMPTY_USER_INPUT.equals(userInput) || userInput == null;
-    }
-
-    private static void validateUserInput(String[] results) {
-        Arrays.stream(results)
-                .filter(s -> isNotNumber(s) || isNegativeNumber(s))
-                .findFirst()
-                .ifPresent(s -> {
-                    throw new RuntimeException("0 혹은 양수만 입력 가능합니다.");
-                });
-    }
-
-    private static boolean isNegativeNumber(String s) {
-        return Integer.parseInt(s) < 0;
-    }
-
-    private static boolean isNotNumber(String s) {
-        return !s.chars().allMatch(Character::isDigit);
     }
 
     private static String getUserInputToSplit(String userInput, Delimiter delimiter) {
@@ -63,6 +44,8 @@ public record Numbers(
         if (numbers.isEmpty()) {
             return CALCULATION_RESULT_ZERO;
         }
-        return this.numbers.stream().mapToInt(Integer::intValue).sum();
+        return this.numbers.stream()
+                .mapToInt(Number::number)
+                .sum();
     }
 }
