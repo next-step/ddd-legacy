9e4212831ea1119e0e268f58a9eb6be1d6998877
tongnamuu <thban33@naver.com>
2024-05-04 20:42:58 +0900

    feat: step1 구분자로 콜론을 사용한다

--- src/test/java/stringcalculator/StringCalculator.java
+++ src/test/java/stringcalculator/StringCalculator.java
@@ -4,11 +4,13 @@ import java.util.Arrays;
 import java.util.Optional;
 
 public class StringCalculator {
+    private static final String DELIMITERS = "[,:]";
+
     public int  add(String text) {
         if (text == null || text.isBlank()) {
             return 0;
         }
-        String[] split = text.split(",");
+        String[] split = text.split(DELIMITERS);
         Optional<Integer> result = Arrays.stream(split).map(Integer::parseInt).reduce((a, b) -> a + b);
         if (result.isPresent()) {
             return result.get();
