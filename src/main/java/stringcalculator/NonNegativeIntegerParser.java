1a724b8cd066dd01feb4169f069e2cfcf407a676
dongyub.park <ninjasul@gmail.com>
2024-05-05 13:11:39 +0900

    refactor(stringcalculator): change magic numbers to constants

--- src/main/java/stringcalculator/NonNegativeIntegerParser.java
+++ src/main/java/stringcalculator/NonNegativeIntegerParser.java
@@ -7,6 +7,9 @@ import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class NonNegativeIntegerParser {
+	private static final int DELIMITER_GROUP_INDEX = 1;
+	private static final int OPERAND_GROUP_INDEX = 2;
+
 	private static final Pattern INPUT_PATTERN = Pattern.compile("//(.)\n(.*)");
 
 	private static final String DELIMITER_REGEX = ",|:";
@@ -28,8 +31,8 @@ public class NonNegativeIntegerParser {
 		Matcher matcher = INPUT_PATTERN.matcher(input);
 
 		if (matcher.find()) {
-			String customDelimiter = matcher.group(1);
-			return matcher.group(2).split(Pattern.quote(customDelimiter));
+			String customDelimiter = matcher.group(DELIMITER_GROUP_INDEX);
+			return matcher.group(OPERAND_GROUP_INDEX).split(Pattern.quote(customDelimiter));
 		}
 
 		return input.split(DELIMITER_REGEX);
