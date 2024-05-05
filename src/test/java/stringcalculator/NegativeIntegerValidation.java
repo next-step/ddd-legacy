30ed9d2f633767e3441eb3a3e8db0164f5c0bb78
jmpark <ppjjmmhp@gmail.com>
2024-05-05 15:24:23 +0900

    refactor: add NonNegativeInteger Class

--- src/test/java/stringcalculator/NegativeIntegerValidation.java
+++
@@ -1,9 +0,0 @@
-package stringcalculator;
-
-public class NegativeIntegerValidation {
-    public static void checkForNegative(int num) {
-        if (num < 0) {
-            throw new RuntimeException("Negative integer found: " + num);
-        }
-    }
-}
