7d864cb6c0dd6d046dec0b80252ccdad914ce1d8
Steve Shim <nightpw1@gmail.com>
2024-05-05 14:27:47 +0900

    feat(step1): implements add function for pass the test.

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,7 +1,35 @@
 package stringcalculator;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     public int add(String text) {
-        return 0;
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+
+        String[] values = new String[0];
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            values = m.group(2).split(customDelimiter);
+        } else {
+            values = text.split("[,:]");
+        }
+
+        long negativeNumberCnt = Arrays.stream(values)
+                .mapToInt(Integer::parseInt)
+                .filter(val -> val < 0)
+                .count();
+        if (negativeNumberCnt > 0) {
+            throw new RuntimeException();
+        }
+
+        return Arrays.stream(values)
+                .mapToInt(Integer::parseInt)
+                .sum();
     }
 }
