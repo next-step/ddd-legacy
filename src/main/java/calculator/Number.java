ebbea4148239b74f553701db7dfd7b8eda55f024
sjhello <sjhello235@gmail.com>
2024-05-05 11:51:37 +0900

    refactor: Number 객체 수정
     - number 필드를 final로 선언한다.
     - equals & hashCode를 활용하여 동등성을 부여한다.

--- src/main/java/calculator/Number.java
+++ src/main/java/calculator/Number.java
@@ -1,8 +1,10 @@
 package calculator;
 
+import java.util.Objects;
+
 public class Number {
 
-    private String number;
+    private final String number;
 
     public Number(String number) {
         validation(number);
@@ -15,5 +17,18 @@ public class Number {
 
     private void validation(String input) {
         if (Integer.parseInt(input) < 0) throw new IllegalArgumentException("입력값은 음수 일 수 없습니다.");
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Number number1 = (Number) o;
+        return Objects.equals(number, number1.number);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(number);
     }
 }
--- src/test/java/calculator/NumberTest.java
+++ src/test/java/calculator/NumberTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,11 +9,23 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NumberTest {
 
+    private Number number;
+
+    @BeforeEach
+    void setUp() {
+        number = new Number("20");
+    }
+
     @Test
     @DisplayName("숫자를 생성한다")
     void createNumber() {
-        Number number = new Number("20");
-        assertThat(number.getNumber()).isEqualTo(20);
+        assertThat(number).isEqualTo(new Number("20"));
+    }
+
+    @Test
+    @DisplayName("정수형 숫자를 리턴한다")
+    void getIntegerNumber() {
+        assertThat(number.getNumber()).isSameAs(20);
     }
 
     @Test
