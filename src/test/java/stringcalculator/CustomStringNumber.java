d3a98a4e8f20f22f776f1cf6d77bf3cb18396296
OhJeUng <bioman3238@gmail.com>
2024-05-06 22:49:12 +0900

    fix(stringcalculator) : isNumber 역할 변경

--- src/test/java/stringcalculator/CustomStringNumber.java
+++ src/test/java/stringcalculator/CustomStringNumber.java
@@ -1,9 +1,6 @@
 package stringcalculator;
 
-import java.util.regex.Pattern;
-
 public class CustomStringNumber {
-  private static final Pattern STRING_NUMBER_PATTERN = Pattern.compile("-?\\d+(\\.\\d+)?");
   private final Integer number;
 
   public CustomStringNumber(String stringNumber) {
@@ -11,7 +8,7 @@ public class CustomStringNumber {
   }
 
   private Integer conversion(String stringNumber) {
-    if (this.isNumber(stringNumber)) {
+    if (DelimiterSplit.isNumber(stringNumber)) {
       final int number = Integer.parseInt(stringNumber);
       CustomStringNumberValidator.negativeNumberValid(number);
 
@@ -19,20 +16,6 @@ public class CustomStringNumber {
     }
 
     return 0;
-  }
-
-  /***
-   * <h1>숫자 확인</h1>
-   * <pre>
-   *     isNumber("1")   = true
-   *     isNumber("123") = true
-   *     isNumber("-1")  = true
-   *     isNumber("A")   = false
-   *     isNumber("ABC") = false
-   * </pre>
-   */
-  private boolean isNumber(final String stringNumber) {
-    return STRING_NUMBER_PATTERN.matcher(stringNumber).matches();
   }
 
   public Integer getNumber() {
