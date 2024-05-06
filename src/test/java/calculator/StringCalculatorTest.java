2b83f4a033762797d5ada10cfa4e50341b457a1d
sloth-bear <slothbear.hj@gmail.com>
2024-05-07 06:43:39 +0900

    refactor(sloth-bear): refactor parameterize test arguments

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -3,12 +3,10 @@ package calculator;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.List;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -29,28 +27,48 @@ class StringCalculatorTest {
     }
 
     @ParameterizedTest
-    @MethodSource("singleNumberArguments")
+    @CsvSource({
+        "1, 1",
+        "10, 10",
+    })
     @DisplayName("하나의 숫자를 문자열로 전달 시 해당 숫자를 반환한다.")
     void singleNumber(final String input, final int expected) {
         assertThat(calculator.add(input)).isEqualTo(expected);
     }
 
     @ParameterizedTest
-    @MethodSource("twoNumbersWithComma")
+    @CsvSource({
+        "'1,2', 3",
+        "'4,5', 9",
+        "'4,5,6', 15",
+        "'4,5,6,7', 22",
+        "'100,23,1,2,3', 129",
+        "'999,1,15,5,100,6', 1126"
+    })
     @DisplayName("두 개 이상의 숫자를 쉼표(,)를 구분자로 가지는 문자열로 전달 시 각 숫자의 합을 반환한다.")
     void commaTwoNumbers(final String input, final int expected) {
         assertThat(calculator.add(input)).isEqualTo(expected);
     }
 
     @ParameterizedTest
-    @MethodSource("twoNumbersWithColon")
+    @CsvSource({
+        "'1:2', 3",
+        "'4:5', 9",
+        "'4:5:6', 15",
+        "'4:5:6:7', 22",
+        "'100:23:1:2:3', 129",
+        "'999:1:15:5:100:6', 1126"
+    })
     @DisplayName("두 개 이상의 숫자를 콜론(:)을 구분자로 가지는 문자열로 전달 시 각 숫자의 합을 반환한다.")
     void colonTwoNumbers(final String input, final int expected) {
         assertThat(calculator.add(input)).isEqualTo(expected);
     }
 
     @ParameterizedTest
-    @MethodSource("customSeparatorArguments")
+    @CsvSource({
+        "'//;\n1;2', 3",
+        "'//.\n5.6', 11"
+    })
     @DisplayName("'//'와 '\n' 사이에 커스텀 구분자를 지정할 수 있다.")
     void customSeparator(final String input, final int expected) {
         assertThat(calculator.add(input)).isEqualTo(expected);
@@ -82,39 +100,4 @@ class StringCalculatorTest {
             .isInstanceOf(RuntimeException.class);
     }
 
-    private static List<Arguments> singleNumberArguments() {
-        return List.of(
-            Arguments.of("1", 1),
-            Arguments.of("10", 10)
-        );
-    }
-
-    private static List<Arguments> twoNumbersWithComma() {
-        return List.of(
-            Arguments.of("1,2", 3),
-            Arguments.of("4,5", 9),
-            Arguments.of("4,5,6", 15),
-            Arguments.of("4,5,6,7", 22),
-            Arguments.of("100,23,1,2,3", 129),
-            Arguments.of("999,1,15,5,100,6", 1126)
-        );
-    }
-
-    private static List<Arguments> twoNumbersWithColon() {
-        return List.of(
-            Arguments.of("1:2", 3),
-            Arguments.of("4:5", 9),
-            Arguments.of("4:5:6", 15),
-            Arguments.of("4:5:6:7", 22),
-            Arguments.of("100:23:1:2:3", 129),
-            Arguments.of("999:1:15:5:100:6", 1126)
-        );
-    }
-
-    private static List<Arguments> customSeparatorArguments() {
-        return List.of(
-            Arguments.of("//;\n1;2", 3),
-            Arguments.of("//.\n5.6", 11)
-        );
-    }
 }
