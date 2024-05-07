28ccd1a63ad3e966ada91f10d0f355d6a6b1a55a
deepredk <deepred.kim@gmail.com>
2024-05-07 20:54:28 +0900

    refactor: static 메서드들 인스턴스 메서드로 변경

--- src/test/java/calculator/StringCalculator.java
+++ src/test/java/calculator/StringCalculator.java
@@ -8,31 +8,31 @@ public class StringCalculator {
     public static final String PREFIX_OF_CUSTOM_DELIMITER = "//";
     public static final String SUFFIX_OF_CUSTOM_DELIMITER = "\n";
 
-    public static int calculate(String expression) {
+    public int calculate(String expression) {
         if (expression == null || expression.isEmpty()) {
             return 0;
         }
         return sum(toNumbers(split(expression)));
     }
 
-    private static int sum(int[] numbers) {
+    private int sum(int[] numbers) {
         return Arrays.stream(numbers).sum();
     }
 
-    private static int[] toNumbers(String[] strNumbers) {
+    private int[] toNumbers(String[] strNumbers) {
         return Arrays.stream(strNumbers)
-            .map(StringCalculator::toPositive)
+            .map(this::toPositive)
             .mapToInt(Integer::intValue)
             .toArray();
     }
 
-    private static String[] split(String expression) {
+    private String[] split(String expression) {
         String delimiter = extractDelimiter(expression);
         expression = removeDelimiter(expression);
         return expression.split(delimiter);
     }
 
-    private static String extractDelimiter(String expression) {
+    private String extractDelimiter(String expression) {
         if (!expression.startsWith(PREFIX_OF_CUSTOM_DELIMITER)) {
             return DEFAULT_DELIMITER;
         }
@@ -40,14 +40,14 @@ public class StringCalculator {
         return expression.substring(PREFIX_OF_CUSTOM_DELIMITER.length(), endIndex);
     }
 
-    private static String removeDelimiter(String expression) {
+    private String removeDelimiter(String expression) {
         if (!expression.startsWith(PREFIX_OF_CUSTOM_DELIMITER)) {
             return expression;
         }
         return expression.substring(expression.indexOf(SUFFIX_OF_CUSTOM_DELIMITER) + 1);
     }
 
-    private static int toPositive(String strNumber) {
+    private int toPositive(String strNumber) {
         int number = parseInt(strNumber);
         if (number < 0) {
             throw new RuntimeException("음수는 입력할 수 없습니다.");
@@ -55,7 +55,7 @@ public class StringCalculator {
         return number;
     }
 
-    private static int parseInt(String strNumber) {
+    private int parseInt(String strNumber) {
         try {
             return Integer.parseInt(strNumber);
         } catch (NumberFormatException e) {
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -12,18 +12,20 @@ import org.junit.jupiter.params.provider.ValueSource;
 
 public class StringCalculatorTest {
 
+    private StringCalculator stringCalculator = new StringCalculator();
+
     @ParameterizedTest
     @NullAndEmptySource
     @DisplayName("빈 문자열 또는 null이 들어오면 0을 반환한다")
     void null_or_empty(String input) {
-        int result = StringCalculator.calculate(input);
+        int result = stringCalculator.calculate(input);
         assertThat(result).isEqualTo(0);
     }
 
     @Test
     @DisplayName("숫자 하나가 들어오면 해당 숫자를 반환한다")
     void one_number() {
-        int result = StringCalculator.calculate("1");
+        int result = stringCalculator.calculate("1");
         assertThat(result).isEqualTo(1);
     }
 
@@ -31,7 +33,7 @@ public class StringCalculatorTest {
     @DisplayName("쉼표 또는 콜론을 기준으로 분리한 각 숫자의 합을 반환한다")
     @CsvSource(value = {"1,2|3", "1,2:3|6", "1,2:3,4|10"}, delimiter = '|')
     public void multiple_numbers_comma_or_colon(String input, int expected) {
-        int result = StringCalculator.calculate(input);
+        int result = stringCalculator.calculate(input);
         assertThat(result).isEqualTo(expected);
     }
 
@@ -39,14 +41,14 @@ public class StringCalculatorTest {
     @DisplayName("숫자 이외의 값 또는 음수를 전달하는 경우 RuntimeException을 throw 한다")
     @ValueSource(strings = {"-1", "a"})
     public void negative_or_not_number(String input) {
-        assertThatThrownBy(() -> StringCalculator.calculate(input))
+        assertThatThrownBy(() -> stringCalculator.calculate(input))
             .isInstanceOf(RuntimeException.class);
     }
 
     @Test
     @DisplayName("커스텀 구분자를 사용할 수 있다")
     public void custom_delimiter() {
-        int result = StringCalculator.calculate("//;\n1;2;3");
+        int result = stringCalculator.calculate("//;\n1;2;3");
         assertThat(result).isEqualTo(6);
     }
 }
