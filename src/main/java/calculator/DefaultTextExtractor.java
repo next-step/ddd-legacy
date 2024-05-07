560362e942ad437b5018408232df422936bef82c
GYUTAEKIM <kkt219a@gmail.com>
2024-05-08 03:53:39 +0900

    fix: 기본 regex 미존재 시 본문 추출 실패 에러 수정

--- src/main/java/calculator/DefaultTextExtractor.java
+++ src/main/java/calculator/DefaultTextExtractor.java
@@ -5,23 +5,13 @@ import java.util.regex.Pattern;
 public class DefaultTextExtractor implements TextExtractor {
 
   private final static String REGEX_PATTERN = ",|:";
-  private final Pattern pattern;
-
-  public DefaultTextExtractor() {
-    this.pattern = Pattern.compile(REGEX_PATTERN);
-  }
-
   @Override
   public boolean isSupport(String text) {
-    return pattern.matcher(text)
-        .find();
+    return true;
   }
 
   @Override
   public String[] extract(String text) {
-    if(!isSupport(text)) {
-      throw new TextExtractionException("본문 추출에 실패했습니다.");
-    }
     return text.split(REGEX_PATTERN);
   }
 }
