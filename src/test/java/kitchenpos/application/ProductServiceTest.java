ced4230538834f93b54a9752c978db5d9325737b
boradol2 <boradol0902@gmail.com>
2024-05-09 03:27:41 +0900

    test(ProductTest): 상품 등록하기 - 이름 예외 테스트
    1. 이름이 공백일 경우
    2. 이름이 비속어일 경우

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -7,6 +7,9 @@ import kitchenpos.domain.ProductRepository;
 import kitchenpos.infra.PurgomalumClient;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
@@ -15,6 +18,7 @@ import static kitchenpos.fixture.ProductFixture.PRICE_17000;
 import static kitchenpos.fixture.ProductFixture.productCreateRequest;
 import static kitchenpos.fixture.ProductFixture.productResponse;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -49,5 +53,33 @@ class ProductServiceTest {
                 () -> assertThat(result.getName()).isEqualTo(NAME_강정치킨),
                 () -> assertThat(result.getPrice()).isEqualTo(PRICE_17000)
         );
+    }
+
+    @DisplayName("상품을 등록할 때, 이름은 공백일 수 없다.")
+    @NullSource
+    @ParameterizedTest
+    void createProduct_nullNameException(String name) {
+        // given
+        Product request = productCreateRequest(name, PRICE_17000);
+
+        // when
+        // then
+        assertThatThrownBy(() -> productService.create(request))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("상품을 등록할 때, 이름에 비속어가 포함되면 예외가 발생한다.")
+    @ValueSource(strings = {"욕설", "비속어", "나쁜말"})
+    @ParameterizedTest
+    void createProduct_containProfanityNameException(String name) {
+        // given
+        Product request = productCreateRequest(name, PRICE_17000);
+
+        // when
+        when(purgomalumClient.containsProfanity(name)).thenReturn(true);
+
+        // then
+        assertThatThrownBy(() -> productService.create(request))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
