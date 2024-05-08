7aa384a862b30914b169afe6bea5aa70cff4cc66
pawoo0211 <pawoo0211@naver.com>
2024-05-08 20:19:45 +0900

    test: 상품 요구 사항 예외 케이스 테스트 코드 작성

--- src/test/java/kitchenpos/menu/fixture/ProductFixture.java
+++ src/test/java/kitchenpos/menu/fixture/ProductFixture.java
@@ -7,6 +7,9 @@ import java.util.UUID;
 
 public class ProductFixture {
     public static final Product 떡볶이 = create(UUID.randomUUID(), "떡볶이", new BigDecimal(2000));
+    public static final Product 가격_없는_상품 = create(UUID.randomUUID(), "음식", null);
+    public static final Product 가격_음수_상품 = create(UUID.randomUUID(), "음식", new BigDecimal(-1));
+    public static final Product 부적절한_이름_상품 = create(UUID.randomUUID(), "fuck", new BigDecimal(-1));
 
     public static Product create(UUID id, String name, BigDecimal price) {
         Product product = new Product();
--- src/test/java/kitchenpos/menu/service/ProductServiceTest.java
+++ src/test/java/kitchenpos/menu/service/ProductServiceTest.java
@@ -10,10 +10,14 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.stream.Stream;
 
 @DisplayName("상품 서비스 테스트")
 @ExtendWith(MockitoExtension.class)
@@ -40,5 +44,26 @@ public class ProductServiceTest {
         Product result = productService.create(떡볶이);
 
         Assertions.assertThat(result.getName()).isEqualTo(떡볶이.getName());
+    }
+
+    @ParameterizedTest
+    @MethodSource(value = "providePriceExceptionProduct")
+    @DisplayName("상품의 가격은 반드시 존재해야 하며 0보다 커야 한다.")
+    void create_exception_상품_가격(Product product) {
+        Assertions.assertThatThrownBy(
+                () -> productService.create(product)
+        ).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    static Stream<Product> providePriceExceptionProduct() {
+        return Stream.of(ProductFixture.가격_없는_상품, ProductFixture.가격_음수_상품);
+    }
+
+    @Test
+    @DisplayName("상품의 이름은 반드시 존재해야 하며 부적절한지 검사한다.")
+    void create_exception_상품_이름() {
+        Assertions.assertThatThrownBy(
+                () -> productService.create(ProductFixture.부적절한_이름_상품)
+        ).isInstanceOf(IllegalArgumentException.class);
     }
 }
