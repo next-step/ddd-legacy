b8777a106fe8c73002c986999789ebb7da34ed40
woodyplt <wodn4131@naver.com>
2024-05-09 01:09:35 +0900

    test: 메뉴 그룹 테스트 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,8 @@ dependencies {
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.kotest:kotest-runner-junit5:5.8.1")
+    testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.3")
+    testImplementation("com.ninja-squad:springmockk:4.0.2")
 }
 
 tasks.withType<KotlinCompile> {
---
+++ src/test/java/kitchenpos/application/MenuGroupServiceMockTest.kt
@@ -0,0 +1,47 @@
+package kitchenpos.application
+
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import kitchenpos.domain.MenuGroup
+import kitchenpos.domain.MenuGroupRepository
+import org.junit.jupiter.api.assertThrows
+
+class MenuGroupServiceMockTest : BehaviorSpec({
+    val menuGroupRepository = mockk<MenuGroupRepository>()
+    val menuGroupService = MenuGroupService(menuGroupRepository)
+
+    given("메뉴 그룹을 생성할 때") {
+        `when`("이름이 null 이면") {
+            val newMenuGroup = MenuGroup().apply { name = null }
+
+            then("예외가 발생한다.") {
+                assertThrows<IllegalArgumentException> {
+                    menuGroupService.create(newMenuGroup)
+                }
+            }
+        }
+
+        `when`("이름이 빈 문자열이면") {
+            val newMenuGroup = MenuGroup().apply { name = "" }
+
+            then("예외가 발생한다.") {
+                assertThrows<IllegalArgumentException> {
+                    menuGroupService.create(newMenuGroup)
+                }
+            }
+        }
+    }
+
+    given("메뉴 그룹을 조회할 떄") {
+        `when`("메뉴 그룹이 존재하지 않으면") {
+            every { menuGroupRepository.findAll() } returns emptyList()
+
+            then("빈 목록을 반환한다.") {
+                val results = menuGroupService.findAll()
+                results.size shouldBe 0
+            }
+        }
+    }
+})
