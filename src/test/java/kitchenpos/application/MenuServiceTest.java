ae2df03ddb1c88390a1865bf74dc3fa6411efb10
boradol2 <boradol0902@gmail.com>
2024-05-09 16:01:10 +0900

    refactor(MenuTest): Add Nested Class

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -12,6 +12,7 @@ import kitchenpos.infra.PurgomalumClient;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -82,190 +83,218 @@ class MenuServiceTest {
         ID_MENU_순살치킨 = MENU_순살치킨.getId();
     }
 
-    @DisplayName("메뉴를 등록한다.")
-    @Test
-    void creatMenu() {
-        // given
-        Menu request = buildCreateRequest();
-        commonStubForCreateMenu();
-        stubMenuRepositorySave();
+    @Nested
+    @DisplayName("메뉴 등록 테스트")
+    class Create {
+        @DisplayName("메뉴를 등록한다.")
+        @Test
+        void creatMenu() {
+            // given
+            Menu request = buildCreateRequest();
+            commonStubForCreateMenu();
+            stubMenuRepositorySave();
 
-        // when
-        Menu result = menuService.create(request);
+            // when
+            Menu result = menuService.create(request);
 
-        // then
-        assertAll(
-                () -> assertThat(result.getId()).isNotNull(),
-                () -> assertThat(result.getName()).isEqualTo(NAME_순살치킨),
-                () -> assertThat(result.getPrice()).isEqualTo(PRICE_32000),
-                () -> assertThat(result.isDisplayed()).isTrue(),
-                () -> assertThat(result.getMenuGroupId()).isEqualTo(ID_MENU_GOURP_추천메뉴),
-                () -> assertThat(result.getMenuProducts()).hasSize(2)
-                        .containsExactly(강정치킨_1개, 후라이드치킨_1개)
-        );
+            // then
+            assertAll(
+                    () -> assertThat(result.getId()).isNotNull(),
+                    () -> assertThat(result.getName()).isEqualTo(NAME_순살치킨),
+                    () -> assertThat(result.getPrice()).isEqualTo(PRICE_32000),
+                    () -> assertThat(result.isDisplayed()).isTrue(),
+                    () -> assertThat(result.getMenuGroupId()).isEqualTo(ID_MENU_GOURP_추천메뉴),
+                    () -> assertThat(result.getMenuProducts()).hasSize(2)
+                            .containsExactly(강정치킨_1개, 후라이드치킨_1개)
+            );
+        }
+
+        @Nested
+        @DisplayName("이름 테스트")
+        class Name {
+            @DisplayName("메뉴를 등록할 때, 이름이 공백이면 예외가 발생한다.")
+            @NullSource
+            @ParameterizedTest
+            void creatMenu_nullNameException(String name) {
+                // given
+                Menu request = buildCreateRequest(name);
+                commonStubForCreateMenu();
+
+                // when
+                // then
+                assertThatThrownBy(() -> menuService.create(request))
+                        .isInstanceOf(IllegalArgumentException.class);
+            }
+
+            @DisplayName("메뉴를 등록할 때, 이름에 비속어가 포함되면 예외가 발생한다.")
+            @ValueSource(strings = {"욕설", "비속어"})
+            @ParameterizedTest
+            void creatMenu_containProfanityNameException(String name) {
+                // given
+                Menu request = buildCreateRequest(name);
+                commonStubForCreateMenu();
+
+                // when
+                when(purgomalumClient.containsProfanity(any())).thenReturn(true);
+
+                // then
+                assertThatThrownBy(() -> menuService.create(request))
+                        .isInstanceOf(IllegalArgumentException.class);
+            }
+        }
+
+        @Nested
+        @DisplayName("가격 테스트")
+        class Price {
+            @DisplayName("메뉴를 등록할 때, 메뉴 가격이 공백이거나 0원보다 작으면 예외가 발생한다.")
+            @NullSource
+            @MethodSource("provideInvalidPrices")
+            @ParameterizedTest
+            void creatMenu_NullOrNegativePriceException(BigDecimal price) {
+                // given
+                Menu request = buildCreateRequest(price);
+
+                // when
+                // then
+                assertThatThrownBy(() -> menuService.create(request))
+                        .isInstanceOf(IllegalArgumentException.class);
+            }
+
+            @DisplayName("메뉴를 등록할 때, 메뉴가격이 메뉴구성상품의 (가격*수량)의 총 합보다 비싸면 예외 발생한다.")
+            @ValueSource(longs = {35_001, 50_000, 100_000})
+            @ParameterizedTest
+            void creatMenu_invalidPricePolicyException(long price) {
+                // given
+                Menu request = buildCreateRequest(BigDecimal.valueOf(price));
+                commonStubForCreateMenu();
+
+                // when
+                // then
+                assertThatThrownBy(() -> menuService.create(request))
+                        .isInstanceOf(IllegalArgumentException.class);
+            }
+
+            private static Stream<BigDecimal> provideInvalidPrices() {
+                return Stream.of(BigDecimal.valueOf(-1), BigDecimal.valueOf(-1000), BigDecimal.valueOf(-99999990));
+            }
+        }
+
+        @DisplayName("메뉴를 등록할 때, 미리 등록되어 있지 않은 메뉴그룹이면 예외 발생한다.")
+        @Test
+        void creatMenu_notExistsMenuGroupException() {
+            // given
+            Menu request = buildCreateRequest();
+
+            // when
+            when(menuGroupRepository.findById(any())).thenReturn(Optional.empty());
+
+            // then
+            assertThatThrownBy(() -> menuService.create(request))
+                    .isInstanceOf(NoSuchElementException.class);
+        }
+
+        @Nested
+        @DisplayName("메뉴구성상품 테스트")
+        class MenuProduct {
+            @DisplayName("메뉴를 등록할 때, 메뉴구성상품을 하나도 담지 않으면 예외 발생한다.")
+            @Test
+            void creatMenu_emptyMenuProductException() {
+                // given
+                Menu request = buildCreateRequest();
+                stubMenuGroupRepositoryFindById();
+
+                // when
+                when(productRepository.findAllByIdIn(any())).thenReturn(emptyList());
+
+                // then
+                assertThatThrownBy(() -> menuService.create(request))
+                        .isInstanceOf(IllegalArgumentException.class);
+            }
+
+            @DisplayName("메뉴를 등록할 때, 메뉴구성상품이 미리 등록되지 않은 상품이 하나라도 있으면 예외 발생한다.")
+            @Test
+            void creatMenu_notExistsMenuProductException() {
+                // given
+                Menu request = buildCreateRequest();
+                stubMenuGroupRepositoryFindById();
+
+                // when
+                when(productRepository.findAllByIdIn(any())).thenReturn(List.of(PRODUCT_후라이드치킨));
+
+                // then
+                assertThatThrownBy(() -> menuService.create(request))
+                        .isInstanceOf(IllegalArgumentException.class);
+            }
+
+            @DisplayName("메뉴를 등록할 때, 메뉴구성상품의 수량이 0개 미만이면 예외 발생한다.")
+            @ValueSource(longs = {-1, -111, -999999})
+            @ParameterizedTest
+            void creatMenu_negativeQuantityMenuProductException(long quantity) {
+                // given
+                Menu request = buildCreateRequest(menuProductResponse(PRODUCT_강정치킨, quantity));
+                stubMenuGroupRepositoryFindById();
+
+                // when
+                when(productRepository.findAllByIdIn(any())).thenReturn(List.of(PRODUCT_강정치킨));
+
+                // then
+                assertThatThrownBy(() -> menuService.create(request))
+                        .isInstanceOf(IllegalArgumentException.class);
+            }
+        }
     }
 
-    @DisplayName("메뉴를 등록할 때, 이름이 공백이면 예외가 발생한다.")
-    @NullSource
-    @ParameterizedTest
-    void creatMenu_nullNameException(String name) {
-        // given
-        Menu request = buildCreateRequest(name);
-        commonStubForCreateMenu();
+    @Nested
+    @DisplayName("가격 수정 테스트")
+    class ChangePrice {
+        @DisplayName("메뉴의 가격을 수정한다.")
+        @Test
+        void changeMenuPrice() {
+            // given
+            Menu request = menuChangePriceRequest(BigDecimal.valueOf(30_000));
+            when(menuRepository.findById(any())).thenReturn(Optional.of(MENU_순살치킨));
 
-        // when
-        // then
-        assertThatThrownBy(() -> menuService.create(request))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+            // when
+            Menu result = menuService.changePrice(ID_MENU_순살치킨, request);
 
-    @DisplayName("메뉴를 등록할 때, 이름에 비속어가 포함되면 예외가 발생한다.")
-    @ValueSource(strings = {"욕설", "비속어"})
-    @ParameterizedTest
-    void creatMenu_containProfanityNameException(String name) {
-        // given
-        Menu request = buildCreateRequest(name);
-        commonStubForCreateMenu();
+            // then
+            assertAll(
+                    () -> assertThat(result.getId()).isEqualTo(ID_MENU_순살치킨),
+                    () -> assertThat(result.getPrice()).isEqualTo(BigDecimal.valueOf(30_000))
+            );
+        }
 
-        // when
-        when(purgomalumClient.containsProfanity(any())).thenReturn(true);
+        @DisplayName("메뉴의 가격을 수정할 때, 가격이 공백이거나  0원보다 작으면 예외가 발생한다.")
+        @NullSource
+        @MethodSource("provideInvalidPrices")
+        @ParameterizedTest
+        void changeMenuPrice_NullOrNegativePriceException(BigDecimal price) {
+            // given
+            Menu request = menuChangePriceRequest(price);
 
-        // then
-        assertThatThrownBy(() -> menuService.create(request))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+            // when
+            // then
+            assertThatThrownBy(() -> menuService.changePrice(ID_MENU_순살치킨, request))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-    @DisplayName("메뉴를 등록할 때, 메뉴 가격이 공백이거나 0원보다 작으면 예외가 발생한다.")
-    @NullSource
-    @MethodSource("provideInvalidPrices")
-    @ParameterizedTest
-    void creatMenu_NullOrNegativePriceException(BigDecimal price) {
-        // given
-        Menu request = buildCreateRequest(price);
+        @DisplayName("메뉴의 가격을 수정할 때, 메뉴가격이 메뉴구성상품의 (가격*수량)의 총 합보다 비싸면 예외 발생한다.")
+        @ValueSource(longs = {35_001, 50_000, 100_000})
+        @ParameterizedTest
+        void changeMenuPrice_invalidPricePolicyException(long price) {
+            // given
+            Menu request = buildCreateRequest(BigDecimal.valueOf(price));
+            when(menuRepository.findById(any())).thenReturn(Optional.of(MENU_순살치킨));
 
-        // when
-        // then
-        assertThatThrownBy(() -> menuService.create(request))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
+            // when
+            // then
+            assertThatThrownBy(() -> menuService.changePrice(ID_MENU_순살치킨, request))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-    @DisplayName("메뉴를 등록할 때, 메뉴가격이 메뉴구성상품의 (가격*수량)의 총 합보다 비싸면 예외 발생한다.")
-    @ValueSource(longs = {35_001, 50_000, 100_000})
-    @ParameterizedTest
-    void creatMenu_invalidPricePolicyException(long price) {
-        // given
-        Menu request = buildCreateRequest(BigDecimal.valueOf(price));
-        commonStubForCreateMenu();
-
-        // when
-        // then
-        assertThatThrownBy(() -> menuService.create(request))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("메뉴를 등록할 때, 미리 등록되어 있지 않은 메뉴그룹이면 예외 발생한다.")
-    @Test
-    void creatMenu_notExistsMenuGroupException() {
-        // given
-        Menu request = buildCreateRequest();
-
-        // when
-        when(menuGroupRepository.findById(any())).thenReturn(Optional.empty());
-
-        // then
-        assertThatThrownBy(() -> menuService.create(request))
-                .isInstanceOf(NoSuchElementException.class);
-    }
-
-    @DisplayName("메뉴를 등록할 때, 메뉴구성상품을 하나도 담지 않으면 예외 발생한다.")
-    @Test
-    void creatMenu_emptyMenuProductException() {
-        // given
-        Menu request = buildCreateRequest();
-        stubMenuGroupRepositoryFindById();
-
-        // when
-        when(productRepository.findAllByIdIn(any())).thenReturn(emptyList());
-
-        // then
-        assertThatThrownBy(() -> menuService.create(request))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("메뉴를 등록할 때, 메뉴구성상품이 미리 등록되지 않은 상품이 하나라도 있으면 예외 발생한다.")
-    @Test
-    void creatMenu_notExistsMenuProductException() {
-        // given
-        Menu request = buildCreateRequest();
-        stubMenuGroupRepositoryFindById();
-
-        // when
-        when(productRepository.findAllByIdIn(any())).thenReturn(List.of(PRODUCT_후라이드치킨));
-
-        // then
-        assertThatThrownBy(() -> menuService.create(request))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("메뉴를 등록할 때, 메뉴구성상품의 수량이 0개 미만이면 예외 발생한다.")
-    @ValueSource(longs = {-1, -111, -999999})
-    @ParameterizedTest
-    void creatMenu_negativeQuantityMenuProductException(long quantity) {
-        // given
-        Menu request = buildCreateRequest(menuProductResponse(PRODUCT_강정치킨, quantity));
-        stubMenuGroupRepositoryFindById();
-
-        // when
-        when(productRepository.findAllByIdIn(any())).thenReturn(List.of(PRODUCT_강정치킨));
-
-        // then
-        assertThatThrownBy(() -> menuService.create(request))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("메뉴의 가격을 수정한다.")
-    @Test
-    void changeMenuPrice() {
-        // given
-        Menu request = menuChangePriceRequest(BigDecimal.valueOf(30_000));
-        when(menuRepository.findById(any())).thenReturn(Optional.of(MENU_순살치킨));
-
-        // when
-        Menu result = menuService.changePrice(ID_MENU_순살치킨, request);
-
-        // then
-        assertAll(
-                () -> assertThat(result.getId()).isEqualTo(ID_MENU_순살치킨),
-                () -> assertThat(result.getPrice()).isEqualTo(BigDecimal.valueOf(30_000))
-        );
-    }
-
-    @DisplayName("메뉴의 가격을 수정할 때, 가격이 공백이거나  0원보다 작으면 예외가 발생한다.")
-    @NullSource
-    @MethodSource("provideInvalidPrices")
-    @ParameterizedTest
-    void changeMenuPrice_NullOrNegativePriceException(BigDecimal price) {
-        // given
-        Menu request = menuChangePriceRequest(price);
-
-        // when
-        // then
-        assertThatThrownBy(() -> menuService.changePrice(ID_MENU_순살치킨, request))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("메뉴의 가격을 수정할 때, 메뉴가격이 메뉴구성상품의 (가격*수량)의 총 합보다 비싸면 예외 발생한다.")
-    @ValueSource(longs = {35_001, 50_000, 100_000})
-    @ParameterizedTest
-    void changeMenuPrice_invalidPricePolicyException(long price) {
-        // given
-        Menu request = buildCreateRequest(BigDecimal.valueOf(price));
-        when(menuRepository.findById(any())).thenReturn(Optional.of(MENU_순살치킨));
-
-        // when
-        // then
-        assertThatThrownBy(() -> menuService.changePrice(ID_MENU_순살치킨, request))
-                .isInstanceOf(IllegalArgumentException.class);
+        private static Stream<BigDecimal> provideInvalidPrices() {
+            return Stream.of(BigDecimal.valueOf(-1), BigDecimal.valueOf(-1000), BigDecimal.valueOf(-99999990));
+        }
     }
 
     @NotNull
@@ -301,9 +330,5 @@ class MenuServiceTest {
         when(productRepository.findAllByIdIn(any())).thenReturn(List.of(PRODUCT_강정치킨, PRODUCT_후라이드치킨));
         when(productRepository.findById(PRODUCT_강정치킨.getId())).thenAnswer(invocation -> Optional.ofNullable(PRODUCT_강정치킨));
         when(productRepository.findById(PRODUCT_후라이드치킨.getId())).thenAnswer(invocation -> Optional.ofNullable(PRODUCT_후라이드치킨));
-    }
-
-    private static Stream<BigDecimal> provideInvalidPrices() {
-        return Stream.of(BigDecimal.valueOf(-1), BigDecimal.valueOf(-1000), BigDecimal.valueOf(-99999990));
     }
 }
