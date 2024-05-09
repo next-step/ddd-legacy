38155cea932fa95e72005b82a4c354b339b21b35
boradol2 <boradol0902@gmail.com>
2024-05-09 14:21:37 +0900

    test(MenuTest): 메뉴 등록하기 - 이름 예외 테스트
    1. 이름이 공백이면 예외 발생 케이스
    2. 이름에 비속어 포함되면 예외 발생 케이스

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -9,9 +9,13 @@ import kitchenpos.domain.MenuRepository;
 import kitchenpos.domain.Product;
 import kitchenpos.domain.ProductRepository;
 import kitchenpos.infra.PurgomalumClient;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
@@ -31,6 +35,7 @@ import static kitchenpos.fixture.ProductFixture.PRICE_17000;
 import static kitchenpos.fixture.ProductFixture.PRICE_18000;
 import static kitchenpos.fixture.ProductFixture.productResponse;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
@@ -88,5 +93,57 @@ class MenuServiceTest {
                 () -> assertThat(result.getMenuProducts()).hasSize(2)
                         .containsExactly(강정치킨_1개, 후라이드치킨_1개)
         );
+    }
+
+    @DisplayName("메뉴를 등록할 때, 이름이 공백이면 예외가 발생한다.")
+    @NullSource
+    @ParameterizedTest
+    void creatMenu_nullNameException(String name) {
+        // given
+        Menu request = buildCreateRequest(name);
+        commonStubForCreateMenu();
+
+        // when
+        // then
+        assertThatThrownBy(() -> menuService.create(request))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("메뉴를 등록할 때, 이름에 비속어가 포함되면 예외가 발생한다.")
+    @ValueSource(strings = {"욕설", "비속어"})
+    @ParameterizedTest
+    void creatMenu_containProfanityNameException(String name) {
+        // given
+        Menu request = buildCreateRequest(name);
+        commonStubForCreateMenu();
+
+        // when
+        when(purgomalumClient.containsProfanity(any())).thenReturn(true);
+
+        // then
+        assertThatThrownBy(() -> menuService.create(request))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @NotNull
+    private Menu buildCreateRequest() {
+        return menuCreateRequest(NAME_순살치킨, PRICE_32000, ID_MENU_GOURP_추천메뉴, true, 강정치킨_1개, 후라이드치킨_1개);
+    }
+
+    @NotNull
+    private Menu buildCreateRequest(String name) {
+        return menuCreateRequest(name, PRICE_32000, ID_MENU_GOURP_추천메뉴, true, 강정치킨_1개, 후라이드치킨_1개);
+    }
+
+    private void stubMenuRepositorySave() {
+        Menu MENU_순살치킨 = menuResponse(NAME_순살치킨, PRICE_32000, ID_MENU_GOURP_추천메뉴, true, 강정치킨_1개, 후라이드치킨_1개);
+        when(menuRepository.save(any())).thenReturn(MENU_순살치킨);
+    }
+
+    private void commonStubForCreateMenu() {
+        when(menuGroupRepository.findById(any())).thenReturn(Optional.ofNullable(MENU_GROUP_추천메뉴));
+        when(productRepository.findAllByIdIn(any())).thenReturn(List.of(PRODUCT_강정치킨, PRODUCT_후라이드치킨));
+        when(productRepository.findById(PRODUCT_강정치킨.getId())).thenAnswer(invocation -> Optional.ofNullable(PRODUCT_강정치킨));
+        when(productRepository.findById(PRODUCT_후라이드치킨.getId())).thenAnswer(invocation -> Optional.ofNullable(PRODUCT_후라이드치킨));
     }
 }
