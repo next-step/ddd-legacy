31980ee806c9db25c5a91314b862c61af67c643a
bkRyusim <bkryusim@gmail.com>
2024-05-10 19:16:51 +0900

    refactor: assert with assertAll()

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -2,6 +2,7 @@ package calculator;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -19,15 +20,15 @@ public class StringCalculatorTest {
 	@DisplayName("빈 문자열 또는 null을 입력하는 경우 0을 반환한다.")
 	@Test
 	void nullOrEmptyReturnZero() {
-		assertThat(stringCalculator.calculate("")).isEqualTo(0);
-		assertThat(stringCalculator.calculate(null)).isEqualTo(0);
+		assertAll(() -> assertThat(stringCalculator.calculate("")).isEqualTo(0),
+			() -> assertThat(stringCalculator.calculate(null)).isEqualTo(0));
 	}
 
 	@DisplayName("숫자 하나를 문자열로 입력하는 경우 해당 숫자를 반환한다.")
 	@Test
 	void oneNumberReturnNumber() {
-		assertThat(stringCalculator.calculate("1")).isEqualTo(1);
-		assertThat(stringCalculator.calculate("11")).isEqualTo(11);
+		assertAll(() -> assertThat(stringCalculator.calculate("1")).isEqualTo(1),
+			() -> assertThat(stringCalculator.calculate("11")).isEqualTo(11));
 	}
 
 	@DisplayName("쉼표 구분자로 구분되는 숫자들의 합을 반환한다.")
@@ -45,16 +46,14 @@ public class StringCalculatorTest {
 	@DisplayName("음수를 전달할 경우 예외가 발생한다.")
 	@Test
 	void negativeNumberThrowException() {
-		assertThatCode(() -> stringCalculator.calculate("1,-2,3"))
-			.isInstanceOf(IllegalArgumentException.class)
+		assertThatCode(() -> stringCalculator.calculate("1,-2,3")).isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("Negative number not allowed: -2");
 	}
 
 	@DisplayName("숫자가 아닌 값을 전달할 경우 예외가 발생한다.")
 	@Test
 	void onlyNumberCanBeInput() {
-		assertThatCode(() -> stringCalculator.calculate("1,*,3"))
-			.isInstanceOf(IllegalArgumentException.class)
+		assertThatCode(() -> stringCalculator.calculate("1,*,3")).isInstanceOf(IllegalArgumentException.class)
 			.hasMessage("Character not allowed: *");
 	}
 
