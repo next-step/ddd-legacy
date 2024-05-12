a3065e985a917878a3cbe36748c1b5a1c53af2c3
jinan159 <wlsdhks0423@naver.com>
2024-05-12 20:44:09 +0900

    chore: mockk 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -33,6 +33,7 @@ dependencies {
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("io.kotest:kotest-runner-junit5:5.8.1")
+    testImplementation("io.mockk:mockk:1.13.10")
 }
 
 tasks.withType<KotlinCompile> {
