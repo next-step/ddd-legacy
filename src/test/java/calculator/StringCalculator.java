fbe0a44358404164102349f40a0b218d41805f55
kimjunbo <ns9337@naver.com>
2024-05-12 11:53:12 +0900

    feat(StringCalculator): implement adding custom delimiter

--- src/test/java/calculator/StringCalculator.java
+++ src/test/java/calculator/StringCalculator.java
@@ -1,17 +1,27 @@
 package calculator;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
 
-	private static final String DEFAULT_DELIMITER = "[,:]";
+	private List<String> delimiterList = new ArrayList<>(List.of(",", ":"));
 
 	public int add(String text) {
 		if (text == null || text.isEmpty()) {
 			return 0;
 		}
-
-		String[] tokens = text.split(DEFAULT_DELIMITER);
-		return Arrays.stream(tokens).mapToInt(Integer::parseInt).sum();
+		String target = text;
+		Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+		if (m.find()) {
+			String customDelimiter = m.group(1);
+			delimiterList.add(customDelimiter);
+			target = m.group(2);
+		}
+		String regex = String.join("|", delimiterList);
+		return Arrays.stream(target.split(regex)).mapToInt(Integer::parseInt).sum();
 	}
 }
