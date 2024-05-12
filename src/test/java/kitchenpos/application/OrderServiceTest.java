77aee4212924c560c738c44c4007d9dfa553050a
seyun(marco) <ksy90101@gmail.com>
2024-05-12 21:29:09 +0900

    test: orderService의 accept(주문 수락) 메서드 테스트 코드 작성

--- src/test/java/kitchenpos/application/OrderServiceTest.java
+++ src/test/java/kitchenpos/application/OrderServiceTest.java
@@ -7,6 +7,7 @@ import kitchenpos.domain.MenuProduct;
 import kitchenpos.domain.MenuRepository;
 import kitchenpos.domain.Order;
 import kitchenpos.domain.OrderLineItem;
+import kitchenpos.domain.OrderRepository;
 import kitchenpos.domain.OrderStatus;
 import kitchenpos.domain.OrderTable;
 import kitchenpos.domain.OrderTableRepository;
@@ -14,26 +15,32 @@ import kitchenpos.domain.OrderType;
 import kitchenpos.domain.Product;
 import kitchenpos.domain.ProductRepository;
 import kitchenpos.fixture.MenuGroupFixture;
+import kitchenpos.infra.KitchenridersClient;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.EnumSource;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.UUID;
 
 import static kitchenpos.fixture.MenuFixture.createMenu;
 import static kitchenpos.fixture.MenuProductFixture.createMenuProduct;
 import static kitchenpos.fixture.OrderFixture.createOrder;
+import static kitchenpos.fixture.OrderFixture.createOrderWithId;
 import static kitchenpos.fixture.OrderLineItemFixture.createOrderLineItem;
 import static kitchenpos.fixture.OrderTableFixture.createOrderTableWithId;
 import static kitchenpos.fixture.ProductFixture.createProduct;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.verify;
 
 
 @SpringBootTest
@@ -43,7 +50,10 @@ class OrderServiceTest {
     private OrderService orderService;
 
     @Autowired
+    private OrderRepository orderRepository;
+    @Autowired
     private MenuRepository menuRepository;
+
 
     @Autowired
     private ProductRepository productRepository;
@@ -54,87 +64,86 @@ class OrderServiceTest {
     @Autowired
     private OrderTableRepository orderTableRepository;
 
+    @Autowired
+    private KitchenridersClient kitchenridersClient;
+
     @Nested
     class createTest {
-        @DisplayName("공통 정책 테스트")
-        @Nested
-        class commonTest {
-            @DisplayName("주문 유형이 없는 경우 예외가 발생한다.")
-            @Test
-            void createFailWhenNotExistTypeTest() {
-                Product product = createProduct("떡볶이", BigDecimal.valueOf(16000));
-                product = productRepository.save(product);
-                MenuGroup menuGroup = MenuGroupFixture.createMenuGroupWithId("추천 그룹");
-                menuGroup = menuGroupRepository.save(menuGroup);
-                MenuProduct menuProduct = createMenuProduct(product, 1);
-                Menu menu = createMenu(menuGroup, "떡볶이", BigDecimal.valueOf(16000), true, List.of(menuProduct));
-                menu = menuRepository.save(menu);
-                OrderLineItem orderLineItem = createOrderLineItem(BigDecimal.valueOf(16000), menu, 1);
-                Order order = createOrder(null, null, List.of(orderLineItem), null, null, null);
+        @DisplayName("주문 유형이 없는 경우 예외가 발생한다.")
+        @Test
+        void createFailWhenNotExistTypeTest() {
+            Product product = createProduct("떡볶이", BigDecimal.valueOf(16000));
+            product = productRepository.save(product);
+            MenuGroup menuGroup = MenuGroupFixture.createMenuGroupWithId("추천 그룹");
+            menuGroup = menuGroupRepository.save(menuGroup);
+            MenuProduct menuProduct = createMenuProduct(product, 1);
+            Menu menu = createMenu(menuGroup, "떡볶이", BigDecimal.valueOf(16000), true, List.of(menuProduct));
+            menu = menuRepository.save(menu);
+            OrderLineItem orderLineItem = createOrderLineItem(BigDecimal.valueOf(16000), menu, 1);
+            Order order = createOrder(null, null, List.of(orderLineItem), null, null, null);
 
-                assertThatThrownBy(() -> orderService.create(order))
-                        .isInstanceOf(IllegalArgumentException.class);
-            }
+            assertThatThrownBy(() -> orderService.create(order))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-            @DisplayName("주문 항목이 없는 경우 예외가 발생한다.")
-            @NullAndEmptySource
-            @ParameterizedTest
-            void createFailWhenNotExistOrderLineItemsTest(List<OrderLineItem> orderLineItems) {
-                Order order = createOrder(null, null, orderLineItems, null, null, null);
+        @DisplayName("주문 항목이 없는 경우 예외가 발생한다.")
+        @NullAndEmptySource
+        @ParameterizedTest
+        void createFailWhenNotExistOrderLineItemsTest(List<OrderLineItem> orderLineItems) {
+            Order order = createOrder(null, null, orderLineItems, null, null, null);
 
-                assertThatThrownBy(() -> orderService.create(order))
-                        .isInstanceOf(IllegalArgumentException.class);
-            }
+            assertThatThrownBy(() -> orderService.create(order))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-            @DisplayName("존재하지 않는 메뉴를 주문한 경우 예외가 발생한다.")
-            @Test
-            void createFailWhenNotExistMenuTest() {
-                Product product = createProduct("떡볶이", BigDecimal.valueOf(16000));
-                product = productRepository.save(product);
-                MenuGroup menuGroup = MenuGroupFixture.createMenuGroupWithId("추천 그룹");
-                menuGroup = menuGroupRepository.save(menuGroup);
-                MenuProduct menuProduct = createMenuProduct(product, 1);
-                Menu notExistMenu = createMenu(menuGroup, "떡볶이", BigDecimal.valueOf(16000), true, List.of(menuProduct));
-                OrderLineItem orderLineItem = createOrderLineItem(BigDecimal.valueOf(16000), notExistMenu, 1);
-                Order order = createOrder(null, null, List.of(orderLineItem), OrderType.EAT_IN, null, null);
+        @DisplayName("존재하지 않는 메뉴를 주문한 경우 예외가 발생한다.")
+        @Test
+        void createFailWhenNotExistMenuTest() {
+            Product product = createProduct("떡볶이", BigDecimal.valueOf(16000));
+            product = productRepository.save(product);
+            MenuGroup menuGroup = MenuGroupFixture.createMenuGroupWithId("추천 그룹");
+            menuGroup = menuGroupRepository.save(menuGroup);
+            MenuProduct menuProduct = createMenuProduct(product, 1);
+            Menu notExistMenu = createMenu(menuGroup, "떡볶이", BigDecimal.valueOf(16000), true, List.of(menuProduct));
+            OrderLineItem orderLineItem = createOrderLineItem(BigDecimal.valueOf(16000), notExistMenu, 1);
+            Order order = createOrder(null, null, List.of(orderLineItem), OrderType.EAT_IN, null, null);
 
-                assertThatThrownBy(() -> orderService.create(order))
-                        .isInstanceOf(IllegalArgumentException.class);
-            }
+            assertThatThrownBy(() -> orderService.create(order))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-            @DisplayName("메뉴가 비노출인 경우에 예외가 발생한다.")
-            @Test
-            void createFailWhenNotDisplayedMenuTest() {
-                Product product = createProduct("떡볶이", BigDecimal.valueOf(16000));
-                product = productRepository.save(product);
-                MenuGroup menuGroup = MenuGroupFixture.createMenuGroupWithId("추천 그룹");
-                menuGroup = menuGroupRepository.save(menuGroup);
-                MenuProduct menuProduct = createMenuProduct(product, 1);
-                Menu menu = createMenu(menuGroup, "떡볶이", BigDecimal.valueOf(16000), false, List.of(menuProduct));
-                menu = menuRepository.save(menu);
-                OrderLineItem orderLineItem = createOrderLineItem(BigDecimal.valueOf(16000), menu, 1);
-                Order order = createOrder(null, null, List.of(orderLineItem), OrderType.EAT_IN, null, null);
+        @DisplayName("메뉴가 비노출인 경우에 예외가 발생한다.")
+        @Test
+        void createFailWhenNotDisplayedMenuTest() {
+            Product product = createProduct("떡볶이", BigDecimal.valueOf(16000));
+            product = productRepository.save(product);
+            MenuGroup menuGroup = MenuGroupFixture.createMenuGroupWithId("추천 그룹");
+            menuGroup = menuGroupRepository.save(menuGroup);
+            MenuProduct menuProduct = createMenuProduct(product, 1);
+            Menu menu = createMenu(menuGroup, "떡볶이", BigDecimal.valueOf(16000), false, List.of(menuProduct));
+            menu = menuRepository.save(menu);
+            OrderLineItem orderLineItem = createOrderLineItem(BigDecimal.valueOf(16000), menu, 1);
+            Order order = createOrder(null, null, List.of(orderLineItem), OrderType.EAT_IN, null, null);
 
-                assertThatThrownBy(() -> orderService.create(order))
-                        .isInstanceOf(IllegalStateException.class);
-            }
+            assertThatThrownBy(() -> orderService.create(order))
+                    .isInstanceOf(IllegalStateException.class);
+        }
 
-            @DisplayName("메뉴의 가격과 주문 항목의 가격이 다른 경우 예외가 발생한다.")
-            @Test
-            void createFailWhenDifferentPriceTest() {
-                Product product = createProduct("떡볶이", BigDecimal.valueOf(16000));
-                product = productRepository.save(product);
-                MenuGroup menuGroup = MenuGroupFixture.createMenuGroupWithId("추천 그룹");
-                menuGroup = menuGroupRepository.save(menuGroup);
-                MenuProduct menuProduct = createMenuProduct(product, 1);
-                Menu menu = createMenu(menuGroup, "떡볶이", BigDecimal.valueOf(16000), true, List.of(menuProduct));
-                menu = menuRepository.save(menu);
-                OrderLineItem orderLineItem = createOrderLineItem(BigDecimal.valueOf(17000), menu, 1);
-                Order order = createOrder(null, null, List.of(orderLineItem), OrderType.EAT_IN, null, null);
+        @DisplayName("메뉴의 가격과 주문 항목의 가격이 다른 경우 예외가 발생한다.")
+        @Test
+        void createFailWhenDifferentPriceTest() {
+            Product product = createProduct("떡볶이", BigDecimal.valueOf(16000));
+            product = productRepository.save(product);
+            MenuGroup menuGroup = MenuGroupFixture.createMenuGroupWithId("추천 그룹");
+            menuGroup = menuGroupRepository.save(menuGroup);
+            MenuProduct menuProduct = createMenuProduct(product, 1);
+            Menu menu = createMenu(menuGroup, "떡볶이", BigDecimal.valueOf(16000), true, List.of(menuProduct));
+            menu = menuRepository.save(menu);
+            OrderLineItem orderLineItem = createOrderLineItem(BigDecimal.valueOf(17000), menu, 1);
+            Order order = createOrder(null, null, List.of(orderLineItem), OrderType.EAT_IN, null, null);
 
-                assertThatThrownBy(() -> orderService.create(order))
-                        .isInstanceOf(IllegalArgumentException.class);
-            }
+            assertThatThrownBy(() -> orderService.create(order))
+                    .isInstanceOf(IllegalArgumentException.class);
         }
 
         @DisplayName("먹고가기 유형인 경우")
@@ -303,6 +312,69 @@ class OrderServiceTest {
 
     @Nested
     class acceptTest {
+        @DisplayName("주문을 수락할 수 있다.")
+        @Test
+        void acceptTest() {
+            Product product = createProduct("떡볶이", BigDecimal.valueOf(16000));
+            product = productRepository.save(product);
+            MenuGroup menuGroup = MenuGroupFixture.createMenuGroupWithId("추천 그룹");
+            menuGroup = menuGroupRepository.save(menuGroup);
+            MenuProduct menuProduct = createMenuProduct(product, 1);
+            Menu menu = createMenu(menuGroup, "떡볶이", BigDecimal.valueOf(16000), true, List.of(menuProduct));
+            menu = menuRepository.save(menu);
+            OrderLineItem orderLineItem = createOrderLineItem(BigDecimal.valueOf(16000), menu, 1);
+            Order order = createOrder(null, null, List.of(orderLineItem), OrderType.TAKEOUT, null, null);
+            order = orderService.create(order);
+
+            order = orderService.accept(order.getId());
+
+            assertThat(order.getStatus()).isEqualTo(OrderStatus.ACCEPTED);
+        }
+
+        @DisplayName("주문 상태가 대기 상태가 아닌 경우에 예외가 발생한다.")
+        @ParameterizedTest
+        @EnumSource(value = OrderStatus.class, mode = EnumSource.Mode.EXCLUDE, names = "WAITING")
+        void acceptFailWhenOrderStatusIsNotWaitingTest(OrderStatus orderStatus) {
+            Product product = createProduct("떡볶이", BigDecimal.valueOf(16000));
+            product = productRepository.save(product);
+            MenuGroup menuGroup = MenuGroupFixture.createMenuGroupWithId("추천 그룹");
+            menuGroup = menuGroupRepository.save(menuGroup);
+            MenuProduct menuProduct = createMenuProduct(product, 1);
+            Menu menu = createMenu(menuGroup, "떡볶이", BigDecimal.valueOf(16000), true, List.of(menuProduct));
+            menu = menuRepository.save(menu);
+            OrderLineItem orderLineItem = createOrderLineItem(BigDecimal.valueOf(16000), menu, 1);
+            Order order = createOrderWithId(null, List.of(orderLineItem), OrderType.TAKEOUT, orderStatus, null, LocalDateTime.now());
+            order = orderRepository.save(order);
+
+            UUID orderId = order.getId();
+
+            assertThatThrownBy(() -> orderService.accept(orderId))
+                    .isInstanceOf(IllegalStateException.class);
+        }
+
+        @DisplayName("배달하기 유형인 경우")
+        @Nested
+        class DeliveryTest {
+            @DisplayName("라이더 매칭 시스템에 요청을 보낸다.")
+            @Test
+            void acceptTest() {
+                Product product = createProduct("떡볶이", BigDecimal.valueOf(16000));
+                product = productRepository.save(product);
+                MenuGroup menuGroup = MenuGroupFixture.createMenuGroupWithId("추천 그룹");
+                menuGroup = menuGroupRepository.save(menuGroup);
+                MenuProduct menuProduct = createMenuProduct(product, 1);
+                Menu menu = createMenu(menuGroup, "떡볶이", BigDecimal.valueOf(16000), true, List.of(menuProduct));
+                menu = menuRepository.save(menu);
+                OrderLineItem orderLineItem = createOrderLineItem(BigDecimal.valueOf(16000), menu, 1);
+                Order order = createOrder(null, null, List.of(orderLineItem), OrderType.DELIVERY, null, "서울 강남구 테헤란로 411, 성담빌딩 13층");
+                order = orderService.create(order);
+
+                order = orderService.accept(order.getId());
+
+                assertThat(order.getStatus()).isEqualTo(OrderStatus.ACCEPTED);
+                verify(kitchenridersClient).requestDelivery(order.getId(), BigDecimal.valueOf(16000), order.getDeliveryAddress());
+            }
+        }
     }
 
     @Nested
