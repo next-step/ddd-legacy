0deacf5ad1965a910ed723b3db964a1214db2bc1
jkshin0602 <sjk6437@gmail.com>
2024-05-12 13:58:33 +0900

    test: 메뉴 가격 변경 테스트 추가

--- src/test/java/kitchenpos/application/MenuServiceTest.kt
+++ src/test/java/kitchenpos/application/MenuServiceTest.kt
@@ -6,6 +6,7 @@ import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.justRun
 import io.mockk.mockk
 import io.mockk.verify
 import kitchenpos.domain.*
@@ -300,6 +301,106 @@ class MenuServiceTest {
             result.menuGroup shouldBe request.menuGroup
             result.isDisplayed shouldBe request.isDisplayed
             result.menuProducts shouldBe request.menuProducts
+        }
+    }
+
+    @Nested
+    inner class `메뉴 가격 변경 테스트` {
+        @DisplayName("가격 정보가 null 이면, IllegalArgumentException 예외 처리한다.")
+        @Test
+        fun test1() {
+            // given
+            val menuId = UUID.randomUUID()
+            val request = mockk<Menu>().also {
+                every { it.price } returns null
+            }
+
+            // when & then
+            shouldThrowExactly<IllegalArgumentException> {
+                menuService.changePrice(menuId, request)
+            }
+        }
+
+        @DisplayName("가격 정보가 0보다 작으면, IllegalArgumentException 예외 처리 한다.")
+        @Test
+        fun test2() {
+            // given
+            val menuId = UUID.randomUUID()
+            val request = mockk<Menu>().also {
+                every { it.price } returns BigDecimal.valueOf(-1L)
+            }
+
+            // when & then
+            shouldThrowExactly<IllegalArgumentException> {
+                menuService.changePrice(menuId, request)
+            }
+        }
+
+        @DisplayName("메뉴 id가 존재하지 않으면, NoSuchElementException 예외 처리한다.")
+        @Test
+        fun test3() {
+            // given
+            val menuId = UUID.randomUUID()
+            val request = mockk<Menu>().also {
+                every { it.price } returns BigDecimal.valueOf(1000L)
+            }
+
+            every { menuRepository.findById(any()) } returns Optional.empty()
+
+            // when & then
+            shouldThrowExactly<NoSuchElementException> {
+                menuService.changePrice(menuId, request)
+            }
+        }
+
+        @DisplayName("메뉴에 속한 상품들의 가격보다 메뉴의 가격이 더 크면, IllegalArgumentException 예외 처리한다.")
+        @Test
+        fun test4() {
+            // given
+            val menuId = UUID.randomUUID()
+            val request = mockk<Menu>().also {
+                every { it.price } returns BigDecimal.valueOf(1000L)
+                every { it.id } returns menuId
+                every { it.menuProducts } returns listOf(mockk<MenuProduct>().also {
+                    every { it.product } returns mockk<Product>().also {
+                        every { it.price } returns BigDecimal.valueOf(999L)
+                    }
+                    every { it.quantity } returns 1
+                })
+            }
+
+            every { menuRepository.findById(any()) } returns Optional.of(request)
+
+            // when & then
+            shouldThrowExactly<IllegalArgumentException> {
+                menuService.changePrice(menuId, request)
+            }
+        }
+
+        @DisplayName("메뉴 가격 변경 요청이 정상이라면, 가격 변경")
+        @Test
+        fun test5() {
+            // given
+            val menuId = UUID.randomUUID()
+            val request = mockk<Menu>().also {
+                every { it.price } returns BigDecimal.valueOf(1000L)
+                every { it.id } returns menuId
+                every { it.menuProducts } returns listOf(mockk<MenuProduct>().also {
+                    every { it.product } returns mockk<Product>().also {
+                        every { it.price } returns BigDecimal.valueOf(1001L)
+                    }
+                    every { it.quantity } returns 1
+                })
+                justRun { it.price = BigDecimal.valueOf(1000L) }
+            }
+
+            every { menuRepository.findById(any()) } returns Optional.of(request)
+
+            // when
+            val result = menuService.changePrice(menuId, request)
+
+            // then
+            result.price shouldBe request.price
         }
     }
 }
