3a9834b24aa8e84468c05899d78b0f49a7f4a06f
domodazzi <domodazzi0@gmail.com>
2024-05-14 00:33:35 +0900

    feat(calculator): support custom delimiter

--- src/test/java/calculator/StringCalculator.java
+++ src/test/java/calculator/StringCalculator.java
@@ -1,18 +1,55 @@
 package calculator;
 
-import java.util.List;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class StringCalculator {
 
-    private static final String DEFAULT_DELIMITER = "(,|:)";
+    private static final String ANY_WORD_WITHOUT_NUMBER_AND_SPACE_PATTERN = "[^0-9\\s]";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile(
+        String.format("^//%s\n(.*)", ANY_WORD_WITHOUT_NUMBER_AND_SPACE_PATTERN)
+    );
 
-    public int add(final String expression) {
-        if (expression == null || expression.isEmpty()) {
+    private static final String NUMBER_PATTERN = "[1-9][d]*";
+    private static final String DEFAULT_DELIMITER = ",|:";
+
+    StringCalculator() {
+    }
+
+    public int add(final String input) {
+        if (input == null || input.isEmpty()) {
             return 0;
         }
 
-        final var numbers = getNumbers(expression);
+        final var matcher = CUSTOM_DELIMITER_PATTERN.matcher(input);
+
+        String delimiter;
+        String expression;
+
+        if (matcher.matches()) {
+            delimiter = matcher.group(0)
+                .substring(2, 3);
+            expression = matcher.group(1);
+        } else {
+            delimiter = DEFAULT_DELIMITER;
+            expression = input;
+        }
+
+        final var expressionPattern = String.format(
+            "^%s[(%s)%s]*$",
+            NUMBER_PATTERN,
+            delimiter,
+            NUMBER_PATTERN
+        );
+
+        if (!expression.matches(expressionPattern)) {
+            throw new IllegalArgumentException("입력을 해석할 수 없습니다.");
+        }
+
+        final var numbers = Stream.of(expression.split(delimiter))
+            .map(Integer::parseInt)
+            .toList();
+
         numbers.stream()
             .filter(number -> number < 0)
             .findAny()
@@ -25,9 +62,9 @@ public class StringCalculator {
             .orElse(0);
     }
 
-    private List<Integer> getNumbers(final String expression) {
-        return Stream.of(expression.split(DEFAULT_DELIMITER))
-            .map(Integer::parseInt)
-            .toList();
-    }
+//    private List<Integer> getNumbers(final String expression) {
+//        return Stream.of(expression.split(delimiter))
+//            .map(Integer::parseInt)
+//            .toList();
+//    }
 }
