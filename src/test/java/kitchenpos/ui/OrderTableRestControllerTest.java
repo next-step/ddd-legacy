c01c5082e9aebb5ba01307ecb407a5ef0d7cb691
kst <kst@osstem.com>
2024-05-13 20:32:07 +0900

    refactor(kitchenPos) : ordertable/product controller test 클래스 생성

---
+++ src/test/java/kitchenpos/ui/OrderTableRestControllerTest.java
@@ -0,0 +1,39 @@
+package kitchenpos.ui;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Class Description
+ *
+ * @author SungTae Kim
+ * @version 1.0
+ * @class OrderTableRestControllerTest
+ * @modification <pre>
+ * since	    author	        description
+ * ---------	------------	---------------------
+ * 2024-05-13      SungTae Kim	    최초 작성
+ * </pre>
+ * @copyRight COPYRIGHT © OSSTEM IMPLANT CO., LTD. ALL RIGHTS RESERVED.\n
+ */
+class OrderTableRestControllerTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void sit() {
+    }
+
+    @Test
+    void clear() {
+    }
+
+    @Test
+    void changeNumberOfGuests() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
---
+++ src/test/java/kitchenpos/ui/ProductRestControllerTest.java
@@ -0,0 +1,31 @@
+package kitchenpos.ui;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Class Description
+ *
+ * @author SungTae Kim
+ * @version 1.0
+ * @class ProductRestControllerTest
+ * @modification <pre>
+ * since	    author	        description
+ * ---------	------------	---------------------
+ * 2024-05-13      SungTae Kim	    최초 작성
+ * </pre>
+ * @copyRight COPYRIGHT © OSSTEM IMPLANT CO., LTD. ALL RIGHTS RESERVED.\n
+ */
+class ProductRestControllerTest {
+
+    @Test
+    void create() {
+    }
+
+    @Test
+    void changePrice() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
