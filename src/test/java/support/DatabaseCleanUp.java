6a8a906e2abd5d4c66680c81c38d549c50398618
dev-yoonjung <dear__yoon@naver.com>
2024-05-14 21:27:16 +0900

    config: configure acceptance test environment

---
+++ src/test/java/support/DatabaseCleanUp.java
@@ -0,0 +1,61 @@
+package support;
+
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jakarta.persistence.Table;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * <pre>
+ * 매 테스트 이후 Truncate 쿼리로 모든 테이블 초기화를
+ * 도와주는 모듈
+ * </pre>
+ */
+@Component
+@ActiveProfiles("test")
+public class DatabaseCleanUp {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    private List<String> tableNames;
+
+    @PostConstruct
+    public void init() {
+        tableNames = entityManager
+                .getMetamodel()
+                .getEntities()
+                .stream()
+                .filter(entity -> entity.getJavaType().getAnnotation(Entity.class) != null)
+                .map(entity -> entity.getJavaType().getAnnotation(Table.class).name())
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void execute() {
+        entityManager
+                .flush();
+
+        entityManager
+                .createNativeQuery("SET REFERENTIAL_INTEGRITY FALSE")
+                .executeUpdate();
+
+        for (String tableName : tableNames) {
+            entityManager
+                    .createNativeQuery(String.format("TRUNCATE TABLE %s", tableName))
+                    .executeUpdate();
+        }
+
+        entityManager
+                .createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE")
+                .executeUpdate();
+    }
+
+}
