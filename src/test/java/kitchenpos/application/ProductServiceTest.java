658e1b05108b763438232854ae7873541ee26278
boradol2 <boradol0902@gmail.com>
2024-05-15 03:17:18 +0900

    test(ProductTest): 테스트 코드 수정

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -60,162 +60,146 @@ class ProductServiceTest {
         ID_양념치킨 = 상품_양념치킨.getId();
     }
 
-    @DisplayName("상품 등록")
+    @DisplayName("상품을 등록한다.")
     @Nested
     class ProductCreate {
-        @DisplayName("[성공]")
-        @Nested
-        class Success {
-            @DisplayName("상품을 등록한다.")
-            @Test
-            void creatProduct() {
-                // given
-                Product request = productCreateRequest(이름_양념치킨, 가격_20000);
-                when(productRepository.save(any())).thenReturn(상품_양념치킨);
+        @DisplayName("[성공] 등록")
+        @Test
+        void success() {
+            // given
+            Product request = productCreateRequest(이름_양념치킨, 가격_20000);
+            when(productRepository.save(any())).thenReturn(상품_양념치킨);
 
-                // when
-                Product result = productService.create(request);
+            // when
+            Product result = productService.create(request);
 
-                // then
-                assertAll(
-                        () -> assertThat(result.getId()).isNotNull(),
-                        () -> assertThat(result.getName()).isEqualTo(이름_양념치킨),
-                        () -> assertThat(result.getPrice()).isEqualTo(가격_20000)
-                );
-            }
+            // then
+            assertAll(
+                    () -> assertThat(result.getId()).isNotNull(),
+                    () -> assertThat(result.getName()).isEqualTo(이름_양념치킨),
+                    () -> assertThat(result.getPrice()).isEqualTo(가격_20000)
+            );
         }
 
-        @DisplayName("[실패]")
-        @Nested
-        class Fail {
-            @DisplayName("이름은 필수로 입력해야 한다.")
-            @NullSource
-            @ParameterizedTest
-            void name1(String name) {
-                // given
-                Product request = productCreateRequest(name, 가격_20000);
+        @DisplayName("[실패] 이름은 필수로 입력해야 한다.")
+        @NullSource
+        @ParameterizedTest
+        void fail1(String name) {
+            // given
+            Product request = productCreateRequest(name, 가격_20000);
 
-                // when
-                // then
-                assertThatThrownBy(() -> productService.create(request))
-                        .isInstanceOf(IllegalArgumentException.class);
-            }
+            // when
+            // then
+            assertThatThrownBy(() -> productService.create(request))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-            @DisplayName("이름에 비속어를 포함하지 않는다.")
-            @ValueSource(strings = {"욕설", "비속어", "나쁜말"})
-            @ParameterizedTest
-            void name2(String name) {
-                // given
-                Product request = productCreateRequest(name, 가격_20000);
+        @DisplayName("[실패] 이름에 비속어를 포함하지 않는다.")
+        @ValueSource(strings = {"욕설", "비속어", "나쁜말"})
+        @ParameterizedTest
+        void fail2(String name) {
+            // given
+            Product request = productCreateRequest(name, 가격_20000);
 
-                // when
-                when(purgomalumClient.containsProfanity(name)).thenReturn(true);
+            // when
+            when(purgomalumClient.containsProfanity(name)).thenReturn(true);
 
-                // then
-                assertThatThrownBy(() -> productService.create(request))
-                        .isInstanceOf(IllegalArgumentException.class);
-            }
+            // then
+            assertThatThrownBy(() -> productService.create(request))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-            @DisplayName("가격은 필수로 입력해야 한다.")
-            @NullSource
-            @ParameterizedTest
-            void price1(BigDecimal price) {
-                // given
-                Product request = productCreateRequest(이름_양념치킨, price);
+        @DisplayName("[실패] 가격은 필수로 입력해야 한다.")
+        @NullSource
+        @ParameterizedTest
+        void fail3(BigDecimal price) {
+            // given
+            Product request = productCreateRequest(이름_양념치킨, price);
 
-                // when
-                // then
-                assertThatThrownBy(() -> productService.create(request))
-                        .isInstanceOf(IllegalArgumentException.class);
-            }
+            // when
+            // then
+            assertThatThrownBy(() -> productService.create(request))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-            @DisplayName("가격은 0원 이상이다.")
-            @ValueSource(longs = {-1, -1000, -9999999})
-            @ParameterizedTest
-            void price2(long price) {
-                // given
-                Product request = productCreateRequest(이름_양념치킨, BigDecimal.valueOf(price));
+        @DisplayName("[실패] 가격은 0원 이상이다.")
+        @ValueSource(longs = {-1, -1000, -9999999})
+        @ParameterizedTest
+        void fail4(long price) {
+            // given
+            Product request = productCreateRequest(이름_양념치킨, BigDecimal.valueOf(price));
 
-                // when
-                // then
-                assertThatThrownBy(() -> productService.create(request))
-                        .isInstanceOf(IllegalArgumentException.class);
-            }
+            // when
+            // then
+            assertThatThrownBy(() -> productService.create(request))
+                    .isInstanceOf(IllegalArgumentException.class);
         }
     }
 
-    @DisplayName("상품 가격 수정")
+    @DisplayName("상품의 가격이 수정된다.")
     @Nested
     class ProductPriceChange {
-        @DisplayName("[성공]")
-        @Nested
-        class Success {
-            @DisplayName("상품의 가격이 수정된다.")
-            @Test
-            void changeProductPriceTest() {
-                // given
-                Product request = productChangePriceRequest(가격_18000);
-                when(productRepository.findById(any())).thenReturn(Optional.of(상품_양념치킨));
+        @DisplayName("[성공] 가격 수정")
+        @Test
+        void success() {
+            // given
+            Product request = productChangePriceRequest(가격_18000);
+            when(productRepository.findById(any())).thenReturn(Optional.of(상품_양념치킨));
 
-                // when
-                Product result = productService.changePrice(ID_양념치킨, request);
+            // when
+            Product result = productService.changePrice(ID_양념치킨, request);
 
-                // then
-                assertThat(result.getPrice()).isEqualTo(request.getPrice());
-            }
+            // then
+            assertThat(result.getPrice()).isEqualTo(request.getPrice());
         }
 
-        @DisplayName("[실패]")
-        @Nested
-        class Fail {
-            @DisplayName("가격은 필수로 입력해야 한다.")
-            @NullSource
-            @ParameterizedTest
-            void price1(BigDecimal price) {
-                // given
-                Product request = productChangePriceRequest(price);
+        @DisplayName("[실패] 가격은 필수로 입력해야 한다.")
+        @NullSource
+        @ParameterizedTest
+        void fail1(BigDecimal price) {
+            // given
+            Product request = productChangePriceRequest(price);
 
-                // when
-                // then
-                assertThatThrownBy(() -> productService.changePrice(ID_양념치킨, request))
-                        .isInstanceOf(IllegalArgumentException.class);
-            }
+            // when
+            // then
+            assertThatThrownBy(() -> productService.changePrice(ID_양념치킨, request))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-            @DisplayName("가격은 0원 이상이다.")
-            @ValueSource(longs = {-1, -1000, -9999999})
-            @ParameterizedTest
-            void price2(long price) {
-                // given
-                Product request = productChangePriceRequest(BigDecimal.valueOf(price));
+        @DisplayName("[실패] 가격은 0원 이상이다.")
+        @ValueSource(longs = {-1, -1000, -9999999})
+        @ParameterizedTest
+        void fail2(long price) {
+            // given
+            Product request = productChangePriceRequest(BigDecimal.valueOf(price));
 
-                // when
-                // then
-                assertThatThrownBy(() -> productService.changePrice(ID_양념치킨, request))
-                        .isInstanceOf(IllegalArgumentException.class);
-            }
+            // when
+            // then
+            assertThatThrownBy(() -> productService.changePrice(ID_양념치킨, request))
+                    .isInstanceOf(IllegalArgumentException.class);
+        }
 
-            @DisplayName("상품을 포함한 각 메뉴의 가격이 상품의 (가격 * 수량)의 총 합보다 비싸면 메뉴는 노출하지 않는다.")
-            @Test
-            void pricePolicy() {
-                // given
-                BigDecimal 변경할_상품가격 = BigDecimal.valueOf(10_000);
-                List<Menu> 강정치킨상품_포함된_메뉴들 = settingMenus();
+        @DisplayName("[실패] 상품을 포함한 각 메뉴의 가격이 상품의 (가격 * 수량)의 총 합보다 비싸면 메뉴는 노출하지 않는다.")
+        @Test
+        void fail3() {
+            // given
+            BigDecimal 변경할_상품가격 = BigDecimal.valueOf(10_000);
+            List<Menu> 강정치킨상품_포함된_메뉴들 = settingMenus();
 
-                when(productRepository.findById(any())).thenReturn(Optional.of(상품_양념치킨));
-                when(menuRepository.findAllByProductId(any())).thenReturn(강정치킨상품_포함된_메뉴들);
+            when(productRepository.findById(any())).thenReturn(Optional.of(상품_양념치킨));
+            when(menuRepository.findAllByProductId(any())).thenReturn(강정치킨상품_포함된_메뉴들);
 
-                Product request = productChangePriceRequest(변경할_상품가격);
+            Product request = productChangePriceRequest(변경할_상품가격);
 
-                // when
-                productService.changePrice(ID_양념치킨, request);
+            // when
+            productService.changePrice(ID_양념치킨, request);
 
-                // then
-                강정치킨상품_포함된_메뉴들.forEach(menu -> assertThat(menu.isDisplayed()).isFalse());
-            }
+            // then
+            강정치킨상품_포함된_메뉴들.forEach(menu -> assertThat(menu.isDisplayed()).isFalse());
         }
     }
 
-    @DisplayName("상품 목록을 볼 수 있다")
+    @DisplayName("[성공] 상품 목록을 볼 수 있다")
     @Test
     void getMenuGroups() {
         // given
