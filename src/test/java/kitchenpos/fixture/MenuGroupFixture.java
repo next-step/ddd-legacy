a39990e24161bee32358625734f2e2e476a2a29c
hoa0217 <gjwjdghk123@naver.com>
2024-05-15 02:44:56 +0900

    refactor(kitchenpos): MenuGroupFixture 수정 및 ReadMe 체크

--- src/test/java/kitchenpos/fixture/MenuGroupFixture.java
+++ src/test/java/kitchenpos/fixture/MenuGroupFixture.java
@@ -6,14 +6,8 @@ import kitchenpos.domain.MenuGroup;
 public class MenuGroupFixture {
 
     public static MenuGroup createRequest(final String name) {
-        return createRequest(UUID.randomUUID(), name);
-    }
-
-    public static MenuGroup createRequest(final UUID id, final String name) {
         MenuGroup menuGroup = new MenuGroup();
-        menuGroup.setId(id);
         menuGroup.setName(name);
         return menuGroup;
     }
-
 }
