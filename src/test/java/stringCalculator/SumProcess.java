d9d366f07dfa02a88677dc3910ea0998c8b7d179
jinyoonYang <didwlsdbs123@naver.com>
2024-05-14 19:58:32 +0900

    소스 최신화

--- src/test/java/stringCalculator/SumProcess.java
+++ src/test/java/stringCalculator/SumProcess.java
@@ -1,36 +1,15 @@
 package stringCalculator;
 
-import java.util.Arrays;
+import java.util.List;
 
 public class SumProcess {
 
     public SumProcess() {
     }
 
-    public int sum(String[] numbers){
-        this.validateNumber(numbers);
-
-        return Arrays.stream(numbers)
-//                .filter(a -> this.isPositiveNumber(a))
-                .mapToInt(a -> Integer.parseInt(a))
+    public int sum(List<PositiveNumber> numbers){
+        return numbers.stream()
+                .mapToInt(PositiveNumber::getNumber)
                 .sum();
-    }
-
-    private void validateNumber(String[] numbers) {
-        Arrays.stream(numbers).forEach(a -> {
-            this.validateNumberFormatAndPositiveNumber(a);
-        });
-    }
-
-    private void validateNumberFormatAndPositiveNumber(String input) {
-        try {
-            int num = Integer.parseInt(input);
-
-            if (num < 0) {
-                throw new RuntimeException("input included negative number");
-            }
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("input is invalid value. please input string type number (input : "+input+")");
-        }
     }
 }
