ce151f06f2eecdb25e392862c12b2cbb444371a4
boradol2 <boradol0902@gmail.com>
2024-05-15 03:21:56 +0900

    test(AcceptanceTestConfig): Add @Primary

--- src/test/java/kitchenpos/acceptacne/AcceptanceTestConfig.java
+++ src/test/java/kitchenpos/acceptacne/AcceptanceTestConfig.java
@@ -4,10 +4,12 @@ import kitchenpos.infra.PurgomalumClient;
 import kitchenpos.infra.FakePurgomalumClient;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
 
 @TestConfiguration
 public class AcceptanceTestConfig {
     @Bean
+    @Primary
     public PurgomalumClient purgomalumClient() {
         return new FakePurgomalumClient();
     }
