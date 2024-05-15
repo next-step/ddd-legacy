e528e2d04bdbabbb643a37842aa0922c2bf11877
sjhello <sjhello235@gmail.com>
2024-05-15 17:04:49 +0900

    refactor: Nested 어노테이션을 이용하여 테스트 그룹화

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -4,6 +4,7 @@ import kitchenpos.domain.*;
 import kitchenpos.infra.PurgomalumClient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -53,120 +54,130 @@ class ProductServiceTest {
         후라이드치킨 = createProduct("후라이드치킨", BigDecimal.valueOf(11000));
     }
 
-    @ParameterizedTest
-    @DisplayName("등록 - 상품을 등록한다")
-    @ValueSource(strings = {"", " ", "강정치킨"})
-    void create(String name) {
-        // given
-        Product product = createProduct(name, BigDecimal.valueOf(18000));
-        given(productRepository.save(any())).willReturn(product);
+    @DisplayName("상품 등록")
+    @Nested
+    class ProductCreate {
 
-        // when
-        Product createdProduct = productService.create(product);
+        @ParameterizedTest
+        @DisplayName("상품을 등록한다")
+        @ValueSource(strings = {"", " ", "강정치킨"})
+        void create(String name) {
+            // given
+            Product product = createProduct(name, BigDecimal.valueOf(18000));
+            given(productRepository.save(any())).willReturn(product);
 
-        // then
-        assertThat(createdProduct.getId()).isEqualByComparingTo(product.getId());
+            // when
+            Product createdProduct = productService.create(product);
+
+            // then
+            assertThat(createdProduct.getId()).isEqualByComparingTo(product.getId());
+        }
+
+        @ParameterizedTest
+        @DisplayName("상품의 이름은 비속어가 될 수 없다")
+        @ValueSource(strings = {"비속어1", "비속어2"})
+        void createProductNameCannotBeProfanity(String name) {
+            Product product = createProduct(name);
+
+            when(purgomalumClient.containsProfanity(name)).thenReturn(true);
+
+            assertThatThrownBy(() -> {
+                productService.create(product);
+            }).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @ParameterizedTest
+        @DisplayName("상품의 이름은 null 이면 예외가 발생한다")
+        @NullSource
+        void createProductNameIsNotNull(String name) {
+            Product product = createProduct(name);
+
+            assertThatThrownBy(() -> {
+                productService.create(product);
+            }).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @Test
+        @DisplayName("상품의 가격은 0원 이상이어야 한다")
+        void createProductPriceIsGreaterThanZero() {
+            // given
+            Product product = createProduct(BigDecimal.ZERO);
+            given(productRepository.save(any())).willReturn(product);
+
+            // when
+            Product createdProduct = productService.create(product);
+
+            // then
+            assertThat(createdProduct.getId()).isEqualByComparingTo(product.getId());
+        }
+
+        @ParameterizedTest
+        @DisplayName("상품의 가격은 null 이면 예외가 발생한다")
+        @NullSource
+        void createProductPriceIsNotNull(BigDecimal price) {
+            Product product = createProduct(price);
+
+            assertThatThrownBy(() -> {
+                productService.create(product);
+            }).isInstanceOf(IllegalArgumentException.class);
+        }
     }
 
-    @ParameterizedTest
-    @DisplayName("등록 - 상품의 이름은 비속어가 될 수 없다")
-    @ValueSource(strings = {"비속어1", "비속어2"})
-    void createProductNameCannotBeProfanity(String name) {
-        Product product = createProduct(name);
+    @Nested
+    @DisplayName("상품 수정")
+    class ProductModify {
 
-        when(purgomalumClient.containsProfanity(name)).thenReturn(true);
+        @Test
+        @DisplayName("상품의 가격을 수정한다")
+        void changePrice() {
+            when(productRepository.findById(any())).thenReturn(Optional.of(강정치킨));
 
-        assertThatThrownBy(() -> {
-            productService.create(product);
-        }).isInstanceOf(IllegalArgumentException.class);
-    }
+            Product request = changeProduct(BigDecimal.valueOf(10000));
+            Product result = productService.changePrice(강정치킨.getId(), request);
 
-    @Test
-    @DisplayName("등록 - 상품의 가격은 0원 이상이어야 한다")
-    void createProductPriceIsGreaterThanZero() {
-        // given
-        Product product = createProduct(BigDecimal.ZERO);
-        given(productRepository.save(any())).willReturn(product);
+            assertThat(result.getPrice()).isEqualTo(request.getPrice());
+        }
 
-        // when
-        Product createdProduct = productService.create(product);
+        @ParameterizedTest
+        @DisplayName("변경하려는 상품의 가격이 null 이면 예외가 발생한다")
+        @NullSource
+        void changePriceIsNull(BigDecimal price) {
+            Product request = changeProduct(price);
 
-        // then
-        assertThat(createdProduct.getId()).isEqualByComparingTo(product.getId());
-    }
+            assertThatThrownBy(() -> {
+                productService.changePrice(강정치킨.getId(), request);
+            }).isInstanceOf(IllegalArgumentException.class);
+        }
 
-    @ParameterizedTest
-    @DisplayName("등록 - 상품의 이름은 null 이면 예외가 발생한다")
-    @NullSource
-    void createProductNameIsNotNull(String name) {
-        Product product = createProduct(name);
+        @ParameterizedTest
+        @DisplayName("변경하려는 상품의 가격이 0보다 작으면 예외가 발생한다")
+        @ValueSource(ints = {-1, -20000})
+        void changePriceIsLowerThanZero(int price) {
+            Product request = changeProduct(BigDecimal.valueOf(price));
 
-        assertThatThrownBy(() -> {
-            productService.create(product);
-        }).isInstanceOf(IllegalArgumentException.class);
-    }
+            assertThatThrownBy(() -> {
+                productService.changePrice(강정치킨.getId(), request);
+            }).isInstanceOf(IllegalArgumentException.class);
+        }
 
-    @ParameterizedTest
-    @DisplayName("등록 - 상품의 가격은 null 이면 예외가 발생한다")
-    @NullSource
-    void createProductPriceIsNotNull(BigDecimal price) {
-        Product product = createProduct(price);
+        @Test
+        @DisplayName("메뉴의 가격이 메뉴에 등록되어 있는 상품의 합(가격 * 수량)보다 크다면 메뉴는 전시되지 않는다")
+        void changeProductPriceThenNotDisplayed() {
+            // given
+            BigDecimal 변경하려는_가격 = BigDecimal.valueOf(8000);
+            Product 강정치킨 = createProduct("강정치킨", BigDecimal.valueOf(12000));
+            List<Menu> menus = createMenus(강정치킨);
 
-        assertThatThrownBy(() -> {
-            productService.create(product);
-        }).isInstanceOf(IllegalArgumentException.class);
-    }
+            when(productRepository.findById(any())).thenReturn(Optional.of(강정치킨));
+            when(menuRepository.findAllByProductId(강정치킨.getId())).thenReturn(menus);
+            Product request = createProduct(변경하려는_가격);
 
-    @Test
-    @DisplayName("수정 - 상품의 가격을 수정한다")
-    void changePrice() {
-        when(productRepository.findById(any())).thenReturn(Optional.of(강정치킨));
-
-        Product request = changeProduct(BigDecimal.valueOf(10000));
-        Product result = productService.changePrice(강정치킨.getId(), request);
-
-        assertThat(result.getPrice()).isEqualTo(request.getPrice());
-    }
-
-    @ParameterizedTest
-    @DisplayName("수정 - 변경하려는 상품의 가격이 null 이면 예외가 발생한다")
-    @NullSource
-    void changePriceIsNull(BigDecimal price) {
-        Product request = changeProduct(price);
-
-        assertThatThrownBy(() -> {
+            // when
             productService.changePrice(강정치킨.getId(), request);
-        }).isInstanceOf(IllegalArgumentException.class);
-    }
 
-    @ParameterizedTest
-    @DisplayName("수정 - 변경하려는 상품의 가격이 0보다 작으면 예외가 발생한다")
-    @ValueSource(ints = {-1, -20000})
-    void changePriceIsLowerThanZero(int price) {
-        Product request = changeProduct(BigDecimal.valueOf(price));
-
-        assertThatThrownBy(() -> {
-            productService.changePrice(강정치킨.getId(), request);
-        }).isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @Test
-    @DisplayName("수정 - 메뉴의 가격이 메뉴에 등록되어 있는 상품의 합(가격 * 수량)보다 크다면 메뉴는 전시되지 않는다")
-    void changeProductPriceThenNotDisplayed() {
-        // given
-        BigDecimal 변경하려는_가격 = BigDecimal.valueOf(8000);
-        Product 강정치킨 = createProduct("강정치킨", BigDecimal.valueOf(12000));
-        List<Menu> menus = createMenus(강정치킨);
-
-        when(productRepository.findById(any())).thenReturn(Optional.of(강정치킨));
-        when(menuRepository.findAllByProductId(강정치킨.getId())).thenReturn(menus);
-        Product request = createProduct(변경하려는_가격);
-
-        // when
-        productService.changePrice(강정치킨.getId(), request);
-
-        // then
-        menus.forEach(menu -> assertThat(menu.isDisplayed()).isFalse());
+            // then
+            menus.forEach(menu -> assertThat(menu.isDisplayed()).isFalse());
+        }
     }
 
     @Test
