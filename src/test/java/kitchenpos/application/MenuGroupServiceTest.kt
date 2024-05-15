5e92e250670939a9a5d8643b99aa98a5ee3b3d77
writer0713 <writer0713@naver.com>
2024-05-15 16:10:08 +0900

    test: 메뉴그룹 생성 관련 테스트 추가 - 메뉴 그룹 등록이 가능하다. - 메뉴 그룹 이름을 꼭 입력해야 한다.

---
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.kt
@@ -0,0 +1,42 @@
+package kitchenpos.application
+
+import kitchenpos.domain.MenuGroup
+import kitchenpos.domain.MenuGroupRepository
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.TestConstructor
+
+@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
+@SpringBootTest
+class MenuGroupServiceTest(
+    private val sut: MenuGroupService,
+    private val menuGroupRepository: MenuGroupRepository,
+) {
+    @DisplayName("메뉴그룹 생성시 'name' 은 필수다 - ('name' 있을시)")
+    @Test
+    fun menu_group_create_test1() {
+        // given
+        val request = MenuGroup()
+        request.name = "한마리 치킨"
+
+        // when
+        val createdMenuGroup = sut.create(request)
+
+        // then
+        assertThat(createdMenuGroup.id).isNotNull()
+    }
+
+    @DisplayName("메뉴그룹 생성시 'name' 은 필수다 - ('name' 없을때)")
+    @Test
+    fun menu_group_create_test2() {
+        // given
+        val request = MenuGroup()
+
+        // when
+        // then
+        assertThrows<IllegalArgumentException> { sut.create(request) }
+    }
+}
