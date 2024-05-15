d043e97a3328d062ad8df0d60e9a092459d2208d
seyun(marco) <ksy90101@gmail.com>
2024-05-16 00:04:31 +0900

    test: orderTable 통합 테스트

--- src/test/java/kitchenpos/integration/OrderTableRestControllerTest.java
+++ src/test/java/kitchenpos/integration/OrderTableRestControllerTest.java
@@ -1,28 +1,121 @@
 package kitchenpos.integration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import kitchenpos.config.IntegrationTest;
+import kitchenpos.domain.OrderTable;
+import kitchenpos.domain.OrderTableRepository;
+import kitchenpos.util.MockMvcUtil;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import java.util.List;
+
+import static kitchenpos.fixture.OrderTableFixture.createOrderTable;
+import static kitchenpos.fixture.OrderTableFixture.createOrderTableWithId;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 
 @IntegrationTest
 class OrderTableRestControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Autowired
+    private OrderTableRepository orderTableRepository;
+
     @Test
-    void create() {
+    void create() throws Exception {
+        MvcResult result = mockMvc.perform(post("/api/order-tables")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(createOrderTable("1번테이블"))))
+                .andReturn();
+
+        OrderTable orderTable = MockMvcUtil.readValue(objectMapper, result, OrderTable.class);
+
+        assertAll(
+                () -> assertThat(result.getResponse().getStatus()).isEqualTo(HttpStatus.CREATED.value()),
+                () -> assertThat(result.getResponse().getHeader("Location")).isEqualTo("/api/order-tables/" + orderTable.getId()),
+                () -> assertThat(orderTable.getId()).isNotNull(),
+                () -> assertThat(orderTable.getName()).isEqualTo("1번테이블"),
+                () -> assertThat(orderTable.getNumberOfGuests()).isZero(),
+                () -> assertThat(orderTable.isOccupied()).isFalse()
+        );
     }
 
     @Test
-    void sit() {
+    void sit() throws Exception {
+        OrderTable changedOrderTable = orderTableRepository.save(createOrderTableWithId("1번테이블", false, 0));
+        MvcResult result = mockMvc.perform(put("/api/order-tables/" + changedOrderTable.getId() + "/sit")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andReturn();
+
+        OrderTable orderTable = MockMvcUtil.readValue(objectMapper, result, OrderTable.class);
+
+        assertAll(
+                () -> assertThat(result.getResponse().getStatus()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(orderTable.isOccupied()).isTrue()
+        );
     }
 
     @Test
-    void clear() {
+    void clear() throws Exception {
+        OrderTable changedOrderTable = orderTableRepository.save(createOrderTableWithId("1번테이블", true, 0));
+        MvcResult result = mockMvc.perform(put("/api/order-tables/" + changedOrderTable.getId() + "/clear")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andReturn();
+
+        OrderTable orderTable = MockMvcUtil.readValue(objectMapper, result, OrderTable.class);
+
+        assertAll(
+                () -> assertThat(result.getResponse().getStatus()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(orderTable.isOccupied()).isFalse()
+        );
     }
 
     @Test
-    void changeNumberOfGuests() {
+    void changeNumberOfGuests() throws Exception {
+        OrderTable changedOrderTable = orderTableRepository.save(createOrderTableWithId("1번테이블", true, 0));
+        changedOrderTable.setNumberOfGuests(4);
+
+        MvcResult result = mockMvc.perform(put("/api/order-tables/" + changedOrderTable.getId() + "/number-of-guests")
+                        .contentType(MediaType.APPLICATION_JSON_VALUE)
+                        .content(objectMapper.writeValueAsString(changedOrderTable)))
+                .andReturn();
+
+        OrderTable orderTable = MockMvcUtil.readValue(objectMapper, result, OrderTable.class);
+
+        assertAll(
+                () -> assertThat(result.getResponse().getStatus()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(orderTable.getNumberOfGuests()).isEqualTo(4)
+        );
     }
 
     @Test
-    void findAll() {
+    void findAll() throws Exception {
+        OrderTable orderTable1 = orderTableRepository.save(createOrderTableWithId("1번테이블"));
+        OrderTable orderTable2 = orderTableRepository.save(createOrderTableWithId("2번테이블"));
+
+        MvcResult result = mockMvc.perform(get("/api/order-tables"))
+                .andReturn();
+
+        List<OrderTable> orderTables = MockMvcUtil.readListValue(objectMapper, result, OrderTable.class);
+
+        // then
+        assertAll(
+                () -> assertThat(result.getResponse().getStatus()).isEqualTo(HttpStatus.OK.value()),
+                () -> assertThat(orderTables).hasSize(2),
+                () -> assertThat(orderTables.stream().map(OrderTable::getId)).contains(orderTable1.getId(), orderTable2.getId())
+        );
     }
 }
