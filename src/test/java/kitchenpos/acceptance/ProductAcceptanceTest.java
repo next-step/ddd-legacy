8908703aa19d17554d3f76a24117aea4f266d106
seyun(marco) <ksy90101@gmail.com>
2024-05-15 22:32:47 +0900

    test: 상품 인수테스트 작성

--- src/test/java/kitchenpos/acceptance/ProductAcceptanceTest.java
+++ src/test/java/kitchenpos/acceptance/ProductAcceptanceTest.java
@@ -8,7 +8,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.HttpStatus;
 
 import java.math.BigDecimal;
 import java.math.RoundingMode;
@@ -33,28 +32,15 @@ class ProductAcceptanceTest {
         Response 후라이드_치킨 = ProductAcceptanceStep.create(createProduct("후라이드 치킨", BigDecimal.valueOf(16000)));
         String productId = 후라이드_치킨.getBody().jsonPath().getString("id");
 
-        assertThat(후라이드_치킨.statusCode()).isEqualTo(HttpStatus.CREATED.value());
-        assertThat(후라이드_치킨.header("Location")).isEqualTo("/api/products/" + productId);
-        assertThat(productId).isNotNull();
-        assertThat(후라이드_치킨.getBody().jsonPath().getString("name")).isEqualTo("후라이드 치킨");
-        assertThat(후라이드_치킨.getBody().jsonPath().getObject("price", BigDecimal.class)).isEqualTo(BigDecimal.valueOf(16000));
-
         Response findAll = ProductAcceptanceStep.findAll();
-        assertThat(findAll.statusCode()).isEqualTo(HttpStatus.OK.value());
         assertThat(findAll.getBody().jsonPath().getList("name")).contains("후라이드 치킨");
         assertThat(findAll.getBody().jsonPath().getList("id")).contains(productId);
         assertThat(findAll.getBody().jsonPath().getList("price", BigDecimal.class)).contains(BigDecimal.valueOf(16000).setScale(1, RoundingMode.HALF_UP));
 
-        후라이드_치킨 = ProductAcceptanceStep.changePrice(UUID.fromString(productId), createProduct("후라이드 치킨", BigDecimal.valueOf(17000)));
-
-        assertThat(후라이드_치킨.statusCode()).isEqualTo(HttpStatus.OK.value());
-        assertThat(productId).isNotNull();
-        assertThat(후라이드_치킨.getBody().jsonPath().getString("name")).isEqualTo("후라이드 치킨");
-        assertThat(후라이드_치킨.getBody().jsonPath().getObject("price", BigDecimal.class)).isEqualTo(BigDecimal.valueOf(17000));
+        ProductAcceptanceStep.changePrice(UUID.fromString(productId), createProduct("후라이드 치킨", BigDecimal.valueOf(17000)));
 
         findAll = ProductAcceptanceStep.findAll();
 
-        assertThat(findAll.statusCode()).isEqualTo(HttpStatus.OK.value());
         assertThat(findAll.getBody().jsonPath().getList("name")).contains("후라이드 치킨");
         assertThat(findAll.getBody().jsonPath().getList("id")).contains(productId);
         assertThat(findAll.getBody().jsonPath().getList("price", BigDecimal.class)).contains(BigDecimal.valueOf(17000).setScale(1, RoundingMode.HALF_UP));
--- src/test/java/kitchenpos/acceptance/step/ProductAcceptanceStep.java
+++ src/test/java/kitchenpos/acceptance/step/ProductAcceptanceStep.java
@@ -3,14 +3,18 @@ package kitchenpos.acceptance.step;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import kitchenpos.domain.Product;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 
+import java.math.BigDecimal;
 import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ProductAcceptanceStep {
     public static Response create(Product product) {
         // @formatter:off
-        return RestAssured
+        Response response = RestAssured
                 .given()
                     .log().all()
                     .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -21,11 +25,21 @@ public class ProductAcceptanceStep {
                     .extract()
                     .response();
         // @formatter:on
+
+        String productId = response.getBody().jsonPath().getString("id");
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.header("Location")).isEqualTo("/api/products/" + productId);
+        assertThat(productId).isNotNull();
+        assertThat(response.getBody().jsonPath().getString("name")).isEqualTo(product.getName());
+        assertThat(response.getBody().jsonPath().getObject("price", BigDecimal.class)).isEqualTo(product.getPrice());
+
+        return response;
     }
 
     public static Response changePrice(UUID productId, Product product) {
         // @formatter:off
-        return RestAssured
+        Response response =  RestAssured
                 .given()
                 .log().all()
                     .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -37,11 +51,16 @@ public class ProductAcceptanceStep {
                     .extract()
                     .response();
         // @formatter:on
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.getBody().jsonPath().getString("name")).isEqualTo(product.getName());
+        assertThat(response.getBody().jsonPath().getObject("price", BigDecimal.class)).isEqualTo(product.getPrice());
+
+        return response;
     }
 
     public static Response findAll() {
         // @formatter:off
-        return RestAssured
+        Response response = RestAssured
                 .given()
                     .log().all()
                     .contentType(MediaType.APPLICATION_JSON_VALUE)
@@ -51,5 +70,9 @@ public class ProductAcceptanceStep {
                     .extract()
                     .response();
         // @formatter:on
+
+        assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+
+        return response;
     }
 }
