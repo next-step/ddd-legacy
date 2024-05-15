60c693944808dcd24008dd810d11c3dbb01270cd
writer0713 <writer0713@naver.com>
2024-05-15 18:04:39 +0900

    test: @Nested 사용하여 관련된 테스트들 묶도록 수정

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.kt
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.kt
@@ -4,6 +4,7 @@ import kitchenpos.domain.MenuGroup
 import kitchenpos.domain.MenuGroupRepository
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.springframework.boot.test.context.SpringBootTest
@@ -15,29 +16,33 @@ class MenuGroupServiceTest(
     private val sut: MenuGroupService,
     private val menuGroupRepository: MenuGroupRepository,
 ) {
-    @DisplayName("메뉴그룹 생성시 'name' 은 필수다 - ('name' 있을시)")
-    @Test
-    fun create_menu_group_test1() {
-        // given
-        val request = MenuGroup()
-        request.name = "한마리 치킨"
+    @DisplayName("메뉴 그룹 등록이 가능하다.")
+    @Nested
+    inner class Create {
+        @DisplayName("메뉴그룹 생성시 'name' 은 필수다 - ('name' 있을시)")
+        @Test
+        fun create_menu_group_test1() {
+            // given
+            val request = MenuGroup()
+            request.name = "한마리 치킨"
 
-        // when
-        val createdMenuGroup = sut.create(request)
+            // when
+            val createdMenuGroup = sut.create(request)
 
-        // then
-        assertThat(createdMenuGroup.id).isNotNull()
-    }
+            // then
+            assertThat(createdMenuGroup.id).isNotNull()
+        }
 
-    @DisplayName("메뉴그룹 생성시 'name' 은 필수다 - ('name' 없을때)")
-    @Test
-    fun create_menu_group_test2() {
-        // given
-        val request = MenuGroup()
+        @DisplayName("메뉴그룹 생성시 'name' 은 필수다 - ('name' 없을때)")
+        @Test
+        fun create_menu_group_test2() {
+            // given
+            val request = MenuGroup()
 
-        // when
-        // then
-        assertThrows<IllegalArgumentException> { sut.create(request) }
+            // when
+            // then
+            assertThrows<IllegalArgumentException> { sut.create(request) }
+        }
     }
 
     @DisplayName("등록된 메뉴그룹 목록을 조회할 수 있다")
--- src/test/java/kitchenpos/application/ProductServiceTest.kt
+++ src/test/java/kitchenpos/application/ProductServiceTest.kt
@@ -6,6 +6,7 @@ import kitchenpos.infra.PurgomalumClient
 import kitchenpos.utils.generateUUIDFrom
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 import org.mockito.BDDMockito.any
@@ -36,116 +37,124 @@ class ProductServiceTest {
         assertThat(products.size).isGreaterThan(0)
     }
 
-    @DisplayName("상품 가격을 꼭 입력해야 한다.")
-    @Test
-    fun create_product_test1() {
-        // given
-        val request = Product()
-
-        // when
-        // then
-        assertThrows<IllegalArgumentException> { sut.create(request) }
-    }
-
-    @DisplayName("가격은 음수일수 없다.")
-    @Test
-    fun create_product_test2() {
-        // given
-        val request = Product()
-        request.price = BigDecimal.valueOf(-1000)
-        // when
-        // then
-        assertThrows<IllegalArgumentException> { sut.create(request) }
-    }
-
-    @DisplayName("상품 이름을 꼭 입력해야 한다.")
-    @Test
-    fun create_product_test3() {
-        // given
-        val request = Product()
-        request.price = BigDecimal.valueOf(1000)
-
-        // when
-        // then
-        assertThrows<IllegalArgumentException> { sut.create(request) }
-    }
-
-    @DisplayName("상품 이름에는 비속어가 들어갈 수 없다.")
-    @Test
-    fun create_product_test4() {
-        // given
-        val slang = "비속어"
-        val request = Product()
-        request.price = BigDecimal.valueOf(1000)
-        request.name = slang
-        given(purgomalumCloneable.containsProfanity(slang)).willReturn(true)
-
-        // when
-        // then
-        assertThrows<IllegalArgumentException> { sut.create(request) }
-    }
-
     @DisplayName("상품 등록이 가능하다.")
-    @Test
-    fun create_product_test5() {
-        // given
-        val request = Product()
-        request.price = BigDecimal.valueOf(1000)
-        request.name = "후라이드 치킨"
-        given(purgomalumCloneable.containsProfanity(any())).willReturn(false)
+    @Nested
+    inner class Create {
+        @DisplayName("상품 가격을 꼭 입력해야 한다.")
+        @Test
+        fun create_product_test1() {
+            // given
+            val request = Product()
 
-        // when
-        val createdProduct = sut.create(request)
+            // when
+            // then
+            assertThrows<IllegalArgumentException> { sut.create(request) }
+        }
 
-        // then
-        assertThat(createdProduct.id).isNotNull()
-    }
+        @DisplayName("가격은 음수일수 없다.")
+        @Test
+        fun create_product_test2() {
+            // given
+            val request = Product()
+            request.price = BigDecimal.valueOf(-1000)
+            // when
+            // then
+            assertThrows<IllegalArgumentException> { sut.create(request) }
+        }
 
-    @DisplayName("변경하려는 가격은 음수일 수 없다.")
-    @Test
-    fun change_price_test1() {
-        // given
-        val productId = generateUUIDFrom(uuidWithoutDash = "3b52824434f7406bbb7e690912f66b10")
-        val request = Product()
-        request.price = BigDecimal.valueOf(-1000)
+        @DisplayName("상품 이름을 꼭 입력해야 한다.")
+        @Test
+        fun create_product_test3() {
+            // given
+            val request = Product()
+            request.price = BigDecimal.valueOf(1000)
 
-        // when
-        // then
-        assertThrows<IllegalArgumentException> { sut.changePrice(productId, request) }
+            // when
+            // then
+            assertThrows<IllegalArgumentException> { sut.create(request) }
+        }
+
+        @DisplayName("상품 이름에는 비속어가 들어갈 수 없다.")
+        @Test
+        fun create_product_test4() {
+            // given
+            val slang = "비속어"
+            val request = Product()
+            request.price = BigDecimal.valueOf(1000)
+            request.name = slang
+            given(purgomalumCloneable.containsProfanity(slang)).willReturn(true)
+
+            // when
+            // then
+            assertThrows<IllegalArgumentException> { sut.create(request) }
+        }
+
+        @DisplayName("상품 등록 성공.")
+        @Test
+        fun create_product_test5() {
+            // given
+            val request = Product()
+            request.price = BigDecimal.valueOf(1000)
+            request.name = "후라이드 치킨"
+            given(purgomalumCloneable.containsProfanity(any())).willReturn(false)
+
+            // when
+            val createdProduct = sut.create(request)
+
+            // then
+            assertThat(createdProduct.id).isNotNull()
+        }
     }
 
     @DisplayName("등록된 상품의 가격을 수정할 수 있다.")
-    @Test
-    fun change_price_test2() {
-        // given
-        val productId = generateUUIDFrom(uuidWithoutDash = "3b52824434f7406bbb7e690912f66b10")
-        val targetPrice = BigDecimal.valueOf(17_000)
-        val request = Product()
-        request.price = targetPrice
+    @Nested
+    inner class Update {
+        @DisplayName("변경하려는 가격은 음수일 수 없다.")
+        @Test
+        fun change_price_test1() {
+            // given
+            val productId = generateUUIDFrom(uuidWithoutDash = "3b52824434f7406bbb7e690912f66b10")
+            val request = Product()
+            request.price = BigDecimal.valueOf(-1000)
 
-        // when
-        val changedProduct = sut.changePrice(productId, request)
+            // when
+            // then
+            assertThrows<IllegalArgumentException> { sut.changePrice(productId, request) }
+        }
 
-        // then
-        assertThat(changedProduct.price).isEqualTo(targetPrice)
-    }
+        @DisplayName("등록된 상품의 가격 수정 성공.")
+        @Test
+        fun change_price_test2() {
+            // given
+            val productId = generateUUIDFrom(uuidWithoutDash = "3b52824434f7406bbb7e690912f66b10")
+            val targetPrice = BigDecimal.valueOf(17_000)
+            val request = Product()
+            request.price = targetPrice
 
-    @DisplayName("해당 상품으로 구성된 메뉴의 가격이 수정된 상품의 가격 총합보다 크면 메뉴를 비노출한다.")
-    @Test
-    fun change_price_test3() {
-        // given
-        val productId = generateUUIDFrom(uuidWithoutDash = "3b52824434f7406bbb7e690912f66b10")
-        val targetPrice = BigDecimal.valueOf(13_000)
-        val request = Product()
-        request.price = targetPrice
+            // when
+            val changedProduct = sut.changePrice(productId, request)
 
-        // when
-        val changedProduct = sut.changePrice(productId, request)
+            // then
+            assertThat(changedProduct.price).isEqualTo(targetPrice)
+        }
 
-        // then
-        val menu =
-            menuRepository.findAllByProductId(productId)
-                .first { menu -> menu.price > changedProduct.price }
-        assertThat(menu.isDisplayed).isFalse()
+        @DisplayName("해당 상품으로 구성된 메뉴의 가격이 수정된 상품의 가격 총합보다 크면 메뉴를 비노출한다.")
+        @Test
+        fun change_price_test3() {
+            // given
+            val productId = generateUUIDFrom(uuidWithoutDash = "3b52824434f7406bbb7e690912f66b10")
+            val targetPrice = BigDecimal.valueOf(13_000)
+            val request = Product()
+            request.price = targetPrice
+
+            // when
+            val changedProduct = sut.changePrice(productId, request)
+
+            // then
+            val menu =
+                menuRepository.findAllByProductId(productId)
+                    .first { menu -> menu.price > changedProduct.price }
+            assertThat(menu.isDisplayed).isFalse()
+        }
     }
 }
