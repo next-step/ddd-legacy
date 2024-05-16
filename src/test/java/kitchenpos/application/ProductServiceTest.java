acb8a3d9bdfbf7c26a3db4d30ca7b44d394c1699
김진홍 <jhkim10@miridih.com>
2024-05-16 21:07:44 +0900

    test: ProductService.changePrice()에 대한 테스트

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -1,8 +1,6 @@
 package kitchenpos.application;
 
-import kitchenpos.domain.MenuRepository;
-import kitchenpos.domain.Product;
-import kitchenpos.domain.ProductRepository;
+import kitchenpos.domain.*;
 import kitchenpos.infra.PurgomalumClient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,6 +9,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
@@ -71,6 +71,7 @@ class ProductServiceTest {
     }
 
     @Test
+    @DisplayName("상품이 정상적으로 생성된다.")
     void create_success() {
         // given
         Product product = new Product();
@@ -82,5 +83,99 @@ class ProductServiceTest {
 
         // then
         assertThat(savedProduct.getId()).isNotNull();
+    }
+
+    @ParameterizedTest
+    @NullSource
+    @DisplayName("가격이 null일 경우 상품 가격 변경 시 IllegalArgumentException이 발생한다.")
+    void changePrice_fail_for_price_null(BigDecimal price) {
+        // given
+        Product product = new Product();
+        product.setPrice(price);
+        product.setId(UUID.randomUUID());
+
+        productRepository.save(product);
+
+        // when, then
+        assertThatThrownBy(() -> productService.changePrice(product.getId(), product))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("가격이 음수일 경우 상품 가격 변경 시 IllegalArgumentException이 발생한다.")
+    void changePrice_fail_for_price_negative() {
+        // given
+        Product product = new Product();
+        product.setPrice(BigDecimal.valueOf(-1L));
+        product.setId(UUID.randomUUID());
+
+        productRepository.save(product);
+
+        // when, then
+        assertThatThrownBy(() -> productService.changePrice(product.getId(), product))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("상품이 존재하지 않는데 상품 가격 변경 시 IllegalArgumentException이 발생한다.")
+    void changePrice_fail_for_product_not_found() {
+        assertThatThrownBy(() -> productService.changePrice(UUID.randomUUID(), new Product()))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("상품 가격이 정상적으로 변경된다.")
+    void changePrice_success() {
+        // given
+        Product product = new Product();
+        product.setName("상품 이름");
+        product.setPrice(BigDecimal.valueOf(1000L));
+
+        Product savedProduct = productRepository.save(product);
+
+        // when
+        Product request = new Product();
+        request.setPrice(BigDecimal.valueOf(2000L));
+
+        Product updatedProduct = productService.changePrice(savedProduct.getId(), request);
+
+        // then
+        assertThat(updatedProduct.getPrice()).isEqualTo(request.getPrice());
+    }
+
+    @Test
+    @DisplayName("상품 가격 변경 후 메뉴 상품들의 가격 합이 메뉴 가격보다 작을 경우 메뉴의 display를 false로 변경한다.")
+    void changePrice_menu_display_false_for() {
+        // given
+        Product product = new Product();
+        product.setId(UUID.randomUUID());
+        product.setName("상품 이름");
+        product.setPrice(BigDecimal.valueOf(1000L));
+
+        Product savedProduct = productRepository.save(product);
+
+        Menu menu = new Menu();
+        menu.setId(UUID.randomUUID());
+        menu.setName("메뉴 이름");
+        menu.setPrice(BigDecimal.valueOf(3000L));
+        menu.setDisplayed(true);
+
+        MenuProduct menuProduct = new MenuProduct();
+        menuProduct.setProductId(savedProduct.getId());
+        menuProduct.setProduct(savedProduct);
+        menuProduct.setQuantity(1L);
+
+        menu.setMenuProducts(List.of(menuProduct));
+
+        menuRepository.save(menu);
+
+        Product request = new Product();
+        request.setPrice(BigDecimal.valueOf(2000L));
+
+        // when
+        productService.changePrice(savedProduct.getId(), request);
+
+        // then
+        assertThat(menu.isDisplayed()).isFalse();
     }
 }
