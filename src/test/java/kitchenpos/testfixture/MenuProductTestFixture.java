94cf66e70e2dab378502c6810658aff6733fe3b8
MaroKim <dkgell6@gmail.com>
2024-05-16 23:02:37 +0900

    refactor(kitchenpos - calculator): 메뉴 상품 정렬 수정

--- src/test/java/kitchenpos/testfixture/MenuProductTestFixture.java
+++ src/test/java/kitchenpos/testfixture/MenuProductTestFixture.java
@@ -5,12 +5,12 @@ import kitchenpos.domain.Product;
 
 public class MenuProductTestFixture {
 
-    public static MenuProduct createMenuProductRequest(){
-        Product product =ProductTestFixture.createProductRequest();
+    public static MenuProduct createMenuProductRequest() {
+        Product product = ProductTestFixture.createProductRequest();
         return createMenuProductRequest(1L, 1L, product);
     }
 
-    public static MenuProduct createMenuProductRequest(Long seq, long quantity, Product product){
+    public static MenuProduct createMenuProductRequest(Long seq, long quantity, Product product) {
         MenuProduct menuProduct = new MenuProduct();
         menuProduct.setSeq(seq);
         menuProduct.setProductId(product.getId());
