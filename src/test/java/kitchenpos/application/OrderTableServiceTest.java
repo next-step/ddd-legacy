404be6a8c3e120c4c8e98df07353bd4b62cdc83b
김진홍 <jhkim10@miridih.com>
2024-05-17 21:23:16 +0900

    test: OrderTableService.clear()에 대한 테스트

--- src/test/java/kitchenpos/application/OrderTableServiceTest.java
+++ src/test/java/kitchenpos/application/OrderTableServiceTest.java
@@ -1,8 +1,6 @@
 package kitchenpos.application;
 
-import kitchenpos.domain.OrderRepository;
-import kitchenpos.domain.OrderTable;
-import kitchenpos.domain.OrderTableRepository;
+import kitchenpos.domain.*;
 import kitchenpos.infra.InMemoryOrderRepository;
 import kitchenpos.infra.InMemoryOrderTableRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,6 +9,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
+import java.time.LocalDateTime;
 import java.util.NoSuchElementException;
 import java.util.UUID;
 
@@ -64,6 +63,46 @@ class OrderTableServiceTest {
 
         OrderTable response = orderTableService.sit(orderTable.getId());
         assertThat(response.isOccupied()).isTrue();
+    }
+
+    @Test
+    @DisplayName("테이블이 존재하지 않는데 테이블 비우기 시도할 시 NoSuchElementException이 발생한다.")
+    void clear_fail_for_not_existing_table() {
+        assertThatThrownBy(() -> orderTableService.clear(UUID.randomUUID()))
+            .isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    @DisplayName("주문 상태가 완료가 아닌 테이블을 비우기 시도할 시 IllegalStateException이 발생한다.")
+    void clear_fail_for_order_status_is_not_completed() {
+        OrderTable request = OrderTableRequestBuilder.builder().build();
+        OrderTable orderTable = orderTableService.create(request);
+        orderTableRepository.save(orderTable);
+
+        Order order = new Order();
+        order.setOrderTable(orderTable);
+        order.setStatus(OrderStatus.WAITING);
+        orderRepository.save(order);
+
+        assertThatThrownBy(() -> orderTableService.clear(orderTable.getId()))
+            .isInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    @DisplayName("테이블을 비운다.")
+    void clear_success() {
+        OrderTable request = OrderTableRequestBuilder.builder().build();
+        OrderTable orderTable = orderTableService.create(request);
+        orderTableRepository.save(orderTable);
+
+        Order order = new Order();
+        order.setOrderTable(orderTable);
+        order.setStatus(OrderStatus.COMPLETED);
+        orderRepository.save(order);
+
+        OrderTable response = orderTableService.clear(orderTable.getId());
+        assertThat(response.getNumberOfGuests()).isEqualTo(0);
+        assertThat(response.isOccupied()).isFalse();
     }
 }
 
