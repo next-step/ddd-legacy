426802332aa567624d6e6274d768921ae7606613
gomshiki <gomshiki@gmail.com>
2024-05-18 08:59:21 +0900

    Add(test code) : ProductServiceTest

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -1,23 +1,22 @@
 package kitchenpos.application;
 
-import kitchenpos.domain.MenuRepository;
-import kitchenpos.domain.Product;
-import kitchenpos.domain.ProductRepository;
+import kitchenpos.domain.*;
 import kitchenpos.infra.PurgomalumClient;
 import org.assertj.core.api.Assertions;
 import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.TestConstructor;
+
 
 import java.math.BigDecimal;
-import java.util.UUID;
+import java.util.*;
 
 import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,22 +24,11 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.when;
 
-/*
-@TestConstructor(autowireMode = TestConstructor.AutowireMode.ALL)
-@SpringBootTest 스프링 컨텍스트 컨테이너가 올라오는게 너무 오래걸림 => MockitoExtension 사용
-*/
-@ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
 
-    @Mock
-    private ProductRepository productRepository;
-
-    @Mock
-    private MenuRepository menuRepository;
-
-    // @MockBean // 가짜 객체를 MockBean으로 덮어씌움
-    @Mock
-    private PurgomalumClient purgomalumClient;
+    private final ProductRepository productRepository = new InMemoryProductRepository();
+    private MenuRepository menuRepository = new InMemoryMenuRepository();
+    private PurgomalumClient purgomalumClient = new FakePurgomalumClient();
 
     @InjectMocks
     private ProductService productService;
@@ -50,18 +38,9 @@ class ProductServiceTest {
         productService = new ProductService(productRepository, menuRepository, purgomalumClient);
     }
 
-    /**
-     * SpringBootTest 통합테스트 문제점: 외부 API 호출을 한다면 많은 테스트로 인해 과금이 일어날 수 있음.
-     * 해결방법 : MockBean 사용
-     */
     @Test
     void 상품을_등록할_수_있다(){
         final Product product = createProductRequest("후라이드", 20_000L);
-        final var response = createProduct("후라이드", 20_000L);
-
-        given(purgomalumClient.containsProfanity(any())).willReturn(false);
-        given(productRepository.save(any())).willReturn(response);
-
         final Product actual = productService.create(product);
         assertThat(actual.getId()).isNotNull();
     }
@@ -75,7 +54,62 @@ class ProductServiceTest {
 
     }
 
-    // test fixture
+
+    @ValueSource(strings = {"욕설이 포함된 단어", "비속어"})
+    @ParameterizedTest
+    void 상품의_이름에_비속어가_포함되어_있으면_예외가_발생한다(final String name){
+        final Product request = createProductRequest(name);
+        assertThatIllegalArgumentException().isThrownBy(() -> productService.create(request));
+    }
+
+    @DisplayName("상품 가격이 변경될 때 메뉴 가격이 메뉴 상품 금액의 합보다 크면 메뉴가 숨겨진다")
+    @Test
+    void changePrice(){
+        //given
+        final UUID productId = UUID.randomUUID();
+        final Product product = createProduct(productId, "후라이드", 20_000L);
+        productRepository.save(product);
+
+        final var menuId = UUID.randomUUID();
+        final Menu menu = createMenu(menuId, 15000L, true, product);
+        menuRepository.save(menu);
+
+        final var request = createProductRequest(10000L);
+
+        //when
+        productService.changePrice(productId, request);
+
+        //then
+        final Menu actual = menuRepository.findById(menuId).get();
+        assertThat(actual.isDisplayed()).isFalse();
+    }
+
+    @Test
+    void 상품의_목록을_조회할_수_있다(){
+        productRepository.save(createProduct("후라이드",15000L));
+        productRepository.save(createProduct("양념치킨",17000L));
+        final List<Product> actual = productService.findAll();
+        assertThat(actual).hasSize(2);
+
+    }
+
+    @NotNull
+    private static Menu createMenu(UUID menuId, long price, boolean displayed, Product product) {
+        final Menu menu = new Menu();
+        menu.setId(menuId);
+        menu.setPrice(BigDecimal.valueOf(price));
+        menu.setDisplayed(displayed);
+        final MenuProduct menuProduct = new MenuProduct();
+        menuProduct.setProductId(product.getId());
+        menuProduct.setProduct(product);
+        menu.setMenuProducts(List.of(menuProduct));
+        return menu;
+    }
+
+    private static Product createProductRequest(final String name) {
+        return createProductRequest(name,20_000L);
+    }
+
     private static Product createProductRequest() {
         return createProductRequest(20_000L);
     }
@@ -86,14 +120,21 @@ class ProductServiceTest {
 
     private static Product createProductRequest(final String name, final long price) {
         final Product product = new Product();
+        product.setId(UUID.randomUUID());
         product.setName(name);
         product.setPrice(BigDecimal.valueOf(price));
         return product;
     }
 
-    private static Product createProduct(final String name, final long price) {
+    private static Product createProduct(final String name, final long price){
         final Product product = new Product();
         product.setId(UUID.randomUUID());
+        return createProduct(product.getId(), name, price);
+    }
+
+    private static Product createProduct(final UUID id, final String name, final long price) {
+        final Product product = new Product();
+        product.setId(id);
         product.setName(name);
         product.setPrice(BigDecimal.valueOf(price));
         return product;
