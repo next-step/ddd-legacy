726070b0b8b8aefff735cf06812a2d52a124863c
hvoiunq <hyojunghan1016@gmail.com>
2024-05-20 23:49:05 +0900

    test(orderService) : 주문접수 메뉴 정보 관련 테스트 코드 추가

--- src/test/java/kitchenpos/application/OrderServiceTest.java
+++ src/test/java/kitchenpos/application/OrderServiceTest.java
@@ -1,12 +1,15 @@
 package kitchenpos.application;
 
 import kitchenpos.domain.*;
+import kitchenpos.fixture.OrderFixture;
 import kitchenpos.infra.KitchenridersClient;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
@@ -14,10 +17,12 @@ import java.util.List;
 import java.util.Optional;
 
 import static kitchenpos.MoneyConstants.만원;
+import static kitchenpos.MoneyConstants.이만원;
 import static kitchenpos.fixture.MenuFixture.createMenu;
 import static kitchenpos.fixture.OrderFixture.createOrder;
 import static kitchenpos.fixture.OrderTableFixture.createSittingTable;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 
@@ -43,6 +48,91 @@ public class OrderServiceTest {
     @DisplayName("주문 접수")
     class Waiting {
 
+        @Test
+        @DisplayName("주문이 접수완료된 경우 주문상태가 대기중(WAITING)이 된다.")
+        void success1() {
+            final var menu = createMenu("후라이드", 만원);
+            final var order = createOrder(OrderType.TAKEOUT, menu);
+
+            given(menuRepository.findAllByIdIn(any())).willReturn(List.of(menu));
+            given(menuRepository.findById(any())).willReturn(Optional.of(menu));
+            given(orderRepository.save(any())).willReturn(order);
+
+            Order actual = orderService.create(order);
+
+            assertEquals(OrderStatus.WAITING, actual.getStatus());
+        }
+
+        @Test
+        @DisplayName("하나의 주문에 여러개 메뉴 주문이 가능하다.")
+        void success2() {
+            final var menu1 = createMenu("후라이드", 만원);
+            final var menu2 = createMenu("양념치킨", 만원);
+            final var order = createOrder(OrderType.TAKEOUT, menu1, menu2);
+            final var response = createOrder(OrderType.TAKEOUT, menu1, menu2);
+
+            given(menuRepository.findAllByIdIn(any())).willReturn(List.of(menu1, menu2));
+            given(menuRepository.findById(any())).willReturn(Optional.of(menu1));
+            given(menuRepository.findById(any())).willReturn(Optional.of(menu2));
+            given(orderRepository.save(any())).willReturn(response);
+
+            Order actual = orderService.create(order);
+
+            assertEquals(2, actual.getOrderLineItems().size());
+        }
+
+        @Test
+        @DisplayName("주문타입 정보가 없는 경우 주문을 받을 수 없다.")
+        void typeFail() {
+            final var order = createOrder(null, createMenu("치킨", 만원));
+
+            assertThrows(IllegalArgumentException.class, () -> orderService.create(order));
+        }
+
+        @ParameterizedTest
+        @NullAndEmptySource
+        @DisplayName("메뉴를 하나도 선택하지 않은 경우 주문할 수 없다.")
+        void fail1(List<OrderLineItem> input) {
+            final var order = createOrder(input);
+
+            assertThrows(IllegalArgumentException.class, () -> orderService.create(order));
+        }
+
+        @Test
+        @DisplayName("주문한 메뉴들 중에 하나라도 등록되어 있지 않은 메뉴가 포함되어 있으면 주문할 수 없다.")
+        void fail2() {
+            final var menu = createMenu("치킨", 만원);
+            final var order = createOrder(OrderType.TAKEOUT, menu);
+
+            assertThrows(IllegalArgumentException.class, () -> orderService.create(order));
+        }
+
+        @Test
+        @DisplayName("미노출된 메뉴가 포함되어있는 경우 주문할 수 없다.")
+        void fail3() {
+            final var menu = createMenu("치킨", 만원);
+            menu.setDisplayed(false);
+            final var order = createOrder(OrderType.TAKEOUT, menu);
+
+            given(menuRepository.findAllByIdIn(any())).willReturn(List.of(menu));
+            given(menuRepository.findById(any())).willReturn(Optional.of(menu));
+
+            assertThrows(IllegalStateException.class, () -> orderService.create(order));
+        }
+
+        @Test
+        @DisplayName("주문한 메뉴의 금액과 등록된 메뉴 금액이 다른 경우 주문할 수 없다.")
+        void fail4() {
+            final var savedMenu = createMenu("치킨", 만원);
+            final var orderMenu = createMenu("치킨", 이만원);
+            final var order = createOrder(OrderType.TAKEOUT, orderMenu);
+
+            given(menuRepository.findAllByIdIn(any())).willReturn(List.of(savedMenu));
+            given(menuRepository.findById(any())).willReturn(Optional.of(savedMenu));
+
+            assertThrows(IllegalArgumentException.class, () -> orderService.create(order));
+        }
+
         @Nested
         @DisplayName("매장 주문")
         class EatIn {
@@ -52,9 +142,10 @@ public class OrderServiceTest {
             void success() {
                 final var menu = createMenu("치킨", 만원);
                 final var orderTable = createSittingTable(2);
-                final var order = createOrder(OrderType.EAT_IN, menu, orderTable);
+                final var order = OrderFixture.createEatInOrder(OrderType.EAT_IN, orderTable, menu);
 
-                final var response = createOrder(OrderType.EAT_IN, menu, orderTable);
+                final var response = OrderFixture.createEatInOrder(OrderType.EAT_IN, orderTable, menu);
+                response.setStatus(OrderStatus.WAITING);
 
                 given(menuRepository.findAllByIdIn(any())).willReturn(List.of(menu));
                 given(menuRepository.findById(any())).willReturn(Optional.of(menu));
@@ -63,7 +154,10 @@ public class OrderServiceTest {
 
                 Order actual = orderService.create(order);
 
-                assertThat(actual).isNotNull();
+                assertAll(
+                        () -> assertThat(actual).isNotNull(),
+                        () -> assertEquals(OrderStatus.WAITING, actual.getStatus())
+                );
             }
 
             @Test
--- src/test/java/kitchenpos/fixture/OrderFixture.java
+++ src/test/java/kitchenpos/fixture/OrderFixture.java
@@ -4,21 +4,62 @@ import kitchenpos.domain.*;
 import org.jetbrains.annotations.NotNull;
 
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public class OrderFixture {
 
-    public static Order createOrder(OrderType orderType, Menu menu, OrderTable orderTable) {
+
+    public static @NotNull Order createOrder(List<OrderLineItem> input) {
+        return createOrder(OrderType.TAKEOUT, null, null, input);
+    }
+
+    public static @NotNull Order createOrder(OrderType orderType, Menu... menu) {
+        List<OrderLineItem> orderLineItems = new ArrayList<>();
+        for (Menu m : menu) {
+            OrderLineItem orderLineItem = createOrderLineItem(m);
+            orderLineItems.add(orderLineItem);
+        }
+        return createOrder(orderType, null, null, orderLineItems);
+    }
+
+    public static @NotNull Order createEatInOrder(OrderType orderType, OrderTable orderTable, Menu... menu) {
+        List<OrderLineItem> orderLineItems = new ArrayList<>();
+        for (Menu m : menu) {
+            OrderLineItem orderLineItem = createOrderLineItem(m);
+            orderLineItems.add(orderLineItem);
+        }
+        return createOrder(orderType, orderTable, null, orderLineItems);
+    }
+
+    public static @NotNull Order createDeliveryOrder(OrderType orderType, String deliveryAddress, Menu... menu) {
+        List<OrderLineItem> orderLineItems = new ArrayList<>();
+        for (Menu m : menu) {
+            OrderLineItem orderLineItem = createOrderLineItem(m);
+            orderLineItems.add(orderLineItem);
+        }
+        return createOrder(orderType, null, deliveryAddress, orderLineItems);
+    }
+
+    public static @NotNull Order createOrder(OrderType orderType, OrderTable orderTable, String deliveryAddress, List<OrderLineItem> orderLineItems) {
         final var order = new Order();
         order.setId(UUID.randomUUID());
         order.setType(orderType);
-        final var orderLineItem = createOrderLineItem(menu);
-        order.setOrderLineItems(List.of(orderLineItem));
-        order.setOrderTableId(orderTable.getId());
-        order.setOrderTable(orderTable);
+        order.setStatus(OrderStatus.WAITING);
+        order.setOrderDateTime(LocalDateTime.now());
+        if (orderType == OrderType.DELIVERY) {
+            order.setDeliveryAddress(deliveryAddress);
+        }
+        if (orderType == OrderType.EAT_IN) {
+            order.setOrderTableId(orderTable.getId());
+            order.setOrderTable(orderTable);
+        }
+        order.setOrderLineItems(orderLineItems);
         return order;
     }
+
 
     private static @NotNull OrderLineItem createOrderLineItem(Menu menu) {
         final var orderLineItem = new OrderLineItem();
