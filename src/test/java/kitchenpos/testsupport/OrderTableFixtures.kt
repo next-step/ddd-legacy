6758d850d16617453597a7de7fdba7d3f2e560ae
jinan159 <wlsdhks0423@naver.com>
2024-05-23 00:46:27 +0900

    test: 주문 테이블 손님 수 변경 테스트 코드 추가

--- src/test/java/kitchenpos/testsupport/OrderTableFixtures.kt
+++ src/test/java/kitchenpos/testsupport/OrderTableFixtures.kt
@@ -4,12 +4,14 @@ import java.util.UUID
 import kitchenpos.domain.OrderTable
 
 object OrderTableFixtures {
-    fun createOrderTable(): OrderTable {
+    fun createOrderTable(
+        isOccupied: Boolean = false
+    ): OrderTable {
         return OrderTable().apply {
             this.id = UUID.randomUUID()
             this.name = "test-order-table-name"
             this.numberOfGuests = 0
-            this.isOccupied = false
+            this.isOccupied = isOccupied
         }
     }
 }
