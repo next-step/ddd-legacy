442b94568eece9a581c380243441c586e33f35d7
jinan159 <wlsdhks0423@naver.com>
2024-05-24 00:47:28 +0900

    test: 주문 배달 시작 처리 테스트 코드 추가

---
+++ src/test/java/kitchenpos/application/OrderServiceStartDeliveryTest.kt
@@ -0,0 +1,96 @@
+package kitchenpos.application
+
+import io.kotest.assertions.throwables.shouldThrowAny
+import io.kotest.core.spec.style.ShouldSpec
+import io.kotest.matchers.shouldBe
+import io.kotest.matchers.types.shouldBeTypeOf
+import kitchenpos.domain.Order
+import kitchenpos.domain.OrderRepository
+import kitchenpos.domain.OrderStatus.DELIVERING
+import kitchenpos.domain.OrderStatus.SERVED
+import kitchenpos.domain.OrderStatus.WAITING
+import kitchenpos.domain.OrderType.DELIVERY
+import kitchenpos.domain.OrderType.TAKEOUT
+import kitchenpos.infra.KitchenridersClient
+import kitchenpos.testsupport.FakeMenuRepository
+import kitchenpos.testsupport.FakeOrderRepository
+import kitchenpos.testsupport.FakeOrderTableRepository
+import kitchenpos.testsupport.OrderFixtures
+
+class OrderServiceStartDeliveryTest : ShouldSpec({
+    lateinit var savedOrder: Order
+    lateinit var orderRepository: OrderRepository
+    lateinit var service: OrderService
+
+    beforeTest {
+        orderRepository = FakeOrderRepository()
+
+        service = OrderService(
+            orderRepository,
+            FakeMenuRepository(),
+            FakeOrderTableRepository(),
+            KitchenridersClient()
+        )
+
+        savedOrder = orderRepository.save(
+            OrderFixtures.createOrder(
+                type = DELIVERY,
+                status = SERVED,
+                menus = listOf()
+            )
+        )
+    }
+
+    context("주문 시작") {
+        should("성공") {
+            // given
+            val orderId = savedOrder.id
+
+            // when
+            val order = service.startDelivery(orderId)
+
+            // then
+            order.status shouldBe DELIVERING
+        }
+
+        should("실패 - 배달 주문이 아닌 경우") {
+            // given
+            val invalidOrder = orderRepository.save(
+                OrderFixtures.createOrder(
+                    type = TAKEOUT,
+                    status = SERVED,
+                    menus = listOf()
+                )
+            )
+            val orderId = invalidOrder.id
+
+            // when
+            val exception = shouldThrowAny {
+                service.startDelivery(orderId)
+            }
+
+            // then
+            exception.shouldBeTypeOf<IllegalStateException>()
+        }
+
+        should("실패 - 주문 전달 상태가 아닌 경우") {
+            // given
+            val invalidOrder = orderRepository.save(
+                OrderFixtures.createOrder(
+                    type = DELIVERY,
+                    status = WAITING,
+                    menus = listOf()
+                )
+            )
+            val orderId = invalidOrder.id
+
+            // when
+            val exception = shouldThrowAny {
+                service.startDelivery(orderId)
+            }
+
+            // then
+            exception.shouldBeTypeOf<IllegalStateException>()
+        }
+    }
+})
