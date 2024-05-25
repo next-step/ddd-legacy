0f75395ee5135317889982e127897a23a8adb1f5
wisotge <rnalstn0507@gmail.com>
2024-05-26 03:04:05 +0900

    feat: 0단계 - JUnit 5 학습

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,6 +32,9 @@ dependencies {
     runtimeOnly("com.h2database:h2")
     runtimeOnly("com.mysql:mysql-connector-j")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("io.kotest:kotest-runner-junit5:5.6.2")
+    testImplementation("io.kotest:kotest-assertions-core:5.6.2")
+    testImplementation("io.kotest:kotest-assertions-json:5.6.2")
 }
 
 tasks.withType<KotlinCompile> {
