7deac6c8e62f14aaff3bac494c37c20d778ca5d0
Jivebread <spark@troy.edu>
2024-05-26 18:37:28 +0900

    test(ProductFixture): ProductFixture 생성 - Product를 이름과, 가격이 주어졌을 때 생성

---
+++ src/test/java/kitchenpos/fixture/ProductFixture.java
@@ -0,0 +1,28 @@
+package kitchenpos.fixture;
+
+import kitchenpos.application.product.FakeUuidBuilder;
+import kitchenpos.common.UuidBuilder;
+import kitchenpos.domain.Product;
+
+import java.math.BigDecimal;
+
+public class ProductFixture {
+
+  public static Product createProduct(final String name, final long price) {
+    final UuidBuilder fakeUuidBuilder = new FakeUuidBuilder();
+    final Product product = new Product(fakeUuidBuilder);
+
+    product.setPrice(BigDecimal.valueOf(price));
+    product.setName(name);
+
+    return product;
+  }
+
+  public static Product createProduct(final long price) {
+    return createProduct("한강라면", price);
+  }
+
+  public static Product createProduct(final String name) {
+    return createProduct(name, 20_000L);
+  }
+}
