760a29787a343d64484183d6d26bb5f085c8beac
Jivebread <spark@troy.edu>
2024-05-26 20:55:25 +0900

    test(UuidBuilder): Product에서 uuid(식별자)를 set하는 메서드를 제거하고 테스트를 쉽게 하기 위한 DI - UuidBuilder 인터페이스 생성 - User에서 인터페이스 받아서 id 생성 - test에서 id 확인시에 해당 인터페이스로 확인.

--- src/test/java/kitchenpos/application/product/FakeUuidBuilder.java
+++ src/test/java/kitchenpos/application/product/FakeUuidBuilder.java
@@ -5,8 +5,7 @@ import kitchenpos.common.UuidBuilder;
 import java.util.UUID;
 
 public class FakeUuidBuilder implements UuidBuilder {
-  public static final String SIMON = "SIMON";
-
+  public static final String SIMON = "52588691-d763-45fe-8de6-8a632e08384a";
   @Override
   public UUID createRandomUUID() {
     return UUID.fromString(SIMON);
