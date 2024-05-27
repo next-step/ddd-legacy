ed7746722a81891315d311cdec635420c0370e1f
JhyunJung <jhyun.rigel@gmail.com>
2024-05-28 06:09:45 +0900

    리뷰 반영

--- src/main/java/calculator/Operand.java
+++ src/main/java/calculator/Operand.java
@@ -1,2 +1,33 @@
-package calculator;public class Operand {
+package calculator;
+
+public class Operand {
+    private final Long value;
+    private static final String NUMBER_MATCH_REGEX = "-?\\d+(\\.\\d+)?";
+
+    public Operand(Long value){
+        validatePositiveValue(value);
+        this.value = value;
+    }
+
+    private void validatePositiveValue(Long value) {
+        if(value < 0){
+            throw new IllegalArgumentException("Negative number found: " + value);
+        }
+    }
+
+    public Operand(String value){
+        validateStringValue(value);
+        validatePositiveValue(Long.parseLong(value));
+        this.value = Long.parseLong(value);
+    }
+
+    private void validateStringValue(String str){
+        if(str==null || str.isEmpty() || !str.matches(NUMBER_MATCH_REGEX)){
+            throw new IllegalArgumentException("Invalid number found: " + str);
+        }
+    }
+
+    public Long getValue(){
+        return value;
+    }
 }
--- src/test/java/calculator/OperandTest.java
+++ src/test/java/calculator/OperandTest.java
@@ -1,2 +1,28 @@
-package calculator;public class OperandTest {
+package calculator;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class OperandTest {
+
+    @ParameterizedTest
+    @DisplayName("음수로 생성 할 수는 없다.")
+    @ValueSource(strings = {"-1", "-10"})
+    public void 음수_예외_처리(Long value) throws Exception {
+        assertThatThrownBy(() -> new Operand(value)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @ParameterizedTest
+    @DisplayName("양수 이외로는 생성 할 수는 없다.")
+    @NullSource
+    @ValueSource(strings = {"?", " ", "!"})
+    public void 기호_예외_처리(String value) throws Exception {
+        assertThatThrownBy(() -> new Operand(value)).isInstanceOf(IllegalArgumentException.class);
+    }
+
+
 }
