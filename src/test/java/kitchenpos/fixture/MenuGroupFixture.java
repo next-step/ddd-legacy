3cf11dacfe4c13197b6fc53cbafa633c3c76c5c1
Jivebread <spark@troy.edu>
2024-05-28 23:01:35 +0900

    test(MenuGroupServiceTest): 메뉴 서비스테스트 MenuGroupServiceTest를 요구사항에 대해서 진행

--- src/test/java/kitchenpos/fixture/MenuGroupFixture.java
+++ src/test/java/kitchenpos/fixture/MenuGroupFixture.java
@@ -2,9 +2,12 @@ package kitchenpos.fixture;
 
 import kitchenpos.domain.MenuGroup;
 
+import java.util.UUID;
+
 public class MenuGroupFixture {
   public static MenuGroup createMenuGroup(final String name) {
     MenuGroup menuGroup = new MenuGroup();
+    menuGroup.setId(UUID.randomUUID());
     menuGroup.setName(name);
     return menuGroup;
   }
