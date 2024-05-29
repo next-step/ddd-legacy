ff865343c3c19b04160bca901a135b628909fc8b
OhJeUng <bioman3238@gmail.com>
2024-05-30 01:28:35 +0900

    test(kitchenpos) : 상품 메뉴 테스트

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -1,10 +1,18 @@
 package kitchenpos.application;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.*;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import kitchenpos.domain.Menu;
 import kitchenpos.domain.MenuRepository;
 import kitchenpos.domain.Product;
 import kitchenpos.domain.ProductRepository;
-import kitchenpos.fixtures.ProductFixture;
-import kitchenpos.fixtures.ProductsFixture;
+import kitchenpos.fixtures.Fixture;
 import kitchenpos.infra.PurgomalumClient;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -14,15 +22,6 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.math.BigDecimal;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
-import java.util.UUID;
-
-import static org.mockito.BDDMockito.*;
-import static org.mockito.ArgumentMatchers.any;
 
 @ExtendWith(MockitoExtension.class)
 class ProductServiceTest {
@@ -35,74 +34,74 @@ class ProductServiceTest {
   @Test
   @DisplayName("상품을 등록하기 위해 이름과 금액을 입력해야 한다.")
   void test_case_1() {
-    final ProductFixture createProduct = new ProductFixture("김치찌개", BigDecimal.valueOf(20000L));
-    Mockito.when(productRepository.save(any())).thenReturn(createProduct.getProduct());
+    final Product expected = Fixture.fixtureProduct();
+    given(productRepository.save(any())).willReturn(Fixture.fixtureProduct());
 
-    Product product = productService.create(createProduct.getProduct());
+    final Product product = productService.create(Fixture.fixtureProduct());
 
-    Assertions.assertThat("김치찌개").isEqualTo(product.getName());
-    Assertions.assertThat(BigDecimal.valueOf(20000L)).isEqualTo(product.getPrice());
+    Assertions.assertThat(product.getName()).isEqualTo(expected.getName());
+    Assertions.assertThat(product.getPrice()).isEqualTo(expected.getPrice());
   }
 
   @Test
   @DisplayName("상품을 등록하기 위해 이름에 비속어 또는 욕설을 작성할 수 없다.")
   void test_case_2() {
-    Mockito.when(purgomalumClient.containsProfanity("김치찌개")).thenReturn(true);
-    final ProductFixture createProduct = new ProductFixture("김치찌개", BigDecimal.valueOf(20000L));
+    final Product product = Fixture.fixtureProduct();
+
+    Mockito.when(purgomalumClient.containsProfanity(product.getName())).thenReturn(true);
 
     Assertions.assertThatIllegalArgumentException()
         .isThrownBy(
             () -> {
-              productService.create(createProduct.getProduct());
+              productService.create(product);
             });
   }
 
   @Test
   @DisplayName("상품의 금액을 수정")
   void test_case_3() {
-    final ProductFixture existProduct = new ProductFixture("김치찌개", BigDecimal.valueOf(20000L));
-    Mockito.when(productRepository.findById(any()))
-        .thenReturn(Optional.of(existProduct.getProduct()));
-    Mockito.when(menuRepository.findAllByProductId(any())).thenReturn(Collections.emptyList());
+    final Product expected = Fixture.fixtureProduct();
+    given(productRepository.findById(any())).willReturn(Optional.of(expected));
+    given(menuRepository.findAllByProductId(any())).willReturn(List.of(Fixture.fixtureMenu()));
 
-    existProduct.getProduct().setPrice(BigDecimal.valueOf(10000L));
-    Product change = productService.changePrice(UUID.randomUUID(), existProduct.getProduct());
+    expected.setPrice(BigDecimal.valueOf(10000L));
+    final Product actual = productService.changePrice(UUID.randomUUID(), expected);
 
-    Assertions.assertThat(change.getPrice()).isEqualTo(change.getPrice());
+    Assertions.assertThat(actual.getPrice()).isEqualTo(expected.getPrice());
   }
 
   @Test
   @DisplayName("상품의 금액을 수정하기 위해 상품이 존재해야 하고, 금액을 입력하지 않았거나 또는 0원 밑으로 입력할 수 없다.")
   void test_case_4() {
-    final ProductFixture existProduct = new ProductFixture("김치찌개", BigDecimal.valueOf(20000L));
+    final Product expected = Fixture.fixtureProduct();
 
-    existProduct.getProduct().setPrice(BigDecimal.valueOf(-1));
+    expected.setPrice(BigDecimal.valueOf(-1));
     Assertions.assertThatIllegalArgumentException()
-        .isThrownBy(
-            () -> {
-              productService.changePrice(
-                  existProduct.getProduct().getId(), existProduct.getProduct());
-            });
+        .isThrownBy(() -> productService.changePrice(expected.getId(), expected));
   }
 
   @Test
   @DisplayName("상품의 금액을 수정하면 해당 상품이 등록된 모든 메뉴에 영향이 간다.")
   void test_case_5() {
-    // TODO :: 다른거 다하고
-    final ProductFixture existProduct = new ProductFixture("김치찌개", BigDecimal.valueOf(20000L));
+    final Product expected = Fixture.fixtureProduct();
+    final List<Menu> menus = List.of(Fixture.fixtureMenu());
+    given(productRepository.findById(any())).willReturn(Optional.of(expected));
+    given(menuRepository.findAllByProductId(any())).willReturn(menus);
 
-    Assertions.assertThat(20000L).isGreaterThan(120000);
-    Assertions.assertThat(false).isFalse();
+    expected.setPrice(BigDecimal.valueOf(10L));
+    productService.changePrice(expected.getId(), expected);
+
+    Assertions.assertThat(menus.getFirst().isDisplayed()).isFalse();
   }
 
   @Test
   @DisplayName("상품 전체를 조회할 수 있다.")
   void test_case_6() {
-    given(productRepository.findAll())
-        .willReturn(
-            ProductsFixture.create().getList().stream().map(ProductFixture::getProduct).toList());
-    final List<Product> all = productService.findAll();
+    final List<Product> expected = List.of(Fixture.fixtureProduct());
+    given(productRepository.findAll()).willReturn(expected);
 
-    Assertions.assertThat(4).isEqualTo(all.size());
+    final List<Product> actual = productService.findAll();
+
+    Assertions.assertThat(actual.size()).isEqualTo(expected.size());
   }
 }
