bb3ec098434703a78c493fdc79f0dcd41f801c6e
OhJeUng <bioman3238@gmail.com>
2024-05-30 01:27:56 +0900

    test(kitchenpos) : 메뉴 그룹 테스트

--- src/test/java/kitchenpos/application/MenuGroupServiceTest.java
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -1,11 +1,8 @@
 package kitchenpos.application;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import kitchenpos.domain.MenuGroup;
 import kitchenpos.domain.MenuGroupRepository;
-import kitchenpos.fixtures.MenuGroupFixture;
-import kitchenpos.fixtures.MenuGroupsFixture;
+import kitchenpos.fixtures.Fixture;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -26,23 +23,20 @@ class MenuGroupServiceTest {
   @Test
   @DisplayName("메뉴 그룹을 등록하기 위해 메뉴 그룹 이름 입력해야 한다.")
   void case1() {
-    final MenuGroupFixture menuGroup = new MenuGroupFixture("기사식당 메뉴");
-    given(menuGroupRepository.save(any())).willReturn(menuGroup.getMenuGroup());
+    final MenuGroup menuGroup = Fixture.fixtureMenuGroup();
+    given(menuGroupRepository.save(any())).willReturn(menuGroup);
 
-    final MenuGroup resultMenuGroup = menuGroupService.create(menuGroup.getMenuGroup());
-    Assertions.assertThat(menuGroup.getMenuGroup().getName()).isEqualTo(resultMenuGroup.getName());
+    final MenuGroup resultMenuGroup = menuGroupService.create(menuGroup);
+    Assertions.assertThat(menuGroup.getName()).isEqualTo(resultMenuGroup.getName());
   }
 
   @Test
   @DisplayName("메뉴 그룹을 전체 조회할 수 있다.")
   void case2() {
     given(menuGroupRepository.findAll())
-        .willReturn(
-            MenuGroupsFixture.create().getList().stream()
-                .map(MenuGroupFixture::getMenuGroup)
-                .toList());
+        .willReturn(List.of(Fixture.fixtureMenuGroup()));
 
     final List<MenuGroup> all = menuGroupService.findAll();
-    Assertions.assertThat(4).isEqualTo(all.size());
+    Assertions.assertThat(1).isEqualTo(all.size());
   }
 }
