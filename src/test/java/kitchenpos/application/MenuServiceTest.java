978813b5981c1d983c5240216aaf301c7e5cee5b
OhJeUng <bioman3238@gmail.com>
2024-06-01 20:16:42 +0900

    test(kitchenpos) : nested를 통해 테스트 영역 그룹짓기

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -12,6 +12,7 @@ import kitchenpos.fixtures.*;
 import kitchenpos.infra.PurgomalumClient;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -30,222 +31,242 @@ class MenuServiceTest {
 
   @InjectMocks private MenuService menuService;
 
-  @Test
-  @DisplayName("메뉴를 등록하기 위해 메뉴 그룹, 이름을 입력해야 한다.")
-  void case1() {
-    final Menu menu = FixtureMenu.fixtureMenu();
-    final Product product = FixtureProduct.fixtureProduct();
-    final MenuGroup menuGroup = FixtureMenu.fixtureMenuGroup();
 
-    menu.setName("닭다리");
+  @Nested
+  @DisplayName("메뉴 등록")
+  class Nested1 {
 
-    given(menuGroupRepository.findById(any())).willReturn(Optional.of(menuGroup));
-    given(productRepository.findAllByIdIn(any())).willReturn(List.of(product));
-    given(productRepository.findById(any())).willReturn(Optional.of(product));
-    given(purgomalumClient.containsProfanity(any())).willReturn(false);
-    given(menuRepository.save(any())).willReturn(menu);
+    @Test
+    @DisplayName("메뉴를 등록하기 위해 메뉴 그룹, 이름을 입력해야 한다.")
+    void case1() {
+      final Menu menu = FixtureMenu.fixtureMenu();
+      final Product product = FixtureProduct.fixtureProduct();
+      final MenuGroup menuGroup = FixtureMenu.fixtureMenuGroup();
 
-    menuService.create(menu);
-    Assertions.assertThat(menu.getName()).isEqualTo("닭다리");
+      menu.setName("닭다리");
+
+      given(menuGroupRepository.findById(any())).willReturn(Optional.of(menuGroup));
+      given(productRepository.findAllByIdIn(any())).willReturn(List.of(product));
+      given(productRepository.findById(any())).willReturn(Optional.of(product));
+      given(purgomalumClient.containsProfanity(any())).willReturn(false);
+      given(menuRepository.save(any())).willReturn(menu);
+
+      menuService.create(menu);
+      Assertions.assertThat(menu.getName()).isEqualTo("닭다리");
+    }
+
+    @Test
+    @DisplayName("메뉴를 등록하기 위해 메뉴 이름에 비속어 또는 욕설을 작성할 수 없다.")
+    void case2() {
+
+      final Menu menu = FixtureProduct.fixtureMenu();
+      final Product product = FixtureProduct.fixtureProduct();
+      final MenuGroup menuGroup = FixtureMenu.fixtureMenuGroup();
+
+      given(menuGroupRepository.findById(any())).willReturn(Optional.of(menuGroup));
+      given(productRepository.findAllByIdIn(any())).willReturn(List.of(product));
+
+      given(productRepository.findById(any())).willReturn(Optional.of(product));
+      given(purgomalumClient.containsProfanity(any())).willReturn(true);
+
+      Assertions.assertThatIllegalArgumentException()
+              .isThrownBy(() -> menuService.create(menu));
+    }
+
+    @Test
+    @DisplayName("메뉴를 등록하기 위해 금액을 입력하지 않았다.")
+    void case3() {
+      final Menu menu = FixtureMenu.fixtureMenu();
+
+      menu.setName("닭다리");
+      menu.setPrice(null);
+
+      Assertions.assertThatIllegalArgumentException()
+              .isThrownBy(() -> menuService.create(menu));
+    }
+
+    @Test
+    @DisplayName("메뉴를 등록하기 위해 금액은 음수로 입력할 수 없다.")
+    void case4() {
+      final Menu menu = FixtureMenu.fixtureMenu();
+
+      menu.setName("닭다리");
+      menu.setPrice(BigDecimal.valueOf(-1));
+
+      Assertions.assertThatIllegalArgumentException()
+              .isThrownBy(() -> menuService.create(menu));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-1})
+    @DisplayName("메뉴를 등록하기 위해 상품이 있어야하고, 수량을 0개 밑으로 입력할 수 없다.")
+    void case5(Integer value) {
+      final Menu menu = FixtureMenu.fixtureMenu();
+      final Product product = FixtureProduct.fixtureProduct();
+      final MenuGroup menuGroup = FixtureMenu.fixtureMenuGroup();
+      final MenuProduct menuProduct = FixtureProduct.fixtureMenuProduct();
+
+      menuProduct.setQuantity(value);
+      menu.setMenuProducts(List.of(menuProduct));
+
+      given(menuGroupRepository.findById(any())).willReturn(Optional.of(menuGroup));
+      given(productRepository.findAllByIdIn(any())).willReturn(List.of(product));
+
+      Assertions.assertThatIllegalArgumentException()
+              .isThrownBy(() -> menuService.create(menu));
+    }
+
+    @Test
+    @DisplayName("메뉴를 등록하기 위해 해당 상품 정보가 상품에 존재해야 한다.")
+    void case6() {
+      final Menu menu = FixtureMenu.fixtureMenu();
+      final Product product = FixtureProduct.fixtureProduct();
+      final MenuGroup menuGroup = FixtureMenu.fixtureMenuGroup();
+
+      given(menuGroupRepository.findById(any())).willReturn(Optional.of(menuGroup));
+      given(productRepository.findAllByIdIn(any())).willReturn(List.of(product));
+
+      final Product failProduct = FixtureProduct.fixtureProduct();
+      failProduct.setId(UUID.randomUUID());
+      given(productRepository.findById(any())).willReturn(null);
+
+      Assertions.assertThatException()
+              .isThrownBy(() -> menuService.create(menu));
+    }
+
+    @Test
+    @DisplayName("메뉴 금액이 상품 금액을 수량 만큼 합산한 금액 보다 클 수 없다.")
+    void case7() {
+      final Menu menu = FixtureMenu.fixtureMenu();
+      final Product product = FixtureProduct.fixtureProduct();
+      final MenuGroup menuGroup = FixtureMenu.fixtureMenuGroup();
+
+      menu.setPrice(BigDecimal.valueOf(30_000_000L));
+
+      given(menuGroupRepository.findById(any())).willReturn(Optional.of(menuGroup));
+      given(productRepository.findAllByIdIn(any())).willReturn(List.of(product));
+
+      given(productRepository.findById(any())).willReturn(Optional.of(product));
+
+      Assertions.assertThatIllegalArgumentException()
+              .isThrownBy(() -> menuService.create(menu));
+    }
+
+    @Test
+    @DisplayName("메뉴를 등록하면 해당 상품이 공개된다.")
+    void case8() {
+      final Menu menu = FixtureMenu.fixtureMenu();
+      final Product product = FixtureProduct.fixtureProduct();
+      final MenuGroup menuGroup = FixtureMenu.fixtureMenuGroup();
+
+      given(menuGroupRepository.findById(any())).willReturn(Optional.of(menuGroup));
+      given(productRepository.findAllByIdIn(any())).willReturn(List.of(product));
+      given(productRepository.findById(any())).willReturn(Optional.of(product));
+      given(purgomalumClient.containsProfanity(any())).willReturn(false);
+      given(menuRepository.save(any())).willReturn(menu);
+
+      final Menu actual = menuService.create(menu);
+      Assertions.assertThat(actual.isDisplayed()).isTrue();
+    }
   }
 
-  @Test
-  @DisplayName("메뉴를 등록하기 위해 메뉴 이름에 비속어 또는 욕설을 작성할 수 없다.")
-  void case2() {
+  @Nested
+  @DisplayName("메뉴 금액 변경")
+  class Nested2 {
+    @Test
+    @DisplayName("메뉴의 금액을 변경하기 위해 해당 메뉴가 존재해야 하고, 변경할 금액을 입력해야 한다.")
+    void case9() {
+      final Menu menu = FixtureMenu.fixtureMenu();
+      final Menu changeMenuPrice = FixtureMenu.fixtureMenu();
 
-    final Menu menu = FixtureProduct.fixtureMenu();
-    final Product product = FixtureProduct.fixtureProduct();
-    final MenuGroup menuGroup = FixtureMenu.fixtureMenuGroup();
+      given(menuRepository.findById(any())).willReturn(Optional.of(menu));
 
-    given(menuGroupRepository.findById(any())).willReturn(Optional.of(menuGroup));
-    given(productRepository.findAllByIdIn(any())).willReturn(List.of(product));
+      changeMenuPrice.setPrice(BigDecimal.valueOf(25_000L));
+      final Menu actual = menuService.changePrice(menu.getId(), changeMenuPrice);
+      Assertions.assertThat(actual.getPrice()).isEqualTo(menu.getPrice());
+    }
 
-    given(productRepository.findById(any())).willReturn(Optional.of(product));
-    given(purgomalumClient.containsProfanity(any())).willReturn(true);
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(longs = {-1})
+    @DisplayName("메뉴의 금액을 변경하기 위해 금액을 입력하지 않았거나 또는 0원 밑으로 입력할 수 없다.")
+    void case10(Long value) {
+      final Menu menu = FixtureMenu.fixtureMenu();
+      final Menu changeMenuPrice = FixtureMenu.fixtureMenu();
 
-    Assertions.assertThatIllegalArgumentException()
-        .isThrownBy(() -> menuService.create(menu));
+      changeMenuPrice.setPrice(value == null ? null : BigDecimal.valueOf(value));
+      Assertions.assertThatIllegalArgumentException()
+              .isThrownBy(() -> menuService.changePrice(menu.getId(), changeMenuPrice));
+    }
+
+    @Test
+    @DisplayName("메뉴의 금액을 변경하기 위해 변경할 금액이 상품 금액을 수량 만큼 합산한 금액 보다 클 수 없다.")
+    void case11() {
+      final Menu menu = FixtureMenu.fixtureMenu();
+      final Menu changeMenuPrice = FixtureMenu.fixtureMenu();
+
+      given(menuRepository.findById(any())).willReturn(Optional.of(menu));
+
+      changeMenuPrice.setPrice(BigDecimal.valueOf(30_000L));
+      Assertions.assertThatIllegalArgumentException()
+              .isThrownBy(() -> menuService.changePrice(menu.getId(), changeMenuPrice));
+    }
   }
 
-  @Test
-  @DisplayName("메뉴를 등록하기 위해 금액을 입력하지 않았다.")
-  void case3() {
-    final Menu menu = FixtureMenu.fixtureMenu();
+  @Nested
+  @DisplayName("메뉴 노출")
+  class Nested3 {
 
-    menu.setName("닭다리");
-    menu.setPrice(null);
+    @Test
+    @DisplayName("메뉴를 노출하기 위해 메뉴가 존재해야한다.")
+    void case12() {
+      final Menu menu = FixtureMenu.fixtureMenu();
 
-    Assertions.assertThatIllegalArgumentException()
-            .isThrownBy(() -> menuService.create(menu));
+      menu.setDisplayed(false);
+      given(menuRepository.findById(any())).willReturn(Optional.of(menu));
+
+      final Menu actual = menuService.display(menu.getId());
+      Assertions.assertThat(actual.isDisplayed()).isTrue();
+    }
+
+    @Test
+    @DisplayName("메뉴를 노출하기 위해 메뉴 금액이 상품 금액을 수량 만큼 합산한 금액 보다 클 수 없다.")
+    void case13() {
+      final Menu menu = FixtureMenu.fixtureMenu();
+
+      menu.setDisplayed(false);
+      menu.setPrice(BigDecimal.valueOf(30_000L));
+      given(menuRepository.findById(any())).willReturn(Optional.of(menu));
+
+      Assertions.assertThatIllegalStateException()
+              .isThrownBy(() -> menuService.display(menu.getId()));
+    }
+
+    @Test
+    @DisplayName("메뉴를 숨기기 위해 메뉴가 존재해야 한다.")
+    void case14() {
+      final Menu menu = FixtureMenu.fixtureMenu();
+
+      menu.setDisplayed(true);
+      given(menuRepository.findById(any())).willReturn(Optional.of(menu));
+
+      final Menu actual = menuService.hide(menu.getId());
+      Assertions.assertThat(actual.isDisplayed()).isFalse();
+    }
   }
 
-  @Test
-  @DisplayName("메뉴를 등록하기 위해 금액은 음수로 입력할 수 없다.")
-  void case4() {
-    final Menu menu = FixtureMenu.fixtureMenu();
+  @Nested
+  @DisplayName("메뉴 전체 조회")
+  class Nested4 {
+    @Test
+    @DisplayName("메뉴를 전체 조회할 수 있다.")
+    void case15() {
+      final Menu menu = FixtureMenu.fixtureMenu();
+      given(menuRepository.findAll()).willReturn(List.of(menu));
 
-    menu.setName("닭다리");
-    menu.setPrice(BigDecimal.valueOf(-1));
-
-    Assertions.assertThatIllegalArgumentException()
-            .isThrownBy(() -> menuService.create(menu));
+      final List<Menu> all = menuService.findAll();
+      Assertions.assertThat(all).isNotEmpty();
+    }
   }
 
-  @ParameterizedTest
-  @ValueSource(ints = {-1})
-  @DisplayName("메뉴를 등록하기 위해 상품이 있어야하고, 수량을 0개 밑으로 입력할 수 없다.")
-  void case5(Integer value) {
-    final Menu menu = FixtureMenu.fixtureMenu();
-    final Product product = FixtureProduct.fixtureProduct();
-    final MenuGroup menuGroup = FixtureMenu.fixtureMenuGroup();
-    final MenuProduct menuProduct = FixtureProduct.fixtureMenuProduct();
-
-    menuProduct.setQuantity(value);
-    menu.setMenuProducts(List.of(menuProduct));
-
-    given(menuGroupRepository.findById(any())).willReturn(Optional.of(menuGroup));
-    given(productRepository.findAllByIdIn(any())).willReturn(List.of(product));
-
-    Assertions.assertThatIllegalArgumentException()
-            .isThrownBy(() -> menuService.create(menu));
-  }
-
-  @Test
-  @DisplayName("메뉴를 등록하기 위해 해당 상품 정보가 상품에 존재해야 한다.")
-  void case6() {
-    final Menu menu = FixtureMenu.fixtureMenu();
-    final Product product = FixtureProduct.fixtureProduct();
-    final MenuGroup menuGroup = FixtureMenu.fixtureMenuGroup();
-
-    given(menuGroupRepository.findById(any())).willReturn(Optional.of(menuGroup));
-    given(productRepository.findAllByIdIn(any())).willReturn(List.of(product));
-
-    final Product failProduct = FixtureProduct.fixtureProduct();
-    failProduct.setId(UUID.randomUUID());
-    given(productRepository.findById(any())).willReturn(null);
-
-    Assertions.assertThatException()
-            .isThrownBy(() -> menuService.create(menu));
-  }
-
-  @Test
-  @DisplayName("메뉴 금액이 상품 금액을 수량 만큼 합산한 금액 보다 클 수 없다.")
-  void case7() {
-    final Menu menu = FixtureMenu.fixtureMenu();
-    final Product product = FixtureProduct.fixtureProduct();
-    final MenuGroup menuGroup = FixtureMenu.fixtureMenuGroup();
-
-    menu.setPrice(BigDecimal.valueOf(30_000_000L));
-
-    given(menuGroupRepository.findById(any())).willReturn(Optional.of(menuGroup));
-    given(productRepository.findAllByIdIn(any())).willReturn(List.of(product));
-
-    given(productRepository.findById(any())).willReturn(Optional.of(product));
-
-    Assertions.assertThatIllegalArgumentException()
-            .isThrownBy(() -> menuService.create(menu));
-  }
-
-  @Test
-  @DisplayName("메뉴를 등록하면 해당 상품이 공개된다.")
-  void case8() {
-    final Menu menu = FixtureMenu.fixtureMenu();
-    final Product product = FixtureProduct.fixtureProduct();
-    final MenuGroup menuGroup = FixtureMenu.fixtureMenuGroup();
-
-    given(menuGroupRepository.findById(any())).willReturn(Optional.of(menuGroup));
-    given(productRepository.findAllByIdIn(any())).willReturn(List.of(product));
-    given(productRepository.findById(any())).willReturn(Optional.of(product));
-    given(purgomalumClient.containsProfanity(any())).willReturn(false);
-    given(menuRepository.save(any())).willReturn(menu);
-
-    final Menu actual = menuService.create(menu);
-    Assertions.assertThat(actual.isDisplayed()).isTrue();
-  }
-
-  @Test
-  @DisplayName("메뉴의 금액을 변경하기 위해 해당 메뉴가 존재해야 하고, 변경할 금액을 입력해야 한다.")
-  void case9() {
-    final Menu menu = FixtureMenu.fixtureMenu();
-    final Menu changeMenuPrice = FixtureMenu.fixtureMenu();
-
-    given(menuRepository.findById(any())).willReturn(Optional.of(menu));
-
-    changeMenuPrice.setPrice(BigDecimal.valueOf(25_000L));
-    final Menu actual = menuService.changePrice(menu.getId(), changeMenuPrice);
-    Assertions.assertThat(actual.getPrice()).isEqualTo(menu.getPrice());
-  }
-
-  @ParameterizedTest
-  @NullSource
-  @ValueSource(longs = {-1})
-  @DisplayName("메뉴의 금액을 변경하기 위해 금액을 입력하지 않았거나 또는 0원 밑으로 입력할 수 없다.")
-  void case10(Long value) {
-    final Menu menu = FixtureMenu.fixtureMenu();
-    final Menu changeMenuPrice = FixtureMenu.fixtureMenu();
-
-    changeMenuPrice.setPrice(value == null ? null : BigDecimal.valueOf(value));
-    Assertions.assertThatIllegalArgumentException()
-                    .isThrownBy(() -> menuService.changePrice(menu.getId(), changeMenuPrice));
-  }
-
-  @Test
-  @DisplayName("메뉴의 금액을 변경하기 위해 변경할 금액이 상품 금액을 수량 만큼 합산한 금액 보다 클 수 없다.")
-  void case11() {
-    final Menu menu = FixtureMenu.fixtureMenu();
-    final Menu changeMenuPrice = FixtureMenu.fixtureMenu();
-
-    given(menuRepository.findById(any())).willReturn(Optional.of(menu));
-
-    changeMenuPrice.setPrice(BigDecimal.valueOf(30_000L));
-    Assertions.assertThatIllegalArgumentException()
-            .isThrownBy(() -> menuService.changePrice(menu.getId(), changeMenuPrice));
-  }
-
-  @Test
-  @DisplayName("메뉴를 노출하기 위해 메뉴가 존재해야한다.")
-  void case12() {
-    final Menu menu = FixtureMenu.fixtureMenu();
-
-    menu.setDisplayed(false);
-    given(menuRepository.findById(any())).willReturn(Optional.of(menu));
-
-    final Menu actual = menuService.display(menu.getId());
-    Assertions.assertThat(actual.isDisplayed()).isTrue();
-  }
-
-  @Test
-  @DisplayName("메뉴를 노출하기 위해 메뉴 금액이 상품 금액을 수량 만큼 합산한 금액 보다 클 수 없다.")
-  void case13() {
-    final Menu menu = FixtureMenu.fixtureMenu();
-
-    menu.setDisplayed(false);
-    menu.setPrice(BigDecimal.valueOf(30_000L));
-    given(menuRepository.findById(any())).willReturn(Optional.of(menu));
-
-    Assertions.assertThatIllegalStateException()
-            .isThrownBy(() -> menuService.display(menu.getId()));
-  }
-
-  @Test
-  @DisplayName("메뉴를 숨기기 위해 메뉴가 존재해야 한다.")
-  void case14() {
-    final Menu menu = FixtureMenu.fixtureMenu();
-
-    menu.setDisplayed(true);
-    given(menuRepository.findById(any())).willReturn(Optional.of(menu));
-
-    final Menu actual = menuService.hide(menu.getId());
-    Assertions.assertThat(actual.isDisplayed()).isFalse();
-  }
-
-  @Test
-  @DisplayName("메뉴를 전체 조회할 수 있다.")
-  void case15() {
-    final Menu menu = FixtureMenu.fixtureMenu();
-    given(menuRepository.findAll()).willReturn(List.of(menu));
-
-    final List<Menu> all = menuService.findAll();
-    Assertions.assertThat(all).isNotEmpty();
-  }
 }
--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -18,6 +18,7 @@ import kitchenpos.fixtures.FixtureProduct;
 import kitchenpos.infra.PurgomalumClient;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -33,78 +34,93 @@ class ProductServiceTest {
 
   @InjectMocks private ProductService productService;
 
-  @Test
-  @DisplayName("상품을 등록하기 위해 이름과 금액을 입력해야 한다.")
-  void test_case_1() {
-    final Product expected = FixtureProduct.fixtureProduct();
-    given(productRepository.save(any())).willReturn(FixtureProduct.fixtureProduct());
+  @Nested
+  @DisplayName("상품 등록")
+  class Nested1 {
+    @Test
+    @DisplayName("상품을 등록하기 위해 이름과 금액을 입력해야 한다.")
+    void test_case_1() {
+      final Product expected = FixtureProduct.fixtureProduct();
+      given(productRepository.save(any())).willReturn(FixtureProduct.fixtureProduct());
 
-    final Product product = productService.create(FixtureProduct.fixtureProduct());
+      final Product product = productService.create(FixtureProduct.fixtureProduct());
 
-    Assertions.assertThat(product.getName()).isEqualTo(expected.getName());
-    Assertions.assertThat(product.getPrice()).isEqualTo(expected.getPrice());
+      Assertions.assertThat(product.getName()).isEqualTo(expected.getName());
+      Assertions.assertThat(product.getPrice()).isEqualTo(expected.getPrice());
+    }
+
+    @Test
+    @DisplayName("상품을 등록하기 위해 이름에 비속어 또는 욕설을 작성할 수 없다.")
+    void test_case_2() {
+      final Product product = FixtureProduct.fixtureProduct();
+
+      Mockito.when(purgomalumClient.containsProfanity(product.getName())).thenReturn(true);
+
+      Assertions.assertThatIllegalArgumentException()
+              .isThrownBy(
+                      () -> {
+                        productService.create(product);
+                      });
+    }
+
   }
 
-  @Test
-  @DisplayName("상품을 등록하기 위해 이름에 비속어 또는 욕설을 작성할 수 없다.")
-  void test_case_2() {
-    final Product product = FixtureProduct.fixtureProduct();
+  @Nested
+  @DisplayName("상품 금액 수정")
+  class Nested2 {
+    @Test
+    @DisplayName("상품의 금액을 수정")
+    void test_case_3() {
+      final Product expected = FixtureProduct.fixtureProduct();
+      given(productRepository.findById(any())).willReturn(Optional.of(expected));
+      given(menuRepository.findAllByProductId(any())).willReturn(List.of(FixtureMenu.fixtureMenu()));
 
-    Mockito.when(purgomalumClient.containsProfanity(product.getName())).thenReturn(true);
+      expected.setPrice(BigDecimal.valueOf(10000L));
+      final Product actual = productService.changePrice(UUID.randomUUID(), expected);
 
-    Assertions.assertThatIllegalArgumentException()
-        .isThrownBy(
-            () -> {
-              productService.create(product);
-            });
+      Assertions.assertThat(actual.getPrice()).isEqualTo(expected.getPrice());
+    }
+
+    @Test
+    @DisplayName("상품의 금액을 수정하기 위해 상품이 존재해야 하고, 금액을 입력하지 않았거나 또는 0원 밑으로 입력할 수 없다.")
+    void test_case_4() {
+      final Product expected = FixtureProduct.fixtureProduct();
+
+      expected.setPrice(BigDecimal.valueOf(-1));
+      Assertions.assertThatIllegalArgumentException()
+              .isThrownBy(() -> productService.changePrice(expected.getId(), expected));
+    }
+
+    @Test
+    @DisplayName("상품의 금액을 수정하면 해당 상품이 등록된 모든 메뉴에 영향이 간다.")
+    void test_case_5() {
+      final List<Menu> menus = List.of(FixtureMenu.fixtureMenu());
+      final Product expected = menus.getFirst().getMenuProducts().getFirst().getProduct();
+      final Product request = FixtureProduct.fixtureProduct();
+      given(productRepository.findById(any())).willReturn(Optional.of(expected));
+      given(menuRepository.findAllByProductId(any())).willReturn(menus);
+
+      request.setPrice(BigDecimal.valueOf(10L));
+      productService.changePrice(expected.getId(), request);
+
+      Assertions.assertThat(menus.getFirst().isDisplayed()).isFalse();
+    }
   }
 
-  @Test
-  @DisplayName("상품의 금액을 수정")
-  void test_case_3() {
-    final Product expected = FixtureProduct.fixtureProduct();
-    given(productRepository.findById(any())).willReturn(Optional.of(expected));
-    given(menuRepository.findAllByProductId(any())).willReturn(List.of(FixtureMenu.fixtureMenu()));
 
-    expected.setPrice(BigDecimal.valueOf(10000L));
-    final Product actual = productService.changePrice(UUID.randomUUID(), expected);
+  @Nested
+  @DisplayName("상품 전체 조회")
+  class Nested3 {
+    @Test
+    @DisplayName("상품 전체를 조회할 수 있다.")
+    void test_case_6() {
+      final List<Product> expected = List.of(FixtureProduct.fixtureProduct());
+      given(productRepository.findAll()).willReturn(expected);
 
-    Assertions.assertThat(actual.getPrice()).isEqualTo(expected.getPrice());
-  }
+      final List<Product> actual = productService.findAll();
 
-  @Test
-  @DisplayName("상품의 금액을 수정하기 위해 상품이 존재해야 하고, 금액을 입력하지 않았거나 또는 0원 밑으로 입력할 수 없다.")
-  void test_case_4() {
-    final Product expected = FixtureProduct.fixtureProduct();
+      Assertions.assertThat(actual.size()).isEqualTo(expected.size());
+    }
 
-    expected.setPrice(BigDecimal.valueOf(-1));
-    Assertions.assertThatIllegalArgumentException()
-        .isThrownBy(() -> productService.changePrice(expected.getId(), expected));
-  }
-
-  @Test
-  @DisplayName("상품의 금액을 수정하면 해당 상품이 등록된 모든 메뉴에 영향이 간다.")
-  void test_case_5() {
-    final List<Menu> menus = List.of(FixtureMenu.fixtureMenu());
-    final Product expected = menus.getFirst().getMenuProducts().getFirst().getProduct();
-    final Product request = FixtureProduct.fixtureProduct();
-    given(productRepository.findById(any())).willReturn(Optional.of(expected));
-    given(menuRepository.findAllByProductId(any())).willReturn(menus);
-
-    request.setPrice(BigDecimal.valueOf(10L));
-    productService.changePrice(expected.getId(), request);
-
-    Assertions.assertThat(menus.getFirst().isDisplayed()).isFalse();
-  }
-
-  @Test
-  @DisplayName("상품 전체를 조회할 수 있다.")
-  void test_case_6() {
-    final List<Product> expected = List.of(FixtureProduct.fixtureProduct());
-    given(productRepository.findAll()).willReturn(expected);
-
-    final List<Product> actual = productService.findAll();
-
-    Assertions.assertThat(actual.size()).isEqualTo(expected.size());
   }
 }
