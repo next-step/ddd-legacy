a9eff563fc2ebe8d33d82cf2d801b5db86c8be16
Jivebreaddev <jivebreaddev@gmail.com>
2024-06-02 16:57:32 +0900

    feature(FakeBadWordsValidator): 비속어 검사 클라이언트 간결한 로직 작성

--- src/test/java/kitchenpos/infra/FakeBadWordsValidator.java
+++ src/test/java/kitchenpos/infra/FakeBadWordsValidator.java
@@ -3,9 +3,6 @@ package kitchenpos.infra;
 public class FakeBadWordsValidator implements BadWordsValidator {
   @Override
   public boolean containsProfanity(String text) {
-    if (text.equals("badwords"))
-      return true;
-
-    return false;
+    return text.equals("badwords");
   }
 }
