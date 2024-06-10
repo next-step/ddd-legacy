f1834d70976f36423fa43697d30ea350573f1fd6
tongnamuu <thban33@naver.com>
2024-06-10 12:08:52 +0900

    test: productCreate - price 조건 테스트

--- src/test/java/kitchenpos/application/ProductServiceTest.java
+++ src/test/java/kitchenpos/application/ProductServiceTest.java
@@ -2,14 +2,19 @@ package kitchenpos.application;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.math.BigDecimal;
 import java.util.UUID;
+
 import kitchenpos.domain.Product;
 import kitchenpos.fake.menu.TestMenuRepository;
 import kitchenpos.fake.product.TestProductRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class ProductServiceTest {
 
@@ -18,15 +23,13 @@ class ProductServiceTest {
     void create_product_success() {
         // given
         ProductService productService = new ProductService(
-            new TestProductRepository(),
-            new TestMenuRepository(),
-            (name) -> false
+                new TestProductRepository(),
+                new TestMenuRepository(),
+                (name) -> false
         );
-        UUID productId = UUID.randomUUID();
-        BigDecimal price= BigDecimal.ONE;
+        BigDecimal price = BigDecimal.ONE;
         String name = "goodName";
         Product request = new Product();
-        request.setId(productId);
         request.setPrice(price);
         request.setName(name);
 
@@ -35,9 +38,51 @@ class ProductServiceTest {
 
         // then
         assertAll(
-            () -> assertThat(product.getId()).isEqualTo(productId),
-            () -> assertThat(product.getPrice()).isEqualTo(price),
-            () -> assertThat(product.getName()).isEqualTo(name)
+                () -> assertThat(product.getPrice()).isEqualTo(price),
+                () -> assertThat(product.getName()).isEqualTo(name),
+                () -> assertThat(product.getId()).isNotNull()
         );
     }
+
+    @Test
+    @DisplayName("가격은 반드시 존재해야 한다")
+    @ValueSource(longs = {-1L})
+    void create_product_fail_price1() {
+        // given
+        ProductService productService = new ProductService(
+                new TestProductRepository(),
+                new TestMenuRepository(),
+                (name) -> false
+        );
+        BigDecimal price = null;
+        String name = "goodName";
+        Product request = new Product();
+        request.setPrice(price);
+        request.setName(name);
+
+        // when, then
+        assertThrows(IllegalArgumentException.class, () -> productService.create(request));
+    }
+
+    @ParameterizedTest
+    @DisplayName("가격은 0보다 작을 수 없다")
+    @ValueSource(longs = {-1L})
+    void create_product_fail_price2(long negativePrice) {
+        // given
+        ProductService productService = new ProductService(
+                new TestProductRepository(),
+                new TestMenuRepository(),
+                (name) -> false
+        );
+        BigDecimal price = BigDecimal.valueOf(negativePrice);
+        String name = "goodName";
+        Product request = new Product();
+        request.setPrice(price);
+        request.setName(name);
+
+        // when, then
+        assertThrows(IllegalArgumentException.class, () -> productService.create(request));
+    }
+
+
 }
