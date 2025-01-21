c3c20fd466e2ba3ebbe61c29fa285731de2d413b
dongock <okdong23@naver.com>
2025-01-21 22:11:19 +0900

    feat(racingcar) : stop&movingTest

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,5 +1,10 @@
 package racingcar;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.assertj.core.api.Assertions.*;
 
 class CarTest {
@@ -9,4 +14,23 @@ class CarTest {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new Car("동해물과백두산이"));
     }
+
+    @DisplayName("숫자가 4 이상이면 자동차가 움직인다")
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    @ParameterizedTest
+    void move(final int condition) {
+        final var car = new Car("dongock");
+        car.move(condition);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @DisplayName("숫자가 4 미만이면 자동차가 멈춘다")
+    @ValueSource(ints = {0, 1, 2, 3})
+    @ParameterizedTest
+    void stop(final int condition) {
+        final var car = new Car("dongock");
+        car.move(condition);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
 }
+
