8eb095f0bda9c80de047166add10faacd14aeac5
ch3224bin <ahrocksil@gmail.com>
2025-01-21 22:12:04 +0900

    feat(racingcar): 자동차 move 구현

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,10 +1,12 @@
 package racingcar;
 
-import org.assertj.core.api.Assertions;
 import org.assertj.core.api.ThrowableAssert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 public class CarTest {
@@ -20,5 +22,33 @@ public class CarTest {
         // then
         assertThatIllegalArgumentException()
                 .isThrownBy(throwingCallable);
+    }
+
+    @DisplayName("숫자가 4 이상이면 자동차는 움직인다")
+    @ParameterizedTest
+    @ValueSource(ints = {4, 9})
+    void move(final int condition) {
+        // given
+        Car car = new Car("붕붕");
+
+        // when
+        car.move(condition);
+
+        // then
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @DisplayName("숫자가 4 미만이면 자동차는 정지한다")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 3})
+    void notMove(final int condition) {
+        // given
+        Car car = new Car("붕붕");
+
+        // when
+        car.move(condition);
+
+        // then
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 }
