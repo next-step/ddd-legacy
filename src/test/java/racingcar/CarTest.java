44fef7a2141cd4a524e689571e2225a6edbdfbd3
hoon25 <cchoon95@hanmail.net>
2025-01-21 22:12:02 +0900

    feat(racingcar): add move

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -3,7 +3,10 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 class CarTest {
@@ -13,5 +16,23 @@ class CarTest {
     void constructor() {
         assertThatIllegalArgumentException()
                 .isThrownBy(() -> new Car("123456"));
+    }
+
+    @DisplayName("값이 4 이상이면 자동차는 움직인다")
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    @ParameterizedTest
+    void move(final int condition) {
+        final var car = new Car("hoon");
+        car.move(condition);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @DisplayName("값이 4 미만이면 자동차는 정지한다")
+    @ValueSource(ints = {0, 1, 2, 3})
+    @ParameterizedTest
+    void stop(final int condition) {
+        final var car = new Car("hoon");
+        car.move(condition);
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 }
