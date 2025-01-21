3a7a840f6ca7717bdca1989e4df31635962950ad
SH <66237694+kimsangha617@users.noreply.github.com>
2025-01-21 22:47:01 +0900

    test(racingcar): add car test (#717)
    * feat(racingcar): contstructor test
    
    * feat(racingcar): 숫자가 주어지면 자동차는 move_or_stop 한다

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,40 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CarTest {
+
+    @DisplayName("자동차의 이름은 5글자를 초과하면 예외가 발생한다.")
+    @Test
+    void constructor() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(
+                        () -> new Car("동해물과백두산이"));
+
+    }
+
+    @DisplayName("숫자가 4 이상이면 자동차는 움직인다")
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    @ParameterizedTest
+    void move(final int condition) {
+        final var car = new Car("hmm");
+        car.move(condition);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @DisplayName("숫자가 4 미만이면 자동차는 정지한다")
+    @ValueSource(ints = {0, 1, 2 ,3})
+    @ParameterizedTest
+    void stop(final int condition) {
+        final var car = new Car("hmm");
+        car.move(condition);
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+}
