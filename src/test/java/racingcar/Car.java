d2b04eb01578938dfaece26e3a79c74e974b88f5
woozi <kwj1270@naver.com>
2025-01-22 00:44:56 +0900

    refactor(racingcar): 자동차는 전진 가능한 조건이면 한 칸 움직일 수 있다.

--- src/test/java/racingcar/Car.java
+++ src/test/java/racingcar/Car.java
@@ -12,8 +12,8 @@ public class Car {
         this.position = 0;
     }
 
-    public void move(final int condition) {
-        if (condition >= 4) {
+    public void move(final MovingStrategy movingStrategy) {
+        if (movingStrategy.movable()) {
             position++;
         }
     }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -3,8 +3,8 @@ package racingcar;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import racingcar.strategy.ForwardMovingStrategy;
+import racingcar.strategy.StopMovingStrategy;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -18,23 +18,20 @@ public class CarTest {
                 .isThrownBy(() -> new Car("123456"));
     }
 
-
-    @DisplayName("자동차는 조건이 4이상일 경우 1칸 움직일 수 있다.")
-    @ValueSource(ints = {4, 5, 6, 7, 8, 9, 10})
-    @ParameterizedTest(name = "조건이 {0}일 경우 1칸 움직인다.")
-    void move(final int condition) {
+    @DisplayName("자동차는 전진 가능한 조건이면 한 칸 움직일 수 있다.")
+    @Test
+    void move() {
         final Car car = new Car("woozi");
-        car.move(condition);
+        car.move(new ForwardMovingStrategy());
 
         assertThat(car.getPosition()).isEqualTo(1);
     }
 
-    @DisplayName("자동차는 조건이 4미만일 경우 움직이지 않는다.")
-    @ValueSource(ints = {0, 1, 2, 3})
-    @ParameterizedTest(name = "조건이 {0}일 경우 움직이지 않는다.")
-    void stop(final int condition) {
+    @DisplayName("자동차는 전진 불가능한 조건이면 움직이지 않는다.")
+    @Test
+    void stop() {
         final Car car = new Car("woozi");
-        car.move(condition);
+        car.move(new StopMovingStrategy());
 
         assertThat(car.getPosition()).isEqualTo(0);
     }
