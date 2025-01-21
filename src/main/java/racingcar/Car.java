25ab56209a4aef126fe85782a2a9f35ca52fd1d2
khpark0203 <31102444+khpark0203@users.noreply.github.com>
2025-01-22 01:15:20 +0900

    Step0 - JUnit 5 학습 (#733)
    * feat(racingcar): constructor add
    
    * feat(racingcar): move, stop
    
    * refactor(racingcar): fake object add

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,34 @@
+package racingcar;
+
+import java.util.Random;
+
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(final String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move() {
+        final var condition = new Random().nextInt(10);
+        move(condition);
+    }
+
+    public void move(final int condition) {
+        move(() -> condition >= 4);
+    }
+
+    public void move(final MovingStrategy movingStrategy) {
+        if (movingStrategy.movable()) {
+            position++;
+        }
+    }
+}
