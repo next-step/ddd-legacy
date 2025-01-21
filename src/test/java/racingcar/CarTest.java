ab73f41a4857501997b931bff483a163a12e4f9e
hoon25 <cchoon95@hanmail.net>
2025-01-21 22:24:53 +0900

    refactor(racingcar): interface movable

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -3,8 +3,6 @@ package racingcar;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
@@ -18,21 +16,19 @@ class CarTest {
                 .isThrownBy(() -> new Car("123456"));
     }
 
-    @DisplayName("값이 4 이상이면 자동차는 움직인다")
-    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
-    @ParameterizedTest
-    void move(final int condition) {
+    @Test
+    @DisplayName("자동차는 움직인다")
+    void move() {
         final var car = new Car("hoon");
-        car.move(condition);
+        car.move(new ForwardStrategy());
         assertThat(car.getPosition()).isEqualTo(1);
     }
 
-    @DisplayName("값이 4 미만이면 자동차는 정지한다")
-    @ValueSource(ints = {0, 1, 2, 3})
-    @ParameterizedTest
-    void stop(final int condition) {
+    @DisplayName("자동차는 정지한다")
+    @Test
+    void stop() {
         final var car = new Car("hoon");
-        car.move(condition);
+        car.move(new StopStrategy());
         assertThat(car.getPosition()).isEqualTo(0);
     }
 }
