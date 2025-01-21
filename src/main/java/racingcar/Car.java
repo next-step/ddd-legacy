66246d4bf2016b16ae027b5017f2dfd46abb6a64
leewonsuk <tjdjdjrdl123@naver.com>
2025-01-21 22:11:06 +0900

    feat: racingcar 5글자 예외 및 이동 거리 로직 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,12 +1,25 @@
 package racingcar;
 
+import java.util.Random;
+
 public class Car {
     private final String name;
+    private int position;
 
     public Car(final String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException();
         }
         this.name = name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move(final int condition) {
+        if (condition >= 4) {
+            position++;
+        }
     }
 }
