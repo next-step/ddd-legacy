9ee5116f28e540595013ab1be9577461bcf79c02
Hae-gun <newchk610@gmail.com>
2025-01-21 23:44:13 +0900

    feat(racingcar): Car move 구현 및 테스트

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,11 +2,22 @@ package racingcar;
 
 public class Car {
     private final String name;
+    private int position;
     public Car(String name) {
         if(name.length() > 5){
             throw new IllegalArgumentException("name is invalid");
         }
         this.name = name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move(int condition){
+        if (condition >= 4) {
+            position++;
+        }
     }
 
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,8 +1,10 @@
 package racingcar;
 
-
 import org.junit.jupiter.api.*;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.*;
+
+import static org.assertj.core.api.Assertions.*;
 
 class CarTest {
 
@@ -10,5 +12,23 @@ class CarTest {
     @DisplayName("자동차 이름은 5자를 넘길수 없다.")
     void constructor(){
         assertThatIllegalArgumentException().isThrownBy(() -> new Car("nameiscar"));
+    }
+
+    @DisplayName("숫자가 4 이상이면 자동차가 움직인다")
+    @ValueSource(ints = {4,5,6,7,8,9})
+    @ParameterizedTest
+    void move(final int condition) {
+        Car car = new Car("bab2");
+        car.move(4);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @DisplayName("숫자가 4 미만이면 자동차가 정지한다")
+    @ValueSource(ints = {0,1,2,3})
+    @ParameterizedTest
+    void stop(final int condition) {
+        Car car = new Car("bab2");
+        car.move(3);
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 }
