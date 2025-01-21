66e2d1547cf5221f47854efa4aaa165806dbfa28
hongsumin <>
2025-01-21 22:11:55 +0900

    feat(racingcar): move 기능 추가

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -3,6 +3,10 @@ package racingcar;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
 
 class CarTest {
     @DisplayName(("자동차의 이름은 5글자 이하다"))
@@ -10,5 +14,23 @@ class CarTest {
     void constructor(){
         Assertions.assertThatIllegalArgumentException()
                 .isThrownBy(()-> new Car("동해물과백두산이"));
+    }
+
+    @DisplayName(("숫자가 4 이상이면 자동차는 움직인다"))
+    @ValueSource(ints = {4,5,6,7,8,9})
+    @ParameterizedTest
+    void move(final int condition){
+        final var car = new Car("sumin");
+        car.move(condition);
+        Assertions.assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @DisplayName(("숫자가 4 미만이면 자동차는 정지한다"))
+    @ValueSource(ints = {0,1,2,3})
+    @ParameterizedTest
+    void stop(final int condition){
+        final var car = new Car("sumin");
+        car.move(condition);
+        Assertions.assertThat(car.getPosition()).isEqualTo(0);
     }
 }
