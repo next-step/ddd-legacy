ea092cf88496bc141d1e63aae31c42c819a535fc
leewonsuk <tjdjdjrdl123@naver.com>
2025-01-21 22:27:24 +0900

    refactor: 전략패턴 move 메서드 추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,7 +1,5 @@
 package racingcar;
 
-import java.util.Random;
-
 public class Car {
     private final String name;
     private int position;
@@ -18,7 +16,11 @@ public class Car {
     }
 
     public void move(final int condition) {
-        if (condition >= 4) {
+        move(() -> condition >= 4);
+    }
+
+    public void move(final MovingStrategy movingStrategy) {
+        if (movingStrategy.movable()) {
             position++;
         }
     }
