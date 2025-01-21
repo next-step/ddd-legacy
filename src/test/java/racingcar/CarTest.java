ecde4398a849e831e411b5fce66e4dfd85c84e8c
cjcjon <cjcjon@naver.com>
2025-01-21 22:24:48 +0900

    refactor(racingcar): 자동차 이동 interface 구현

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -3,8 +3,6 @@ package racingcar;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,21 +14,19 @@ public class CarTest {
                 .isThrownBy(() -> new Car("동해물과백두산이"));
     }
 
-    @DisplayName("숫자가 4 이상이면 자동차는 움직인다")
-    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
-    @ParameterizedTest
-    void move(final int condition) {
+    @DisplayName("자동차는 움직인다")
+    @Test
+    void move() {
         final var car = new Car("자동차");
-        car.move(condition);
+        car.move(new ForwardStrategy());
         assertThat(car.getPosition()).isEqualTo(1);
     }
 
-    @DisplayName("숫자가 4 미만이면 자동차는 정지한다")
-    @ValueSource(ints = {0, 1, 2, 3})
-    @ParameterizedTest
-    void stop(final int condition) {
+    @DisplayName("자동차는 정지한다")
+    @Test
+    void stop() {
         final var car = new Car("자동차");
-        car.move(condition);
+        car.move(new StopStrategy());
         assertThat(car.getPosition()).isEqualTo(0);
     }
 }
