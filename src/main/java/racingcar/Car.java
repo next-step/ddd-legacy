3163dc4c2730904c59958c9096ff58b2614fe6d2
restinbeat <thek226@gmail.com>
2025-01-21 22:12:22 +0900

    feat(racingcar): constructor(test)

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+import java.util.Random;
+
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(final String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move() {
+        final var condition = new Random().nextInt(10);
+        move(condition);
+    }
+
+    public void move(final int condition) {
+        if (condition >= 4) {
+            position++;
+        }
+    }
+}
---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class CarTest {
+    @DisplayName("자동차의 이름은 5글자를 초과하면 예외가 발생한다")
+    @Test
+    void constructor() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Car("동해물과백두산이"));
+    }
+
+}
