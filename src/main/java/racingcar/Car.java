f94efe13d68583c861db1492b8219621cc4e09aa
Tom <80812697+JongMinCh0i@users.noreply.github.com>
2025-01-21 23:33:38 +0900

    Step0: racingcar JUnit5  학습 (#728)

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,31 @@
+package racingcar;
+
+import java.util.Random;
+
+public class Car {
+    private final String name;
+    private int position;
+
+
+    public Car(final String name) {
+        if(name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5글자를 초과할 수 없습니다.");
+        }
+
+        this.name = name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move(final int condition) {
+        move(() -> condition >= 4);
+    }
+
+    public void move(final MovingStrategy movingStrategy) {
+        if(movingStrategy.movable()) {
+            position++;
+        }
+    }
+}
