77835df0ab40fd429c20830f67adea94e52fa2cc
gugbab2 <jkhoo1997@gmail.com>
2025-01-21 22:11:25 +0900

    feat(racingcar): move, stop 기능 개발

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,13 +2,26 @@ package racingcar;
 
 import org.hibernate.boot.model.naming.IllegalIdentifierException;
 
+import java.util.Random;
+
 public class Car {
     private final String name;
+    private int position;
 
     public Car(final String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException();
         }
         this.name = name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move(int condition){
+        if (condition >= 4) {
+            position++;
+        }
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,17 +1,36 @@
 package racingcar;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 class CarTest {
 
     @DisplayName("자동차의 이름은 5글자를 초과하면 예외가 발생한다.")
     @Test
     void constructor() {
-        Assertions.assertThatIllegalArgumentException().isThrownBy(() -> new Car("gugbab2"));
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Car("gugbab2"));
     }
 
+    @DisplayName("숫자가 4 이상이면 자동차가 움직인다")
+    @ValueSource(ints = {4,5,6,7,8,9})
+    @ParameterizedTest
+    void move(final int condition) {
+        Car car = new Car("bab2");
+        car.move(4);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @DisplayName("숫자가 4 미만이면 자동차가 정지한다")
+    @ValueSource(ints = {0,1,2,3})
+    @ParameterizedTest
+    void stop(final int condition) {
+        Car car = new Car("bab2");
+        car.move(3);
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
 }
