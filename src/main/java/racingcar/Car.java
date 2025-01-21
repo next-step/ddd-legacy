aabf51835972e1dcd5b066d8a467ce9ebb89948e
nomoreFt <hyunwoo.kim1108@gmail.com>
2025-01-21 22:13:50 +0900

    feat: init Car Class

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+public class Car {
+
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        if(name.length() > 5) {
+            throw new IllegalArgumentException("자동차 이름은 5글자를 초과할 수 없습니다.");
+        }
+        this.name = name;
+    }
+
+    public void move(final int condition) {
+        if(condition >= 4) {
+            position++;
+        }
+
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
