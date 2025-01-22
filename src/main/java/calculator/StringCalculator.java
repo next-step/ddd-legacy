ae51cd42a7ead94d92783ae7c17b337c72ad2f75
wocks1123 <wocks1123@gmail.com>
2025-01-22 21:57:22 +0900

    test: StringCalculator 구현
    - 입력 문자열을 순회하며 구분자를 제외한 숫자 더하는 방식

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -3,8 +3,19 @@ package calculator;
 public class StringCalculator {
 
     public int add(String text) {
-        // TODO implement
-        return 0;
+        int sum = 0;
+        int num = 0;
+
+        for (char c : text.toCharArray()) {
+            if (c == ',' || c == ':') {
+                sum += num;
+                num = 0;
+            } else {
+                num = num * 10 + (c - '0');
+            }
+        }
+
+        return sum + num;
     }
 
 }
