c249ea807195c80a4454c85dc29bd7044b123ef3
Jinwoo Oh <dhwlsdn741@gmail.com>
2025-01-22 15:37:30 +0900

    🚀 0단계 - JUnit 5 학습 (#737)

---
+++ src/main/java/racingcar/RandomMovingStrategy.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
+public class RandomMovingStrategy implements MovingStrategy
+{
+    private final Random random = ThreadLocalRandom.current();
+    private static final int RANGE = 10;
+    private static final int RESTRICTION = 4;
+
+    @Override
+    public boolean movable() {
+        return random.nextInt(RANGE) >= RESTRICTION;
+    }
+}
