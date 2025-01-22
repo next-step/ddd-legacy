344c66664b29797398ff2ee073107ca570eb9e1d
NOMAD <thek226@gmail.com>
2025-01-22 18:08:12 +0900

    0단계 - JUnit 5 학습 (#719)
    * feat(racingcar): constructor(test)
    
    * chore: change port
    
    * feat(racingcar): move, stop (test)
    
    * refactor(racingcar): fake object

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,34 @@
+package racingcar;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class CarTest {
+    @DisplayName("자동차의 이름은 5글자를 초과하면 예외가 발생한다")
+    @Test
+    void constructor() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Car("동해물과백두산이"));
+    }
+
+    @DisplayName("자동차는 움직인다")
+    @Test
+    void move() {
+        final var car = new Car("jason");
+        car.move(new ForwardStrategy());
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @DisplayName("자동차는 정지한다")
+    @Test
+    void stop() {
+        final var car = new Car("jason");
+        car.move(new StopStrategy());
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+}
