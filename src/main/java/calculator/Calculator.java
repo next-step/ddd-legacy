6adb792fff96b4306830f449f0f69ee150504d24
suzhanlee <108535068+suzhanlee@users.noreply.github.com>
2025-01-22 19:15:42 +0900

    feat : Calculator 커스텀 구분자로 덧셈하는 기능 추가 및 테스트

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -4,9 +4,9 @@ import java.util.StringTokenizer;
 
 public class Calculator {
 
-    public int calculate(String value) {
-        int sum = 0;
+    private static final String DEFAULT_DELIMITER = ",:";
 
+    public int calculate(String value) {
         if (value == null || value.isEmpty()) {
             return 0;
         }
@@ -15,7 +15,32 @@ public class Calculator {
             return 0;
         }
 
-        StringTokenizer st = new StringTokenizer(value, ",:");
+        String delimiter = parseDelimiter(value);
+        String targetValue = parseTarget(value, delimiter);
+
+        return calculateSum(targetValue, delimiter);
+    }
+
+    private String parseDelimiter(String value) {
+        if (value.startsWith("//")) {
+            int newLineIndex = value.indexOf("\\n");
+            if (newLineIndex != -1) {
+                return value.substring(2, newLineIndex);
+            }
+        }
+        return DEFAULT_DELIMITER;
+    }
+
+    private String parseTarget(String value, String delimiter) {
+        if (delimiter.equals(DEFAULT_DELIMITER)) {
+            return value;
+        }
+        return value.substring(value.indexOf(delimiter) + 3);
+    }
+
+    private int calculateSum(String numberString, String delimiter) {
+        int sum = 0;
+        StringTokenizer st = new StringTokenizer(numberString, delimiter);
         while (st.hasMoreElements()) {
             String token = st.nextToken().trim();
             sum += Integer.parseInt(token);
--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -3,7 +3,6 @@ package calculator;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -17,7 +16,7 @@ class CalculatorTest {
             "'1', 1",
             "'''', 0"
     })
-    @DisplayName("쉼표를 구분자로 가지는 문자열을 더할 수 있다.")
+    @DisplayName("쉼표를 구분자로 구분된 문자열을 더할 수 있다.")
     void calculate_by_comma(String value, int expected) {
         // given
         Calculator calculator = new Calculator();
@@ -31,10 +30,10 @@ class CalculatorTest {
 
     @ParameterizedTest
     @CsvSource({
-            "'1:2:3', 6",
-            "'10:20', 30",
+            "1:2:3, 6",
+            "10:20, 30",
     })
-    @DisplayName("콜론을 구분자로 가지는 문자열을 더할 수 있다.")
+    @DisplayName("콜론을 구분자로 구분된 문자열을 더할 수 있다.")
     void calculate_by_colon(String value, int expected) {
         // given
         Calculator calculator = new Calculator();
@@ -46,4 +45,20 @@ class CalculatorTest {
         assertThat(result).isEqualTo(expected);
     }
 
+    @ParameterizedTest
+    @CsvSource(value = {
+            "'//;\\n1;2;3', 6",
+            "'//*\\n1*3', 4"
+    }, delimiterString = ",")
+    @DisplayName("커스텀 구분자로 구분된 가지는 문자열을 더할 수 있다.")
+    void calculate_by_custom_delimiter(String value, int expected) {
+        // given
+        Calculator calculator = new Calculator();
+
+        // when
+        int result = calculator.calculate(value);
+
+        // then
+        assertThat(result).isEqualTo(expected);
+    }
 }
