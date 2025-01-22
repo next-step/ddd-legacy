3b3709ca9e5923331c778137b2b2b02aed7650f7
ChangHoon Choi <69202388+hoon25@users.noreply.github.com>
2025-01-22 13:48:48 +0900

    🚀 0단계 - JUnit 5 학습 (#713)
    * feat(racingcar): add name
    
    * feat(racingcar): add move
    
    * refactor(racingcar): interface movable

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,34 @@
+package racingcar;
+
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+class CarTest {
+
+    @DisplayName("자동차의 이름은 5글자를 초과하면 예외가 발생한다")
+    @Test
+    void constructor() {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> new Car("123456"));
+    }
+
+    @Test
+    @DisplayName("자동차는 움직인다")
+    void move() {
+        final var car = new Car("hoon");
+        car.move(new ForwardStrategy());
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @DisplayName("자동차는 정지한다")
+    @Test
+    void stop() {
+        final var car = new Car("hoon");
+        car.move(new StopStrategy());
+        assertThat(car.getPosition()).isEqualTo(0);
+    }
+}
