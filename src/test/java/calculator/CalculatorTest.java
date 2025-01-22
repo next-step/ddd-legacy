b204e91c4eb9b7eff13588a2495e53e2b551b5b7
이동옥 <moveok@idong-og-ui-MacBookAir.local>
2025-01-22 12:23:37 +0900

    refactor(cal): beforeEach add

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
@@ -9,12 +10,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorTest {
 
+    private Calculator calculator;
+
+    // 전체 생성하는거 추가
+    @BeforeEach
+    void setCalculator() {
+        calculator = new Calculator();
+    }
+
     @DisplayName("빈 문자열이나 Null을 입력 시에 0을 반환한다.")
     @ValueSource(strings = {"", "있음"})
     @NullSource
     @ParameterizedTest
     void nullCheck(String input) {
-        assertThat(new Calculator().calculate(input)).isEqualTo(0);
+        assertThat(calculator.calculate(input)).isEqualTo(0);
     }
 
+    @DisplayName("문자열이 숫자하나만 들어왔을 경우 숫자를 반환한다.")
+    @ValueSource(strings = {"1", "22", "333", "1a", "aa"})
+    @ParameterizedTest
+    void onlyNumberCheck(String input) {
+
+    }
 }
