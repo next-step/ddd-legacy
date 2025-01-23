2c16ef45490119bc51871d499f7c4ba1f0fbfeef
woozi <kwj1270@naver.com>
2025-01-23 12:33:12 +0900

    feat(calculator): 빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.

---
+++ src/test/java/calculator/StringCalculator.java
@@ -0,0 +1,8 @@
+package calculator;
+
+public class StringCalculator {
+
+    public int add(final String text) {
+        return 0;
+    }
+}
--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DisplayName("문자열 계산기 단위 테스트")
 class StringCalculatorTest {
 
@@ -16,7 +18,7 @@ class StringCalculatorTest {
     }
 
     @DisplayName(value = "빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
-    @ParameterizedTest
+    @ParameterizedTest(name = "입력 값: {0}")
     @NullAndEmptySource
     void emptyOrNull(final String text) {
 
