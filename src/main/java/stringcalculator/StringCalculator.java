8eb8f9ab30f68d46466186fc510c977a6e1a4505
skdev0619 <skdev0619@naver.com>
2025-01-23 13:47:05 +0900

    숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,11 +1,36 @@
 package stringcalculator;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
+
 public class StringCalculator {
+
+    private static final String SEPARATOR = ",";
 
     public int add(String text) {
         if (text == null || text.isBlank()) {
             return 0;
         }
-        return Integer.parseInt(text);
+        List<Integer> datas = convertToIntArray(text);
+        return sum(datas);
+    }
+
+    private List<Integer> convertToIntArray(String text) {
+        String[] textArray = split(text);
+        return Arrays.stream(textArray)
+                .map(Integer::parseInt)
+                .collect(toList());
+    }
+
+    private int sum(List<Integer> datas) {
+        return datas.stream()
+                .mapToInt(Integer::intValue)
+                .sum();
+    }
+
+    private String[] split(String text) {
+        return text.split(SEPARATOR);
     }
 }
