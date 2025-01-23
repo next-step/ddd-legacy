d6de9a0a4464e9fe219f2d44bb3bca8afd20ffe9
skdev0619 <skdev0619@naver.com>
2025-01-23 13:28:08 +0900

    빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.

---
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -0,0 +1,11 @@
+package stringcalculator;
+
+public class StringCalculator {
+
+    public int add(String text) {
+        if (text == null || text.isBlank()) {
+            return 0;
+        }
+        return 1;
+    }
+}
