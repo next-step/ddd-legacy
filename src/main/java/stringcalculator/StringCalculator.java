24997d08a77527ead776ec439816c8de4dbd8b68
skdev0619 <skdev0619@naver.com>
2025-01-23 14:02:12 +0900

    문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리를 한다.

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,13 +1,14 @@
 package stringcalculator;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
-
-import static java.util.stream.Collectors.toList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
 
-    private static final String SEPARATOR = ",|:";
+    private static final String DEFAULT_DELIMITER = ",|:";
+    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
 
     public int add(String text) {
         if (text == null || text.isBlank()) {
@@ -19,18 +20,35 @@ public class StringCalculator {
 
     private List<Integer> convertToIntArray(String text) {
         String[] textArray = split(text);
-        return Arrays.stream(textArray)
-                .map(Integer::parseInt)
-                .collect(toList());
+
+        List<Integer> numbers = new ArrayList<Integer>();
+        for (String data : textArray) {
+            int number = validate(data);
+            numbers.add(number);
+        }
+        return numbers;
+    }
+
+    private int validate(String data) {
+        int number = Integer.parseInt(data);
+        if (number < 0) {
+            throw new RuntimeException("음수는 계산할 수 없습니다.");
+        }
+        return number;
+    }
+
+    private String[] split(String text) {
+        Matcher matcher = CUSTOM_DELIMITER_PATTERN.matcher(text);
+        if (matcher.find()) {
+            String customDelimiter = matcher.group(1);
+            return matcher.group(2).split(customDelimiter);
+        }
+        return text.split(DEFAULT_DELIMITER);
     }
 
     private int sum(List<Integer> numbers) {
         return numbers.stream()
                 .mapToInt(Integer::intValue)
                 .sum();
-    }
-
-    private String[] split(String text) {
-        return text.split(SEPARATOR);
     }
 }
--- src/test/java/stringcalculator/StringCalculatorTest.java
+++ src/test/java/stringcalculator/StringCalculatorTest.java
@@ -2,11 +2,13 @@ package stringcalculator;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class StringCalculatorTest {
     private StringCalculator calculator;
@@ -44,4 +46,18 @@ class StringCalculatorTest {
         assertThat(calculator.add(text)).isSameAs(6);
     }
 
+    @DisplayName(value = "//와 \\n 문자 사이에 커스텀 구분자를 지정할 수 있다.")
+    @ParameterizedTest
+    @ValueSource(strings = {"//;\n1;2;3"})
+    void customDelimiter(final String text) {
+        assertThat(calculator.add(text)).isSameAs(6);
+    }
+
+    @DisplayName(value = "문자열 계산기에 음수를 전달하는 경우 RuntimeException 예외 처리를 한다.")
+    @Test
+    void negative() {
+        assertThatThrownBy(() -> calculator.add("-1"))
+                .isInstanceOf(RuntimeException.class)
+                .hasMessage("음수는 계산할 수 없습니다.");
+    }
 }
