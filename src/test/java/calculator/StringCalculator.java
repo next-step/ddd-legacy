c061723ddb02f6be86319f9dd97203aa21f177ef
woozi <kwj1270@naver.com>
2025-01-23 12:51:32 +0900

    feat(calculator): 숫자 두개를 쉼표(,) 구분자로 입력할 경우 두 숫자의 합을 반환한다.

--- src/test/java/calculator/StringCalculator.java
+++ src/test/java/calculator/StringCalculator.java
@@ -2,11 +2,19 @@ package calculator;
 
 import org.apache.logging.log4j.util.Strings;
 
+import java.util.Arrays;
+
 public class StringCalculator {
 
     public int add(final String text) {
         if (Strings.isBlank(text)) {
             return 0;
+        }
+        if (text.contains(",")) {
+            return Arrays.stream(text.split(","))
+                    .mapToInt(Integer::parseInt)
+                    .reduce(Integer::sum)
+                    .getAsInt();
         }
         return Integer.parseInt(text);
     }
