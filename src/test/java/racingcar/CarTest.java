995a335ca714d478ad823464b88251fa0c7f8e6c
domo7304 <domo7304@naver.com>
2025-01-24 17:48:12 +0900

    feat(racingcar): Add car class and constructor with test code

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,16 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class CarTest {
+
+    @DisplayName("자동차의 이름은 5글자를 초과하면 예외가 발생한다.")
+    @Test
+    void constructor() {
+        assertThatIllegalArgumentException()
+            .isThrownBy(() -> new Car("동해물과백두산이"));
+    }
+}
