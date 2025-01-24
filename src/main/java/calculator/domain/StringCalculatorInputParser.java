5f86279a7a77393b95f995aeb3e58b9f46d619d8
nomoreFt <hyunwoo.kim1108@gmail.com>
2025-01-24 14:20:01 +0900

    refactor(calculator): StringCalculatorInputParser를 InputParser 인터페이스로 추출하여 역할 분리 및 확장성 강화

--- src/main/java/calculator/domain/StringCalculatorInputParser.java
+++ src/main/java/calculator/domain/StringCalculatorInputParser.java
@@ -3,13 +3,14 @@ package calculator.domain;
 import java.util.Arrays;
 import java.util.List;
 
-public class StringCalculatorInputParser {
+public class StringCalculatorInputParser implements InputParser {
     private final StringCalculatorDelimiters delimiters;
 
     public StringCalculatorInputParser(StringCalculatorDelimiters delimiters) {
         this.delimiters = delimiters;
     }
 
+    @Override
     public List<String> parse(final String input) {
         String processedInput = delimiters.extractAndAddCustomDelimiter(input);
         return Arrays.asList(processedInput.split(getDelimiterRegex()));
