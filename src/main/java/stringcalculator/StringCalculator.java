52656843dadb351c789fefeb5080959825de1c38
서민정 <seominjeong.dev@gmail.com>
2025-01-24 11:06:23 +0900

    🚀 1단계 - 문자열 덧셈 계산기 (#751)
    * feat: 빈 문자열이나 null이 입력된 경우 0을 반환
    
    * feat: 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환
    
    * feat: 컴마로 구분할 경우 두 숫자의 합을 반환한다.
    
    * feat: 구분자를 컴마 외 콜론을 사용할 수 있다.
    
    * feat: 커스텀 구분자 지정
    
    * feat: 음수를 전달하는 경우 RuntimeException을 발생시킨다.
    
    * refactor: 공통 메서드 분리

---
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -0,0 +1,43 @@
+package stringcalculator;
+
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class StringCalculator {
+
+    public int add(String text) {
+        if (text == null || text.isEmpty()) {
+            return 0;
+        }
+
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            String[] tokens = m.group(2).split(customDelimiter);
+            return getSum(tokens);
+        }
+
+        try {
+            var number = Integer.parseInt(text);
+            checkForNegative(number);
+            return number;
+        } catch (NumberFormatException e) {
+            String[] values = text.split("[,:]");
+            return getSum(values);
+        }
+    }
+
+    private int getSum(String[] tokens) {
+        return Arrays.stream(tokens)
+                .mapToInt(Integer::parseInt)
+                .peek(this::checkForNegative)
+                .sum();
+    }
+
+    private void checkForNegative(int number) {
+        if (number < 0) {
+            throw new RuntimeException("Negative numbers not allowed: " + number);
+        }
+    }
+}
