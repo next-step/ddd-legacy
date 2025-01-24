112cc392392d16706233700bac0d642d1bf2e941
ch3224bin <ahrocksil@gmail.com>
2025-01-24 21:41:03 +0900

    test(calculator): 공백으로만 이루어진 문자열을 받으면 RuntimeException 발생

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -14,7 +14,7 @@ public class StringCalculatorTest {
 
     @DisplayName("양의 정수가 아닌 값을 입력하면 RuntimeException이 발생해야 한다.")
     @ParameterizedTest
-    @ValueSource(strings = {"-1", "a", "Z", ".", "-", ".9", "0.11", "1:-1:1"})
+    @ValueSource(strings = {"-1", "a", "Z", ".", "-", ".9", "0.11", "1:-1:1", "   "})
     void validNumber(String value) {
         // when
         ThrowableAssert.ThrowingCallable throwingCallable = () -> StringCalculator.of(value);
