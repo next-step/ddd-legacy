f28e4bc188dad1185ada9928028d13b8bbde3f48
leewonsuk <tjdjdjrdl123@naver.com>
2025-01-25 01:19:19 +0900

    feat: 기본 구분자 및 커스텀 구분자 처리

--- src/main/java/stringcalculator/Calculator.java
+++ src/main/java/stringcalculator/Calculator.java
@@ -1,18 +1,34 @@
 package stringcalculator;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
 
-    public static final String DEFAULT_RESULT = "0";
+    public static final int DEFAULT_RESULT = 0;
     public static final String CUSTOM_REGEX = "//.\\n";
+    static List<Integer> tokens;
 
-    public static String calculate(final String operand) {
+    public static int calculate(final String operand) {
+        validateNegativeNumber(operand);
+
         if (operand == null || operand.isEmpty()) {
             return DEFAULT_RESULT;
         }
-        validateNegativeNumber(operand);
-        return operand;
+
+        if (operand.chars().allMatch(Character::isDigit)) {
+            return Integer.parseInt(operand);
+        }
+        Matcher matcher = Pattern.compile("//(.)\\n(.*)").matcher(operand);
+        if (matcher.find()) {
+            String customDelimeter = matcher.group(1);
+            tokens = Arrays.stream(matcher.group(2).split(customDelimeter)).map(Integer::parseInt).toList();
+            return tokens.stream().mapToInt(Integer::intValue).sum();
+        }
+        tokens = Arrays.stream(operand.split(":|,")).map(Integer::parseInt).toList();
+        return tokens.stream().mapToInt(Integer::intValue).sum();
     }
 
     private static void validateNegativeNumber(String operand) {
