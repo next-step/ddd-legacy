9b2506ce21f562a89c7157b7d0f859677ae3fcc0
louisJu <rokmctkd6@gmail.com>
2025-01-24 18:23:30 +0900

    feat(racingcar) 컨디션에 따른 자동차 position 처리 로직 추가

--- src/main/java/racingcar/Car.kt
+++ src/main/java/racingcar/Car.kt
@@ -1,9 +1,17 @@
 package racingcar
 
 class Car(
-    val name: String,
+    val name: String
 ) {
+    var position: Int = 0
+
     init {
         require(name.length > 5) { throw IllegalArgumentException() }
+    }
+
+    fun move(condition: Int) {
+        if(condition >= 4) {
+            position++
+        }
     }
 }
--- src/test/java/racingcar/CarTest.kt
+++ src/test/java/racingcar/CarTest.kt
@@ -1,13 +1,35 @@
 package racingcar
 
+import org.assertj.core.api.Assertions.assertThat
 import org.assertj.core.api.Assertions.assertThatIllegalArgumentException
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 
 class CarTest {
     @DisplayName("자동차의 이름은 5글자를 초과하면 안된다.")
     @Test
     fun constructor() {
         assertThatIllegalArgumentException().isThrownBy { Car("안녕") }
+    }
+
+    @DisplayName("숫자가 4 이상이면 자동차는 움직인다. ")
+    @ValueSource(ints = [4, 5, 6, 7, 8, 9])
+    @ParameterizedTest
+    fun move(condition: Int) {
+        val car = Car("hwandoli")
+        car.move(condition)
+        assertThat(car.position).isEqualTo(1)
+    }
+
+    @DisplayName("숫자가 4 미만이면 자동차는 정지한다. ")
+    @ValueSource(ints = [0, 1, 2, 3])
+    @ParameterizedTest
+    fun stop(condition: Int) {
+        val car = Car("hwandoli")
+        car.move(condition)
+        assertThat(car.position).isEqualTo(0)
+
     }
 }
