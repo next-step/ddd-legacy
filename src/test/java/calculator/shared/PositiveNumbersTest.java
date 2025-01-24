9450c97331695d5013ccf3e2063ea89b6f9097dc
nomoreFt <hyunwoo.kim1108@gmail.com>
2025-01-25 03:50:34 +0900

    style(calculator): 안쓰는 주석 삭제

--- src/test/java/calculator/shared/PositiveNumbersTest.java
+++ src/test/java/calculator/shared/PositiveNumbersTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
 import java.util.List;
 
 public class PositiveNumbersTest {
