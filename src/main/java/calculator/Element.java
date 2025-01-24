42d56e5a4cb076fd571a532de10905103c7d0659
hoon25 <cchoon95@hanmail.net>
2025-01-24 13:00:17 +0900

    refactor(Element): 숫자에 대한 검증만 하도록 변경

--- src/main/java/calculator/Element.java
+++ src/main/java/calculator/Element.java
@@ -5,25 +5,21 @@ import java.util.Objects;
 public class Element {
     private final int element;
 
-    private Element(final String text) {
-        if (Objects.isNull(text) || text.isBlank()) {
+    private Element(final Integer number) {
+        if (Objects.isNull(number)) {
             this.element = 0;
             return;
         }
-        if (text.trim().length() > 1) {
-            throw new IllegalArgumentException("구분 후 한 자리수 숫자만 입력할 수 있습니다.");
-        }
-        try {
-            this.element = Integer.parseInt(text);
-        } catch (NumberFormatException e) {
-            throw new IllegalArgumentException("숫자가 아닙니다.");
-        }
-        if (this.element < 0) {
+        if (number < 0) {
             throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
         }
+        if (number > 9) {
+            throw new IllegalArgumentException("한 자리 수 이상은 입력할 수 없습니다.");
+        }
+        this.element = number;
     }
 
-    public static Element of(final String number) {
+    public static Element of(final Integer number) {
         return new Element(number);
     }
 
