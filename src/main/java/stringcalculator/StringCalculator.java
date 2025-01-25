ca8bcab0af8108089aa9096e25264ce8b9953c25
seovalue <seominjeong.dev@gmail.com>
2025-01-25 14:42:26 +0900

    refactor: 객체 단위로 분리

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -1,44 +1,15 @@
 package stringcalculator;
 
-import java.util.Arrays;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class StringCalculator {
-    private static final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
-
     public int add(String text) {
         if (text == null || text.isEmpty()) {
             return 0;
         }
 
-        Matcher m = CUSTOM_DELIMITER_PATTERN.matcher(text);
-        if (m.find()) {
-            String customDelimiter = m.group(1);
-            String[] tokens = m.group(2).split(customDelimiter);
-            return getSum(tokens);
-        }
+        Delimiter delimiter = new Delimiter(text);
+        String[] tokens = delimiter.splitText();
 
-        try {
-            var number = Integer.parseInt(text);
-            checkForNegative(number);
-            return number;
-        } catch (NumberFormatException e) {
-            String[] values = text.split("[,:]");
-            return getSum(values);
-        }
-    }
-
-    private int getSum(String[] tokens) {
-        return Arrays.stream(tokens)
-                .mapToInt(Integer::parseInt)
-                .peek(this::checkForNegative)
-                .sum();
-    }
-
-    private void checkForNegative(int number) {
-        if (number < 0) {
-            throw new RuntimeException("Negative numbers not allowed: " + number);
-        }
+        Numbers numbers = new Numbers(tokens);
+        return numbers.sum();
     }
 }
