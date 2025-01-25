2f012a44fa7230b3fb469a74a9137e847398a663
devifancy <fancy.junyongmoon@gmail.com>
2025-01-25 23:02:48 +0900

    feat: 구분자를 위한 클래스 생성

---
+++ src/main/java/calculator/Delimiter.java
@@ -0,0 +1,23 @@
+package calculator;
+
+public class Delimiter {
+
+    private static final String DEFAULT_DELIMITERS = ",|:";
+    public static final String CUSTOM_DELIMITER_PREFIX = "//";
+    public static final String CUSTOM_DELIMITER_SUFFIX = "\n";
+
+    public static String[] split(final String inputText) {
+
+        // check for custom delimiter
+        if (inputText.startsWith(CUSTOM_DELIMITER_PREFIX)) {
+            int customDelimiterEndIndex = inputText.indexOf(CUSTOM_DELIMITER_SUFFIX);
+            String customDelimiter = inputText.substring(CUSTOM_DELIMITER_PREFIX.length(), customDelimiterEndIndex);
+            String numberSection = inputText.substring(customDelimiterEndIndex + CUSTOM_DELIMITER_SUFFIX.length());
+            return numberSection.split(customDelimiter);
+        }
+
+        // default delimiters
+        return inputText.split(DEFAULT_DELIMITERS);
+    }
+}
+
