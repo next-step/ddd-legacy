80c28530c46c2afeedbdf48291517caaae741fec
hongsumin <>
2025-01-26 22:37:29 +0900

    feat : 빈 문자열 또는 null을 입력할 경우 0을 반환 & 숫자 하나를 문자열로 입력할 경우 해당 숫자를 반환

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,22 +1,26 @@
 package calculator;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 
     public int add(final String text) {
-        if (text == null || text.isEmpty()) {
-            return 0;
-        }
+        if (text == null || text.isEmpty()) return 0;
+        return validate(text);
+    }
+
+    private int validate(String number) {
         try {
-            int number = Integer.parseInt(text);
-            if (number < 0) {
+            int num = Integer.parseInt(number);
+            if (num < 0) {
                 throw new RuntimeException("양의 정수를 입력하세요.");
             }
+            return num;
         } catch (NumberFormatException e) {
             throw new RuntimeException("숫자를 입력하세요.");
         }
-
-        return Integer.parseInt(text);
     }
-
 
 }
