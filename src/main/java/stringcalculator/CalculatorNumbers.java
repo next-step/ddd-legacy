ec8f91dd47ac16f5f379d14c00fc666be7b1836b
tjdjdjrdl123 <lee-won-suk@github.io>
2025-01-26 20:24:26 +0900

    feat : 커스텀 구분자, 일반 구분자에 맞게 문자열 계산 클래스 추가

---
+++ src/main/java/stringcalculator/CalculatorNumbers.java
@@ -0,0 +1,27 @@
+package stringcalculator;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class CalculatorNumbers {
+    public static final String DEFAULT_DELIMETER = ":|,";
+    private List<Integer> calculatorNumbers;
+
+    public CalculatorNumbers(String operand) {
+        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(operand);
+        String delimeter = DEFAULT_DELIMETER;
+        if (matcher.find()) {
+            delimeter = matcher.group(1);
+            operand = matcher.group(2);
+        }
+        calculatorNumbers = Arrays.stream(operand.split(delimeter)).map(Integer::parseInt).toList();
+    }
+
+    public int sum() {
+        return calculatorNumbers.stream().mapToInt(Integer::intValue).sum();
+    }
+
+
+}
