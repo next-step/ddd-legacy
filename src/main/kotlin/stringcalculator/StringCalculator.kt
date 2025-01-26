82e1172ecd446ee988d69eccd83f40edf6c5d203
cjcjon <cjcjon30@gmail.com>
2025-01-26 20:57:57 +0900

    feat(StringCalculator): 단순 숫자 입력일경우 합산은 숫자

--- src/main/kotlin/stringcalculator/StringCalculator.kt
+++ src/main/kotlin/stringcalculator/StringCalculator.kt
@@ -5,6 +5,13 @@ class StringCalculator() {
     fun add(input: String?): Int {
         if (input.isNullOrEmpty()) return 0
 
+        val singleNumber = convertSingleNumber(input)
+        singleNumber.map { return@add it }
+
         return 0
+    }
+
+    private fun convertSingleNumber(input: String): Result<Int> {
+        return kotlin.runCatching { input.toInt() }
     }
 }
