d7f1a1f4b4254a9b104685a6affa9e636200b73e
tjdjdjrdl123 <lee-won-suk@github.io>
2025-01-26 20:23:59 +0900

    refactor : 문자열 계산로직 클래스 분리

--- src/main/java/stringcalculator/Calculator.java
+++ src/main/java/stringcalculator/Calculator.java
@@ -1,37 +1,28 @@
 package stringcalculator;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Calculator {
 
     public static final int DEFAULT_RESULT = 0;
     public static final String CUSTOM_REGEX = "//.\\n";
-    static List<Integer> tokens;
+    static CalculatorNumbers calculatorNumbers;
 
     public static int calculate(final String operand) {
-        validateNegativeNumber(operand);
-
         if (operand == null || operand.isEmpty()) {
             return DEFAULT_RESULT;
         }
 
+        validateNegativeNumber(operand);
+
         if (operand.chars().allMatch(Character::isDigit)) {
             return Integer.parseInt(operand);
         }
-        Matcher matcher = Pattern.compile("//(.)\n(.*)").matcher(operand);
-        if (matcher.find()) {
-            String customDelimeter = matcher.group(1);
-            tokens = Arrays.stream(matcher.group(2).split(customDelimeter)).map(Integer::parseInt).toList();
-            return tokens.stream().mapToInt(Integer::intValue).sum();
-        }
-        tokens = Arrays.stream(operand.split(":|,")).map(Integer::parseInt).toList();
-        return tokens.stream().mapToInt(Integer::intValue).sum();
+        calculatorNumbers = new CalculatorNumbers(operand);
+        return calculatorNumbers.sum();
     }
 
-    private static void validateNegativeNumber(String operand) {
+    private static void validateNegativeNumber(final String operand) {
         boolean isCustomOperator = Pattern.compile(CUSTOM_REGEX).matcher(operand).find();
         if (!isCustomOperator && operand.contains("-")) {
             throw new RuntimeException("음수는 입력할 수 없습니다");
