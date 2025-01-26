8476877e751d2bb270292b75bca141fc64e50593
cjcjon <cjcjon30@gmail.com>
2025-01-26 21:01:40 +0900

    feat(StringCalculator): 컴마 구분자로 입력 문자열의 합 계산

--- src/main/kotlin/stringcalculator/StringCalculator.kt
+++ src/main/kotlin/stringcalculator/StringCalculator.kt
@@ -8,10 +8,17 @@ class StringCalculator() {
         val singleNumber = convertSingleNumber(input)
         singleNumber.map { return@add it }
 
-        return 0
+        val tokens = splitInput(input, listOf(","))
+        val numberTokens = tokens.map { it.toInt() }
+
+        return numberTokens.sum()
     }
 
     private fun convertSingleNumber(input: String): Result<Int> {
         return kotlin.runCatching { input.toInt() }
+    }
+
+    private fun splitInput(input: String, delimiters: List<String>): List<String> {
+        return input.split(delimiters.joinToString("|").toRegex())
     }
 }
