05a5cf9b6e1391a59f55fc188b6156c56e521015
tjdjdjrdl123 <lee-won-suk@github.io>
2025-01-26 20:24:46 +0900

    feat : 테스트 픽스쳐 수정

--- src/test/java/stringcalculator/CalculatorTest.java
+++ src/test/java/stringcalculator/CalculatorTest.java
@@ -12,26 +12,25 @@ import static org.assertj.core.api.Assertions.assertThatRuntimeException;
 
 public class CalculatorTest {
 
-    public static final String DEFAULT_VALUE = "0";
+    public static final int DEFAULT_VALUE = 0;
     private static final String DEFAULT_NEGATIVE_VALUE = "-1";
 
     @ParameterizedTest
     @DisplayName(value = "빈문자열이거나 null이면 0 반환한다.")
     @NullAndEmptySource
     void nullAndEmptyValueTest(String operand) {
-        assertThat(Calculator.calculate(operand)).isEqualTo(Calculator.calculate(DEFAULT_VALUE));
+        assertThat(Calculator.calculate(operand)).isEqualTo(DEFAULT_VALUE);
     }
 
-    @ParameterizedTest
+    @Test
     @DisplayName(value = "숫자 문자열 1개만 입력하면 해당 숫자 반환한다.")
-    @ValueSource(strings = {"1","2","3"})
-    void singleValueTest(String operand) {
-        assertThat(Calculator.calculate(operand)).isEqualTo(operand);
+    void singleValueTest() {
+        assertThat(Calculator.calculate("1")).isEqualTo(1);
     }
 
     @Test
     @DisplayName(value = "음수 입력시 RuntimeException을 던진다.")
-    void singleValueTest() {
+    void minusValueTest() {
         assertThatRuntimeException().isThrownBy(
                 () -> Calculator.calculate(DEFAULT_NEGATIVE_VALUE)
         );
