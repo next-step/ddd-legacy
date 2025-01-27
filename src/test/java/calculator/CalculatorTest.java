f99307c8d09c77454e89296b8c065c722d089413
dongock <okdong23@naver.com>
2025-01-27 14:17:23 +0900

    refactor(cal) : `NullAndEmptySource`로 변경

--- src/test/java/calculator/CalculatorTest.java
+++ src/test/java/calculator/CalculatorTest.java
@@ -3,6 +3,7 @@ package calculator;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.NullSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -19,8 +20,7 @@ public class CalculatorTest {
     }
 
     @DisplayName("빈 문자열이나 Null을 입력 시에 0을 반환한다.")
-    @ValueSource(strings = {"", "있음"})
-    @NullSource
+    @NullAndEmptySource
     @ParameterizedTest
     void nullCheck(String input) {
         assertThat(calculator.calculate(input)).isEqualTo(0);
@@ -30,6 +30,13 @@ public class CalculatorTest {
     @ValueSource(strings = {"1", "22", "333", "1a", "aa"})
     @ParameterizedTest
     void onlyNumberCheck(String input) {
+        assertThat(calculator.calculate(input)).isEqualTo(Integer.parseInt(input));
+    }
+
+    @DisplayName("숫자 두개를 컴마 구분자자의 합을 반환한다.")
+    @ValueSource(strings = {"1,1", "2,2", "3,33", "1,a", "1,,1"})
+    @ParameterizedTest
+    void separateCommaCheck(String input) {
         assertThat(calculator.calculate(input)).isEqualTo(Integer.parseInt(input));
     }
 }
