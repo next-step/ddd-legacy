ffb2f89e58ce3ad0801ed88f60031420b25a721c
dongock <okdong23@naver.com>
2025-01-27 14:14:30 +0900

    refactor(cal) : StringUtils 제거

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,13 +1,14 @@
 package calculator;
 
-import io.micrometer.common.util.StringUtils;
-
 public class Calculator {
 
     public int calculate(String input) {
-        if (StringUtils.isBlank(input)) {
+        if (input == null || input.isEmpty()) {
             return 0;
         }
+
+
+
         return Integer.parseInt(input);
     }
 }
