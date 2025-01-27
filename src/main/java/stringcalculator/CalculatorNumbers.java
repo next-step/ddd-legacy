a836502e2de115f3f1bf6370eac788b253626c9a
tjdjdjrdl123 <lee-won-suk@github.io>
2025-01-27 17:50:51 +0900

    refactor : CalculatorNumbers 정적 팩토리 처리

--- src/main/java/stringcalculator/CalculatorNumbers.java
+++ src/main/java/stringcalculator/CalculatorNumbers.java
@@ -9,22 +9,34 @@ public class CalculatorNumbers {
     public static final String DEFAULT_DELIMETER = ":|,";
     private static final String CUSTOM_DELIMETER = "//(.)\n(.*)";
     private static final Pattern CUSTOM_PATTERN = Pattern.compile(CUSTOM_DELIMETER);
+    public static final int DELIMETER_INDEX = 1;
+    public static final int PURE_OPERAND_INDEX = 2;
 
-    private List<Integer> calculatorNumbers;
+    private final List<Integer> calculatorNumbers;
 
-    public CalculatorNumbers(String operand) {
+    public CalculatorNumbers(List<Integer> calculatorNumbers) {
+        this.calculatorNumbers = calculatorNumbers;
+    }
+
+    public static CalculatorNumbers createNumbers(String operand) {
+        return new CalculatorNumbers(convertOperandToNumbers(operand));
+    }
+
+    private static List<Integer> convertOperandToNumbers(String operand) {
         Matcher matcher = CUSTOM_PATTERN.matcher(operand);
         String delimeter = DEFAULT_DELIMETER;
         if (matcher.find()) {
-            delimeter = matcher.group(1);
-            operand = matcher.group(2);
+            delimeter = matcher.group(DELIMETER_INDEX);
+            operand = matcher.group(PURE_OPERAND_INDEX);
         }
-        calculatorNumbers = Arrays.stream(operand.split(delimeter)).map(Integer::parseInt).toList();
+        return Arrays.stream(operand.split(delimeter))
+                .map(Integer::parseInt)
+                .toList();
     }
 
     public int sum() {
-        return calculatorNumbers.stream().mapToInt(Integer::intValue).sum();
+        return calculatorNumbers.stream().
+                mapToInt(Integer::intValue)
+                .sum();
     }
-
-
 }
