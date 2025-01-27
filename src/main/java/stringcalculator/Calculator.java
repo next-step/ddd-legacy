9729fb30c377c1ece9333802d59544dfaa9636bd
tjdjdjrdl123 <lee-won-suk@github.io>
2025-01-27 17:50:43 +0900

    refactor : 메서드의미에 맞지 않는 커스텀 검증 제거 및 CalculatorNumbers 정적 팩토리 처리

--- src/main/java/stringcalculator/Calculator.java
+++ src/main/java/stringcalculator/Calculator.java
@@ -1,12 +1,8 @@
 package stringcalculator;
 
-import java.util.regex.Pattern;
-
 public class Calculator {
 
     private static final int DEFAULT_RESULT = 0;
-    private static final String CUSTOM_DELIMETER = "//.\\n";
-    private static final Pattern CUSTOM_PATTERN = Pattern.compile(CUSTOM_DELIMETER);
     public static final String MINUS_OPERAND = "-";
 
     public static int calculate(final String operand) {
@@ -14,12 +10,11 @@ public class Calculator {
             return DEFAULT_RESULT;
         }
         validateNegativeNumber(operand);
-        return new CalculatorNumbers(operand).sum();
+        return CalculatorNumbers.createNumbers(operand).sum();
     }
 
     private static void validateNegativeNumber(final String operand) {
-        final boolean isCustomOperator = CUSTOM_PATTERN.matcher(operand).find();
-        if (!isCustomOperator && operand.contains(MINUS_OPERAND)) {
+        if (operand.contains(MINUS_OPERAND)) {
             throw new RuntimeException("음수는 입력할 수 없습니다");
         }
     }
