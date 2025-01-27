8bd4efa5aef656efce4d9d4c46895add2fffede3
tjdjdjrdl123 <lee-won-suk@github.io>
2025-01-27 17:37:09 +0900

    refactor :테스트케이스 상수 하드코딩 재처리

--- src/test/java/stringcalculator/CalculatorNumbersTest.java
+++ src/test/java/stringcalculator/CalculatorNumbersTest.java
@@ -9,10 +9,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorNumbersTest {
 
-
-    private static final String CUSTOM_OPERAND = "//&\n3&4&5";
-    public static final int CUSTOM_RESULT = 12;
-
     @ParameterizedTest
     @DisplayName(value = "컴마나 콜론을 붙이면 숫자의 합을 반환한다.")
     @CsvSource(value = {"1,2,3|6","1:3:5|9"},delimiter = '|')
@@ -24,8 +20,8 @@ public class CalculatorNumbersTest {
     @Test
     @DisplayName(value = "커스텀 구분자를 사용해서 숫자의 합을 반환한다.")
     void customAddValueTest() {
-        CalculatorNumbers calculatorNumbers = new CalculatorNumbers(CUSTOM_OPERAND);
-        assertThat(calculatorNumbers.sum()).isEqualTo(CUSTOM_RESULT);
+        CalculatorNumbers calculatorNumbers = new CalculatorNumbers("//&\n3&4&5");
+        assertThat(calculatorNumbers.sum()).isEqualTo(12);
     }
 
 }
