52bbf222bf21e4c1c47f5186bf74bc82ac9c0756
tjdjdjrdl123 <lee-won-suk@github.io>
2025-01-27 17:51:00 +0900

    refactor : CalculatorNumbers 정적 팩토리 처리로 인한 테스트 픽스처 수정

--- src/test/java/stringcalculator/CalculatorNumbersTest.java
+++ src/test/java/stringcalculator/CalculatorNumbersTest.java
@@ -13,14 +13,14 @@ public class CalculatorNumbersTest {
     @DisplayName(value = "컴마나 콜론을 붙이면 숫자의 합을 반환한다.")
     @CsvSource(value = {"1,2,3|6","1:3:5|9"},delimiter = '|')
     void addValueTest(String calculateForm, int result) {
-        CalculatorNumbers calculatorNumbers = new CalculatorNumbers(calculateForm);
+        CalculatorNumbers calculatorNumbers = CalculatorNumbers.createNumbers(calculateForm);
         assertThat(calculatorNumbers.sum()).isEqualTo(result);
     }
 
     @Test
     @DisplayName(value = "커스텀 구분자를 사용해서 숫자의 합을 반환한다.")
     void customAddValueTest() {
-        CalculatorNumbers calculatorNumbers = new CalculatorNumbers("//&\n3&4&5");
+        CalculatorNumbers calculatorNumbers = CalculatorNumbers.createNumbers("//&\n3&4&5");
         assertThat(calculatorNumbers.sum()).isEqualTo(12);
     }
 
