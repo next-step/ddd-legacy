941f3750a8ac3341b4745b03aa4cca7a880eb101
tjdjdjrdl123 <lee-won-suk@github.io>
2025-01-27 17:12:31 +0900

    fix : 테스트케이스의 매직넘버 하드코딩으로 변경처리. 테스트코드에서는 검증값마다 상수처리시 클래스가 비대해질 수 있어서.

--- src/test/java/stringcalculator/CalculatorNumbersTest.java
+++ src/test/java/stringcalculator/CalculatorNumbersTest.java
@@ -9,17 +9,23 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CalculatorNumbersTest {
 
+
+    private static final String CUSTOM_OPERAND = "//&\n3&4&5";
+    public static final int CUSTOM_RESULT = 12;
+
     @ParameterizedTest
     @DisplayName(value = "컴마나 콜론을 붙이면 숫자의 합을 반환한다.")
     @CsvSource(value = {"1,2,3|6","1:3:5|9"},delimiter = '|')
     void addValueTest(String calculateForm, int result) {
-        assertThat(Calculator.calculate(calculateForm)).isEqualTo(result);
+        CalculatorNumbers calculatorNumbers = new CalculatorNumbers(calculateForm);
+        assertThat(calculatorNumbers.sum()).isEqualTo(result);
     }
 
     @Test
     @DisplayName(value = "커스텀 구분자를 사용해서 숫자의 합을 반환한다.")
     void customAddValueTest() {
-        assertThat(Calculator.calculate("//&\n3&4&5")).isEqualTo(12);
+        CalculatorNumbers calculatorNumbers = new CalculatorNumbers(CUSTOM_OPERAND);
+        assertThat(calculatorNumbers.sum()).isEqualTo(CUSTOM_RESULT);
     }
 
 }
