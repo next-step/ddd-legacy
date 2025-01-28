18a98cb3076a77e57db4f887e4168018fa13777c
domo7304 <domo7304@naver.com>
2025-01-29 01:43:59 +0900

    feat(calculator): 커스텀 구분자를 처리할 수 있는 문자열 덧셈 계산기 작성

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,15 +1,30 @@
 package calculator;
 
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class StringCalculator {
 
     public int add(String text) {
         int result = 0;
 
-        String[] numberString = text.split(",|:");
+        String[] numberString = splitNumbers(text);
         result = Arrays.stream(numberString).mapToInt(Integer::parseInt).sum();
 
         return result;
+    }
+
+    public String[] splitNumbers(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        String delimiters = ",|:";
+
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            delimiters = delimiters + "|" + customDelimiter;
+            text = m.group(2);
+        }
+
+        return text.split(delimiters);
     }
 }
