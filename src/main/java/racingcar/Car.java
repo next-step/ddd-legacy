918705341328c3889c8a982c8642f53be9044478
reversalSpring <p_illusion@naver.com>
2025-01-28 21:47:15 +0900

    feat: Add Car class and CarTest with JUnit 5 tests (#794)

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,33 @@
+package racingcar;
+
+import java.util.Random;
+
+public class Car {
+    private static final int MAX_NAME_LENGTH = 5;
+    private String name;
+    private int position;
+
+    public Car(String name) {
+        if (name.length() > MAX_NAME_LENGTH) {
+            throw new IllegalArgumentException("Car name cannot be longer than 5 characters");
+        }
+        this.name = name;
+        this.position = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void move() {
+        Random random = new Random();
+        int randomValue = random.nextInt(10);
+        if (randomValue >= 4) {
+            position++;
+        }
+    }
+}
