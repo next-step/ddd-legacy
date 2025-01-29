5a37e6d31c5e60f3864022d5a13a50e528a6fbdf
Hae-gun <newchk610@gmail.com>
2025-01-29 23:09:18 +0900

    test(MenuService) : MenuService 테스트 일부 작성

---
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -0,0 +1,110 @@
+package kitchenpos.application;
+
+import kitchenpos.application.fixture.MenuFixture;
+import kitchenpos.application.fixture.MenuGroupFixture;
+import kitchenpos.application.fixture.MenuProductFixture;
+import kitchenpos.application.fixture.ProductFixture;
+import kitchenpos.domain.*;
+import kitchenpos.infra.PurgomalumClient;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.extension.*;
+import org.junit.jupiter.params.*;
+import org.junit.jupiter.params.provider.*;
+import org.junit.jupiter.params.shadow.com.univocity.parsers.annotations.*;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+
+@ExtendWith(MockitoExtension.class)
+class MenuServiceTest {
+
+    @Mock
+    private MenuRepository menuRepository;
+    @Mock
+    private MenuGroupRepository menuGroupRepository;
+    @Mock
+    private ProductRepository productRepository;
+    @Mock
+    private PurgomalumClient purgomalumClient;
+
+    @InjectMocks
+    private MenuService menuService;
+
+    @Test
+    @DisplayName("메뉴를 생성한다.")
+    void createMenu() {
+
+        MenuGroup menuGroup = MenuGroupFixture.setMenuGroup("메인메뉴그룹");
+
+        MenuProduct menuProduct =
+                MenuProductFixture.setMenuProduct(
+                        ProductFixture.setProduct("음식1", "10000")
+                        , 1, 1
+                );
+
+        given(menuGroupRepository.findById(any())).willReturn(Optional.of(menuGroup));
+        given(productRepository.findAllByIdIn(any())).willReturn(List.of(menuProduct.getProduct()));
+        given(productRepository.findById(any())).willReturn(Optional.of(menuProduct.getProduct()));
+
+        Menu menu = MenuFixture.setMenuGroup(menuGroup, "메인디쉬", "10000", List.of(menuProduct));
+
+        given(menuRepository.save(any())).willReturn(menu);
+
+        Menu createdMenu = menuService.create(menu);
+
+        assertAll(
+                () -> assertThat(createdMenu.getMenuGroup()).isEqualTo(menuGroup),
+                () -> assertThat(createdMenu.getName()).isEqualTo("메인디쉬"),
+                () -> assertThat(createdMenu.getId()).isInstanceOf(UUID.class),
+                () -> assertThat(createdMenu.getMenuProducts()).containsExactly(menuProduct)
+        );
+    }
+
+    @Test
+    @DisplayName("메뉴는 메뉴그룹을 필수로 가져야 한다.")
+    void throwExceptionWithoutMenuGroup(){
+        MenuProduct menuProduct =
+                MenuProductFixture.setMenuProduct(
+                        ProductFixture.setProduct("음식1", "10000")
+                        , 1, 1
+                );
+
+        Menu menu = MenuFixture.setMenuGroup(null, "메인디쉬", "10000", List.of(menuProduct));
+
+        assertThatThrownBy(() -> menuService.create(menu)).isInstanceOf(NoSuchElementException.class);
+    }
+
+    @DisplayName("가격(price) 은 필수로 입력되어야 하며, 0 이상의 양수값 이어야 한다.")
+    @ParameterizedTest
+    @NullSource
+    @ValueSource(strings = {"-1"})
+    void throwExceptionWhenPriceIsNullOrZero(String value){
+        MenuGroup menuGroup = MenuGroupFixture.setMenuGroup("메인메뉴그룹");
+
+        MenuProduct menuProduct =
+                MenuProductFixture.setMenuProduct(
+                        ProductFixture.setProduct("음식1", "10000")
+                        , 1, 1
+                );
+        Menu menu = MenuFixture.setMenuGroup(menuGroup, "메인디쉬", value, List.of(menuProduct));
+
+        assertThatThrownBy(() -> menuService.create(menu))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+
+
+}
