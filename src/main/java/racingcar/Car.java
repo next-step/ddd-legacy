0210613d5c0d153f7c11851fb58679d2558a7fc1
lim-devv <lim1201mil@gmail.com>
2025-01-29 15:53:42 +0900

    feat: move 테스트 코드추가

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -3,11 +3,22 @@ package racingcar;
 public class Car {
 
     private final String name;
+    private int position;
 
     public Car(String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException();
         }
         this.name = name;
+    }
+
+    public void move(final int condition) {
+        if (condition >= 4) {
+            position++;
+        }
+    }
+
+    public int getPosition() {
+        return position;
     }
 }
--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,9 +1,12 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 
 class CarTest {
@@ -12,5 +15,23 @@ class CarTest {
     @Test
     void constructor() {
         assertThatIllegalArgumentException().isThrownBy(() -> new Car("동해물과백두산이"));
+    }
+
+    @DisplayName("무작위 값이 4 이상이면 자동차가 움직인다.")
+    @ParameterizedTest
+    @ValueSource(ints = {4, 5, 6, 7, 8, 9})
+    void move(final int condition) {
+        Car car = new Car("lim");
+        car.move(condition);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @DisplayName("무작위 값이 4 미만이면 자동차가 정지한다.")
+    @ParameterizedTest
+    @ValueSource(ints = {0, 1, 2, 3})
+    void stop(final int condition) {
+        Car car = new Car("lim");
+        car.move(condition);
+        assertThat(car.getPosition()).isEqualTo(0);
     }
 }
