1da37492a924534e89c2afd531024fd1c524e031
Hae-gun <newchk610@gmail.com>
2025-01-29 23:21:26 +0900

    refactor : 문자열 체크 로직(Object.isNull) -> Strings.isBlank 로 변경

--- src/main/java/kitchenpos/application/MenuService.java
+++ src/main/java/kitchenpos/application/MenuService.java
@@ -1,5 +1,6 @@
 package kitchenpos.application;
 
+import io.micrometer.common.util.StringUtils;
 import kitchenpos.domain.Menu;
 import kitchenpos.domain.MenuGroup;
 import kitchenpos.domain.MenuGroupRepository;
@@ -79,7 +80,7 @@ public class MenuService {
             throw new IllegalArgumentException();
         }
         final String name = request.getName();
-        if (Objects.isNull(name) || purgomalumClient.containsProfanity(name)) {
+        if (StringUtils.isBlank(name) || purgomalumClient.containsProfanity(name)) {
             throw new IllegalArgumentException();
         }
         final Menu menu = new Menu();
