dfba6d9c37950cb93ac7582795871b3577b17868
jay <jaeykweon@naver.com>
2025-01-30 02:17:49 +0900

    feat: 첫번째 요구사항 충족

---
+++ src/main/java/calculator/StringCalculator.java
@@ -0,0 +1,11 @@
+package calculator;
+
+public class StringCalculator  {
+
+    public int add(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+        return 0;
+    }
+}
---
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -0,0 +1,25 @@
+package calculator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCalculatorTest {
+
+    private StringCalculator stringCalculator;
+
+    @BeforeEach
+    void setUp() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @DisplayName(value = "빈 문자열 또는 null 값을 입력할 경우 0을 반환해야 한다.")
+    @ParameterizedTest
+    @NullAndEmptySource
+    public void emptyOrNull(final String text) {
+        assertThat(stringCalculator.add(text)).isZero();
+    }
+}
