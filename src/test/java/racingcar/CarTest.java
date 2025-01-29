a1474f170922633f533f61ef0fc24117357e2bf2
jean lim <157941878+lim-devv@users.noreply.github.com>
2025-01-30 01:26:47 +0900

    Step0 미션입니다. (#801)
    * feat(racingcar): Car 객체 생성
    
    * feat: move 테스트 코드추가
    
    * feat(racingcar) : displayname 수정
    
    * feat(racingcar) : move strategy 추가
    
    * feat(racingcar) : forwarStrategy, StopStrategy 추가

---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,33 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+
+class CarTest {
+
+    @DisplayName("자동차의 이름은 5글자를 초과하면 예외가 발생한다.")
+    @Test
+    void constructor() {
+        assertThatIllegalArgumentException().isThrownBy(() -> new Car("동해물과백두산이"));
+    }
+
+    @DisplayName("자동차는 움직인다.")
+    @Test
+    void move() {
+        Car car = new Car("lim");
+        car.move(new ForwardStrategy());
+        Assertions.assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @DisplayName("자동차는 멈춘다..")
+    @Test
+    void stop() {
+        Car car = new Car("lim");
+        car.move(new StopStrategy());
+        Assertions.assertThat(car.getPosition()).isEqualTo(0);
+    }
+}
