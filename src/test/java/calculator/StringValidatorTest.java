fa2b808feefc930acd65e2cf68ca028aa9011067
domo7304 <domo7304@naver.com>
2025-01-31 17:31:54 +0900

    refactor(calculator): 발생 예상 예외 타입 변경(RuntimeException -> 커스텀 예외 클래스)

--- src/test/java/calculator/StringValidatorTest.java
+++ src/test/java/calculator/StringValidatorTest.java
@@ -2,22 +2,24 @@ package calculator;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
+import exception.InvalidNumberFormatException;
+import exception.NotPositiveNumberException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class StringValidatorTest {
 
-    @DisplayName("음수 문자열을 전달하는 경우 RuntimeException 예외 처리를 한다.")
+    @DisplayName("음수 문자열을 전달하는 경우 NotPositiveNumberException 예외 처리를 한다.")
     @Test
     void negative() {
-        assertThatExceptionOfType(RuntimeException.class)
+        assertThatExceptionOfType(NotPositiveNumberException.class)
             .isThrownBy(() -> StringValidator.validateNumberAndPositive("-1"));
     }
 
-    @DisplayName("숫자가 아닌 문자열을 전달하는 경우 RuntimeException 예외 처리를 한다.")
+    @DisplayName("숫자가 아닌 문자열을 전달하는 경우 InvalidNumberFormatException 예외 처리를 한다.")
     @Test
     void notNumber() {
-        assertThatExceptionOfType(RuntimeException.class)
+        assertThatExceptionOfType(InvalidNumberFormatException.class)
             .isThrownBy(() -> StringValidator.validateNumberAndPositive("notNumber"));
     }
 }
