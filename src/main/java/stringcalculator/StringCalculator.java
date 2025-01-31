0765613c4962c089c8973fe7ba0ab6c536c3f63e
gugbab2 <jkhoo1997@gmail.com>
2025-01-31 22:16:31 +0900

    refactor(StringCalculator): Pattern 객체 재사용

--- src/main/java/stringcalculator/StringCalculator.java
+++ src/main/java/stringcalculator/StringCalculator.java
@@ -5,7 +5,8 @@ import java.util.regex.Pattern;
 
 public class StringCalculator {
 
-    private final String COMMON_SPLITTER = "[,:]";
+    private final String COMMON_DELIMITER_PATTERN = "[,:]";
+    private final Pattern CUSTOM_DELIMITER_PATTERN = Pattern.compile("//(.)\n(.*)");
 
     public int add(String text) {
         if(isEmptyOrNull(text)) {
@@ -25,13 +26,13 @@ public class StringCalculator {
 
     private String[] stringSplit(String text) {
 
-        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        Matcher m = CUSTOM_DELIMITER_PATTERN.matcher(text);
         if (m.find()) {
             String customDelimiter = m.group(1);
             return m.group(2).split(customDelimiter);
         }
 
-        return text.split(COMMON_SPLITTER);
+        return text.split(COMMON_DELIMITER_PATTERN);
     }
 
     private String[] checkNegativeNumbers(String[] numbers) {
