cefea4e13cf0cc9c71d273a4682000f23f581ade
woozi <kwj1270@naver.com>
2025-02-01 09:22:35 +0900

    test(kitchenpos): 메뉴 그룹이 존재하지 않으면 메뉴를 등록할 수 없습니다.

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -1,6 +1,9 @@
 package kitchenpos.application;
 
-import kitchenpos.domain.*;
+import kitchenpos.domain.Menu;
+import kitchenpos.domain.MenuGroupRepository;
+import kitchenpos.domain.MenuRepository;
+import kitchenpos.domain.ProductRepository;
 import kitchenpos.fixture.MenuFixture;
 import kitchenpos.fixture.MenuGroupFixture;
 import kitchenpos.fixture.MenuProductFixture;
@@ -17,8 +20,13 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
+import static kitchenpos.fixture.MenuGroupFixture.*;
+import static kitchenpos.fixture.MenuProductFixture.*;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
@@ -34,7 +42,7 @@ class MenuServiceTest {
 
     @Autowired
     @MockBean
-    ProductRepository productRepositor;
+    ProductRepository productRepository;
 
     @Autowired
     @MockBean
@@ -56,9 +64,22 @@ class MenuServiceTest {
     @ValueSource(strings = {"-1", "-1000", "-100000"})
     void createWithNegativePrice(String price) {
         final Menu menu = MenuFixture.menu(null, "양념 후라이드 세트", new BigDecimal(price),
-                MenuGroupFixture.menuGroup(), List.of(MenuProductFixture.menuProduct()), true
+                menuGroup(), List.of(menuProduct()), true
         );
         assertThatThrownBy(() -> menuService.create(menu))
                 .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @DisplayName("메뉴 그룹이 존재하지 않으면 메뉴를 등록할 수 없습니다.")
+    @Test
+    void createWithNotExistsMenuGroup() {
+        final Menu menu = MenuFixture.menu(null, "양념 후라이드 세트", new BigDecimal("30000"),
+                menuGroup(), List.of(menuProduct()), true
+        );
+        when(menuGroupRepository.findById(menu.getMenuGroupId()))
+                .thenReturn(Optional.empty());
+
+        assertThatThrownBy(() -> menuService.create(menu))
+                .isInstanceOf(NoSuchElementException.class);
     }
 }
