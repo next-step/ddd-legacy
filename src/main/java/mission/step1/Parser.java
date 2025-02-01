e0bf93d6247412fcafdcf4e0155c702b375bb4b3
JongMinCh0i <wonderland314592@gmail.com>
2025-02-01 10:22:15 +0900

    feat : 커스텀 문자가 아닌 기본 입력 파라미터의 첫 번째 문자가 숫자가 아닐 경우 예외처리

--- src/main/java/mission/step1/Parser.java
+++ src/main/java/mission/step1/Parser.java
@@ -5,6 +5,14 @@ public class Parser implements ParserStrategy {
 
     @Override
     public String[] splitWithDelimiter(String input) {
+        isDefaultParser(input);
         return input.split(DELIMITER);
+    }
+
+    public void isDefaultParser(String input) {
+        Character c = input.charAt(0);
+        if (!Character.isDigit(c)) {
+            throw new RuntimeException("커스텀 구분자 형식이 올바르지 않습니다");
+        }
     }
 }
