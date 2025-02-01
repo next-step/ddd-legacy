779d26365cbd67909047a2d9301a4a085afb02d0
woozi <kwj1270@naver.com>
2025-02-01 09:13:42 +0900

    test(kitchenpos): 메뉴의 가격은 0 이상이어야 합니다.

--- src/test/java/kitchenpos/application/MenuServiceTest.java
+++ src/test/java/kitchenpos/application/MenuServiceTest.java
@@ -1,18 +1,24 @@
 package kitchenpos.application;
 
-import kitchenpos.domain.Menu;
-import kitchenpos.domain.MenuGroupRepository;
-import kitchenpos.domain.MenuRepository;
-import kitchenpos.domain.ProductRepository;
+import kitchenpos.domain.*;
 import kitchenpos.fixture.MenuFixture;
+import kitchenpos.fixture.MenuGroupFixture;
+import kitchenpos.fixture.MenuProductFixture;
 import kitchenpos.infra.PurgomalumClient;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
@@ -43,5 +49,16 @@ class MenuServiceTest {
         final Menu menu = MenuFixture.menu();
 
         menuService.create(menu);
+    }
+
+    @DisplayName("메뉴 가격은 0원 이상이어야 합니다.")
+    @ParameterizedTest(name = "입력값 `{0}`")
+    @ValueSource(strings = {"-1", "-1000", "-100000"})
+    void createWithNegativePrice(String price) {
+        final Menu menu = MenuFixture.menu(null, "양념 후라이드 세트", new BigDecimal(price),
+                MenuGroupFixture.menuGroup(), List.of(MenuProductFixture.menuProduct()), true
+        );
+        assertThatThrownBy(() -> menuService.create(menu))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
