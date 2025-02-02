208c92e0c496b2eb93fa9733a76ea3762655485d
domo7304 <domo7304@naver.com>
2025-02-02 17:34:45 +0900

    refactor(calculator): 테스트코드 expected 값 선언 없이 직접 사용하도록 변경

--- src/test/java/calculator/StringParserTest.java
+++ src/test/java/calculator/StringParserTest.java
@@ -13,32 +13,28 @@ class StringParserTest {
     @ParameterizedTest
     @ValueSource(strings = {"1,2,3"})
     void comma(final String text) {
-        String[] expected = {"1", "2", "3"};
-        assertThat(StringParser.splitNumbers(text)).containsExactly(expected);
+        assertThat(StringParser.splitNumbers(text)).containsExactly("1", "2", "3");
     }
 
     @DisplayName("콜론이 포함된 문자열을 파싱할 수 있다.")
     @ParameterizedTest
     @ValueSource(strings = {"1:2:3"})
     void colon(final String text) {
-        String[] expected = {"1", "2", "3"};
-        assertThat(StringParser.splitNumbers(text)).containsExactly(expected);
+        assertThat(StringParser.splitNumbers(text)).containsExactly("1", "2", "3");
     }
 
     @DisplayName("콤마와 콜론이 포함된 문자열을 파싱할 수 있다.")
     @ParameterizedTest
     @ValueSource(strings = {"1,2:3", "1:2,3"})
     void colonsAndCommas(final String text) {
-        String[] expected = {"1", "2", "3"};
-        assertThat(StringParser.splitNumbers(text)).containsExactly(expected);
+        assertThat(StringParser.splitNumbers(text)).containsExactly("1", "2", "3");
     }
 
     @DisplayName("//와 \\n 문자 사이의 커스텀 구분자를 통해 문자열을 파싱할 수 있다.")
     @ParameterizedTest
     @ValueSource(strings = {"//;\\n1;2;3"})
     void customDelimiter(final String text) {
-        String[] expected = {"1", "2", "3"};
-        assertThat(StringParser.splitNumbers(text)).containsExactly(expected);
+        assertThat(StringParser.splitNumbers(text)).containsExactly("1", "2", "3");
     }
 
     @DisplayName("콤마, 콜론과 커스텀 구분자가 포함된 문자열을 파싱할 수 있다.")
@@ -48,7 +44,6 @@ class StringParserTest {
         """)
     @ValueSource(strings = {"//;\\n1,2:3;4"})
     void commaAndColonAndCustomDelimiter(final String text) {
-        String[] expected = {"1", "2", "3", "4"};
-        assertThat(StringParser.splitNumbers(text)).containsExactly(expected);
+        assertThat(StringParser.splitNumbers(text)).containsExactly("1", "2", "3", "4");
     }
 }
