18866893597f72977c859572e4eb40439df1a8eb
hoon25 <cchoon95@hanmail.net>
2025-02-03 18:20:11 +0900

    test(Cucumber): RestAssured 사용에 따른 DB초기화 적용

--- src/test/kotlin/kitchenpos/utils/CucumberTest.kt
+++ src/test/kotlin/kitchenpos/utils/CucumberTest.kt
@@ -3,7 +3,8 @@ package kitchenpos.utils
 import io.cucumber.spring.CucumberContextConfiguration
 import kitchenpos.Application
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment.DEFINED_PORT
 
 @CucumberContextConfiguration
-@SpringBootTest(classes = [Application::class])
+@SpringBootTest(classes = [Application::class], webEnvironment = DEFINED_PORT)
 class CucumberTest
