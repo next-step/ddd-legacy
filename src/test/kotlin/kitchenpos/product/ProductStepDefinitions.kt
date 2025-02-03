aa72fcc31425d022d92d3ebab2d00cec5528ae59
hoon25 <cchoon95@hanmail.net>
2025-02-03 20:42:00 +0900

    test(Product): 가격 수정 테스트 추가

--- src/test/kotlin/kitchenpos/product/ProductStepDefinitions.kt
+++ src/test/kotlin/kitchenpos/product/ProductStepDefinitions.kt
@@ -8,6 +8,7 @@ import io.restassured.RestAssured
 import kitchenpos.domain.Product
 import kitchenpos.utils.CucumberTest
 import org.assertj.core.api.Assertions.assertThat
+import java.math.BigDecimal
 
 
 class ProductStepDefinitions : CucumberTest() {
@@ -16,6 +17,14 @@ class ProductStepDefinitions : CucumberTest() {
     @Before
     fun setUp() {
         product = ProductFixture.fixture()
+    }
+
+    @Given("상품 이름은 {productName} 이고 상품 가격은 {int}원인 상품이 등록되어 있다")
+    fun 상품생성(name: String?, price: Int) {
+        product = ProductFixture.fixture(name = name, price = price.toBigDecimal())
+        RestAssured
+            .given().body(product).contentType("application/json")
+            .`when`().post("/api/products")
     }
 
     @Given("상품 이름은 {productName} 이고 상품 가격은 {int}원")
@@ -30,12 +39,22 @@ class ProductStepDefinitions : CucumberTest() {
             .`when`().post("/api/products")
     }
 
-    @Then("상품 생성 성공")
-    fun 상품생성성공() {
-        val products = RestAssured
-            .get("/api/products")
-            .then().extract().`as`(Array<Product>::class.java)
-        assertThat(products).hasSize(1)
+    @When("상품 이름이 {productName}인 상품의 가격을 {int}원으로 수정한다")
+    fun 상품이름은string이고상품가격은int원으로수정한다(name: String?, price: Int) {
+        val product = 상품이름으로_상품_조회(name!!)
+        product.price = price.toBigDecimal()
+        RestAssured
+            .given().body(product).contentType("application/json")
+            .`when`().put("/api/products/${product.id}/price")
+    }
+
+    @Then("상품 이름은 {productName} 이고 상품 가격은 {int}원이다")
+    fun 상품이름은string이고상품가격은int원이다(name: String?, price: Int) {
+        val product = 상품이름으로_상품_조회(name!!)
+        assertThat(product.name).isEqualTo(name)
+        assertThat(product.price)
+            .usingComparator(BigDecimal::compareTo)
+            .isEqualTo(price.toBigDecimal())
     }
 
     @Then("상품 생성 실패")
@@ -44,5 +63,12 @@ class ProductStepDefinitions : CucumberTest() {
             .get("/api/products")
             .then().extract().`as`(Array<Product>::class.java)
         assertThat(products).hasSize(0)
+    }
+
+    private fun 상품이름으로_상품_조회(name: String): Product {
+        return RestAssured
+            .get("/api/products")
+            .then().log().all().extract().`as`(Array<Product>::class.java)
+            .find { it.name == name }!!
     }
 }
