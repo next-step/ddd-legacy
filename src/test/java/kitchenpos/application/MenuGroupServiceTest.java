98a14afb945ca2db23816ebcfe0a9bb37d5dedbe
skdev0619 <skdev0619@naver.com>
2025-02-03 16:05:36 +0900

    메뉴그룹(MenuGroup) 테스트 코드 추가

---
+++ src/test/java/kitchenpos/application/MenuGroupServiceTest.java
@@ -0,0 +1,64 @@
+package kitchenpos.application;
+
+import kitchenpos.domain.MenuGroup;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
+
+@Transactional
+@SpringBootTest
+class MenuGroupServiceTest {
+
+    @Autowired
+    private MenuGroupService menuGroupService;
+
+    @DisplayName("메뉴그룹명을 입력하여 메뉴그룹을 등록한다")
+    @Test
+    void create() {
+        MenuGroup request = createMenuGroup("세트메뉴");
+
+        MenuGroup menuGroup = menuGroupService.create(request);
+
+        assertThat(menuGroup.getName()).isEqualTo("세트메뉴");
+    }
+
+    @DisplayName("메뉴그룹명은 반드시 입력되어야 하며 공백만 입력할 수 없다")
+    @NullAndEmptySource
+    @ParameterizedTest
+    void validateName(String name) {
+        MenuGroup request = createMenuGroup(name);
+
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> menuGroupService.create(request));
+    }
+
+    @DisplayName("모든 메뉴 그룹을 조회할 수 있다")
+    @Test
+    void findAll() {
+        menuGroupService.create(createMenuGroup("버거"));
+        menuGroupService.create(createMenuGroup("세트메뉴"));
+        menuGroupService.create(createMenuGroup("사이드"));
+
+        List<MenuGroup> menuGroups = menuGroupService.findAll();
+
+        assertThat(menuGroups.size()).isEqualTo(3);
+        assertThat(menuGroups).extracting(MenuGroup::getName)
+                .contains("버거", "세트메뉴", "사이드");
+
+    }
+
+    private MenuGroup createMenuGroup(String name) {
+        MenuGroup menuGroup = new MenuGroup();
+        menuGroup.setName(name);
+        return menuGroup;
+    }
+}
