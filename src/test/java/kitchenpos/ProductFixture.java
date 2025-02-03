4050b3d9231c22d9d888c19bd086758fad37069f
nomoreFt <hyunwoo.kim1108@gmail.com>
2025-02-03 15:35:44 +0900

    test(kitchenpos): 메뉴 Test 및 Fixture 생성 완료

--- src/test/java/kitchenpos/ProductFixture.java
+++ src/test/java/kitchenpos/ProductFixture.java
@@ -12,6 +12,14 @@ public class ProductFixture {
     private static final IdGenerator productIdGenerator = UUID::randomUUID;
     private static final PriceGenerator priceGenerator = BigDecimal::new;
 
+    public static Product 양념_치킨_상품(){
+        Product product = new Product();
+        product.setId(productIdGenerator.ramdom());
+        product.setName("양념 치킨");
+        product.setPrice(priceGenerator.of(17000));
+        return product;
+    }
+
     public static Product 후라이드_치킨_상품(){
         Product product = new Product();
         product.setId(productIdGenerator.ramdom());
