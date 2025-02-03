bceced80fc7f5b4a7481816c054825ee758946d4
woozi <kwj1270@naver.com>
2025-02-03 12:40:28 +0900

    test(kitchenpos): 가게 테이블의 이름이 이름이 없거나 비어있으면 가게 테이블을 생성할 수 없습니다.

--- src/test/java/kitchenpos/application/OrderTableServiceTest.java
+++ src/test/java/kitchenpos/application/OrderTableServiceTest.java
@@ -5,6 +5,8 @@ import kitchenpos.domain.OrderTableRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static kitchenpos.fixture.OrderTableFixture.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.AdditionalAnswers.returnsFirstArg;
@@ -45,5 +48,15 @@ class OrderTableServiceTest {
                 () -> assertThat(orderTable.getNumberOfGuests()).isEqualTo(DEFAULT_NUMBER_OF_GUESTS),
                 () -> assertThat(orderTable.isOccupied()).isEqualTo(DEFAULT_OCCUPIED)
         );
+    }
+
+    @DisplayName("가게 테이블의 이름이 이름이 없거나 비어있으면 가게 테이블을 생성할 수 없습니다.")
+    @ParameterizedTest(name = "입력값 {0}")
+    @NullAndEmptySource
+    void createOrderTableWithEmptyName(final String name) {
+        final OrderTable orderTable = orderTable(name, DEFAULT_NUMBER_OF_GUESTS, DEFAULT_OCCUPIED);
+
+        assertThatThrownBy(() -> orderTableService.create(orderTable))
+                .isInstanceOf(IllegalArgumentException.class);
     }
 }
