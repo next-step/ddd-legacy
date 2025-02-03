4c4ddc2b5fb3b2b705429b8a55d2cf6806ba0c67
woozi <kwj1270@naver.com>
2025-02-04 00:16:47 +0900

    test(kitchenpos): 배달 주문을 생성할 때 메뉴가 미노출 상태이면 예외가 발생합니다

--- src/test/java/kitchenpos/application/DeliveryOrderServiceTest.java
+++ src/test/java/kitchenpos/application/DeliveryOrderServiceTest.java
@@ -211,5 +211,17 @@ class DeliveryOrderServiceTest {
                     deliveryOrder(null, null, deliverAddress, OrderStatus.WAITING, List.of(orderLineItem))
             )).isInstanceOf(IllegalArgumentException.class);
         }
+
+        @DisplayName("메뉴가 미노출 상태이면 예외가 발생합니다")
+        @Test
+        void createOrderWithNonDisplayedMenu() {
+            final Menu nonDisplayedMenu = menu(menu.getId(), menu.getName(), menu.getPrice(), menu.getMenuGroup(), menu.getMenuProducts(), false);
+            when(menuRepository.findAllByIdIn(anyList())).thenReturn(List.of(nonDisplayedMenu));
+            when(menuRepository.findById(nonDisplayedMenu.getId())).thenReturn(Optional.ofNullable(nonDisplayedMenu));
+
+            assertThatThrownBy(() -> orderService.create(
+                    deliveryOrder(null, null, deliverAddress, OrderStatus.WAITING, List.of(orderLineItem))
+            )).isInstanceOf(IllegalStateException.class);
+        }
     }
 }
