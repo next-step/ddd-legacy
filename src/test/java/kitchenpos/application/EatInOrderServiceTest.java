d02876504d4804707c74096202fc7599bd07f17a
woozi <kwj1270@naver.com>
2025-02-03 23:56:04 +0900

    test(kitchenpos): 매장 식사 주문을 완료할 때 서빙 상태의 주문을 완료할 수 있습니다

--- src/test/java/kitchenpos/application/EatInOrderServiceTest.java
+++ src/test/java/kitchenpos/application/EatInOrderServiceTest.java
@@ -284,6 +284,41 @@ class EatInOrderServiceTest {
         }
     }
 
+    @DisplayName("매장 식사 주문을 완료할 때")
+    @Nested
+    class Complete {
+
+        private Menu menu;
+        private long quantity;
+        private BigDecimal price;
+        private OrderLineItem orderLineItem;
+        private OrderTable orderTable;
+        private Order order;
+
+        @BeforeEach
+        void setUp() {
+            this.menu = menu();
+            this.quantity = 1L;
+            this.price = orderLineItemPrice(menu.getPrice(), quantity);
+            this.orderLineItem = orderLineItem(1L, menu, quantity, price);
+            this.orderTable = orderTable(createOrderTableId(), DEFAULT_ORDER_TABLE_NAME, 2, true);
+            this.order = eatInOrder(UUID.randomUUID(), LocalDateTime.now(), orderTable, OrderStatus.SERVED, List.of(orderLineItem));
+        }
+
+        @DisplayName("서빙 상태의 주문을 완료할 수 있습니다")
+        @Test
+        void completeOrder() {
+            when(orderRepository.findById(order.getId())).thenReturn(Optional.of(order));
+
+            final Order actual = orderService.complete(order.getId());
+
+            assertAll(
+                    () -> assertThat(actual.getId()).isEqualTo(order.getId()),
+                    () -> assertThat(actual.getStatus()).isEqualTo(OrderStatus.COMPLETED)
+            );
+        }
+    }
+
 
     private BigDecimal orderLineItemPrice(final BigDecimal price, final long quantity) {
         return price.multiply(BigDecimal.valueOf(quantity));
