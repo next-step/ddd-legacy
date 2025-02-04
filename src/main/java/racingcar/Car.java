abf6fb3edabaf8d22fb33e349b105ec93db691e7
윤태한 <work.taehan.yoon@gmail.com>
2025-02-04 17:39:36 +0900

    [step0] feat(car): define car

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,4 +2,12 @@ package racingcar;
 
 public class Car {
 
+    private String name;
+
+    public Car(String name) {
+        if (name.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+        this.name = name;
+    }
 }
