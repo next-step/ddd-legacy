20d8027fea3e4dd9ba73499082e3d129163d46b3
woozi <kwj1270@naver.com>
2025-02-04 12:28:45 +0900

    test(kitchenpos): 포장 주문을 생성할 때 메뉴가 존재하지 않으면 예외가 발생합니다

--- src/test/java/kitchenpos/application/TaekOutOrderServiceTest.java
+++ src/test/java/kitchenpos/application/TaekOutOrderServiceTest.java
@@ -17,6 +17,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Optional;
 
 import static kitchenpos.fixture.MenuFixture.createMenuId;
@@ -157,6 +158,26 @@ class TaekOutOrderServiceTest {
             assertThatThrownBy(() -> orderService.create(
                     takeoutOrder(null, null, OrderStatus.WAITING, List.of(negativeOrderLineItem))
             )).isInstanceOf(IllegalArgumentException.class);
+        }
+
+        @DisplayName("메뉴가 존재하지 않으면 예외가 발생합니다")
+        @Test
+        void createOrderWithNonExistentMenu() {
+            final Menu nonExistentMenu = menu(
+                    createMenuId(),
+                    "nonExistentMenu",
+                    BigDecimal.valueOf(10000),
+                    menuGroup(),
+                    List.of(menuProduct()),
+                    true
+            );
+            final OrderLineItem nonExistentOrderLineItem = orderLineItem(null, nonExistentMenu, 1L, nonExistentMenu.getPrice());
+            when(menuRepository.findAllByIdIn(anyList())).thenReturn(List.of(menu));
+            when(menuRepository.findById(nonExistentMenu.getId())).thenReturn(Optional.empty());
+
+            assertThatThrownBy(() -> orderService.create(
+                    takeoutOrder(null, null, OrderStatus.WAITING, List.of(nonExistentOrderLineItem))
+            )).isInstanceOf(NoSuchElementException.class);
         }
     }
 
