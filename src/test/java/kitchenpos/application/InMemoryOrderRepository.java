04597b5fd3dcb6ffbcd0d9608865b6fad5e00497
tjdjdjrdl123 <lee-won-suk@github.io>
2025-02-04 23:59:14 +0900

    feat: 서비스 리포지토리를 구현한 가짜객체 추가

---
+++ src/test/java/kitchenpos/application/InMemoryOrderRepository.java
@@ -0,0 +1,34 @@
+package kitchenpos.application;
+
+import kitchenpos.domain.Order;
+import kitchenpos.domain.OrderRepository;
+import kitchenpos.domain.OrderStatus;
+import kitchenpos.domain.OrderTable;
+
+import java.util.*;
+
+public class InMemoryOrderRepository implements OrderRepository {
+    private final Map<UUID, Order> orders = new HashMap<>();
+
+    @Override
+    public Order save(Order order) {
+        orders.put(order.getId(), order);
+        return order;
+    }
+
+    @Override
+    public Optional<Order> findById(UUID orderId) {
+        return Optional.ofNullable(orders.get(orderId));
+    }
+
+    @Override
+    public boolean existsByOrderTableAndStatusNot(OrderTable orderTable, OrderStatus orderStatus) {
+        return orders.values().stream()
+                .anyMatch(order -> order.getOrderTable().equals(orderTable) && order.getStatus().equals(orderStatus));
+    }
+
+    @Override
+    public List<Order> findAll() {
+        return new ArrayList<>(orders.values());
+    }
+}
