b6e60060e0088ef685c92ced1c777945c1fc403e
윤태한 <work.taehan.yoon@gmail.com>
2025-02-04 17:56:05 +0900

    [step0] feat(car): define move

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -2,12 +2,23 @@ package racingcar;
 
 public class Car {
 
-    private String name;
+    private final String name;
+    private int position;
 
     public Car(String name) {
         if (name.length() > 5) {
             throw new IllegalArgumentException();
         }
         this.name = name;
+    }
+
+    public int position() {
+        return this.position;
+    }
+
+    public void move(int value) {
+        if (value >= 4) {
+            this.position++;
+        }
     }
 }
