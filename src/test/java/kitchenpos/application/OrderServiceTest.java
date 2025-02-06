a6339fd6701bba671fd926e7bf748bdeece56ddd
dongock <okdong23@naver.com>
2025-02-06 21:37:13 +0900

    test(pos) : OrderServiceTest - acceptTakeOutOrder() 생성

--- src/test/java/kitchenpos/application/OrderServiceTest.java
+++ src/test/java/kitchenpos/application/OrderServiceTest.java
@@ -139,6 +139,28 @@ class OrderServiceTest {
         assertThat(resultOrder.getStatus()).isEqualTo(OrderStatus.ACCEPTED);
     }
 
+    @DisplayName("테이크 아웃 주문을 수락할 수 있다.")
+    @Test
+    void acceptTakeOutOrder() {
+        // given
+        Product product = makeTestProduct("짜장면", BigDecimal.valueOf(5000));
+        MenuProduct menuProduct = makeTestMenuProduct(product);
+        MenuGroup menuGroup = makeTestMenuGroup("추천메뉴");
+        Menu menu = makeTestMenu("중식", BigDecimal.valueOf(5000), menuGroup, menuProduct);
+
+        OrderLineItem orderLineItem = makeTestOrderLineItem(BigDecimal.valueOf(5000), menu, 1);
+
+        Order eatInOrder = makeTestTakeOutOrder(OrderStatus.WAITING, orderLineItem);
+
+        when(orderRepository.findById(any())).thenReturn(Optional.of(eatInOrder));
+
+        // when
+        Order resultOrder = orderService.accept(eatInOrder.getId());
+
+        // then
+        assertThat(resultOrder.getStatus()).isEqualTo(OrderStatus.ACCEPTED);
+    }
+
     @Test
     void serve() {
     }
