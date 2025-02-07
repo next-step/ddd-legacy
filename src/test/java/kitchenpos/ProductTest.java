fe20e7c7fab8a547ce8102b01e107ccef4713db4
leewonsuk <tjdjdjrdl123@naver.com>
2025-02-08 02:06:09 +0900

    fix: 상품테스트 수정

--- src/test/java/kitchenpos/ProductTest.java
+++ src/test/java/kitchenpos/ProductTest.java
@@ -1,6 +1,6 @@
 package kitchenpos;
 
-import kitchenpos.application.ProductService;
+import kitchenpos.application.*;
 import kitchenpos.domain.*;
 import kitchenpos.infra.PurgomalumClient;
 import org.junit.jupiter.api.BeforeEach;
@@ -11,10 +11,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mockito;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.SpyBean;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.math.BigDecimal;
@@ -25,10 +21,9 @@ import java.util.UUID;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 import static org.assertj.core.api.ThrowableAssert.ThrowingCallable;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
-@SpringBootTest
+
 @DisplayName(value = "ProductService 테스트")
 @Sql(value = "/delete.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
 public class ProductTest {
@@ -41,14 +36,24 @@ public class ProductTest {
     public static final UUID 후라이드치킨_MENU_UUID = UUID.fromString("f59b1e1c-b145-440a-aa6f-6095a0e2d63b");
     public static final UUID 후라이드치킨_MENU_GROUP_UUID = UUID.fromString("cbc75fae-feb0-4bb1-8be2-cb8ce5d8fded");
     private static final String 한마리메뉴_MENU_GROUP_NAME = "한마리메뉴";
+    public static final String 비속어_NAME = "비속어";
 
 
     private ProductService productService;
-    @SpyBean
     private ProductRepository productRepository;
     private PurgomalumClient purgomalumClient;
     private MenuRepository menuRepository;
     private MenuGroupRepository menuGroupRepository;
+
+
+    @BeforeEach
+    void setUp() {
+        productRepository = spy(new InMemoryProductRepository());
+        menuGroupRepository = spy(new InMemoryMenuGroupRepository());
+        menuRepository = spy(new InMemoryMenuRepository());
+        purgomalumClient = spy(new FakeDefaultPurgomalumClient());
+        productService = spy(new ProductService(productRepository, menuRepository, purgomalumClient));
+    }
 
     @DisplayName(value = "상품 등록 기능")
     @Nested
@@ -59,19 +64,16 @@ public class ProductTest {
         void createProduct() {
             //상품명이나 가격이 없을때 에러처리
             Product product = ProductTest.createProduct(후라이드치킨_PRODUCT_UUID, "", BIG_DECIMAL_MINUS_ONE);
-
             ThrowingCallable throwingCallable = () -> productService.create(product);
             assertThatIllegalArgumentException().isThrownBy(throwingCallable);
         }
 
         @DisplayName(value = "상품명이 없거나 비속어가 있으면 안됩니다.")
-        @ParameterizedTest
-        @CsvSource(value = {",", "fucking 맛있는 치킨"})
-        void productInvalidName(final String productName) {
+        @Test
+        void productInvalidName() {
             //상품명에 빈값이나 비속어가 들어간 경우
-            Product product = ProductTest.createProduct(후라이드치킨_PRODUCT_UUID, productName, BigDecimal.ONE);
-            Mockito.when(purgomalumClient.containsProfanity(productName)).thenReturn(true);
-
+            Product product = ProductTest.createProduct(후라이드치킨_PRODUCT_UUID, 비속어_NAME, BigDecimal.ONE);
+            Mockito.when(purgomalumClient.containsProfanity(Mockito.anyString())).thenReturn(true);
             //에러처리
             ThrowingCallable throwingCallable = () -> productService.create(product);
             assertThatIllegalArgumentException().isThrownBy(throwingCallable);
@@ -96,34 +98,20 @@ public class ProductTest {
     class ProductPriceChangeTest {
 
 
-
-        @BeforeEach
-        void initialize() {
-            Product product = createProduct(ProductTest.후라이드치킨_PRODUCT_UUID, TEST_PRODUCT_NAME, 후라이드치킨_DEFAULT_PRICE);
-            productRepository.save(product);
-            MenuGroup menuGroup = createMenuGroup(한마리메뉴_MENU_GROUP_NAME,후라이드치킨_MENU_GROUP_UUID);
-            menuGroupRepository.save(menuGroup);
-            List<MenuProduct> menuProducts = List.of(createMenuProduct(product, 1));
-            Menu menu = createMenu(후라이드치킨_MENU_UUID, 후라이드치킨_MENU_NAME, 후라이드치킨_DEFAULT_PRICE, menuGroup, menuProducts, 후라이드치킨_MENU_GROUP_UUID);
-            menuRepository.save(menu);
-        }
-
-
         @DisplayName(value = "상품의 가격을 변경합니다")
         @Test
         void changeProductPrice() {
 
-            Product product = productRepository.findById(후라이드치킨_PRODUCT_UUID)
-                    .orElseThrow(NoSuchElementException::new);
-            //상품 금액 변경
+            Product product = createProduct();
+            productRepository.save(product);
+            MenuProduct menuProduct = createMenuProduct(product, 1);
+            Menu menu = createMenu(createMenuGroup(), menuProduct);
+            menuRepository.save(menu);
+
             product.setPrice(BigDecimal.valueOf(21000));
             productService.changePrice(product.getId(), product);
 
-            //비노출 처리되었는지 확인
-            Menu menu = menuRepository.findById(후라이드치킨_MENU_UUID)
-                    .orElseThrow(NoSuchElementException::new);
-
-            assertThat(menu.isDisplayed()).isTrue();
+            assertThat(menuRepository.findById(후라이드치킨_MENU_UUID).orElseThrow().isDisplayed()).isTrue();
 
         }
 
@@ -131,8 +119,7 @@ public class ProductTest {
         @Test
         void zeroProductPrice() {
             //상품명이나 가격이 없을때 에러처리
-            Product product = createProduct(ProductTest.후라이드치킨_PRODUCT_UUID, TEST_PRODUCT_NAME, BIG_DECIMAL_MINUS_ONE);
-
+            Product product = createProduct(BIG_DECIMAL_MINUS_ONE);
             ThrowingCallable throwingCallable = () -> productService.changePrice(ProductTest.후라이드치킨_PRODUCT_UUID, product);
             assertThatIllegalArgumentException().isThrownBy(throwingCallable);
         }
@@ -140,39 +127,37 @@ public class ProductTest {
         @DisplayName(value = "메뉴의 가격이 메뉴의 상품들의 총 가격 합보다 크면, 비노출처리합니다")
         @Test
         void invalidTotalProductPrice() {
-
-            Product product = productRepository.findById(후라이드치킨_PRODUCT_UUID)
-                    .orElseThrow(NoSuchElementException::new);
-            //상품 금액 변경
+            Product product = createProduct();
+            productRepository.save(product);
+            Menu menu = createMenu(createMenuGroup(), createMenuProduct(product, 1));
+            menuRepository.save(menu);
             product.setPrice(BigDecimal.valueOf(19000));
+
             productService.changePrice(product.getId(), product);
-
-            //비노출 처리되었는지 확인
-            Menu menu = menuRepository.findById(후라이드치킨_MENU_UUID)
-                    .orElseThrow(NoSuchElementException::new);
-
-            assertThat(menu.isDisplayed()).isFalse();
-
+            assertThat(menuRepository.findById(후라이드치킨_MENU_UUID).orElseThrow().isDisplayed()).isFalse();
         }
     }
 
     @DisplayName(value = "모든 상품 조회 기능")
     @Nested
     class AllProductFindTest {
-        @BeforeEach
-        void initialize() {
-            Product product = createProduct(ProductTest.후라이드치킨_PRODUCT_UUID, TEST_PRODUCT_NAME, 후라이드치킨_DEFAULT_PRICE);
-            productRepository.save(product);
-        }
-
         @DisplayName(value = "모든 상품을 조회합니다")
         @Test
         void changeProductPrice() {
+            Product product = createProduct();
+            productRepository.save(product);
             List<Product> products = productService.findAll();
-
             verify(productRepository, times(1)).findAll();
             assertThat(products.size()).isEqualTo(1);
         }
+    }
+
+    private Product createProduct() {
+        Product product = new Product();
+        product.setId(후라이드치킨_PRODUCT_UUID);
+        product.setName(TEST_PRODUCT_NAME);
+        product.setPrice(후라이드치킨_DEFAULT_PRICE);
+        return product;
     }
 
     private MenuProduct createMenuProduct(Product product, int quantity) {
@@ -180,6 +165,14 @@ public class ProductTest {
         menuProduct.setProduct(product);
         menuProduct.setQuantity(quantity);
         return menuProduct;
+    }
+
+    private Product createProduct(BigDecimal bigDecimalMinusOne) {
+        Product product = new Product();
+        product.setId(후라이드치킨_PRODUCT_UUID);
+        product.setName(TEST_PRODUCT_NAME);
+        product.setPrice(BIG_DECIMAL_MINUS_ONE);
+        return product;
     }
 
 
@@ -191,11 +184,30 @@ public class ProductTest {
         return product;
     }
 
+    private static MenuGroup createMenuGroup() {
+        MenuGroup menuGroup = new MenuGroup();
+        menuGroup.setName(한마리메뉴_MENU_GROUP_NAME);
+        menuGroup.setId(후라이드치킨_MENU_GROUP_UUID);
+        return menuGroup;
+    }
+
     private static MenuGroup createMenuGroup(String name, UUID id) {
         MenuGroup menuGroup = new MenuGroup();
         menuGroup.setName(name);
         menuGroup.setId(id);
         return menuGroup;
+    }
+
+    private static Menu createMenu(MenuGroup menuGroup, MenuProduct menuProducts) {
+        Menu menu = new Menu();
+        menu.setId(후라이드치킨_MENU_UUID);
+        menu.setName(후라이드치킨_MENU_NAME);
+        menu.setPrice(후라이드치킨_DEFAULT_PRICE);
+        menu.setMenuGroup(menuGroup);
+        menu.setDisplayed(true);
+        menu.setMenuProducts(List.of(menuProducts));
+        menu.setMenuGroupId(후라이드치킨_MENU_GROUP_UUID);
+        return menu;
     }
 
     private static Menu createMenu(UUID id, String name, BigDecimal price, MenuGroup menuGroup, List<MenuProduct> menuProducts, UUID menuGroupId) {
