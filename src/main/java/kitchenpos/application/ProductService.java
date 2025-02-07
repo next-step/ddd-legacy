51e0d0305595ec29fe55c3aed8d473ea59d26991
leewonsuk <tjdjdjrdl123@naver.com>
2025-02-08 02:05:52 +0900

    fix: 클래스 명칭 오수정 변경

--- src/main/java/kitchenpos/application/ProductService.java
+++ src/main/java/kitchenpos/application/ProductService.java
@@ -6,6 +6,7 @@ import kitchenpos.domain.MenuRepository;
 import kitchenpos.domain.Product;
 import kitchenpos.domain.ProductRepository;
 import kitchenpos.infra.DefaultPurgomalumClient;
+import kitchenpos.infra.PurgomalumClient;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,16 +20,16 @@ import java.util.UUID;
 public class ProductService {
     private final ProductRepository productRepository;
     private final MenuRepository menuRepository;
-    private final DefaultPurgomalumClient defaultPurgomalumClient;
+    private final PurgomalumClient purgomalumClient;
 
     public ProductService(
         final ProductRepository productRepository,
         final MenuRepository menuRepository,
-        final DefaultPurgomalumClient defaultPurgomalumClient
+        final PurgomalumClient purgomalumClient
     ) {
         this.productRepository = productRepository;
         this.menuRepository = menuRepository;
-        this.defaultPurgomalumClient = defaultPurgomalumClient;
+        this.purgomalumClient = purgomalumClient;
     }
 
     @Transactional
@@ -38,7 +39,7 @@ public class ProductService {
             throw new IllegalArgumentException();
         }
         final String name = request.getName();
-        if (Objects.isNull(name) || defaultPurgomalumClient.containsProfanity(name)) {
+        if (Objects.isNull(name) || purgomalumClient.containsProfanity(name)) {
             throw new IllegalArgumentException();
         }
         final Product product = new Product();
