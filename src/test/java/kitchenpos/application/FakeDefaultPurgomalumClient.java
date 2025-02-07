4e643723808b23b2d84478af6ff7ede6e16bb16d
leewonsuk <tjdjdjrdl123@naver.com>
2025-02-08 02:05:18 +0900

    refactor: 가짜객체 수정

--- src/test/java/kitchenpos/application/FakeDefaultPurgomalumClient.java
+++ src/test/java/kitchenpos/application/FakeDefaultPurgomalumClient.java
@@ -15,6 +15,6 @@ public class FakeDefaultPurgomalumClient implements PurgomalumClient {
     @Override
     public boolean containsProfanity(final String text) {
         return profanities.stream()
-            .anyMatch(profanity -> text.contains(profanity));
+            .anyMatch(text::contains);
     }
 }
