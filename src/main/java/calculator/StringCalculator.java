2420acdadcc059a79ddab9616a05d1b3e862e566
sunggyun-jo <sunggyun.jo@gmail.com>
2025-02-10 21:34:24 +0900

    feat(calculator):  커스텀 구분자를 지정할 수 있다.

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
 
     public int add(String text) {
@@ -10,6 +13,11 @@ public class StringCalculator {
     }
 
     private String[] parse(String text) {
+        Matcher m = Pattern.compile("//(.)\n(.*)").matcher(text);
+        if (m.find()) {
+            String customDelimiter = m.group(1);
+            return m.group(2).split(customDelimiter);
+        }
         return text.split("[,:]");
     }
 
