039efed216a8811b5ef1ae8cd3765b5f09b591bd
SungGyun Jo <62526969+sunggyun-jo@users.noreply.github.com>
2025-02-19 23:28:49 +0900

    0단계 - JUnit 5 학습 (#851)
    * feat(racingcar): 자동차 이름은 5 글자를 넘을 수 없다. 5글자가 넘는 경우, IllegalArgumentException이 발생한다.
    
    * feat(racingcar): 자동차가 움직이는 조건은 0에서 9 사이의 무작위 값을 구한 후, 무작위 값이 4 이상인 경우이다.
    
    * refactor(racingcar): 자동차가 움직이는 조건을 테스트 가능하도록 movable 함수 변경

---
+++ src/test/java/racingcar/Car.java
@@ -0,0 +1,30 @@
+package racingcar;
+
+import java.util.Random;
+
+public class Car {
+
+    private String carName;
+
+    public Car(String carName) {
+        if (carName == null) {
+            return;
+        }
+
+        if (carName.length() > 5) {
+            throw new IllegalArgumentException();
+        }
+
+        this.carName = carName;
+    }
+
+    public boolean movable(int num) {
+        return num >= 4;
+    }
+
+    public boolean movable() {
+        Random random = new Random();
+        var num = random.nextInt(10);
+        return movable(num);
+    }
+}
---
+++ src/test/java/racingcar/CarTest.java
@@ -0,0 +1,26 @@
+package racingcar;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class CarTest {
+
+    @Test
+    @DisplayName("자동차 이름은 5글자를 넘을 수 없다.")
+    void throwsExceptionWhenCreateCar() {
+        assertThatThrownBy(() -> {
+            Car car = new Car("가나다라마바");
+        }).isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    @DisplayName("자동차는 무작위 값이 4이상인 경우 움직인다.")
+    void moving() {
+        Car car = new Car("가나다라마");
+        assertThat(car.movable(4)).isTrue();
+        assertThat(car.movable(3)).isFalse();
+    }
+}
