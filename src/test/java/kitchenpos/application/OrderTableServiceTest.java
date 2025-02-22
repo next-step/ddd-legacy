f2ad0ea63d79d2ae6682427cf37b1ddc0808cd9f
devifancy <fancy.junyongmoon@gmail.com>
2025-02-23 01:43:34 +0900

    test: Fake 객체와 InMemory를 활용하여 주문 테이블 서비스에 대한 테스트코드 수정

--- src/test/java/kitchenpos/application/OrderTableServiceTest.java
+++ src/test/java/kitchenpos/application/OrderTableServiceTest.java
@@ -1,51 +1,43 @@
 package kitchenpos.application;
 
-import kitchenpos.IntegrationTestSupport;
 import kitchenpos.domain.OrderRepository;
 import kitchenpos.domain.OrderTable;
 import kitchenpos.domain.OrderTableRepository;
-import static kitchenpos.fixtures.OrderTableFixtures.orderTableChange;
-import static kitchenpos.fixtures.OrderTableFixtures.orderTableClear;
-import static kitchenpos.fixtures.OrderTableFixtures.orderTableCreate;
-import static kitchenpos.fixtures.OrderTableFixtures.orderTableSit;
+import kitchenpos.fixtures.OrderTableFixtures;
+import static kitchenpos.fixtures.OrderTableFixtures.changeOrderTable;
+import static kitchenpos.fixtures.OrderTableFixtures.clearOrderTable;
+import static kitchenpos.fixtures.OrderTableFixtures.createOrderTable;
+import static kitchenpos.fixtures.OrderTableFixtures.sitOrderTable;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import org.junit.jupiter.api.AfterEach;
 import static org.junit.jupiter.api.Assertions.assertAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.UUID;
 
 
-@Service
-class OrderTableServiceTest extends IntegrationTestSupport {
+class OrderTableServiceTest {
 
-    @Autowired
     private OrderTableService orderTableService;
-
-    @Autowired
     private OrderTableRepository orderTableRepository;
-
-    @Autowired
     private OrderRepository orderRepository;
 
-    @AfterEach
-    void tearDown() {
-        orderTableRepository.deleteAllInBatch();
-        orderRepository.deleteAllInBatch();
+    @BeforeEach
+    void setUp() {
+        orderTableRepository = new InMemoryOrderTableRepository();
+        orderRepository = new InMemoryOrderRepository();
+        orderTableService = new OrderTableService(orderTableRepository, orderRepository);
     }
-
     @Test
     void 주문_테이블을_등록하면_손님_수는_0명이고_사용_가능_상태여야_한다() {
         // given
-        OrderTable expected = orderTableCreate("1번 테이블");
+        OrderTable expected = OrderTableFixtures.createOrderTable("1번 테이블");
 
         // when
         OrderTable actual = orderTableService.create(expected);
@@ -77,7 +69,7 @@ class OrderTableServiceTest extends IntegrationTestSupport {
     @Test
     void 주문_테이블에_손님이_앉으면_사용중_상태로_변경된다() {
         // given
-        OrderTable orderTable = orderTableSit(UUID.randomUUID(), "1번 테이블", false, 0);
+        OrderTable orderTable = sitOrderTable(UUID.randomUUID(), "1번 테이블", false, 0);
         OrderTable expected = orderTableRepository.save(orderTable);
 
         // when
@@ -101,7 +93,7 @@ class OrderTableServiceTest extends IntegrationTestSupport {
     @Test
     void 주문이_완료된_경우_주문_테이블을_정리할_수_있다() {
         // given
-        OrderTable orderTable = orderTableClear(UUID.randomUUID(), "1번 테이블", true, 4);
+        OrderTable orderTable = clearOrderTable(UUID.randomUUID(), "1번 테이블", true, 4);
         OrderTable expected = orderTableRepository.save(orderTable);
 
         // when
@@ -117,7 +109,7 @@ class OrderTableServiceTest extends IntegrationTestSupport {
     @Test
     void 주문_테이블의_손님_수를_변경할_수_있다() {
         // given
-        OrderTable orderTable = orderTableChange(UUID.randomUUID(), "1번 테이블", true, 4);
+        OrderTable orderTable = changeOrderTable(UUID.randomUUID(), "1번 테이블", true, 4);
         orderTableRepository.save(orderTable);
 
         // when
@@ -131,7 +123,7 @@ class OrderTableServiceTest extends IntegrationTestSupport {
     @Test
     void 손님_수가_0_미만이면_변경할_수_없다() {
         // given
-        OrderTable orderTable = orderTableChange(UUID.randomUUID(), "1번 테이블", true, 4);
+        OrderTable orderTable = changeOrderTable(UUID.randomUUID(), "1번 테이블", true, 4);
         orderTableRepository.save(orderTable);
 
         // when & then
@@ -144,7 +136,7 @@ class OrderTableServiceTest extends IntegrationTestSupport {
     @Test
     void 주문_테이블이_사용중이_아니면_손님_수를_변경할_수_없다() {
         // given
-        OrderTable orderTable = orderTableChange(UUID.randomUUID(), "1번 테이블", false, 4);
+        OrderTable orderTable = changeOrderTable(UUID.randomUUID(), "1번 테이블", false, 4);
         orderTableRepository.save(orderTable);
 
         // when & then
@@ -157,9 +149,9 @@ class OrderTableServiceTest extends IntegrationTestSupport {
     @Test
     void 등록된_주문_테이블을_모두_조회할_수_있다() {
         // given
-        OrderTable orderTable1 = orderTableCreate(UUID.randomUUID(),"1번 테이블");
+        OrderTable orderTable1 = createOrderTable(UUID.randomUUID(),"1번 테이블");
         orderTableRepository.save(orderTable1);
-        OrderTable orderTable2 = orderTableCreate(UUID.randomUUID(),"2번 테이블");
+        OrderTable orderTable2 = createOrderTable(UUID.randomUUID(),"2번 테이블");
         orderTableRepository.save(orderTable2);
 
         // when
--- src/test/java/kitchenpos/fixtures/OrderTableFixtures.java
+++ src/test/java/kitchenpos/fixtures/OrderTableFixtures.java
@@ -6,29 +6,20 @@ import java.util.UUID;
 
 public class OrderTableFixtures {
 
-    public static OrderTable orderTableCreate(final String name) {
+    public static OrderTable createOrderTable(final String name) {
         OrderTable orderTable = new OrderTable();
         orderTable.setName(name);
         return orderTable;
     }
 
-    public static OrderTable orderTableCreate(final UUID uuid, final String name) {
-        OrderTable orderTable = new OrderTable();
-        orderTable.setId(uuid);
-        orderTable.setName(name);
-        return orderTable;
-    }
-
-    public static OrderTable orderTableSit(final UUID uuid, final String name, final boolean occupied, final int numberOfGuests) {
+    public static OrderTable createOrderTable(final UUID uuid, final String name) {
         OrderTable orderTable = new OrderTable();
         orderTable.setId(uuid);
         orderTable.setName(name);
-        orderTable.setOccupied(occupied);
-        orderTable.setNumberOfGuests(numberOfGuests);
         return orderTable;
     }
 
-    public static OrderTable orderTableClear(final UUID uuid, final String name, final boolean occupied, final int numberOfGuests) {
+    public static OrderTable sitOrderTable(final UUID uuid, final String name, final boolean occupied, final int numberOfGuests) {
         OrderTable orderTable = new OrderTable();
         orderTable.setId(uuid);
         orderTable.setName(name);
@@ -37,12 +28,33 @@ public class OrderTableFixtures {
         return orderTable;
     }
 
-    public static OrderTable orderTableChange(final UUID uuid, final String name, final boolean occupied, final int numberOfGuests) {
+    public static OrderTable clearOrderTable(final UUID uuid, final String name, final boolean occupied, final int numberOfGuests) {
         OrderTable orderTable = new OrderTable();
         orderTable.setId(uuid);
         orderTable.setName(name);
         orderTable.setOccupied(occupied);
         orderTable.setNumberOfGuests(numberOfGuests);
+        return orderTable;
+    }
+
+    public static OrderTable changeOrderTable(final UUID uuid, final String name, final boolean occupied, final int numberOfGuests) {
+        OrderTable orderTable = new OrderTable();
+        orderTable.setId(uuid);
+        orderTable.setName(name);
+        orderTable.setOccupied(occupied);
+        orderTable.setNumberOfGuests(numberOfGuests);
+        return orderTable;
+    }
+
+    /**
+     * OrderServiceTest
+     */
+    public static OrderTable orderTable(final boolean occupied, final int numberOfGuests) {
+        final OrderTable orderTable = new OrderTable();
+        orderTable.setId(UUID.randomUUID());
+        orderTable.setName("1번");
+        orderTable.setNumberOfGuests(numberOfGuests);
+        orderTable.setOccupied(occupied);
         return orderTable;
     }
 }
